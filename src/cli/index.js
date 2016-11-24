f9c4a09509c69b777dbaa2558651cacefbcaaa2d
Ran Mizrahi <ran@cocycles.com>
2016-11-24 13:46:59 +0200

    added readme file

---
+++ src/cli/index.js
@@ -0,0 +1,8 @@
+/** @flow */
+import buildRegistrar from './command-registrar-builder';
+import CommandRegistrar from './command-registrar';
+
+export default {
+  buildRegistrar,
+  CommandRegistrar
+};
