fc6e2d396c93c1c6bf55255737a0b5348b9c61b6
Ran Yitzhaki <rany@cocycles.com>
2016-12-01 17:08:11 +0200

    add bit not found exception

--- src/cli/default-error-handler.js
+++ src/cli/default-error-handler.js
@@ -1,12 +1,17 @@
 // all errors that the command does not handle comes to this switch statement
 // if you handle the error, then return true
 import BoxNotFound from '../box/exceptions/box-not-found';
+import BitNotFound from '../bit/exceptions/bit-not-found';
 
 const chalk = require('chalk');
 
 export default (err: Error): boolean => {
   if (err instanceof BoxNotFound) {
-    return chalk.red('box not found. to create a new box, please use `box init`');
+    return chalk.red('box not found. to create a new box, please use `bit init`');
+  }
+
+  if (err instanceof BitNotFound) {
+    return chalk.red('bit not found. to create a new bit, please use `bit create {bitName}`');
   }
 
   return null;
