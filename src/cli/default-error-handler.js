96dce5f575df5ccf5ba9362ba9040c124c9a195a
Ran Yitzhaki <rany@cocycles.com>
2016-12-01 17:19:43 +0200

    add already exist error

--- src/cli/default-error-handler.js
+++ src/cli/default-error-handler.js
@@ -2,6 +2,7 @@
 // if you handle the error, then return true
 import BoxNotFound from '../box/exceptions/box-not-found';
 import BitNotFound from '../bit/exceptions/bit-not-found';
+import BitAlreadyExist from '../bit/exceptions/bit-already-exist';
 
 const chalk = require('chalk');
 
@@ -13,6 +14,10 @@ export default (err: Error): boolean => {
   if (err instanceof BitNotFound) {
     return chalk.red('bit not found. to create a new bit, please use `bit create {bitName}`');
   }
+
+  if (err instanceof BitAlreadyExist) {
+    return chalk.red(`bit ${err.bitName} already exists!`);
+  } 
 
   return null;
 };
