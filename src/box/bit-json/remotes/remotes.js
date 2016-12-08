25168b13e60775f0fc2e1abc7587f21ab2832e38
Ran Mizrahi <ran@cocycles.com>
2016-12-08 10:38:50 +0200

    converted remotes to a map

--- src/box/bit-json/remotes/remotes.js
+++ src/box/bit-json/remotes/remotes.js
@@ -3,13 +3,13 @@ import Remote from './remote';
 import { forEach, prependBang } from '../../../utils';
 import { PrimaryOverloaded } from '../exceptions';
 
-export default class Remotes extends Array<Remote> {
-  constructor(remotes: Remote[] = []) {
-    super(...remotes);
+export default class Remotes extends Map<string, Remote> {
+  constructor(remotes: [string, Remote][] = []) {
+    super(remotes);
   }
 
   validate() {
-    const primary = this.filter(remote => remote.primary);
+    const primary = this.values.filter(remote => remote.primary);
     if (primary.length > 1) throw new PrimaryOverloaded();
     return this.forEach(remote => remote.validate());
   }
@@ -30,7 +30,8 @@ export default class Remotes extends Array<Remote> {
     const models = [];
     
     forEach(remotes, (host, alias) => {
-      models.push(Remote.load(alias, host));
+      const remote = Remote.load(alias, host); 
+      models.push([remote.alias, remote]);
     });
 
     return new Remotes(models);
