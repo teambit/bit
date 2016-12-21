674315ec08bb55a6709f1bbf152936eb903bf81f
Eyal Segal <eyal@cocycles.com>
2016-12-21 16:18:12 +0200

    updated default ssh private key path

--- src/network/ssh/key-getter.js
+++ src/network/ssh/key-getter.js
@@ -4,7 +4,7 @@ import * as fs from 'fs';
 const userHome = require('user-home');
 
 function composeDefaultPath() {
-  return `${userHome}/.ssh/id_rsa.pub`;
+  return `${userHome}/.ssh/id_rsa`;
 }
 
 export default function keyGetter(keyPath: ?string) {
