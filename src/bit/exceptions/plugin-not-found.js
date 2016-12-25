751568b4b9b63001174d87368d5677ec3519a65d
Ran Yitzhaki <rany@cocycles.com>
2016-12-25 18:15:58 +0200

    fix plugin-not-found exception

--- src/bit/exceptions/plugin-not-found.js
+++ src/bit/exceptions/plugin-not-found.js
@@ -1,5 +1,5 @@
 /** @flow */
-export default class CompilerNotFoundException extends Error {
+export default class PluginNotFoundException extends Error {
   plugin: string;
     
   constructor(plugin : string) {
