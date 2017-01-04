d58c7577e74e0f24b61be82739d52603437e5940
Amit <amit@cocycles.com>
2017-01-04 12:35:53 +0200

    excluded distribution from tar script

--- scripts/build-tar.sh
+++ scripts/build-tar.sh
@@ -12,5 +12,5 @@ cd ..
 #set package json with corret packeing type
 packageDest=$(cd $(dirname "$1") && pwd -P)/$(basename "package.json")
 node ./scripts/set-installation-method.js $packageDest tar
-tar --exclude='./Jenkinsfile' --exclude='./scripts/' -zcvf ${tarName} * 
+tar --exclude='./Jenkinsfile' --exclude='./distribution' --exclude='./scripts/' -zcvf ${tarName} *
 shasum -a 256 $tarName
