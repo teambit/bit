54f0a757c498ad34e75086790e9b454c48eb9345
Amit <amit@cocycles.com>
2017-01-10 13:54:33 +0200

    removed test in bit.rb

--- scripts/bit.rb
+++ scripts/bit.rb
@@ -12,16 +12,4 @@ class Bit < Formula
     bin.install_symlink Dir["#{libexec}/bin/*"]# if this fails, try separate make/make install steps
   end
 
-  test do
-    # `test do` will create, run in and delete a temporary directory.
-    #
-    # This test will fail and we won't accept that! It's enough to just replace
-    # "false" with the main program this formula installs, but it'd be nice if you
-    # were more thorough. Run the test with `brew test bit`. Options passed
-    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
-    #
-    # The installed folder is not in the path, so use the entire path to any
-    # executables being tested: `system "#{bin}/program", "do", "something"`.
-    system "false"
-  end
 end
