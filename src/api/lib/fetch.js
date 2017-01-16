6c97d576f17d98893494dffe1c54459ab9b079cc
Ran Mizrahi <ran@cocycles.com>
2017-01-16 17:28:17 +0200

    working fetch

--- src/api/lib/fetch.js
+++ src/api/lib/fetch.js
@@ -1,7 +1,8 @@
 /** @flow */
 import { loadScope } from '../../scope';
+import { BitIds } from '../../bit-id';
 
 export default function fetch(path: string, ids: string[]) {
   return loadScope(path)
-    .then(scope => scope.getManyObjects(ids));
+    .then(scope => scope.getManyObjects(BitIds.deserialize(ids)));
 }
