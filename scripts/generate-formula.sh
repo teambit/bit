2fbaf1003f6eb02fee042676ea8354f2a57a3638
Amit <amit@cocycles.com>
2017-01-17 14:22:07 +0200

    fixed upload to jfrog + brew

--- scripts/generate-formula.sh
+++ scripts/generate-formula.sh
@@ -6,4 +6,4 @@ set -ex
 rm -rf ./distribution/bit.rb
 mkdir -p ./distribution
 SHA=$( curl -s $1 |shasum -b -a 256| cut -d ' ' -f 1 )
-sed  's#sha256 ""#sha256 "'${SHA}'"#' bit.rb | sed  's#url ""#url "'$1'"#' > ./distribution/bit.rb
+sed  's#sha256 ""#sha256 "'${SHA}'"#' ./scripts/bit.rb | sed  's#url ""#url "'$1'"#' > ./distribution/bit.rb
