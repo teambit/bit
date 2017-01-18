d7ace5f91d52169054a53a730bbf70134e303ace
Amit <amit@cocycles.com>
2017-01-18 14:36:03 +0200

    fixed rpm and deb installation

--- scripts/build-deb.sh
+++ scripts/build-deb.sh
@@ -15,7 +15,7 @@ PACKAGE_DIST=$(cd $(dirname "$1") && pwd -P)/distribution
 PACKAGE_TMPDIR=./tmp/linux
 VERSION=$(cat ./package.json | grep version | head -1 | awk -F: '{ print $2 }' | sed 's/[",]//g' | xargs echo -n)
 TARBALL_NAME=./bit-$VERSION.tar.gz
-BIT_PACKAGE_NAME=bit-$VERSION
+BIT_PACKAGE_NAME="bit_${VERSION}_all.deb"
 if [ ! -e $TARBALL_NAME ]; then
   echo "Hey! Listen! You need to run build-tar.sh first."
   exit 1
@@ -30,7 +30,7 @@ mkdir ./distribution
 mkdir -p $PACKAGE_TMPDIR/bit
 umask 0022 # Ensure permissions are correct (0755 for dirs, 0644 for files)
 tar zxf $TARBALL_NAME -C $PACKAGE_TMPDIR/bit
-PACKAGE_TMPDIR_ABSOLUTE=$(cd $(dirname "$1") && pwd -P)/$PACKAGE_TMPDIR
+PACKAGE_TMPDIR_ABSOLUTE=$(cd $(dirname "$1") && pwd -P)/tmp/linux
 
 
 # Create Linux package structure
@@ -39,27 +39,27 @@ mkdir -p ./usr/share/bit/
 mv bit/* ./usr/share/bit/
 rm -rf ./bit
 
-
+pwd
 # Common FPM parameters for all packages we'll build using FPM
-FPM="fpm --input-type dir --chdir ./usr --name bit --version $VERSION "`
+FPM="fpm --input-type dir --chdir . --name bit --version $VERSION "`
   `"--vendor 'Cocycles, LTD <team@cocycles.com>' --maintainer 'Cocycles, LTD <team@cocycles.com>' "`
   `"--url https://www.bitsrc.io  --description 'Bit - Distributed Code Component Manager' --after-install ../../scripts/linux/postInstall.sh "
 
 #### Build DEB (Debian, Ubuntu) package
 node ../../scripts/set-installation-method.js $PACKAGE_TMPDIR_ABSOLUTE/usr/share/bit/package.json deb
-eval "$FPM --output-type deb  --architecture noarch --depends nodejs -p $PACKAGE_DIST --category 'Development/Languages' ."
-mv $PACKAGE_DIST/*.deb $PACKAGE_DIST/$BIT_PACKAGE_NAME'.deb'
+eval "$FPM --output-type deb  --architecture noarch -p ../../distribution/ --depends nodejs --category 'Development/Languages' ."
+#mv $PACKAGE_DIST/$BIT_PACKAGE_NAME $PACKAGE_DIST/
 
 
 
 # Common FPM parameters for all packages we'll build using FPM
-FPM="fpm --input-type dir --chdir ./usr --name bit --version $VERSION "`
+FPM="fpm --input-type dir --chdir . --name bit --version $VERSION "`
   `"--vendor 'Cocycles, LTD <team@cocycles.com>' --maintainer 'Cocycles, LTD <team@cocycles.com>' "`
   `"--url https://www.bitsrc.io  --description 'Bit - Distributed Code Component Manager' --after-install ../../scripts/linux/postInstall.sh "
 
-#### Build DEB (Debian, Ubuntu) package
+#### Build RPM (Debian, Ubuntu) package
 node ../../scripts/set-installation-method.js $PACKAGE_TMPDIR_ABSOLUTE/usr/share/bit/package.json deb
-eval "$FPM --output-type rpm  --architecture noarch --depends nodejs -p $PACKAGE_DIST --category 'Development/Languages' ."
+eval "$FPM --output-type rpm  --architecture noarch -p ../../distribution/ --depends nodejs  --category 'Development/Languages' ."
 #mv $PACKAGE_DIST/*.rpm $PACKAGE_DIST/
 
 
