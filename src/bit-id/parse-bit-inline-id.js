3fad9378fdf07f247e1f0dde8ba11667d667659c
Ran Yitzhaki <rany@cocycles.com>
2017-01-19 15:02:01 +0200

    fixed bug with box parsing

--- src/bit-id/parse-bit-inline-id.js
+++ src/bit-id/parse-bit-inline-id.js
@@ -2,16 +2,15 @@ const { ID_DELIMITER, DEFAULT_BOXNAME } = require('../constants');
 
 module.exports = (bitId) => {
   const splitted = bitId.split(ID_DELIMITER);
-  let bitName;
   let boxName;
+  let bitName;
 
   if (splitted.length === 1) {
     bitName = splitted[0];
     boxName = DEFAULT_BOXNAME;
   } else if (splitted.length === 2) {
-    bitName = splitted[0];
-    boxName = splitted[1];
+    bitName = splitted[1];
+    boxName = splitted[0];
   } else throw new Error('bit id must consist of "name" or "box/name" pattern');
-
   return { bitName, boxName };
 };
--- src/strategies/load-bit-assuming-only-one.js
+++ src/strategies/load-bit-assuming-only-one.js
@@ -5,7 +5,6 @@ const resolveBit = require('../bit-resolver');
 module.exports = (consumer, boxName, bitName) => {
   const directoryToLookIn = path.join(consumer.getBitsDir(), boxName, bitName);
   const optionalBits = glob.sync(`${directoryToLookIn}/*/*`);
-
   if (optionalBits.length === 1) {
     return resolveBit(optionalBits[0]);
   }
