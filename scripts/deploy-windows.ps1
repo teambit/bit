40f7635b2186310495d121543a7c5de3930dc86d
Amit <amit@cocycles.com>
2017-01-24 14:59:18 +0200

    added deploy script for nupkg + msi

---
+++ scripts/deploy-windows.ps1
@@ -0,0 +1,25 @@
+$VERSION= $(node -p -e "require('./package.json').version")
+$repoUrl = "http://104.154.76.155:8081/artifactory"
+$AF_USER = $env:repoUser 
+$AF_PWD = ConvertTo-SecureString "$env:repoPassword" -AsPlainText -Force  
+$CREDS = New-Object System.Management.Automation.PSCredential ($AF_USER, $AF_PWD)  
+
+$URI = New-Object System.Uri("${repoUrl}/bit-msi/bit-${VERSION}-unsigned.msi")  
+$SOURCE = "artifacts\bit-${VERSION}-unsigned.msi"  
+Invoke-WebRequest -Uri $URI -InFile $SOURCE -Method Put -Credential $CREDS
+
+$URI = New-Object System.Uri("${repoUrl}/bit-nuget/bit.${VERSION}.nupkg")  
+$SOURCE = "artifacts\bit.${VERSION}.nupkg"  
+Invoke-WebRequest -Uri $URI -InFile $SOURCE -Method Put -Credential $CREDS
+
+$body = @{
+    version="$VERSION"
+    method='msi'
+    file="$repoUrl/bit-msi/bit-${VERSION}-unsigned.msi"
+}
+$AF_USER = "$env:releaseUser"  
+$AF_PWD = ConvertTo-SecureString "$env:releasePassword" -AsPlainText -Force  
+$CREDS = New-Object System.Management.Automation.PSCredential ($AF_USER, $AF_PWD)  
+
+$json = $body | ConvertTo-Json
+Invoke-RestMethod 'https://api-stg.bitsrc.io/release/update' -Method Put -Body $json -ContentType 'application/json' -Credential $CREDS
