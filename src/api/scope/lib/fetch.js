472f5306b2a50a2b76de6bb18182f3727555e081
Ran Mizrahi <ran@cocycles.com>
2017-01-25 11:22:29 +0200

    fetch: default value

--- src/api/scope/lib/fetch.js
+++ src/api/scope/lib/fetch.js
@@ -2,7 +2,7 @@
 import { loadScope } from '../../../scope';
 import { BitIds } from '../../../bit-id';
 
-export default function fetch(path: string, ids: string[], noDependencies: bool) {
+export default function fetch(path: string, ids: string[], noDependencies: bool = false) {
   return loadScope(path)
     .then((scope) => {
       const bitIds = BitIds.deserialize(ids);
