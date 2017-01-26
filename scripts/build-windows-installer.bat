514f8808551ba43d641dd82d13aaec374d2d69f4
Amit <amit@cocycles.com>
2017-01-26 17:15:08 +0200

    add removal of posix dependencie for windows build

--- scripts/build-windows-installer.bat
+++ scripts/build-windows-installer.bat
@@ -1,1 +1,3 @@
+mv .\package.json .\package.json.bak
+Get-Content .\package.json.bak | Where-Object {$_ -nomatch 'posix'} | Set-Content package.json
 "%ProgramFiles(x86)%\MSBuild\14.0\Bin\MSBuild.exe" ./scripts/windows/BitSetup.wixproj /p:Configuration=Release
