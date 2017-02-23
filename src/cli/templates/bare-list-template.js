bba9ad3f0aad6d0333cf98ec236782e35e9797b2
Ran Yitzhaki <rany@cocycles.com>
2017-02-23 12:14:54 +0200

    remove flow error

--- src/cli/templates/bare-list-template.js
+++ src/cli/templates/bare-list-template.js
@@ -3,6 +3,7 @@ import ConsumerComponent from '../../consumer/component/consumer-component';
 
 export default (components: ConsumerComponent[]) => {
   function paintBareComponent(component) {
+    // $FlowFixMe
     return `${component.scope}/${component.box}/${component.name}::${component.version}`;
   }
 
