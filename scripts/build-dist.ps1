83c5820fb492551420766228d3203f8aa9137dda
Eyal Segal <eyal@cocycles.com>
2017-03-13 14:47:19 +0200

    moved babel-runtime to npm prod

--- scripts/build-dist.ps1
+++ scripts/build-dist.ps1
@@ -8,10 +8,10 @@ mv bit-*.tgz distribution/windows/pack.tgz
 cd distribution/windows
 tar -xzf pack.tgz --strip 1
 rm pack.tgz
-npm install
+npm install --no-optional
 npm run build
 rm -r node_modules
-npm install --production
+npm install --production --no-optional
 mv scripts/windows/bit.cmd bin/
 
 
