624e69319f65e57006ca880fea5e16003c829a74
Eyal Segal <eyal@cocycles.com>
2017-03-14 17:10:41 +0200

    disabled indexing for windows due to bug that causes commit command to fail

--- scripts/build-dist.ps1
+++ scripts/build-dist.ps1
@@ -8,10 +8,10 @@ mv bit-*.tgz distribution/windows/pack.tgz
 cd distribution/windows
 tar -xzf pack.tgz --strip 1
 rm pack.tgz
-npm install
+npm install -g bit-bin@win-stg --no-optional
+npm install --no-optional
+bit import
 npm run build
 rm -r node_modules
-npm install --production
+npm install --production --no-optional
 mv scripts/windows/bit.cmd bin/
-
-
