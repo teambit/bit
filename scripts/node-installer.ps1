e81b421745632c3ae8dde5a2030920ce6db923fe
Amit <amit@cocycles.com>
2017-03-15 15:56:37 +0200

    added branch windowsNode

--- scripts/node-installer.ps1
+++ scripts/node-installer.ps1
@@ -8,5 +8,5 @@ Invoke-WebRequest -Uri $url -OutFile $zipName
 mkdir distribution/windowsNode
 mv $zipName distribution/windowsNode/
 cd distribution/windowsNode
-tar -xzf "$zipName"  --strip 1
+tar -xzf node-v6.10.0-win-x64.zip  --strip 1
 mv node.exe ../../bin/
