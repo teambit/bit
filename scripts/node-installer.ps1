0b2954b5793f655789d7e7b38efc30686584e8a5
Amit <amit@cocycles.com>
2017-03-15 16:52:29 +0200

    wget4

--- scripts/node-installer.ps1
+++ scripts/node-installer.ps1
@@ -1,11 +1,11 @@
 $url = "https://nodejs.org/dist/v6.10.0/node-v6.10.0-win-x64.zip"
-$zipName = "node-v6.10.0-win-x64.zip"
+$zipName = "$PSScriptRoot/../node-v6.10.0-win-x64.zip"
 
 Invoke-WebRequest $url -OutFile $zipName
 
 mkdir distribution
 mkdir distribution/windowsNode
-mv node-v6.10.0-win-x64.zip distribution/windowsNode/
+mv "$PSScriptRoot/../node-v6.10.0-win-x64.zip" "$PSScriptRoot/../distribution/windowsNode/"
 cd distribution/windowsNode
 tar -xzf node-v6.10.0-win-x64.zip  --strip 1
 mv node.exe ../../bin/
