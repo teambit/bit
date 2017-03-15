e855150ed95448771a93ab1d51a846e93c4668a1
Amit <amit@cocycles.com>
2017-03-15 16:34:08 +0200

    wget2

--- scripts/node-installer.ps1
+++ scripts/node-installer.ps1
@@ -1,9 +1,8 @@
 $url = "https://nodejs.org/dist/v6.10.0/node-v6.10.0-win-x64.zip"
 $zipName = "$PSScriptRoot/../node-v6.10.0-win-x64.zip"
 
+Invoke-WebRequest $url -OutFile $zipName
 
-$client = New-Object System.Net.WebClient
-$client.DownloadFile($url, $zipName)
 mkdir distribution
 mkdir distribution/windowsNode
 mv node-v6.10.0-win-x64.zip distribution/windowsNode/
