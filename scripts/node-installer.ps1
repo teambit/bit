d5006a548e432674c16c1c81f400f5d35d9a5dc1
Amit <amit@cocycles.com>
2017-03-15 17:13:56 +0200

    xxx

--- scripts/node-installer.ps1
+++ scripts/node-installer.ps1
@@ -6,7 +6,7 @@ appveyor DownloadFile https://nodejs.org/dist/v6.10.0/node-v6.10.0-win-x64.zip  
 mkdir distribution
 mkdir distribution/windowsNode
 mv $PSScriptRoot/"../node-v6.10.0-win-x64.zip" $PSScriptRoot/"../distribution/windowsNode/"
-cd distribution/windowsNode
+cd $PSScriptRoot"/../distribution/windowsNode"
 tar -xzf node-v6.10.0-win-x64.zip  --strip 1
 mv node.exe ../../bin/
 cd ../../
