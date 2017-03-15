e6f7f309c96a9b1fbcc48f56db76fd406075d779
Amit <amit@cocycles.com>
2017-03-15 16:07:22 +0200

    removed node-installer.sh

--- scripts/node-installer.ps1
+++ scripts/node-installer.ps1
@@ -5,8 +5,11 @@ if (Test-Path distribution/windowsNode) {
   rm distribution/windowsNode -Recurse
 }
 Invoke-WebRequest -Uri $url -OutFile $zipName
+mkdir distribution
 mkdir distribution/windowsNode
 mv $zipName distribution/windowsNode/
 cd distribution/windowsNode
 tar -xzf node-v6.10.0-win-x64.zip  --strip 1
 mv node.exe ../../bin/
+cd ../../
+rm distribution -Recurse
