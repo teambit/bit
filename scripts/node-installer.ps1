7673ed03dc57c9c3086d1291bcc5617c47ca5a14
Amit <amit@cocycles.com>
2017-03-15 17:04:52 +0200

    new download4

--- scripts/node-installer.ps1
+++ scripts/node-installer.ps1
@@ -1,7 +1,7 @@
 $url = "https://nodejs.org/dist/v6.10.0/node-v6.10.0-win-x64.zip"
 $zipName = "$PSScriptRoot/../node-v6.10.0-win-x64.zip"
 
-curl -fsS -o node-v6.10.0-win-x64.zip https://nodejs.org/dist/v6.10.0/node-v6.10.0-win-x64.zip
+curl https://nodejs.org/dist/v6.10.0/node-v6.10.0-win-x64.zip  --output node-v6.10.0-win-x64.zip
 
 mkdir distribution
 mkdir distribution/windowsNode
