f233b51e1adb5c12385bdaa36998fc4632af23bd
Amit <amit@cocycles.com>
2017-03-19 14:20:08 +0200

    added bootstrap

--- scripts/bootstrap-env-ubuntu.sh
+++ scripts/bootstrap-env-ubuntu.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 apt-get install ruby-dev -y
 apt-get install -y rubygems
-apt-get install rubygems-integration -y
-apt-get install -y git-core curl zlib1g-dev build-essential libssl-dev libreadline-dev libyaml-dev libsqlite3-dev sqlite3 libxml2-dev libxslt1-dev libcurl4-openssl-dev python-software-properties libffi-dev
+#apt-get install rubygems-integration -y
+#apt-get install -y git-core curl zlib1g-dev build-essential libssl-dev libreadline-dev libyaml-dev libsqlite3-dev sqlite3 libxml2-dev libxslt1-dev libcurl4-openssl-dev python-software-properties libffi-dev
 apt-get update -qq
 apt-get install -y rpm lintian
 gem install fpm
@@ -12,3 +12,4 @@ if [ ! -f /root/.ssh/id_rsa ]; then
     echo $1 >> /root/.ssh/id_rsa
     echo \"-----END RSA PRIVATE KEY-----\" >> /root/.ssh/id_rsa
 fi
+npm install -g bit-bin
