e7daac5ff4f3da4ae5d7a04216d041ae4721788f
Eyal Segal <eyal@cocycles.com>
2017-03-23 18:07:04 +0200

    compatible with old bit version

--- src/cli/commands/private-cmds/_put-cmd.js
+++ src/cli/commands/private-cmds/_put-cmd.js
@@ -5,13 +5,13 @@ import { fromBase64, toBase64 } from '../../../utils';
 import { put } from '../../../api/scope';
 
 export default class Put extends Command {
-  name = '_put <path>';
+  name = '_put <path> [objects]';
   private = true;
   description = 'upload a component to a scope';
   alias = '';
   opts = [];
   
-  action([path, objects, ]: [ string, ?string, ?string ]): Promise<any> {
+  action([path, objects, ]: [ string, string, string, ]): Promise<any> {
     if (objects) return put({
       componentObjects: fromBase64(objects),
       path: fromBase64(path)
