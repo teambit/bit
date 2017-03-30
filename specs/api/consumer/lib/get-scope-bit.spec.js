e42f086fe4ece4d565f0c092a7842e423ad30cb7
David First <david@cocycles.com>
2017-03-30 14:11:49 -0400

    add unit-test for "bit show" outside a scope

---
+++ specs/api/consumer/lib/get-scope-bit.spec.js
@@ -0,0 +1,28 @@
+import { expect } from 'chai';
+import sinon from 'sinon';
+import getScopeBit from '../../../../src/api/consumer/lib/get-scope-bit';
+import { ConsumerNotFound } from '../../../../src/consumer/exceptions';
+import { ScopeNotFound } from '../../../../src/scope/exceptions';
+import * as consumer from '../../../../src/consumer';
+import { GlobalRemotes } from '../../../../src/global-config';
+import Remotes from '../../../../src/remotes/remotes';
+import * as scope from '../../../../src/scope';
+
+describe('getScopeBit', () => {
+  it('should show a component when is running outside a scope', () => {
+    sinon.stub(consumer, 'loadConsumer').returns(Promise.reject(new ConsumerNotFound()));
+    sinon.stub(scope, 'loadScope').returns(Promise.reject(new ScopeNotFound()));
+    sinon.stub(GlobalRemotes, 'load').returns(Promise.resolve({ toPlainObject: () => {}}));
+    const showSpy = sinon.spy();
+    const resolveStub = sinon.stub(Remotes.prototype, 'resolve').returns(Promise.resolve({show: showSpy}));
+
+    const result = getScopeBit({id: 'my-scope/box/name'});
+    expect(result).to.be.a.Promise;
+
+    return result
+      .then(() => {
+        expect(resolveStub.getCall(0).args[0]).to.equal('my-scope');
+        expect(showSpy.called).to.be.true;
+      });
+  });
+});
