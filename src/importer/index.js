8b1dbfec58f65da60db38accbd37a5301e4d6645
Ran Yitzhaki <rany@cocycles.com>
2017-04-05 14:37:09 +0300

    add flow eslint

--- src/importer/index.js
+++ src/importer/index.js
@@ -1,41 +1,4 @@
-// @flow
-import R from 'ramda';
-// import importComponents from 'bit-scope-client';
-import componentsMock from './component-mock';
-import modelOnFs from './model-on-fs';
-import locateConsumer from '../consumer/locate-consumer';
-import BitJson from '../bit-json';
+import importer from './importer';
 
-const readIdsFromBitJson = consumerPath =>
-  new Promise((resolve, reject) => {
-    try {
-      const bitJson = BitJson.load(consumerPath);
-      const dependencies = bitJson.getDependenciesArray();
-      resolve(dependencies);
-    } catch (e) { reject(e); }
-  });
+module.exports = importer;
 
-function getIdsFromBitJsonIfNeeded(componentIds, consumerPath) {
-  return new Promise((resolve, reject) => {
-    if (!componentIds || R.isEmpty(componentIds)) {
-      return readIdsFromBitJson(consumerPath)
-      .then((ids) => {
-        if (!ids || R.isEmpty(ids)) return [];
-        return resolve(ids);
-      }).catch(reject);
-    }
-
-    return resolve(componentIds);
-  });
-}
-
-module.exports = (componentIds: string[]) => {
-  const currentDir = '/Users/ran/Projects/bit-scope-client' || process.cwd(); // TODO - replace with cwd this is mock
-  const consumerPath = locateConsumer(currentDir);
-
-  return getIdsFromBitJsonIfNeeded(componentIds, consumerPath)
-  .then((ids) => {
-    // return importComponents(ids); // mock - replace to the real importer
-    return Promise.resolve(componentsMock);
-  }).then(componentDependencies => modelOnFs(componentDependencies));
-};
