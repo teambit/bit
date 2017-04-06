127b47ee7ad56c7e4a55b4cc4166316c885d33d6
David First <davidnet@gmail.com>
2017-04-06 04:32:34 -0400

    enable running all inline components tests when not specifying a bit ID (#118)
    * enable running all inline components tests when not specifying a bit ID
    
    * add unit tests for running all inline specs functionality

---
+++ specs/consumer/consumer.spec.js
@@ -0,0 +1,41 @@
+import { expect } from 'chai';
+import sinon from 'sinon';
+import Consumer from '../../src/consumer/consumer';
+import ConsumerComponent from '../../src/consumer/component/consumer-component';
+
+describe('Consumer', () => {
+  describe('runAllInlineSpecs', () => {
+    let sandbox;
+    beforeEach(() => {
+      sandbox = sinon.sandbox.create();
+    });
+    afterEach(() => {
+      sandbox.restore();
+    });
+    it('should return an empty array if there are no inline components', () => {
+      sandbox.stub(Consumer.prototype, 'listInline').returns(Promise.resolve([]));
+      const consumer = new Consumer({ projectPath: '' });
+      const result = consumer.runAllInlineSpecs();
+      return result
+        .then((data) => {
+          expect(data).to.be.an('Array');
+          expect(data.length).to.be.equal(0);
+        });
+    });
+    it('should return an array with results for all inline components', () => {
+      const consumerComponent1 = new ConsumerComponent({ name: 'component-name1', box: 'box'});
+      const consumerComponent2 = new ConsumerComponent({ name: 'component-name2', box: 'box'});
+      sandbox.stub(Consumer.prototype, 'listInline').returns(Promise.resolve([consumerComponent1, consumerComponent2]));
+
+      const consumer = new Consumer({ projectPath: '' });
+      const result = consumer.runAllInlineSpecs();
+      expect(result).to.be.a('Promise');
+      return result
+        .then((data) => {
+          expect(data).to.be.an('Array');
+          expect(data.length).to.be.equal(2);
+          data.map(result => expect(result).to.have.all.keys('specs', 'component'));
+        });
+    });
+  });
+});
--- src/cli/chalk-box.js
+++ src/cli/chalk-box.js
@@ -58,5 +58,15 @@ const paintStats = (results) => {
   return `${statsHeader}\n${totalDuration}\n`;
 };
 
-export const paintSpecsResults = (results: SpecsResults): string => (results.tests ?
-  paintStats(results) + results.tests.map(paintTest).join('\n') : '');
+export const paintSpecsResults = (results: SpecsResults): string => {
+  return paintStats(results) + results.tests.map(paintTest).join('\n');
+};
+
+export const paintAllSpecsResults = (results: Array<*>): string => {
+  if (results.length === 0) return c.red('There are no inline components to test');
+  return results.map((result) => {
+    const componentId = c.bold(`${result.component.box}/${result.component.name}: `);
+    if (result.specs) return componentId + paintSpecsResults(result.specs);
+    return `${componentId}couldn't get test results`;
+  }).join('\n');
+};
