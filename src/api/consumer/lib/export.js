9589202d542c0f885dc43c4d9963e3eef80608cf
David First <davidnet@gmail.com>
2017-04-13 04:11:20 -0400

    when exporting and missing @this, show friendly error (#117)
    * when exporting and missing @this, show friendly error
    
    * add unit-test for friendly error message when @this is forgotten while exporting a component
    
    * refactor - create an Error class for bit export without "@this"

--- src/api/consumer/lib/export.js
+++ src/api/consumer/lib/export.js
@@ -3,11 +3,20 @@ import { loadConsumer } from '../../../consumer';
 import ConsumerComponent from '../../../consumer/component/consumer-component';
 import loader from '../../../cli/loader';
 import { BEFORE_EXPORT } from '../../../cli/loader/loader-messages';
+import { ComponentNotFound } from '../../../scope/exceptions';
+import { LOCAL_SCOPE_NOTATION } from '../../../constants';
+import ExportWithoutThis from './exceptions/export-without-this';
 
 export default function exportAction(id: string, remote: string, save: ?bool) {
   return loadConsumer().then((consumer) => {
     loader.start(BEFORE_EXPORT);
     return consumer.exportAction(id, remote)
+    .catch((err) => {
+      if (err instanceof ComponentNotFound && !id.startsWith(LOCAL_SCOPE_NOTATION)) {
+        throw new ExportWithoutThis(id, remote);
+      }
+      throw err;
+    })
     .then((component: ConsumerComponent) => {
       if (save) {
         return consumer.bitJson.addDependency(component.id)
