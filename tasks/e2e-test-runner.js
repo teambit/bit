898011318922d3c3c63f67ceacffe9b8644621d5
Ran Yitzhaki <rany@cocycles.com>
2017-05-23 11:16:03 +0300

    gather the e2e's

--- tasks/e2e-test-runner.js
+++ tasks/e2e-test-runner.js
@@ -3,5 +3,5 @@ const gulp = require('gulp');
 const mocha = require('gulp-mocha');
 
 module.exports = () => gulp
-    .src('./tests/**/*.e2e.js')
+    .src('./e2e/**/*.e2e.js')
     .pipe(mocha({ reporter: 'nyan' }));
