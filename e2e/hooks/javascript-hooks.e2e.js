9b473cd34f7f8f4be9fde08c4ad1a3ce83dad783
David First <david@cocycles.com>
2017-05-23 18:28:00 -0400

    add e2e tests for bind components with dependencies

--- e2e/hooks/javascript-hooks.e2e.js
+++ e2e/hooks/javascript-hooks.e2e.js
@@ -217,6 +217,94 @@ describe('javascript-hooks', function () {
         expectLinksInRootLevel();
       });
     });
+
+    describe('with dependencies', () => {
+      before(() => {
+        helper.cleanEnv();
+        helper.runCmd('bit init');
+        helper.runCmd('bit create foo');
+        fs.writeFileSync(fooImplPath, fooComponentFixture);
+        helper.runCmd('bit commit foo commit-msg');
+
+        helper.runCmd('bit init --bare', helper.remoteScopePath);
+        helper.runCmd(`bit remote add file://${helper.remoteScopePath}`);
+        helper.runCmd(`bit export @this/global/foo @${helper.remoteScope}`);
+
+        const barComponentFixture = "const foo = require('bit/global/foo'); module.exports = function bar() { return 'got bar and ' + foo(); };";
+        const barImplPath = path.join(helper.localScopePath, 'inline_components', 'global', 'bar', 'impl.js');
+        helper.runCmd('bit create bar --json');
+        fs.writeFileSync(barImplPath, barComponentFixture);
+        const barJsonPath = path.join(helper.localScopePath, 'inline_components', 'global', 'bar', 'bit.json');
+        const barJson = JSON.parse(fs.readFileSync(barJsonPath).toString());
+        barJson.dependencies[`@${helper.remoteScope}/global/foo`] = '1';
+        fs.writeFileSync(barJsonPath, JSON.stringify(barJson, null, 4));
+        helper.runCmd('bit commit bar commit-msg');
+        helper.runCmd(`bit export @this/global/bar @${helper.remoteScope}`);
+      });
+      before(() => {
+        fs.emptyDirSync(helper.localScopePath); // a new local scope
+        helper.runCmd('bit init');
+        helper.runCmd(`bit remote add file://${helper.remoteScopePath}`);
+        helper.runCmd(`bit import @${helper.remoteScope}/global/bar`);
+      });
+      describe('of depth=1, "bar" depends on "foo"', () => {
+        it('should create links in the component level', () => {
+          const appJs = "const bar = require('bit/global/bar'); console.log(bar());";
+          fs.outputFileSync(path.join(helper.localScopePath, 'app.js'), appJs);
+          const result = helper.runCmd('node app.js');
+          expect(result.trim()).to.equal('got bar and got foo');
+        });
+        it('should create links in the namespace level', () => {
+          const appJs = "const bitGlobal = require('bit/global'); console.log(bitGlobal.bar());";
+          fs.outputFileSync(path.join(helper.localScopePath, 'app.js'), appJs);
+          const result = helper.runCmd('node app.js');
+          expect(result.trim()).to.equal('got bar and got foo');
+        });
+        it('should create links in the root level', () => {
+          const appJs = "const bit = require('bit'); console.log(bit.global.bar());";
+          fs.outputFileSync(path.join(helper.localScopePath, 'app.js'), appJs);
+          const result = helper.runCmd('node app.js');
+          expect(result.trim()).to.equal('got bar and got foo');
+        });
+      });
+      describe('of depth=2, "baz" depends on "bar" that depends on "foo"', () => {
+        before(() => {
+          const bazComponentFixture = "const bar = require('bit/global/bar'); module.exports = function baz() { return 'got baz and ' + bar(); };";
+          const bazImplPath = path.join(helper.localScopePath, 'inline_components', 'global', 'baz', 'impl.js');
+          helper.runCmd('bit create baz --json');
+          fs.writeFileSync(bazImplPath, bazComponentFixture);
+          const bazJsonPath = path.join(helper.localScopePath, 'inline_components', 'global', 'baz', 'bit.json');
+          const bazJson = JSON.parse(fs.readFileSync(bazJsonPath).toString());
+          bazJson.dependencies[`@${helper.remoteScope}/global/bar`] = '1';
+          fs.writeFileSync(bazJsonPath, JSON.stringify(bazJson, null, 4));
+          helper.runCmd('bit commit baz commit-msg');
+          helper.runCmd(`bit export @this/global/baz @${helper.remoteScope}`);
+
+          fs.emptyDirSync(helper.localScopePath); // a new local scope
+          helper.runCmd('bit init');
+          helper.runCmd(`bit remote add file://${helper.remoteScopePath}`);
+          helper.runCmd(`bit import @${helper.remoteScope}/global/baz`);
+        });
+        it('should create links in the component level', () => {
+          const appJs = "const baz = require('bit/global/baz'); console.log(baz());";
+          fs.outputFileSync(path.join(helper.localScopePath, 'app.js'), appJs);
+          const result = helper.runCmd('node app.js');
+          expect(result.trim()).to.equal('got baz and got bar and got foo');
+        });
+        it('should create links in the namespace level', () => {
+          const appJs = "const bitGlobal = require('bit/global'); console.log(bitGlobal.baz());";
+          fs.outputFileSync(path.join(helper.localScopePath, 'app.js'), appJs);
+          const result = helper.runCmd('node app.js');
+          expect(result.trim()).to.equal('got baz and got bar and got foo');
+        });
+        it('should create links in the root level', () => {
+          const appJs = "const bit = require('bit'); console.log(bit.global.baz());";
+          fs.outputFileSync(path.join(helper.localScopePath, 'app.js'), appJs);
+          const result = helper.runCmd('node app.js');
+          expect(result.trim()).to.equal('got baz and got bar and got foo');
+        });
+      });
+    });
   });
 });
 
