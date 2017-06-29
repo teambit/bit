afbdabf98f69591768432fe44f4b0b9dc97802f3
David First <david@cocycles.com>
2017-06-29 10:45:26 -0400

    change watch to work with the new bit.map (was bit.lock before)

--- src/api/consumer/lib/watch.js
+++ src/api/consumer/lib/watch.js
@@ -8,23 +8,22 @@ import logger from '../../../logger/logger';
 import ComponentsList from '../../../consumer/component/components-list';
 
 /**
- * Watch all components specified in bit.lock.
+ * Watch all components specified in bit.map.
  * Run buildAll for each change in the watched paths
- * 
+ *
  * @export
- * @param {boolean} verbose - showing verbose output for inspection 
- * @returns 
+ * @param {boolean} verbose - showing verbose output for inspection
+ * @returns
  */
 export async function watchAll(verbose) {
   // TODO: run build in the begining of process (it's work like this in other envs)
   const consumer = await loadConsumer();
   const consumerPath = consumer.getPath();
   const componentsList = new ComponentsList(consumer);
-  const bitlockComponents = await componentsList.getFromBitLock();
-  const bitlockComponentsValues = R.values(bitlockComponents);
+  const bitMapComponentsValues = await componentsList.idsFromBitMap();
   const addConsumerPath = x => consumerPath + '/' + x.path;
-  const bitlockComponentsPaths = R.map(addConsumerPath, bitlockComponentsValues);
-  const watcher = chokidar.watch(bitlockComponentsPaths, {
+  const bitMapComponentsPaths = R.map(addConsumerPath, bitMapComponentsValues);
+  const watcher = chokidar.watch(bitMapComponentsPaths, {
         ignoreInitial: true,
         ignored: '**/dist/**',
       });
@@ -33,13 +32,13 @@ export async function watchAll(verbose) {
   console.log(`Starting watch for changes`); // eslint-disable-line
 
   if (verbose){
-    // Print all watched paths 
-    bitlockComponentsPaths.forEach(path => console.log(`Watching ${path}`));
+    // Print all watched paths
+    bitMapComponentsPaths.forEach(path => console.log(`Watching ${path}`));
   }
 
   watcher
     .on('change', (p) => {
-      const log = console.log.bind(console); 
+      const log = console.log.bind(console);
       log(`File ${p} has been changed, calling build`);
       // TODO: Make sure the log for build is printed to console
       buildAll()
