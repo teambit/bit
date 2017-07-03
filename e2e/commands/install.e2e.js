ba822523a7151c7272035fe740454d013dfc3c97
David First <david@cocycles.com>
2017-07-03 15:41:44 -0400

    fix bit-install, fix e2e-tests of bit-install

--- e2e/commands/install.e2e.js
+++ e2e/commands/install.e2e.js
@@ -6,6 +6,7 @@ import { expect } from 'chai';
 import Helper from '../e2e-helper';
 
 const helper = new Helper();
+const fooComponentFixture = "module.exports = function foo() { return 'got foo'; };";
 
 describe('bit install command', function () {
   this.timeout(0);
@@ -16,23 +17,24 @@ describe('bit install command', function () {
     before(() => {
       helper.cleanEnv();
       helper.runCmd('bit init');
-      // export a new component "foo"
-      helper.runCmd('bit create foo');
-      helper.runCmd('bit commit foo commit-msg');
+      // export a new component "bar/foo"
+      fs.outputFileSync(path.join(helper.localScopePath, 'bar', 'foo.js'), fooComponentFixture);
+      helper.runCmd('bit add bar/foo.js');
+      helper.runCmd('bit commit bar/foo -m commit-msg');
       helper.runCmd('bit init --bare', helper.remoteScopePath);
       helper.runCmd(`bit remote add file://${helper.remoteScopePath}`);
-      helper.runCmd(`bit export @this/global/foo @${helper.remoteScope}`);
+      helper.runCmd(`bit export @${helper.remoteScope} bar/foo`);
       fs.emptyDirSync(helper.localScopePath); // a new local scope
       helper.runCmd('bit init');
       helper.runCmd(`bit remote add file://${helper.remoteScopePath}`);
       // add "foo" as a bit.json dependency
       const bitJsonPath = path.join(helper.localScopePath, 'bit.json');
-      helper.addBitJsonDependencies(bitJsonPath, { [`@${helper.remoteScope}/global/foo`]: '1' });
+      helper.addBitJsonDependencies(bitJsonPath, { [`@${helper.remoteScope}/bar/foo`]: '1' });
     });
     it('should display a successful message with the list of installed components', () => {
       const output = helper.runCmd('bit install');
       expect(output.includes('successfully imported the following Bit components')).to.be.true;
-      expect(output.includes('global/foo')).to.be.true;
+      expect(output.includes('bar/foo')).to.be.true;
     });
   });
 });
