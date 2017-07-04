31c04bfa8f0dd7de2d2ed7e558585603a54035a0
Amit <amit@cocycles.com>
2017-07-04 21:11:48 +0300

    added tests

--- __tests__/dependency-resloution/dependency.test.js
+++ __tests__/dependency-resloution/dependency.test.js
@@ -1,33 +1,23 @@
 import mockFs from 'mock-fs';
-import {getDependecyTree} from '../../src/dependency-builder';
+import { getDependecyTree } from '../../src/dependency-builder';
 const vm = jest.genMockFromModule('vm');
 jest.mock('vm');
 
-const bitJsonFixture = {
-  sources: {
-    impl: 'impl.js',
-    misc: [],
-  },
-  env: {
-    compiler: 'none',
-    tester: 'none',
-  },
-  dependencies: {},
-};
-
-
-describe('get dependency tree src/actions/bind.js', () => {
-  it('should return dependency tree for bind.js with correct (ramda,bit-scope-client', () => {
-    return getDependecyTree(process.cwd(), 'src/actions/bind.js').then((dependencies) => {
-      //console.log(tree)
-      expect(dependencies.missing).toEqual([]);
-      dependencies.tree.forEach(component =>{
-        if( component.component ==='src/actions/bind.js')
-          component.packages.forEach(dep =>{
-            if (dep.name  === 'bit-scope-client')  expect(dep.version).toEqual('0.6.3')
-            if (dep.name  === 'ramda')  expect(dep.version).toEqual('0.22.1')
-          })
-      })
+const fileOne = 'import test1 from \'./test2\';';
+const fileTwo = 'import test2 from \'npmPackage\';';
+beforeEach(() => {
+  mockFs({
+    'my/project/component': {
+      'test.js': fileOne,
+      'test2.js': fileTwo,
+    },
+  });
+});
+describe('getDependecyTree', () => {
+  it('should return missing dependency for component', () => {
+    return getDependecyTree(process.cwd(), 'my/project/component/test.js').then((dependencies) => {
+      expect(dependencies.missing).toEqual(['npmPackage']);
     });
   });
 });
+
