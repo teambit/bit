e0ad61979561400f8d1fbb56b5db36f5b26a6fa0
Gilad Shoham <shoham.gilad@gmail.com>
2017-07-04 11:17:02 +0300

    e2e test cases for commit

---
+++ e2e/commands/commit.e2e.js
@@ -0,0 +1,72 @@
+// covers also init, create, commit, import and export commands
+
+import path from 'path';
+import fs from 'fs-extra';
+import { expect } from 'chai';
+import Helper from '../e2e-helper';
+
+const helper = new Helper();
+
+describe('bit commit command', function () {
+  this.timeout(0);
+  after(() => {
+    helper.destroyEnv();
+  });
+  describe('commit one component', () => {
+    before(() => {
+    });
+
+    it('should throw error if the bit id not exists', () => {
+    });
+
+    it('should persist the model in the scope', () => {
+    });
+
+    it('should run the onCommit hook', () => {
+    });
+
+    describe('commit imported component', () => {
+      it('should index the component', () => {
+      });
+    });
+
+    describe('commit added component', () => {
+      it('should index the component', () => {
+      });
+
+      it('Should throw error if there is tracked files which not commited yet', () => {
+      });
+
+      it('should add the correct dependencies to each component', () => {
+      });
+    });
+  });
+
+  describe('commit all components', () => {
+
+    it('Should throw error if there is untracked files dependencies', () => {
+    });
+
+    // We throw this error because we don't know the packege version in this case
+    it('should throw error if there is missing package dependency', () => {
+    });
+
+    it('should index all components', () => {
+    });
+
+    it('should commit the components in the correct order', () => {
+    });
+
+    it('should add the correct dependencies to each component', () => {
+    });
+
+    it('should persist all models in the scope', () => {
+    });
+
+    it('should run the onCommit hook', () => {
+    });
+
+    it('', () => {
+    });
+  });
+});
