90e62e1d3b744ec81e2e89b10eed0b5076bcd629
Gilad Shoham <shoham.gilad@gmail.com>
2017-07-05 12:10:20 +0300

    linting errors

--- src/dependency-builder/build-tree.js
+++ src/dependency-builder/build-tree.js
@@ -1,3 +1,5 @@
+// TODO: This should be exported as a bit component
+
 // @flow
 import madge from 'madge';
 import Toposort from 'toposort-class';
@@ -9,7 +11,7 @@ import findPackage from 'find-package';
  * @param cwd
  * @return {Object}
  */
-const resloveNodePackages = (dependenciesObject: Object, cwd: String) => {
+const resloveNodePackages = (dependenciesObject: Object, cwd: string) => {
   const newDependencyObject:Object = {};
   Object.keys(dependenciesObject).forEach((key) => {
     const packagesArr = [];
@@ -31,15 +33,18 @@ const resloveNodePackages = (dependenciesObject: Object, cwd: String) => {
  * sortDependency- will sort depdency tree according to depndecies
  * @param tree
  * @param cwd
- * @return {Array.<*>}
+ * @return {Array<*>}
  */
 const sortDependency = (tree, cwd) => {
   const t:Toposort = new Toposort();
   const sortedDependencieArr:Array<*> = [];
   Object.keys(tree).forEach(key => t.add(key, tree[key].filter(item => !item.includes('node_modules'))));
-  const sortedTree:Array<String> = t.sort().reverse();
+  const sortedTree:string[] = t.sort().reverse();
   const treeWithResolvedDependencies = resloveNodePackages(tree, cwd);
-  sortedTree.forEach(key => sortedDependencieArr.push({ component: key,internDependencies: treeWithResolvedDependencies[key].internDependencies, packages: treeWithResolvedDependencies[key].packages }));
+  sortedTree.forEach(key => sortedDependencieArr.push({
+    component: key,
+    internDependencies: treeWithResolvedDependencies[key].internDependencies,
+    packages: treeWithResolvedDependencies[key].packages }));
   return sortedDependencieArr;
 };
 
@@ -48,9 +53,9 @@ const sortDependency = (tree, cwd) => {
  * Function for fetching dependecy tree of file or dir
  * @param cwd
  * @param filePath
- * @return {Promise.<{missing, tree}>}
+ * @return {Promise<{missing, tree}>}
  */
-export default function getDependecyTree(cwd: String, filePath: String): promise<*> {
+export default function getDependecyTree(cwd: string, filePath: string): Promise<*> {
   return madge(filePath, { baseDir: cwd, includeNpm: true })
    .then(res => ({ missing: res.skipped, tree: sortDependency(res.tree, cwd) }));
 }
