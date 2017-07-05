926a836027677c8cdd981d69a7b518c97a37b37c
Gilad Shoham <shoham.gilad@gmail.com>
2017-07-05 18:26:11 +0300

    add MissingDependencies, ComponentNotFoundInPath errors

--- src/consumer/exceptions/index.js
+++ src/consumer/exceptions/index.js
@@ -3,11 +3,13 @@ import ConsumerAlreadyExists from './consumer-already-exists';
 import NothingToImport from './nothing-to-import';
 import ComponentSpecsFailed from './component-specs-failed';
 import DriverNotFound from './driver-not-found';
+import MissingDependencies from './missing-dependencies';
 
 export {
   ConsumerNotFound,
   ConsumerAlreadyExists,
   NothingToImport,
   ComponentSpecsFailed,
-  DriverNotFound
+  DriverNotFound,
+  MissingDependencies
 };
