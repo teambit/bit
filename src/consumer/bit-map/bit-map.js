c8ed028c4c761360871247b4dada087676126623
Gilad Shoham <shoham.gilad@gmail.com>
2017-07-07 01:09:05 +0300

    fix call to this.getComponentObjectByPath

--- src/consumer/bit-map/bit-map.js
+++ src/consumer/bit-map/bit-map.js
@@ -132,7 +132,7 @@ export default class BitMap {
    * @memberof BitMap
    */
   getComponentIdByPath(path: string): string {
-    const componentObject = getComponentObjectByPath(path);
+    const componentObject = this.getComponentObjectByPath(path);
     return R.keys(componentObject)[0];
   }
 
