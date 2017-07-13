c10f88f097de300fc605f950af91611bc4b23b26
Gilad Shoham <shoham.gilad@gmail.com>
2017-07-13 19:13:36 +0300

    start implement commit e2e tests

--- e2e/commands/commit.e2e.js
+++ e2e/commands/commit.e2e.js
@@ -7,12 +7,15 @@ import Helper from '../e2e-helper';
 
 describe.skip('bit commit command', function () {
   this.timeout(0);
-  // const helper = new Helper();
+  const helper = new Helper();
   after(() => {
-    // helper.destroyEnv();
+    helper.destroyEnv();
+  });
+  before(() => {
+    helper.reInitLocalScope();
   });
   describe('commit one component', () => {
-    it('should throw error if the bit id not exists', () => {
+    it('should throw error if the bit id does not exists', () => {
     });
 
     it('should persist the model in the scope', () => {
@@ -21,13 +24,30 @@ describe.skip('bit commit command', function () {
     it('should run the onCommit hook', () => {
     });
 
+    it('should throw error if the build failed', () => {
+    });
+
+    it('should throw error if the tests failed', () => {
+    });
+
     describe('commit imported component', () => {
       it('should index the component', () => {
+      });
+
+      it('should write the full id to bit map (include scope and version)', () => {
+      });
+
+      it('should create fork of the component', () => {
       });
     });
 
     describe('commit added component', () => {
       it('should index the component', () => {
+      });
+
+      it('should successfuly commit if there is no special error', () => {
+        // Validate output
+        // Validate model
       });
 
       it('Should throw error if there is tracked files dependencies which not commited yet', () => {
@@ -39,11 +59,38 @@ describe.skip('bit commit command', function () {
   });
 
   describe('commit all components', () => {
+    beforeEach(() => {
+      helper.reInitLocalScope();
+    });
 
-    it('Should print there is nothing to commit if there is no changes', () => {
+    it('Should print there is nothing to commit right after success commit all', () => {
+      // Create component and try to commit twice
+      helper.createComponentBarFoo();
+      helper.addComponentBarFoo();
+      let output = helper.commitAllComponents();
+      output = helper.commitAllComponents();
+      expect(output.includes('There is nothing to commit')).to.be.true;
+    });
+
+    it('Should print there is nothing to commit after import only', () => {
+      // Import component then try to commit
+    });
+
+    it('Should throw error if there is missing dependencies on file system', () => {
+      const fixture = "import foo from ./foo; module.exports = function foo2() { return 'got foo'; };";
+      helper.createComponent('bar', 'foo2.js', fixture);
+      helper.addComponent('bar/foo2.js');
+      const output = helper.commitAllComponents();
+      expect(output.includes('fatal: The following dependencies not found on file system - ./foo')).to.be.true;
     });
 
     it('Should throw error if there is untracked files dependencies', () => {
+      helper.createComponentBarFoo();
+      const fixture = "import foo from ./foo; module.exports = function foo2() { return 'got foo'; };";
+      helper.createComponent('bar', 'foo2.js', fixture);
+      helper.addComponent('bar/foo2.js');
+      const output = helper.commitAllComponents();
+      expect(output.includes('fatal: The following dependencies not found - ./foo')).to.be.true;
     });
 
     // We throw this error because we don't know the packege version in this case
