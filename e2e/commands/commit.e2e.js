985cdf0e066dc8b03a50976cf6ce9df2016aaa29
Gilad Shoham <shoham.gilad@gmail.com>
2017-07-16 14:01:14 +0300

    Added some commit tests

--- e2e/commands/commit.e2e.js
+++ e2e/commands/commit.e2e.js
@@ -4,8 +4,13 @@ import path from 'path';
 import fs from 'fs-extra';
 import { expect } from 'chai';
 import Helper from '../e2e-helper';
+import chalk from 'chalk';
+import sinon from 'sinon';
 
-describe.skip('bit commit command', function () {
+let logSpy;
+let errorSpy
+
+describe('bit commit command', function () {
   this.timeout(0);
   const helper = new Helper();
   after(() => {
@@ -13,9 +18,22 @@ describe.skip('bit commit command', function () {
   });
   before(() => {
     helper.reInitLocalScope();
+    logSpy = sinon.spy(console, 'log');
+    errorSpy = sinon.spy(console, 'error');
   });
-  describe('commit one component', () => {
+  describe.skip('commit one component', () => {
     it('should throw error if the bit id does not exists', () => {
+    });
+
+    it('should print warning if the a driver is not installed', () => {
+      
+      const fixture = "import foo from ./foo; module.exports = function foo2() { return 'got foo'; };";
+      helper.createComponent('bar', 'foo2.js', fixture);
+      helper.addComponent('bar/foo2.js');
+      const output = helper.commitAllComponents();
+      // var myargs = logSpy.getCalls()[4].args
+      // console.log("args", myargs);
+      expect( logSpy.calledWith('Warning: Bit is not be able calculate the dependencies tree. Please install bit-javascript driver and run commit again.\n') ).to.be.true;
     });
 
     it('should persist the model in the scope', () => {
@@ -30,7 +48,7 @@ describe.skip('bit commit command', function () {
     it('should throw error if the tests failed', () => {
     });
 
-    describe('commit imported component', () => {
+    describe.skip('commit imported component', () => {
       it('should index the component', () => {
       });
 
@@ -41,7 +59,7 @@ describe.skip('bit commit command', function () {
       });
     });
 
-    describe('commit added component', () => {
+    describe.skip('commit added component', () => {
       it('should index the component', () => {
       });
 
@@ -72,46 +90,46 @@ describe.skip('bit commit command', function () {
       expect(output.includes('There is nothing to commit')).to.be.true;
     });
 
-    it('Should print there is nothing to commit after import only', () => {
+    it.skip('Should print there is nothing to commit after import only', () => {
       // Import component then try to commit
     });
 
     it('Should throw error if there is missing dependencies on file system', () => {
-      const fixture = "import foo from ./foo; module.exports = function foo2() { return 'got foo'; };";
+      const fixture = "import foo from './foo'; module.exports = function foo2() { return 'got foo'; };";
       helper.createComponent('bar', 'foo2.js', fixture);
       helper.addComponent('bar/foo2.js');
-      const output = helper.commitAllComponents();
-      expect(output.includes('fatal: The following dependencies not found on file system - ./foo')).to.be.true;
+      const commitAll = () => helper.commitAllComponents();
+      expect(commitAll).to.throw('Command failed: bit-dev commit -am commit-message\nfatal: The following dependencies not found on file system - "./foo"\n');
     });
 
     it('Should throw error if there is untracked files dependencies', () => {
       helper.createComponentBarFoo();
-      const fixture = "import foo from ./foo; module.exports = function foo2() { return 'got foo'; };";
+      const fixture = "import foo from './foo'; module.exports = function foo2() { return 'got foo'; };";
       helper.createComponent('bar', 'foo2.js', fixture);
       helper.addComponent('bar/foo2.js');
-      const output = helper.commitAllComponents();
-      expect(output.includes('fatal: The following dependencies not found - ./foo')).to.be.true;
+      const commitAll = () => helper.commitAllComponents();
+      expect(commitAll).to.throw('Command failed: bit-dev commit -am commit-message\nfatal: The following dependencies not found - "bar/foo.js"\n');      
     });
 
     // We throw this error because we don't know the packege version in this case
-    it('should throw error if there is missing package dependency', () => {
+    it.skip('should throw error if there is missing package dependency', () => {
     });
 
-    it('should index all components', () => {
+    it.skip('should index all components', () => {
     });
 
-    it('should commit the components in the correct order', () => {
+    it.skip('should commit the components in the correct order', () => {
     });
 
-    it('should add the correct dependencies to each component', () => {
+    it.skip('should add the correct dependencies to each component', () => {
       // Make sure the use case contain dependenceis from all types -
       // Packages, files and bits
     });
 
-    it('should persist all models in the scope', () => {
+    it.skip('should persist all models in the scope', () => {
     });
 
-    it('should run the onCommit hook', () => {
+    it.skip('should run the onCommit hook', () => {
     });
   });
 });
