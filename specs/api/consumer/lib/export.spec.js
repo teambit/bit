84d49d855178beb254eb194539c9562a5bb02938
David First <david@cocycles.com>
2017-07-20 17:27:37 -0400

    attempt to fix a randomly failed unit-test on circle-ci

--- specs/api/consumer/lib/export.spec.js
+++ specs/api/consumer/lib/export.spec.js
@@ -2,7 +2,6 @@ import { expect } from 'chai';
 import sinon from 'sinon';
 import exportAction from '../../../../src/api/consumer/lib/export';
 import { ComponentNotFound } from '../../../../src/scope/exceptions';
-import ExportWithoutThis from '../../../../src/api/consumer/lib/exceptions/export-without-this';
 import * as consumer from '../../../../src/consumer';
 
 describe('export', () => {
@@ -15,9 +14,10 @@ describe('export', () => {
   });
   it('should throw a ComponentNotFound error if the component-id does include "@this" annotation', () => {
     sandbox.stub(consumer, 'loadConsumer').returns(Promise.resolve({ exportAction: () => Promise.reject(new ComponentNotFound()) }));
-    const result = exportAction('@this/box/name', 'my.remote');
-    return result.catch((err) => {
-      expect(err).to.be.an.instanceof(ComponentNotFound);
-    });
+    return exportAction('@this/box/name', 'my.remote')
+      .then(() => expect.fail('should not be here'))
+      .catch((err) => {
+        expect(err).to.be.an.instanceof(ComponentNotFound);
+      });
   });
 });
