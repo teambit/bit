c10c4368d232cb1b786091e59d1501f5d863a469
Gilad Shoham <shoham.gilad@gmail.com>
2017-07-25 20:50:03 +0300

    Show files in bit show (without args)

--- src/cli/templates/component-template.js
+++ src/cli/templates/component-template.js
@@ -11,15 +11,16 @@ export default (component: ConsumerComponent) => {
     wordWrap: true,
   });
 
-  const { name, box, lang, compilerId, testerId, dependencies, packageDependencies, docs } = component;
+  const { name, box, lang, compilerId, testerId, dependencies, packageDependencies, docs, files } = component;
 
   const rows = [
     { [c.cyan('ID')]: `${box}/${name}` },
     compilerId ? { [c.cyan('Compiler')]: compilerId.toString() }: null,
     lang ? { [c.cyan('Language')]: lang }: null,
     testerId ? { [c.cyan('Tester')]: testerId.toString() }: null,
-    !R.isEmpty(dependencies) ? { [c.cyan('Dependencies')]: dependencies.map(dependency => dependency.id.toString()).join(', ') } : null,
-    !R.isEmpty(packageDependencies) ? { [c.cyan('Packages')]: Object.keys(packageDependencies).join(', ') } : null
+    !R.isEmpty(dependencies) ? { [c.cyan('Dependencies')]: dependencies.map(dependency => dependency.id.toString()).join(',\n') } : null,
+    !R.isEmpty(packageDependencies) ? { [c.cyan('Packages')]: Object.keys(packageDependencies).join(',\n') } : null,
+    !R.isEmpty(files) ? { [c.cyan('Files')]: files.map(file => file.relative).join(',\n') } : null,
   ].filter(x => x);
 
   table.push(...rows);
