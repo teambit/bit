769748f07911ed2e4eaad428a7d034fc54556157
Gilad Shoham <shoham.gilad@gmail.com>
2017-08-02 11:38:08 +0300

    Update babel compiler for e2e tests

--- e2e/commands/show.e2e.js
+++ e2e/commands/show.e2e.js
@@ -107,7 +107,7 @@ describe('bit show command', function () {
 
       // TODO: get the version dynamically
       it('should include the compiler correctly', () => {
-        expect(output).to.include({compilerId: "bit.envs/compilers/babel3::1"});
+        expect(output).to.include({compilerId: "bit.envs/compilers/babel4::1"});
       });
 
       it('should include the language correctly', () => {
