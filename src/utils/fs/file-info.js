fd57ea22883d1d3559fce4256aa7cf484940f6fc
Amit <amit@cocycles.com>
2017-08-03 20:53:24 +0300

    added util function for getting file info

---
+++ src/utils/fs/file-info.js
@@ -0,0 +1,20 @@
+/** @flow */
+import path from 'path';
+
+/**
+ * get the current working dir name of file and file name.
+ * @name fileInfo
+ * @param file relative path
+ * @returns {object}
+ * @example
+ * ```js
+ *  currentDirName() // => 'bit'
+ * ```
+ */
+export default function calculateFileInfo(relativePath: string) {
+  const fileInfo =path.parse(relativePath)
+  const fullPath = path.dirname(relativePath);
+  const rootDir = path.dirname(fullPath);
+  const parentDir = path.relative(rootDir, fullPath)
+  return {parent: parentDir, fileName: fileInfo.name};
+}
