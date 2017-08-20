6e78003e5685d0ae7348ddb01607a92714d5a5cf
Gilad Shoham <shoham.gilad@gmail.com>
2017-08-20 12:23:50 +0300

    Create failing e2e test to make sure we really fix the problem

---
+++ e2e/commands/config.e2e.js
@@ -0,0 +1,20 @@
+// covers also init, create, commit, modify commands
+
+import { expect } from 'chai';
+import path from 'path';
+import fs from 'fs-extra';
+import Helper from '../e2e-helper';
+
+describe('bit config', function () {
+  this.timeout(0);
+  let helper;
+  before(() => {
+    helper = new Helper();
+  });
+
+  it.only('should not throw an error (on linux)', () => {
+    helper.reInitLocalScope();
+    const output = helper.runCmd('bit config');
+    expect(output).to.be.empty();
+  });
+});
