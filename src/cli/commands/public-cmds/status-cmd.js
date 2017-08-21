6c67eea388e4c6249b30f709231cf875a142bf62
Ran Mizrahi <ran@cocycles.com>
2017-08-21 14:57:27 +0300

    reformatted status output

--- src/cli/commands/public-cmds/status-cmd.js
+++ src/cli/commands/public-cmds/status-cmd.js
@@ -2,7 +2,7 @@
 import chalk from 'chalk';
 import Command from '../../command';
 import { status } from '../../../api/consumer';
-import { immutableUnshift } from '../../../utils';
+import { immutableUnshift, forEach } from '../../../utils';
 import { formatBitString, formatNewBit } from '../../chalk-box';
 import missingDepsTemplate from '../../templates/missing-dependencies-template';
 
@@ -19,24 +19,47 @@ export default class Status extends Command {
   }
 
   report({ newComponents, modifiedComponent, stagedComponents, componentsWithMissingDeps }: Object): string {
+    function formatMissing(missingComponent) {
+      function formatMissingStr(array, label) {
+        if (!array || array.length === 0) return '';
+        return chalk.yellow(`\n       ${label}: `) + chalk.white(array.join(', '));
+      }
+
+      return '       '  +
+        formatMissingStr(missingComponent.missingDependencies.untrackedDependencies, 'untracked file dependencies') +
+        formatMissingStr(missingComponent.missingDependencies.missingPackagesDependenciesOnFs, 'missing packages dependencies') +
+        formatMissingStr(missingComponent.missingDependencies.missingDependenciesOnFs, 'non-existing dependency files') + 
+        '\n';
+    }
+
+    function format(component) {
+      const missing = componentsWithMissingDeps.find((missingComp) => {
+        return missingComp.id.toString() === component.id.toString();
+      });
+
+      if (!missing) return formatNewBit(component) + '... ' + chalk.green('ok');
+      return formatNewBit(component)  + '... ' + chalk.red('missing dependencies') + formatMissing(missing);
+    }
+
     const newComponentsOutput = immutableUnshift(
-      newComponents.map(formatNewBit),
+      newComponents.map(format).sort((itemA) => {
+        if (itemA.indexOf('ok') !== -1) return -1;
+        return 1;
+      }),
       newComponents.length ? chalk.underline.white('new components') : chalk.green('no new components')
     ).join('\n');
 
     const modifiedComponentOutput = immutableUnshift(
-      modifiedComponent.map(formatNewBit),
+      modifiedComponent.map(format),
       modifiedComponent.length ? chalk.underline.white('modified components') : chalk.green('no modified components')
     ).join('\n');
 
     const stagedComponentsOutput = immutableUnshift(
-      stagedComponents.map(formatBitString),
+      stagedComponents.map(format),
       stagedComponents.length ? chalk.underline.white('staged components') : chalk.green('no staged components')
     ).join('\n');
 
-    const componentsWithMissingDepsOutput = missingDepsTemplate(componentsWithMissingDeps);
-
-    return [newComponentsOutput, modifiedComponentOutput, stagedComponentsOutput, componentsWithMissingDepsOutput].join(
+    return [newComponentsOutput, modifiedComponentOutput, stagedComponentsOutput].join(
       chalk.underline('\n                         \n')
     + chalk.white('\n'));
   }
