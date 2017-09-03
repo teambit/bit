c77bf345c8ec4a4325ab597ddeb15ae7b8b16237
Amit <amit@cocycles.com>
2017-09-03 15:33:40 +0300

    removed test to check packaging

--- scripts/build-windows-installer.bat
+++ scripts/build-windows-installer.bat
@@ -1,3 +1,1 @@
 "%ProgramFiles(x86)%\MSBuild\14.0\Bin\MSBuild.exe" ./scripts/windows/BitSetup.wixproj /p:Configuration=Release
-$VERSION= $(node -p -e "require('./package.json').version")
-mv distribution\windows\distribution\winMsibin\Release\Bit.msi  artifacts\bit-$VERSION-unsigned.msi
