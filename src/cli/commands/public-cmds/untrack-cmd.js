d17e2d8b1c9190f66f103c712644cb4041304ab7
Amit <amit@cocycles.com>
2017-09-04 12:07:52 +0300

    untrack

--- src/cli/commands/public-cmds/untrack-cmd.js
+++ src/cli/commands/public-cmds/untrack-cmd.js
@@ -1,21 +1,39 @@
 /** @flow */
 import chalk from 'chalk';
-import path from 'path';
+import R from 'Ramda';
 import Command from '../../command';
 import { untrack } from '../../../api/consumer';
 
 export default class Untrack extends Command {
-  name = 'untrack <files...>';
-  description = 'untrack new components/files';
+  name = 'untrack [ids...]';
+  description = 'untrack new components';
   alias = 'u';
   opts = [];
   loader = true;
 
   action([components]: [string[]]):Promise<*> {
-    return untrack(components);
+    return untrack(components || []);
   }
 
-  report(results):string {
-    console.log(results);
+  report({ untrackedComponents, unRemovableComponents, missingComponents }:
+    { untrackedComponents: Array<string>,
+      unRemovableComponents: Array<string>,
+      missingComponents: Array<string>
+    }):string {
+    let msg = [];
+    if (R.isEmpty(untrackedComponents) && R.isEmpty(unRemovableComponents) && R.isEmpty(missingComponents)) {
+      return chalk.underline.red('no components untracked');
+    }
+    if (!R.isEmpty(untrackedComponents)) {
+      const title = chalk.underline('untracked components:\n');
+      msg.push(title + untrackedComponents.map(result => chalk.green(result)).join('\n'));
+    }
+    if (!R.isEmpty(unRemovableComponents)) {
+      msg.push(chalk.red(`error: unable to untrack ${unRemovableComponents.join(', ')}, please use the bit remove command.\n`));
+    }
+    if (!R.isEmpty(missingComponents)) {
+      msg.push(chalk.red(`fatal: component ${missingComponents.join(', ')} did not match any component.`));
+    }
+    return msg.join('\n');
   }
 }
