9ce31cffc9e1d5ec74f115b4b4ec7216dcec3520
David First <david@cocycles.com>
2017-09-11 22:47:34 -0400

    skip the benchmark e2e-test for Appveyor

--- e2e/flows/benchmark.e2e.js
+++ e2e/flows/benchmark.e2e.js
@@ -10,20 +10,25 @@ describe('benchmark', function () {
     helper.destroyEnv();
   });
   describe('committing and exporting lots of components', () => {
-    before(() => {
-      helper.setNewLocalAndRemoteScopes();
-      for (let i = 0; i < numOfComponents; i += 1) {
-        const file = `foo${i}`;
-        const impl = `module.exports = function ${file}() { return 'got ${file}'; };`;
-        helper.createFile('utils', `${file}.js`, impl);
-      }
-      helper.addComponent('utils/*');
-      helper.commitAllComponents();
-      helper.exportAllComponents();
-    });
-    it('bit list should show the correct number of exported components', () => {
-      const output = helper.listRemoteScope();
-      expect(output.includes(`found ${numOfComponents} components`)).to.be.true;
-    });
+    // with current configuration APPVEYOR is much slower than CircleCI (which runs multiple tests in parallel)
+    if (process.env.APPVEYOR === 'True') {
+      this.skip;
+    } else {
+      before(() => {
+        helper.setNewLocalAndRemoteScopes();
+        for (let i = 0; i < numOfComponents; i += 1) {
+          const file = `foo${i}`;
+          const impl = `module.exports = function ${file}() { return 'got ${file}'; };`;
+          helper.createFile('utils', `${file}.js`, impl);
+        }
+        helper.addComponent('utils/*');
+        helper.commitAllComponents();
+        helper.exportAllComponents();
+      });
+      it('bit list should show the correct number of exported components', () => {
+        const output = helper.listRemoteScope();
+        expect(output.includes(`found ${numOfComponents} components`)).to.be.true;
+      });
+    }
   });
 });
