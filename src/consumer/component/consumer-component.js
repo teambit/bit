c9b345e8c1962689a8f547898e5d55b2df94c2d8
Gilad Shoham <shoham.gilad@gmail.com>
2017-09-14 13:04:08 +0300

    support commit force even if there is an exeaption during tests execution

--- src/consumer/component/consumer-component.js
+++ src/consumer/component/consumer-component.js
@@ -456,8 +456,11 @@ export default class Component {
 
           return this.specsResults;
         } catch (err) {
-          if (verbose) throw err;
-          throw new ComponentSpecsFailed();
+          if (rejectOnFailure) {
+            if (verbose) throw err;
+            throw new ComponentSpecsFailed();
+          }
+          return this.specsResults;
         }
       };
 
