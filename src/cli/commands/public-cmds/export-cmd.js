5ae6925fd203fe53c38a5f642f067624a8649f2d
David First <david@cocycles.com>
2017-09-20 15:43:55 -0400

    change export process to not import the components after pushing them to a remote-scope. it prevents local corrupted scope in case the import fails

--- src/cli/commands/public-cmds/export-cmd.js
+++ src/cli/commands/public-cmds/export-cmd.js
@@ -2,7 +2,7 @@
 import R from 'ramda';
 import Command from '../../command';
 import { exportAction } from '../../../api/consumer';
-import Component from '../../../consumer/component/consumer-component';
+import { BitId } from '../../../bit-id';
 
 const chalk = require('chalk');
 
@@ -14,15 +14,15 @@ export default class Export extends Command {
   loader = true;
 
   action([remote, ids]: [string, string[]], { forget }: any): Promise<*> {
-    return exportAction(ids, remote, !forget).then(component => ({ component, remote }));
+    return exportAction(ids, remote, !forget).then(id => ({ id, remote }));
   }
 
-  report({ component, remote }: { component: Component | Component[], remote: string }): string {
-    if (R.isEmpty(component)) return chalk.green(`no components to export to scope ${chalk.bold(remote)}`);
-    if (Array.isArray(component)) {
-      return chalk.green(`exported ${component.length} components to scope ${chalk.bold(remote)}`);
+  report({ componentId, remote }: { componentId: BitId | BitId[], remote: string }): string {
+    if (R.isEmpty(componentId)) return chalk.green(`no components to export to scope ${chalk.bold(remote)}`);
+    if (Array.isArray(componentId)) {
+      return chalk.green(`exported ${componentId.length} components to scope ${chalk.bold(remote)}`);
     }
 
-    return chalk.green(`exported component ${chalk.bold(component.id.toString())} to scope ${chalk.bold(remote)}`);
+    return chalk.green(`exported component ${chalk.bold(componentId.toString())} to scope ${chalk.bold(remote)}`);
   }
 }
