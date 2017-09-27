25fd1bceeaa91b2af407ed268f62b8aeee38f8bb
Gilad Shoham <shoham.gilad@gmail.com>
2017-09-27 13:44:40 +0300

    Add to-base64-array-buffer function

--- src/utils/index.js
+++ src/utils/index.js
@@ -21,6 +21,7 @@ import { propogateUntil, pathHas } from './fs/propogate-until';
 import isBitUrl from './is-bit-url';
 import bufferToReadStream from './buffer/to-read-stream';
 import toBase64 from './string/to-base64';
+import toBase64ArrayBuffer from './string/to-base64-array-buffer';
 import fromBase64 from './string/from-base64';
 import parseSSHUrl from './ssh/parse-url';
 import listDirectories from './fs/list-directories';
@@ -84,6 +85,7 @@ export {
   deflate,
   values,
   toBase64,
+  toBase64ArrayBuffer,
   fromBase64,
   glob,
   resolveBoolean,
