3f838a3fed635ee778a62b3b1d8020a4ddb79a17
David First <david@cocycles.com>
2017-10-17 16:50:05 +0300

    remove the obsolete INLINE_COMPONENTS_DIRNAME constant

--- src/constants.js
+++ src/constants.js
@@ -7,8 +7,6 @@ export const PACKAGE_JSON = 'package.json';
 
 export const COMPONENTS_DIRNAME = 'components';
 
-export const INLINE_COMPONENTS_DIRNAME = 'inline_components';
-
 export const VERSION_DELIMITER = '@';
 
 export const NO_PLUGIN_TYPE = 'none';
