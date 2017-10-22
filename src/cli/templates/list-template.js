d4898616bb4208d7f650a89ddb9c921ba9fb8065
David First <david@cocycles.com>
2017-10-22 14:01:36 +0300

    replace cli-table2 with tty-table for bit list command. remove cli-table2 dependency

--- src/cli/templates/list-template.js
+++ src/cli/templates/list-template.js
@@ -1,14 +1,16 @@
 // @flow
 import c from 'chalk';
-import Table from 'cli-table2';
+import Table from 'tty-table';
 import ConsumerComponent from '../../consumer/component/consumer-component';
 
 export default (components: ConsumerComponent[]) => {
-  const table = new Table({
-    head: [c.cyan('ID'), c.cyan('Version')],
-    colWidths: [46, 9],
-    wordWrap: true
-  });
+  const header = [
+    { value: 'Id', width: 46, headerColor: 'cyan', headerAlign: 'left' },
+    { value: 'Version', width: 9, headerColor: 'cyan', headerAlign: 'left' }
+  ];
+  const opts = {
+    align: 'left'
+  };
 
   function tablizeComponent(component) {
     return [
@@ -17,6 +19,8 @@ export default (components: ConsumerComponent[]) => {
     ]; // Add date, author
   }
 
-  table.push(...components.map(tablizeComponent));
-  return table.toString();
+  const rows = components.map(tablizeComponent);
+  const table = new Table(header, rows, opts);
+
+  return table.render();
 };
