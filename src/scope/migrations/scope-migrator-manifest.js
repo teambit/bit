ae3f568a7d36118e74cf32ce55f347156a516fdd
Gilad Shoham <shoham.gilad@gmail.com>
2017-11-06 16:50:42 +0200

    update migration manifest

--- src/scope/migrations/scope-migrator-manifest.js
+++ src/scope/migrations/scope-migrator-manifest.js
@@ -6,8 +6,8 @@ import * as componentMigrations from './component';
 import * as versionMigrations from './component-version';
 
 export default {
-  '0.10.10': {
-    // Component: [componentMigrations.changeVersionToSemVerDeclartaion]
+  '0.10.10-dev.1': {
+    // Component: [componentMigrations.changeVersionToSemVerDeclartaion],
     Version: [versionMigrations.addBindingPrefixDeclartaion]
   }
 };
