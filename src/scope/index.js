7d7f4c6bc0513fa00f4d12dfd0f9f7e7c13863e0
Amit <amit@cocycles.com>
2017-11-13 18:48:29 +0200

    added index.js in scope back

---
+++ src/scope/index.js
@@ -0,0 +1,6 @@
+/** @flow */
+import loadScope from './scope-loader';
+import Scope from './scope';
+import ComponentWithDependencies from './component-dependencies';
+
+export { loadScope, Scope, ComponentWithDependencies };
