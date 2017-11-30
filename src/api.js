ca5fe98183c99e10d98048f75d98f304057b058c
Gilad Shoham <shoham.gilad@gmail.com>
2017-11-30 15:42:44 +0200

    add loadExtension api

--- src/api.js
+++ src/api.js
@@ -1,5 +1,9 @@
+/** @flow */
+
 import { getScopeComponent } from './api/consumer/index';
 import { scopeList } from './api/scope/index';
+import Extension from './extensions/extension';
+import logger from './logger/logger';
 
 module.exports = {
   show: (scopePath, id, opts) =>
@@ -9,5 +13,11 @@ module.exports = {
       }
       return c.toObject();
     }),
-  list: scopePath => scopeList(scopePath).then(components => components.map(c => c.id.toString()))
+  list: scopePath => scopeList(scopePath).then(components => components.map(c => c.id.toString())),
+  loadExtension: async (extensionName: string, extensionFilePath: string, extensionConfig: Object): Extension => {
+    console.log('here2');
+    const extension = await Extension.loadFromFile(extensionName, extensionFilePath, extensionConfig);
+    console.log('ds', extension.script);
+    return Promise.resolve(extension);
+  }
 };
