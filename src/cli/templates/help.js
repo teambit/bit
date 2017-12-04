44fbab3e2533a2109f063fdbc8eacb10d051bb3b
Ran Mizrahi <me@ranmizrahi.com>
2017-12-04 14:32:07 +0200

    removed search from help

--- src/cli/templates/help.js
+++ src/cli/templates/help.js
@@ -29,7 +29,6 @@ ${chalk.underline('collaborate and share components')}
 
 ${chalk.underline('discover components')}
   ${chalk.cyan('list')}       list components on a local or a remote scope.
-  ${chalk.cyan('search')}     search for components by desired functionality.
 
 ${chalk.underline('examine component history and state')}
   ${chalk.cyan('log')}        show components(s) tag history.
