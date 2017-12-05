9bb56b6269cfb355fcc7b2b4aa950a9cdc363c61
Gilad Shoham <shoham.gilad@gmail.com>
2017-12-06 00:06:13 +0200

    remove debug log

--- src/api.js
+++ src/api.js
@@ -15,7 +15,6 @@ module.exports = {
     }),
   list: scopePath => scopeList(scopePath).then(components => components.map(c => c.id.toString())),
   loadExtension: async (extensionName: string, extensionFilePath: string, extensionConfig: Object): Extension => {
-    console.log('here2');
     const extension = await Extension.loadFromFile(extensionName, extensionFilePath, extensionConfig);
     console.log('ds', extension.script);
     return Promise.resolve(extension);
