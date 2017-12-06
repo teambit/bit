50fe53ba28a0f3153d577e1066758971cedff13d
Gilad Shoham <shoham.gilad@gmail.com>
2017-12-06 14:18:28 +0200

    trigger pre/post send-objects hook

--- src/api/scope/lib/fetch.js
+++ src/api/scope/lib/fetch.js
@@ -1,11 +1,20 @@
 /** @flow */
 import { loadScope } from '../../../scope';
 import { BitIds } from '../../../bit-id';
+import { PRE_SEND_OBJECTS, POST_SEND_OBJECTS } from '../../../constants';
+import HooksManager from '../../../hooks';
+
+const HooksManagerInstance = HooksManager.getInstance();
 
 export default function fetch(path: string, ids: string[], noDependencies: boolean = false) {
+  const bitIds = BitIds.deserialize(ids);
+  const args = { path, bitIds, noDependencies };
+  HooksManagerInstance.triggerHook(PRE_SEND_OBJECTS, args);
   return loadScope(path).then((scope) => {
-    const bitIds = BitIds.deserialize(ids);
     if (noDependencies) return scope.manyOneObjects(bitIds);
-    return scope.getObjects(bitIds);
+    return scope.getObjects(bitIds).then((objects) => {
+      HooksManagerInstance.triggerHook(POST_SEND_OBJECTS, objects);
+      return objects;
+    });
   });
 }
