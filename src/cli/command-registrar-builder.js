c3b5af5843d48806043c29c0f287aa52c132217f
Gilad Shoham <shoham.gilad@gmail.com>
2017-12-06 14:57:30 +0200

    remove more pack command leftovers

--- src/cli/command-registrar-builder.js
+++ src/cli/command-registrar-builder.js
@@ -27,7 +27,6 @@ import Fetch from './commands/private-cmds/_fetch-cmd';
 import Log from './commands/public-cmds/log-cmd';
 import Build from './commands/public-cmds/build-cmd';
 import CiUpdate from './commands/private-cmds/ci-update-cmd';
-import Pack from './commands/private-cmds/pack-in-scope-cmd';
 import RefreshScope from './commands/private-cmds/refresh-scope-cmd';
 import CatScope from './commands/private-cmds/cat-scope-cmd';
 import ScopeConfig from './commands/public-cmds/scope-config-cmd';
@@ -88,8 +87,7 @@ export default function registerCommands(extensionsCommands): CommandRegistrar {
       new Delete(),
       new DeprecatePrivate(),
       new Latest(),
-      new Migrate(),
-      new Pack()
+      new Migrate()
     ],
     extensionsCommands
   );
