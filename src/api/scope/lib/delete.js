531d762d30a552f0d3c167b169f135b52749f316
Gilad Shoham <shoham.gilad@gmail.com>
2017-12-06 14:17:50 +0200

    trigger pre/post remove-remote hook

--- src/api/scope/lib/delete.js
+++ src/api/scope/lib/delete.js
@@ -1,10 +1,19 @@
 // @flow
 import { loadScope } from '../../../scope';
-import BitId from '../../../bit-id/bit-id';
+import { BitIds } from '../../../bit-id';
+import { PRE_REMOVE_REMOTE, POST_REMOVE_REMOTE } from '../../../constants';
+import HooksManager from '../../../hooks';
 
-export default function remove({ path, bitIds, force }): Promise<string[]> {
-  const ids = bitIds.map(bitId => BitId.parse(bitId));
+const HooksManagerInstance = HooksManager.getInstance();
+
+export default function remove({ path, ids, force }): Promise<string[]> {
+  const bitIds = BitIds.deserialize(ids);
+  const args = { path, bitIds };
+  HooksManagerInstance.triggerHook(PRE_REMOVE_REMOTE, args);
   return loadScope(path).then((scope) => {
-    return scope.removeMany(ids, force);
+    return scope.removeMany(bitIds, force).then((res) => {
+      HooksManagerInstance.triggerHook(POST_REMOVE_REMOTE, res);
+      return res;
+    });
   });
 }
