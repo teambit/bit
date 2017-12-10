6e6d786ec259979878a1f286edec8db739c9d42a
Gilad Shoham <shoham.gilad@gmail.com>
2017-12-10 16:22:50 +0200

    Load global extensions

--- src/extensions/extensions-loader.js
+++ src/extensions/extensions-loader.js
@@ -1,19 +1,33 @@
 /** @flow */
+import path from 'path';
 import R from 'ramda';
+import fs from 'fs-extra';
 import Extension from './extension';
 import { loadConsumer, Consumer } from '../consumer';
 import logger from '../logger/logger';
+import { GLOBAL_CONFIG, BIT_JSON } from '../constants';
 
 export default (async function loadExtensions(): Extension[] {
   try {
     logger.info('start loading extensions');
     const consumer: Consumer = await loadConsumer();
-    const rawExtensions = consumer.bitJson.extensions || {};
+    let rawExtensions = consumer.bitJson.extensions || {};
+    // Load global extensions
+    const globalBitJson = await _getGlobalRawExtensions(false);
+    const globalRawExtensions = globalBitJson && globalBitJson.extensions;
+    // Merge the global with the local extensions only if exists
+    // The local extension is higher priority than the global ones since they are closer to the user
+    // This mechanism is for internal use by bitsrc server and should not be used by the users
+    if (globalRawExtensions) {
+      rawExtensions = R.mergeDeepLeft(rawExtensions, globalRawExtensions);
+    }
     const extensions = R.values(
       R.mapObjIndexed(_loadExtension(consumer.getPath(), consumer.scope.path), rawExtensions)
     );
     return Promise.all(extensions);
   } catch (err) {
+    logger.error('loading extensions failed');
+    logger.error(err);
     return [];
   }
 });
@@ -23,4 +37,24 @@ const _loadExtension = (consumerPath: string, scopePath: string) => (
   name: string
 ): Promise<Extension> => {
   return Extension.load(name, rawConfig.config, rawConfig.options, consumerPath, scopePath);
+};
+
+const _getGlobalRawExtensions = async (throws: boolean) => {
+  const globalBitJsonPath = path.join(GLOBAL_CONFIG, BIT_JSON);
+  const exists = await fs.pathExists(globalBitJsonPath);
+  if (!exists) return null;
+
+  return (
+    fs
+      .readJson(globalBitJsonPath, { throws })
+      // Implementing the catch my self since the throws: false not really workging
+      .catch((e) => {
+        if (throws) {
+          throw e;
+        }
+        logger.debug('error during loading global bit.json');
+        logger.debug(e);
+        return null;
+      })
+  );
 };
