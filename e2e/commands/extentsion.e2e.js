2d784336be1ac74159f8e7d764f1a63ced52d95a
Oded Reuveny <oded.reuveny@gmail.com>
2017-12-14 10:56:27 +0200

    added a few test scenarios to extension system

--- e2e/commands/extentsion.e2e.js
+++ e2e/commands/extentsion.e2e.js
@@ -26,8 +26,22 @@ describe('bit extension system', function () {
     it.skip('Should be able to trigger the hook they created', () => {});
     it.skip('Should not be able to trigger a hook they didnt create', () => {});
   });
-
-  it.skip('', () => {});
-  it.skip('', () => {});
-  describe('', () => {});
+  it.skip('2 different extentions create hook with the same name', () => {});
+  it.skip('default hook should run when triggered', () => {});
+  describe('running a hook', () => {
+    it.skip('Should trigger loader', () => {});
+    it.skip('Should write to log', () => {});
+    it.skip('Should check that the name of the extension appears in the log', () => {});
+    it.skip('Should not run a disabled hook ', () => {});
+  });
+  describe('when importing extension', () => {
+    it.skip('Should be able to import extension with --extention flag', () => {});
+    it.skip('if imported more that once should have the latest import in the bit.json', () => {});
+    it.skip('Should pass the config', () => {});
+    it.skip('Should be able to upload from a local file using relative path', () => {});
+    it.skip('Should be able to upload from a local file using absolute path', () => {});
+    it.skip('extension should be able to use a dependency', () => {});
+    it.skip('extension can be loaded programatically', () => {});
+    it.skip('api of an isolated component should work in an extension', () => {});
+  });
 });
