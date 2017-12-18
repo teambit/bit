be64ce8a4824dc0ca63bad2c7fea569b0568e0fc
Amit <amit@cocycles.com>
2017-12-19 01:09:30 +0200

    removed only

--- e2e/commands/remove.e2e.js
+++ e2e/commands/remove.e2e.js
@@ -2,7 +2,7 @@ import { expect } from 'chai';
 import path from 'path';
 import Helper from '../e2e-helper';
 
-describe.only('bit remove command', function () {
+describe('bit remove command', function () {
   this.timeout(0);
   const helper = new Helper();
   after(() => {
