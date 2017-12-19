19a339cd59a4c4c3ff6065fe5669c36a84ca433e
Oded Reuveny <oded.reuveny@gmail.com>
2017-12-19 14:16:19 +0200

    changes to extension tests

--- e2e/commands/extentsion.e2e.js
+++ e2e/commands/extentsion.e2e.js
@@ -16,29 +16,24 @@ describe('bit extension system', function () {
   });
 
   it.skip('Exception from hook should not affect others which are registers for that hook', () => {});
-  it.skip('Should be able to trigger a hook', () => {});
-  describe('new command for extension', () => {
+  describe('new command by extension', () => {
     it.skip('Should be shown in --help', () => {});
     it.skip('Should be able to run "bit <command> -h"', () => {});
-    it.skip('Should trigger loader', () => {});
-  });
-  describe('extension can register a new hook', () => {
-    it.skip('Should be able to trigger the hook they created', () => {});
-    it.skip('Should not be able to trigger a hook they didnt create', () => {});
+    it.skip('should be able to run the command', () => {});
   });
   it.skip('2 different extentions create hook with the same name', () => {});
   describe('extension logger', () => {
     it.skip('Should write to log', () => {});
     it.skip('Should check that the name of the extension appears in the log', () => {});
   });
-  describe('default hooks', () => {
-    it.skip('default hook should run when triggered', () => {});
-  });
-  describe('when importing another version of the same extension', () => {
+
+  describe('when importing extension', () => {
     it.skip('Should be able to import extension with --extention flag', () => {});
-    it.skip('Should have the latest import in the bit.json', () => {});
-    it.skip('Should copy the config / options in bit.json to the new version', () => {});
     it.skip('extension should be able to use a dependency', () => {});
+    describe('when importing another version of the same extension', () => {
+      it.skip('Should have the latest import in the bit.json', () => {});
+      it.skip('Should copy the config / options in bit.json to the new version', () => {});
+    });
   });
   describe('loading extension', () => {
     it.skip('Should be able to load from a local file using relative path', () => {});
@@ -46,12 +41,21 @@ describe('bit extension system', function () {
     it.skip('Should pass the config to extensions init function', () => {});
     it.skip('Default extension should load', () => {});
     it.skip('Extension with exception during init should not corrupt bit', () => {});
-    it.skip('Should not run a disabled hook ', () => {});
+    it.skip('Should not load a disabled extension ', () => {});
   });
   describe('load extension programmatically', () => {
     it.skip('extension can be loaded programatically', () => {});
   });
   describe('isolated component', () => {
     it.skip('api of an isolated component should work in an extension', () => {});
+  });
+  describe('Hooks', () => {
+    describe('extension can register a new hook', () => {
+      it.skip('Should be able to trigger the hook they created', () => {});
+      it.skip('Should not be able to trigger a hook they didnt create', () => {});
+    });
+    describe('default hooks', () => {
+      it.skip('default hook should run when triggered', () => {});
+    });
   });
 });
