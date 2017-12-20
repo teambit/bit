d182ec0787d09b9f5e3d213621559e7c96579e5b
David First <david@cocycles.com>
2017-12-20 15:30:01 -0500

    add a migration script to update the bindingPrefix in the model from bit to @bit

--- src/scope/migrations/component/index.js
+++ src/scope/migrations/component/index.js
@@ -1,4 +1,5 @@
 /** @flow */
 import changeVersionToSemVerDeclartaion from './to-semver-migration';
+import updateBindingPrefixToNewDefault from './update-binding-prefix-to-new-default';
 
-export { changeVersionToSemVerDeclartaion };
+export { changeVersionToSemVerDeclartaion, updateBindingPrefixToNewDefault };
