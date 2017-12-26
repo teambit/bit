d8eac5121b398b975d729b02d7c1586982b18a20
odedre <oded.reuveny@gmail.com>
2017-12-26 18:58:23 +0200

    test for 'bit init' when bit.map already exists (#483)

--- e2e/commands/init.e2e.js
+++ e2e/commands/init.e2e.js
@@ -24,3 +24,18 @@ describe('run bit init', function () {
     expect(errorMsg).to.have.string("there's already a scope");
   });
 });
+
+describe('automatic bit init when .bit.map.json already exists', function () {
+  this.timeout(0);
+  const helper = new Helper();
+  after(() => {
+    helper.destroyEnv();
+  });
+  before(() => {
+    helper.createBitMap();
+  });
+  it('Should not tell you there is already a scope when running "bit init"', () => {
+    const init = helper.initLocalScope();
+    expect(init).to.have.string('successfully initialized an empty bit scope');
+  });
+});
