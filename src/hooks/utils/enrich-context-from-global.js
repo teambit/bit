fb77623e35491d745ee248812c6ba1dc8cc93af6
Gilad Shoham <shoham.gilad@gmail.com>
2017-12-27 15:17:32 +0200

    validte public ssh key exists before reading it

--- src/hooks/utils/enrich-context-from-global.js
+++ src/hooks/utils/enrich-context-from-global.js
@@ -2,12 +2,14 @@
 import fs from 'fs-extra';
 import * as globalConfig from '../../api/consumer/lib/global-config';
 import { CFG_USER_NAME_KEY, CFG_USER_EMAIL_KEY, CFG_SSH_KEY_FILE_KEY, DEFAULT_SSH_KEY_FILE } from '../../constants';
+import logger from '../../logger/logger';
 
 /**
  * Add more keys to the context which will be passed to hooks
  * @param {Object} context
  */
 export default function enrichContextFromGlobal(context: Object = {}) {
+  logger.debug('enrich context from global config');
   const username = globalConfig.getSync(CFG_USER_NAME_KEY);
   const email = globalConfig.getSync(CFG_USER_EMAIL_KEY);
   const sshKeyFile = globalConfig.getSync(CFG_SSH_KEY_FILE_KEY);
@@ -17,6 +19,10 @@ export default function enrichContextFromGlobal(context: Object = {}) {
 }
 
 function _getSshPubKey(pubSshKeyFile: string = `${DEFAULT_SSH_KEY_FILE}.pub`) {
+  logger.debug(`reading ssh public key from ${pubSshKeyFile}`);
+  if (!fs.pathExistsSync(pubSshKeyFile)) {
+    return null;
+  }
   const buf = fs.readFileSync(pubSshKeyFile);
   return buf.toString();
 }
