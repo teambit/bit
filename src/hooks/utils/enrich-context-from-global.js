c46c7c28e2d91d2ad877f1d5242f4faf88794da3
Gilad Shoham <shoham.gilad@gmail.com>
2017-12-27 14:00:06 +0200

    add public ssh key to context for server side hooks

--- src/hooks/utils/enrich-context-from-global.js
+++ src/hooks/utils/enrich-context-from-global.js
@@ -1,6 +1,7 @@
 /** @flow */
+import fs from 'fs-extra';
 import * as globalConfig from '../../api/consumer/lib/global-config';
-import { CFG_USER_NAME_KEY, CFG_USER_EMAIL_KEY } from '../../constants';
+import { CFG_USER_NAME_KEY, CFG_USER_EMAIL_KEY, CFG_SSH_KEY_FILE_KEY, DEFAULT_SSH_KEY_FILE } from '../../constants';
 
 /**
  * Add more keys to the context which will be passed to hooks
@@ -9,5 +10,13 @@ import { CFG_USER_NAME_KEY, CFG_USER_EMAIL_KEY } from '../../constants';
 export default function enrichContextFromGlobal(context: Object = {}) {
   const username = globalConfig.getSync(CFG_USER_NAME_KEY);
   const email = globalConfig.getSync(CFG_USER_EMAIL_KEY);
-  Object.assign(context, { username, email });
+  const sshKeyFile = globalConfig.getSync(CFG_SSH_KEY_FILE_KEY);
+  const pubSshKeyFile = sshKeyFile ? `${sshKeyFile}.pub` : '';
+  const pubSshKey = _getSshPubKey(pubSshKeyFile);
+  Object.assign(context, { username, email, pubSshKey });
+}
+
+function _getSshPubKey(pubSshKeyFile: string = `${DEFAULT_SSH_KEY_FILE}.pub`) {
+  const buf = fs.readFileSync(pubSshKeyFile);
+  return buf.toString();
 }
