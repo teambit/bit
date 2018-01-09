58b41ac578ac8871cca16eeb527869786be3f66c
Gilad Shoham <shoham.gilad@gmail.com>
2018-01-09 18:01:39 +0200

    fix bug with writing dists when there is no compiler

--- src/consumer/component/consumer-component.js
+++ src/consumer/component/consumer-component.js
@@ -1072,7 +1072,7 @@ export default class Component {
     componentFromModel: ModelComponent
   }): Component {
     const deprecated = componentFromModel ? componentFromModel.component.deprecated : false;
-    const dists = componentFromModel ? componentFromModel.component.dists : undefined;
+    let dists = componentFromModel ? componentFromModel.component.dists : undefined;
     let packageDependencies;
     let bitJson = consumerBitJson;
     const getLoadedFiles = (files: ComponentMapFile[]): SourceFile[] => {
@@ -1118,6 +1118,11 @@ export default class Component {
     // use the component from the model to get their bit.json values
     if (!fs.existsSync(path.join(bitDir, BIT_JSON)) && componentFromModel) {
       bitJson.mergeWithComponentData(componentFromModel.component);
+    }
+
+    // Remove dists if compiler has been deleted
+    if (dists && !bitJson.compilerId) {
+      dists = undefined;
     }
 
     return new Component({
