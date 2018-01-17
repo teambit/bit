338544000ac8465d7cc93bdacd86085cc2b0ae9e
Gilad Shoham <shoham.gilad@gmail.com>
2018-01-17 16:52:36 +0200

    Update help.js

--- src/cli/templates/help.js
+++ src/cli/templates/help.js
@@ -26,7 +26,7 @@ const baseTemplate = (extensionsTemplate) => {
     ${chalk.cyan('insall')}     install node packages of all components and calls the link command.
     ${chalk.cyan('remote')}     manage set of tracked bit scope(s).
     ${chalk.cyan('remove')}     remove component(s) from your working area, or a remote scope.
-    ${chalk.cyan('link')}       Call the driver link action to link components to their required packages.
+    ${chalk.cyan('link')}       call the driver link action to link components to their required packages.
 
   ${chalk.underline('discover components')}
     ${chalk.cyan('list')}       list components on a local or a remote scope.
