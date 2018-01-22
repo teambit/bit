cc9ab8737ddf2a93ed8baf823ac39782af48b44c
Amit <amit@cocycles.com>
2018-01-22 12:56:23 +0200

    fixed deserialize bug with bit remove

--- src/scope/component-remove.js
+++ src/scope/component-remove.js
@@ -1,20 +1,33 @@
-import { BitIds } from '../bit-id';
+import { BitId, BitIds } from '../bit-id';
 
 export class RemovedObjects {
   removedComponentIds: BitIds;
   missingComponents: BitIds;
   dependentBits: Object;
   removedDependencies: BitIds;
-  constructor(
-    bitIds: BitIds = [],
-    missingComponents: BitIds = [],
-    removedDependencies: BitIds = [],
-    dependentBits: Object = {}
-  ) {
-    this.removedComponentIds = bitIds;
+  constructor({
+    removedComponentIds = [],
+    missingComponents = [],
+    removedDependencies = [],
+    dependentBits = {}
+  }: {
+    removedComponentIds: BitIds,
+    missingComponents: BitIds,
+    removedDependencies: BitIds,
+    dependentBits: Object
+  }) {
+    this.removedComponentIds = removedComponentIds;
     this.missingComponents = missingComponents;
     this.dependentBits = dependentBits;
     this.removedDependencies = removedDependencies;
+  }
+
+  static fromObjects(payload: Object): ComponentObjects {
+    let { missingComponents, removedComponentIds, removedDependencies, dependentBits } = payload;
+    missingComponents = missingComponents.map(id => new BitId(id));
+    removedComponentIds = removedComponentIds.map(id => new BitId(id));
+    removedDependencies = removedDependencies.map(id => new BitId(id));
+    return new RemovedObjects({ missingComponents, removedComponentIds, removedDependencies, dependentBits });
   }
 }
 export class RemovedLocalObjects extends RemovedObjects {
