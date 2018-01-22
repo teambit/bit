7ca9ed5ec1233b05fc0ada71e15d7423ad637743
Gilad Shoham <shoham.gilad@gmail.com>
2018-01-22 12:34:08 +0200

    ignore errors in gitconfig

--- src/api/consumer/lib/global-config.js
+++ src/api/consumer/lib/global-config.js
@@ -31,20 +31,30 @@ export function delSync(key: string): Config {
   return config;
 }
 
-export function get(key: string): Promise<?string> {
-  return GlobalConfig.load().then((config) => {
-    const val = config.get(key);
-    if (val) return val;
-    return gitconfig.get(key);
-  });
+export async function get(key: string): Promise<?string> {
+  const config = await GlobalConfig.load();
+  const val = config.get(key);
+  if (val) return val;
+  try {
+    const gitVal = await gitconfig.get(key);
+    return gitVal;
+    // Ignore error from git config get
+  } catch (err) {
+    return undefined;
+  }
 }
 
 export function getSync(key: string): ?string {
   const config = GlobalConfig.loadSync();
   const val = config.get(key);
   if (val) return val;
-  const gitVal = gitconfig.get.sync(key);
-  return gitVal;
+  try {
+    const gitVal = gitconfig.get.sync(key);
+    return gitVal;
+    // Ignore error from git config get
+  } catch (err) {
+    return undefined;
+  }
 }
 
 export function list(): Promise<any> {
