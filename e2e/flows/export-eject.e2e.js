5f18c67125f7055583e8ed4322073bf51c7a5f31
David First <david@bitsrc.io>
2018-02-05 10:33:42 -0500

    add BitsrcTester class for testing functionality against bitsrc (#699)
    * add BitsrcTester class for testing functionality against bitsrc
    
    * Add public key to circleci and appveyor
    
    * store artifacts
    
    * add_ssh_keys
    
    * enable getting username for bitsrc.io from env variable

--- e2e/flows/export-eject.e2e.js
+++ e2e/flows/export-eject.e2e.js
@@ -1,39 +1,42 @@
 import chai, { expect } from 'chai';
 import path from 'path';
 import Helper from '../e2e-helper';
+import BitsrcTester, { username } from '../bitsrc-tester';
 
 chai.use(require('chai-fs'));
 
-const remoteScopeCI = 'david.ci';
-
-// @todo: figure out how to authenticate into bitsrc from e2e-tests.
-describe.skip('export --eject functionality using bitsrc.io', function () {
+describe('export --eject functionality using bitsrc.io', function () {
   this.timeout(0);
   const helper = new Helper();
+  const bitsrcTester = new BitsrcTester();
+  let scopeName;
+  before(() => {
+    return bitsrcTester
+      .loginToBitSrc()
+      .then(() => bitsrcTester.createScope())
+      .then((scope) => {
+        scopeName = scope;
+      });
+  });
   after(() => {
     helper.destroyEnv();
+    return bitsrcTester.deleteScope(scopeName);
   });
   describe('as author', () => {
-    const randomBoxName = Math.random()
-      .toString(36)
-      .substr(2, 5);
     before(() => {
       helper.reInitLocalScope();
-      helper.createComponent(randomBoxName, 'foo.js');
-      helper.addComponent(path.join(randomBoxName, 'foo.js'));
+      helper.createComponentBarFoo();
+      helper.addComponentBarFoo();
       helper.tagAllWithoutMessage();
-      helper.exportAllComponents(`${remoteScopeCI} --eject`);
-    });
-    after(() => {
-      helper.removeComponent(`${remoteScopeCI}/${randomBoxName}/foo`, '--silent');
+      helper.exportAllComponents(`${username}.${scopeName} --eject`);
     });
     it('should delete the original component files from the file-system', () => {
-      expect(path.join(helper.localScopePath, randomBoxName, 'foo.js')).not.to.be.a.path();
+      expect(path.join(helper.localScopePath, 'bar', 'foo.js')).not.to.be.a.path();
     });
     it('should have the component files as a package (in node_modules)', () => {
       expect(
-        path.join(helper.localScopePath, 'node_modules', '@bit', `${randomBoxName}.${randomBoxName}.foo`, 'foo.js')
-      ).not.to.be.a.path();
+        path.join(helper.localScopePath, 'node_modules', '@bit', `${username}.${scopeName}.bar.foo`, 'foo.js')
+      ).to.be.a.path();
     });
     it('should delete the component from bit.map', () => {
       const bitMap = helper.readBitMap();
