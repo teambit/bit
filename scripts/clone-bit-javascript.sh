2da236b4b7bcc5ef95d40d4f81e56117ed26d028
Amit <amit@cocycles.com>
2018-02-14 21:50:16 +0200

    fix npm install for circleci

--- scripts/clone-bit-javascript.sh
+++ scripts/clone-bit-javascript.sh
@@ -1,17 +1,17 @@
 #!/bin/sh
 BIT_BRANCH=$(git rev-parse --abbrev-ref HEAD)
-echo "bit branch: $BIT_BRANCH" 
+echo "bit branch: $BIT_BRANCH"
 BIT_JS_BRANCH_EXISTS=$(git ls-remote --heads https://github.com/teambit/bit-javascript $BIT_BRANCH | wc -l)
 BIT_JS_BRANCH="master"
 
 if [ "$BIT_JS_BRANCH_EXISTS" -eq "1" ]; then
    BIT_JS_BRANCH=$BIT_BRANCH;
 fi
-echo "cloneing bit javascript: $BIT_JS_BRANCH" 
+echo "cloneing bit javascript: $BIT_JS_BRANCH"
 
 cd ../
 
-git clone --depth 1 https://github.com/teambit/bit-javascript -b $BIT_JS_BRANCH
+git clone --depth 1 https://github.com/teambit/bit-javascript -b "$BIT_JS_BRANCH"
 
 cd ./bit-javascript
 
