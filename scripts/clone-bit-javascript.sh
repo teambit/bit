1a7c6f8c152b1f874e8d3238f0bffe8441c74067
Gilad Shoham <shoham.gilad@gmail.com>
2018-02-14 16:39:02 +0200

    echo branches

--- scripts/clone-bit-javascript.sh
+++ scripts/clone-bit-javascript.sh
@@ -2,14 +2,13 @@
 set -x
 # BIT_BRANCH=$(git rev-parse --abbrev-ref HEAD)
 BIT_BRANCH="feature/vue-support"
-echo $BIT_BRANCH
+echo "bit branch: $BIT_BRANCH" 
 BIT_JS_BRANCH_EXISTS=$(git ls-remote --heads https://github.com/teambit/bit-javascript $BIT_BRANCH | wc -l)
-echo $BIT_JS_BRANCH_EXISTS
 BIT_JS_BRANCH="master"
 
 if [ "$BIT_JS_BRANCH_EXISTS" -eq "1" ]; then
    BIT_JS_BRANCH=$BIT_BRANCH;
 fi
-echo $BIT_JS_BRANCH
+echo "cloneing bit javascript: $BIT_JS_BRANCH" 
 
 git clone --depth 1 https://github.com/teambit/bit-javascript -b $BIT_JS_BRANCH
