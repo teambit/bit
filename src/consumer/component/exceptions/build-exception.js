a38661a4a726a84d899766880d980b93c225e512
Gilad Shoham <shoham.gilad@gmail.com>
2018-03-05 18:30:06 +0200

    - improve handling of errors from compilers which return promises

--- src/consumer/component/exceptions/build-exception.js
+++ src/consumer/component/exceptions/build-exception.js
@@ -3,7 +3,7 @@ export default class BuildException extends Error {
   id: string;
   message: string;
   stack: string;
-  constructor(id: string, message: string, stack: string) {
+  constructor(id: string, message?: string, stack?: string) {
     super();
     this.id = id;
     this.message = message;
