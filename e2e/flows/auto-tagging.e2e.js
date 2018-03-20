c2bb46fda59d72f0d3d032a04e1159f6cb7f4b99
David First <david@cocycles.com>
2018-03-20 08:04:36 -0400

    skipping a failing test until PR #851 is merged

--- e2e/flows/auto-tagging.e2e.js
+++ e2e/flows/auto-tagging.e2e.js
@@ -58,7 +58,8 @@ describe('auto tagging functionality', function () {
         // notice the "v2" (!)
         expect(result.trim()).to.equal('got is-type v2 and got is-string');
       });
-      describe('auto-tagging after export', () => {
+      // @todo: remove the 'skip' after merging PR https://github.com/teambit/bit/pull/851
+      describe.skip('auto-tagging after export', () => {
         before(() => {
           helper.getClonedLocalScope(clonedScope);
           const isTypeFixtureV3 = "module.exports = function isType() { return 'got is-type v3'; };";
