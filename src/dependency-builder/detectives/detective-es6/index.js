3d81457295584181d1af592cc104ef56b594de86
David First <david@cocycles.com>
2018-03-22 14:17:24 -0400

    support link-files with "export { default as ... }"; syntax

--- src/dependency-builder/detectives/detective-es6/index.js
+++ src/dependency-builder/detectives/detective-es6/index.js
@@ -42,6 +42,15 @@ module.exports = function(src, options) {
       case 'ExportAllDeclaration':
         if (node.source && node.source.value) {
           dependencies.push(node.source.value);
+          node.specifiers.forEach((specifier) => {
+            var specifierValue = {
+              isDefault: specifier.local.name === 'default', // e.g. export { default as isArray } from './is-array';
+              name: specifier.exported.name
+            };
+            importSpecifiers[node.source.value]
+              ? importSpecifiers[node.source.value].push(specifierValue)
+              : importSpecifiers[node.source.value] = [specifierValue];
+          });
         }
         break;
       case 'CallExpression':
