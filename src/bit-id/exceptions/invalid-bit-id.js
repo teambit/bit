7c155aa3b5d25a24aa893e856c79f1496531bd54
Amit <amit@cocycles.com>
2018-03-29 17:33:23 +0300

    fixed useless constructor

--- src/bit-id/exceptions/invalid-bit-id.js
+++ src/bit-id/exceptions/invalid-bit-id.js
@@ -1,8 +1,4 @@
 /** @flow */
 import AbstractError from '../../error/abstract-error';
 
-export default class InvalidBitId extends AbstractError {
-  constructor() {
-    super();
-  }
-}
+export default class InvalidBitId extends AbstractError {}
