3b4ff419ee87e46dcec29d27a48e04a2b57d86f9
amit <amitgilad3@gmail.com>
2018-04-02 14:47:39 +0300

    added link to analytics docs

--- src/prompts/schemas/analytics-reporting.js
+++ src/prompts/schemas/analytics-reporting.js
@@ -9,7 +9,7 @@ export default {
       required: true,
       default: 'Yes',
       description: `To help us prioritize new features and bug fixes, Bit can collect anonymous statistics about its usage. Staying opted-in is completely voluntary and helps us improve Bit and build a better product for you.
-Read the analytics documentation - [link]
+Read the analytics documentation - https://docs.bitsrc.io/docs/conf-analytics.html
 Is it ok to help Bit with anonymous usage analytics? [yes/no]`,
       message: 'please use yes or no.',
       type: 'string',
