918d315b95247c05e1f320ad41e294c9b9e13252
Amit <amit@cocycles.com>
2018-04-15 14:17:46 +0300

    fixed lint errors

--- src/utils/eol.js
+++ src/utils/eol.js
@@ -1,48 +1,34 @@
-const replaceBuffer = require('replace-buffer');
+/** @flow */
+import replaceBuffer from 'replace-buffer';
 
-!(function (root, name, make) {
-  if (typeof module !== 'undefined' && module.exports) module.exports = make();
-  else root[name] = make();
-}(this, 'eol', function () {
-  const api = {};
-  const isWindows = typeof process !== 'undefined' && process.platform === 'win32';
-  const linebreak = isWindows ? '\r\n' : '\n';
-  const newLines = ['\r\n', '\r', '\n'];
-  const newline = /\r\n|\r|\n/g;
+const isWindows = typeof process !== 'undefined' && process.platform === 'win32';
+const linebreak = isWindows ? '\r\n' : '\n';
+const newLines = ['\r\n', '\r', '\n'];
+const newline = /\r\n|\r|\n/g;
 
-  function before(text) {
-    return linebreak + text;
+function converts(text: string | Buffer, to: string) {
+  let str = text;
+  if (Buffer.isBuffer(text)) {
+    newLines.forEach(function (newLine) {
+      str = replaceBuffer(str, newLine, to);
+    });
+    return str;
   }
+  return str.toString().replace(newline, to);
+}
 
-  function after(text) {
-    return text + linebreak;
-  }
+exports.lf = function (text: string | Buffer) {
+  return converts(text, '\n');
+};
 
-  function converts(to) {
-    function convert(text) {
-      let str = text;
-      if (Buffer.isBuffer(text)) {
-        newLines.forEach(newLine => (str = replaceBuffer(str, newLine, to)));
-        return str;
-      }
-      return newLines.forEach(newLine => (str = str.replace(newLine, to)));
-    }
-    convert.toString = function () {
-      return to;
-    };
-    return convert;
-  }
+exports.auto = function (text: string | Buffer) {
+  return converts(text, linebreak);
+};
 
-  function split(text) {
-    return text.split(newline);
-  }
+exports.cr = function (text: string | Buffer) {
+  return converts(text, '\r');
+};
 
-  api.lf = converts('\n');
-  api.cr = converts('\r');
-  api.crlf = converts('\r\n');
-  api.auto = converts(linebreak);
-  api.before = before;
-  api.after = after;
-  api.split = split;
-  return api;
-}));
+exports.crlf = function (text: string | Buffer) {
+  return converts(text, '\r\n');
+};
