9ce205bb75d93b3a3b7f24e87149f0d421844438
Tamar Stern <tamarstern@Tamars-MacBook-Pro.local>
2018-05-03 15:09:19 +0300

    dependency status new command

---
+++ e2e/commands/dependency-status.e2e.js
@@ -0,0 +1,39 @@
+import chai, { expect } from 'chai';
+import path from 'path';
+import Helper from '../e2e-helper';
+
+describe.only('bit dependency status', function () {
+  this.timeout(0);
+  const helper = new Helper();  
+  after(() => {
+    helper.destroyEnv();
+  });
+
+  describe('all files mapped', () => {
+    before(() => {      
+      helper.setNewLocalAndRemoteScopes();
+      helper.reInitLocalScope();
+      helper.copyFixtureComponents('dependency-status');
+      helper.addComponent(path.normalize('all-files-in-component-map/a.js'));
+      helper.addComponent(path.normalize('all-files-in-component-map/b.js'));
+    });
+    it('Should verify that all dependency tree is in bitmap components', () => {
+        const output = helper.runCmd('bit dependency-status all-files-in-component-map/b.js');
+        expect(output).to.have.string('All files in dependency tree are marked as components');        
+    });
+  });
+  describe('not all files mapped', () => {
+    before(() => {      
+      helper.setNewLocalAndRemoteScopes();
+      helper.reInitLocalScope();
+      helper.copyFixtureComponents('dependency-status');
+      helper.addComponent(path.normalize('missing-files-in-component-map/a.js'));
+      helper.addComponent(path.normalize('missing-files-in-component-map/b.js'));
+    });
+    it('Should verify that all dependency tree is in bitmap components', () => {
+        const output = helper.runCmd('bit dependency-status missing-files-in-component-map/b.js');
+        expect(output).to.have.string('The following file exist in dependency tree but are not a component');        
+        expect(output).to.have.string('c.js');        
+    });
+  });
+});
---
+++ src/cli/commands/private-cmds/dependency-status-cmd.js
@@ -0,0 +1,36 @@
+/** @flow */
+import Command from '../../command';
+import chalk from 'chalk';
+import { getDependencyStatus } from '../../../api/consumer';
+import { DependencyStatusResult } from '../../../consumer/component-ops/dependency-status';
+import logger from '../../../logger/logger';
+
+export default class DependencyStatus extends Command {
+  name = 'dependency-status <main_file> ';
+  private = true;
+  description = 'returns the status of the dependency status of bit map against bit dependencies';
+  alias = '';
+  opts = [];
+  migration = true;
+
+  action([main_file]: [string]): Promise<DependencyStatusResult> {
+    logger.info('main file is ' +  main_file);
+    const dependencyStatusProps: DependencyStatusProps = {
+      main_file : main_file
+    };
+    return getDependencyStatus(dependencyStatusProps);
+}
+
+report(dependencyStatusResult: DependencyStatusResul): string {
+  if(dependencyStatusResult.missing_files.length == 0) {
+    const output = chalk.green(`All files in dependency tree are marked as components`);
+    return output;  
+  } 
+  let output = chalk.green(`The following file exist in dependency tree but are not a component:\n`);
+  output += dependencyStatusResult.missing_files.map((missing_file) => {   
+    const file = chalk.bold(missing_file + '\n');    
+    return file;
+  });
+  return output;
+}
+}
