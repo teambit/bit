cc9369a78dc6ecc05216eb7057dcaaf18f6ad769
tamarstern <tamarstern@Tamars-MacBook-Pro.local>
2018-05-07 17:32:09 +0300

    linting

--- src/consumer/component-ops/dependency-status.js
+++ src/consumer/component-ops/dependency-status.js
@@ -8,23 +8,23 @@ export type DependencyStatusResult = { missingFiles: string[] };
 export type DependencyStatusProps = { mainFile: string[] };
 
 async function getTopLevelDependencies(consumer: Consumer, dependencyStatusProps:DependencyStatusProps) {
-  const driver = await consumer.driver.getDriver(false);
-  const paths = dependencyStatusProps.mainFile;
-  const consumerPath = consumer.getPath();
-  const tree = await driver.getDependencyTree(consumerPath, consumerPath, paths, DEFAULT_BINDINGS_PREFIX);
-  const topLevelDependencies = Object.keys(tree.tree).map(topLevelFile => topLevelFile);    
-  return topLevelDependencies;
+ const driver = await consumer.driver.getDriver(false);
+ const paths = dependencyStatusProps.mainFile;
+ const consumerPath = consumer.getPath();
+ const tree = await driver.getDependencyTree(consumerPath, consumerPath, paths, DEFAULT_BINDINGS_PREFIX);
+ const topLevelDependencies = Object.keys(tree.tree).map(topLevelFile => topLevelFile);    
+ return topLevelDependencies;
 }
 
 function getComponentFiles(consumer: Consumer) {
-  const bitmap = consumer.bitMap;
-  const componentsMaps = bitmap.getAllComponents();
-  let componentFile = [];
-  Object.values(componentsMaps).forEach( function(value) {
-    const currentFiles = value.files.map(file => file.relativePath);
-    componentFile = componentFile.concat(currentFiles);
-  });
-  return componentFile;
+ const bitmap = consumer.bitMap;
+ const componentsMaps = bitmap.getAllComponents();
+ let componentFile = [];
+ Object.values(componentsMaps).forEach( function(value) {
+   const currentFiles = value.files.map(file => file.relativePath);
+   componentFile = componentFile.concat(currentFiles);
+ });
+ return componentFile;
 }
 
 export default async function getDependencyStatus(consumer: Consumer, dependencyStatusProps: DependencyStatusProps): Promise<DependencyStatusResult[]> {
@@ -40,6 +40,6 @@ export default async function getDependencyStatus(consumer: Consumer, dependency
     }
   });
 
-    const results: DependencyStatusResult = { missingFiles: missingDependencyFiles };    
-    return results;
+  const results: DependencyStatusResult = { missingFiles: missingDependencyFiles };    
+  return results;
 }
