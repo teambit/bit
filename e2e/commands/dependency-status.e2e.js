da5ba80aa114184dba3b4bd087238f51b45ceb54
tamarstern <tamarstern@Tamars-MacBook-Pro.local>
2018-05-07 13:49:45 +0300

    fix tests

--- e2e/commands/dependency-status.e2e.js
+++ e2e/commands/dependency-status.e2e.js
@@ -14,11 +14,12 @@ describe('bit dependency status', function () {
       helper.setNewLocalAndRemoteScopes();
       helper.reInitLocalScope();
       helper.copyFixtureComponents('dependency-status');
-      helper.addComponent(path.normalize('all-files-in-component-map/a.js'));
-      helper.addComponent(path.normalize('all-files-in-component-map/b.js'));
+      helper.addComponent(path.normalize('dependency-status-test-files/a.js'));
+      helper.addComponent(path.normalize('dependency-status-test-files/b.js'));
+      helper.addComponent(path.normalize('dependency-status-test-files/c.js'));
     });
     it('Should print no missing files as all files are mapped', () => {
-        const output = helper.runCmd('bit dependency-status all-files-in-component-map/b.js');
+        const output = helper.runCmd('bit dependency-status dependency-status-test-files/b.js');
         expect(output).to.have.string('All files in dependency tree are marked as components');        
     });
   });
@@ -27,11 +28,11 @@ describe('bit dependency status', function () {
       helper.setNewLocalAndRemoteScopes();
       helper.reInitLocalScope();
       helper.copyFixtureComponents('dependency-status');
-      helper.addComponent(path.normalize('missing-files-in-component-map/a.js'));
-      helper.addComponent(path.normalize('missing-files-in-component-map/b.js'));
+      helper.addComponent(path.normalize('dependency-status-test-files/a.js'));
+      helper.addComponent(path.normalize('dependency-status-test-files/b.js'));
     });
     it('Should print missing files which are not mapped to bit components', () => {
-        const output = helper.runCmd('bit dependency-status missing-files-in-component-map/b.js');
+        const output = helper.runCmd('bit dependency-status dependency-status-test-files/b.js');
         expect(output).to.have.string('The following file exist in dependency tree but are not a component');        
         expect(output).to.have.string('c.js');        
     });
