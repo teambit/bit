6bcd5a2091cb2926b6fe8c3d0efda5bdec7c9330
tamarstern <tamarstern@Tamars-MacBook-Pro.local>
2018-05-07 15:18:36 +0300

    linting

--- e2e/commands/dependency-status.e2e.js
+++ e2e/commands/dependency-status.e2e.js
@@ -19,8 +19,8 @@ describe('bit dependency status', function () {
       helper.addComponent(path.normalize('dependency-status-test-files/c.js'));
     });
     it('Should print no missing files as all files are mapped', () => {
-        const output = helper.runCmd('bit dependency-status dependency-status-test-files/b.js');
-        expect(output).to.have.string('All files in dependency tree are marked as components');        
+      const output = helper.runCmd('bit dependency-status dependency-status-test-files/b.js');
+      expect(output).to.have.string('All files in dependency tree are marked as components');        
     });
   });
   describe('not all files mapped', () => {
@@ -32,9 +32,9 @@ describe('bit dependency status', function () {
       helper.addComponent(path.normalize('dependency-status-test-files/b.js'));
     });
     it('Should print missing files which are not mapped to bit components', () => {
-        const output = helper.runCmd('bit dependency-status dependency-status-test-files/b.js');
-        expect(output).to.have.string('The following file exist in dependency tree but are not a component');        
-        expect(output).to.have.string('c.js');        
+      const output = helper.runCmd('bit dependency-status dependency-status-test-files/b.js');
+      expect(output).to.have.string('The following file exist in dependency tree but are not a component');        
+      expect(output).to.have.string('c.js');        
     });
   });
 });
