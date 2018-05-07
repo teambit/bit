a5b4379c086de0edc586f1cdd5e5165962e2c2ad
tamarstern <tamarstern@Tamars-MacBook-Pro.local>
2018-05-07 18:44:32 +0300

    remove await

--- src/api/consumer/lib/dependency_status.js
+++ src/api/consumer/lib/dependency_status.js
@@ -3,12 +3,12 @@ import type {
   DependencyStatusResult,
   DependencyStatusProps,
   getDependencyStatus
-} from '../../../consumer/component-ops/dependency-status';
+git } from '../../../consumer/component-ops/dependency-status';
 import { loadConsumer, Consumer } from '../../../consumer';
 
 export default (async function dependencyStatus(
   dependencyStatusProps: DependencyStatusProps
 ): Promise<DependencyStatusResult> {
   const consumer: Consumer = await loadConsumer();
-  return await getDependencyStatus(consumer, dependencyStatusProps);
+  return getDependencyStatus(consumer, dependencyStatusProps);
 });
