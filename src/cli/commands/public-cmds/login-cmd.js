e98bff2f988534463ec84c418d74396d7f75a5a7
amit <amitgilad3@gmail.com>
2018-05-08 13:00:19 +0300

    set bitsrc as scoped registry with bit login (#965)

--- src/cli/commands/public-cmds/login-cmd.js
+++ src/cli/commands/public-cmds/login-cmd.js
@@ -9,14 +9,16 @@ export default class Login extends Command {
   alias = '';
   opts = [
     ['p', 'port <port>', 'port number to open for localhost server (default 8085)'],
-    ['', 'no-launch-browser', 'do not open a browser for authentication']
+    ['', 'no-launch-browser', 'do not open a browser for authentication'],
+    ['', 'npmrc-path <path>', 'path to npmrc file to configure bitsrc registry'],
+    ['', 'skip-registry-config [boolean]', 'dont configure bitsrc registry'],
   ];
   // $FlowFixMe
   action(
     [nothing]: [string[]],
-    { port, noLaunchBrowser = false }: { port: string, noLaunchBrowser?: boolean }
+    { port, noLaunchBrowser = false, npmrcPath, skipRegistryConfig = false }: { port: string, noLaunchBrowser?: boolean, npmrcPath: string, skipRegistryConfig: boolean }
   ): Promise<any> {
-    return login(port, noLaunchBrowser);
+    return login(port, noLaunchBrowser, npmrcPath, skipRegistryConfig);
   }
   report({ isAlreadyLoggedIn = false, username }: { isAlreadyLoggedIn: boolean, username: string }): string {
     if (isAlreadyLoggedIn) return chalk.yellow('already logged in');
