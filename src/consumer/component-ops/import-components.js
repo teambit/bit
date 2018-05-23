ffe2ff8c518e4df80e16b72f8bcdacf6e3adf177
David First <david@bitsrc.io>
2018-05-23 10:57:53 -0400

    prevent deleting non-empty directories when running "bit import" unless --override is used (#996)

--- src/consumer/component-ops/import-components.js
+++ src/consumer/component-ops/import-components.js
@@ -1,7 +1,6 @@
 /** @flow */
 import R from 'ramda';
 import chalk from 'chalk';
-import fs from 'fs-extra';
 import { NothingToImport } from '../exceptions';
 import { BitId, BitIds } from '../../bit-id';
 import Component from '../component';
@@ -10,7 +9,7 @@ import { ComponentWithDependencies } from '../../scope';
 import loader from '../../cli/loader';
 import { BEFORE_IMPORT_ACTION } from '../../cli/loader/loader-messages';
 import logger from '../../logger/logger';
-import { filterAsync, pathNormalizeToLinux, isDir, isDirEmptySync } from '../../utils';
+import { filterAsync, pathNormalizeToLinux } from '../../utils';
 import GeneralError from '../../error/general-error';
 import type { MergeStrategy, FilesStatus } from '../versions-ops/merge-version/merge-version';
 import { applyModifiedVersion } from '../versions-ops/checkout-version';
@@ -75,7 +74,6 @@ export default class ImportComponents {
     logger.debug(`importSpecificComponents, Ids: ${this.options.ids.join(', ')}`);
     // $FlowFixMe - we check if there are bitIds before we call this function
     const bitIds = this.options.ids.map(raw => BitId.parse(raw));
-    if (this.options.writeToPath) this._throwErrorWhenDirectoryNotEmpty(this.options.writeToPath);
     const beforeImportVersions = await this._getCurrentVersions(bitIds);
     await this._warnForModifiedOrNewComponents(bitIds);
     const componentsWithDependencies = await this.consumer.scope.getManyWithAllVersions(bitIds, false);
@@ -85,7 +83,7 @@ export default class ImportComponents {
   }
 
   async importAccordingToBitJsonAndBitMap(): ImportResult {
-    this.options.objectsOnly = !(this.options.merge || this.options.override);
+    this.options.objectsOnly = !this.options.merge && !this.options.override;
 
     const dependenciesFromBitJson = BitIds.fromObject(this.consumer.bitJson.dependencies);
     const componentsFromBitMap = this.consumer.bitMap.getAuthoredExportedComponents();
@@ -117,7 +115,7 @@ export default class ImportComponents {
       this.options.writePackageJson = false;
       this.options.installNpmPackages = false;
       // don't force the writing to the filesystem because as an author I may have some modified files
-      await this._writeToFileSystem(componentsAndDependenciesBitMap, false);
+      await this._writeToFileSystem(componentsAndDependenciesBitMap);
     }
     const componentsAndDependencies = [...componentsAndDependenciesBitJson, ...componentsAndDependenciesBitMap];
     const importDetails = await this._getImportDetails(beforeImportVersions, componentsAndDependencies);
@@ -198,19 +196,6 @@ export default class ImportComponents {
       );
     }
     return Promise.resolve();
-  }
-
-  _throwErrorWhenDirectoryNotEmpty(writeToPath: string) {
-    if (fs.pathExistsSync(writeToPath)) {
-      if (!isDir(writeToPath)) {
-        throw new GeneralError(`unable to import to ${writeToPath} because it's a file`);
-      }
-      if (!isDirEmptySync(writeToPath) && !this.options.override) {
-        throw new GeneralError(
-          `unable to import to ${writeToPath}, the directory is not empty. use --override flag to delete the directory and then import`
-        );
-      }
-    }
   }
 
   async _getMergeStatus(componentWithDependencies: ComponentWithDependencies): Promise<ComponentMergeStatus> {
@@ -316,7 +301,7 @@ export default class ImportComponents {
     return removeNulls(componentsToWrite);
   }
 
-  async _writeToFileSystem(componentsWithDependencies: ComponentWithDependencies[], override: boolean = true) {
+  async _writeToFileSystem(componentsWithDependencies: ComponentWithDependencies[]) {
     if (this.options.objectsOnly) return;
     const componentsToWrite = await this.updateAllComponentsAccordingToMergeStrategy(componentsWithDependencies);
     await writeComponents({
@@ -329,7 +314,7 @@ export default class ImportComponents {
       installNpmPackages: this.options.installNpmPackages,
       saveDependenciesAsComponents: this.options.saveDependenciesAsComponents,
       verbose: this.options.verbose,
-      override
+      override: this.options.override
     });
   }
 }
