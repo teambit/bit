db3083ded175447a43d145e313ae7c7eb09579b5
David First <david@cocycles.com>
2018-06-26 14:45:04 +0300

    fix detection of "export * from" syntax of ES6

--- src/dependency-builder/detectives/detective-es6/index.js
+++ src/dependency-builder/detectives/detective-es6/index.js
@@ -1,8 +1,8 @@
 /**
-* this file had been forked (and changed since then) from https://github.com/dependents/node-detective-es6
-*/
+ * this file had been forked (and changed since then) from https://github.com/dependents/node-detective-es6
+ */
 
-var Walker = require('node-source-walk');
+const Walker = require('node-source-walk');
 
 /**
  * Extracts the dependencies of the supplied es6 module
@@ -10,8 +10,8 @@ var Walker = require('node-source-walk');
  * @param  {String|Object} src - File's content or AST
  * @return {String[]}
  */
-module.exports = function(src) {
-  var walker = new Walker();
+module.exports = function (src) {
+  const walker = new Walker();
 
   const dependencies = {};
   const addDependency = (dependency) => {
@@ -27,13 +27,15 @@ module.exports = function(src) {
     }
   };
 
-  if (typeof src === 'undefined') { throw new Error('src not given'); }
+  if (typeof src === 'undefined') {
+    throw new Error('src not given');
+  }
 
   if (src === '') {
     return dependencies;
   }
 
-  walker.walk(src, function(node) {
+  walker.walk(src, function (node) {
     switch (node.type) {
       case 'ImportDeclaration':
         if (node.source && node.source.value) {
@@ -53,34 +55,40 @@ module.exports = function(src) {
         if (node.source && node.source.value) {
           const dependency = node.source.value;
           addDependency(dependency);
-          node.specifiers.forEach((specifier) => {
-            const specifierValue = {
-              isDefault: !specifier.local || specifier.local.name === 'default', // e.g. export { default as isArray } from './is-array';
-              name: specifier.exported.name
-            };
-            addImportSpecifier(dependency, specifierValue);
-          });
+          if (node.specifiers) {
+            // in case of "export * from" there are no node.specifiers
+            node.specifiers.forEach((specifier) => {
+              const specifierValue = {
+                isDefault: !specifier.local || specifier.local.name === 'default', // e.g. export { default as isArray } from './is-array';
+                name: specifier.exported.name
+              };
+              addImportSpecifier(dependency, specifierValue);
+            });
+          }
         }
         break;
       case 'CallExpression':
         if (node.callee.type === 'Import' && node.arguments.length) {
           addDependency(node.arguments[0].value);
         }
-        if (node.callee.type === 'Identifier' // taken from detective-cjs
-        && node.callee.name === 'require'
-        && node.arguments
-        && node.arguments.length
-        && (node.arguments[0].type === 'Literal' || node.arguments[0].type === 'StringLiteral')) {
+        if (
+          node.callee.type === 'Identifier' && // taken from detective-cjs
+          node.callee.name === 'require' &&
+          node.arguments &&
+          node.arguments.length &&
+          (node.arguments[0].type === 'Literal' || node.arguments[0].type === 'StringLiteral')
+        ) {
           addDependency(node.arguments[0].value);
         }
         break;
       case 'MemberExpression':
-        if (node.object.type === 'CallExpression'
-         && node.object.callee.type === 'Identifier'
-         && node.object.callee.name === 'require'
-         && node.object.arguments
-         && node.object.arguments.length
-         && (node.object.arguments[0].type === 'Literal' || node.object.arguments[0].type === 'StringLiteral')
+        if (
+          node.object.type === 'CallExpression' &&
+          node.object.callee.type === 'Identifier' &&
+          node.object.callee.name === 'require' &&
+          node.object.arguments &&
+          node.object.arguments.length &&
+          (node.object.arguments[0].type === 'Literal' || node.object.arguments[0].type === 'StringLiteral')
         ) {
           const depValue = node.object.arguments[0].value;
           addDependency(depValue);
@@ -91,14 +99,16 @@ module.exports = function(src) {
             };
             addImportSpecifier(depValue, specifierValue);
           }
-          if (node.property && node.property.type === 'Identifier'
-            && node.parent.type === 'AssignmentExpression'
-            && node.parent.left === 'MemberExpression'
-            && node.parent.left.object === 'MemberExpression'
-            && node.parent.left.object.object.type === 'Identifier'
-            && node.parent.left.object.object.name === 'module'
-            && node.parent.left.property.type === 'Identifier'
-           ) {
+          if (
+            node.property &&
+            node.property.type === 'Identifier' &&
+            node.parent.type === 'AssignmentExpression' &&
+            node.parent.left === 'MemberExpression' &&
+            node.parent.left.object === 'MemberExpression' &&
+            node.parent.left.object.object.type === 'Identifier' &&
+            node.parent.left.object.object.name === 'module' &&
+            node.parent.left.property.type === 'Identifier'
+          ) {
             const specifierValue = {
               isDefault: node.property.name === 'default', // e.g. module.exports.DraggableCore = require('./lib/DraggableCore').default;
               name: node.parent.left.property.name
--- src/dependency-builder/detectives/detective-es6/index.spec.js
+++ src/dependency-builder/detectives/detective-es6/index.spec.js
@@ -1,107 +1,116 @@
 /**
-* this file had been forked (and changed since then) from https://github.com/dependents/node-detective-es6
-*/
+ * this file had been forked (and changed since then) from https://github.com/dependents/node-detective-es6
+ */
 
 const assert = require('assert');
 const detective = require('./');
 
-describe('detective-es6', function() {
-  var ast = {
+describe('detective-es6', () => {
+  const ast = {
     type: 'Program',
-    body: [{
-      type: 'VariableDeclaration',
-      declarations: [{
-        type: 'VariableDeclarator',
-        id: {
-            type: 'Identifier',
-            name: 'x'
-        },
-        init: {
-            type: 'Literal',
-            value: 4,
-            raw: '4'
-        }
-      }],
-      kind: 'let'
-    }]
+    body: [
+      {
+        type: 'VariableDeclaration',
+        declarations: [
+          {
+            type: 'VariableDeclarator',
+            id: {
+              type: 'Identifier',
+              name: 'x'
+            },
+            init: {
+              type: 'Literal',
+              value: 4,
+              raw: '4'
+            }
+          }
+        ],
+        kind: 'let'
+      }
+    ]
   };
 
-  it('accepts an ast', function() {
-    var deps = detective(ast);
-    assert(!deps.length);
+  it('accepts an ast', () => {
+    const deps = detective(ast);
+    const depsKeys = Object.keys(deps);
+    assert(!depsKeys.length);
   });
 
-  it('retrieves the dependencies of es6 modules', function() {
-    var deps = detective('import {foo, bar} from "mylib";');
+  it('retrieves the dependencies of es6 modules', () => {
+    const deps = detective('import {foo, bar} from "mylib";');
     const depsKeys = Object.keys(deps);
     assert(depsKeys.length === 1);
     assert(depsKeys[0] === 'mylib');
   });
 
-  it('retrieves the re-export dependencies of es6 modules', function() {
+  it('retrieves the re-export dependencies of es6 modules', () => {
     const deps = detective('export {foo, bar} from "mylib";');
     const depsKeys = Object.keys(deps);
     assert(depsKeys.length === 1);
     assert(depsKeys[0] === 'mylib');
   });
 
-  it('retrieves the re-export * dependencies of es6 modules', function() {
-    var deps = detective('export * from "mylib";');
+  it('retrieves the re-export * dependencies of es6 modules', () => {
+    const deps = detective('export * from "mylib";');
     const depsKeys = Object.keys(deps);
     assert(depsKeys.length === 1);
     assert(depsKeys[0] === 'mylib');
   });
 
-  it('handles multiple imports', function() {
-    var deps = detective('import {foo, bar} from "mylib";\nimport "mylib2"');
+  it('handles multiple imports', () => {
+    const deps = detective('import {foo, bar} from "mylib";\nimport "mylib2"');
     const depsKeys = Object.keys(deps);
     assert(depsKeys.length === 2);
     assert(depsKeys[0] === 'mylib');
     assert(depsKeys[1] === 'mylib2');
   });
 
-  it('handles default imports', function() {
-    var deps = detective('import foo from "foo";');
+  it('handles default imports', () => {
+    const deps = detective('import foo from "foo";');
     const depsKeys = Object.keys(deps);
     assert(depsKeys.length === 1);
     assert(depsKeys[0] === 'foo');
   });
 
-  it('handles dynamic imports', function() {
-    var deps = detective('import("foo").then(foo => foo());');
-    const depsKeys = Object.keys(deps);
-    assert(depsKeys.length === 1);
-    assert(depsKeys[0] === 'foo');
-  })
-
-  it('should support commonJS syntax', function() {
-    var deps = detective('var foo = require("foo");');
+  it('handles dynamic imports', function () {
+    const deps = detective('import("foo").then(foo => foo());');
     const depsKeys = Object.keys(deps);
     assert(depsKeys.length === 1);
     assert(depsKeys[0] === 'foo');
   });
 
-  it('returns an empty list for empty files', function() {
-    var deps = detective('');
+  it('should support commonJS syntax', function () {
+    const deps = detective('var foo = require("foo");');
+    const depsKeys = Object.keys(deps);
+    assert(depsKeys.length === 1);
+    assert(depsKeys[0] === 'foo');
+  });
+
+  it('returns an empty list for empty files', function () {
+    const deps = detective('');
     const depsKeys = Object.keys(deps);
     assert.equal(depsKeys.length, 0);
   });
 
-  it('throws when content is not provided', function() {
-    assert.throws(function() {
-      detective();
-    }, Error, 'src not given');
+  it('throws when content is not provided', function () {
+    assert.throws(
+      function () {
+        detective();
+      },
+      Error,
+      'src not given'
+    );
   });
 
-  it('does not throw with jsx in a module', function() {
-    assert.doesNotThrow(function() {
-      detective('import foo from \'foo\'; var templ = <jsx />;');
+  it('does not throw with jsx in a module', function () {
+    assert.doesNotThrow(function () {
+      detective("import foo from 'foo'; var templ = <jsx />;");
     });
   });
 
-  it('does not throw on an async ES7 function', function() {
-    assert.doesNotThrow(function() {
-      detective('import foo from \'foo\'; export default async function foo() {}');
+  it('does not throw on an async ES7 function', function () {
+    assert.doesNotThrow(function () {
+      detective("import foo from 'foo'; export default async function foo() {}");
     });
   });
 });
