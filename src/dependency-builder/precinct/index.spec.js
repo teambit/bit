9693776c03ba47eec5e6d3044d557a6879f4e62c
David First <david@cocycles.com>
2018-06-27 08:01:56 -0400

    add back Precinct unit tests (were deleted by mistake)

---
+++ src/dependency-builder/precinct/index.spec.js
@@ -0,0 +1,293 @@
+import { expect } from 'chai';
+
+const assert = require('assert');
+const fs = require('fs');
+const path = require('path');
+const rewire = require('rewire');
+const sinon = require('sinon');
+
+const fixtures = `${__dirname}/../../../fixtures/precinct`;
+const ast = require(`${fixtures}/exampleAST`);
+const precinct = rewire('./');
+
+function read(filename) {
+  return fs.readFileSync(path.join(fixtures, filename), 'utf8');
+}
+
+describe('node-precinct', () => {
+  it('accepts an AST', () => {
+    const deps = precinct(ast);
+    const depsKeys = Object.keys(deps);
+    assert(depsKeys.length === 1);
+  });
+
+  it('dangles off a given ast', () => {
+    const deps = precinct(ast);
+    assert.deepEqual(precinct.ast, ast);
+  });
+
+  it('dangles off the parsed ast from a .js file', () => {
+    precinct(read('amd.js'));
+    assert.ok(precinct.ast);
+    assert.notDeepEqual(precinct.ast, ast);
+  });
+
+  it('dangles off the parsed ast from a scss detective', () => {
+    precinct(read('styles.scss'), 'scss');
+    assert.notDeepEqual(precinct.ast, {});
+  });
+
+  it('dangles off the parsed ast from a sass detective', () => {
+    precinct(read('styles.sass'), 'sass');
+    assert.notDeepEqual(precinct.ast, {});
+  });
+
+  it('grabs dependencies of amd modules', () => {
+    const amd = precinct(read('amd.js'));
+    assert(amd.indexOf('./a') !== -1);
+    assert(amd.indexOf('./b') !== -1);
+    assert(amd.length === 2);
+  });
+
+  it('grabs dependencies of commonjs modules', () => {
+    const cjs = precinct(read('commonjs.js'));
+    expect(cjs).to.have.property('./a');
+    expect(cjs).to.have.property('./b');
+    assert(Object.keys(cjs).length === 2);
+  });
+
+  it('grabs dependencies of es6 modules', () => {
+    const cjs = precinct(read('es6.js'));
+    expect(cjs).to.have.property('lib');
+    assert(Object.keys(cjs).length === 1);
+  });
+
+  it('grabs dependencies of es6 modules with embedded jsx', () => {
+    const cjs = precinct(read('jsx.js'));
+    expect(cjs).to.have.property('lib');
+    assert(Object.keys(cjs).length === 1);
+  });
+
+  it('grabs dependencies of es6 modules with embedded es7', () => {
+    const cjs = precinct(read('es7.js'));
+    expect(cjs).to.have.property('lib');
+    assert(Object.keys(cjs).length === 1);
+  });
+
+  it('does not grabs dependencies of es6 modules with syntax errors', () => {
+    const cjs = precinct(read('es6WithError.js'));
+    assert(Object.keys(cjs).length === 0);
+  });
+
+  // @todo: fix. it might be the postcss
+  it.skip('grabs dependencies of css files', () => {
+    const css = precinct(read('styles.css'), 'css');
+    expect(css).to.have.property('foo.css');
+    expect(css).to.have.property('baz.css');
+    expect(css).to.have.property('bla.css');
+    expect(css).to.have.property('another.css');
+  });
+
+  it('grabs dependencies of scss files', function () {
+    const scss = precinct(read('styles.scss'), 'scss');
+    assert.deepEqual(scss, ['_foo', 'baz.scss']);
+  });
+
+  it('grabs dependencies of sass files', () => {
+    const sass = precinct(read('styles.sass'), 'sass');
+    assert.deepEqual(sass, ['_foo']);
+  });
+
+  it('grabs dependencies of stylus files', () => {
+    const result = precinct(read('styles.styl'), 'stylus');
+    const expected = ['mystyles', 'styles2.styl', 'styles3.styl', 'styles4'];
+
+    assert.deepEqual(result, expected);
+  });
+
+  it('grabs dependencies of less files', () => {
+    const result = precinct(read('styles.less'), 'less');
+    const expected = ['_foo', '_bar.css', 'baz.less'];
+
+    assert.deepEqual(result, expected);
+  });
+
+  it('grabs dependencies of typescript files', () => {
+    const result = precinct(read('typescript.ts'), 'ts');
+    const expected = ['fs', 'lib', './bar', './my-module.js', './ZipCodeValidator'];
+
+    assert.deepEqual(Object.keys(result), expected);
+  });
+
+  it('does not grabs dependencies of typescript modules with syntax errors', () => {
+    const result = precinct(read('typescriptWithError.ts'));
+    assert(result.length === 0);
+  });
+
+  it('supports the object form of type configuration', () => {
+    const result = precinct(read('styles.styl'), { type: 'stylus' });
+    const expected = ['mystyles', 'styles2.styl', 'styles3.styl', 'styles4'];
+
+    assert.deepEqual(result, expected);
+  });
+
+  it('yields no dependencies for es6 modules with no imports', () => {
+    const cjs = precinct(read('es6NoImport.js'));
+    assert.equal(Object.keys(cjs).length, 0);
+  });
+
+  it('yields no dependencies for non-modules', () => {
+    const none = precinct(read('none.js'));
+    assert.equal(Object.keys(none).length, 0);
+  });
+
+  it('ignores unparsable .js files', () => {
+    const cjs = precinct(read('unparseable.js'));
+
+    assert(cjs.indexOf('lib') < 0);
+    assert.equal(cjs.length, 0);
+  });
+
+  it('does not throw on unparsable .js files', () => {
+    assert.doesNotThrow(() => {
+      precinct(read('unparseable.js'));
+    }, SyntaxError);
+  });
+
+  it('does not blow up when parsing a gruntfile #2', () => {
+    assert.doesNotThrow(() => {
+      precinct(read('Gruntfile.js'));
+    });
+  });
+
+  describe('paperwork', () => {
+    it('returns the dependencies for the given filepath', () => {
+      assert.ok(Object.keys(precinct.paperwork(`${fixtures}/es6.js`)).length);
+      assert.ok(Object.keys(precinct.paperwork(`${fixtures}/styles.scss`)).length);
+      assert.ok(Object.keys(precinct.paperwork(`${fixtures}/typescript.ts`)).length);
+      assert.ok(Object.keys(precinct.paperwork(`${fixtures}/styles.css`)).length);
+    });
+
+    it('throws if the file cannot be found', () => {
+      assert.throws(() => {
+        precinct.paperwork('foo');
+      });
+    });
+
+    it('filters out core modules if options.includeCore is false', () => {
+      const deps = precinct.paperwork(`${fixtures}/coreModules.js`, {
+        includeCore: false
+      });
+
+      assert(!deps.length);
+    });
+
+    it('does not filter out core modules by default', () => {
+      const deps = precinct.paperwork(`${fixtures}/coreModules.js`);
+      assert(Object.keys(deps).length);
+    });
+
+    it('supports passing detective configuration', () => {
+      const stub = sinon.stub().returns([]);
+      const revert = precinct.__set__('detectiveAmd', stub);
+      const config = {
+        amd: {
+          skipLazyLoaded: true
+        }
+      };
+
+      const deps = precinct.paperwork(`${fixtures}/amd.js`, {
+        includeCore: false,
+        amd: config.amd
+      });
+
+      assert.deepEqual(stub.args[0][1], config.amd);
+      revert();
+    });
+
+    describe('when given detective configuration', () => {
+      it('still does not filter out core module by default', () => {
+        const stub = sinon.stub().returns([]);
+        const revert = precinct.__set__('precinct', stub);
+
+        const deps = precinct.paperwork(`${fixtures}/amd.js`, {
+          amd: {
+            skipLazyLoaded: true
+          }
+        });
+
+        assert.equal(stub.args[0][1].includeCore, true);
+        revert();
+      });
+    });
+  });
+
+  describe('when given a configuration object', () => {
+    it('passes amd config to the amd detective', () => {
+      const stub = sinon.stub();
+      const revert = precinct.__set__('detectiveAmd', stub);
+      const config = {
+        amd: {
+          skipLazyLoaded: true
+        }
+      };
+
+      precinct(read('amd.js'), config);
+
+      assert.deepEqual(stub.args[0][1], config.amd);
+      revert();
+    });
+
+    describe('that sets mixedImports for es6', () => {
+      describe('for a file identified as es6', () => {
+        it('returns both the commonjs and es6 dependencies', () => {
+          const deps = precinct(read('es6MixedImport.js'), {
+            es6: {
+              mixedImports: true
+            }
+          });
+
+          assert.equal(Object.keys(deps).length, 2);
+        });
+      });
+
+      describe('for a file identified as cjs', () => {
+        it('returns both the commonjs and es6 dependencies', () => {
+          const deps = precinct(read('cjsMixedImport.js'), {
+            es6: {
+              mixedImports: true
+            }
+          });
+
+          assert.equal(Object.keys(deps).length, 2);
+        });
+      });
+    });
+  });
+
+  describe('when lazy exported dependencies in CJS', () => {
+    it('grabs those lazy dependencies', () => {
+      const cjs = precinct(read('cjsExportLazy.js'));
+      expect(cjs).to.have.property('./amd');
+      expect(cjs).to.have.property('./es6');
+      expect(cjs).to.have.property('./es7');
+      assert.equal(Object.keys(cjs).length, 3);
+    });
+  });
+
+  describe('when given an es6 file', () => {
+    describe('that uses CJS imports for lazy dependencies', () => {
+      it('grabs the lazy imports', () => {
+        const es6 = precinct(read('es6MixedExportLazy.js'), {
+          es6: {
+            mixedImports: true
+          }
+        });
+        expect(es6).to.have.property('./amd');
+        expect(es6).to.have.property('./es6');
+        expect(es6).to.have.property('./es7');
+        assert.equal(Object.keys(es6).length, 3);
+      });
+    });
+  });
+});
