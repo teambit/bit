7d6c6bc8f63faffd0c8d1eaf7dd172d7a9ea6385
Gilad Shoham <shoham.gilad@gmail.com>
2018-07-30 17:00:05 +0300

    rename strip trailing char

--- src/utils/string/strip-trailing-char.js
+++ src/utils/string/strip-trailing-char.js
@@ -1,5 +1,8 @@
 // @flow
 export default function stripTrailingChar(str: string, char: string): string {
+  if (!str || !char) {
+    return str;
+  }
   if (str[str.length - 1] === char) {
     return str.slice(0, -1);
   }
