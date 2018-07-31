240e03170edfada5f9248532ec99c50057a9f371
Gilad Shoham <shoham.gilad@gmail.com>
2018-07-31 13:09:28 +0300

    improve eject conf outputs

--- src/cli/commands/public-cmds/eject-conf-cmd.js
+++ src/cli/commands/public-cmds/eject-conf-cmd.js
@@ -1,11 +1,13 @@
 /** @flow */
+import chalk from 'chalk';
 import * as nodePath from 'path';
 import Command from '../../command';
 import { ejectConf } from '../../../api/consumer';
 import type { EjectConfResult } from '../../../consumer/component-ops/eject-conf';
+import type { PathOsBased } from '../../../utils/path';
 
 type EjectConfCliResult = EjectConfResult & {
-  ejectPathRelativeToCwd: string
+  ejectPathRelativeToCwd: PathOsBased
 };
 
 export default class EjectConf extends Command {
@@ -24,6 +26,8 @@ export default class EjectConf extends Command {
   }
 
   report(ejectResults: EjectConfCliResult): string {
-    return `${ejectResults.id} configuration ejected to:\n${ejectResults.ejectPathRelativeToCwd}`;
+    return `successfully ejected ${chalk.bold(ejectResults.id)} configuration to:\n${chalk.green(
+      ejectResults.ejectPathRelativeToCwd
+    )}`;
   }
 }
