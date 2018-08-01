39ba46d1b47c1b93d6ba88d01977e7852d520cee
David First <david@bitsrc.io>
2018-08-01 11:49:02 -0400

    remove old/wrong implementation of envs-dependencies

--- src/consumer/component/dependencies/dependency-resolver/index.js
+++ src/consumer/component/dependencies/dependency-resolver/index.js
@@ -2,4 +2,3 @@
 
 export { default as loadDependenciesForComponent } from './dependencies-resolver';
 export { default as updateDependenciesVersions } from './dependencies-versions-resolver';
-export { default as dependenciesEnvsResolver } from './dependencies-envs-resolver';
