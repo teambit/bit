8f26796da8fcffbd0a750288490bd446f1bb3f24
Gilad Shoham <shoham.gilad@gmail.com>
2018-08-19 14:09:28 +0300

    linting

--- src/consumer/component-ops/import-components.js
+++ src/consumer/component-ops/import-components.js
@@ -17,7 +17,6 @@ import { threeWayMerge, MergeOptions, FileStatus, getMergeStrategyInteractive } 
 import Version from '../../version';
 import type { MergeResultsThreeWay } from '../versions-ops/merge-version/three-way-merge';
 import writeComponents from './write-components';
-import ConfigDir from '../bit-map/config-dir';
 
 export type ImportOptions = {
   ids: string[], // array might be empty
