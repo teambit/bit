4552443e63b8e6745d8df345059ed31c586bf1b1
Gilad Shoham <shoham.gilad@gmail.com>
2018-08-22 15:56:07 +0300

    WIP - validating schema during extension loading

--- src/extensions/exceptions/extension-load-error.js
+++ src/extensions/exceptions/extension-load-error.js
@@ -4,9 +4,11 @@ import ExternalError from '../../error/external-error';
 
 export default class ExtensionLoadError extends ExternalError {
   name: string;
+  printStack: boolean;
 
-  constructor(originalError: Error, name: string) {
+  constructor(originalError: Error, name: string, printStack: boolean = true) {
     super(originalError);
     this.name = name;
+    this.printStack = printStack;
   }
 }
