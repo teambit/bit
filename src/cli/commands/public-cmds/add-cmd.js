43ae853c579e2f910acd8814e0227d5e24c333eb
David First <david@bitsrc.io>
2018-09-26 11:04:56 -0400

    a temporarily workaround to not having a namespace for bit-add

--- src/cli/commands/public-cmds/add-cmd.js
+++ src/cli/commands/public-cmds/add-cmd.js
@@ -23,6 +23,7 @@ export default class Add extends Command {
     ['m', 'main <file>', 'implementation/index file name or dsl (src/{PARENT}/{FILE_NAME})'],
     ['t', 'tests <file...>', 'spec/test file name or dsl (tests/{PARENT}/{FILE_NAME})'],
     ['n', 'namespace <namespace>', 'component namespace'],
+    ['N', 'no-namespace', 'do not add any namespace'],
     ['e', 'exclude <file...>', 'exclude file name or dsl (src/{PARENT}/{FILE_NAME})'],
     ['o', 'override <boolean>', 'override existing component if exists (default = false)']
   ];
@@ -36,6 +37,7 @@ export default class Add extends Command {
       main,
       tests,
       namespace,
+      noNamespace = false,
       exclude,
       override = false
     }: {
@@ -43,6 +45,7 @@ export default class Add extends Command {
       main: ?string,
       tests: ?(string[]),
       namespace: ?string,
+      noNamespace: boolean,
       exclude: ?(string[]),
       override: boolean
     }
@@ -67,6 +70,7 @@ export default class Add extends Command {
       id,
       main: main ? path.normalize(main) : undefined,
       namespace,
+      noNamespace,
       tests: testsArray,
       exclude: excludedFiles,
       override
