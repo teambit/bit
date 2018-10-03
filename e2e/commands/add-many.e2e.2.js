3eede82aa1c13f460f2c7ca057014ff884f3d559
tamarstern <tamarstern@Tamars-MacBook-Pro.local>
2018-10-03 20:25:08 +0300

    fix test

--- e2e/commands/add-many.e2e.2.js
+++ e2e/commands/add-many.e2e.2.js
@@ -34,9 +34,14 @@ describe('bit add many programmatically', function () {
       const scriptPath = path.join(helper.localScopePath, 'add_many_test_files/add_components_programmatically.js');
       nodeStartOutput = helper.nodeStart(`${scriptPath} PROCESS`);
       nodeStartOutputObj = JSON.parse(nodeStartOutput);
+      nodeStartOutputObj = nodeStartOutputObj.sort(function (a, b) {
+        const textA = a.addedComponents[0].id.toLowerCase();
+        const textB = b.addedComponents[0].id.toLowerCase();
+        return textA < textB ? -1 : textA > textB ? 1 : 0;
+      });
       expect(nodeStartOutputObj[0]).to.have.property('addedComponents');
       expect(nodeStartOutputObj[0].addedComponents[0]).to.have.property('id');
-      expect(nodeStartOutputObj[0].addedComponents[0].id).to.equal('my_namespace/b');
+      expect(nodeStartOutputObj[0].addedComponents[0].id).to.equal('add_many_test_files/c');
     });
   });
   describe('should add many components programmatically, process.cwd() is inside project path', function () {
@@ -98,30 +103,35 @@ describe('bit add many programmatically', function () {
       const scriptAbsolutePath = path.join(newDirPath, 'add_many_test_files/add_components_programmatically.js');
       nodeStartOutput = helper.nodeStart(`${scriptAbsolutePath} ${helper.localScopePath}`, process.cwd());
       nodeStartOutputObj = JSON.parse(nodeStartOutput);
+      nodeStartOutputObj = nodeStartOutputObj.sort(function (a, b) {
+        const textA = a.addedComponents[0].id.toLowerCase();
+        const textB = b.addedComponents[0].id.toLowerCase();
+        return textA < textB ? -1 : textA > textB ? 1 : 0;
+      });
       status = helper.status();
     });
     it('should add a component with no id and no spec', function () {
       expect(nodeStartOutputObj).to.be.array();
-      expect(nodeStartOutputObj[2]).to.have.property('addedComponents');
-      expect(nodeStartOutputObj[2].addedComponents[0]).to.have.property('id');
-      expect(nodeStartOutputObj[2].addedComponents[0].id).to.equal('add_many_test_files/c');
-      expect(nodeStartOutputObj[2].addedComponents[0]).to.have.property('files');
-      expect(nodeStartOutputObj[2].addedComponents[0].files).to.be.array();
-      expect(nodeStartOutputObj[2].addedComponents[0].files).to.be.ofSize(1);
-      expect(nodeStartOutputObj[2].addedComponents[0].files[0].test).to.equal(false);
+      expect(nodeStartOutputObj[0]).to.have.property('addedComponents');
+      expect(nodeStartOutputObj[0].addedComponents[0]).to.have.property('id');
+      expect(nodeStartOutputObj[0].addedComponents[0].id).to.equal('add_many_test_files/c');
+      expect(nodeStartOutputObj[0].addedComponents[0]).to.have.property('files');
+      expect(nodeStartOutputObj[0].addedComponents[0].files).to.be.array();
+      expect(nodeStartOutputObj[0].addedComponents[0].files).to.be.ofSize(1);
+      expect(nodeStartOutputObj[0].addedComponents[0].files[0].test).to.equal(false);
       expect(status).to.have.string('add_many_test_files/c ... ok');
       const compData = JSON.parse(helper.showComponentWithOptions('add_many_test_files/c', { j: '' }));
       expect(compData).to.not.property('Specs');
     });
     it('should add a component with spec file', function () {
       expect(nodeStartOutputObj).to.be.array();
-      expect(nodeStartOutputObj[1]).to.have.property('addedComponents');
-      expect(nodeStartOutputObj[1].addedComponents[0].files).to.be.array();
-      expect(nodeStartOutputObj[1].addedComponents[0].files).to.be.ofSize(2);
-      expect(nodeStartOutputObj[1].addedComponents[0].files[0].test).to.equal(false);
-      expect(nodeStartOutputObj[1].addedComponents[0].files[0].name).to.equal('a.js');
-      expect(nodeStartOutputObj[1].addedComponents[0].files[1].test).to.equal(true);
-      expect(nodeStartOutputObj[1].addedComponents[0].files[1].name).to.equal('a.spec.js');
+      expect(nodeStartOutputObj[3]).to.have.property('addedComponents');
+      expect(nodeStartOutputObj[3].addedComponents[0].files).to.be.array();
+      expect(nodeStartOutputObj[3].addedComponents[0].files).to.be.ofSize(2);
+      expect(nodeStartOutputObj[3].addedComponents[0].files[0].test).to.equal(false);
+      expect(nodeStartOutputObj[3].addedComponents[0].files[0].name).to.equal('a.js');
+      expect(nodeStartOutputObj[3].addedComponents[0].files[1].test).to.equal(true);
+      expect(nodeStartOutputObj[3].addedComponents[0].files[1].name).to.equal('a.spec.js');
       expect(status).to.have.string('add_many_test_files/my_defined_id ... ok');
       const compData = JSON.parse(helper.showComponentWithOptions('add_many_test_files/my_defined_id', { j: '' }));
       expect(compData).to.have.property('files');
@@ -129,8 +139,8 @@ describe('bit add many programmatically', function () {
     });
     it('should add a component with user defined id', function () {
       expect(nodeStartOutputObj).to.be.array();
-      expect(nodeStartOutputObj[1]).to.have.property('addedComponents');
-      expect(nodeStartOutputObj[1].addedComponents[0].id).to.equal('add_many_test_files/my_defined_id');
+      expect(nodeStartOutputObj[3]).to.have.property('addedComponents');
+      expect(nodeStartOutputObj[3].addedComponents[0].id).to.equal('add_many_test_files/my_defined_id');
       expect(status).to.have.string('add_many_test_files/my_defined_id ... ok');
       const compData = JSON.parse(helper.showComponentWithOptions('add_many_test_files/my_defined_id', { j: '' }));
       expect(compData).to.have.property('name');
@@ -138,32 +148,32 @@ describe('bit add many programmatically', function () {
     });
     it('should add a component with namespace and no id', async function () {
       expect(nodeStartOutputObj).to.be.array();
-      expect(nodeStartOutputObj[0]).to.have.property('addedComponents');
-      expect(nodeStartOutputObj[0].addedComponents[0].id).to.equal('my_namespace/b');
+      expect(nodeStartOutputObj[4]).to.have.property('addedComponents');
+      expect(nodeStartOutputObj[4].addedComponents[0].id).to.equal('my_namespace/b');
       expect(status).to.have.string('my_namespace/b ... ok');
       const compData = JSON.parse(helper.showComponentWithOptions('my_namespace/b', { j: '' }));
       expect(compData).to.have.property('name');
       expect(compData.name).to.equal('my_namespace/b');
     });
     it('should add a component with excluded test file', function () {
-      expect(nodeStartOutputObj[4].addedComponents[0]).to.have.property('id');
-      expect(nodeStartOutputObj[4].addedComponents[0].id).to.equal('add_many_test_files/d');
-      expect(nodeStartOutputObj[4].addedComponents[0]).to.have.property('files');
-      expect(nodeStartOutputObj[4].addedComponents[0].files).to.be.array();
-      expect(nodeStartOutputObj[4].addedComponents[0].files).to.be.ofSize(1);
-      expect(nodeStartOutputObj[4].addedComponents[0].files[0].test).to.equal(false);
+      expect(nodeStartOutputObj[2].addedComponents[0]).to.have.property('id');
+      expect(nodeStartOutputObj[2].addedComponents[0].id).to.equal('add_many_test_files/d');
+      expect(nodeStartOutputObj[2].addedComponents[0]).to.have.property('files');
+      expect(nodeStartOutputObj[2].addedComponents[0].files).to.be.array();
+      expect(nodeStartOutputObj[2].addedComponents[0].files).to.be.ofSize(1);
+      expect(nodeStartOutputObj[2].addedComponents[0].files[0].test).to.equal(false);
       expect(status).to.have.string('add_many_test_files/d ... ok');
       const compData = JSON.parse(helper.showComponentWithOptions('add_many_test_files/d', { j: '' }));
       expect(compData).to.not.property('Specs');
     });
     it('should add a component with many files', function () {
-      expect(nodeStartOutputObj[3].addedComponents[0]).to.have.property('id');
-      expect(nodeStartOutputObj[3].addedComponents[0].id).to.equal('add_many_test_files/component_with_many_paths');
-      expect(nodeStartOutputObj[3].addedComponents[0]).to.have.property('files');
-      expect(nodeStartOutputObj[3].addedComponents[0].files).to.be.array();
-      expect(nodeStartOutputObj[3].addedComponents[0].files).to.be.ofSize(2);
-      expect(nodeStartOutputObj[3].addedComponents[0].files[0].relativePath).to.equal('add_many_test_files/e.js');
-      expect(nodeStartOutputObj[3].addedComponents[0].files[1].relativePath).to.equal('add_many_test_files/f.js');
+      expect(nodeStartOutputObj[1].addedComponents[0]).to.have.property('id');
+      expect(nodeStartOutputObj[1].addedComponents[0].id).to.equal('add_many_test_files/component_with_many_paths');
+      expect(nodeStartOutputObj[1].addedComponents[0]).to.have.property('files');
+      expect(nodeStartOutputObj[1].addedComponents[0].files).to.be.array();
+      expect(nodeStartOutputObj[1].addedComponents[0].files).to.be.ofSize(2);
+      expect(nodeStartOutputObj[1].addedComponents[0].files[0].relativePath).to.equal('add_many_test_files/e.js');
+      expect(nodeStartOutputObj[1].addedComponents[0].files[1].relativePath).to.equal('add_many_test_files/f.js');
       expect(status).to.have.string('add_many_test_files/component_with_many_paths ... ok');
     });
     it('should add many components', function () {
