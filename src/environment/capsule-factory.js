c4085b11d5d2dd2cb84c8024307a43daeaec6a0c
David First <david@bitsrc.io>
2019-01-02 15:49:06 -0500

    add a class Isolator

---
+++ src/environment/capsule-factory.js
@@ -0,0 +1,19 @@
+// @flow
+import Capsule from '@bit/bit.capsule-dev.core.capsule';
+import FsContainer from '@bit/bit.capsule-dev.container.fs-container';
+
+export default (async function createCapsule(type: string = 'fs'): Promise<Capsule> {
+  const containerFactory = getContainerFactory(type);
+  const capsule = await Capsule.create(containerFactory);
+  await capsule.start();
+  return capsule;
+});
+
+function getContainerFactory(type: string): Function {
+  switch (type) {
+    case 'fs':
+    default:
+      // $FlowFixMe
+      return async () => new FsContainer();
+  }
+}
