9e55c9ca02557c3e70a3cb1880669760f78b300f
David First <david@bitsrc.io>
2019-02-13 08:20:09 -0500

    fix indexComponents unit tests

--- src/scope/objects/components-index.spec.js
+++ src/scope/objects/components-index.spec.js
@@ -1,17 +1,19 @@
 import { expect } from 'chai';
 import ComponentsIndex from './components-index';
 import { BitId } from '../../bit-id';
+import { ModelComponent, Symlink } from '../models';
 
-describe.skip('ComponentsIndex', () => {
+describe('ComponentsIndex', () => {
   describe('add', () => {
     let componentsIndex;
     before(() => {
       componentsIndex = new ComponentsIndex([], 'scope-path');
     });
     it('should add to the index array', () => {
-      const id = new BitId({ scope: 'my-scope', name: 'is-string' });
-      componentsIndex.add(id);
+      const modelComponent = new ModelComponent({ scope: 'my-scope', name: 'is-string' });
+      componentsIndex.addOne(modelComponent);
       const allIds = componentsIndex.getIds();
+      const id = new BitId({ scope: 'my-scope', name: 'is-string' });
       expect(allIds[0]).to.deep.equal(id);
     });
   });
@@ -21,10 +23,10 @@ describe.skip('ComponentsIndex', () => {
       componentsIndex = new ComponentsIndex([], 'scope-path');
     });
     it('should remove from the index array', () => {
-      const id = new BitId({ scope: 'my-scope', name: 'is-string' });
-      componentsIndex.add(id);
+      const modelComponent = new ModelComponent({ scope: 'my-scope', name: 'is-string' });
+      componentsIndex.addOne(modelComponent);
       expect(componentsIndex.getIds()).to.have.lengthOf(1);
-      componentsIndex.remove(id);
+      componentsIndex.remove(modelComponent.hash().toString());
       expect(componentsIndex.getIds()).to.have.lengthOf(0);
     });
   });
@@ -34,8 +36,8 @@ describe.skip('ComponentsIndex', () => {
       componentsIndex = new ComponentsIndex([], 'scope-path');
     });
     it('should not return symlinks', () => {
-      const id = new BitId({ scope: 'my-scope', name: 'is-string' });
-      componentsIndex.add(id, true);
+      const symlink = new Symlink({ scope: 'my-scope', name: 'is-type' });
+      componentsIndex.addOne(symlink);
       const allIds = componentsIndex.getIds();
       expect(allIds).to.have.lengthOf(0);
     });
@@ -46,16 +48,15 @@ describe.skip('ComponentsIndex', () => {
       componentsIndex = new ComponentsIndex([], 'scope-path');
     });
     it('should return symlinks', () => {
-      const id = new BitId({ scope: 'my-scope', name: 'is-string' });
-      componentsIndex.add(id, true);
+      const symlink = new Symlink({ scope: 'my-scope', name: 'is-type' });
+      componentsIndex.addOne(symlink);
       const allIds = componentsIndex.getIdsIncludesSymlinks();
       expect(allIds).to.have.lengthOf(1);
     });
     it('should return components and symlinks if both exist', () => {
-      const id1 = new BitId({ scope: 'my-scope', name: 'is-string' });
-      const id2 = new BitId({ scope: 'my-scope', name: 'is-type' });
-      componentsIndex.add(id1, true);
-      componentsIndex.add(id2, false);
+      const symlink = new Symlink({ scope: 'my-scope', name: 'is-type' });
+      const modelComponent = new ModelComponent({ scope: 'my-scope', name: 'is-string' });
+      componentsIndex.addMany([symlink, modelComponent]);
       const allIds = componentsIndex.getIdsIncludesSymlinks();
       expect(allIds).to.have.lengthOf(2);
     });
