83794a2557698c4af506d23f64995e1df52f61c5
David First <david@bitsrc.io>
2019-03-08 14:02:42 -0500

    avoid loading the same consumer multiple times during the process (#1478)

--- src/consumer/consumer-loader.js
+++ src/consumer/consumer-loader.js
@@ -2,7 +2,14 @@
 import path from 'path';
 import Consumer from './consumer';
 
-export default function loadConsumer(currentPath: ?string): Promise<Consumer> {
-  if (!currentPath) currentPath = process.cwd();
-  return Consumer.load(path.resolve(currentPath));
+export default async function loadConsumer(
+  currentPath?: string = process.cwd(),
+  newInstance?: boolean = false
+): Promise<Consumer> {
+  if (newInstance || !loadConsumer.cache || !loadConsumer.cache[currentPath]) {
+    const consumer = await Consumer.load(path.resolve(currentPath));
+    if (!loadConsumer.cache) loadConsumer.cache = {};
+    loadConsumer.cache[currentPath] = consumer;
+  }
+  return loadConsumer.cache[currentPath];
 }
