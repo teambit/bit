246bbbf2c60c25422ca8b6140f74b3e5cad28568
David First <david@bitsrc.io>
2019-03-08 13:24:30 -0500

    validate consumer bit.json path properties

--- src/consumer/bit-json/exceptions/index.js
+++ src/consumer/bit-json/exceptions/index.js
@@ -2,5 +2,6 @@
 import BitJsonAlreadyExists from './bit-json-already-exists';
 import BitJsonNotFound from './bit-json-not-found';
 import InvalidBitJson from './invalid-bit-json';
+import InvalidBitJsonPropPath from './invalid-bit-json-prop-path';
 
-export { BitJsonNotFound, BitJsonAlreadyExists, InvalidBitJson };
+export { BitJsonNotFound, BitJsonAlreadyExists, InvalidBitJson, InvalidBitJsonPropPath };
