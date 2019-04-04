c93dd894f0e414273fa3b1134e97f2935a6c3f7f
Gilad Shoham <shoham.gilad@gmail.com>
2019-04-04 15:34:31 +0300

    add git exe validation

--- src/doctor/doctor-registrar-builder.js
+++ src/doctor/doctor-registrar-builder.js
@@ -3,8 +3,9 @@
 import DoctorRegistrar from './doctor-registrar';
 import Diagnosis from './diagnosis';
 import ValidateWorkspaceBitJsonSyntax from './core-diagnoses/validate-workspace-bit-json-syntax';
+import ValidateGitExe from './core-diagnoses/validate-git-exe';
 
 export default function registerCoreAndExtensionsDiagnoses(extensionDiagnoses: Diagnosis[] = []) {
-  const diagnoses = [new ValidateWorkspaceBitJsonSyntax()].concat(extensionDiagnoses);
+  const diagnoses = [new ValidateWorkspaceBitJsonSyntax(), new ValidateGitExe()].concat(extensionDiagnoses);
   DoctorRegistrar.init(diagnoses);
 }
