f7a6ffeac3722ceecf865866474ce5fda1a338de
David First <david@bit.dev>
2019-04-30 20:50:35 -0400

    add a new bit-doctor diagnose for broken symlinks paths on environments directories (#1590)

--- src/doctor/doctor-registrar-builder.js
+++ src/doctor/doctor-registrar-builder.js
@@ -5,10 +5,14 @@ import Diagnosis from './diagnosis';
 import ValidateWorkspaceBitJsonSyntax from './core-diagnoses/validate-workspace-bit-json-syntax';
 import ValidateGitExe from './core-diagnoses/validate-git-exe';
 import OrphanSymlinkObjects from './core-diagnoses/orphan-symlink-objects';
+import BrokenSymlinkFiles from './core-diagnoses/broken-symlink-files';
 
 export default function registerCoreAndExtensionsDiagnoses(extensionDiagnoses: Diagnosis[] = []) {
-  const diagnoses = [new ValidateWorkspaceBitJsonSyntax(), new ValidateGitExe(), new OrphanSymlinkObjects()].concat(
-    extensionDiagnoses
-  );
+  const diagnoses = [
+    new ValidateWorkspaceBitJsonSyntax(),
+    new ValidateGitExe(),
+    new OrphanSymlinkObjects(),
+    new BrokenSymlinkFiles()
+  ].concat(extensionDiagnoses);
   DoctorRegistrar.init(diagnoses);
 }
