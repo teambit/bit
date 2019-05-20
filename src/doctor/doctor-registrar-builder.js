cb3c1a89bc269378dce5e317fe905a499ee6692e
Gilad Shoham <shoham.gilad@gmail.com>
2019-05-20 15:27:20 +0300

    add validation for yarn executable on `bit doctor`

--- src/doctor/doctor-registrar-builder.js
+++ src/doctor/doctor-registrar-builder.js
@@ -6,13 +6,15 @@ import ValidateWorkspaceBitJsonSyntax from './core-diagnoses/validate-workspace-
 import ValidateGitExe from './core-diagnoses/validate-git-exe';
 import OrphanSymlinkObjects from './core-diagnoses/orphan-symlink-objects';
 import BrokenSymlinkFiles from './core-diagnoses/broken-symlink-files';
+import ValidateYarnExec from './core-diagnoses/validate-yarn-exec';
 
 export default function registerCoreAndExtensionsDiagnoses(extensionDiagnoses: Diagnosis[] = []) {
   const diagnoses = [
     new ValidateWorkspaceBitJsonSyntax(),
     new ValidateGitExe(),
     new OrphanSymlinkObjects(),
-    new BrokenSymlinkFiles()
+    new BrokenSymlinkFiles(),
+    new ValidateYarnExec()
   ].concat(extensionDiagnoses);
   DoctorRegistrar.init(diagnoses);
 }
