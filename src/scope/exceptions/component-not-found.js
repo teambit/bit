59b9dfb1c272b103ee5d9b830daf544e7c630333
Gilad Shoham <shoham.gilad@gmail.com>
2019-05-27 19:29:15 +0300

    add suggestion to run `bit doctor` on various errors (#1670)

--- src/scope/exceptions/component-not-found.js
+++ src/scope/exceptions/component-not-found.js
@@ -5,11 +5,13 @@ export default class ComponentNotFound extends AbstractError {
   id: string;
   dependentId: ?string;
   code: number;
+  showDoctorMessage: boolean;
 
   constructor(id: string, dependentId?: string) {
     super();
     this.code = 127;
     this.id = id;
     this.dependentId = dependentId;
+    this.showDoctorMessage = true;
   }
 }
--- src/scope/network/exceptions/network-error.js
+++ src/scope/network/exceptions/network-error.js
@@ -3,9 +3,11 @@ import AbstractError from '../../../error/abstract-error';
 
 export default class NetworkError extends AbstractError {
   remoteErr: string;
+  showDoctorMessage: boolean;
 
   constructor(remoteErr: string) {
     super();
     this.remoteErr = remoteErr;
+    this.showDoctorMessage = true;
   }
 }
