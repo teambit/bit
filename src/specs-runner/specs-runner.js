ec88c332b3761b2338f73aa2b6940588030d5a7e
Gilad Shoham <shoham.gilad@gmail.com>
2019-06-24 20:10:41 +0300

    add missing import for Ramda

--- src/specs-runner/specs-runner.js
+++ src/specs-runner/specs-runner.js
@@ -1,5 +1,5 @@
 /** @flow */
-import path from 'path';
+import R from 'ramda';
 import execa from 'execa';
 import deserializeError from 'deserialize-error';
 import { Results } from '../consumer/specs-results';
