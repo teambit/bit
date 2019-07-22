c1794907781bdce202f2647f606658a0d993d1ce
David First <david@bitsrc.io>
2019-07-22 16:58:10 -0400

    build only the modified component and not all of them. also, fix #1634, improve the bit-watch output

--- src/api/consumer/lib/watch.js
+++ src/api/consumer/lib/watch.js
@@ -1,10 +1,11 @@
 /** @flow */
+/* eslint no-console: 0 */
 
 import chokidar from 'chokidar';
 import R from 'ramda';
 import chalk from 'chalk';
 import { loadConsumer } from '../../../consumer';
-import { buildAll } from '../lib/build';
+import { build } from '../lib/build';
 import loader from '../../../cli/loader';
 import Consumer from '../../../consumer/consumer';
 
@@ -19,29 +20,10 @@ import Consumer from '../../../consumer/consumer';
 export default (async function watchAll(verbose: boolean) {
   // TODO: run build in the beginning of process (it's work like this in other envs)
   const consumer = await loadConsumer();
-  const pathsToWatch = _getPathsToWatch(consumer, verbose);
-  const watcher = chokidar.watch(pathsToWatch, {
-    ignoreInitial: true,
-    // Using the function way since the regular way not working as expected
-    // It might be solved when upgrading to chokidar > 3.0.0
-    // See:
-    // https://github.com/paulmillr/chokidar/issues/773
-    // https://github.com/paulmillr/chokidar/issues/492
-    // https://github.com/paulmillr/chokidar/issues/724
-    ignored: (path) => {
-      // Ignore package.json temporarily since it cerates endless loop since it's re-written after each build
-      if (path.includes('dist') || path.includes('node_modules') || path.includes('package.json')) {
-        return true;
-      }
-      return false;
-    },
-    persistent: true,
-    useFsEvents: false
-  });
+  const watcher = _getWatcher();
 
-  console.log(chalk.yellow('start watching for changes')); // eslint-disable-line no-console
-
-  const log = console.log.bind(console); // eslint-disable-line no-console
+  console.log(chalk.yellow('started watching for component changes to rebuild'));
+  const log = console.log.bind(console);
 
   return new Promise((resolve, reject) => {
     // prefix your command with "BIT_LOG=*" to see all watch events
@@ -52,56 +34,88 @@ export default (async function watchAll(verbose: boolean) {
     }
     watcher.on('ready', () => {
       log(chalk.yellow('Initial scan complete. Ready for changes'));
-      // if (verbose) {
-      //   const watchedPaths = watcher.getWatched();
-      //   console.log('watchedPaths', watchedPaths)
-      // }
     });
     watcher.on('change', (p) => {
       log(`File ${p} has been changed, calling build`);
-      _handleChange().catch(err => reject(err));
+      _handleChange(p).catch(err => reject(err));
     });
     watcher.on('add', (p) => {
       log(`File ${p} has been added`);
-      _handleChange().catch(err => reject(err));
+      _handleChange(p, true).catch(err => reject(err));
     });
     watcher.on('unlink', (p) => {
       log(`File ${p} has been removed`);
-      _handleChange().catch(err => reject(err));
+      _handleChange(p).catch(err => reject(err));
     });
     watcher.on('error', (err) => {
       log(`Watcher error ${err}`);
       reject(err);
     });
   });
-});
 
-async function _handleChange() {
-  loadConsumer.cache = null;
-  // TODO: Make sure the log for build is printed to console
-  buildAll(false, false)
-    .then((buildResult) => {
-      console.log(buildResult); // eslint-disable-line no-console
-      loader.stop();
-      console.log(chalk.yellow('watching for changes')); // eslint-disable-line no-console
-    })
-    .catch((err) => {
-      console.log(err); // eslint-disable-line
-      throw err;
-    });
-}
-
-function _getPathsToWatch(consumer: Consumer, verbose: boolean): string[] {
-  const componentsFromBitMap = consumer.bitMap.getAllComponents();
-  const pathsToWatch = Object.keys(componentsFromBitMap).map((componentId) => {
-    const componentMap = componentsFromBitMap[componentId];
-    const trackDir = componentMap.getTrackDir();
-    const relativePaths = trackDir ? [trackDir] : componentMap.getFilesRelativeToConsumer();
-    const absPaths = relativePaths.map(relativePath => consumer.toAbsolutePath(relativePath));
-    if (verbose) {
-      console.log(`watching ${chalk.bold(componentId)}\n${absPaths.join('\n')}`); // eslint-disable-line no-console
+  async function _handleChange(filePath: string, isNew: boolean = false) {
+    const relativeFile = consumer.getPathRelativeToConsumer(filePath);
+    let componentId = consumer.bitMap.getComponentIdByPath(relativeFile);
+    if (!isNew && !componentId) {
+      log(`file ${filePath} is not part of any component, ignoring it`);
+      return;
     }
-    return absPaths;
-  });
-  return R.flatten(pathsToWatch);
-}
+    const updatedConsumer = await loadConsumer(undefined, true);
+    if (!componentId) {
+      componentId = updatedConsumer.bitMap.getComponentIdByPath(relativeFile);
+    }
+    if (!componentId) {
+      log(`file ${filePath} is not part of any component, ignoring it`);
+      return;
+    }
+    const idStr = componentId.toString();
+    console.log(`running build for ${chalk.bold(idStr)}`);
+    // TODO: Make sure the log for build is printed to console
+    const buildResults = await build(idStr, false, verbose);
+    if (buildResults) {
+      console.log(`\t${buildResults.join('\n\t')}`);
+    } else {
+      console.log(`${idStr} doesn't have a compiler, nothing to build`);
+    }
+
+    loader.stop();
+    console.log(chalk.yellow('watching for changes'));
+  }
+
+  function _getWatcher() {
+    const pathsToWatch = _getPathsToWatch();
+    return chokidar.watch(pathsToWatch, {
+      ignoreInitial: true,
+      // Using the function way since the regular way not working as expected
+      // It might be solved when upgrading to chokidar > 3.0.0
+      // See:
+      // https://github.com/paulmillr/chokidar/issues/773
+      // https://github.com/paulmillr/chokidar/issues/492
+      // https://github.com/paulmillr/chokidar/issues/724
+      ignored: (path) => {
+        // Ignore package.json temporarily since it cerates endless loop since it's re-written after each build
+        if (path.includes('dist') || path.includes('node_modules') || path.includes('package.json')) {
+          return true;
+        }
+        return false;
+      },
+      persistent: true,
+      useFsEvents: false
+    });
+  }
+
+  function _getPathsToWatch(): string[] {
+    const componentsFromBitMap = consumer.bitMap.getAllComponents();
+    const paths = Object.keys(componentsFromBitMap).map((componentId) => {
+      const componentMap = componentsFromBitMap[componentId];
+      const trackDir = componentMap.getTrackDir();
+      const relativePaths = trackDir ? [trackDir] : componentMap.getFilesRelativeToConsumer();
+      const absPaths = relativePaths.map(relativePath => consumer.toAbsolutePath(relativePath));
+      if (verbose) {
+        console.log(`watching ${chalk.bold(componentId)}\n${absPaths.join('\n')}`);
+      }
+      return absPaths;
+    });
+    return R.flatten(paths);
+  }
+});
