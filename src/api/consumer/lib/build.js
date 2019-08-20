8a9e28668a37124c352614d4894e5b9020fd96d6
David First <david@bit.dev>
2019-08-20 13:58:42 -0400

    fix #1916, do not throw an error when there are no components to build (#1939)

--- src/api/consumer/lib/build.js
+++ src/api/consumer/lib/build.js
@@ -5,7 +5,6 @@ import Component from '../../../consumer/component';
 import { COMPONENT_ORIGINS } from '../../../constants';
 import loader from '../../../cli/loader';
 import { BEFORE_LOADING_COMPONENTS } from '../../../cli/loader/loader-messages';
-import GeneralError from '../../../error/general-error';
 
 export async function build(id: string, noCache: boolean, verbose: boolean): Promise<?Array<string>> {
   const consumer = await loadConsumer();
@@ -21,7 +20,9 @@ export async function build(id: string, noCache: boolean, verbose: boolean): Pro
 export async function buildAll(noCache: boolean, verbose: boolean): Promise<Object> {
   const consumer: Consumer = await loadConsumer();
   const authoredAndImportedIds = consumer.bitMap.getAllBitIds([COMPONENT_ORIGINS.IMPORTED, COMPONENT_ORIGINS.AUTHORED]);
-  if (R.isEmpty(authoredAndImportedIds)) throw new GeneralError('nothing to build');
+  if (R.isEmpty(authoredAndImportedIds)) {
+    return {};
+  }
 
   loader.start(BEFORE_LOADING_COMPONENTS);
   const { components } = await consumer.loadComponents(authoredAndImportedIds);
