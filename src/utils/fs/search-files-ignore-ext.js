daa830be4f712dfd52fb629b2ebf5f03ad089eed
David First <david@bitsrc.io>
2019-08-21 17:17:53 -0400

    fix e2e-tests

--- src/utils/fs/search-files-ignore-ext.js
+++ src/utils/fs/search-files-ignore-ext.js
@@ -4,6 +4,8 @@ import Vinyl from 'vinyl';
 import getWithoutExt from './fs-no-ext';
 import type { PathOsBased } from '../path';
 
+const LOWER_PRIORITY_EXTENSIONS = ['d.ts'];
+
 export default function searchFilesIgnoreExt(
   files: Vinyl[],
   fileName: PathOsBased,
@@ -12,8 +14,22 @@ export default function searchFilesIgnoreExt(
   const _byFileNoExt = file => getWithoutExt(file.relative) === getWithoutExt(fileName);
   const _byFileWithExt = file => file.relative === fileName;
 
+  const getFile = () => {
+    const foundFileWithExt = R.find(_byFileWithExt, files);
+    if (foundFileWithExt) return foundFileWithExt;
+    const foundFilesWithExt = R.filter(_byFileNoExt, files);
+    if (!foundFilesWithExt.length) return null;
+    if (foundFilesWithExt.length === 1) return foundFilesWithExt[0];
+    // prefer files with extensions that are not listed in LOWER_PRIORITY_EXTENSIONS.
+    const withoutLowerPriorities = foundFilesWithExt.filter(
+      file => !LOWER_PRIORITY_EXTENSIONS.some(extension => file.relative.endsWith(extension))
+    );
+    if (withoutLowerPriorities.length) return withoutLowerPriorities[0];
+    return null;
+  };
+
   if (files && !R.isEmpty(files)) {
-    const foundFile = R.find(_byFileWithExt, files) || R.find(_byFileNoExt, files);
+    const foundFile = getFile();
     return foundFile && returnProp && foundFile[returnProp] ? foundFile[returnProp] : foundFile;
   }
   return null;
