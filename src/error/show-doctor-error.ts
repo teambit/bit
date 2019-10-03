374a0f182b51342da2d2ee1c39b45fc5797f8360
David First <david@bitsrc.io>
2019-10-03 11:52:35 -0400

    fix e2e-tests

--- src/error/show-doctor-error.ts
+++ src/error/show-doctor-error.ts
@@ -2,7 +2,6 @@
 import GeneralError from './general-error';
 
 export default class ShowDoctorError extends GeneralError {
-  msg: string;
   showDoctorMessage: boolean;
 
   constructor(msg: string) {
