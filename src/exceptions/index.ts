2539737ed48cbe919ee3716ec1218446df56ed9d
David First <david@bitsrc.io>
2019-10-22 16:44:14 -0400

    remove lots of unused code from package-json.ts

--- src/exceptions/index.ts
+++
@@ -1,4 +0,0 @@
-import PackageJsonNotFound from './package-json-not-found';
-import PackageJsonAlreadyExists from './package-json-already-exists';
-
-export { PackageJsonNotFound, PackageJsonAlreadyExists };
