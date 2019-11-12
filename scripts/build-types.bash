4508a33887b4938fcc1547d86e350d7de47786bc
Doron Tsur <qballer@gmail.com>
2019-11-13 01:51:55 +0200

    refactor build-types.bash

--- scripts/build-types.bash
+++ scripts/build-types.bash
@@ -1,10 +1,7 @@
 #!/bin/bash
 set -e
 node_modules/.bin/tsc --project tsconfig.types.json
-cd types/src
-mv * ../
-cd ../
-rm -r src
-cd ..
+mv types/src/*  types/
+rm -r types/src
 cp -rl types/* dist
 rm -rf types
