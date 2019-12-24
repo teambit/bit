6675c04dd90f05c4426429f7ea1c4f73690575c2
Amit <amit@cocycles.com>
2019-12-24 18:37:32 +0200

    add utils for orchestartor

---
+++ src/utils/abortable-promise/abortable-promise.spec.ts
@@ -0,0 +1,22 @@
+import { expect } from 'chai';
+import AbortablePromise from './abortable-promise';
+import { MissingAbortFn } from './exceptions';
+
+describe('AbortablePromise', () => {
+  describe('abort()', () => {
+    it('abort a native promise by executing given abort fn', done => {
+      function abortFn() {
+        expect(true).to.eq(true);
+        done();
+      }
+
+      const promise = new AbortablePromise(() => {}, abortFn);
+      promise.abort();
+    });
+
+    it('should throw a MissingAbortFn exception if abort was called when abort fn was not provided', () => {
+      const promise = new AbortablePromise(() => {});
+      expect(() => promise.abort()).to.throw(MissingAbortFn);
+    });
+  });
+});
