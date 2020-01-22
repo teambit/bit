a8aefcbff09ff591c280e83bfb315ec503af7425
Gilad Shoham <shoham.gilad@gmail.com>
2020-01-22 10:27:39 +0200

    added types

--- src/component/types.ts
+++ src/component/types.ts
@@ -1,4 +1,31 @@
+/**
+ * Snap metadata
+ *
+ * @export
+ * @interface Log
+ */
+export interface Log {
+  /**
+   * message added by the `Snap` author.
+   */
+  message: string;
+  /**
+   * Snap date.
+   */
+  date: string;
+  /**
+   * author of the component `Snap`.
+   */
+  author: Author;
+}
+
 export interface Author {
-  displayName: string;
-  email: string;
+  /**
+   * The author display name.
+   */
+  displayName?: string;
+  /**
+   * The author display email.
+   */
+  email?: string;
 }
