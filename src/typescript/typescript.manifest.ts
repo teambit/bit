e1a8a76c954cd6bb78275d3613eeb960bcfe8294
Doron Tsur <qballer@gmail.com>
2020-02-02 16:55:31 +0200

    remove typescript

--- src/typescript/typescript.manifest.ts
+++
@@ -1,9 +0,0 @@
-import { Extension } from '../harmony';
-import { TypeScript } from './typescript';
-
-Extension.instantiate({
-  name: 'TypeScript',
-  dependencies: [],
-  config: {},
-  provider: TypeScript.provide
-});
--- src/typescript/typescript.ts
+++
@@ -1,7 +0,0 @@
-export type TypeScriptDeps = [];
-
-export class TypeScript {
-  static async provide() {
-    // compiler.
-  }
-}
