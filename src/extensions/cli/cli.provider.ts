35136cb5b9ca981dd8743586a87a180052bb3063
Doron Tsur <qballer@gmail.com>
2020-02-03 15:57:50 +0200

    uncomment ran

--- src/extensions/cli/cli.provider.ts
+++ src/extensions/cli/cli.provider.ts
@@ -7,20 +7,17 @@ import legacyLoadExtensions from '../../legacy-extensions/extensions-loader';
 export type BitCLIDeps = [Paper];
 
 export async function CLIProvider(config: {}, [paper]: BitCLIDeps) {
-  const legacyExtensions = await legacyLoadExtensions(); // const legacyExtensions = await legacyLoadExtensions();
-  // Make sure to register all the hooks actions in the global hooks manager	  // // Make sure to register all the hooks actions in the global hooks manager
+  const legacyExtensions = await legacyLoadExtensions();
+  // Make sure to register all the hooks actions in the global hooks manager
   legacyExtensions.forEach(extension => {
-    // legacyExtensions.forEach(extension => {
-    extension.registerHookActionsOnHooksManager(); //   extension.registerHookActionsOnHooksManager();
-  }); // });
+    extension.registerHookActionsOnHooksManager();
+  });
   const extensionsCommands = legacyExtensions.reduce((acc, curr) => {
-    // const extensionsCommands = legacyExtensions.reduce((acc, curr) => {
     if (curr.commands && curr.commands.length) {
-      //   if (curr.commands && curr.commands.length) {
-      // @ts-ignore AUTO-ADDED-AFTER-MIGRATION-PLEASE-FIX!	  //     // @ts-ignore AUTO-ADDED-AFTER-MIGRATION-PLEASE-FIX!
-      acc = acc.concat(curr.commands); //     acc = acc.concat(curr.commands);
-    } //   }
-    return acc; //   return acc;
+      // @ts-ignore AUTO-ADDED-AFTER-MIGRATION-PLEASE-FIX!
+      acc = acc.concat(curr.commands);
+    }
+    return acc;
   }, []);
 
   const legacyRegistry = buildRegistry(extensionsCommands);
