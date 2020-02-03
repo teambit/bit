891056663c5928b22cbd7a4639bde16134a77938
Ran Mizrahi <ran@bit.dev>
2020-02-03 13:30:56 +0200

    chnages

--- src/extensions/composer/compose.cmd.tsx
+++ src/extensions/composer/compose.cmd.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import { start } from '@teambit/composer';
 import { Color } from 'ink';
-import { Command } from '../paper';
+import { Command, CLIArgs } from '../paper';
 import { Workspace } from '../workspace';
 import { Build } from '../build';
+import { Flags } from '../paper/command';
 
 export default class ComposeCmd implements Command {
   name = 'start [id]';
@@ -18,17 +19,20 @@ export default class ComposeCmd implements Command {
     private build: Build
   ) {}
 
+  // TODO: remove this ts-ignore
+  // @ts-ignore
   async render() {
-    // return <Color green>compose start</Color>;
-    return new Promise(async (resolve, reject) => {
+    // eslint-disable-next-line no-async-promise-executor
+    return new Promise(async () => {
       const components = await this.workspace.list();
       const resolved = await this.build.run('build', components);
-      
+
       const data = resolved.reduce((map, component) => {
         map[component.component.id.toString()] = component.capsule.wrkDir;
         return map;
       }, {});
 
+      // eslint-disable-next-line no-console
       Object.keys(data).forEach(key => console.log(data[key].wrkDir));
 
       start(data);
