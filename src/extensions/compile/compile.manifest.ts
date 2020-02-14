03b79f5818d134ee6f8537c3a9ff1bcac46f3664
Ran Mizrahi <ran@bit.dev>
2020-02-14 20:45:20 +0200

    made compile task work

--- src/extensions/compile/compile.manifest.ts
+++ src/extensions/compile/compile.manifest.ts
@@ -3,7 +3,10 @@ import { WorkspaceExt } from '../workspace';
 import { provideCompile } from './compile.provider';
 
 export default {
-  name: 'compile',
+  name: '@compile',
+  actions: {
+    default: () => console.log('hi there')
+  },
   dependencies: [BitCliExt, WorkspaceExt],
   provider: provideCompile
 };
