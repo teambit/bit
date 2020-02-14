5afc47ae37b7537ef7810fe29c852d96b5eb3d9d
Aram Drevekenin <aram@poor.dev>
2020-02-14 11:35:14 +0100

    refactor(package-manager): move configuration and fix names

--- src/extensions/package-manager/package-manager.manifest.ts
+++ src/extensions/package-manager/package-manager.manifest.ts
@@ -1,8 +1,12 @@
 import { providePackageManager } from './package-manager.provider';
 
+const DEFAULT_PACKAGE_MANAGER = 'librarian';
+
 export default {
   name: 'PackageManager',
   dependencies: [],
-  config: {},
+  config: {
+    packageManager: DEFAULT_PACKAGE_MANAGER
+  },
   provider: providePackageManager
 };
--- src/extensions/package-manager/package-manager.provider.ts
+++ src/extensions/package-manager/package-manager.provider.ts
@@ -2,12 +2,12 @@ import { Workspace } from '../workspace';
 import { BitCli } from '../cli';
 import PackageManager from './package-manager';
 
-const DEFAULT_PACKAGE_MANAGER = 'librarian';
+export type PMConfig = {
+  packageManager: string;
+};
 
-export type ServeConfig = {};
+export type PMDeps = [BitCli, Workspace];
 
-export type ServeDeps = [BitCli, Workspace];
-
-export async function providePackageManager(config: ServeConfig, [cli, workspace]: ServeDeps) {
-  return new PackageManager(DEFAULT_PACKAGE_MANAGER);
+export async function providePackageManager(config: PMConfig, [cli, workspace]: PMDeps) {
+  return new PackageManager(config.packageManager);
 }
