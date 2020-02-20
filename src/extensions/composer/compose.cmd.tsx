4ddbb248c0db335b005e6b3e8f582f81d3eb4820
Doron Tsur <qballer@gmail.com>
2020-02-20 13:53:53 +0200

    changing run API for communication

--- src/extensions/composer/compose.cmd.tsx
+++ src/extensions/composer/compose.cmd.tsx
@@ -25,10 +25,10 @@ export default class ComposeCmd implements Command {
       // const components = await this.workspace.get('base/card');
       const resolved = await this.pipes.run('build', components);
 
-      const data = resolved.reduce((map, component) => {
-        map[component.component.id.toString()] = component.capsule.wrkDir;
-        return map;
-      }, {});
+      // const data = resolved.reduce((map, component) => {
+      //   map[component.component.id.toString()] = component.capsule.wrkDir;
+      //   return map;
+      // }, {});
 
       // eslint-disable-next-line no-console
       // start(data);
