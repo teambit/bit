d522719e31520e5c1a6f9f93ff7f2df489fe9a0e
David First <david@bitsrc.io>
2020-03-05 17:56:30 -0500

    fix e2e-tests

--- e2e/typescript/typescript-link-files.e2e.3.ts
+++ e2e/typescript/typescript-link-files.e2e.3.ts
@@ -3,6 +3,7 @@ import fs from 'fs-extra';
 import * as path from 'path';
 import Helper from '../../src/e2e-helper/e2e-helper';
 import { statusFailureMsg } from '../../src/cli/commands/public-cmds/status-cmd';
+import { componentIssuesLabels } from '../../src/cli/templates/component-issues-template';
 
 describe('typescript components with link files', function() {
   this.timeout(0);
@@ -43,10 +44,8 @@ describe('typescript components with link files', function() {
       helper.command.addComponent('bar/foo.ts', { i: 'bar/foo' });
     });
     it('should not consider that index file as a dependency', () => {
-      helper.command.linkAndRewire();
-      output = helper.command.runCmd('bit status');
-      expect(output.includes('bar/foo ... ok')).to.be.true;
-      expect(output.includes(statusFailureMsg)).to.be.false;
+      output = helper.command.status();
+      expect(output).not.to.have.string(componentIssuesLabels.untrackedDependencies);
     });
   });
 
