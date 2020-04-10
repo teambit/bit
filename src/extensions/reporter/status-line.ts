369b0003e090bfd2d2bf4eed4b69f0d14f73b199
Aram Drevekenin <aram@poor.dev>
2020-04-10 17:12:06 +0200

    fix(spinner): make arbitrary stdout play nice with the status line (#2570)
    * fix(spinner): make arbitrary stdout play nice with the status line
    
    * style(lint): prettier issues
    
    * style(comment): add explanation about stdout patching

--- src/extensions/reporter/status-line.ts
+++ src/extensions/reporter/status-line.ts
@@ -18,13 +18,41 @@ function clearStatusRow() {
   );
 }
 
+// we send a proxy to the spinner instance rather than proxess.stdout
+// so that we would be able to bypass our monkey-patch of process.stdout
+// this is so that we won't have a case where the stdout "write" method
+// triggers itself through the spinner by doing "spinner.start()" or "spinner.stop()"
+const originalStdoutWrite = process.stdout.write.bind(process.stdout);
+const stdoutProxy = new Proxy(process.stdout, {
+  get(obj, prop) {
+    if (prop === 'write') {
+      return originalStdoutWrite;
+    }
+    return obj[prop];
+  }
+});
+
 export default class StatusLine {
   public buffer = SPACE_BUFFER;
-  private spinner: any = ora({ spinner: 'bouncingBar', stream: process.stdout }).stop();
+  private spinner: any = ora({ spinner: 'bouncingBar', stream: stdoutProxy, isEnabled: true }).stop();
   private spinnerLength = 7; // 6 for spinner, 1 for space after it
   private ids: Array<string> = [];
   constructor() {
     this.reRender = debounce(this.reRender, 100);
+    // @ts-ignore
+    // here we monkey-patch the process.stdout stream so that whatever is printed
+    // does not break the status line with the spinner, and that this line always
+    // remains at the bottom of the screen
+    process.stdout.write = (buffer, encoding, callback) => {
+      const wasSpinning = this.spinner.isSpinning;
+      if (wasSpinning) {
+        this.spinner.stop();
+      }
+      originalStdoutWrite(buffer, encoding, callback);
+      if (wasSpinning) {
+        this.spinner.start();
+      }
+    };
   }
   addId(id) {
     this.ids.push(id);
