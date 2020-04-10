d7e4ec0dffa53284459281db6a4ae21c4fe2aa09
Kevin Gilbert <kegi@users.noreply.github.com>
2020-04-10 10:01:31 -0400

    fix #2551, Updated open dependency and added url:true (#2557)

--- src/consumer/login/login.ts
+++ src/consumer/login/login.ts
@@ -122,7 +122,7 @@ export default function loginToBitSrc(
       );
       if (!suppressBrowserLaunch) {
         console.log(chalk.yellow(`Your browser has been opened to visit:\n${encoded}`)); // eslint-disable-line no-console
-        open(encoded);
+        open(encoded, { url: true });
       } else {
         console.log(chalk.yellow(`Go to the following link in your browser::\n${encoded}`)); // eslint-disable-line no-console
       }
