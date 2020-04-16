d6e85e851bb06d7a4a867d547ced439c6aef6821
Doron Tsur <qballer@gmail.com>
2020-04-16 16:46:11 +0300

    unused import

--- src/extensions/logger/logger.spec.ts
+++ src/extensions/logger/logger.spec.ts
@@ -1,5 +1,5 @@
 import { expect } from 'chai';
-import { Logger, Level, LogEntry } from './Logger';
+import { Logger } from './Logger';
 
 describe('logger', function() {
   it('should support log', function(done) {
@@ -7,13 +7,13 @@ describe('logger', function() {
     logger.log('TEST!', 'message');
     logger.listen(done);
   });
+
   it('should support listening to logs', function() {
     const logger = new Logger();
     const messagesToSend = ['1', '2', '3'];
     const results: string[] = [];
 
     messagesToSend.forEach(msg => logger.log('TEST', msg));
-
     logger.listen(entry => results.push(entry.message));
 
     expect(messagesToSend).equalTo(results);
