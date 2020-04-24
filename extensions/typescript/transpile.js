157392eaa3129168c95a52db18d569097fcbbaf6
David First <davidnet@gmail.com>
2020-04-24 16:50:11 -0400

    fix "transpile" script to compile typescript using npx, this eliminate the need to find the tsc binary file

--- extensions/typescript/transpile.js
+++ extensions/typescript/transpile.js
@@ -7,9 +7,14 @@ const typescript = require('typescript');
 console.log(process.cwd());
 function transpile() {
   fs.writeFileSync('tsconfig.json', JSON.stringify(tsconfig, null, 2));
-  const cwd = path.join(__dirname, 'node_modules/.bin');
-  console.log('transpile -> tsc path', cwd);
-  const results = childProcess.execSync('./tsc . -d', { cwd });
+  let results;
+  try {
+    result = childProcess.execSync('npx tsc -d', { cwd: 'node_modules/.bin' });
+  } catch (err) {
+    console.log('transpile -> err', err);
+    console.log('transpile -> stdour', err.stdout ? err.stdout.toString() : '');
+    console.log('transpile -> stderr', err.stderr ? err.stderr.toString() : '');
+  }
   console.log('transpile -> results', results);
   return { dir: 'dist', results };
 }
