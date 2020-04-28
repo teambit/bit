eb8114c5e9e9ac00653cb2e17de14774096b65ce
Aram Drevekenin <aram@poor.dev>
2020-04-28 14:55:43 +0200

    fix(status-line): play nice with stderr (#2631)
    * fix(status-line): play nice with stderr
    
    * fix(flows): call reporter end on error

--- src/extensions/flows/run/run.cmd.tsx
+++ src/extensions/flows/run/run.cmd.tsx
@@ -31,56 +31,62 @@ export class RunCmd implements Command {
   constructor(private flows: Flows, private reporter: Reporter, private logger: Logger) {}
 
   async render([flow, components]: CLIArgs, { parallel, noCache, verbose }: Flags) {
-    let capsulesInstalled = 0;
-    let totalCapsules = 0;
-    let flowsExecuted = 0;
-    let totalFlows = 0;
-    let flowRunning = ''; // this is just a sample for the log because concurrency
-    onCapsuleInstalled(componentName => {
-      capsulesInstalled += 1;
+    try {
+      let capsulesInstalled = 0;
+      let totalCapsules = 0;
+      let flowsExecuted = 0;
+      let totalFlows = 0;
+      let flowRunning = ''; // this is just a sample for the log because concurrency
+      onCapsuleInstalled(componentName => {
+        capsulesInstalled += 1;
+        this.reporter.setStatusText(
+          `⏳ Resolving Components from the workspace (${capsulesInstalled}/${totalCapsules}). ${componentName}`
+        );
+      });
+      beforeInstallingCapsules(numCapsules => {
+        totalCapsules += numCapsules;
+      });
+      flowEvents.on('flowStarted', flowName => {
+        totalFlows += 1;
+        flowRunning = flowName;
+        this.reporter.setStatusText(`⏳ Running flows (${flowsExecuted}/${totalFlows}). Running ${flowRunning}`);
+      });
+      flowEvents.on('flowExecuted', flowName => {
+        flowsExecuted += 1;
+        this.reporter.setStatusText(`⏳ Running flows (${flowsExecuted}/${totalFlows}). Running ${flowRunning}`);
+      });
+      this.reporter.title(`Starting "${flow}"`);
+      const concurrencyN = parallel && typeof parallel === 'string' ? Number.parseInt(parallel) : 5;
+      const actualComps = typeof components === 'string' ? [components] : components;
+      const comps = this.flows.getIds(actualComps);
+      this.reporter.title('setting up component execution');
       this.reporter.setStatusText(
-        `⏳ Resolving Components from the workspace (${capsulesInstalled}/${totalCapsules}). ${componentName}`
+        `⏳ Resolving Components from the workspace (${capsulesInstalled}/${totalCapsules}).`
       );
-    });
-    beforeInstallingCapsules(numCapsules => {
-      totalCapsules += numCapsules;
-    });
-    flowEvents.on('flowStarted', flowName => {
-      totalFlows += 1;
-      flowRunning = flowName;
-      this.reporter.setStatusText(`⏳ Running flows (${flowsExecuted}/${totalFlows}). Running ${flowRunning}`);
-    });
-    flowEvents.on('flowExecuted', flowName => {
-      flowsExecuted += 1;
-      this.reporter.setStatusText(`⏳ Running flows (${flowsExecuted}/${totalFlows}). Running ${flowRunning}`);
-    });
-    this.reporter.title(`Starting "${flow}"`);
-    const concurrencyN = parallel && typeof parallel === 'string' ? Number.parseInt(parallel) : 5;
-    const actualComps = typeof components === 'string' ? [components] : components;
-    const comps = this.flows.getIds(actualComps);
-    this.reporter.title('setting up component execution');
-    this.reporter.setStatusText(`⏳ Resolving Components from the workspace (${capsulesInstalled}/${totalCapsules}).`);
-    this.flows.onWorkspaceLoaded(numComponents => {
-      capsulesInstalled
-        ? this.reporter.info(undefined, `📦 ${capsulesInstalled} Components resolved`)
-        : this.reporter.info(undefined, `📦 Components loaded from cache`);
+      this.flows.onWorkspaceLoaded(numComponents => {
+        capsulesInstalled
+          ? this.reporter.info(undefined, `📦 ${capsulesInstalled} Components resolved`)
+          : this.reporter.info(undefined, `📦 Components loaded from cache`);
 
-      this.reporter.title('🎬  Executing flows');
-      this.reporter.setStatusText('⏳ Executing flows');
-    });
-    const runStream: ReplaySubject<any> = await this.flows.run(comps, flow as string, {
-      concurrency: concurrencyN,
-      caching: !noCache
-    });
+        this.reporter.title('🎬  Executing flows');
+        this.reporter.setStatusText('⏳ Executing flows');
+      });
+      const runStream: ReplaySubject<any> = await this.flows.run(comps, flow as string, {
+        concurrency: concurrencyN,
+        caching: !noCache
+      });
 
-    const logPublisher = this.logger.createLogPublisher('flows');
+      const logPublisher = this.logger.createLogPublisher('flows');
 
-    const report = await reportRunStream(runStream, logPublisher, verbose as boolean);
-
-    // console.log('DONE')
-    this.reporter.info(undefined, `✔️  ${flowsExecuted} Flows executed`);
-    this.reporter.end();
-    const reportComp = <Report props={report} />;
-    return reportComp;
+      const report = await reportRunStream(runStream, logPublisher, verbose as boolean);
+      this.reporter.info(undefined, `✔️  ${flowsExecuted} Flows executed`);
+      this.reporter.end();
+      const reportComp = <Report props={report} />;
+      return reportComp;
+    } catch (e) {
+      // this is a (hopefully) temporary hack until we formalize the reporter end behaviour
+      this.reporter.end();
+      throw e;
+    }
   }
 }
