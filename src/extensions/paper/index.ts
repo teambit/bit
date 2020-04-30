29336398121bafd411d2e4b3d55da08162f52260
David First <davidnet@gmail.com>
2020-04-30 17:45:47 -0400

    fix some more tsc errors thrown during "bit compile" of core extensions

--- src/extensions/paper/index.ts
+++ src/extensions/paper/index.ts
@@ -1,4 +1,4 @@
 export { Paper } from './paper';
 export { default as PaperExt } from './paper.extension';
-export { Command, CLIArgs, PaperOptions, Flags } from './command';
+export { Command, CLIArgs, PaperOptions, Flags, GenericObject } from './command';
 export * from './exceptions';
