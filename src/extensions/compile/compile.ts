36f00d799c47d5e8c53407e710777d7459788657
David First <davidnet@gmail.com>
2020-05-11 21:01:49 -0400

    fix compile to work with the full-name "bit.core/compile" (#2662)

--- src/extensions/compile/compile.ts
+++ src/extensions/compile/compile.ts
@@ -57,7 +57,11 @@ export class Compile {
     const idsAndFlows = new IdsAndFlows();
     const componentsWithLegacyCompilers: ComponentAndCapsule[] = [];
     componentsAndCapsules.forEach(c => {
-      const compileConfig = c.component.config.extensions.findCoreExtension('compile')?.config;
+      const compileCore = c.component.config.extensions.findCoreExtension('compile');
+      const compileComponent = c.component.config.extensions.findExtension('compile');
+      const compileComponentExported = c.component.config.extensions.findExtension('bit.core/compile');
+      const compileExtension = compileCore || compileComponent || compileComponentExported;
+      const compileConfig = compileExtension?.config;
       const compiler = compileConfig ? [compileConfig.compiler] : [];
       if (compileConfig) {
         idsAndFlows.push({ id: c.consumerComponent.id, value: compiler });
