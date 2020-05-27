de6e51665aac9ba92850fafcece5c0e1ab957af8
Gilad Shoham <shoham.gilad@gmail.com>
2020-05-27 22:36:28 +0300

    improve components dir transform to legacy

--- src/extensions/config/workspace-config.ts
+++ src/extensions/config/workspace-config.ts
@@ -397,7 +397,7 @@ export class WorkspaceConfig implements HostConfig {
     };
 
     let componentsDefaultDirectory = this.extension('@teambit/workspace', true)?.defaultDirectory;
-    if (componentsDefaultDirectory && !componentsDefaultDirectory.include('{name}')) {
+    if (componentsDefaultDirectory && !componentsDefaultDirectory.includes('{name}')) {
       componentsDefaultDirectory = `${componentsDefaultDirectory}/{name}`;
     }
 
