4bbaf49deddf1075b7f184b68eee95dfd4d39ea0
David First <davidnet@gmail.com>
2020-06-12 23:02:42 -0400

    fix e2e/commands/status.e2e.2.ts tests

--- e2e/commands/status.e2e.2.ts
+++ e2e/commands/status.e2e.2.ts
@@ -580,7 +580,7 @@ describe('bit status command', function() {
       describe('running bit diff', () => {
         it('should throw an exception ComponentNotFoundInPath', () => {
           const diffFunc = () => helper.command.diff('bar/foo');
-          const error = new ComponentNotFoundInPath(path.join(helper.scopes.localPath, 'bar'));
+          const error = new ComponentNotFoundInPath('bar');
           helper.general.expectToThrow(diffFunc, error);
         });
       });
