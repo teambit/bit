56518fd8b88ac9b4130bd8ab16582ae3cdfedfb4
David First <davidnet@gmail.com>
2020-06-13 22:06:25 -0400

    introduce a new flag "harmony" to "bit init" to start a new workspace with workspace.jsonc (#2732)

---
+++ src/api/consumer/lib/feature-toggle.spec.ts
@@ -0,0 +1,30 @@
+import { expect } from 'chai';
+import { ENV_VAR_FEATURE_TOGGLE, isFeatureEnabled, addFeature } from './feature-toggle';
+
+describe('featureToggle', () => {
+  after(() => {
+    process.env[ENV_VAR_FEATURE_TOGGLE] = '';
+  });
+  describe('isFeatureEnabled', () => {
+    it('should work with multiple features', () => {
+      process.env[ENV_VAR_FEATURE_TOGGLE] = 'feature1, feature2';
+      expect(isFeatureEnabled('feature1')).to.be.true;
+      expect(isFeatureEnabled('feature2')).to.be.true;
+      expect(isFeatureEnabled('feature3')).to.be.false;
+    });
+  });
+  describe('addFeature', () => {
+    it('should add feature', () => {
+      addFeature('add1');
+      expect(isFeatureEnabled('add1')).to.be.true;
+    });
+    it('should add feature to existing features', () => {
+      process.env[ENV_VAR_FEATURE_TOGGLE] = 'feature1, feature2';
+      addFeature('add1');
+      expect(isFeatureEnabled('add1')).to.be.true;
+      expect(isFeatureEnabled('feature1')).to.be.true;
+      expect(isFeatureEnabled('feature2')).to.be.true;
+      expect(isFeatureEnabled('feature3')).to.be.false;
+    });
+  });
+});
