820bcace776beb3ca6d1b4241e210ab20f707d52
Doron Tsur <qballer@gmail.com>
2020-06-15 15:33:53 +0300

    eslint issue

--- src/extensions/cli/commands/example.cmd.tsx
+++ src/extensions/cli/commands/example.cmd.tsx
@@ -7,9 +7,9 @@ export class ExampleCMD extends React.Component {
   name: 'yo';
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   render(num: number) {
-    // eslint-disable-next-line no-plusplus
     return (
       <Color green>
+        {/* eslint-disable-next-line no-plusplus */}
         <div>Hello World {++num}</div>
       </Color>
     );
