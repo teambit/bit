55897612bc510d67bb6ec77ff214f1ab39325bec
David First <davidnet@gmail.com>
2020-06-22 17:50:44 -0400

    throw an error when command does not exist, fix PaperError to be handled correctly, refacot app.ts to be readable

--- src/extensions/cli/cli.extension.ts
+++ src/extensions/cli/cli.extension.ts
@@ -9,6 +9,8 @@ import { Help } from './commands/help.cmd';
 import { buildRegistry } from '../../cli';
 import LegacyLoadExtensions from '../../legacy-extensions/extensions-loader';
 import { LegacyCommandAdapter } from './legacy-command-adapter';
+import { CommandNotFound } from './exceptions/command-not-found';
+import didYouMean from 'didyoumean';
 
 export class CLIExtension {
   readonly groups: { [k: string]: string } = {};
@@ -59,7 +61,7 @@ export class CLIExtension {
    */
   async run() {
     const args = process.argv.slice(2); // remove the first two arguments, they're not relevant
-    if ((args[0] && ['-h', '--help'].includes(args[0])) || process.argv.length === 2) {
+    if (!args[0] || ['-h', '--help'].includes(args[0])) {
       Help()(this.commands, this.groups);
       return;
     }
@@ -70,6 +72,7 @@ export class CLIExtension {
     }
 
     Object.values(this.commands).forEach(command => register(command as any, commander, packageManagerArgs));
+    this.throwForNonExistsCommand(args[0]);
 
     // this is what runs the `execAction` of the specific command and eventually exits the process
     commander.parse(params);
@@ -80,6 +83,24 @@ export class CLIExtension {
   private shouldOutputJson() {
     const showCommand = commander.commands.find(c => c._name === 'show');
     return showCommand.versions;
+  }
+  private throwForNonExistsCommand(commandName: string) {
+    const commands = Object.keys(this.commands);
+    const aliases = commands.map(c => this.commands[c].alias).filter(a => a);
+    const globalFlags = ['-V', '--version'];
+    const validCommands = [...commands, ...aliases, ...globalFlags];
+    const commandExist = validCommands.includes(commandName);
+
+    if (!commandExist) {
+      const suggestions = didYouMean(
+        commandName,
+        Object.keys(this.commands)
+          .filter(c => !this.commands[c].private)
+          .map(c => this.commands[c].name)
+      );
+      const suggestion = suggestions && suggestions.length ? suggestions[0] : suggestions;
+      throw new CommandNotFound(commandName, suggestion);
+    }
   }
   registerGroup(name: string, description: string) {
     if (this.groups[name]) {
