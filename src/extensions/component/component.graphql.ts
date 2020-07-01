426688a46aaaf5001e132bc30c4db1d63fe311ca
Uri Kutner <urikutner@gmail.com>
2020-07-01 22:39:14 +0300

    add snaps to graphql schema

--- src/extensions/component/component.graphql.ts
+++ src/extensions/component/component.graphql.ts
@@ -6,7 +6,9 @@ export function componentSchema() {
       type Component {
         id: String
         displayName: String
-        versions(limit: Int): [String]
+        # TEMP, to
+        versions(limit: Int): [Snap]
+        snaps(limit: Int): [Snap]
         isNew: Boolean
         isModified: Boolean
       }
@@ -14,6 +16,27 @@ export function componentSchema() {
       type ComponentMeta {
         id: String
         displayName: String
+      }
+      
+      type Snap {
+        message: String
+        owner: Account
+        timestamp: String
+        """test status (pass/fail/pending/skipped)"""
+        test: String
+        #"""ci status"""
+        # ci: String
+      }
+      
+      type Account {
+        name: String
+        # type: String
+        """gravatar link or equivalent"""
+        image: String
+        """Could be empty due to security"""
+        email: String
+        #"""The display name chosen by user in his settings page. Could be family name or nickname."""
+        # displayName: String # required, TBD
       }
   `,
     resolvers: {
@@ -26,6 +49,8 @@ export function componentSchema() {
         displayName: (component: Component) => component.displayName,
         isNew: (component: Component) => component.isNew(),
         isModified: (component: Component) => component.isModified(),
+        snaps: (component: Component, { limit = 10 }: { limit?: number }) => [...component.tags.keys()].slice(0, limit), //TODO
+        // TEMP, to remove
         versions: (component: Component, { limit = 10 }: { limit?: number }) =>
           [...component.tags.keys()].slice(0, limit).map(x => x.toString())
       }
