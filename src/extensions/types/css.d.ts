1fc72dd64bfb9a0c231c86d14c1d5fde88ed4b84
Ran Mizrahi <ran@bit.dev>
2020-07-02 20:23:29 +0300

    css typing update

--- src/extensions/types/css.d.ts
+++ src/extensions/types/css.d.ts
@@ -1,4 +1,12 @@
-declare module '*.scss' {
-  const content: { [className: string]: string };
-  export default content;
+declare module '*.module.css' {
+  const classes: { readonly [key: string]: string };
+  export default classes;
+}
+declare module '*.module.scss' {
+  const classes: { readonly [key: string]: string };
+  export default classes;
+}
+declare module '*.module.sass' {
+  const classes: { readonly [key: string]: string };
+  export default classes;
 }
