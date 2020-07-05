9499b13fa95dcc2ef7582435891a3b156f84b162
Kutner JS <urikutner@gmail.com>
2020-07-05 18:19:29 +0300

    sticky urls (#2803)
    * refactor component tree to use memo hook
    
    * improve component page sub routes
    
    * sticky urls

--- src/extensions/component/ui/top-bar-nav/top-bar-nav.tsx
+++ src/extensions/component/ui/top-bar-nav/top-bar-nav.tsx
@@ -3,16 +3,19 @@ import classnames from 'classnames';
 import { NavLink, NavLinkProps, useRouteMatch } from 'react-router-dom';
 
 import styles from './top-bar-nav.module.scss';
+import { extendPath } from '../../../react-router/extend-path/extend-path';
 
 export function TopBarNav(props: NavLinkProps) {
   const { url } = useRouteMatch();
+  const { to } = props;
+  const target = typeof to === 'string' ? extendPath(url, to) : to;
 
   return (
     <NavLink
       {...props}
       className={classnames(props.className, styles.topBarLink)}
       activeClassName={classnames(props.className, styles.active)}
-      to={`${url}/${props.to}`}
+      to={target}
     />
   );
 }
