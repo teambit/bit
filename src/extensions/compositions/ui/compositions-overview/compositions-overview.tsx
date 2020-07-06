558c40ba51a0e1c9174db661c6e063f4e4ab22f5
oded <oded@bit.dev>
2020-07-06 23:50:40 +0300

    compositions overview

--- src/extensions/compositions/ui/compositions-overview/compositions-overview.tsx
+++ src/extensions/compositions/ui/compositions-overview/compositions-overview.tsx
@@ -1,5 +1,21 @@
 import React from 'react';
+import classNames from 'classnames';
+import styles from './compositions-overview.module.scss';
 
-export function CompositionsOverview() {
-  return <div></div>;
+export type CompositionsOverviewProps = {
+  compositions: {};
+};
+export function CompositionsOverview({ compositions }: CompositionsOverviewProps) {
+  return (
+    <div className={styles.background}>
+      {compositions &&
+        Object.keys(compositions).map(key => {
+          return (
+            <div key={key} style={compositions[key].canvas} className={classNames(styles.singleCompositionBox)}>
+              {compositions[key]()}
+            </div>
+          );
+        })}
+    </div>
+  );
 }
