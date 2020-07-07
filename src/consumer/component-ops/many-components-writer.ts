bb72a5aeb7e445fa33ad41c1785fa85b2a8599ee
David First <davidnet@gmail.com>
2020-07-06 23:04:19 -0400

    fix e2e-tests

--- src/consumer/component-ops/many-components-writer.ts
+++ src/consumer/component-ops/many-components-writer.ts
@@ -63,7 +63,7 @@ export default class ManyComponentsWriter {
   override: boolean;
   writePackageJson: boolean;
   writeConfig: boolean;
-  ignoreBitDependencies: boolean;
+  ignoreBitDependencies: boolean | undefined;
   createNpmLinkFiles: boolean;
   writeDists: boolean;
   installNpmPackages: boolean;
@@ -93,7 +93,7 @@ export default class ManyComponentsWriter {
     this.isolated = this._setBooleanDefault(params.isolated, false);
     this.writePackageJson = this._setBooleanDefault(params.writePackageJson, true);
     this.writeConfig = this._setBooleanDefault(params.writeConfig, false);
-    this.ignoreBitDependencies = this._setBooleanDefault(params.ignoreBitDependencies, true);
+    this.ignoreBitDependencies = params.ignoreBitDependencies;
     this.createNpmLinkFiles = this._setBooleanDefault(params.createNpmLinkFiles, false);
     this.writeDists = this._setBooleanDefault(params.writeDists, true);
     this.installPeerDependencies = this._setBooleanDefault(params.installPeerDependencies, false);
@@ -213,7 +213,10 @@ export default class ManyComponentsWriter {
       component: componentWithDeps.component,
       writeToPath: componentRootDir,
       writeConfig: this.writeConfig,
-      ignoreBitDependencies: this.ignoreBitDependencies || componentWithDeps.component.dependenciesSavedAsComponents, // when dependencies are written as npm packages, they must be written in package.json
+      ignoreBitDependencies:
+        typeof this.ignoreBitDependencies === 'boolean'
+          ? this.ignoreBitDependencies
+          : componentWithDeps.component.dependenciesSavedAsComponents, // when dependencies are written as npm packages, they must be written in package.json
       ...getParams()
     };
   }
