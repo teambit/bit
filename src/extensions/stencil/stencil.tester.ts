981f2f31799a756ab72b09d0e57f46d14984dfaa
Gilad Shoham <shoham.gilad@gmail.com>
2020-07-14 11:38:28 +0300

    fix types

--- src/extensions/stencil/stencil.tester.ts
+++ src/extensions/stencil/stencil.tester.ts
@@ -5,10 +5,10 @@ import { Workspace } from '../workspace';
 export class StencilTester implements Tester {
   constructor(private workspace: Workspace) {}
 
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   async test(context: TesterContext): Promise<TestResults> {
     const testing = await createTesting({
-      rootDir: this.workspace.path,
-      cwd: this.workspace.path
+      rootDir: this.workspace.path
     });
 
     testing.run({});
