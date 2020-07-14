81c9fdc5cc29c11af40ce503d73d8e058b4e276e
oded <oded@bit.dev>
2020-07-14 18:11:26 +0300

    docs styles

--- src/extensions/react/docs/component-overview/component-overview.tsx
+++ src/extensions/react/docs/component-overview/component-overview.tsx
@@ -1,12 +1,11 @@
 import React from 'react';
-import classNames from 'classnames';
+import { textColumn } from '@bit/bit.base-ui.layout.page-frame';
 import { Section } from '@bit/bit.test-scope.ui.section';
 import { H1 } from '@bit/bit.test-scope.ui.heading';
 import { Subtitle } from '@bit/bit.test-scope.ui.sub-title';
 import { Separator } from '@bit/bit.test-scope.ui.separator';
 import { ConsumableLink } from '@bit/bit.test-scope.ui.consumable-link';
 import { LabelList } from '@bit/bit.test-scope.ui.label-list';
-import styles from './component-overview.module.scss';
 
 export type ComponentOverviewProps = {
   displayName: string;
@@ -19,10 +18,12 @@ export type ComponentOverviewProps = {
 export function ComponentOverview({ displayName, abstract, labels, packageName, ...rest }: ComponentOverviewProps) {
   return (
     <Section {...rest}>
-      <H1 className={classNames(styles.maxWidth, styles.heading)}>{displayName}</H1>
-      <Subtitle className={styles.maxWidth}>{abstract}</Subtitle>
-      <LabelList>{labels}</LabelList>
-      <ConsumableLink className={styles.maxWidth} title="Package name" link={packageName}></ConsumableLink>
+      <div className={textColumn}>
+        <H1>{displayName}</H1>
+        <Subtitle>{abstract}</Subtitle>
+        <LabelList>{labels}</LabelList>
+        <ConsumableLink title="Package name" link={packageName}></ConsumableLink>
+      </div>
       <Separator />
     </Section>
   );
