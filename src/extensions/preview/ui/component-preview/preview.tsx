3bb40a2c5686682fe8afc92249b409c146f0ac1a
Uri Kutner <urikutner@gmail.com>
2020-07-22 16:06:01 +0300

    ComponentPreview - safer url generation

--- src/extensions/preview/ui/component-preview/preview.tsx
+++ src/extensions/preview/ui/component-preview/preview.tsx
@@ -1,4 +1,4 @@
-import React, { CSSProperties } from 'react';
+import React from 'react';
 import { ComponentModel } from '../../../component/ui';
 
 export type ComponentPreviewProps = {
@@ -13,23 +13,25 @@ export type ComponentPreviewProps = {
   previewName?: string;
 
   /**
-   * preview style.
-   */
-  style?: CSSProperties;
-
-  /**
    * string in the format of query params. e.g. foo=bar&bar=there
    */
   queryParams?: string;
-};
+} & React.IframeHTMLAttributes<HTMLIFrameElement>;
 
 /**
  * renders a preview of a component.
  */
-export function ComponentPreview({ component, style, previewName, queryParams }: ComponentPreviewProps) {
-  const url = `${component.server.url}/#${component.id.fullName}${
-    `?preview=${previewName}&${queryParams && queryParams}` || ''
-  }`;
+export function ComponentPreview({ component, previewName, queryParams, ...rest }: ComponentPreviewProps) {
+  const query = [optional('preview=', previewName), queryParams].filter((x) => !!x).join('&');
+  const hash = component.id.fullName + optional('?', query);
+  const path = component.server.url;
 
-  return <iframe style={style} src={url} />;
+  return <iframe {...rest} src={`${path}/#${hash}`} />;
+}
+
+// aka - bit.utils/string/optional
+function optional(prefix = '', str?: string, suffix = '') {
+  if (!str) return '';
+
+  return `${prefix}${str}${suffix}`;
 }
