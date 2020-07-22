ac717e603d29655df01f8dc60df30031e1bd6fae
David First <davidnet@gmail.com>
2020-07-21 22:01:28 -0400

    copy unsupported files to the dist directoy in the capsule (#2880)

--- src/extensions/compiler/compiler.task.ts
+++ src/extensions/compiler/compiler.task.ts
@@ -1,5 +1,7 @@
+import path from 'path';
 import { BuildTask, BuildContext, BuildResults } from '../builder';
 import { Compiler } from './types';
+import { Capsule } from '../isolator';
 
 /**
  * compiler build task. Allows to compile components during component build.
@@ -9,6 +11,20 @@ export class CompilerTask implements BuildTask {
 
   async execute(context: BuildContext): Promise<BuildResults> {
     const compilerInstance: Compiler = context.env.getCompiler();
-    return compilerInstance.build(context);
+    const buildResults = await compilerInstance.build(context);
+
+    context.capsuleGraph.capsules.forEach((capsule) => this.copyNonSupportedFiles(capsule.capsule, compilerInstance));
+
+    return buildResults;
+  }
+
+  copyNonSupportedFiles(capsule: Capsule, compiler: Compiler) {
+    const component = capsule.component;
+    component.filesystem.files.forEach((file) => {
+      if (!compiler.isFileSupported(file.path)) {
+        const content = file.contents;
+        capsule.fs.writeFileSync(path.join(compiler.getDistDir(), file.relative), content);
+      }
+    });
   }
 }
