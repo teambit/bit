3239f4bb9c5162bd3760a6c40bd840d98a86311d
oded <oded@bit.dev>
2020-07-23 16:07:09 +0300

    sidebar - add overview gallery link

---
+++ src/extensions/stage-components/side-bar/overview-link/overview-link.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import classNames from 'classnames';
+import { Icon } from '@bit/bit.evangelist.elements.icon';
+import { Separator } from '@bit/bit.test-scope.ui.separator';
+import { NavLink } from '../../../react-router/nav-link';
+import { hoverable } from '../../../../to-eject/css-components/hoverable';
+import { clickable } from '../../../../to-eject/css-components/clickable';
+
+import styles from './overview-link.module.scss';
+
+export function OverviewLink() {
+  return (
+    <div className={styles.overview}>
+      <NavLink
+        exact
+        href="/"
+        activeClassName={styles.active}
+        className={classNames(hoverable, clickable, styles.overviewLink)}
+      >
+        Gallery
+        <Icon of="comps" className={styles.icon} />
+      </NavLink>
+      <Separator className={styles.separator} />
+    </div>
+  );
+}
