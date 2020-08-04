fea154049a0b47f95eb9a7800f5865ef818caf9e
oded <oded@bit.dev>
2020-08-04 15:29:09 +0300

    draggable sidebar - initial commit

--- src/extensions/workspace/ui/workspace/workspace.tsx
+++ src/extensions/workspace/ui/workspace/workspace.tsx
@@ -2,7 +2,9 @@ import React, { ReactNode } from 'react';
 import { gql } from 'apollo-boost';
 import { Route } from 'react-router-dom';
 import 'reset-css';
-import styles from './workspace.module.scss';
+import { TupleSplitPane } from '@bit/bit.gui.atoms.tuple-split-pane';
+import { Layout } from '@bit/bit.rendering.constants.layouts';
+import { CollapsibleSplitter } from '@bit/bit.gui.atoms.split-pane.collapsible-splitter';
 import { Workspace as WorkspaceModel } from './workspace-model';
 import { WorkspaceProvider } from './workspace-provider';
 import { RouteSlot, SlotRouter } from '../../../react-router/slot-router';
@@ -12,6 +14,9 @@ import { Corner } from '../../../stage-components/corner';
 import { SideBar } from '../../../stage-components/side-bar';
 import { TopBar } from '../../../stage-components/top-bar';
 import { WorkspaceOverview } from './workspace-overview';
+import styles from './workspace.module.scss';
+// import { CollapsibleSplitter } from '../../../stage-components/side-bar/splitter';
+// import { TopBar } from '../../../component/ui/top-bar';
 
 const WORKSPACE = gql`
   {
@@ -70,16 +75,17 @@ export function Workspace({ routeSlot, menuSlot }: WorkspaceProps) {
 
   return (
     <WorkspaceProvider workspace={workspace}>
-      <div className={styles.workspace}>
+      <div className={styles.workspaceWrapper}>
         <TopBar Corner={() => <Corner name={workspace.name} />} menu={menuSlot} />
-        <SideBar className={styles.sideBar} components={workspace.components} />
-        <div className={styles.main}>
-          <SlotRouter slot={routeSlot} />
-          {/* TODO - @oded move to route slot once we can register more than one slot at a time */}
-          <Route exact path="/">
-            <WorkspaceOverview />
-          </Route>
-        </div>
+        <TupleSplitPane max={85} min={15} layout={Layout.row} Splitter={CollapsibleSplitter}>
+          <Left workspace={workspace} />
+          <div className={styles.main}>
+            <SlotRouter slot={routeSlot} />
+            <Route exact path="/">
+              <WorkspaceOverview />
+            </Route>
+          </div>
+        </TupleSplitPane>
       </div>
     </WorkspaceProvider>
   );
@@ -88,3 +94,11 @@ export function Workspace({ routeSlot, menuSlot }: WorkspaceProps) {
 export type WorkspaceContextProps = {
   children: ReactNode;
 };
+
+function Left({ workspace }: any) {
+  return (
+    <div className={styles.left}>
+      <SideBar className={styles.sideBar} components={workspace.components} />
+    </div>
+  );
+}
