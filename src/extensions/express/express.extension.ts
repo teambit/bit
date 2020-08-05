4f49567616d6f40bbd857b580206506edcb345a0
Guysaar <guy@bit.dev>
2020-08-06 01:01:44 +0300

    add option to pass app to express

--- src/extensions/express/express.extension.ts
+++ src/extensions/express/express.extension.ts
@@ -59,11 +59,11 @@ export class ExpressExtension {
     ];
   }
 
-  createApp(): Express {
+  createApp(expressApp?: Express): Express {
     const internalRoutes = this.createRootRoutes();
     const routes = this.createRoutes();
     const allRoutes = concat(routes, internalRoutes);
-    const app = express();
+    const app = expressApp || express();
     app.use(cors());
 
     allRoutes.forEach((routeInfo) => {
