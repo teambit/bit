07a9630780d654d21ef71e8b4b96efd6e62a8219
oded <oded@bit.dev>
2020-08-05 20:01:34 +0300

    scope now uses split pane as well

--- src/extensions/scope/ui/scope.tsx
+++ src/extensions/scope/ui/scope.tsx
@@ -1,16 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import gql from 'graphql-tag';
 import { Route } from 'react-router-dom';
+import { TupleSplitPane } from '@bit/bit.gui.atoms.tuple-split-pane';
+import { Layout } from '@bit/bit.rendering.constants.layouts';
 import { RouteSlot, SlotRouter } from '../../react-router/slot-router';
 import { ScopeOverview } from './scope-overview';
 import { FullLoader } from '../../../to-eject/full-loader';
 import { ScopeModel } from './scope-model';
 import { useDataQuery } from '../../ui/ui/data/use-data-query';
 import { ScopeProvider } from './scope-provider';
-import styles from './scope.module.scss';
 import { Corner } from '../../../components/stage-components/corner';
 import { SideBar } from '../../../components/stage-components/side-bar';
 import { TopBar } from '../../../components/stage-components/top-bar';
+import { CollapsibleSplitter } from '../../../components/stage-components/splitter';
+import styles from './scope.module.scss';
 
 export type ScopeProps = {
   routeSlot: RouteSlot;
@@ -37,28 +40,34 @@ const SCOPE = gql`
  * root component of the scope
  */
 export function Scope({ routeSlot, menuSlot }: ScopeProps) {
+  // TODO - @oded find a way to reuse the sidebar collapse functionality between scope and workspace
+  const [isSidebarOpen, toggleOpenness] = useState(true);
   const { data, loading } = useDataQuery(SCOPE);
 
   if (loading) {
     return <FullLoader />;
   }
 
+  const handleOpenness = () => {
+    toggleOpenness(!isSidebarOpen);
+  };
+
   const scope = ScopeModel.from(data);
   const ids = scope.components.map((component) => component);
-
+  const sidebarOpenness = isSidebarOpen ? Layout.row : Layout.right;
   return (
     <ScopeProvider scope={scope}>
       <div className={styles.scope}>
-        <TopBar Corner={() => <Corner name={scope.name} />} menu={menuSlot} />
-        <SideBar components={ids} className={styles.sideBar} />
-        <div className={styles.main}>
-          <SlotRouter slot={routeSlot} />
-          {/* TODO - @oded move to route slot once we can register more than one slot at a time */}
-          {/* TODO - scope still uses ComponentMeta so we dont get all the data here */}
-          <Route exact path="/">
-            <ScopeOverview />
-          </Route>
-        </div>
+        <TopBar Corner={() => <Corner name={scope.name} onClick={handleOpenness} />} menu={menuSlot} />
+        <TupleSplitPane max={60} min={10} layout={sidebarOpenness} Splitter={CollapsibleSplitter}>
+          <SideBar components={ids} className={styles.sideBar} />
+          <div className={styles.main}>
+            <SlotRouter slot={routeSlot} />
+            <Route exact path="/">
+              <ScopeOverview />
+            </Route>
+          </div>
+        </TupleSplitPane>
       </div>
     </ScopeProvider>
   );
