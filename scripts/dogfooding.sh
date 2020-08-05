8c2fb68999c0ca9a654d6087423c55f5c61cf962
David First <davidnet@gmail.com>
2020-08-04 21:02:16 -0400

    add missing index.ts files for some extensions

--- scripts/dogfooding.sh
+++ scripts/dogfooding.sh
@@ -1,15 +1,24 @@
 #!/usr/bin/env bash
 
 rm -rf extensions
+rm -rf components
 mkdir extensions
+mkdir components
 cp -R src/extensions/* extensions
+cp -R src/components/* components
 rm .bitmap
 bit init --harmony
 bit add extensions/*
+bit add components/utils/* -n utils
+bit add components/stage-components/workspace-components/* -n staged-components/workspace-components
+bit add components/stage-components/workspace-sections/* -n staged-components/workspace-sections
+bit add components/stage-components/* -n staged-components
 bit link --rewire
 find extensions/ -type f -exec sed -i '' "s/'..\/..\/..\//'bit-bin\//g" {} \;
 find extensions/ -type f -exec sed -i '' "s/'..\/..\//'bit-bin\//g" {} \;
 find extensions/ -type f -exec sed -i '' "s/'bit-bin\/runtime/'..\/..\/runtime/g" {} \;
 find extensions/ -type f -exec sed -i '' "s/'bit-bin\/workspace.ui/'..\/..\/workspace.ui/g" {} \;
+find components/ -type f -exec sed -i '' "s/'..\/..\/..\//'bit-bin\//g" {} \;
+find components/ -type f -exec sed -i '' "s/'..\/..\//'bit-bin\//g" {} \;
 rm -rf node_modules/bit-bin
 ln -s `pwd`/dist node_modules/bit-bin
