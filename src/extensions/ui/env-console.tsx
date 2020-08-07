753825f0535dca65654152bf822c3e3e33b77716
Ran Mizrahi <ran@bit.dev>
2020-08-08 02:51:16 +0300

    removed redudent interval

--- src/extensions/ui/env-console.tsx
+++ src/extensions/ui/env-console.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import { Box } from 'ink';
 import { UIServer } from './ui-server';
 
@@ -7,17 +7,5 @@ export type UIServerConsoleProps = {
 };
 
 export function UIServerConsole({ uiServer }: UIServerConsoleProps) {
-  const [, setCounter] = useState(0);
-
-  useEffect(() => {
-    const timer = setInterval(() => {
-      setCounter((previousCounter) => previousCounter + 1);
-    }, 100);
-
-    return () => {
-      clearInterval(timer);
-    };
-  }, []);
-
   return <Box>Bit UI server is listening to port {uiServer.port}</Box>;
 }
