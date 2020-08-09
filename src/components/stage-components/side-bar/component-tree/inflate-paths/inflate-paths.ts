b0f0e8267f77e452862f9fae71a467696216e1cf
Uri Kutner <urikutner@gmail.com>
2020-08-09 18:02:40 +0300

    inflate-path - support inner and leaf nodes with the same name, + tests

---
+++ src/components/stage-components/side-bar/component-tree/inflate-paths/inflate-paths.ts
@@ -0,0 +1,29 @@
+import { buildKeyTree } from './key-tree';
+import { keyTreeToNodeTree } from './key-tree-to-node-tree';
+import { attachPayload } from './attach-payload';
+
+type KeySelector<T> = (data: T) => string;
+type PayloadSelector<T, Payload> = (data: T) => Payload;
+
+const multiRootNodeId = '';
+
+export function inflateToTree<T, Payload>(
+  rawData: T[],
+  idSelector: KeySelector<T>,
+  payloadSelector?: PayloadSelector<T, Payload>
+) {
+  const paths = rawData.map(idSelector);
+  const treeSkeleton = buildKeyTree(paths);
+
+  const rootItems = Object.entries(treeSkeleton);
+
+  const [rootKey, rootNode] = rootItems.length === 1 ? rootItems[0] : [multiRootNodeId, treeSkeleton];
+  const tree = keyTreeToNodeTree<Payload>(rootKey, rootNode);
+
+  if (payloadSelector) {
+    const payloadMap = new Map(rawData.map((data) => [idSelector(data), payloadSelector(data)]));
+    attachPayload(tree, payloadMap);
+  }
+
+  return tree;
+}
