f2f9446f0ba096fbd02edcf69281fabbda82f012
David First <davidnet@gmail.com>
2020-08-13 15:48:42 -0400

    add missing packages to capsules, fix peerDependencies to not be duplicated with dependencies, move non-extensions code from app.ts to bootstrap.ts so then cli-ext will use app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,46 +1,16 @@
-import semver from 'semver';
-import chalk from 'chalk';
-import Bluebird from 'bluebird';
-import fs from 'fs-extra';
 import harmony from '@teambit/harmony';
-import HooksManager from './hooks';
-import { handleErrorAndExit, handleUnhandledRejection } from './cli/command-runner';
+import { handleErrorAndExit } from './cli/command-runner';
 import { ConfigExt } from './extensions/config';
 import { BitExt, registerCoreExtensions } from './extensions/bit';
 import { CLIExtension } from './extensions/cli';
-import { Analytics } from './analytics/analytics';
-import { GLOBAL_CONFIG, GLOBAL_LOGS, BIT_VERSION } from './constants';
-import { printWarning } from './logger/logger';
-
-const MINIMUM_NODE_VERSION = '10.13.0';
-
-process.env.MEMFS_DONT_WARN = 'true'; // suppress fs experimental warnings from memfs
-
-require('events').EventEmitter.defaultMaxListeners = 100; // set max listeners to a more appropriate numbers
-
-require('regenerator-runtime/runtime');
-
-process.on('unhandledRejection', (err) => handleUnhandledRejection(err));
-
-// by default Bluebird enables the longStackTraces when env is `development`, or when
-// BLUEBIRD_DEBUG is set.
-// the drawback of enabling it all the time is a performance hit. (see http://bluebirdjs.com/docs/api/promise.longstacktraces.html)
-// some commands are slower by 20% with this enabled.
-Bluebird.config({
-  longStackTraces: Boolean(process.env.BLUEBIRD_DEBUG || process.env.BIT_LOG),
-});
+import { bootstrap } from './bootstrap';
 
 initApp();
 
 async function initApp() {
   try {
-    printBitVersionIfAsked();
-    warnIfRunningAsRoot();
-    verifyNodeVersionCompatibility();
-    await ensureDirectories();
-    await Analytics.promptAnalyticsIfNeeded();
+    await bootstrap();
     registerCoreExtensions();
-    HooksManager.init();
     await harmony.run(ConfigExt);
     await harmony.set([BitExt]);
     await runCLI();
@@ -54,39 +24,4 @@ async function runCLI() {
   const cli: CLIExtension = harmony.get('CLIExtension');
   if (!cli) throw new Error(`failed to get CLIExtension from Harmony`);
   await cli.run();
-}
-
-async function ensureDirectories() {
-  await fs.ensureDir(GLOBAL_CONFIG);
-  await fs.ensureDir(GLOBAL_LOGS);
-}
-
-function verifyNodeVersionCompatibility() {
-  const nodeVersion = process.versions.node.split('-')[0];
-  const isCompatible = semver.satisfies(nodeVersion, `>=${MINIMUM_NODE_VERSION}`);
-  if (!isCompatible) {
-    // eslint-disable-next-line no-console
-    console.log(
-      chalk.red(
-        `Node version ${nodeVersion} is not supported, please use Node.js ${MINIMUM_NODE_VERSION} or higher. If you must use legacy versions of Node.js, please use our binary installation methods. https://docs.bit.dev/docs/installation`
-      )
-    );
-    process.exit(1);
-  }
-}
-
-function warnIfRunningAsRoot() {
-  const isRoot = process.getuid && process.getuid() === 0;
-  if (isRoot) {
-    printWarning('running bit as root might cause permission issues later');
-  }
-}
-
-function printBitVersionIfAsked() {
-  if (process.argv[2]) {
-    if (['-V', '-v', '--version'].includes(process.argv[2])) {
-      console.log(BIT_VERSION); // eslint-disable-line no-console
-      process.exit();
-    }
-  }
 }
--- src/extensions/isolator/isolator.extension.ts
+++ src/extensions/isolator/isolator.extension.ts
@@ -88,13 +88,17 @@ export class IsolatorExtension {
         .map((capsuleWithPackageData) => capsuleWithPackageData.capsule);
       // await this.dependencyResolver.capsulesInstall(capsulesToInstall, { packageManager: config.packageManager });
       const installer = this.dependencyResolver.getInstaller();
-      // When using isolator we don't want to use the policy defined in the workspace directly, we only want to instal deps from components
-      await installer.install(
-        capsulesDir,
-        this.dependencyResolver.getEmptyDepsObject(),
-        this.toComponentMap(capsules),
-        { dedupe: true }
-      );
+      // When using isolator we don't want to use the policy defined in the workspace directly,
+      // we only want to instal deps from components and the peer from the workspace
+
+      const workspacePolicy = this.dependencyResolver.getWorkspacePolicy() || {};
+      const rootDepsObject = {
+        peerDependencies: {
+          ...workspacePolicy.peerDependencies,
+        },
+      };
+
+      await installer.install(capsulesDir, rootDepsObject, this.toComponentMap(capsules), { dedupe: true });
       await symlinkDependenciesToCapsules(capsulesToInstall, capsuleList, this.logger);
       // TODO: this is a hack to have access to the bit bin project in order to access core extensions from user extension
       // TODO: remove this after exporting core extensions as components
