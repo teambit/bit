b62f3bada17a0b00f5d1e2c1ea98592ef8687e43
Uri Kutner <urikutner@gmail.com>
2020-08-18 19:54:52 +0300

    fix types

--- src/utils/object/foreach.ts
+++ src/utils/object/foreach.ts
@@ -9,7 +9,7 @@
  *  // => a 1 b 2 c 3
  * ```
  */
-export default function forEach(obj: Record<string, any>, cb: (val: any, key: any) => void) {
+export default function forEach<T>(obj: Record<string, T>, cb: (val: T, key: string) => void) {
   const keys = Object.keys(obj);
   keys.forEach((key) => cb(obj[key], key));
 }
