83f4102b8196791d92e5abd7d8a2267138a5eb85
Gilad Shoham <shoham.gilad@gmail.com>
2020-08-19 14:00:50 +0300

    more fixes

--- src/extensions/ui/index.ts
+++ src/extensions/ui/index.ts
@@ -2,5 +2,5 @@ export { useDataQuery } from './ui/data/use-data-query';
 export { UIRoot } from './ui-root';
 export type { UiMain } from './ui.main.runtime';
 export { UIAspect } from './ui.aspect';
-export { UiUI } from './ui.ui.runtime';
+export type { UiUI } from './ui.ui.runtime';
 export { UIRootUI } from './ui-root.ui';
