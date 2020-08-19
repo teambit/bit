08dc0da510d117772b0a517c85e232b04a908cc5
Ran Mizrahi <ran@bit.dev>
2020-08-20 01:00:18 +0300

    fixed config

--- src/extensions/config/config.aspect.ts
+++ src/extensions/config/config.aspect.ts
@@ -1,8 +1,9 @@
 import { Aspect, RuntimeDefinition } from '@teambit/harmony';
 
-export const MainRuntime = new RuntimeDefinition('main');
+export const ConfigRuntime = new RuntimeDefinition('main');
 
 export const ConfigAspect = Aspect.create({
   id: '@teambit/config',
   dependencies: [],
+  declareRuntime: ConfigRuntime,
 });
