e346891911a3011c046ac15147adfb13673ed0d1
Ran Mizrahi <ran@bit.dev>
2020-08-20 00:50:21 +0300

    fixed

--- src/extensions/aspect-loader/aspect-loader.aspect.ts
+++ src/extensions/aspect-loader/aspect-loader.aspect.ts
@@ -1,6 +1,6 @@
 import { Aspect } from '@teambit/harmony/dist/aspect';
 
 export const AspectLoaderAspect = Aspect.create({
-  id: 'teambit.bit/aspect',
+  id: 'teambit.bit/aspect-loader',
   dependencies: [],
 });
