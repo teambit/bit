4381e3c06cac511f2dc4e07d109b03652b4e4cc3
David First <davidnet@gmail.com>
2020-08-21 18:35:02 -0400

    make sure to symlink bit-bin when running build-harmony

--- bin/bit.js
+++ bin/bit.js
@@ -4,6 +4,8 @@
 try {
   require('../node_modules/@teambit/cli/dist/app');
 } catch (err) {
-  console.error(`** bit was moved. please run "npm run install-harmony && npm run build-harmony", then try again **`);
-  process.exit(1);
+  if (err.code && err.code === 'MODULE_NOT_FOUND' && err.message && err.message.includes('cli/dist')) {
+    console.error(`** bit was moved. please run "npm run install-harmony && npm run build-harmony", then try again **`);
+    process.exit(1);
+  }
 }
