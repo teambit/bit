c589f5f6d29e52e67ac509f865d236435315ff00
David First <davidnet@gmail.com>
2020-08-21 08:52:55 -0400

    move git history from src/extensions to extensions. step6: re-add the dummy component

---
+++ extensions/dummy/dummy.main.runtime.ts
@@ -0,0 +1,13 @@
+import { MainRuntime } from '@teambit/cli';
+import { DummyAspect } from './dummy.aspect';
+
+export class DummyMain {
+  static runtime = MainRuntime;
+
+  static async provider() {
+    console.log('hi from dummy');
+    return new DummyMain();
+  }
+}
+
+DummyAspect.addRuntime(DummyMain);
