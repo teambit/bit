d1210d6c5ea22d535a5e145de2f325d5aa29dd86
David First <davidnet@gmail.com>
2020-08-21 16:51:32 -0400

    fix bit/bit.js to point to the right location of the app.js

--- bin/bit.js
+++ bin/bit.js
@@ -2,7 +2,7 @@
 'use strict';
 
 try {
-  require('../build-harmony/node_modules/@teambit/cli/dist/app');
+  require('../node_modules/@teambit/cli/dist/app');
 } catch (err) {
   console.error(`** bit was moved. please run "npm run install-harmony && npm run build-harmony", then try again **`);
   process.exit(1);
