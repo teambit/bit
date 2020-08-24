1a9f565994618ee36d2e70bfe12e4b4a03132212
Uri Talyosef <uritalyosef@Uris-MBP-2.lan>
2020-08-24 10:59:06 +0300

    Extend Parser Interface

--- extensions/schema/parser.ts
+++ extensions/schema/parser.ts
@@ -2,12 +2,17 @@ import { Module } from './schemas';
 
 export interface Parser {
   /**
-   * regex to apply on which components compiler applies.
+   * Regex to apply on which components compiler applies.
    */
   extension: RegExp;
 
   /**
-   * parse a module.
+   * Parse a module.
    */
   parseModule(modulePath: string): Module;
+
+  /**
+   * Extract module public api.
+   */
+  extractApi?: (modulePath: string) => any; //TODO:[uri]: Define public api interface
 }
