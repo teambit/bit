7e3396f897e89d0b43107934cd1c29c7d8bda918
Ran Mizrahi <ran@bit.dev>
2020-08-24 14:43:08 +0300

    done

--- extensions/sidebar/index.ts
+++ extensions/sidebar/index.ts
@@ -2,4 +2,5 @@ import { SidebarAspect } from './sidebar.aspect';
 
 export { Drawer } from './drawer';
 export { SidebarUI } from './sidebar.ui.runtime';
+export { SidebarAspect };
 export default SidebarAspect;
