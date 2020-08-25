6ca28dc297ffedd01702c004fb136786d6154fdf
Gilad Shoham <shoham.gilad@gmail.com>
2020-08-25 15:44:53 +0300

    update tests

---
+++ e2e/fixtures/extensions/extension-add-dependencies/index.ts
@@ -0,0 +1,4 @@
+export type { ExtensionAddDependenciesMain } from './extension-add-dependencies.main.runtime';
+export { ExtensionAddDependenciesAspect } from './extension-add-dependencies.aspect';
+export default ExtensionAddDependenciesAspect;
+
