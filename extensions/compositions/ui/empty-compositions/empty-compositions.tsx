92694838a0129fd8a188c66758e50d55c729ff48
Oded Reuveny <oded@bit.dev>
2020-08-26 22:05:49 +0300

    Ui/last fixes (#3025)
    * fixed empty compositions link
    
    * compositions - removed console warning
    
    * fixed console warning for <p> inside <p>

--- extensions/compositions/ui/empty-compositions/empty-compositions.tsx
+++ extensions/compositions/ui/empty-compositions/empty-compositions.tsx
@@ -8,7 +8,11 @@ export function EmptyCompositions() {
     <div className={styles.emptyCompositions}>
       <div className={styles.emptyCompositionsBox}>
         <div>There are no compositions for this component.</div>
-        <a href="https://bit-new-docs.netlify.app/docs/getting-started/compositions#creating-compositions">
+        <a
+          href="https://bit-new-docs.netlify.app/docs/compositions/develop-in-isolation"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
           Learn how to create compositions
           <Icon of="arrow_right" />
         </a>
