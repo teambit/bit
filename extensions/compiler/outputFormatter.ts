580887f14a36cf751f9b6d86515d7aef7ba06b2c
Uri Talyosef <uritalyosef@Uris-MBP-2.lan>
2020-08-26 19:09:17 +0300

    typos

--- extensions/compiler/outputFormatter.ts
+++ extensions/compiler/outputFormatter.ts
@@ -2,16 +2,16 @@ import chalk from 'chalk';
 
 export const formatCompileResults = (compileResults, verbose) =>
   compileResults
-    .map((componentResults) => ({
-      componentId: componentResults.component,
-      files: componentResults.buildResults,
+    .map((componentResult) => ({
+      componentId: componentResult.component,
+      files: componentResult.buildResults,
       status: 'SUCCESS',
     }))
     .reduce((outputString, result) => {
       outputString += `${chalk.green('√')} ${result.status}\t${result.componentId}`;
       if (verbose) {
-        outputString += ':\n';
-        outputString += result?.files?.reduce((fileList, file) => `${fileList}\t\t - ${file}\n`, '');
+        outputString += ':';
+        outputString += result?.files?.reduce((fileList, file) => `${fileList}\t\t - ${file}\n`, '\n');
       }
       outputString += '\n';
       return outputString;
