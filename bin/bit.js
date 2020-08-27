8a40c0c8f0fbb847502c85028fad8a1b0bff216f
David First <davidnet@gmail.com>
2020-08-26 23:23:58 -0400

    update build-harmony to work with @teambit/bit@0.0.11

--- bin/bit.js
+++ bin/bit.js
@@ -5,7 +5,7 @@ try {
   require('../node_modules/@teambit/bit/dist/app');
   // require('../node_modules/@teambit/cli/dist/app');
 } catch (err) {
-  if (err.code && err.code === 'MODULE_NOT_FOUND' && err.message && err.message.includes('cli/dist')) {
+  if (err.code && err.code === 'MODULE_NOT_FOUND' && err.message && err.message.includes('bit/dist')) {
     console.error(`** bit was moved. please run "npm run install-harmony && npm run build-harmony", then try again **`);
     process.exit(1);
   }
