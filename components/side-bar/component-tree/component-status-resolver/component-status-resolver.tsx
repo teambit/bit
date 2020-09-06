dd6b2196f6342c45cb46d7d8f42648eb21327095
oded <oded@bit.dev>
2020-09-06 15:55:25 +0300

    finished sidebar issue

--- components/side-bar/component-tree/component-status-resolver/component-status-resolver.tsx
+++ components/side-bar/component-tree/component-status-resolver/component-status-resolver.tsx
@@ -1,27 +1,28 @@
 import { ComponentID } from '@teambit/component';
 import { ComponentStatus as StatusProps } from '@teambit/workspace';
 import React from 'react';
-
+import classNames from 'classnames';
 import { ComponentStatus } from '../component-status/component-status';
 import { StatusTooltip } from '../component-tooltip';
 import styles from './component-status-resolver.module.scss';
 
 export type ComponentStatusResolverProps = {
   status?: StatusProps;
+  issuesCount?: number;
   id: ComponentID;
 };
 
-export function ComponentStatusResolver({ status, id }: ComponentStatusResolverProps) {
+export function ComponentStatusResolver({ status, id, issuesCount = 0 }: ComponentStatusResolverProps) {
   const isModified = status && (status.modifyInfo.hasModifiedDependencies || status.modifyInfo.hasModifiedFiles);
   if (!status) return null;
-  const colorOverride = isModified ? 'modified' : '';
+  const colorOverride = issuesCount > 0 ? 'error' : isModified ? 'modified' : '';
   return (
     <div className={styles.statusLine}>
+      {issuesCount > 0 && <div className={classNames(styles.errorBlock, styles.error)}>{issuesCount},</div>}
       {status.isNew && <ComponentStatus className={styles[colorOverride]} status="new" />}
       {isModified && <ComponentStatus className={styles[colorOverride]} status="modified" />}
-      {isModified && status.isStaged && ','}
+      {isModified && status.isStaged && <span className={styles[colorOverride]}>,</span>}
       {status.isStaged && <ComponentStatus className={styles[colorOverride]} status="staged" />}
-      {/* {status.isError && <ComponentStatus status="error" />} */}
       <StatusTooltip status={status} name={id?.name} />
     </div>
   );
