6cde780db3d08391651ee9ce5406bdf1376b0318
Ran Mizrahi <ran@bit.dev>
2020-09-09 05:08:54 +0300

    clean on start

--- extensions/ui/start.cmd.tsx
+++ extensions/ui/start.cmd.tsx
@@ -27,7 +27,7 @@ export class StartCmd implements Command {
   ) {}
 
   private clearConsole() {
-    // process.stdout.write(process.platform === 'win32' ? '\x1B[2J\x1B[0f' : '\x1B[2J\x1B[3J\x1B[H');
+    process.stdout.write(process.platform === 'win32' ? '\x1B[2J\x1B[0f' : '\x1B[2J\x1B[3J\x1B[H');
   }
 
   async render(
