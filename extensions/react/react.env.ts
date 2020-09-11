e4928d2728277c10c43d22ef2731a8f7db96e307
Guysaar <guy@bit.dev>
2020-09-11 16:15:47 +0300

    fix compiler task

--- extensions/react/react.env.ts
+++ extensions/react/react.env.ts
@@ -173,7 +173,7 @@ export class ReactEnv implements Environment {
    * returns the component build pipeline.
    */
   getPipe(): BuildTask[] {
-    return [this.tester.task, this.pkg.preparePackagesTask, this.pkg.dryRunTask];
+    return [this.compiler.task, this.tester.task, this.pkg.preparePackagesTask, this.pkg.dryRunTask];
   }
 
   async __getDescriptor() {
