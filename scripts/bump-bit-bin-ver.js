ae25935964b7b3cc496e85f7ffa80ddb06a19710
David First <davidnet@gmail.com>
2020-09-18 22:45:07 -0400

    add script to bump bit-bin version according to the teambit/bit

---
+++ scripts/bump-bit-bin-ver.js
@@ -0,0 +1,22 @@
+const { execSync } = require('child_process');
+const { SemVer } = require('semver');
+const path = require('path');
+
+const currentBitBinVersion = require('../package.json').version;
+console.log('currentBitBinVersion', currentBitBinVersion);
+const currentTeambitVersion = execSync('npm show @teambit/bit version').toString();
+console.log('currentTeambitVersion', currentTeambitVersion);
+const teambitSemVer = new SemVer(currentTeambitVersion);
+const nextTeambitSemVer = teambitSemVer.inc('patch');
+const nextTeambitSemVerPatch = nextTeambitSemVer.patch;
+const currentBitBinSemVer = new SemVer(currentBitBinVersion);
+const nextBitBinVersion = `${currentBitBinSemVer.major}.${currentBitBinSemVer.minor}.${currentBitBinSemVer.patch}-dev.${nextTeambitSemVerPatch}`;
+console.log('nextBitBinVersion', nextBitBinVersion);
+
+const rootDir = path.resolve(__dirname, '..');
+const sed = `sed -i '' "s/${currentBitBinVersion}/${nextBitBinVersion}/g"`;
+execSync(`${sed} package.json`, { cwd: rootDir });
+execSync(`${sed} workspace.jsonc`, { cwd: rootDir });
+execSync(`find extensions/*/*.json -type f -exec ${sed} {} \\;`, { cwd: rootDir });
+
+console.log(`completed changing all occurrences of "${currentBitBinVersion}" to "${nextBitBinVersion}"`);
