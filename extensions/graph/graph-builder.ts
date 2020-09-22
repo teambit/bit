97a26b33c3a5b5a7557b5f6bae79d31cc9da6330
Gilad Shoham <shoham.gilad@gmail.com>
2020-09-23 01:04:31 +0300

    component graph query (#3139)
    * fix get graph function
    
    * fix insights template to ink3 syntax
    
    * first version of working graph query from graphql

--- extensions/graph/graph-builder.ts
+++ extensions/graph/graph-builder.ts
@@ -1,4 +1,4 @@
-import { ComponentFactory } from '@teambit/component';
+import { ComponentID } from '@teambit/component';
 import { ScopeMain } from '@teambit/scope';
 import { Workspace } from '@teambit/workspace';
 import { buildOneGraphForComponents } from 'bit-bin/dist/scope/graph/components-graph';
@@ -8,17 +8,21 @@ import { ComponentGraph } from './component-graph';
 export class GraphBuilder {
   _graph?: ComponentGraph;
   _initialized = false;
-  constructor(private componentFactory: ComponentFactory, private workspace?: Workspace, private scope?: ScopeMain) {}
+  constructor(private workspace?: Workspace, private scope?: ScopeMain) {}
 
-  async getGraph(): Promise<ComponentGraph | undefined> {
-    if (this._graph || this._initialized) {
-      return this._graph;
-    }
+  async getGraph(ids?: string[] | ComponentID[]): Promise<ComponentGraph | undefined> {
+    // if (this._graph || this._initialized) {
+    //   return this._graph;
+    // }
     if (this.workspace) {
-      const ids = (await this.workspace.list()).map((comp) => comp.id);
-      const bitIds = ids.map((id) => id._legacy);
-      const initialGraph = await buildOneGraphForComponents(bitIds, this.workspace.consumer);
-      const graph = await ComponentGraph.buildFromLegacy(initialGraph, this.componentFactory);
+      let listIds = (ids && ids.length) ? ids : (await this.workspace.list()).map((comp) => comp.id);
+      if (typeof listIds[0] === 'string'){
+        listIds = await this.workspace.resolveMultipleComponentIds(listIds);
+      }
+      // @ts-ignore
+      const bitIds = listIds.map(id => id._legacy)
+      const legacyGraph = await buildOneGraphForComponents(bitIds, this.workspace.consumer);
+      const graph = await ComponentGraph.buildFromLegacy(legacyGraph, this.workspace);
       this._graph = graph;
       this._initialized = true;
       return this._graph;
