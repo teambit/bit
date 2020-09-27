5473b1e28c4c8701ea65a9363f68fe8bae04bd13
Uri <urigat@gmail.com>
2020-09-27 15:38:59 +0300

    add

---
+++ extensions/docs/events/click-inside-an-iframe.ts
@@ -0,0 +1,14 @@
+/* eslint-disable max-classes-per-file */
+import { BitBaseEvent } from '@teambit/pubsub';
+
+class ClickInsideAnIframeEventData {
+  constructor(private clickEvent: any) {}
+}
+
+export class ClickInsideAnIframeEvent extends BitBaseEvent<ClickInsideAnIframeEventData> {
+  static readonly TYPE = 'click-inside-an-iframe';
+
+  constructor(readonly timestamp, readonly clickEvent) {
+    super(ClickInsideAnIframeEvent.TYPE, '0.0.1', timestamp, new ClickInsideAnIframeEventData(clickEvent));
+  }
+}
