ead5b63bf0d6b9c9e4a849b9f4c1968162768dce
Uri Kutner <urikutner@gmail.com>
2020-09-30 20:19:50 +0300

    restyle according to new design

--- extensions/graph-ui-temp/ui/component-node/component-node.tsx
+++ extensions/graph-ui-temp/ui/component-node/component-node.tsx
@@ -3,6 +3,9 @@ import classnames from 'classnames';
 import { mutedText } from '@teambit/base-ui.text.muted-text';
 import { ComponentID, ComponentModel } from '@teambit/component';
 import { DeprecationIcon } from '@teambit/staged-components.deprecation-icon';
+import { ellipsis } from '@teambit/styles.ellipsis';
+import { ElevationHeight } from '@teambit/base-ui.css-components.elevation';
+import { Card, CardProps } from '@teambit/base-ui.surfaces.card';
 
 import { NodeModel } from '../query/graph-model';
 import { ComponentGraphContext } from '../dependencies-graph/';
@@ -11,25 +14,35 @@ import { ComponentGraphContext } from '../dependencies-graph/';
 import styles from './component-node.module.scss';
 import variants from './variants.module.scss';
 
-export function ComponentNode({ node, type = 'defaultNode' }: { node: NodeModel; type: string }) {
+export interface ComponentNode extends CardProps {
+  node: NodeModel;
+  type: string;
+}
+
+export function ComponentNode({ node, type = 'defaultNode', ...rest }: ComponentNode) {
   const graphContext = useContext(ComponentGraphContext);
   const { component } = node;
   const { id } = component;
 
   return (
-    <div className={classnames(styles.compNode, variants[type])}>
-      <Breadcrumbs componentId={id} className={mutedText} />
+    <Card className={classnames(styles.compNode, variants[type])} elevation={typeToElevation(type)} {...rest}>
+      <div className={styles.firstRow}>
+        <EnvIcon component={component} className={styles.envIcon} />
+        <Breadcrumbs componentId={id} className={mutedText} />
+      </div>
       <div className={styles.nameLine}>
-        <span className={styles.name}>{id.name}</span>
+        <span className={classnames(styles.name, ellipsis)}>{id.name}</span>
         {id.version && <span className={styles.version}>{id.version}</span>}
+
+        <div className={styles.buffs}>
+          <DeprecationIcon component={component} />
+          {graphContext &&
+            graphContext.componentWidgets
+              .toArray()
+              .map(([widgetId, Widget]) => <Widget key={widgetId} component={component} />)}
+        </div>
       </div>
-      <div className={styles.buffs}>
-        <EnvIcon component={component} />
-        <DeprecationIcon component={component} />
-        {graphContext &&
-          graphContext.componentWidgets.toArray().map(([widgetId, Widget]) => <Widget key={widgetId} component={component} />)}
-      </div>
-    </div>
+    </Card>
   );
 }
 
@@ -40,7 +53,7 @@ function Breadcrumbs({ componentId, className, ...rest }: BreadcrumbsProps) {
   const showSep = !!scope && !!namespace;
 
   return (
-    <div {...rest} className={classnames(styles.breadcrumbs, className)}>
+    <div {...rest} className={classnames(styles.breadcrumbs, ellipsis, className)}>
       {scope}
       {showSep && '/'}
       {namespace}
@@ -54,4 +67,13 @@ function EnvIcon({ component, ...rest }: EnvIconProps) {
   if (!component || !component.environment?.icon) return null;
 
   return <img src={component.environment?.icon} alt={component.environment.id} {...rest} />;
+}
+
+function typeToElevation(componentType: string): ElevationHeight {
+  switch (componentType) {
+    case 'root':
+      return 'medium';
+    default:
+      return 'none';
+  }
 }
