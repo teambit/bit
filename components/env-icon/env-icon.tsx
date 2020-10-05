28b97cba15e7fa27ad451b43dc8515bba3ad4bfb
Uri Kutner <urikutner@gmail.com>
2020-10-05 17:56:03 +0300

    fix

--- components/env-icon/env-icon.tsx
+++ components/env-icon/env-icon.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { ComponentModel } from '@teambit/component';
 
-export type EnvIconProps = { component: ComponentModel } & React.HTMLAttributes<HTMLDivElement>;
+export type EnvIconProps = { component: ComponentModel } & React.HTMLAttributes<HTMLImageElement>;
 
 export function EnvIcon({ component, ...rest }: EnvIconProps) {
   if (!component || !component.environment?.icon) return null;
