392b589be7d0547bca5e15bd7420fd09973f065e
oded <oded@bit.dev>
2020-10-21 20:25:09 +0300

    sidebar - separated scope and namespace tree nodes

--- components/side-bar/component-tree/index.ts
+++ components/side-bar/component-tree/index.ts
@@ -1,6 +1,7 @@
 export * from './component-tree';
 export * from './recursive-tree';
 export { ComponentView } from './component-view';
-export { ScopeView, NamespaceView } from './component-nodes';
+export { ScopeTreeNode } from './scope-tree-node';
+export { NamespaceTreeNode } from './namespace-tree-node';
 export { ScopePayload } from './payload-type';
 export type { PayloadType } from './payload-type';
--- components/side-bar/index.ts
+++ components/side-bar/index.ts
@@ -1,4 +1,11 @@
 export { ComponentTree } from './component-tree';
 export { Collapser } from './sidebar-collapser';
 export { StatusTypes } from './component-tree/recursive-tree';
-export { ComponentView, ScopeView, NamespaceView, PayloadType, TreeNodeProps, ScopePayload } from './component-tree';
+export {
+  ComponentView,
+  ScopeTreeNode,
+  NamespaceTreeNode,
+  PayloadType,
+  TreeNodeProps,
+  ScopePayload,
+} from './component-tree';
