7738e38ad5f888751a0a231bfb3ad8a7ad3dc263
oded <oded@bit.dev>
2020-10-22 13:36:49 +0300

    merged exports

--- components/side-bar/index.ts
+++ components/side-bar/index.ts
@@ -1,7 +1,7 @@
-export { ComponentTree } from './component-tree';
 export { Collapser } from './sidebar-collapser';
 export { StatusTypes } from './component-tree/recursive-tree';
 export {
+  ComponentTree,
   ComponentView,
   ScopeTreeNode,
   NamespaceTreeNode,
