a58809afa05c8c6ef45f0dd2929e6175c6275097
David First <davidnet@gmail.com>
2020-10-23 17:41:47 -0400

    another attempt

--- scripts/soft-tag-teambit.js
+++ scripts/soft-tag-teambit.js
@@ -1,6 +1,9 @@
 const { execSync } = require('child_process');
 const { SemVer } = require('semver');
 
+const currentBitBinVersionInNpm = execSync('npm view bit-bin@dev version').toString();
+console.log('currentBitBinVersionInNpm', currentBitBinVersionInNpm);
+
 // const currentTeambitVersion = execSync('npm show @teambit/bit version').toString();
 const currentTeambitVersion = '0.0.81'; // TEMP ONLY
 console.log('currentTeambitVersion', currentTeambitVersion);
@@ -8,6 +11,9 @@ const teambitSemVer = new SemVer(currentTeambitVersion);
 const nextTeambitSevVer = teambitSemVer.inc('patch');
 const nextTeambitVersion = nextTeambitSevVer.version;
 console.log('nextTeambitSemVer', nextTeambitVersion);
+
+const currentBitBinVersionInNpmAgain = execSync('npm view bit-bin@dev version').toString();
+console.log('currentBitBinVersionInNpmAgain', currentBitBinVersionInNpmAgain);
 
 try {
   const output = execSync(`bit tag -a -s ${nextTeambitVersion}`);
