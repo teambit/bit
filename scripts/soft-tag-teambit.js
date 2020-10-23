ee4aac23e49d7feeac3bf00b0a5eca0dc83d2537
David First <davidnet@gmail.com>
2020-10-23 17:25:55 -0400

    attempt to fix CircleCI publishing issues

--- scripts/soft-tag-teambit.js
+++ scripts/soft-tag-teambit.js
@@ -1,6 +1,9 @@
 const { execSync } = require('child_process');
 const { SemVer } = require('semver');
 
+const currentBitBinVersionInNpm = execSync('npm view bit-bin@dev version').toString();
+console.log('currentBitBinVersionInNpm', currentBitBinVersionInNpm);
+
 // const currentTeambitVersion = execSync('npm show @teambit/bit version').toString();
 const currentTeambitVersion = '0.0.81'; // TEMP ONLY
 console.log('currentTeambitVersion', currentTeambitVersion);
