6c58c1ae0f07821b4d808ddd4fc492d63e9048b5
Ran Mizrahi <ran@bit.dev>
2020-10-24 05:57:18 +0300

    jest and ts config for linter

--- extensions/react/eslint/eslintrc.js
+++ extensions/react/eslint/eslintrc.js
@@ -1,8 +1,22 @@
 module.exports = () => {
   return {
-    extends: ['airbnb-typescript'],
-    // parserOptions: {
-    //   project: require.resolve('')
-    // }
+    extends: ['airbnb-typescript', 'plugin:jest/recommended'],
+    plugins: ['jest'],
+    parserOptions: {
+      project: require.resolve('../typescript/tsconfig.json'),
+    },
+    settings: {
+      jest: {
+        version: 26,
+      },
+    },
+    rules: {
+      '@typescript-eslint/camelcase': 'off',
+      'import/no-extraneous-dependencies': 'off',
+      'import/prefer-default-export': 'off',
+    },
+    env: {
+      'jest/globals': true,
+    },
   };
 };
