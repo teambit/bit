e5b093926ff1f3499734a3ee59b48565ca8383ce
Oded Reuveny <oded@bit.dev>
2020-10-26 15:44:59 +0200

    aspect link in menu moved to right side (#3199)
    - moved aspect menu link to the right
    - for now, the link doesn't have the icons or the count of the aspects, just 'config'.
    Co-authored-by: Gilad Shoham <shoham.gilad@gmail.com>

--- extensions/component/aspect.section.tsx
+++ extensions/component/aspect.section.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
+import { Icon } from '@teambit/evangelist.elements.icon';
 import { Section } from './section';
 import { AspectPage } from './ui/aspect-page';
+import styles from './aspect.section.module.scss';
 
 export class AspectSection implements Section {
   route = {
@@ -9,7 +11,7 @@ export class AspectSection implements Section {
   };
   navigationLink = {
     href: '~aspect',
-    children: 'Configuration',
+    children: <Icon of="settings" className={styles.icon} />,
   };
   order = 50;
 }
