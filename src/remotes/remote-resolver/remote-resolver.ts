7feca2724476d093717c81ba6ad4144a8e9c3ff2
Ran Mizrahi <ranm8@users.noreply.github.com>
2020-10-26 22:36:50 +0200

    Symphony/default resolver (#3269)
    * useComponent supports version
    
    * node env api
    
    * initial commit
    
    * try catch for legacy in case of failure.
    
    * authorization

--- src/remotes/remote-resolver/remote-resolver.ts
+++ src/remotes/remote-resolver/remote-resolver.ts
@@ -1,56 +1,62 @@
 /* @flow */
 import R from 'ramda';
-// import { GraphQLClient, gql } from 'graphql-request';
+import { GraphQLClient, gql } from 'graphql-request';
 import { getSync } from '../../api/consumer/lib/global-config';
 import {
   CFG_HUB_DOMAIN_KEY,
   DEFAULT_HUB_DOMAIN,
-  // CFG_USER_TOKEN_KEY,
-  // SYMPHONY_URL,
-  // CFG_SYMPHONY_URL_KEY,
+  CFG_USER_TOKEN_KEY,
+  SYMPHONY_URL,
+  CFG_SYMPHONY_URL_KEY,
 } from '../../constants';
 import Scope from '../../scope/scope';
+import logger from '../../../src/logger/logger';
 
 const hubDomain = getSync(CFG_HUB_DOMAIN_KEY) || DEFAULT_HUB_DOMAIN;
-// const symphonyUrl = getSync(CFG_SYMPHONY_URL_KEY) || SYMPHONY_URL;
+const symphonyUrl = getSync(CFG_SYMPHONY_URL_KEY) || SYMPHONY_URL;
 
-// const scopeCache = {};
+const scopeCache = {};
 
-// const SCOPE_GET = gql`
-//   query GET_SCOPE($id: String!) {
-//     getScope(id: $id) {
-//       isLegacy
-//       api {
-//         url
-//       }
-//     }
-//   }
-// `;
+const SCOPE_GET = gql`
+  query GET_SCOPE($id: String!) {
+    getScope(id: $id) {
+      isLegacy
+      apis {
+        url
+      }
+    }
+  }
+`;
 
 // comment this out once on production
-// async function getScope(name: string) {
-//   if (scopeCache[name]) return scopeCache[name];
-//   const token = getSync(CFG_USER_TOKEN_KEY);
-//   const client = new GraphQLClient(symphonyUrl);
-//   if (token) client.setHeader('Authorization', token);
+async function getScope(name: string) {
+  if (scopeCache[name]) return scopeCache[name];
+  const token = getSync(CFG_USER_TOKEN_KEY);
+  const client = new GraphQLClient(`https://${symphonyUrl}/graphql`);
+  if (token) client.setHeader('Authorization', `Bearer ${token}`);
 
-//   const res = client.request(SCOPE_GET, {
-//     name,
-//   });
+  try {
+    const res = client.request(SCOPE_GET, {
+      id: name,
+    });
 
-//   scopeCache[name] = res;
-//   return res;
-// }
+    scopeCache[name] = res;
+    return res;
+  } catch (err) {
+    logger.error(err);
+    return undefined;
+  }
+}
 
 const hubResolver = async (scopeName) => {
   // check if has harmony
-  // const scope = await getScope(scopeName);
-  // if (scope.getScope.isLegacy) {
-  const hubPrefix = `ssh://bit@${hubDomain}:`;
-  return hubPrefix + scopeName;
-  // }
+  const scope = await getScope(scopeName);
+  if (scope && scope.getScope.isLegacy) {
+    const hubPrefix = `ssh://bit@${hubDomain}:`;
+    return hubPrefix + scopeName;
+  }
 
-  // return scope.getScope.apis.url;
+  return `https://${scope.getScope.apis.url}`;
 };
 
 const remoteResolver = (scopeName: string, thisScope?: Scope): Promise<string> => {
