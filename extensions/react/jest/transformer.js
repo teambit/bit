5b1b819c45280cc4beb3030e28d0b2ffbb96e74d
David First <davidnet@gmail.com>
2020-10-27 16:09:45 -0400

    fix jest tester when running on other projects

--- extensions/react/jest/transformer.js
+++ extensions/react/jest/transformer.js
@@ -14,23 +14,27 @@ const presets = [
     },
   ],
 ];
-const plugins = [
-  [require.resolve('@babel/plugin-transform-modules-commonjs')],
-  require.resolve('babel-plugin-transform-typescript-metadata'),
-  [require.resolve('@babel/plugin-proposal-decorators'), { legacy: true }],
-  [require.resolve('@babel/plugin-transform-runtime')],
-  [require.resolve('@babel/plugin-proposal-object-rest-spread')],
-  [require.resolve('@babel/plugin-proposal-class-properties')],
-  [require.resolve('@babel/plugin-transform-async-to-generator')],
-  [require.resolve('babel-preset-jest')],
-];
+
+// @todo: it is needed? on bit-bin it works regardless, but on bad-jokes it fails with an error:
+// Error: [BABEL] /Users/davidfirst/Library/Caches/Bit/capsules/5e3640683e6a965d934a01a4cb6b74c7d1d5edd1/teambit.bad-jokes_ui-primitives_button/dist/button.spec.jsx: .plugins is not a valid Plugin property
+
+// const plugins = [
+//   [require.resolve('@babel/plugin-transform-modules-commonjs')],
+//   require.resolve('babel-plugin-transform-typescript-metadata'),
+//   [require.resolve('@babel/plugin-proposal-decorators'), { legacy: true }],
+//   [require.resolve('@babel/plugin-transform-runtime')],
+//   [require.resolve('@babel/plugin-proposal-object-rest-spread')],
+//   [require.resolve('@babel/plugin-proposal-class-properties')],
+//   [require.resolve('@babel/plugin-transform-async-to-generator')],
+//   [require.resolve('babel-preset-jest')],
+// ];
 
 module.exports = {
   process(src, filename) {
     const result = transform(src, {
       filename,
       presets,
-      plugins,
+      // plugins,
     });
 
     return result ? result.code : src;
