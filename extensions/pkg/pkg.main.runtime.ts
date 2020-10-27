e5da0327043e094ccf981707e8a59f22cc07312b
Gilad Shoham <shoham.gilad@gmail.com>
2020-10-27 21:15:58 +0200

    filter get manifest for versions without manifests

--- extensions/pkg/pkg.main.runtime.ts
+++ extensions/pkg/pkg.main.runtime.ts
@@ -302,6 +302,10 @@ export class PkgMain {
     }
     const currentExtension = updatedComponent.state.aspects.get(PkgAspect.id);
     const currentData = (currentExtension?.data as unknown) as ComponentPkgExtensionData;
+    // If for some reason the version has no package.json manifest, return undefined
+    if (!currentData?.pkgJson) {
+      return undefined;
+    }
     const pkgJson = currentData?.pkgJson ?? {};
     const checksum = currentData?.checksum;
     if (!checksum) {
