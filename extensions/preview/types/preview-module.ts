12fc14559ee1a7a02ddd2c4dbb3510cc73a2c0a3
Uri Kutner <urikutner@gmail.com>
2020-10-28 19:01:08 +0200

    cleanup

--- extensions/preview/types/preview-module.ts
+++ extensions/preview/types/preview-module.ts
@@ -5,7 +5,6 @@ export type PreviewModule<T = any> = {
   /** Dictionary mapping components to their module files. */
   componentMap: Record<string, ModuleFile<T>[]>;
 
-  // TODO - refactor to be single render method
   /** The 'main file' for this Preview type */
   mainModule: {
     default: (...args: any[]) => void;
