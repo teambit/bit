ac486683c1e1758e14c8f76b8fd94dc9fae24992
Gilad Shoham <shoham.gilad@gmail.com>
2020-10-29 16:43:19 +0200

    fix bump bit bin version

--- scripts/bump-bit-bin-ver.js
+++ scripts/bump-bit-bin-ver.js
@@ -24,6 +24,6 @@ const rootDir = path.resolve(__dirname, '..');
 const sed = `sed -i "s/${currentBitBinVersionInCode}/${nextBitBinVersion}/g"`;
 execSync(`${sed} package.json`, { cwd: rootDir });
 execSync(`${sed} workspace.jsonc`, { cwd: rootDir });
-execSync(`find **/component.json -type f -exec ${sed} {} \\;`, { cwd: rootDir });
+execSync(`find scopes/**/component.json -type f -exec ${sed} {} \\;`, { cwd: rootDir });
 
 console.log(`completed changing all occurrences of "${currentBitBinVersionInCode}" to "${nextBitBinVersion}"`);
