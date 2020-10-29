c33f20613880e9dec0380c4733580998131e97e3
Uri <urigat@gmail.com>
2020-10-29 19:40:33 +0200

    types

--- scopes/compilers/compiler/events/component-compilation-on-done.ts
+++ scopes/compilers/compiler/events/component-compilation-on-done.ts
@@ -1,14 +1,26 @@
 /* eslint-disable max-classes-per-file */
 import { BitBaseEvent } from '@teambit/pubsub';
+import { BuildResult, CompileError } from '../workspace-compiler';
+
+import type ConsumerComponent from 'bit-bin/dist/consumer/component';
 
 class ComponentCompilationOnDoneEventData {
-  constructor(readonly errors: Array<any>, readonly component: any, readonly buildResults: Array<any>) {}
+  constructor(
+    readonly errors: Array<CompileError>,
+    readonly component: ConsumerComponent,
+    readonly buildResults: Array<BuildResult>
+  ) {}
 }
 
 export class ComponentCompilationOnDoneEvent extends BitBaseEvent<ComponentCompilationOnDoneEventData> {
   static readonly TYPE = 'component-compilation-on-done';
 
-  constructor(readonly errors, readonly component, readonly buildResults, readonly timestamp = Date.now().toString()) {
+  constructor(
+    readonly errors: Array<CompileError>,
+    readonly component: ConsumerComponent,
+    readonly buildResults: Array<BuildResult>,
+    readonly timestamp = Date.now().toString()
+  ) {
     super(
       ComponentCompilationOnDoneEvent.TYPE,
       '0.0.1',
