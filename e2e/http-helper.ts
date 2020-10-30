8dd76045fe5ebcef619c375be1504c10bb26a76f
David First <davidnet@gmail.com>
2020-10-30 14:58:05 -0400

    re-enable-http-tests (#3294)
    fix http e2e-tests to use `spawn` instead of `exec` for the `bit start` process. now it exits the process properly.

--- e2e/http-helper.ts
+++ e2e/http-helper.ts
@@ -5,7 +5,9 @@ import rightpad from 'pad-right';
 
 import Helper from '../src/e2e-helper/e2e-helper';
 
-const HTTP_TIMEOUT_FOR_MSG = 60000; // 1 min
+const HTTP_TIMEOUT_FOR_MSG = 120000; // 2 min
+
+const HTTP_SERVER_READY_MSG = 'Main UI server is running on';
 
 export class HttpHelper {
   httpProcess: ChildProcess;
@@ -16,12 +18,11 @@ export class HttpHelper {
       const cwd = this.helper.scopes.remotePath;
       if (this.helper.debugMode) console.log(rightpad(chalk.green('cwd: '), 20, ' '), cwd); // eslint-disable-line no-console
       if (this.helper.debugMode) console.log(rightpad(chalk.green('command: '), 20, ' '), cmd); // eslint-disable-line
-      this.httpProcess = childProcess.exec(cmd, { cwd });
+      this.httpProcess = childProcess.spawn(this.helper.command.bitBin, ['start', '--verbose'], { cwd });
       // @ts-ignore AUTO-ADDED-AFTER-MIGRATION-PLEASE-FIX!
       this.httpProcess.stdout.on('data', (data) => {
         if (this.helper.debugMode) console.log(`stdout: ${data}`);
-        const msg = 'Starting the development servers';
-        if (data.includes(msg)) {
+        if (data.includes(HTTP_SERVER_READY_MSG)) {
           if (this.helper.debugMode) console.log('http server is up and running');
           resolve();
         }
