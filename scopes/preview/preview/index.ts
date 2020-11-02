9fdca8ba710cb110969f356e081e7027e93b0c20
Kutner JS <urikutner@gmail.com>
2020-11-02 20:25:58 +0200

    Multi compositions (#3250)
    - live updates for compositions files and entries.
    - show default not found page when a composition is missing (in compositions iframe)
    - merge multiple compositions files to a single object.
    - within the same component, compositions are expected to be unique, even in different files (decreed by Ran)

--- scopes/preview/preview/index.ts
+++ scopes/preview/preview/index.ts
@@ -1,9 +1,8 @@
-import { PreviewAspect, PreviewRuntime } from './preview.aspect';
+export { PreviewAspect as default, PreviewAspect, PreviewRuntime } from './preview.aspect';
 
 export * from './events';
 export type { PreviewMain } from './preview.main.runtime';
 export type { PreviewPreview } from './preview.preview.runtime';
 export { PreviewDefinition } from './preview-definition';
 export { ComponentPreview } from './ui';
-export { PreviewAspect, PreviewRuntime };
-export default PreviewAspect;
+export type { PreviewModule, ModuleFile } from './types/preview-module';
