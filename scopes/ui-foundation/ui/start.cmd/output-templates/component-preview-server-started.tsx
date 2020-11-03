ef1447b45382a97fafd91531b22ea643cabb57c7
urigat <urigat@gmail.com>
2020-11-03 13:11:33 +0200

    fixed the order of servers on the cli (#3318)

--- scopes/ui-foundation/ui/start.cmd/output-templates/component-preview-server-started.tsx
+++ scopes/ui-foundation/ui/start.cmd/output-templates/component-preview-server-started.tsx
@@ -10,18 +10,20 @@ export const ComponentPreviewServerStarted = ({ items }: props) =>
   !items.length ? null : (
     <>
       <ComponentPreviewServerStartedHeaders />
-      {items.map((item, index) => (
-        <Box key={index}>
-          <Box width="40%">
-            <Text color="rgb(45, 164, 157)">{item.id}</Text>
+      {items
+        .sort((serverA, serverB) => (serverA.id > serverB.id ? 1 : -1))
+        .map((item, index) => (
+          <Box key={index}>
+            <Box width="40%">
+              <Text color="rgb(45, 164, 157)">{item.id}</Text>
+            </Box>
+            <Box width="40%">
+              <Text>{`http://${item.server.host || 'localhost'}:${item.server.port}`}</Text>
+            </Box>
+            <Box width="20%">
+              <Text color="yellow">{item.status}</Text>
+            </Box>
           </Box>
-          <Box width="40%">
-            <Text>{`http://${item.server.host || 'localhost'}:${item.server.port}`}</Text>
-          </Box>
-          <Box width="20%">
-            <Text color="yellow">{item.status}</Text>
-          </Box>
-        </Box>
-      ))}
+        ))}
     </>
   );
