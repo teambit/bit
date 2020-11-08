dd8354c88eac20f7fe2385d7b64c13420ddaed03
Uri Kutner <urikutner@gmail.com>
2020-11-08 18:49:59 +0200

    show available ui roots when current one is invalid

--- scopes/ui-foundation/ui/exceptions/unknown-ui.ts
+++ scopes/ui-foundation/ui/exceptions/unknown-ui.ts
@@ -1,9 +1,9 @@
 export class UnknownUI extends Error {
-  constructor(readonly uiRoot: string) {
+  constructor(readonly uiRoot: string, readonly available?: string[]) {
     super();
   }
 
   toString() {
-    return `unknown UI root: ${this.uiRoot}`;
+    return `unknown UI root: ${this.uiRoot}. Available ui roots are: [${this.available?.join(', ')}]`;
   }
 }
