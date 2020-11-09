be5be089b2c2f757a6e5840ef7e04c43940e17bd
Ran Mizrahi <ran@bit.dev>
2020-11-09 22:49:14 +0200

    remove parser from babel

--- scopes/compilation/babel/babel.main.runtime.ts
+++ scopes/compilation/babel/babel.main.runtime.ts
@@ -1,13 +1,11 @@
 import { MainRuntime } from '@teambit/cli';
 import { Compiler, CompilerAspect, CompilerMain } from '@teambit/compiler';
 import { Logger, LoggerAspect, LoggerMain } from '@teambit/logger';
-import type { SchemaMain } from '@teambit/schema';
 import { SchemaAspect } from '@teambit/schema';
 import * as babel from '@babel/core';
 import { BabelCompilerOptions } from './compiler-options';
 import { BabelAspect } from './babel.aspect';
 import { BabelCompiler } from './babel.compiler';
-import { BabelParser } from './babel.parser';
 
 export class BabelMain {
   constructor(private logger: Logger, private compiler: CompilerMain) {}
@@ -25,8 +23,7 @@ export class BabelMain {
   static runtime = MainRuntime;
   static dependencies = [SchemaAspect, LoggerAspect, CompilerAspect];
 
-  static async provider([schema, loggerExt, compiler]: [SchemaMain, LoggerMain, CompilerMain]) {
-    schema.registerParser(new BabelParser());
+  static async provider([loggerExt, compiler]: [LoggerMain, CompilerMain]) {
     const logger = loggerExt.createLogger(BabelAspect.id);
     return new BabelMain(logger, compiler);
   }
