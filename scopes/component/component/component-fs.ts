bf4cd425a5aa5aa691475a771f12b59c96cfca26
Ran Mizrahi <ranm8@users.noreply.github.com>
2020-11-11 01:42:01 +0200

    Dev files (#3355)
    - Supporting dev files in dep resolver to identify dev deps automatically.
    - Fixed performance issue with TS compilation on the Bit workspace during aspect compilation.
    - Dev files can be configured in config.
    - Dev files can be configured through the env new getDevFiles() API.
    - Extensions can configure dev file patterns through a new API registerDevPattern()
    - Harmony link is written once in the root node_modules
    - Compositions now accepts .composition. and .compositions. by default.
    - Docs now accepts .docs. by default.
    - Tester now accepts .spec. and .test. by default.
    - Per aspect dev files API.
    - All glob file predicate now accept glob patterns. all apply on each component file paths,

--- scopes/component/component/component-fs.ts
+++ scopes/component/component/component-fs.ts
@@ -2,6 +2,7 @@ import { MemoryFS } from '@teambit/any-fs';
 import type { AbstractVinyl } from 'bit-bin/dist/consumer/component/sources';
 import { auto } from 'bit-bin/dist/utils/eol';
 import path from 'path';
+import minimatch from 'minimatch';
 
 /**
  * The virtual component filesystem
@@ -27,6 +28,18 @@ export default class ComponentFS extends MemoryFS {
    */
   byRegex(extension: RegExp): AbstractVinyl[] {
     return this.files.filter((file) => file.path.match(extension));
+  }
+
+  /**
+   * filter all files using an array of glob patterns.
+   */
+  byGlob(patterns: string[]) {
+    return this.files.filter((file) => {
+      return patterns.find((pattern) => {
+        const match = minimatch(file.relative, pattern);
+        return match;
+      });
+    });
   }
 
   toObject() {}
