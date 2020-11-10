6f6e6df53a489bdcc376e1bbddb3b0a1e5e03eb3
Josh Kuttler <yoshuakuttler@gmail.com>
2020-11-10 19:53:23 +0200

    add ts-ignore for expect undefined

--- scopes/component/ui/component-status-resolver/component-status-resolver.spec.tsx
+++ scopes/component/ui/component-status-resolver/component-status-resolver.spec.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable no-undef */
 import React from 'react';
 import { render } from '@testing-library/react';
 import {
@@ -15,28 +16,36 @@ describe('Component Status Resolver', () => {
     const { getByText } = render(<ComponentStatusResolverWithModifiedDependencies />);
     const textStatus = getByText(/^M$/);
     const textTooltip = getByText(/^Modified dependencies$/);
+    // @ts-ignore
     expect(textStatus).toBeInTheDocument();
+    // @ts-ignore
     expect(textTooltip).toBeInTheDocument();
   });
   it('should render a modified status with modified files', () => {
     const { getByText } = render(<ComponentStatusResolverWithModifiedFiles />);
     const textStatus = getByText(/^M$/);
     const textTooltip = getByText(/^Modified files$/);
+    // @ts-ignore
     expect(textStatus).toBeInTheDocument();
+    // @ts-ignore
     expect(textTooltip).toBeInTheDocument();
   });
   it('should render a new status with new component', () => {
     const { getByText } = render(<ComponentStatusResolverWithNewStatus />);
     const textStatus = getByText(/^N$/);
     const textTooltip = getByText(/^New component$/);
+    // @ts-ignore
     expect(textStatus).toBeInTheDocument();
+    // @ts-ignore
     expect(textTooltip).toBeInTheDocument();
   });
   it('should render a staged status with staged component', () => {
     const { getByText } = render(<ComponentStatusResolverWithStagedStatus />);
     const textStatus = getByText(/^S$/);
     const textTooltip = getByText(/^Staged component$/);
+    // @ts-ignore
     expect(textStatus).toBeInTheDocument();
+    // @ts-ignore
     expect(textTooltip).toBeInTheDocument();
   });
   it('should render a new status with issue', () => {
@@ -44,8 +53,11 @@ describe('Component Status Resolver', () => {
     const textStatus = getByText(/^N$/);
     const textTooltip = getByText(/^New component$/);
     const textTooltipIssue = getByText(/^1 issue found$/);
+    // @ts-ignore
     expect(textStatus).toBeInTheDocument();
+    // @ts-ignore
     expect(textTooltip).toBeInTheDocument();
+    // @ts-ignore
     expect(textTooltipIssue).toBeInTheDocument();
   });
   it('should render a staged status with issue', () => {
@@ -53,8 +65,11 @@ describe('Component Status Resolver', () => {
     const textStatus = getByText(/^S$/);
     const textTooltip = getByText(/^Staged component$/);
     const textTooltipIssue = getByText(/^1 issue found$/);
+    // @ts-ignore
     expect(textStatus).toBeInTheDocument();
+    // @ts-ignore
     expect(textTooltip).toBeInTheDocument();
+    // @ts-ignore
     expect(textTooltipIssue).toBeInTheDocument();
   });
 
@@ -64,9 +79,13 @@ describe('Component Status Resolver', () => {
     const textTooltipIssue = getByText(/^1 issue found$/);
     const textModifiedDependencies = getByText(/^Modified dependencies$/);
     const textModifiedFiles = getByText(/^Modified files$/);
+    // @ts-ignore
     expect(textStatus).toBeInTheDocument();
+    // @ts-ignore
     expect(textTooltipIssue).toBeInTheDocument();
+    // @ts-ignore
     expect(textModifiedDependencies).toBeInTheDocument();
+    // @ts-ignore
     expect(textModifiedFiles).toBeInTheDocument();
   });
 });
