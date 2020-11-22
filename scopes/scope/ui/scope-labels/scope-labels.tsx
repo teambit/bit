e017ba7cebfad13a445fe7c9541c35ce0a997ef3
Oded Reuveny <oded@bit.dev>
2020-11-22 21:17:18 +0200

    Scope overview (#3420)
    - added sidebar links slot to workspace and scope
    - added overview link to workspace and scope
    - removed the overview link from the component drawer

--- scopes/scope/ui/scope-labels/scope-labels.tsx
+++ scopes/scope/ui/scope-labels/scope-labels.tsx
@@ -1,30 +1,31 @@
-import { Icon } from '@teambit/evangelist.elements.icon';
+import { flatten } from 'lodash';
 import { ScopeBadgeSlot } from '@teambit/scope';
-import { PillLabel } from '@teambit/ui.pill-label';
 import classNames from 'classnames';
 import React from 'react';
+import { PillLabel } from '@teambit/ui.pill-label';
 
 import styles from './scope-labels.module.scss';
 
 type ScopeLabelsProps = {
   badgeSlot: ScopeBadgeSlot;
+  componentCount?: number;
 } & React.HTMLAttributes<HTMLDivElement>;
 
-export function ScopeLabels({ badgeSlot, className }: ScopeLabelsProps) {
-  const badges = badgeSlot.values();
+export function ScopeLabels({ badgeSlot, componentCount, className }: ScopeLabelsProps) {
+  const badges = flatten(badgeSlot.values());
 
   return (
     <div className={classNames(styles.pillsContainer, className)}>
       {badges.map((badge, key) => {
-        const UserBadge = badge.badge;
-        if (UserBadge) return <UserBadge key={key} label={badge.label} icon={badge.icon} />;
-        return (
-          <PillLabel key={key}>
-            <Icon of={badge.icon} className={styles.pillIcon} />
-            {badge.label}
-          </PillLabel>
-        );
+        const UserBadge = badge;
+        return <UserBadge key={key} />;
       })}
+      {componentCount && (
+        <PillLabel>
+          <span className={styles.componentCount}>{componentCount}</span>
+          <span>Components</span>
+        </PillLabel>
+      )}
     </div>
   );
 }
--- scopes/ui-foundation/routing/native-link/native-link.tsx
+++ scopes/ui-foundation/routing/native-link/native-link.tsx
@@ -1,10 +1,10 @@
 import React, { useCallback } from 'react';
 
 export type LinkProps = {
-	/** When true, clicking the link will replace the current entry in the history stack instead of adding a new one */
-	replace?: boolean;
-	/** Open link in a new tab */
-	external?: boolean;
+  /** When true, clicking the link will replace the current entry in the history stack instead of adding a new one */
+  replace?: boolean;
+  /** Open link in a new tab */
+  external?: boolean;
 } & React.AnchorHTMLAttributes<HTMLAnchorElement>;
 
 /** anchor tag attributes that securely open link in a new tab. */
@@ -15,20 +15,20 @@ const externalLinkAttributes = { rel: 'noopener', target: '_blank' };
  * Used to provide default fallbacks for react-router link
  */
 export function NativeLink({ href = '', external, replace, onClick, ...rest }: LinkProps) {
-	const externalProps = external ? externalLinkAttributes : {};
+  const externalProps = external ? externalLinkAttributes : {};
 
-	const handleClick = useCallback(
-		(e: React.MouseEvent<HTMLAnchorElement, MouseEvent>) => {
-			const res = onClick?.(e);
-			if (e.defaultPrevented || !replace || external) return res;
+  const handleClick = useCallback(
+    (e: React.MouseEvent<HTMLAnchorElement, MouseEvent>) => {
+      const res = onClick?.(e);
+      if (e.defaultPrevented || !replace || external) return res;
 
-			e.preventDefault();
-			window.location.replace(href);
+      e.preventDefault();
+      window.location.replace(href);
 
-			return res;
-		},
-		[href, replace, onClick]
-	);
+      return res;
+    },
+    [href, replace, onClick]
+  );
 
-	return <a {...rest} {...externalProps} onClick={handleClick} href={href} />;
+  return <a {...rest} {...externalProps} onClick={handleClick} href={href} />;
 }
