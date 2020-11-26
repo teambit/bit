e4f4a5e6fab3224ce24b806d995e57d13e27f66a
Ran Mizrahi <ran@bit.dev>
2020-11-26 14:50:16 +0200

    working json

--- scopes/react/react/eslint/eslintrc.js
+++ scopes/react/react/eslint/eslintrc.js
@@ -18,13 +18,3 @@ module.exports = {
     'jest/globals': true,
   },
 };
-/**
- * @gilad please set these deps.
- *
- * "eslint-plugin-import": "^2.22.0",
- * "eslint-plugin-jest": "^24.1.0",
- * "@typescript-eslint/eslint-plugin": "^4.4.1",
- * "eslint-plugin-react-hooks": "^4.0.8",
- * "eslint-plugin-react": "^7.20.3",
- * "eslint-plugin-jsx-a11y": "^6.3.1",
- */
