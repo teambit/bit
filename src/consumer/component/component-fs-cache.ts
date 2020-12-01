b43aed6f58a57b16047f98028c902e26058727db
David First <davidnet@gmail.com>
2020-12-01 13:24:56 -0500

    move all last-track cache into a dedicated dir

--- src/consumer/component/component-fs-cache.ts
+++ src/consumer/component/component-fs-cache.ts
@@ -3,23 +3,22 @@ import path from 'path';
 import { CACHE_ROOT } from '../../constants';
 
 const CACHE_PATH = path.join(CACHE_ROOT, 'components-cache');
+const LAST_TRACK_CACHE_PATH = path.join(CACHE_PATH, 'last-track');
 
 export async function getLastTrackTimestamp(idStr: string): Promise<number> {
-  const key = `${idStr}-last-track`;
-  const data = await getFromCacheIfExist(key);
+  const data = await getFromCacheIfExist(LAST_TRACK_CACHE_PATH, idStr);
   return data ? parseInt(data.toString()) : 0;
 }
 
 export async function setLastTrackTimestamp(idStr: string, timestamp: number): Promise<void> {
-  const key = `${idStr}-last-track`;
-  await cacache.put(CACHE_PATH, key, Buffer.from(timestamp.toString()));
+  await cacache.put(LAST_TRACK_CACHE_PATH, idStr, Buffer.from(timestamp.toString()));
 }
 
-async function getFromCacheIfExist(key: string): Promise<Buffer | null> {
+async function getFromCacheIfExist(cachePath: string, key: string): Promise<Buffer | null> {
   try {
-    const results = await cacache.get(CACHE_PATH, key);
+    const results = await cacache.get(cachePath, key);
     return results.data;
-  } catch(err) {
+  } catch (err) {
     if (err.code === 'ENOENT') {
       return null; // cache doesn't exists
     }
