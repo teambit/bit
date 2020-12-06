5d38b789cb747f420da0f74255d02c983a30cd17
Ran Mizrahi <ran@bit.dev>
2020-12-06 21:09:24 +0200

    removed test file

--- scopes/pipelines/modules/merge-component-results/merge-component-results.spec.ts
+++
@@ -1,8 +0,0 @@
-// import { expect } from 'chai';
-// import { mergeComponentResults } from './merge-component-results';
-
-// describe('mergeComponentResults()', () => {
-//   it('should merge two simple component result arrays', () => {
-//     // mergeComponentResults([]);
-//   });
-// });
