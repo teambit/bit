8199bc544769c1b48d545ebdc2f3be710cb3145c
David First <davidnet@gmail.com>
2020-12-07 09:26:59 -0500

    introduce a new flag "headers" for "bit cat-object" command to show the object headers

--- src/api/scope/lib/cat-object.ts
+++ src/api/scope/lib/cat-object.ts
@@ -1,9 +1,12 @@
 import { loadScope } from '../../../scope';
 
-export default function catObject(hash: string, pretty: boolean, stringify: boolean) {
+export default function catObject(hash: string, pretty: boolean, stringify: boolean, headers: boolean) {
   // @ts-ignore AUTO-ADDED-AFTER-MIGRATION-PLEASE-FIX!
   return loadScope().then((scope) => {
     return scope.getRawObject(hash).then((object) => {
+      if (headers) {
+        return object.headers;
+      }
       if (!object) return 'object not found';
       if (stringify) return JSON.stringify(object.content.toString());
       return object.getString(pretty);
