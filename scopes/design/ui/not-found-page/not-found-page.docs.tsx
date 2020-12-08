20f1951816ae34a2dee9da07b14a3453493ceeb1
Josh Kuttler <yoshuakuttler@gmail.com>
2020-12-08 19:01:46 +0200

    add composition, test and docs files

--- scopes/design/ui/not-found-page/not-found-page.docs.tsx
+++ scopes/design/ui/not-found-page/not-found-page.docs.tsx
@@ -1,5 +1,31 @@
 import React from 'react';
+import { Section } from '@teambit/documenter.ui.section';
+import { ThemeContext } from '@teambit/documenter.theme.theme-context';
+import { Separator } from '@teambit/documenter.ui.separator';
+import { NotFoundPage } from './not-found-page';
 
 export default function Overview() {
-  return null;
+  return (
+    <ThemeContext>
+      <>
+        <Section>This component is shown when a page does not exist.</Section>
+        <Separator />
+      </>
+    </ThemeContext>
+  );
 }
+
+Overview.abstract = 'A component that shows a 404 error page.';
+
+Overview.labels = ['react', 'typescript', '404', 'error'];
+
+Overview.examples = [
+  {
+    scope: {
+      NotFoundPage,
+    },
+    title: 'Error Component Status',
+    description: 'Using the component with error status property',
+    jsx: <NotFoundPage />,
+  },
+];
