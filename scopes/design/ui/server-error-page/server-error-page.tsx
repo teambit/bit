c659199f3a9cd117a49dac961e7cd2835df88bae
Josh Kuttler <35892475+JoshK2@users.noreply.github.com>
2020-12-10 17:04:32 +0200

    server-error-page component - add composition, test and docs files (#3496)

--- scopes/design/ui/server-error-page/server-error-page.tsx
+++ scopes/design/ui/server-error-page/server-error-page.tsx
@@ -6,9 +6,6 @@ import styles from './server-error-page.module.scss';
 
 type ServerErrorPageProps = {} & React.HTMLAttributes<HTMLDivElement>;
 
-/**
- * A component that shows a server error page
- */
 export function ServerErrorPage({ ...rest }: ServerErrorPageProps) {
   return (
     <ErrorPage {...rest} code={500} title="Internal server error">
