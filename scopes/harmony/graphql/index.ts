be508b817e8d5fd5a799504812938bf32d799c45
Guysaar <guy@bit.dev>
2020-12-17 16:31:40 +0200

    try to fix build

--- scopes/harmony/graphql/index.ts
+++ scopes/harmony/graphql/index.ts
@@ -4,7 +4,6 @@ export type { SchemaSlot } from './graphql.main.runtime';
 export type { Schema } from './schema';
 export type { GraphqlMain } from './graphql.main.runtime';
 export type { GraphqlUI } from './graphql.ui.runtime';
-export { Verb } from './graphql.main.runtime';
 export { GraphQLProvider } from './graphql-provider';
 export { GraphQLServer } from './graphql-server';
 export { GraphqlAspect };
