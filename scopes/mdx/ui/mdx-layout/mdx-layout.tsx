ab4d48442db69f90679416fc880e0572085ed974
oded <oded@bit.dev>
2020-12-20 21:44:20 +0200

    wip - mdx components

--- scopes/mdx/ui/mdx-layout/mdx-layout.tsx
+++ scopes/mdx/ui/mdx-layout/mdx-layout.tsx
@@ -1,16 +1,43 @@
-import React, { ReactNode } from 'react';
-import { MDXProvider } from '@mdx-js/react';
+import React, { ReactNode, createContext, useContext } from 'react';
+import { MDXProvider, mdx } from '@mdx-js/react';
+// import { ComponentID } from '@teambit/component-id';
+// import { ComponentContext } from '@teambit/component';
 import type { Sizes } from '@teambit/documenter.ui.heading';
-import { LinkedHeading } from '@teambit/documenter.ui.linked-heading';
+// import { LinkedHeading } from '@teambit/documenter.ui.linked-heading';
 import { Paragraph } from '@teambit/documenter.ui.paragraph';
-import { CodeSnippet } from '@teambit/documenter.ui.code-snippet';
-import { ExternalLink } from '@teambit/documenter.routing.external-link';
+// import { CodeSnippet } from '@teambit/documenter.ui.code-snippet';
 import { HighlightedText } from '@teambit/documenter.ui.highlighted-text';
+// import { ExternalLink } from '@teambit/documenter.routing.external-link';
+
+import { Separator } from '@teambit/documenter.ui.separator';
+// import { Image } from '@teambit/evangelist.elements.image';
+// import { List } from '@teambit/documenter.ui.list';
+// import { Playground } from '@teambit/documenter.code.react-playground';
+
+import { Bold } from '../../../../../react-new-project/components/bold';
+import { Italic } from '../../../../../react-new-project/components/italic';
+import { Sup } from '../../../../../react-new-project/components/sup';
+import { Table } from '../../../../../react-new-project/components/base-table';
+// import { Pre } from '../../../../../react-new-project/components/pre';
+import { Tr } from '../../../../../react-new-project/components/tr';
+import { Td } from '../../../../../react-new-project/components/td';
+import { Ul } from '../../../../../react-new-project/components/ul';
+import { Ol } from '../../../../../react-new-project/components/ol';
+import { Image } from '../../../../../react-new-project/components/image';
+import { LinkedHeading } from '../../../../../react-new-project/components/linked-heading';
+import { BlockQuote } from '../../../../../react-new-project/components/block-quote';
+import { ExternalLink } from '../../../../../react-new-project/components/external-link';
+import { CodeSnippet } from '../../../../../react-new-project/components/code-snippet';
+import { Playground } from '../../../../../react-new-project/components/code/react-playground';
+
+import styles from './mdx.module.scss';
 
 function createHeading(size: Sizes) {
   return function Heading({ children }: { children: ReactNode }) {
+    // const component = useContext(ComponentContext);
+    // console.log("component", component)
     return (
-      <LinkedHeading size={size} link="">
+      <LinkedHeading className={styles.mdxLinkedHeading} size={size} link="">
         {children}
       </LinkedHeading>
     );
@@ -18,20 +45,56 @@ function createHeading(size: Sizes) {
 }
 
 function HighlightedTextSpan({ children }: { children: ReactNode }) {
-  return <HighlightedText element="span">{children}</HighlightedText>;
+  return (
+    <HighlightedText element="span" size="xxs">
+      {children}
+    </HighlightedText>
+  );
+}
+
+function Snippet({ children, live }: { live: string; children: string }) {
+  if (live) {
+    return <Playground code={children} scope={{ mdx }} />;
+  }
+  return <CodeSnippet>{children}</CodeSnippet>;
+}
+
+function Link(props) {
+  return <ExternalLink {...props} className={styles.link} />;
+}
+
+function P({ children }) {
+  return (
+    <Paragraph size="xs" className={styles.mdxParagraph}>
+      {children}
+    </Paragraph>
+  );
 }
 
 const defaultMdxComponents = {
   h1: createHeading('lg'),
-  h2: createHeading('lg'),
-  h3: createHeading('md'),
-  h4: createHeading('sm'),
-  h5: createHeading('xs'),
+  h2: createHeading('md'),
+  h3: createHeading('sm'),
+  h4: createHeading('xs'),
+  h5: createHeading('xxs'),
   h6: createHeading('xxs'),
-  p: Paragraph,
-  code: CodeSnippet,
-  a: ExternalLink,
+  p: P,
+  code: Snippet,
+  a: Link,
   inlineCode: HighlightedTextSpan,
+  ol: Ol,
+  ul: Ul,
+  hr: Separator,
+  thematicBreak: Separator,
+  img: Image,
+  strong: Bold,
+  em: Italic,
+  // pre: Snippet, // TODO - find a way to add this. it collides with code tag and overrides the pre tag in the code snippet component.
+  sup: Sup,
+  table: Table,
+  tr: Tr,
+  td: Td,
+  blockquote: BlockQuote,
 };
 
 type ComponentType =
