6db2fee0fcab723a249157e198dc6143ad7795ef
Benjamin Gilbert <benjamingilbert@Benjamins-MacBook-Pro.local>
2020-12-24 09:52:37 +0200

    remove compositions css module

--- scopes/design/ui/styles/ellipsis/ellipsis.compositions.tsx
+++ scopes/design/ui/styles/ellipsis/ellipsis.compositions.tsx
@@ -1,14 +1,13 @@
 import React from 'react';
 import { Ellipsis } from './index';
-import styles from './ellipsis.compositions.module.scss';
 
 const longName = "This is a really long name so you'll see the ellipsis";
 const shortName = 'Short';
 
 export const LongString = () => {
-  return <Ellipsis className={styles['div-width']}>{longName}</Ellipsis>;
+  return <Ellipsis style={{ width: 100 }}>{longName}</Ellipsis>;
 };
 
 export const ShortString = () => {
-  return <Ellipsis className={styles['div-width']}>{shortName}</Ellipsis>;
+  return <Ellipsis style={{ width: 100 }}>{shortName}</Ellipsis>;
 };
