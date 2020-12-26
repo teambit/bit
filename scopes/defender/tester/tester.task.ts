ff85f42f27d53b3a1f7383b9f5f76522a14f2cfa
Guysaar <guy@bit.dev>
2020-12-26 02:55:35 +0200

    return error on fail test

--- scopes/defender/tester/tester.task.ts
+++ scopes/defender/tester/tester.task.ts
@@ -56,12 +56,20 @@ export class TesterTask implements BuildTask {
     return {
       artifacts: [], // @ts-ignore
       componentsResults: testsResults.components.map((componentTests) => {
+        const errors = componentTests.results?.testFiles.reduce((errors: string[], file) => {
+          if (file?.error?.failureMessage) {
+            errors.push(file.error.failureMessage);
+          }
+          file.tests.forEach((test) => {
+            if (test.error) errors.push(test.error);
+          });
+
+          return errors;
+        }, []);
         return {
           component: context.capsuleNetwork.graphCapsules.getCapsule(componentTests.componentId)?.component,
           metadata: { tests: componentTests.results },
-          errors: componentTests.results?.testFiles
-            .map((testFile) => testFile.error?.failureMessage)
-            .filter((item) => item),
+          errors,
         };
       }),
     };
