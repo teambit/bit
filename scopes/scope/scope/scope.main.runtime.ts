5a771add98e54256433b138a966e093dc043fe83
David First <davidnet@gmail.com>
2020-12-29 11:49:29 -0500

    pass auth-data to remote events, add onPostExport and onPostObjectsPersist slots (#3569)

--- scopes/scope/scope/scope.main.runtime.ts
+++ scopes/scope/scope/scope.main.runtime.ts
@@ -28,8 +28,8 @@ import type { UiMain } from '@teambit/ui';
 import { UIAspect } from '@teambit/ui';
 import { RequireableComponent } from '@teambit/modules.requireable-component';
 import { BitId, BitIds as ComponentsIds } from 'bit-bin/dist/bit-id';
-import { ModelComponent, Version } from 'bit-bin/dist/scope/models';
-import { Ref } from 'bit-bin/dist/scope/objects';
+import { ModelComponent, Version, Lane } from 'bit-bin/dist/scope/models';
+import { Ref, Repository } from 'bit-bin/dist/scope/objects';
 import LegacyScope, { LegacyOnTagResult, OnTagFunc, OnTagOpts } from 'bit-bin/dist/scope/scope';
 import { ComponentLog } from 'bit-bin/dist/scope/models/model-component';
 import { loadScopeIfExist } from 'bit-bin/dist/scope/scope-loader';
@@ -39,6 +39,8 @@ import BluebirdPromise from 'bluebird';
 import { ExportPersist, PostSign } from 'bit-bin/dist/scope/actions';
 import { getScopeRemotes } from 'bit-bin/dist/scope/scope-remotes';
 import { Remotes } from 'bit-bin/dist/remotes';
+import { Scope } from 'bit-bin/dist/scope';
+import { Http, DEFAULT_AUTH_TYPE, AuthData } from 'bit-bin/dist/scope/network/http/http';
 import { buildOneGraphForComponentsUsingScope } from 'bit-bin/dist/scope/graph/components-graph';
 import { compact, slice, uniqBy } from 'lodash';
 import { SemVer } from 'semver';
@@ -54,9 +56,17 @@ type TagRegistry = SlotRegistry<OnTag>;
 export type OnTagResults = { aspectListMap: ComponentMap<AspectList>; pipeResults: TaskResultsList[] };
 export type OnTag = (components: Component[], options?: OnTagOpts) => Promise<OnTagResults>;
 
-export type OnPostPut = (ids: ComponentID[]) => void;
+type RemoteEventMetadata = { auth?: AuthData; clientBitVersion?: string };
+type RemoteEvent<Data> = (data: Data, metadata: RemoteEventMetadata, errors?: Array<string | Error>) => Promise<void>;
+type OnPostPutData = { ids: ComponentID[]; lanes: Lane[] };
+
+type OnPostPut = RemoteEvent<OnPostPutData>;
+type OnPostExport = RemoteEvent<OnPostPutData>;
+type OnPostObjectsPersist = RemoteEvent<undefined>;
 
 export type OnPostPutSlot = SlotRegistry<OnPostPut>;
+export type OnPostExportSlot = SlotRegistry<OnPostExport>;
+export type OnPostObjectsPersistSlot = SlotRegistry<OnPostObjectsPersist>;
 
 export type ScopeConfig = {
   description: string;
@@ -84,10 +94,11 @@ export class ScopeMain implements ComponentFactory {
      */
     private tagRegistry: TagRegistry,
 
-    /**
-     * slot registry for subscribing to post-export
-     */
     private postPutSlot: OnPostPutSlot,
+
+    private postExportSlot: OnPostExportSlot,
+
+    private postObjectsPersist: OnPostObjectsPersistSlot,
 
     private isolator: IsolatorMain,
 
@@ -161,6 +172,19 @@ export class ScopeMain implements ComponentFactory {
    */
   onPostPut(postPutFn: OnPostPut) {
     this.postPutSlot.register(postPutFn);
+    return this;
+  }
+
+  /**
+   * register to the post-export slot.
+   */
+  registerOnPostExport(postExportFn: OnPostExport) {
+    this.postExportSlot.register(postExportFn);
+    return this;
+  }
+
+  registerOnPostObjectsPersist(postObjectsPersistFn: OnPostObjectsPersist) {
+    this.postObjectsPersist.register(postObjectsPersistFn);
     return this;
   }
 
@@ -541,7 +565,12 @@ export class ScopeMain implements ComponentFactory {
   /**
    * declare the slots of scope extension.
    */
-  static slots = [Slot.withType<OnTag>(), Slot.withType<OnPostPut>()];
+  static slots = [
+    Slot.withType<OnTag>(),
+    Slot.withType<OnPostPut>(),
+    Slot.withType<OnPostExport>(),
+    Slot.withType<OnPostObjectsPersist>(),
+  ];
   static runtime = MainRuntime;
 
   static dependencies = [
@@ -567,7 +596,12 @@ export class ScopeMain implements ComponentFactory {
       LoggerMain
     ],
     config: ScopeConfig,
-    [tagSlot, postPutSlot]: [TagRegistry, OnPostPutSlot],
+    [tagSlot, postPutSlot, postExportSlot, postObjectsPersistSlot]: [
+      TagRegistry,
+      OnPostPutSlot,
+      OnPostExportSlot,
+      OnPostObjectsPersistSlot
+    ],
     harmony: Harmony
   ) {
     cli.register(new ExportCmd());
@@ -584,6 +618,8 @@ export class ScopeMain implements ComponentFactory {
       componentExt,
       tagSlot,
       postPutSlot,
+      postExportSlot,
+      postObjectsPersistSlot,
       isolator,
       aspectLoader,
       config,
@@ -594,16 +630,49 @@ export class ScopeMain implements ComponentFactory {
       await scope.loadAspects(aspectLoader.getNotLoadedConfiguredExtensions());
     });
 
-    const onPutHook = async (ids: string[]): Promise<void> => {
-      logger.debug(`ExportPersist.onPutHook, started. (${ids.length} components)`);
+    const onPutHook = async (ids: string[], lanes: Lane[], authData?: AuthData): Promise<void> => {
+      logger.debug(`onPutHook, started. (${ids.length} components)`);
       const componentIds = await scope.resolveMultipleComponentIds(ids);
       const fns = postPutSlot.values();
-      await Promise.all(fns.map(async (fn) => fn(componentIds)));
-      logger.debug(`ExportPersist.onPutHook, completed. (${ids.length} components)`);
+      const data = {
+        ids: componentIds,
+        lanes,
+      };
+      const metadata = { auth: authData };
+      await Promise.all(fns.map(async (fn) => fn(data, metadata)));
+      logger.debug(`onPutHook, completed. (${ids.length} components)`);
+    };
+
+    const getAuthData = (): AuthData | undefined => {
+      const token = Http.getToken();
+      return token ? { type: DEFAULT_AUTH_TYPE, credentials: token } : undefined;
+    };
+
+    const onPostExportHook = async (ids: BitId[], lanes: Lane[]): Promise<void> => {
+      logger.debug(`onPostExportHook, started. (${ids.length} components)`);
+      const componentIds = await scope.resolveMultipleComponentIds(ids);
+      const fns = postExportSlot.values();
+      const data = {
+        ids: componentIds,
+        lanes,
+      };
+      const metadata = { auth: getAuthData() };
+      await Promise.all(fns.map(async (fn) => fn(data, metadata)));
+      logger.debug(`onPostExportHook, completed. (${ids.length} components)`);
+    };
+
+    const onPostObjectsPersistHook = async (): Promise<void> => {
+      logger.debug(`onPostObjectsPersistHook, started`);
+      const fns = postObjectsPersistSlot.values();
+      const metadata = { auth: getAuthData() };
+      await Promise.all(fns.map(async (fn) => fn(undefined, metadata)));
+      logger.debug(`onPostObjectsPersistHook, completed`);
     };
 
     ExportPersist.onPutHook = onPutHook;
     PostSign.onPutHook = onPutHook;
+    Scope.onPostExport = onPostExportHook;
+    Repository.onPostObjectsPersist = onPostObjectsPersistHook;
 
     express.register([
       new PutRoute(scope, postPutSlot),
