825fd323df6254a13408ba75c849f34e422a199e
Gilad Shoham <shoham.gilad@gmail.com>
2020-12-30 12:01:02 +0200

    linting

--- scopes/component/ui/version-block/version-block.tsx
+++ scopes/component/ui/version-block/version-block.tsx
@@ -1,4 +1,4 @@
-import { Author, Snap } from '@teambit/component';
+import { Author } from '@teambit/component';
 import { H3 } from '@teambit/documenter.ui.heading';
 import { Contributors } from '@teambit/ui.contributors';
 import { NavLink } from '@teambit/ui.react-router.nav-link';
