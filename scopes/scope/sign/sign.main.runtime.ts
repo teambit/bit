f8a0d02266f1beb1c460936b1c32fb2c58396646
David First <davidnet@gmail.com>
2021-01-01 13:39:52 -0500

    bit-sign supports multiple scopes

--- scopes/scope/sign/sign.main.runtime.ts
+++ scopes/scope/sign/sign.main.runtime.ts
@@ -12,18 +12,35 @@ import ConsumerComponent from 'bit-bin/dist/consumer/component';
 import { BuildStatus } from 'bit-bin/dist/constants';
 import { getScopeRemotes } from 'bit-bin/dist/scope/scope-remotes';
 import { PostSign } from 'bit-bin/dist/scope/actions';
+import { ObjectList } from 'bit-bin/dist/scope/objects/object-list';
 import { Remotes } from 'bit-bin/dist/remotes';
 import { BitIds } from 'bit-bin/dist/bit-id';
+import loader from 'bit-bin/dist/cli/loader';
 import { SignCmd } from './sign.cmd';
 import { SignAspect } from './sign.aspect';
 
-export type SignResult = { components: Component[]; publishedPackages: string[]; error: string | null };
+export type SignResult = {
+  components: Component[];
+  publishedPackages: string[];
+  error: string | null;
+};
 
 export class SignMain {
   constructor(private scope: ScopeMain, private logger: Logger, private builder: BuilderMain) {}
 
-  async sign(ids: ComponentID[]): Promise<SignResult> {
-    const components = await this.scope.getMany(ids);
+  async sign(ids: ComponentID[], isMultiple?: boolean): Promise<SignResult | null> {
+    if (isMultiple) await this.scope.import(ids);
+    const { componentsToSkip, componentsToSign } = await this.getComponentIdsToSign(ids);
+    if (componentsToSkip.length) {
+      // eslint-disable-next-line no-console
+      console.log(`the following component(s) were already signed successfully:
+${componentsToSkip.map((c) => c.toString()).join('\n')}\n`);
+    }
+    if (!componentsToSign.length) {
+      return null;
+    }
+
+    const components = await this.scope.getMany(componentsToSign);
     const { builderDataMap, pipeResults } = await this.builder.tagListener(
       components,
       { throwOnError: false },
@@ -35,8 +52,13 @@ export class SignMain {
     const publishedPackages = getPublishedPackages(legacyComponents);
     const pipeWithError = pipeResults.find((pipe) => pipe.hasErrors());
     const buildStatus = pipeWithError ? BuildStatus.Failed : BuildStatus.Succeed;
-    await this.saveExtensionsDataIntoScope(legacyComponents, buildStatus);
+    if (isMultiple) {
+      await this.exportExtensionsDataIntoScopes(legacyComponents, buildStatus);
+    } else {
+      await this.saveExtensionsDataIntoScope(legacyComponents, buildStatus);
+    }
     await this.clearScopesCaches(legacyComponents);
+
     return {
       components,
       publishedPackages,
@@ -62,6 +84,49 @@ export class SignMain {
       await this.scope.legacyScope.sources.enrichSource(component);
     });
     await this.scope.legacyScope.objects.persist();
+  }
+
+  private async exportExtensionsDataIntoScopes(components: ConsumerComponent[], buildStatus: BuildStatus) {
+    const scopeRemotes: Remotes = await getScopeRemotes(this.scope.legacyScope);
+    const objectListPerScope: { [scopeName: string]: ObjectList } = {};
+    await mapSeries(components, async (component) => {
+      component.buildStatus = buildStatus;
+      const objects = await this.scope.legacyScope.sources.getObjectsToEnrichSource(component);
+      const scopeName = component.scope as string;
+      const objectList = await ObjectList.fromBitObjects(objects);
+      if (objectListPerScope[scopeName]) {
+        objectListPerScope[scopeName].mergeObjectList(objectList);
+      } else {
+        objectListPerScope[scopeName] = objectList;
+      }
+    });
+    await mapSeries(Object.keys(objectListPerScope), async (scopeName) => {
+      const remote = await scopeRemotes.resolve(scopeName, this.scope.legacyScope);
+      const objectList = objectListPerScope[scopeName];
+      loader.start(`transferring ${objectList.count()} objects to the remote "${remote.name}"...`);
+      await remote.pushMany(objectList, { persist: true });
+    });
+  }
+
+  private async getComponentIdsToSign(
+    ids: ComponentID[]
+  ): Promise<{
+    componentsToSkip: ComponentID[];
+    componentsToSign: ComponentID[];
+  }> {
+    // using `loadComponents` instead of `getMany` to make sure component aspects are loaded.
+    this.logger.setStatusLine(`loading ${ids.length} components and their extensions...`);
+    const components = await this.scope.loadMany(ids);
+    const componentsToSign: ComponentID[] = [];
+    const componentsToSkip: ComponentID[] = [];
+    components.forEach((component) => {
+      if (component.state._consumer.buildStatus === BuildStatus.Succeed) {
+        componentsToSkip.push(component.id);
+      } else {
+        componentsToSign.push(component.id);
+      }
+    });
+    return { componentsToSkip, componentsToSign };
   }
 
   static runtime = MainRuntime;
