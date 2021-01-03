f0bd00bbe5c0859293c62f8fc9c815fa2d16ca61
Uri Kutner <urikutner@gmail.com>
2021-01-03 15:39:31 +0200

    fix ssr hooks types

--- scopes/ui-foundation/ui/ui.ui.runtime.tsx
+++ scopes/ui-foundation/ui/ui.ui.runtime.tsx
@@ -18,47 +18,55 @@ import { Html, MountPoint, Assets } from './ssr/html';
 import type { SsrContent } from './ssr/ssr-content';
 import type { BrowserData } from './ssr/request-browser';
 
-export type RenderLifecycle<T = any, Y = any> = {
+export type RenderLifecycle<RenderCtx = any, Serialized = any> = {
   /**
    * Initialize a context state for this specific rendering.
    * Context state will only be available to the current Aspect, in the other hooks, as well as a prop to the react context component.
    */
-  serverInit?: (browser: BrowserData | undefined) => T | void | undefined | Promise<T | void | undefined>;
+  serverInit?: (
+    browser: BrowserData | undefined
+  ) => RenderCtx | void | undefined | Promise<RenderCtx | void | undefined>;
   /**
    * Executes before running ReactDOM.renderToString(). Return value will replace the existing context state.
    */
-  onBeforeRender?: (ctx: T, app: ReactNode) => T | void | undefined | Promise<T | void | undefined>;
+  onBeforeRender?: (
+    ctx: RenderCtx,
+    app: ReactNode
+  ) => RenderCtx | void | undefined | Promise<RenderCtx | void | undefined>;
   /**
    * Produce html assets. Runs after the body is rendered, and before rendering the final html.
    * @returns
    * json: will be rendered to the dom as a `<script type="json"/>`.
    * More assets will be available in the future.
    */
-  serialize?: (ctx: T, app: ReactNode) => { json: string } | Promise<{ json: string }> | undefined;
+  serialize?: (ctx: RenderCtx, app: ReactNode) => { json: string } | Promise<{ json: string }> | undefined;
   /**
    * Converts serialized data from raw string back to structured data.
    * @example deserialize: (data) => { const parsed = JSON.parse(data); return { analytics: new AnalyticsService(parsed); } }
    */
-  deserialize?: (data?: string) => Y;
+  deserialize?: (data?: string) => Serialized;
   /**
    * Initialize the context state for client side rendering.
    * Context state will only be available to the current Aspect, in the other hooks, as well as a prop to the react context component.
    */
-  browserInit?: (deserializedData: Y) => T | void | undefined | Promise<T | void | undefined>;
+  browserInit?: (deserializedData: Serialized) => RenderCtx | void | undefined | Promise<RenderCtx | void | undefined>;
   /**
    * Executes before running ReactDOM.hydrate() (or .render() in case server side rendering is skipped). Receives the context produced by `deserialize()`
    */
-  onBeforeHydrate?: (context: T, app: ReactNode) => T | void | undefined | Promise<T | void | undefined>;
+  onBeforeHydrate?: (
+    context: RenderCtx,
+    app: ReactNode
+  ) => RenderCtx | void | undefined | Promise<RenderCtx | void | undefined>;
   /**
    * Executes after browser rendering is complete. Receives context from the previous steps.
    * @example onHydrate: (ref, { analytics }) => { analytics.reportPageView() }
    */
-  onHydrate?: (context: T, ref: HTMLElement | null) => void;
+  onHydrate?: (context: RenderCtx, ref: HTMLElement | null) => void;
 
   /**
    * Wraps dom with a context. Will receive render context, produced by `onBeforeRender()` (at server-side) or `deserialize()` (at the browser)
    */
-  reactContext?: ComponentType<ContextProps<T>>;
+  reactContext?: ComponentType<ContextProps<RenderCtx>>;
 };
 
 export type ContextProps<T = any> = { renderCtx?: T; children: ReactNode };
@@ -186,7 +194,7 @@ export class UiUI {
     return this.uiRootSlot.register(uiRoot);
   }
 
-  registerRenderHooks(hooks: RenderLifecycle) {
+  registerRenderHooks<T, Y>(hooks: RenderLifecycle<T, Y>) {
     return this.lifecycleSlot.register(hooks);
   }
 
