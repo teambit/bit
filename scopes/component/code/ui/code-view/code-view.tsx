cb3f0f6dc3876e8df33e5cb041587e77297880cb
oded <oded@bit.dev>
2021-01-04 14:50:10 +0200

    fixed code view performance

--- scopes/component/code/ui/code-view/code-view.tsx
+++ scopes/component/code/ui/code-view/code-view.tsx
@@ -1,6 +1,6 @@
 import { H1 } from '@teambit/documenter.ui.heading';
 import classNames from 'classnames';
-import React, { HTMLAttributes } from 'react';
+import React, { HTMLAttributes, useMemo } from 'react';
 import { CodeSnippet } from '@teambit/documenter.ui.code-snippet';
 
 import styles from './code-view.module.scss';
@@ -12,17 +12,20 @@ export type CodeViewProps = {
 } & HTMLAttributes<HTMLDivElement>;
 
 export function CodeView({ className, fileContent, currentFile, icon }: CodeViewProps) {
+  const title = useMemo(() => currentFile?.split('/').pop(), [currentFile]);
+  const lang = useMemo(() => currentFile?.split('.').pop(), [currentFile]);
+  if (!fileContent) return null; // is there a state where the is no file content? what should be presented then?
   return (
     <div className={classNames(styles.codeView, className)}>
       <H1 size="sm" className={styles.fileName}>
         {currentFile && <img className={styles.img} src={icon} />}
-        <span>{currentFile?.split('/').pop()}</span>
+        <span>{title}</span>
       </H1>
       <CodeSnippet
         className={styles.codeSnippetWrapper}
         frameClass={styles.codeSnippet}
         showLineNumbers
-        language={currentFile?.split('.').pop()}
+        language={lang}
       >
         {fileContent || ''}
       </CodeSnippet>
