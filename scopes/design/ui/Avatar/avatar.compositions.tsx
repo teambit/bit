3d58b1d83c046087d528c4a811f8d91d94bba953
benjgil <67269267+benjgil@users.noreply.github.com>
2021-01-05 15:46:56 +0200

    Adding docs and comps for teambit.design components (#3545)

---
+++ scopes/design/ui/Avatar/avatar.compositions.tsx
@@ -0,0 +1,58 @@
+import React from 'react';
+import { ThemeCompositions } from '@teambit/documenter.theme.theme-compositions';
+import { DefaultAvatar, OrgAvatar, UserAvatar } from './index';
+
+const accounts = {
+  defAccount: { name: 'defaultAccount', type: 'default', profileImage: 'https://static.bit.dev/harmony/support.svg' },
+  orgAccount: { name: 'defaultAccount', type: 'organization', profileImage: 'https://static.bit.dev/bit-logo.svg' },
+  userAccount: { name: 'defaultAccount', type: 'user', profileImage: 'https://static.bit.dev/harmony/github.svg' },
+  noPicOrgAccount: { name: 'defaultAccount', type: 'organization' },
+  noPicUserAccount: { name: 'defaultAccount', type: 'user' },
+};
+
+export const DefaultAvatarExample = () => (
+  <ThemeCompositions>
+    <DefaultAvatar size={32} account={accounts.defAccount} />
+  </ThemeCompositions>
+);
+
+export const OrganizationAvatarExample = () => (
+  <ThemeCompositions>
+    <OrgAvatar size={32} account={accounts.orgAccount} />
+  </ThemeCompositions>
+);
+
+export const UserAvatarExample = () => (
+  <ThemeCompositions>
+    <UserAvatar size={32} account={accounts.userAccount} />
+  </ThemeCompositions>
+);
+
+export const LargeAvatarExample = () => (
+  <ThemeCompositions>
+    <OrgAvatar size={100} account={accounts.orgAccount} />
+  </ThemeCompositions>
+);
+
+export const NoSetIconOrgAvatar = () => (
+  <ThemeCompositions>
+    <OrgAvatar size={32} account={accounts.noPicOrgAccount} />
+  </ThemeCompositions>
+);
+
+export const NoSetIconUserAvatar = () => (
+  <ThemeCompositions>
+    <UserAvatar size={32} account={accounts.noPicUserAccount} />
+  </ThemeCompositions>
+);
+
+const compositions = [
+  DefaultAvatarExample,
+  OrganizationAvatarExample,
+  UserAvatarExample,
+  LargeAvatarExample,
+  NoSetIconOrgAvatar,
+  NoSetIconUserAvatar,
+];
+// @ts-ignore
+compositions.map((comp) => (comp.canvas = { height: 120 }));
