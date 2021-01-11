c4238542274c227daf83c2af653a0e7bb4687b4a
oded <oded@bit.dev>
2021-01-11 20:25:48 +0200

    file tree - pass rest and improved composition

--- scopes/ui-foundation/tree/file-tree/file-tree.compositions.tsx
+++ scopes/ui-foundation/tree/file-tree/file-tree.compositions.tsx
@@ -1,38 +1,45 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { ThemeCompositions } from '@teambit/documenter.theme.theme-compositions';
 import { FolderTreeNode } from '@teambit/ui.tree.folder-tree-node';
 import { getFileIcon } from '@teambit/ui.utils.get-file-icon';
+import { TreeNode as Node } from '@teambit/ui.tree.tree-node';
 import { FileTree } from './file-tree';
 
 const currentFile = 'index.ts';
-const fileTree = ['folder/file.tsx', 'folder/index.ts'];
-
-function TreeNode(props: any) {
-  const children = props.node.children;
-  if (!children) {
-    return (
-      <div style={{ padding: '20px', display: 'flex' }}>
-        <img style={{ width: '20px', marginRight: '10px' }} src={getFileIcon(undefined, props.node.id)} />
-        <span>{props.node.id}</span>
-      </div>
-    );
-  }
-  return <FolderTreeNode {...props} />;
-}
+const fileTree = ['folder/file.tsx', 'folder/file.module.scss', 'folder/index.ts'];
 
 export const FileTreeEXample = () => {
+  const [active, setToActive] = useState('folder/index.ts');
+
+  function TreeNode(props: any) {
+    const children = props.node.children;
+    if (!children) {
+      return (
+        <Node
+          onClick={() => setToActive(props.node.id)}
+          isActive={props.node.id === active}
+          icon={getFileIcon(undefined, props.node.id) || ''}
+          node={{ id: props.node.id }}
+          depth={1}
+          href="#"
+        />
+      );
+    }
+    return <FolderTreeNode {...props} />;
+  }
   return (
     <ThemeCompositions>
-      <div
+      <FileTree
         style={{
           height: '100px',
           width: '200px',
           padding: '20px',
           display: 'flex',
         }}
-      >
-        <FileTree TreeNode={TreeNode} files={fileTree || ['']} selected={currentFile} />
-      </div>
+        TreeNode={TreeNode}
+        files={fileTree || ['']}
+        selected={currentFile}
+      />
     </ThemeCompositions>
   );
 };
