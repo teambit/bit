bdb21d3c31f991f44b4888980683550b3694b55e
Guysaar <guy@bit.dev>
2021-01-12 14:37:49 +0200

    add bit.dev aspect

---
+++ scopes/harmony/bit-dev/bit-dev.main.runtime.ts
@@ -0,0 +1,42 @@
+import { gql, GraphQLClient } from 'graphql-request';
+import { MainRuntime } from '@teambit/cli';
+import { ScopeMain, ScopeAspect } from '@teambit/scope';
+import { LoggerAspect, LoggerMain } from '@teambit/logger';
+import { BitDevAspect } from './bit-dev.aspect';
+
+export type Config = {
+  hub: string;
+};
+
+export class BitDev {
+  static runtime = MainRuntime;
+  static dependencies: any = [ScopeAspect, LoggerAspect];
+  static defaultConfig = {
+    hub: 'https://symphony.bit.dev',
+  };
+
+  getAuthHeader(authData?: { type?: string; credentials?: string }) {
+    return {
+      Authorization: `${authData?.type} ${authData?.credentials}`,
+    };
+  }
+
+  static async provider([scope, loggerMain]: [ScopeMain, LoggerMain], config: Config) {
+    const logger = loggerMain.createLogger(ScopeAspect.id);
+    const onExportMain = new BitDev();
+    scope.registerOnPostExport(async ({ ids }, { auth }) => {
+      logger.info(`export completed, reporting to: ${config.hub}`);
+      const graphClient = new GraphQLClient(`${config.hub}/graphql`, { headers: onExportMain.getAuthHeader(auth) });
+      const EXPORT_COMPLETED = gql`
+        mutation export_completed($bitIds: [String!]!) {
+          export_completed(bitIds: $bitIds)
+        }
+      `;
+      await graphClient.request(EXPORT_COMPLETED, { bitIds: ids });
+    });
+
+    return onExportMain;
+  }
+}
+
+BitDevAspect.addRuntime(BitDev);
