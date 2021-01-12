9ccd5629a82132439b636d1a18a6110a82aaf401
Guysaar <guy@bit.dev>
2021-01-12 16:08:46 +0200

    remove on export

--- scopes/harmony/bit-dev/on-export/index.ts
+++
@@ -1,5 +0,0 @@
-import { OnExportAspect } from './on-export.aspect';
-
-export type {} from './on-export.main.runtime';
-export { OnExportAspect };
-export default OnExportAspect;
--- scopes/harmony/bit-dev/on-export/on-export.aspect.ts
+++
@@ -1,5 +0,0 @@
-import { Aspect } from '@teambit/harmony';
-
-export const OnExportAspect = Aspect.create({
-  id: 'teambit.bit-dev/on-export',
-});
