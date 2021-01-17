1b16ab17062790aca9f6ede8a7357b20efbca992
oded <oded@bit.dev>
2021-01-17 18:48:49 +0200

    iport action - pass versions for component only when not latest

--- scopes/component/component/ui/menu/menu.tsx
+++ scopes/component/component/ui/menu/menu.tsx
@@ -1,4 +1,3 @@
-// import { NavigationSlot } from '@teambit/react-router';
 import { MainDropdown, MenuItemSlot } from '@teambit/ui.main-dropdown';
 import { ImportAction } from '@teambit/documenter.ui.import-action';
 import { VersionDropdown } from '@teambit/ui.version-dropdown';
@@ -8,7 +7,6 @@ import { flatten, groupBy } from 'lodash';
 import classnames from 'classnames';
 import React, { useMemo } from 'react';
 
-// import { TopBarWidgetLink } from '../top-bar-widget-link';
 import { useComponent } from '../use-component';
 import { MenuNav } from './menu-nav';
 import styles from './menu.module.scss';
@@ -17,11 +15,17 @@ import { OrderedNavigationSlot } from './nav-plugin';
 export type MenuProps = {
   className?: string;
   /**
-   * slot for top bar menu items
+   * slot for top bar menu nav items
    */
   navigationSlot: OrderedNavigationSlot;
+  /**
+   * right side menu item slot
+   */
   widgetSlot: OrderedNavigationSlot;
   host: string;
+  /**
+   * main dropdown item slot
+   */
   menuItemSlot: MenuItemSlot;
 };
 
@@ -32,13 +36,20 @@ export function Menu({ navigationSlot, widgetSlot, className, host, menuItemSlot
   const { component } = useComponent(host);
   const mainMenuItems = useMemo(() => groupBy(flatten(menuItemSlot.values()), 'category'), [menuItemSlot]);
 
+  const versionList =
+    useMemo(
+      () =>
+        component?.tags
+          ?.toArray()
+          .map((tag) => tag?.version?.version)
+          .reverse(),
+      [component?.tags]
+    ) || [];
+
+  const isLatestVersion = useMemo(() => component?.version === versionList[0], [component?.version]);
+  const packageVersion = useMemo(() => (isLatestVersion ? '' : `@${component?.version}`), [component?.version]);
+
   if (!component) return <FullLoader />;
-
-  const versionList = component.tags
-    ?.toArray()
-    .map((tag) => tag?.version?.version)
-    .reverse();
-
   return (
     <div className={classnames(styles.topBar, className)}>
       <div className={styles.leftSide}>
@@ -51,16 +62,13 @@ export function Menu({ navigationSlot, widgetSlot, className, host, menuItemSlot
         {versionList.length > 0 && (
           <ImportAction
             componentName={component.id.name}
-            bitLink={component.id.toString()}
+            bitLink={component.id.toString({ ignoreVersion: isLatestVersion })}
             Link={(props) => <Link {...props} />}
-            packageLink={component.packageName}
+            packageLink={`${component.packageName}${packageVersion}`}
             registryName={component.packageName.split('/')[0]}
           />
         )}
         <VersionDropdown versions={versionList} currentVersion={component.version} />
-        {/* <span className={styles.widget}>
-          <Icon className={classnames(styles.icon)} of="dependency" />
-        </span> */}
         <MainDropdown menuItems={mainMenuItems} />
       </div>
     </div>
