11af4aee75a45661cb898b837959894dcd725908
Gilad Shoham <shoham.gilad@gmail.com>
2021-01-24 14:42:19 +0200

    support multi providers in the preview

--- scopes/react/react/react.preview.runtime.ts
+++ scopes/react/react/react.preview.runtime.ts
@@ -1,21 +1,22 @@
 import { ComponentType } from 'react';
+import flatten from 'lodash.flatten';
 import { SlotRegistry, Slot } from '@teambit/harmony';
 import PreviewAspect, { PreviewPreview, PreviewRuntime } from '@teambit/preview';
 import { ReactAspect } from './react.aspect';
 
 export type Provider = ComponentType<{}>;
-export type ProviderSlot = SlotRegistry<Provider>;
+export type ProviderSlot = SlotRegistry<Provider[]>;
 
 export class ReactPreview {
   constructor(private preview: PreviewPreview, private providerSlot: ProviderSlot) {}
 
-  registerProvider(provider: Provider) {
+  registerProvider(provider: Provider[]) {
     this.providerSlot.register(provider);
   }
 
   getRenderingContext() {
     return {
-      providers: this.providerSlot.values(),
+      providers: flatten(this.providerSlot.values()),
     };
   }
 
