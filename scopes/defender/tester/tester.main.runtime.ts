3c65d61f363e521649ed187d4d69bdc5ad7dcd14
Josh Kuttler <yoshuakuttler@gmail.com>
2021-02-01 17:15:49 +0200

    update regex pattern

--- scopes/defender/tester/tester.main.runtime.ts
+++ scopes/defender/tester/tester.main.runtime.ts
@@ -169,7 +169,7 @@ export class TesterMain {
     /**
      * default test regex for which files tester to apply on.
      */
-    patterns: ['**/*.spec.*', '**/*.test.*'],
+    patterns: ['**/*.spec.+(js|ts|jsx|tsx)', '**/*.test.*'],
 
     /**
      * determine whether to watch on start.
