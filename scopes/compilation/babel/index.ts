ecc50eb0d7f496776a76cd2fd5c218d0f08003f8
Benjamin Gilbert <benjamingilbert@Benjamins-MacBook-Pro.local>
2021-02-01 21:58:39 +0200

    expose options  types

--- scopes/compilation/babel/index.ts
+++ scopes/compilation/babel/index.ts
@@ -1,3 +1,4 @@
 export { BabelCompiler } from './babel.compiler';
+export { BabelCompilerOptions } from './compiler-options';
 export type { BabelMain } from './babel.main.runtime';
 export { BabelAspect } from './babel.aspect';
