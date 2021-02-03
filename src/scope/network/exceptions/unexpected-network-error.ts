4a63489a6e6c78dfcf6ccd2038e4c3aef73ca901
David First <davidnet@gmail.com>
2021-02-03 15:08:36 -0500

    fix an error "getExternalMany expects to get external ids only" (#3734)

--- src/scope/network/exceptions/unexpected-network-error.ts
+++ src/scope/network/exceptions/unexpected-network-error.ts
@@ -8,7 +8,7 @@ export default class UnexpectedNetworkError extends AbstractError {
     super();
     this.message = message;
     // @todo: delete this hack once a new version is deployed.
-    if (message.includes("warning: '_action' is not a valid command")) {
+    if (message && message.includes("warning: '_action' is not a valid command")) {
       this.message =
         'the server version is older than yours, it does not support the new four-steps export process, please update the server';
     }
