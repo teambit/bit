4edc4b364b091378dc077f53bab910d65052028c
Benjamin Gilbert <benjamingilbert@Benjamins-MacBook-Pro.local>
2021-02-04 20:49:41 +0200

    attempt at extender class

--- scopes/react/react/react.extender.ts
+++ scopes/react/react/react.extender.ts
@@ -1,24 +1,35 @@
 import { TsConfigSourceFile } from 'typescript';
-import { EnvsMain, EnvsAspect, EnvTransformer } from '@teambit/envs';
+import { EnvsMain, EnvsAspect } from '@teambit/envs';
 import { ReactAspect, ReactMain } from '@teambit/react';
-import { TsConfigs } from './typescript/interfaces';
+import { ReactEnvState } from './state';
+import { useTypescript } from './extenders';
 
-const jest = require('jest');
-const tsconfig = require('./typescript/tsconfig.json') as TsConfigSourceFile;
+/**
+ * Example usage:
+ * CompanyNameReact extends HarmonyReactExtender
+ * Then override the getUsages function, to return a list of all the
+ * UseXyz overrides you wish to apply to your environment
+ */
+
+// const jest = require('jest');
+// const tsconfig = require('./typescript/tsconfig.json') as TsConfigSourceFile;
 // const webpackConfig = require('./webpack/webpack.config');
 
 export class HarmonyReactExtender {
   constructor(private react: ReactMain) {}
 
-  protected static getUsages = (react: ReactMain): EnvTransformer[] => {
-    return [...react.useTypescript({ workspaceConfig: tsconfig }, {})];
+  protected static getUsages = (): Partial<ReactEnvState>[] => {
+    return [
+      // Example useXyz implementation
+      //useTypescript({ overrideExistingConfig: true,  tsconfig})
+    ];
   };
 
   static dependencies: any = [EnvsAspect, ReactAspect];
 
   static async provider([envs, react]: [EnvsMain, ReactMain]) {
-    const usages = this.getUsages(react);
-    const harmonyReactEnv = react.compose([...usages]);
+    const usages = this.getUsages();
+    const harmonyReactEnv = react.extend(...usages);
 
     envs.registerEnv(harmonyReactEnv);
     return new HarmonyReactExtender(react);
