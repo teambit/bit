39a330e01d714ba1ad3fcf875dc0ac1513e410e2
Uri Kutner <urikutner@gmail.com>
2021-02-07 16:20:23 +0200

    fix tests

--- scopes/component/ui/component-tooltip/component-tooltip.spec.tsx
+++ scopes/component/ui/component-tooltip/component-tooltip.spec.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 import { expect } from 'chai';
 import {
   ComponentTooltipWithNewStatus,
@@ -10,25 +10,45 @@ import {
 
 it('should render a tooltip with new component', () => {
   const { getByText } = render(<ComponentTooltipWithNewStatus />);
-  const textTooltip = getByText(/^New component$/);
+
+  const textStatus = getByText('N');
+  fireEvent.mouseEnter(textStatus);
+  const textTooltip = () => getByText(/^New component$/);
+
   expect(textTooltip).to.exist;
 });
+
 it('should render a tooltip with staged component', () => {
   const { getByText } = render(<ComponentTooltipWithStagedStatus />);
-  const textTooltip = getByText(/^Staged component$/);
+
+  const textStatus = getByText('S');
+  fireEvent.mouseEnter(textStatus);
+  const textTooltip = () => getByText(/^Staged component$/);
+
   expect(textTooltip).to.exist;
 });
+
 it('should render a tooltip with multiple status, modified dependencies and files', () => {
   const { getByText } = render(<ComponentTooltipWithModifiedFilesAndDependenciesStatus />);
+
+  const textStatus = getByText('M');
+  fireEvent.mouseEnter(textStatus);
   const textModifiedDependencies = getByText(/^Modified dependencies$/);
   const textModifiedFiles = getByText(/^Modified files$/);
+
   expect(textModifiedDependencies).to.exist;
   expect(textModifiedFiles).to.exist;
 });
+
 it('should render a tooltip with multiple status, modified files and 2 issues found', () => {
   const { getByText } = render(<ComponentTooltipWithModifiedFilesStatusAndIssues />);
+
+  const textStatus = getByText('M');
+  fireEvent.mouseEnter(textStatus);
+  textStatus.parentElement && fireEvent.mouseEnter(textStatus.parentElement);
   const textTooltip = getByText(/^Modified files$/);
   const textTooltipIssue = getByText(/^2 issues found$/);
+
   expect(textTooltip).to.exist;
   expect(textTooltipIssue).to.exist;
 });
