5458781cbabb559ebe2ea4058774d8798bc6848e
David First <davidnet@gmail.com>
2021-02-08 16:48:33 -0500

    remove "snap" prop from "tag" class

--- scopes/component/component/tag/tag.ts
+++ scopes/component/component/tag/tag.ts
@@ -1,14 +1,8 @@
 import { SemVer } from 'semver';
 
-import { Snap, SnapProps } from '../snap';
-
 export type TagProps = {
   hash: string;
   version: string;
-  /**
-   * @deprecated
-   */
-  snap: SnapProps;
 };
 
 /**
@@ -24,12 +18,7 @@ export class Tag {
     /**
      * sematic version of the snap.
      */
-    readonly version: SemVer,
-
-    /**
-     * @deprecated to get a snap object, use component-factory.getSnap(this.hash);
-     */
-    readonly snap: Snap
+    readonly version: SemVer
   ) {}
 
   /**
@@ -39,11 +28,10 @@ export class Tag {
     return {
       hash: this.hash,
       version: this.version.raw,
-      snap: this.snap.toObject(),
     };
   }
 
   static fromObject(tag: TagProps) {
-    return new Tag(tag.hash, new SemVer(tag.version), Snap.fromObject(tag.snap));
+    return new Tag(tag.hash, new SemVer(tag.version));
   }
 }
