a871d9a02a560bba666a07cca84466dde234f148
Guysaar <guy@bit.dev>
2021-02-10 02:30:41 +0200

    export Network  and PushOptions

--- scopes/scope/scope/index.ts
+++ scopes/scope/scope/index.ts
@@ -6,5 +6,6 @@ export type { ScopeModel } from './ui/scope-model';
 export { ScopeContext } from './ui/scope-context';
 export type { ScopeUI, ScopeBadgeSlot, ScopeOverview, ScopeOverviewSlot, OverviewLineSlot } from './scope.ui.runtime';
 export { ScopeAspect };
+export { Network, PushOptions } from 'bit-bin/dist/scope/network/network';
 export { ObjectList } from 'bit-bin/dist/scope/objects/object-list';
 export default ScopeAspect;
