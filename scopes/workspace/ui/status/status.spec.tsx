b5c885577c4aac292421218468f0fbd96d64f47e
Josh Kuttler <yoshuakuttler@gmail.com>
2021-02-15 13:25:55 +0200

    update spec of status

--- scopes/workspace/ui/status/status.spec.tsx
+++ scopes/workspace/ui/status/status.spec.tsx
@@ -1,13 +1,12 @@
 import React from 'react';
 import { render } from '@testing-library/react';
+import { expect } from 'chai';
 import { Status, JobStatus } from './status';
 
 describe('Button', () => {
   it('should render a test button', () => {
     const { getByText } = render(<Status status={JobStatus.pass}></Status>);
     const testButton = getByText(/pass/);
-    // @ts-ignore
-    // eslint-disable-next-line no-undef
-    expect(testButton).toBeInTheDocument();
+    expect(testButton).to.exist;
   });
 });
