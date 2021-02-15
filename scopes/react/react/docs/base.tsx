16587b1eeca7d5a0c66173ba0175330b70c27173
Ran Mizrahi <me@ranmizrahi.com>
2021-02-15 14:50:34 +0200

    initial commit

--- scopes/react/react/docs/base.tsx
+++ scopes/react/react/docs/base.tsx
@@ -1,23 +1,24 @@
 import 'reset-css';
-
 import React, { HTMLAttributes } from 'react';
 import { docsFile } from '@teambit/documenter.types.docs-file';
 import classNames from 'classnames';
 import { isFunction } from 'ramda-adjunct';
 import { MDXLayout } from '@teambit/ui.mdx-layout';
-
+import { RenderingContext } from '@teambit/preview';
+import { withProviders } from '../mount';
+import { ReactAspect } from '../react.aspect';
 import styles from './base.module.scss';
 import { ComponentOverview } from './component-overview';
 import { CompositionsSummary } from './compositions-summary/compositions-summary';
 import { ExamplesOverview } from './examples-overview';
 import { Properties } from './properties/properties';
-
 import { useFetchDocs } from './use-fetch-docs';
 
 export type DocsSectionProps = {
   docs?: docsFile;
   compositions: React.ComponentType[];
   componentId: string;
+  renderingContext: RenderingContext;
 } & HTMLAttributes<HTMLDivElement>;
 
 const defaultDocs = {
@@ -29,7 +30,7 @@ const defaultDocs = {
 /**
  * base template for react component documentation.
  */
-export function Base({ docs = defaultDocs, componentId, compositions, ...rest }: DocsSectionProps) {
+export function Base({ docs = defaultDocs, componentId, compositions, renderingContext, ...rest }: DocsSectionProps) {
   const { loading, error, data } = useFetchDocs(componentId);
 
   if (!data || loading) return null;
@@ -41,6 +42,8 @@ export function Base({ docs = defaultDocs, componentId, compositions, ...rest }:
   const { examples = [], labels = [], abstract = docsModel.abstract } = docs;
   const { displayName, version, packageName, description } = component;
   const Content: any = isFunction(docs.default) ? docs.default : () => null;
+  const reactContext = renderingContext.get(ReactAspect.id);
+  const Provider = withProviders(reactContext?.providers);
 
   return (
     <div className={classNames(styles.docsMainBlock)} {...rest}>
@@ -52,21 +55,23 @@ export function Base({ docs = defaultDocs, componentId, compositions, ...rest }:
         packageName={packageName}
       />
 
-      {Content.isMDXComponent ? (
-        <MDXLayout>
-          <div className={styles.mdx}>
-            <Content />
-          </div>
-        </MDXLayout>
-      ) : (
-        <Content />
-      )}
+      <Provider>
+        {Content.isMDXComponent ? (
+          <MDXLayout>
+            <div className={styles.mdx}>
+              <Content />
+            </div>
+          </MDXLayout>
+        ) : (
+          <Content />
+        )}
 
-      <CompositionsSummary compositions={compositions} className={styles.compositionSection} />
+        <CompositionsSummary compositions={compositions} className={styles.compositionSection} />
 
-      <ExamplesOverview examples={Content.examples || examples} />
+        <ExamplesOverview examples={Content.examples || examples} />
 
-      <Properties properties={docsModel.properties} />
+        <Properties properties={docsModel.properties} />
+      </Provider>
     </div>
   );
 }
