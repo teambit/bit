95577dd4d429dcc709674ece27dba13df5d1b576
Josh Kuttler <35892475+JoshK2@users.noreply.github.com>
2021-02-24 17:00:09 +0200

    Create a new component for owner-avatar (#3852)
    - Create a new component for the owner avatar that use the UI avatar.
    
    - Remove unnecessary code from the avatar component that was used for the owner.

--- scopes/design/ui/Avatar/index.ts
+++ scopes/design/ui/Avatar/index.ts
@@ -1,4 +1,5 @@
 export * from './avatar';
 export { UserAvatar } from './user-avatar';
+export type { UserAvatarProps } from './user-avatar';
 export { OrgAvatar } from './org-avatar';
 export { DefaultAvatar } from './default-avatar';
---
+++ scopes/design/ui/owner-avatar/index.ts
@@ -0,0 +1,2 @@
+export { OwnerAvatar } from './owner-avatar';
+export type { OwnerAvatarProps } from './owner-avatar';
