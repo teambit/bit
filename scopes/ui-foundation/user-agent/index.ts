3db0422f0fcabec6e5bfe199f476fe1b09b4b65a
oded <oded@bit.dev>
2021-03-07 20:00:28 +0200

    user agent aspect

---
+++ scopes/ui-foundation/user-agent/index.ts
@@ -0,0 +1,3 @@
+export { UserAgentAspect, UserAgentAspect as default } from './user-agent.aspect';
+export type { UserAgentRenderCtx } from './user-agent.ui.runtime';
+export { UserAgentUI } from './user-agent.ui.runtime';
