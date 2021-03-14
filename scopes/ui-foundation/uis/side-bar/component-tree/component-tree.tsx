b5c17f7350d46290b38053110df00c988327e569
Josh Kuttler <yoshuakuttler@gmail.com>
2021-03-14 20:22:38 +0200

    remove text from component tree

--- scopes/ui-foundation/uis/side-bar/component-tree/component-tree.tsx
+++ scopes/ui-foundation/uis/side-bar/component-tree/component-tree.tsx
@@ -1,15 +1,12 @@
 import { ComponentModel } from '@teambit/component';
 import React, { useMemo } from 'react';
-import classNames from 'classnames';
 import { TreeContextProvider } from '@teambit/base-ui.graph.tree.tree-context';
 import { indentStyle } from '@teambit/base-ui.graph.tree.indent';
 import { inflateToTree, attachPayload } from '@teambit/base-ui.graph.tree.inflate-paths';
 import { TreeNodeContext, TreeNodeRenderer } from '@teambit/base-ui.graph.tree.recursive-tree';
 import { RootNode } from '@teambit/base-ui.graph.tree.root-node';
-import { textSize } from '@teambit/base-ui.text.text-sizes';
 import { PayloadType, ScopePayload } from './payload-type';
 import { DefaultTreeNodeRenderer } from './default-tree-node-renderer';
-import styles from './component-tree.module.scss';
 
 type ComponentTreeProps = {
   onSelect?: (id: string, event?: React.MouseEvent) => void;
@@ -38,11 +35,7 @@ export function ComponentTree({
     <div style={indentStyle(1)}>
       <TreeNodeContext.Provider value={TreeNode}>
         <TreeContextProvider onSelect={onSelect} selected={selected}>
-          {rootNode.children?.length === 0 ? (
-            <span className={classNames(textSize.xs, styles.emptyScope)}>Scope is empty</span>
-          ) : (
-            <RootNode node={rootNode} depth={1} />
-          )}
+          <RootNode node={rootNode} depth={1} />
         </TreeContextProvider>
       </TreeNodeContext.Provider>
     </div>
