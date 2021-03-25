f8d5ad6c362df3c9017c566a3da5a537a3103d69
David First <davidnet@gmail.com>
2021-03-25 11:15:12 -0400

    add the message prop to ComponentNotFound error to be shown correctly on grpahql, fixes #3993 (#3996)

--- src/scope/exceptions/component-not-found.ts
+++ src/scope/exceptions/component-not-found.ts
@@ -1,12 +1,16 @@
-import AbstractError from '../../error/abstract-error';
+import { BitError } from '@teambit/bit-error';
+import chalk from 'chalk';
 
-export default class ComponentNotFound extends AbstractError {
+export default class ComponentNotFound extends BitError {
   id: string;
   dependentId: string | null | undefined;
   code: number;
 
   constructor(id: string, dependentId?: string) {
-    super();
+    const baseMsg = dependentId
+      ? `error: the component dependency "${chalk.bold(id)}" required by "${chalk.bold(dependentId)}" was not found`
+      : `error: component "${chalk.bold(id)}" was not found`;
+    super(`${baseMsg}\nconsider running "bit dependents ${id}" to understand why this component was needed`);
     this.code = 127;
     this.id = id;
     this.dependentId = dependentId;
