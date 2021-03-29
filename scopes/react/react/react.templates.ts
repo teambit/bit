409c552b16dc170a3f0941014fb713c0ad30884c
Debbie O'Brien <debs-obrien@users.noreply.github.com>
2021-03-29 14:18:10 +0200

    feat: react component template (#4012)

--- scopes/react/react/react.templates.ts
+++ scopes/react/react/react.templates.ts
@@ -1,4 +1,5 @@
 import { ComponentTemplate } from '@teambit/generator/component-template';
 import { exampleButton } from './templates/example-button';
+import { reactComponent } from './templates/react-component';
 
-export const componentTemplates: ComponentTemplate[] = [exampleButton];
+export const componentTemplates: ComponentTemplate[] = [exampleButton, reactComponent];
---
+++ scopes/react/react/templates/react-component.ts
@@ -0,0 +1,70 @@
+import { ComponentTemplate, GeneratorContext } from '@teambit/generator/component-template';
+
+export const reactComponent: ComponentTemplate = {
+  name: 'react-component',
+  generateFiles: (context: GeneratorContext) => {
+    const { componentName, componentNameCamelCase } = context;
+    const indexFile = {
+      relativePath: 'index.ts',
+      content: `export { ${componentNameCamelCase} }  from './${componentName}';`,
+    };
+    const componentFile = {
+      relativePath: `${componentName}.tsx`,
+      content: `import React from 'react';
+
+export type ${componentNameCamelCase}Props = {
+text: string;
+};
+
+export const ${componentNameCamelCase} = ({
+text
+}: ${componentNameCamelCase}Props) => {
+return <p>{text}</p>
+};`,
+    };
+    const compositionFile = {
+      relativePath: `${componentName}.composition.tsx`,
+      content: `import React from 'react';
+import { ${componentNameCamelCase} } from './${componentName}';
+
+export const Basic${componentNameCamelCase} = () => {
+return <${componentNameCamelCase} text="hello from ${componentNameCamelCase}" />;
+};
+`,
+    };
+    const docsFile = {
+      relativePath: `${componentName}.docs.mdx`,
+      content: `---
+labels: ['react', 'typescript', 'ui', '${componentName}']
+description: 'A ${componentNameCamelCase} component.'
+---
+
+import { ${componentNameCamelCase} } from './${componentName}';
+
+${componentNameCamelCase} example:
+
+\`\`\`js live
+<${componentNameCamelCase} text="hello from ${componentNameCamelCase}"/>
+\`\`\`
+`,
+    };
+
+    const testFile = {
+      relativePath: `${componentName}.spec.tsx`,
+      content: `import React from 'react';
+import { render } from '@testing-library/react';
+import { expect } from 'chai';
+
+import { Basic${componentNameCamelCase} } from './${componentName}.composition';
+
+it('component should render', () => {
+const { getByText } = render(<Basic${componentNameCamelCase} />);
+const rendered = getByText('hello from ${componentNameCamelCase}');
+
+expect(rendered).to.exist;
+});`,
+    };
+
+    return [indexFile, componentFile, compositionFile, docsFile, testFile];
+  },
+};
