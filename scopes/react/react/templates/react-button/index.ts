f2ab23ff6f639d7a8f7074cbb2025e3dd3225463
Debbie O'Brien <debs_obrien@yahoo.co.uk>
2021-03-30 18:46:46 +0200

    fix: refactor into folders
    refactor tempates
    improve react
    add code comments

---
+++ scopes/react/react/templates/react-button/index.ts
@@ -0,0 +1,19 @@
+import { ComponentTemplate, GeneratorContext } from '@teambit/generator/component-template';
+import { componentFile } from './component';
+import { compositionFile } from './composition';
+import { docsFile } from './docs';
+import { testFile } from './test';
+
+export const reactButton: ComponentTemplate = {
+  name: 'react-button',
+
+  generateFiles: (context: GeneratorContext) => {
+    const { componentName: name, componentNameCamelCase: Name } = context;
+    const indexFile = {
+      relativePath: 'index.ts',
+      content: `export { ${Name} }  from './${name}';`,
+    };
+
+    return [indexFile, componentFile(context), compositionFile(context), docsFile(context), testFile(context)];
+  },
+};
