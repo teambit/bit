35b53a3c7cd9d4efa815b3be63e4689125c76ed0
Itay Mendelawy <itaymendel@users.noreply.github.com>
2021-03-31 19:45:51 +0300

    add templates to extend envs (#4034)
    * adding a template to create react env
    
    * adding a template to create react-native env
    
    * adding a template to create node env
    
    Co-authored-by: David First <davidnet@gmail.com>

---
+++ scopes/react/react-native/templates/react-native-env/files/doc.ts
@@ -0,0 +1,11 @@
+export function docFile() {
+  return `---
+labels: ['extension', 'react-native', 'env', 'environment']
+description: 'A customized extension for React environment.'
+---
+
+This is a customized extension for ReactNative, based of the [ReactNative Aspect](https://bit.dev/teambit/react/react-native).
+
+Explain here the modified configurations and tools applied.
+`;
+}
