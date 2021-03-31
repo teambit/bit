eec39ac4d4b55c5d89ef4e74e2475c7623cd85f1
David First <davidnet@gmail.com>
2021-03-31 10:31:28 -0400

    fix react-env template

--- scopes/react/react/react.templates.ts
+++ scopes/react/react/react.templates.ts
@@ -1,6 +1,6 @@
 import { ComponentTemplate } from '@teambit/generator';
 import { reactButton } from './templates/react-button';
 import { reactComponent } from './templates/react-component';
-import { reactExtension } from './templates/react-extension';
+import { customReactEnv } from './templates/custom-react-env';
 
-export const componentTemplates: ComponentTemplate[] = [reactButton, reactComponent, reactExtension];
+export const componentTemplates: ComponentTemplate[] = [reactButton, reactComponent, customReactEnv];
