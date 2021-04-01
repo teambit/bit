1aaec1f1c3c73180305b27eb81ab686311fe0740
Itay Mendelawy <itaymendel@users.noreply.github.com>
2021-04-01 12:14:48 +0300

    added missing ; to react env

--- scopes/react/react/templates/react-env/files/extension.ts
+++ scopes/react/react/templates/react-env/files/extension.ts
@@ -1,24 +1,24 @@
 import { GeneratorContext } from '@teambit/generator';
 
 export function extensionFile({ componentNameCamelCase: Name }: GeneratorContext) {
-  return `import { EnvsMain, EnvsAspect } from '@teambit/envs'
-import { ReactAspect, ReactMain } from '@teambit/react'
+  return `import { EnvsMain, EnvsAspect } from '@teambit/envs';
+import { ReactAspect, ReactMain } from '@teambit/react';
 
 export class ${Name}Extension {
   constructor(private react: ReactMain) {}
 
-  static dependencies: any = [EnvsAspect, ReactAspect]
+  static dependencies: any = [EnvsAspect, ReactAspect];
 
   static async provider([envs, react]: [EnvsMain, ReactMain]) {
     const ${Name}Env = react.compose([
       /*
         Use any of the "react.override..." transformers to
       */
-    ])
+    ]);
 
-    envs.registerEnv(${Name}Env)
+    envs.registerEnv(${Name}Env);
 
-    return new ${Name}Extension(react)
+    return new ${Name}Extension(react);
   }
 }
 `;
