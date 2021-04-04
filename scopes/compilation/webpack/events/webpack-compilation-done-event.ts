61ca4e23cdbb1ce70e0f4493e77532fdca970bd8
Kutner JS <urikutner@gmail.com>
2021-04-04 15:47:17 +0300

    Fix webpack cli outputs (#3933)
    - start listening to pubsub BEFORE setting up dev servers
    - consolidate our webpack listener plugins (start and done) into one plugin.
    - fix types

--- scopes/compilation/webpack/events/webpack-compilation-done-event.ts
+++ scopes/compilation/webpack/events/webpack-compilation-done-event.ts
@@ -1,14 +1,15 @@
 /* eslint-disable max-classes-per-file */
+import type { Stats } from 'webpack';
 import { BitBaseEvent } from '@teambit/pubsub';
 
 class WebpackCompilationDoneEventData {
-  constructor(readonly stats, readonly devServerID) {}
+  constructor(readonly stats: Stats, readonly devServerID: string) {}
 }
 
 export class WebpackCompilationDoneEvent extends BitBaseEvent<WebpackCompilationDoneEventData> {
   static readonly TYPE = 'webpack-compilation-done';
 
-  constructor(readonly timestamp, readonly stats, readonly devServerID) {
+  constructor(readonly timestamp: number, readonly stats: Stats, readonly devServerID: string) {
     super(
       WebpackCompilationDoneEvent.TYPE,
       '0.0.1',
