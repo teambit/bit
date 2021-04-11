d106f5d485406a49df638ada214aa9b751965ac3
David First <davidnet@gmail.com>
2021-04-11 19:46:50 -0400

    remove duplicate "react-env" in bit-templates

--- scopes/react/react/react.templates.ts
+++ scopes/react/react/react.templates.ts
@@ -4,11 +4,6 @@ import { reactComponentJS } from './templates/react-component-js';
 import { reactEnvTemplate } from './templates/react-env';
 import { reactWorkspaceTemplate } from './templates/react-workspace';
 
-export const componentTemplates: ComponentTemplate[] = [
-  reactComponent,
-  reactComponentJS,
-  reactEnvTemplate,
-  reactEnvTemplate,
-];
+export const componentTemplates: ComponentTemplate[] = [reactComponent, reactComponentJS, reactEnvTemplate];
 
 export const workspaceTemplates: WorkspaceTemplate[] = [reactWorkspaceTemplate];
