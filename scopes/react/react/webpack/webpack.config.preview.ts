558c85a9a338422bfed30df7f11f5a8ab1582745
Gilad Shoham <shoham.gilad@gmail.com>
2021-04-11 23:21:32 +0300

    restore ignore for mdx in file-loader

--- scopes/react/react/webpack/webpack.config.preview.ts
+++ scopes/react/react/webpack/webpack.config.preview.ts
@@ -422,7 +422,7 @@ export default function (fileMapPath: string): Configuration {
               // its runtime that would otherwise be processed through "file" loader.
               // Also exclude `html` and `json` extensions so they get processed
               // by webpacks internal loaders.
-              exclude: [/\.(js|mjs|jsx|ts|tsx)$/, /\.html$/, /\.json$/, /\.css$/],
+              exclude: [/\.(js|mjs|jsx|ts|tsx)$/, /\.html$/, /\.mdx?/, /\.json$/, /\.css$/],
               options: {
                 name: 'static/media/[name].[hash:8].[ext]',
               },
