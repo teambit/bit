f91d1bc729439d788fbdb9bde10d9192d2ccc117
David First <davidnet@gmail.com>
2021-04-16 20:20:29 -0400

    fix script on circle

--- scripts/link-bit-legacy.js
+++ scripts/link-bit-legacy.js
@@ -2,12 +2,14 @@ const fs = require('fs');
 const path = require('path');
 
 const bitDir = path.resolve(__dirname, '..');
-const dest = path.join(bitDir, 'node_modules', '@teambit/legacy');
+const destParent = path.join(bitDir, 'node_modules', '@teambit');
+const dest = path.join(destParent, 'legacy');
 
 try {
   fs.unlinkSync(dest);
 } catch (err) {} // maybe doesn't exist
 
+fs.mkdirSync(destParent, { recursive: true });
 fs.symlinkSync(bitDir, dest, 'junction');
 
 console.log(`symlink has been created from "${bitDir}" to "${dest}"`);
