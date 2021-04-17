6a47d35a0fa4015bf7389fa4cae4f76d151204ae
David First <davidnet@gmail.com>
2021-04-16 20:58:47 -0400

    compile after fixture population

--- src/e2e-helper/e2e-fixtures-helper.ts
+++ src/e2e-helper/e2e-fixtures-helper.ts
@@ -174,6 +174,7 @@ module.exports = () => 'comp${index}${additionalStr} and ' + ${nextComp}();`;
     this.fs.outputFile('app.js', "const comp1 = require('./comp1');\nconsole.log(comp1())");
     if (rewire) {
       this.command.linkAndRewire();
+      this.command.compile();
     }
     return Array(numOfComponents)
       .fill(null)
