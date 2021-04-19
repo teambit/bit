6be5b3ecdf7f2180f9a057208936bbcd939204b2
David First <davidnet@gmail.com>
2021-04-19 17:37:17 -0400

    change {scope} placeholder to be the scope-name only without owner, improve bit-create to work with the defaultDirectory settings of workspace.jsonc

--- src/utils/bit/compose-component-path.ts
+++ src/utils/bit/compose-component-path.ts
@@ -5,18 +5,37 @@ import BitId from '../../bit-id/bit-id';
 import { DEFAULT_COMPONENTS_DIR_PATH, DEFAULT_DEPENDENCIES_DIR_PATH } from '../../constants';
 import { PathOsBased } from '../path';
 
+/**
+ * the following place-holders are permitted:
+ * name - component name includes namespace, e.g. 'ui/button'.
+ * scopeId - full scope-id includes the owner, e.g. 'teambit.compilation'.
+ * scope - scope name only, e.g. 'compilation'.
+ * owner - owner name in bit.dev, e.g. 'teambit'.
+ */
 export function composeComponentPath(
   bitId: BitId,
-  // @ts-ignore AUTO-ADDED-AFTER-MIGRATION-PLEASE-FIX!
-  componentsDefaultDirectory?: string = DEFAULT_COMPONENTS_DIR_PATH
+  componentsDefaultDirectory: string = DEFAULT_COMPONENTS_DIR_PATH
 ): string {
   let defaultDir = componentsDefaultDirectory;
+  const { scope, owner } = parseScope(bitId.scope);
   // Prevent case where for example {scope}/{name} becomes /my-comp (in case the scope is empty)
   if (componentsDefaultDirectory.includes('{scope}/') && !bitId.scope) {
     defaultDir = componentsDefaultDirectory.replace('{scope}/', '');
   }
-  const result = format(defaultDir, { name: bitId.name, scope: bitId.scope });
+  if (componentsDefaultDirectory.includes('{owner}.') && !owner) {
+    defaultDir = componentsDefaultDirectory.replace('{owner}.', '');
+  }
+  const result = format(defaultDir, { name: bitId.name, scope, owner, scopeId: bitId.scope });
   return result;
+}
+
+function parseScope(scopeId?: string | null): { scope?: string; owner?: string } {
+  if (!scopeId) return {};
+  if (scopeId.includes('.')) {
+    const [owner, scope] = scopeId.split('.');
+    return { scope, owner };
+  }
+  return { scope: scopeId };
 }
 
 export function composeDependencyPath(
