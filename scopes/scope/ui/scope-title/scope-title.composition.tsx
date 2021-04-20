b54806f949783ce5554ca0e8d7620261ac278696
Josh Kuttler <yoshuakuttler@gmail.com>
2021-04-20 19:16:06 +0300

    add composition to check single word example

--- scopes/scope/ui/scope-title/scope-title.composition.tsx
+++ scopes/scope/ui/scope-title/scope-title.composition.tsx
@@ -2,3 +2,5 @@ import React from 'react';
 import { ScopeTitle } from './scope-title';
 
 export const ScopeTitleExample = () => <ScopeTitle scopeName="teambit.base-ui" />;
+
+export const ScopeTitleWithSingleWordExample = () => <ScopeTitle scopeName="base-ui" />;
