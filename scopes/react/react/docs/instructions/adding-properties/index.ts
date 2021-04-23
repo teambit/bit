f2030cc3674d872d48f9d2bfa4dcae1beaf96e3d
Debbie O'Brien <debs_obrien@yahoo.co.uk>
2021-04-23 21:31:37 +0200

    fix: remove properties and improving sytling for md

--- scopes/react/react/docs/instructions/adding-properties/index.ts
+++
@@ -1,1 +0,0 @@
-export { default as AddingProperties } from './adding-properties.mdx';
--- scopes/react/react/docs/properties/properties.tsx
+++ scopes/react/react/docs/properties/properties.tsx
@@ -1,17 +1,10 @@
 import { LinkedHeading } from '@teambit/documenter.ui.linked-heading';
 import { PropTable } from '@teambit/documenter.ui.property-table';
 import { Section } from '@teambit/documenter.ui.section';
-import { AddingProperties } from '@teambit/instructions.adding-properties';
-import { MdxPage } from '@teambit/ui.mdx-page';
 import React from 'react';
 
 export function Properties({ properties }: any) {
-  if (properties.length === 0)
-    return (
-      <MdxPage>
-        <AddingProperties />
-      </MdxPage>
-    );
+  if (properties.length === 0) return null;
 
   return (
     <Section>
