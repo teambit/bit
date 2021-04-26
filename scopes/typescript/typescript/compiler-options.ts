414bfa4c55edbced4cbbbf241bb9a2a35ae32a9e
Gilad Shoham <shoham.gilad@gmail.com>
2021-04-26 21:58:16 +0300

    add new compiler options for ts compiler - compileJs and compileJsx

--- scopes/typescript/typescript/compiler-options.ts
+++ scopes/typescript/typescript/compiler-options.ts
@@ -1,4 +1,4 @@
-import { CompilerOptions } from '@teambit/compiler';
+import type { CompilerOptions } from '@teambit/compiler';
 
 export type TypeScriptCompilerOptions = {
   /**
@@ -10,4 +10,16 @@ export type TypeScriptCompilerOptions = {
    * path for .d.ts files to include during build.
    */
   types: string[];
+
+  /**
+   * Run the compiler for .js files. this will only affect weather to run the compiler on the files.
+   * it won't change the tsconfig to support or not support js files.
+   */
+  compileJs?: boolean;
+
+  /**
+   * Run the compiler for .jsx files. this will only affect weather to run the compiler on the files.
+   * it won't change the tsconfig to support or not support jsx files.
+   */
+  compileJsx?: boolean;
 } & Partial<CompilerOptions>;
