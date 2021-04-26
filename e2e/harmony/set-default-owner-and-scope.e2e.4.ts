2103f5b965bfdc1d2f6f443d809be0a9eaf40d52
David First <davidnet@gmail.com>
2021-04-26 09:19:42 -0400

    roll back #4135 for Linux users. symlink _src dir from node_modules to source files for Windows users only

--- e2e/harmony/set-default-owner-and-scope.e2e.4.ts
+++ e2e/harmony/set-default-owner-and-scope.e2e.4.ts
@@ -41,9 +41,10 @@ describe('set default owner and scope', function () {
       helper.command.tagAllComponents();
     });
     it('should create link with default owner as prefix', () => {
-      const linkFolderPath = path.normalize(`node_modules/${componentPackageName}/src`);
+      const linkFolderPath = path.normalize(`node_modules/${componentPackageName}`);
+      const linkFullPath = path.join(linkFolderPath, 'is-type.js');
       const outputLinkPath = parsedLinkOutput.legacyLinkResults[0].bound[0].to;
-      expect(outputLinkPath).to.equal(linkFolderPath);
+      expect(outputLinkPath).to.equal(linkFullPath);
       expect(path.join(helper.scopes.localPath, 'node_modules')).to.be.a.directory();
       expect(path.join(helper.scopes.localPath, linkFolderPath)).to.be.a.directory();
     });
