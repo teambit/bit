229121955c06279d9a5497736c5510e48907799a
David First <davidnet@gmail.com>
2021-04-30 16:59:50 -0400

    remove node-cache, it is not in used, "lru-cache" replaced it (#4210)

--- src/cache/cache-factory.ts
+++ src/cache/cache-factory.ts
@@ -1,5 +1,4 @@
 import { InMemoryCache, CacheOptions } from './in-memory-cache';
-import { NodeCacheAdapter } from './node-cache-adapter';
 import { LRUCacheAdapter } from './lru-cache-adapter';
 
 export function createInMemoryCache<T>(
@@ -7,8 +6,6 @@ export function createInMemoryCache<T>(
   type: 'node' | 'redis' | 'lru' = 'lru'
 ): InMemoryCache<T> {
   switch (type) {
-    case 'node':
-      return new NodeCacheAdapter();
     case 'lru':
       return new LRUCacheAdapter(options);
     default:
