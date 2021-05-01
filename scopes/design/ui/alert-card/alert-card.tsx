4398d982a2b733f0502b4196365ce9e084acd27d
Debbie O'Brien <debs_obrien@yahoo.co.uk>
2021-05-01 11:28:22 +0200

    fix: docs improvements
    minor fixes and docs improvements

--- scopes/design/ui/alert-card/alert-card.tsx
+++ scopes/design/ui/alert-card/alert-card.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { ReactNode } from 'react';
 import { Card, CardProps } from '@teambit/base-ui.surfaces.card';
 import { LevelIcon, Level } from '@teambit/ui.elements.level-icon';
 import { backgrounds } from '@teambit/base-ui.surfaces.background';
@@ -16,6 +16,10 @@ interface InfoCardProps extends CardProps {
    * Level of Icon, info, error, warning, success
    */
   level: Level;
+  /**
+   * children to be rendered within this component.
+   */
+  children: ReactNode;
 }
 
 export function AlertCard({ title, children, className, level, ...rest }: InfoCardProps) {
@@ -25,7 +29,7 @@ export function AlertCard({ title, children, className, level, ...rest }: InfoCa
         <LevelIcon level={level} className={classNames(styles.icon, className)} />
         <H6 className={classNames(styles.title, className)}>{title}</H6>
       </div>
-      <Separator />
+      <Separator className={classNames(styles.separator, className)} />
       {children}
     </Card>
   );
