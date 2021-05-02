ab4bfad96bf37b228f4e6a59509a4701a4f692a5
Gilad Shoham <shoham.gilad@gmail.com>
2021-05-02 17:19:31 +0300

    change config-mutator from utils/ to modules/

--- scopes/compilation/webpack/index.ts
+++ scopes/compilation/webpack/index.ts
@@ -3,4 +3,4 @@ export { WebpackAspect } from './webpack.aspect';
 export type { WebpackConfigWithDevServer } from './webpack.dev-server';
 export * from './events';
 export { Configuration } from 'webpack';
-export { WebpackConfigMutator } from '@teambit/utils.config-mutator';
+export { WebpackConfigMutator } from '@teambit/modules.config-mutator';
