324982577fface105c6b8dcf3383507d43cba672
Kutner JS <urikutner@gmail.com>
2021-05-03 17:04:32 +0300

    Update component-url.spec.ts

--- scopes/component/component-url/component-url.spec.ts
+++ scopes/component/component-url/component-url.spec.ts
@@ -2,13 +2,15 @@ import { expect } from 'chai';
 import { ComponentID } from '@teambit/component-id';
 import { ComponentUrl } from './component-url';
 
+const BASE_URL = 'https://bit.dev';
+
 describe('component url', () => {
   it('should convert to url', () => {
     const id = ComponentID.fromString('teambit.component/component-id@0.0.312');
 
     const result = ComponentUrl.toUrl(id);
 
-    expect(result).to.equal('https://bit.dev/teambit/component/component-id?version=0.0.312');
+    expect(result).to.equal(`${BASE_URL}/teambit/component/component-id?version=0.0.312`);
   });
 
   it('should not require version', () => {
@@ -16,7 +18,7 @@ describe('component url', () => {
 
     const result = ComponentUrl.toUrl(id);
 
-    expect(result).to.equal('https://bit.dev/teambit/component/component-id');
+    expect(result).to.equal(`${BASE_URL}/teambit/component/component-id');
   });
 
   it('should skip url when opt out in options', () => {
@@ -24,6 +26,6 @@ describe('component url', () => {
 
     const result = ComponentUrl.toUrl(id, { includeVersion: false });
 
-    expect(result).to.equal('https://bit.dev/teambit/component/component-id');
+    expect(result).to.equal(`${BASE_URL}/teambit/component/component-id`);
   });
 });
