5e56f8667fe2f57a057fa167df6cf17f76e1f9bc
Mikilll <wasniewskimikolaj@gmail.com>
2021-05-04 22:17:38 +0200

    Refactored exec calls to fs api

--- scripts/establish-dev-link-windows.js
+++ scripts/establish-dev-link-windows.js
@@ -1,3 +1,4 @@
+const { rmSync, mkdirSync, writeFileSync } = require('fs');
 const { exec } = require('child_process');
 const path = require('path');
 
@@ -5,11 +6,19 @@ const userLinkName = process.argv[3];
 const linkName = userLinkName || 'bit-dev';
 
 const source = path.join(__dirname, '..', 'bin', 'bit.js');
-const dest = `%LocalAppData%\\${linkName}`;
+const dest = `${process.env.localappdata}\\${linkName}`;
 
-const batFileContent = `node ${source} %*`;
+rmSync(dest, { recursive: true, force: true });
+mkdirSync(dest);
 
-exec(`if exist ${dest} rmdir /s/q ${dest}`, (error, stdout, stderr) => {
+writeFileSync(`${dest}\\${linkName}.bat`, `@echo off\nnode ${source} %*`);
+
+if (process.env.PATH.includes(dest)) {
+  console.log(`Success!!!\nNow you can use the "${linkName}" command to run your dev app.`);
+  return;
+}
+
+exec(`powershell -NoProfile -ExecutionPolicy Bypass -Command "[Environment]::SetEnvironmentVariable('path', [Environment]::GetEnvironmentVariable('path', [EnvironmentVariableTarget]::User) + ';${dest}', 'User');"`, (error, stdout, stderr) => {
   if (error) {
     console.error(`exec error: ${error}`);
     return;
@@ -18,35 +27,5 @@ exec(`if exist ${dest} rmdir /s/q ${dest}`, (error, stdout, stderr) => {
   if (stdout) console.log(stdout);
   if (stderr) console.error(stderr);
 
-  exec(`mkdir ${dest}`, (error, stdout, stderr) => {
-    if (error) {
-      console.error(`exec error: ${error}`);
-      return;
-    }
-  
-    if (stdout) console.log(stdout);
-    if (stderr) console.error(stderr);
-
-    exec(`echo @echo off > ${dest}\\${linkName}.bat && echo ${batFileContent} >> ${dest}\\${linkName}.bat`, (error, stdout, stderr) => {
-      if (error) {
-        console.error(`exec error: ${error}`);
-        return;
-      }
-    
-      if (stdout) console.log(stdout);
-      if (stderr) console.error(stderr);
-    
-      exec(`powershell -NoProfile -ExecutionPolicy Bypass -Command "if ((Get-Command ${linkName} -ErrorAction SilentlyContinue) -eq $null) { [Environment]::SetEnvironmentVariable('path', [Environment]::GetEnvironmentVariable('path', [EnvironmentVariableTarget]::User) + ';${dest}', 'User'); }"`, (error, stdout, stderr) => {
-        if (error) {
-          console.error(`exec error: ${error}`);
-          return;
-        }
-      
-        if (stdout) console.log(stdout);
-        if (stderr) console.error(stderr);
-    
-        console.log(`Success!!!\nPlease close and reopen the terminal.\nIf you are using VSCode, you need to close it and reopen.\nThen you will be able to use the ${linkName} command to run your dev app :)`);
-      });
-    });
-  });
+  console.log(`Success!!!\nPlease close and reopen the terminal.\nIf you are using VSCode, you need to close it and reopen.\nThen you will be able to use the "${linkName}" command to run your dev app :)`);
 });
