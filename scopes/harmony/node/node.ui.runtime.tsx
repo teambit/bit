50048fde2b6d2e69051b7487b52a8a82930ed180
Debbie O'Brien <debs_obrien@yahoo.co.uk>
2021-05-05 08:00:18 +0200

    add emptyStates for runtimes

--- scopes/harmony/node/node.ui.runtime.tsx
+++ scopes/harmony/node/node.ui.runtime.tsx
@@ -3,46 +3,40 @@ import { CompositionsAspect, CompositionsUI } from '@teambit/compositions';
 import { UIRuntime } from '@teambit/ui';
 import { TesterAspect, TesterUI } from '@teambit/tester';
 import { EmptyBox } from '@teambit/ui.empty-box';
-import { ReactAspect } from './react.aspect';
-import { HighlighterWidget } from './highlighter-widget';
+import { NodeAspect } from './node.aspect';
 
-export class ReactUI {
+export class NodeEnvUI {
   static runtime = UIRuntime;
   static slots = [];
   static dependencies = [CompositionsAspect, TesterAspect];
 
   static async provider([compositionsUI, testerUi]: [CompositionsUI, TesterUI]) {
-    const reactUI = new ReactUI();
+    const nodeEnvUI = new NodeEnvUI();
+
     testerUi.registerEmptyState(() => {
       return (
         <EmptyBox
           title="This component doesn’t have any tests."
-          linkText="Learn how to add tests to your react components"
+          linkText="Learn how to add tests to your node components"
           link="https://harmony-docs.bit.dev/testing/overview/"
         />
       );
     });
+
     compositionsUI.registerEmptyState(() => {
       return (
         <EmptyBox
           title="This component doesn’t have any compositions."
-          linkText="Learn how to add tests to your react components"
+          linkText="Learn how to add compositions to your node components"
           link="https://harmony-docs.bit.dev/testing/overview/"
         />
       );
     });
 
-    if (typeof window !== 'undefined' && window.location.search.includes('highlighter')) {
-      compositionsUI.registerMenuWidget({
-        location: 'start',
-        content: <HighlighterWidget />,
-      });
-    }
-
-    return reactUI;
+    return nodeEnvUI;
   }
 }
 
-ReactAspect.addRuntime(ReactUI);
+NodeAspect.addRuntime(NodeEnvUI);
 
-export default ReactUI;
+export default NodeEnvUI;
