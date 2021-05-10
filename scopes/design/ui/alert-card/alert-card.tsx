14a0b43503ebca076912f2a8e1538981eb3354bd
Debbie O'Brien <debs_obrien@yahoo.co.uk>
2021-05-10 18:30:56 +0200

    fix: changes after pr review
    add size to be optional
    export types

--- scopes/design/ui/alert-card/alert-card.tsx
+++ scopes/design/ui/alert-card/alert-card.tsx
@@ -2,10 +2,12 @@ import React from 'react';
 import { Card, CardProps } from '@teambit/base-ui.surfaces.card';
 import { LevelIcon, Level } from '@teambit/ui.elements.level-icon';
 import { backgrounds } from '@teambit/base-ui.surfaces.background';
-import { H3 } from '@teambit/documenter.ui.heading';
+import { H3, Sizes } from '@teambit/documenter.ui.heading';
 import classNames from 'classnames';
 import { Separator } from '@teambit/ui.separator';
 import styles from './alert-card.module.scss';
+
+export type { Sizes, Level };
 
 export interface AlertCardProps extends CardProps {
   /**
@@ -16,14 +18,18 @@ export interface AlertCardProps extends CardProps {
    * Level of Icon, info, error, warning, success
    */
   level: Level;
+  /**
+   *  Size of the title
+   */
+  titleSize?: Sizes;
 }
 
-export function AlertCard({ title, children, className, level, ...rest }: AlertCardProps) {
+export function AlertCard({ title, titleSize = 'xxs', children, className, level, ...rest }: AlertCardProps) {
   return (
     <Card className={classNames(backgrounds.dent, styles.card, className)} {...rest}>
       <div className={classNames(styles.heading, className)}>
         <LevelIcon level={level} className={classNames(styles.icon, className)} />
-        <H3 size="xs" className={classNames(styles.title, className)}>
+        <H3 size={titleSize} className={classNames(styles.title, className)}>
           {title}
         </H3>
       </div>
--- scopes/design/ui/alert-card/index.ts
+++ scopes/design/ui/alert-card/index.ts
@@ -1,2 +1,3 @@
 export { AlertCard } from './alert-card';
 export type { AlertCardProps } from './alert-card';
+export type { Sizes, Level } from './alert-card';
