de23358a0cdebdb89ff11ea0a2691588cc29279f
Uri Kutner <urikutner@gmail.com>
2021-05-11 14:28:15 +0300

    react router anchor with base url

---
+++ scopes/ui-foundation/react-router/ui/link-anchor/link-anchor.tsx
@@ -0,0 +1,35 @@
+import React, { forwardRef } from 'react';
+import urlJoin from 'url-join';
+import { useLinkContext } from './link-context';
+// @ts-ignore - original file copied from another project
+import { LinkAnchor as BaseLinkAnchor } from './base-link-anchor';
+
+const fullUrlRegex = /^(\w*:)?\/\//;
+
+type ReactRouterLinkProps = {
+  /** @deprecated */
+  innerRef: any;
+  navigate: () => void;
+};
+type LinkAnchorProps = ReactRouterLinkProps & React.AnchorHTMLAttributes<HTMLAnchorElement>;
+
+/**
+ * React Router compatible Anchor element, with base-url from context.
+ */
+export const LinkAnchor = forwardRef<HTMLAnchorElement, LinkAnchorProps>(
+  ({ href, ...rest }: LinkAnchorProps, forwardedRef) => {
+    const _href = useContextUrl(href);
+
+    return <BaseLinkAnchor {...rest} ref={forwardedRef} href={_href} />;
+  }
+);
+
+LinkAnchor.displayName = 'LinkAnchor';
+
+function useContextUrl(href?: string) {
+  const { baseUrl } = useLinkContext();
+
+  if (!href || !baseUrl || fullUrlRegex.test(href)) return href;
+
+  return urlJoin(baseUrl, href);
+}
