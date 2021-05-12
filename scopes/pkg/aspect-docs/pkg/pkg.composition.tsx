491de3470bba295703f57c12c2dffc10df724f4c
Debbie O'Brien <debs_obrien@yahoo.co.uk>
2021-05-12 16:49:20 +0200

    feat: pkg docs

---
+++ scopes/pkg/aspect-docs/pkg/pkg.composition.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { ThemeContext } from '@teambit/documenter.theme.theme-context';
+import { MDXLayout } from '@teambit/ui.mdx-layout';
+import { Pkg } from './index';
+
+export const PkgDocs = () => (
+  <ThemeContext>
+    <MDXLayout>
+      <Pkg />
+    </MDXLayout>
+  </ThemeContext>
+);
