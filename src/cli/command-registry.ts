teambit/bit

commit a05ec4caeb0cbf35bff253ad392d86db9b8150f9

feat: add colors for cli commands (#4289)
* feat: add colors for cli commands

improve colors for cli commands

* fix tests

Co-authored-by: David First <davidnet@gmail.com>

src/cli/command-registry.ts
@@ -1,5 +1,5 @@
+import chalk from 'chalk';
 import commander from 'commander';
-
 import { Analytics } from '../analytics/analytics';
 import { SKIP_UPDATE_FLAG, TOKEN_FLAG, TOKEN_FLAG_NAME } from '../constants';
 import { Commands } from '../legacy-extensions/extension';
@@ -92,7 +92,7 @@ function createOptStr(alias, name) {
 export function register(command: Command, commanderCmd, packageManagerArgs?: string[]) {
   const concrete = commanderCmd
     .command(command.name, null, { noHelp: command.private })
-    .description(command.description)
+    .description(chalk.yellow(command.description as string))
     .alias(command.alias);
 
   if (command.remoteOp) {
@@ -118,7 +118,7 @@ export function register(command: Command, commanderCmd, packageManagerArgs?: st
   }
 
   command.options.forEach(([alias, name, description]) => {
-    concrete.option(createOptStr(alias, name), description);
+    concrete.option(chalk.green(createOptStr(alias, name)), `${chalk.white(description)}\n`);
   });
 
   // attach skip-update to all commands

commit 520d63f3427b87d124df4042688a0c307a023bea

fix commands flag after a regression caused by #4289

src/cli/command-registry.ts
@@ -95,11 +95,12 @@ export function register(command: Command, commanderCmd, packageManagerArgs?: st
     .description(chalk.yellow(command.description as string))
     .alias(command.alias);
 
+  const globalOptions: CommandOptions = [];
   if (command.remoteOp) {
-    command.options.push(['', TOKEN_FLAG, 'authentication token']);
+    globalOptions.push(['', TOKEN_FLAG, 'authentication token']);
   }
   if (!command.internal) {
-    command.options.push(
+    globalOptions.push(
       [
         '',
         'log [level]',
@@ -118,7 +119,14 @@ export function register(command: Command, commanderCmd, packageManagerArgs?: st
   }
 
   command.options.forEach(([alias, name, description]) => {
-    concrete.option(chalk.green(createOptStr(alias, name)), `${chalk.white(description)}\n`);
+    concrete.option(createOptStr(alias, name), description);
+  });
+
+  styleOptions(concrete);
+  addGlobalOptionsDelimiter(concrete);
+
+  globalOptions.forEach(([alias, name, description]) => {
+    concrete.option(createOptStr(alias, name), description);
   });
 
   // attach skip-update to all commands
@@ -131,6 +139,18 @@ export function register(command: Command, commanderCmd, packageManagerArgs?: st
   }
 
   return registerAction(command, concrete);
+}
+
+function styleOptions(concrete) {
+  concrete.options.forEach((option) => {
+    option.flags = chalk.green(option.flags);
+  });
+}
+
+function addGlobalOptionsDelimiter(concrete) {
+  if (!concrete.options.length) return;
+  const lastOption = concrete.options[concrete.options.length - 1];
+  lastOption.description = `${lastOption.description}\n\nGlobalOptions:`;
 }
 
 export default class CommandRegistry {