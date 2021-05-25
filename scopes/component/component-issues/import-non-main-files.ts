f63eef472e5594af12ff52ca3fd66ad837fd576d
David First <davidnet@gmail.com>
2021-05-25 18:07:58 -0400

    add GrpahQL query to get component issues (#4336)
    This is helpful to show the issues (normally shown in bit status) in the web UI.

--- scopes/component/component-issues/import-non-main-files.ts
+++ scopes/component/component-issues/import-non-main-files.ts
@@ -3,5 +3,5 @@ import { ComponentIssue, StringsPerFilePath } from './component-issue';
 export class ImportNonMainFiles extends ComponentIssue {
   description = 'importing non-main files (the dependency should expose its API from the main file)';
   data: StringsPerFilePath = {};
-  isCacheBlocker: false;
+  isCacheBlocker = false;
 }
