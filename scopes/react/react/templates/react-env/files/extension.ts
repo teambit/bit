cf7a9a4e981617cf5fd4dde9ee70e1856b366ca1
Benjamin Gilbert <benjamingilbert@Benjamins-MacBook-Pro.local>
2021-05-27 17:46:13 +0300

    move env main file contents to common

--- scopes/react/react/templates/react-env/files/extension.ts
+++ scopes/react/react/templates/react-env/files/extension.ts
@@ -1,25 +1,6 @@
 import { ComponentContext } from '@teambit/generator';
+import { MainFileContents } from '../../common/common-env-main';
 
-export function extensionFile({ namePascalCase: Name }: ComponentContext) {
-  return `import { EnvsMain, EnvsAspect } from '@teambit/envs'
-import { ReactAspect, ReactMain } from '@teambit/react'
-
-export class ${Name}Extension {
-  constructor(private react: ReactMain) {}
-
-  static dependencies: any = [EnvsAspect, ReactAspect]
-
-  static async provider([envs, react]: [EnvsMain, ReactMain]) {
-    const ${Name}Env = react.compose([
-      /*
-        Use any of the "react.override..." transformers to
-      */
-    ])
-
-    envs.registerEnv(${Name}Env)
-
-    return new ${Name}Extension(react)
-  }
-}
-`;
+export function extensionFile(context: ComponentContext) {
+  return MainFileContents(context);
 }
--- scopes/react/react/templates/react-provider/files/main.runtime.ts
+++ scopes/react/react/templates/react-provider/files/main.runtime.ts
@@ -1,32 +1,9 @@
 import { ComponentContext } from '@teambit/generator';
+import { MainFileContents } from '../../common/common-env-main';
 
 export const mainRuntimeFile = (context: ComponentContext) => {
-  const { name, namePascalCase: Name } = context;
   return {
-    relativePath: `${name}.main.runtime.tsx`,
-    content: `import { MainRuntime } from '@teambit/cli';
-import { EnvsAspect, EnvsMain } from '@teambit/envs';
-import { ReactAspect, ReactMain } from '@teambit/react';
-import { ${Name} } from './${name}.aspect';
-
-// e.g. const tsconfig = require.resolve(./ts/ts.config);
-
-export class ${Name}Main {
-  static slots = [];
-  static dependencies = [ReactAspect, EnvsAspect];
-  static runtime = MainRuntime;
-  static async provider([react, envs]: [ReactMain, EnvsMain])  {
-    const ${Name}Env = envs.compose(react.reactEnv, [
-      // e.g. react.overrideTsConfig(tsconfig) // tsconfig require-d above
-      
-    // Add overrides here via envs API functions as in the above example
-  ]);
-  envs.registerEnv(${Name}Env);
-  return new ${Name}Main(react);
-
-}
-
-${Name}.addRuntime(${Name}Main);
-`,
+    relativePath: `${context.name}.main.runtime.tsx`,
+    content: MainFileContents(context),
   };
 };
