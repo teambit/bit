7ef2d48e7140e2100b0efd5231bca6a338e23370
Benjamin Gilbert <benjamingilbert@Benjamins-MacBook-Pro.local>
2021-05-27 17:46:13 +0300

    move env main file contents to common

---
+++ scopes/react/react/templates/common/common-env-main.ts
@@ -0,0 +1,26 @@
+export const MainFileContents = ({ name, namePascalCase: Name }) => {
+  return `import { MainRuntime } from '@teambit/cli';
+    import { EnvsAspect, EnvsMain } from '@teambit/envs';
+    import { ReactAspect, ReactMain } from '@teambit/react';
+    import { ${Name} } from './${name}.aspect';
+    
+    // e.g. const tsconfig = require.resolve(./ts/ts.config);
+    
+    export class ${Name}Main {
+      static slots = [];
+      static dependencies = [ReactAspect, EnvsAspect];
+      static runtime = MainRuntime;
+      static async provider([react, envs]: [ReactMain, EnvsMain])  {
+        const ${Name}Env = envs.compose(react.reactEnv, [
+          // e.g. react.overrideTsConfig(tsconfig) // tsconfig require-d above
+          
+        // Add overrides here via envs API functions as in the above example
+      ]);
+      envs.registerEnv(${Name}Env);
+      return new ${Name}Main(react);
+    
+    }
+    
+    ${Name}.addRuntime(${Name}Main);
+    `;
+};
