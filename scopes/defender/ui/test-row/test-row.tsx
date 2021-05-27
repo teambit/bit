da9161c8bdbd61f6da5e55f6a29d1647018a0d8d
oded <oded@bit.dev>
2021-05-27 19:53:15 +0300

    removed theme context from test row component

--- scopes/defender/ui/test-row/test-row.tsx
+++ scopes/defender/ui/test-row/test-row.tsx
@@ -1,7 +1,6 @@
 import classNames from 'classnames';
 import React from 'react';
 import { CliSnippet } from '@teambit/design.ui.cli-snippet';
-import { ThemeContext } from '@teambit/documenter.theme.theme-context';
 import styles from './test-row.module.scss';
 
 export type TestRowProps = {
@@ -11,11 +10,9 @@ export type TestRowProps = {
 
 export function TestRow({ children, content, className, rowClass }: TestRowProps) {
   return (
-    <ThemeContext>
-      <div className={classNames(styles.testBlock, className)}>
-        <div className={classNames(styles.row, rowClass)}>{children}</div>
-        {content && <CliSnippet content={content} className={styles.snippet} />}
-      </div>
-    </ThemeContext>
+    <div className={classNames(styles.testBlock, className)}>
+      <div className={classNames(styles.row, rowClass)}>{children}</div>
+      {content && <CliSnippet content={content} className={styles.snippet} />}
+    </div>
   );
 }
