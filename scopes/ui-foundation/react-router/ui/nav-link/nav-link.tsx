315c28c999ac0aba54a1e9487ac1ebd12df815b2
Kutner JS <urikutner@gmail.com>
2021-06-01 22:03:18 +0300

    Update scopes/ui-foundation/react-router/ui/nav-link/nav-link.tsx

--- scopes/ui-foundation/react-router/ui/nav-link/nav-link.tsx
+++ scopes/ui-foundation/react-router/ui/nav-link/nav-link.tsx
@@ -5,7 +5,7 @@ import { LinkAnchor } from '@teambit/ui-foundation.ui.react-router.link-anchor';
 
 export { NavLinkProps };
 
-/** Adapter between React router's Nave and our isomorphic link components. Learn more [Here](https://bit.dev/teambit/base-ui/routing/routing-provider) */
+/** Adapter between React router's Nav and our isomorphic link components. Learn more [Here](https://bit.dev/teambit/base-ui/routing/routing-provider) */
 export function NavLink({ href = '', ...rest }: NavLinkProps) {
   if (rest.external) {
     return <NativeNavLink {...rest} href={href} />;
