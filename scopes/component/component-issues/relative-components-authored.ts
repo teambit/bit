a161ee0d13cb56e67b74efcc930937b8a070b166
David First <davidnet@gmail.com>
2021-06-02 17:45:36 -0400

    component-issues: split the description field to description and solution

--- scopes/component/component-issues/relative-components-authored.ts
+++ scopes/component/component-issues/relative-components-authored.ts
@@ -11,8 +11,8 @@ export type RelativeComponentsAuthoredEntry = {
 };
 
 export class relativeComponentsAuthored extends ComponentIssue {
-  description =
-    'components with relative import statements found (replace to module paths or use "bit link --rewire" to replace)';
+  description = 'components with relative import statements found';
+  solution = 'replace to module paths or use "bit link --rewire" to replace';
   data: { [fileName: string]: RelativeComponentsAuthoredEntry[] } = {};
   isCacheBlocker = false;
   formatDataFunction = relativeComponentsAuthoredIssuesToString;
