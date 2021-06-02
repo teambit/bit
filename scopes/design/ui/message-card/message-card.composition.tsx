777420a2e8f8a688e0da3579ca27b8d1b6ec69f7
Guysaar <guy@bit.dev>
2021-06-02 16:22:48 +0300

    add ThemeCompositions

--- scopes/design/ui/message-card/message-card.composition.tsx
+++ scopes/design/ui/message-card/message-card.composition.tsx
@@ -1,14 +1,19 @@
 import React from 'react';
+import { ThemeCompositions } from '@teambit/documenter.theme.theme-compositions';
 import { MessageCard } from './message-card';
 
 export const BasicMessageCard = () => <MessageCard title="message card" />;
 
 export const MessageCardWithIcon = () => (
-  <MessageCard data-testid="card-with-icon" title="message card with icon" icon="Ripple-pending" />
+  <ThemeCompositions>
+    <MessageCard data-testid="card-with-icon" title="message card with icon" icon="Ripple-pending" />
+  </ThemeCompositions>
 );
 
 export const OverflowingCardWithIcon = () => (
-  <div style={{ width: '200px' }}>
-    <MessageCard title="message card with iconAndLongText" icon="Ripple-pending" />
-  </div>
+  <ThemeCompositions>
+    <div style={{ width: '200px' }}>
+      <MessageCard title="message card with iconAndLongText" icon="Ripple-pending" />
+    </div>
+  </ThemeCompositions>
 );
--- scopes/design/ui/status-message-card/status-message-card.composition.tsx
+++ scopes/design/ui/status-message-card/status-message-card.composition.tsx
@@ -1,33 +1,46 @@
 import React from 'react';
+import { ThemeCompositions } from '@teambit/documenter.theme.theme-compositions';
 import { StatusMessageCard } from './status-message-card';
 
 export const PassingMessageCard = () => (
-  <StatusMessageCard status="SUCCESS" title="success card">
-    this might take a minute
-  </StatusMessageCard>
+  <ThemeCompositions>
+    <StatusMessageCard status="SUCCESS" title="success card">
+      this might take a minute
+    </StatusMessageCard>
+  </ThemeCompositions>
 );
 export const FailingMessageCard = () => (
-  <StatusMessageCard status="FAILURE" title="failure card">
-    this might take a minute
-  </StatusMessageCard>
+  <ThemeCompositions>
+    <StatusMessageCard status="FAILURE" title="failure card">
+      this might take a minute
+    </StatusMessageCard>
+  </ThemeCompositions>
 );
 export const PendingMessageCard = () => (
-  <StatusMessageCard status="PENDING" title="pending card">
-    this might take a minute
-  </StatusMessageCard>
+  <ThemeCompositions>
+    <StatusMessageCard status="PENDING" title="pending card">
+      this might take a minute
+    </StatusMessageCard>
+  </ThemeCompositions>
 );
 export const ProcessingMessageCard = () => (
-  <StatusMessageCard status="PROCESSING" title="processing card">
-    this might take a minute
-  </StatusMessageCard>
+  <ThemeCompositions>
+    <StatusMessageCard status="PROCESSING" title="processing card">
+      this might take a minute
+    </StatusMessageCard>
+  </ThemeCompositions>
 );
 export const SkippedMessageCard = () => (
-  <StatusMessageCard status="SKIPPED" title="skipped card">
-    this might take a minute
-  </StatusMessageCard>
+  <ThemeCompositions>
+    <StatusMessageCard status="SKIPPED" title="skipped card">
+      this might take a minute
+    </StatusMessageCard>
+  </ThemeCompositions>
 );
 export const UnknownMessageCard = () => (
-  <StatusMessageCard status="UNKNOWN" title="unknown card">
-    this might take a minute
-  </StatusMessageCard>
+  <ThemeCompositions>
+    <StatusMessageCard status="UNKNOWN" title="unknown card">
+      this might take a minute
+    </StatusMessageCard>
+  </ThemeCompositions>
 );
