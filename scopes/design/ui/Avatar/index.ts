a0b06aeb7320760225da9d118335a9c0937b3d2b
Oded Reuveny <oded@bit.dev>
2021-06-16 17:33:51 +0300

    UI updates (#4434)
    - pass ...rest in test table
    - exposed orgAvatarProps

--- scopes/design/ui/Avatar/index.ts
+++ scopes/design/ui/Avatar/index.ts
@@ -2,4 +2,5 @@ export * from './avatar';
 export { UserAvatar } from './user-avatar';
 export type { UserAvatarProps } from './user-avatar';
 export { OrgAvatar } from './org-avatar';
+export type { OrgAvatarProps } from './org-avatar';
 export { DefaultAvatar } from './default-avatar';
