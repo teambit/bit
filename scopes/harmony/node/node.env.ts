a3ea98c7876a5e8ec67647fcd3e19e957bcd73a4
Gilad Shoham <shoham.gilad@gmail.com>
2021-06-21 23:40:23 +0300

    fix the get dependencies for node components and aspect components

--- scopes/harmony/node/node.env.ts
+++ scopes/harmony/node/node.env.ts
@@ -7,6 +7,9 @@ export class NodeEnv implements Environment {
     return {
       devDependencies: {
         '@types/jest': '26.0.20',
+        '@types/node': '12.20.4',
+        // This is added as dev dep since our jest file transformer uses babel plugins that require this to be installed
+        '@babel/runtime': '7.12.18',
       },
     };
   }
