8bcec8eaa4b51a20da176807d875726ff1680b21
bengil <benjyg@gmail.com>
2021-06-22 20:12:18 +0300

    index file

--- scopes/html/index.ts
+++ scopes/html/index.ts
@@ -3,6 +3,9 @@ import { HtmlAspect } from './html.aspect';
 export type { HtmlPreview } from './html.preview.runtime';
 export type { HtmlEnv } from './html.env';
 export type { HtmlMain } from './html.main.runtime';
-export { HtmlComponent, defaultHtmlComponent } from './html-component-model'
+export type { HtmlComposition, HtmlFunctionComposition } from './interfaces';
+export { HtmlComponent, defaultHtmlComponent } from './html-component-model';
+export { renderTemplate } from './mount';
+export { fetchHtmlFromUrl, createElementFromString } from './utils'
 export default HtmlAspect;
 export { HtmlAspect };
