9fa29260a4106379f2fc92f23de80bc011f4f68d
benjgil <67269267+benjgil@users.noreply.github.com>
2021-06-27 20:49:21 +0300

    Html env (#4459)
    - add html env to process no-framework components and render compositions
    - Compositions can be one of 3 types - template string, js function to manipulate the root element, html element to append.
    - html env also includes some utility functions to assist producing these compositions, e.g. a function to fetch html from a url

---
+++ scopes/html/html/index.ts
@@ -0,0 +1,7 @@
+import { HtmlAspect } from './html.aspect';
+
+export { HtmlEnv } from './html.env';
+export type { HtmlMain } from './html.main.runtime';
+export type { HtmlComposition, HtmlFunctionComposition } from './interfaces';
+export default HtmlAspect;
+export { HtmlAspect };
