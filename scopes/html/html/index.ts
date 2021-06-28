ad102b5ce1effe632585810dcf0cdca46fb676e1
bengil <benjyg@gmail.com>
2021-06-28 14:24:28 +0300

    fix import

--- scopes/html/html/index.ts
+++ scopes/html/html/index.ts
@@ -3,6 +3,6 @@ import { HtmlAspect } from './html.aspect';
 export { HtmlEnv } from './html.env';
 export type { HtmlMain } from './html.main.runtime';
 export type { HtmlComposition, HtmlFunctionComposition } from './interfaces';
-export { default as Node } from './node.mdx';
+export { default as Html } from './html.docs.mdx';
 export default HtmlAspect;
 export { HtmlAspect };
