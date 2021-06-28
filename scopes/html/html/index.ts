09d3f1648fd44592ac76e60d2c2b3b35fc751059
bengil <benjyg@gmail.com>
2021-06-28 11:30:06 +0300

    export html env docs to be used on docs site

--- scopes/html/html/index.ts
+++ scopes/html/html/index.ts
@@ -3,5 +3,6 @@ import { HtmlAspect } from './html.aspect';
 export { HtmlEnv } from './html.env';
 export type { HtmlMain } from './html.main.runtime';
 export type { HtmlComposition, HtmlFunctionComposition } from './interfaces';
+export { default as Node } from './node.mdx';
 export default HtmlAspect;
 export { HtmlAspect };
