9f3dae35c137b18118d981b0274fc45f1e347e31
Gilad Shoham <shoham.gilad@gmail.com>
2021-06-29 15:24:02 +0300

    linting

--- scopes/dependencies/dependency-resolver/dependency-resolver.main.runtime.ts
+++ scopes/dependencies/dependency-resolver/dependency-resolver.main.runtime.ts
@@ -628,6 +628,7 @@ export class DependencyResolverMain {
         );
         updatedRegistries = updatedRegistries.updateScopedRegistry(name, registryWithAuth);
       }
+      return updatedRegistries;
     });
     return updatedRegistries;
   }
