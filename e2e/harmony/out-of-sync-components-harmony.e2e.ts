cddec09a6fd51ddef203370c793c63ebb35fb65b
David First <davidnet@gmail.com>
2021-06-29 11:52:57 -0400

    fix tests

--- e2e/harmony/out-of-sync-components-harmony.e2e.ts
+++ e2e/harmony/out-of-sync-components-harmony.e2e.ts
@@ -8,7 +8,6 @@ describe('components that are not synced between the scope and the consumer', fu
   let helper: Helper;
   before(() => {
     helper = new Helper();
-    helper.command.setFeatures('legacy-workspace-config');
   });
   after(() => {
     helper.scopeHelper.destroy();
