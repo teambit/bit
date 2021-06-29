3eb152ea0af1782791698b3b14527a586c5e5e78
Gilad Shoham <shoham.gilad@gmail.com>
2021-06-29 13:48:45 +0300

    add md/mdx rules to react env linting config, using the plugin:mdx/recommended

--- scopes/react/react/eslint/eslintrc.js
+++ scopes/react/react/eslint/eslintrc.js
@@ -4,15 +4,24 @@ import '@teambit/react.eslint-config-bit-react';
 module.exports = {
   extends: [require.resolve('@teambit/react.eslint-config-bit-react')],
   parserOptions: {
+    parser: require.resolve('@typescript-eslint/parser'),
     createDefaultProgram: true,
     // resolve the env tsconfig.
     project: require.resolve('../typescript/tsconfig.json'),
   },
+  overrides: [
+    {
+      files: ['*.md', '*.mdx'],
+      extends: ['plugin:mdx/recommended'],
+      parserOptions: {
+        // parser: '@typescript-eslint/parser',
+        ecmaVersion: 6,
+        sourceType: 'module',
+        ecmaFeatures: {
+          modules: true,
+        },
+        extensions: ['.md', '.mdx'],
+      },
+    },
+  ],
 };
-
-// module.exports = eslintConfig;
-
-// Using function
-// import generateEslint from '@teambit/react.eslint-config-bit-react';
-// const config = generateEslint(require.resolve('../typescript/tsconfig.json'));
-// module.exports = config;
