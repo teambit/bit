16d442892796a9d209a4888ab70b1af6d45275c5
Gilad Shoham <shoham.gilad@gmail.com>
2021-06-30 16:19:28 +0300

    change link-bit-legacy to only symlink dist and package.json

--- scripts/link-bit-legacy.js
+++ scripts/link-bit-legacy.js
@@ -2,18 +2,23 @@ const fs = require('fs');
 const path = require('path');
 
 const bitDir = path.resolve(__dirname, '..');
-const destParent = path.join(bitDir, 'node_modules', '@teambit');
-const dest = path.join(destParent, 'legacy');
+const destParent = path.join(bitDir, 'node_modules', '@teambit', 'legacy');
+const srcDist = path.join(bitDir, 'dist');
+const destDist = path.join(destParent, 'dist');
+const srcPackageJson = path.join(bitDir, 'package.json');
+const destPackageJson = path.join(destParent, 'package.json');
 
 try {
-  fs.rmdirSync(dest, { recursive: true });
+  fs.rmdirSync(destParent, { recursive: true });
 } catch (err) {} // maybe doesn't exist
 
 try {
-  fs.unlinkSync(dest);
+  fs.unlinkSync(destParent);
 } catch (err) {} // maybe doesn't exist or not a symlink
 
 fs.mkdirSync(destParent, { recursive: true });
-fs.symlinkSync(bitDir, dest, 'junction');
+fs.symlinkSync(srcDist, destDist, 'junction');
+fs.symlinkSync(srcPackageJson, destPackageJson);
 
-console.log(`symlink has been created from "${bitDir}" to "${dest}"`);
+console.log(`symlink has been created from "${srcDist}" to "${destDist}"`);
+console.log(`symlink has been created from "${srcPackageJson}" to "${destPackageJson}"`);
