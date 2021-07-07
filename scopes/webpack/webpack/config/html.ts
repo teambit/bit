5d7f09c7c71d147df835f2040254a24832f277dd
Uri Kutner <urikutner@gmail.com>
2021-07-07 13:33:10 +0300

    add viewport meta for html templates

--- scopes/webpack/webpack/config/html.ts
+++ scopes/webpack/webpack/config/html.ts
@@ -1,9 +1,11 @@
-module.exports = function html(title) {
+/** html template for Previews (docs, compositions, etc) */
+module.exports = function html(title: string) {
   return () => `
   <!DOCTYPE html>
   <html lang="en">
     <head>
       <meta charset="utf-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
       <title>${title}</title>
       <script>
       // Allow to use react dev-tools inside the examples
