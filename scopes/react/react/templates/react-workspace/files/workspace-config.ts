ef673f06cf9518e3ed8cd6b622c5e57f00f7d619
David First <davidnet@gmail.com>
2021-07-13 16:50:36 -0400

    preserve workspace.jsonc comments when creating workspaces via bit-new (#4570)
    - change "react-workspace" template to re-use the default workspace.jsonc template.
    - change the `generateFiles` API to return a promise.

--- scopes/react/react/templates/react-workspace/files/workspace-config.ts
+++ scopes/react/react/templates/react-workspace/files/workspace-config.ts
@@ -1,29 +1,17 @@
 import { WorkspaceContext } from '@teambit/generator';
+import { getWorkspaceConfigTemplateFile } from '@teambit/config';
+import { parse, stringify } from 'comment-json';
 
-export function workspaceConfig({ name, defaultScope }: WorkspaceContext) {
-  const data = {
-    $schema: 'https://static.bit.dev/teambit/schemas/schema.json',
-    'teambit.workspace/workspace': {
-      name,
-      icon: 'https://static.bit.dev/bit-logo.svg',
-      defaultDirectory: '{scope}/{name}',
-      defaultScope: defaultScope || 'my-scope',
-    },
-    'teambit.dependencies/dependency-resolver': {
-      packageManager: 'teambit.dependencies/pnpm',
-      policy: {
-        dependencies: {},
-        peerDependencies: {
-          react: '16.13.1',
-          'react-dom': '16.13.1',
-        },
-      },
-    },
-    'teambit.workspace/variants': {
-      '*': {
-        'teambit.react/react': {},
-      },
+export async function workspaceConfig({ name, defaultScope }: WorkspaceContext) {
+  const workspaceConfigTemplate = await getWorkspaceConfigTemplateFile();
+  const configParsed = parse(workspaceConfigTemplate);
+  configParsed['teambit.workspace/workspace'].name = name;
+  configParsed['teambit.workspace/workspace'].defaultScope = defaultScope || 'my-scope';
+  configParsed['teambit.workspace/variants'] = {
+    '*': {
+      'teambit.react/react': {},
     },
   };
-  return JSON.stringify(data, undefined, 2);
+
+  return stringify(configParsed, undefined, 2);
 }
