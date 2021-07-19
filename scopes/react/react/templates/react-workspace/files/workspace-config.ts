b1824a9079dc348d7c8a9303d39f1562df6b552d
Gilad Shoham <shoham.gilad@gmail.com>
2021-07-19 12:16:00 +0300

    add react, react-dom and testing library react to template dependencies

--- scopes/react/react/templates/react-workspace/files/workspace-config.ts
+++ scopes/react/react/templates/react-workspace/files/workspace-config.ts
@@ -5,6 +5,15 @@ export async function workspaceConfig({ name, defaultScope }: WorkspaceContext) 
   const configParsed = await getWorkspaceConfigTemplateParsed();
   configParsed['teambit.workspace/workspace'].name = name;
   configParsed['teambit.workspace/workspace'].defaultScope = defaultScope || 'my-scope';
+  configParsed['teambit.dependencies/dependency-resolver'].packageManager = 'teambit.dependencies/pnpm';
+  configParsed['teambit.dependencies/dependency-resolver'].policy = {
+    dependencies: {},
+    peerDependencies: {
+      '@testing-library/react': '11.2.6',
+      react: '16.13.1',
+      'react-dom': '16.13.1',
+    },
+  };
   configParsed['teambit.workspace/variants'] = {
     '*': {
       'teambit.react/react': {},
