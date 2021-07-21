09c9baf896b05cc29d751f8df67dc4af02234bab
Debbie O'Brien <debs-obrien@users.noreply.github.com>
2021-07-21 21:46:09 +0200

    shorten template names (#4618)
    * add links to docsshorten names for bit create but keep others as hidden so no breaking changes. both bit create react-component and bit create react will work
    
    * add links to docs for workspace and component generators

--- scopes/generator/generator/templates.cmd.ts
+++ scopes/generator/generator/templates.cmd.ts
@@ -30,8 +30,8 @@ export class TemplatesCmd implements Command {
 
     const grouped = groupBy(results, 'aspectId');
     const titleStr = this.generator.isRunningInsideWorkspace()
-      ? `The following template(s) are available with the command bit create: \nbit create react-component ui/button`
-      : `The following template(s) are available with the command bit new: \nbit new react-workspace my-workspace`;
+      ? `The following template(s) are available with the command bit create:  \nExample - bit create react my-component`
+      : `The following template(s) are available with the command bit new: \nExample - bit new react-workspace my-workspace`;
     const title = chalk.green(`\n${titleStr}\n`);
     const templateOutput = (template: TemplateDescriptor) => {
       const desc = template.description ? ` (${template.description})` : '';
--- scopes/html/html/html.main.runtime.ts
+++ scopes/html/html/html.main.runtime.ts
@@ -10,7 +10,7 @@ import { EnvsAspect, EnvsMain, EnvTransformer } from '@teambit/envs';
 import { ReactAspect, ReactMain } from '@teambit/react';
 import { GeneratorAspect, GeneratorMain } from '@teambit/generator';
 import { htmlEnvTemplate } from './templates/html-env';
-import { htmlComponentTemplate } from './templates/html-component';
+import { htmlComponentTemplate, deprecatedHtmlComponentTemplate } from './templates/html-component';
 import { HtmlAspect } from './html.aspect';
 import { HtmlEnv } from './html.env';
 
@@ -98,7 +98,7 @@ export class HtmlMain {
   static async provider([envs, react, generator]: [EnvsMain, ReactMain, GeneratorMain]) {
     const htmlEnv: HtmlEnv = envs.merge(new HtmlEnv(), react.reactEnv);
     envs.registerEnv(htmlEnv);
-    generator.registerComponentTemplate([htmlEnvTemplate, htmlComponentTemplate]);
+    generator.registerComponentTemplate([htmlEnvTemplate, htmlComponentTemplate, deprecatedHtmlComponentTemplate]);
     return new HtmlMain(react, htmlEnv, envs);
   }
 }
