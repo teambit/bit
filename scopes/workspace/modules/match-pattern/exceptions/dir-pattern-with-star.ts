fd643c3c44086a4f12a183b2c2ee7583089dba7d
Gilad Shoham <shoham.gilad@gmail.com>
2021-07-22 12:37:31 +0300

    update error message

--- scopes/workspace/modules/match-pattern/exceptions/dir-pattern-with-star.ts
+++ scopes/workspace/modules/match-pattern/exceptions/dir-pattern-with-star.ts
@@ -3,7 +3,7 @@ import { BitError } from '@teambit/bit-error';
 export class DirPatternWithStar extends BitError {
   constructor(readonly pattern: string) {
     super(
-      `the variants pattern "${pattern}" is invalid. a dir pattern should not contain "*" as it always include any subfolder by default`
+      `variant "${pattern}" is not valid. glob patterns are not supported for file paths. by default variants applied to all sub-directories`
     );
   }
 }
