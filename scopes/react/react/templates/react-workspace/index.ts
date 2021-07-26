b956af66b31958662314882c2dded1f03dec7426
Debbie O'Brien <debs_obrien@yahoo.co.uk>
2021-07-26 16:08:12 +0200

    file file imports

--- scopes/react/react/templates/react-workspace/index.ts
+++ scopes/react/react/templates/react-workspace/index.ts
@@ -1,9 +1,9 @@
 import { WorkspaceContext, WorkspaceTemplate } from '@teambit/generator';
 import { workspaceConfig } from './files/workspace-config';
 import { readme } from './files/readme-file';
-import { gitIgnore } from './files/git-ignore';
-import { assetTypes } from './files/types/asset';
-import { styleTypes } from './files/types/style';
+import { gitIgnore } from '../common-files/git-ignore';
+import { assetTypes } from '../common-files/types/asset';
+import { styleTypes } from '../common-files/types/style';
 
 export const reactWorkspaceTemplate: WorkspaceTemplate = {
   name: 'react',
