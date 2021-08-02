aafbc87d77f7a9784be1d0262b7bb7c7a6da9c3d
Debbie O'Brien <debs_obrien@yahoo.co.uk>
2021-08-02 17:19:08 +0200

    Update readme-tpl.ts

--- scopes/generator/generator/templates/workspace-generator/files/readme-tpl.ts
+++ scopes/generator/generator/templates/workspace-generator/files/readme-tpl.ts
@@ -1,6 +1,39 @@
 export function readmeTemplate() {
   return `export function readme() {
-  return 'hello world';
+  return \`## Workspace Generator
+
+Easily generate a new workspace with a single command.
+
+## Registering your Workspace
+
+Register your workspace template under the aspect environment in the variants section of the workspace.jsonc file.
+
+\`\`\`json
+"teambit.workspace/variants": {
+    "{workspace-name}": {
+      "teambit.harmony/aspect": {}
+    }
+  }
+\`\`\`
+
+## Customizing your Workspace
+
+See the docs for more info on [Customizing your Generator](https://harmony-docs.bit.dev/extending-bit/creating-a-custom-workspace-generator)
+
+## Using the Workspace Generator
+
+How to use this generator locally, essentially for development purposes:
+
+\`\`\`js
+bit new <template-name> <workspace-name> --load-from /Users/me/path/to/this/dir --aspect <workspace-template-id>
+\`\`\`
+
+How to use this generator after exporting to a remote scope:
+
+\`\`\`js
+bit new <template-name> <workspace-name> --aspect <workspace-template-id>
+\`\`\`
+\`;
 }
 `;
 }
