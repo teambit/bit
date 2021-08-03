d8326966aa883e1611f920cf0eaa2c3150980bc2
Uri Kutner <urikutner@gmail.com>
2021-08-03 20:13:37 +0300

    wip

--- scopes/ui-foundation/uis/pages/static-error/static-error-pages.tsx
+++ scopes/ui-foundation/uis/pages/static-error/static-error-pages.tsx
@@ -5,14 +5,30 @@ import { PreviewNotFoundPage } from '@teambit/ui-foundation.ui.pages.preview-not
 
 import { fullPageToStaticString } from '@teambit/ui-foundation.ui.rendering.full-page';
 
+const center = `
+  body { 
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
+
+    font-family: sans-serif;
+  }
+`;
+
+const assets = {
+  style: [center],
+  css: ['https://static.bit.dev/fonts/book-font.css'],
+};
+
 export function notFound(): string {
-  return fullPageToStaticString(<NotFoundPage />);
+  return fullPageToStaticString(<NotFoundPage className="bit-book-font" />, assets);
 }
 
 export function serverError(): string {
-  return fullPageToStaticString(<ServerErrorPage />);
+  return fullPageToStaticString(<ServerErrorPage className="bit-book-font" />, assets);
 }
 
 export function noPreview(): string {
-  return fullPageToStaticString(<PreviewNotFoundPage />);
+  return fullPageToStaticString(<PreviewNotFoundPage className="bit-book-font" />, assets);
 }
