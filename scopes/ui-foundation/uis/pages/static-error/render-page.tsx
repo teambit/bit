542ab917d8ed84bc7b3f8addae3cae0044bd4b1c
Uri Kutner <urikutner@gmail.com>
2021-08-04 12:01:44 +0300

    fix

--- scopes/ui-foundation/uis/pages/static-error/render-page.tsx
+++ scopes/ui-foundation/uis/pages/static-error/render-page.tsx
@@ -2,8 +2,6 @@ import React from 'react';
 import { renderToStaticMarkup } from 'react-dom/server';
 import { Html, Assets } from '@teambit/ui-foundation.ui.rendering.html';
 
-import { fullPageToStaticString } from '@teambit/ui-foundation.ui.rendering.full-page';
-
 export function fullPageToStaticString(content: ReactNode, assets?: Assets) {
   const html = (
     <Html assets={assets} fullHeight>
