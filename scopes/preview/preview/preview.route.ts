10a2a93b9b22e5e09d8475de5906089f17352e12
Kutner JS <urikutner@gmail.com>
2021-08-15 12:56:01 +0300

    refactor the dev-server process, to make it more readable (#4724)
    Refactor devServer code, to make it a little more readable, as part of #4723 
    
    - extracting dev server de-dup logic to its own file
    - fix `Request` type, to allow using generic type

--- scopes/preview/preview/preview.route.ts
+++ scopes/preview/preview/preview.route.ts
@@ -7,6 +7,11 @@ import type { Logger } from '@teambit/logger';
 import { PreviewMain } from './preview.main.runtime';
 import { PreviewArtifact } from './preview-artifact';
 
+type UrlParams = {
+  /** `/preview/:previewPath(*)` */
+  previewPath?: string;
+};
+
 export class PreviewRoute implements Route {
   constructor(
     /**
@@ -20,7 +25,7 @@ export class PreviewRoute implements Route {
   method = 'get';
 
   middlewares = [
-    async (req: Request, res: Response) => {
+    async (req: Request<UrlParams>, res: Response) => {
       try {
         // @ts-ignore TODO: @guy please fix.
         const component = req.component as Component | undefined;
