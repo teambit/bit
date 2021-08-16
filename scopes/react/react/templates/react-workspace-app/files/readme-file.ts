553e486dcdd02de807ff96c11b29b61973656900
Debbie O'Brien <debs-obrien@users.noreply.github.com>
2021-08-16 19:17:40 +0200

    Bit new target name (#4747)
    - add targetName
    - add dynamic value for env scope
    - add better comments for env
    - fixes to readme

--- scopes/react/react/templates/react-workspace-app/files/readme-file.ts
+++ scopes/react/react/templates/react-workspace-app/files/readme-file.ts
@@ -9,26 +9,28 @@ bit start
 
 ## What's included
 
-- **UI Components and Page Components**
+- **workspace.jsonc**
 
-Some basic components with composition file, doc file, test file, scss styles and the component file which is then exported using the index file.
+This is the main configuration file of your bit workspace. Here you can modify the workspace name and icon as well as default directory and scope. It is where dependencies are found when you install anything. It is also where you register aspects, bit extensions as well as apply the environments for your components. This workspace has been setup so that all components use the React env. However you can create other components and apply other envs to them such as node, html, angular and aspect envs.
 
 - **.bitmap**
 
 This is an auto-generated file and includes the mapping of your components. There is one component included here. In order to remove this component you can run the following command.
 
+
+- **Demo Components**
+
+A folder containing demo components are included in this workspace. These components are used to demonstrate the different features of Bit. If you would like to remove these components you can run the following command.
+
 \`\`\`jsx
-bit remove ui/* --delete files
+bit remove "ui/*" --delete files
 \`\`\`
 
-This removes the component from the bitmap as well as remove the files.
+This removes the components from the bitmap as well as removes the files.
+
 
 - **.gitignore**
 
 Ignoring any files from version control
-
-- **workspace.jsonc**
-
-This is the main configuration file of your bit workspace. Here you can modify the workspace name and icon as well as default directory and scope. It is where dependencies are found when you install anything. It is also where you register aspects, bit extensions as well as apply the environments for your components. This workspace has been setup so that all components use the React env. However you can create other components and apply other envs to them such as node, html, angular and aspect envs.
 `;
 }
