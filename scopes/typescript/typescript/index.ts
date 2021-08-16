6e53368e2b77bb8ebe2fbc1d0918e469673bb166
Gilad Shoham <shoham.gilad@gmail.com>
2021-08-16 16:21:32 +0300

    export TypescriptConfigMutator via typescript aspect

--- scopes/typescript/typescript/index.ts
+++ scopes/typescript/typescript/index.ts
@@ -1,5 +1,5 @@
+export { TypescriptConfigMutator } from '@teambit/typescript.modules.ts-config-mutator';
 export { TypescriptCompiler } from './typescript.compiler';
 export type { TypescriptMain, TsConfigTransformer } from './typescript.main.runtime';
 export type { TypeScriptCompilerOptions, TsCompilerOptionsWithoutTsConfig } from './compiler-options';
 export { TypescriptAspect } from './typescript.aspect';
-
