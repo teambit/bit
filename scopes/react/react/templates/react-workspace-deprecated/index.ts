75f5f9cf26599cf73cf761cfc99da6d4be76da16
Debbie O'Brien <debs-obrien@users.noreply.github.com>
2021-08-23 23:40:43 +0200

    Improve workspace template (#4798)
    - update to react 17
    - update testing lib
    - create one common workspace file
    - create one common readme file
    - delete non needed files
    - add more variants

--- scopes/react/react/templates/react-workspace-deprecated/index.ts
+++ scopes/react/react/templates/react-workspace-deprecated/index.ts
@@ -1,6 +1,6 @@
 import { WorkspaceContext, WorkspaceTemplate } from '@teambit/generator';
-import { workspaceConfig } from '../react-workspace/files/workspace-config';
-import { readme } from '../react-workspace/files/readme-file';
+import { workspaceConfig } from '../common-files/workspace-config';
+import { readme } from '../common-files/readme-file';
 import { gitIgnore } from '../common-files/git-ignore';
 import { assetTypes } from '../common-files/types/asset';
 import { styleTypes } from '../common-files/types/style';
