994eb20899cdefaed94aaeadb5bf0ed29389a035
David First <davidnet@gmail.com>
2021-08-30 15:56:28 -0400

    improve ComponentNotFound error when the component was set in the workspace.jsonc file

--- e2e/harmony/workspace-config.e2e.ts
+++ e2e/harmony/workspace-config.e2e.ts
@@ -21,4 +21,14 @@ describe('workspace config (workspace.jsonc)', function () {
       );
     });
   });
+  describe('adding a non-existing component to a variant', () => {
+    before(() => {
+      helper.scopeHelper.reInitLocalScopeHarmony();
+      helper.fixtures.populateComponents(1);
+      helper.bitJsonc.addToVariant('*', 'teambit.harmony/non-exist', {});
+    });
+    it('any command should throw a ComponentNotFound error with specific suggestions for the workspace.jsonc file', () => {
+      expect(() => helper.command.status()).to.throw(`your workspace.jsonc has this component-id set`);
+    });
+  });
 });
