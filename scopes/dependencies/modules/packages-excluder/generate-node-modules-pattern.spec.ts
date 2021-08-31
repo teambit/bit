e75c54408bae0a991ee4774b173010bbf0265554
Josh Kuttler <yoshuakuttler@gmail.com>
2021-08-31 19:52:49 +0300

    improve composition and tests

--- scopes/dependencies/modules/packages-excluder/generate-node-modules-pattern.spec.ts
+++ scopes/dependencies/modules/packages-excluder/generate-node-modules-pattern.spec.ts
@@ -1,12 +1,18 @@
 import { generateNodeModulesPattern } from './generate-node-modules-pattern';
 
 describe('generateNodeModulesPattern()', () => {
-  it('should return an empty regex when the received array is empty', () => {
-    expect(generateNodeModulesPattern({ packages: [] })).toEqual('node_modules/(?!()/)');
+  describe('should work with empty array', () => {
+    it('should return an empty regex', () => {
+      expect(generateNodeModulesPattern({ packages: [] })).toEqual('node_modules/(?!()/)');
+    });
   });
   describe('should work with one package', () => {
-    const pattern = generateNodeModulesPattern({ packages: ['@myorg'] });
-    const regex = new RegExp(pattern);
+    let pattern;
+    let regex;
+    beforeAll(() => {
+      pattern = generateNodeModulesPattern({ packages: ['@myorg'] });
+      regex = new RegExp(pattern);
+    });
 
     describe('should exclude the package', () => {
       it('should have yarn structure excluded', () => {
@@ -21,9 +27,14 @@ describe('generateNodeModulesPattern()', () => {
     });
   });
   describe('should work with more than one packages', () => {
-    const packagesToTransform = ['react', '@myorg', 'testing-library__dom'];
-    const pattern = generateNodeModulesPattern({ packages: packagesToTransform });
-    const regex = new RegExp(pattern);
+    let packagesToTransform;
+    let pattern;
+    let regex;
+    beforeAll(() => {
+      packagesToTransform = ['react', '@myorg', 'testing-library__dom'];
+      pattern = generateNodeModulesPattern({ packages: packagesToTransform });
+      regex = new RegExp(pattern);
+    });
 
     describe('should exclude the first package', () => {
       it('should have yarn structure excluded', () => {
@@ -60,9 +71,14 @@ describe('generateNodeModulesPattern()', () => {
     });
   });
   describe('should not exclude the package when is not in the regex', () => {
-    const packagesToTransform = ['react'];
-    const pattern = generateNodeModulesPattern({ packages: packagesToTransform });
-    const regex = new RegExp(pattern);
+    let packagesToTransform;
+    let pattern;
+    let regex;
+    beforeAll(() => {
+      packagesToTransform = ['react'];
+      pattern = generateNodeModulesPattern({ packages: packagesToTransform });
+      regex = new RegExp(pattern);
+    });
 
     describe('should not exclude', () => {
       it('with yarn structure', () => {
--- scopes/dependencies/modules/packages-excluder/packages-excluder.composition.tsx
+++ scopes/dependencies/modules/packages-excluder/packages-excluder.composition.tsx
@@ -5,10 +5,8 @@ export function LiveExample() {
   const [text, setText] = useState('react,@myorg,some-lib');
   return (
     <div>
-      Live example, write packages to exclude separate with a comma:
-      <br />
-      <input value={text} onChange={(e) => setText(e.target.value)} style={{ width: 500 }} />
-      <br />
+      <div>Live example, write packages to exclude separate with a comma:</div>
+      <input value={text} onChange={(e) => setText(e.target.value)} style={{ width: 300 }} />
       <div style={{ backgroundColor: '#ededed', padding: 8 }}>
         {text && generateNodeModulesPattern({ packages: text.split(',') })}
       </div>
@@ -18,25 +16,29 @@ export function LiveExample() {
 
 export function RegexExample() {
   const [packagesToExclude, setPackagesToExclude] = useState('@myorg,react,some-lib');
-  const [packageToCheck, setPackageToCheck] = useState('@myorg/something');
+  const [packageToCheck, setPackageToCheck] = useState('@myorg');
   const [regexResult, setRegexResult] = useState(true);
 
   useEffect(() => {
     const pattern = generateNodeModulesPattern({ packages: packagesToExclude.split(',') });
     const regex = new RegExp(pattern);
-    setRegexResult(regex.test(`node_modules/${packageToCheck}`) === false);
+    setRegexResult(regex.test(`node_modules/${packageToCheck}/some-path`) === false);
   }, [packagesToExclude, packageToCheck]);
 
   return (
-    <div>
-      Live example, write packages to exclude separate with a comma:
-      <br />
-      <input value={packagesToExclude} onChange={(e) => setPackagesToExclude(e.target.value)} style={{ width: 500 }} />
-      <br />
-      Write a package that you want to check with Regex test:
-      <br />
-      <input value={packageToCheck} onChange={(e) => setPackageToCheck(e.target.value)} style={{ width: 500 }} />
-      <br />
+    <div style={{ width: 500 }}>
+      <div>Live example, write packages to exclude separate with a comma:</div>
+      <input
+        value={packagesToExclude}
+        onChange={(e) => setPackagesToExclude(e.target.value)}
+        style={{ width: 300, marginBottom: 12 }}
+      />
+      <div>Write a package that you want to check with Regex test:</div>
+      <div style={{ marginBottom: 12 }}>
+        <input value="node_modules/" disabled />
+        <input value={packageToCheck} onChange={(e) => setPackageToCheck(e.target.value)} />
+        <input value="/some-path" disabled />
+      </div>
       <div style={{ backgroundColor: '#ededed', padding: 8 }}>
         regex exclude {packageToCheck} package: {regexResult.toString()}
       </div>
