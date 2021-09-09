7fa17a3c1e5920da8ee1c4227909277c10d178c7
David First <davidnet@gmail.com>
2021-09-09 11:24:29 -0400

    fix bit-artifacts command when a component was not exported yet

--- scopes/scope/scope/scope.main.runtime.ts
+++ scopes/scope/scope/scope.main.runtime.ts
@@ -568,18 +568,16 @@ export class ScopeMain implements ComponentFactory {
    * @param includeCache whether or not include components that their scope-name is different than the current scope-name
    */
   async listIds(includeCache = false): Promise<ComponentID[]> {
-    let modelComponents = await this.legacyScope.list();
-    const ids = modelComponents.map((component) => ComponentID.fromLegacy(component.toBitIdWithLatestVersion()));
-    this.logger.debug(`scope listIds: modelComponents ids: ${JSON.stringify(ids)}`);
-    if (!includeCache) {
-      modelComponents = modelComponents.filter((modelComponent) => this.exists(modelComponent));
-    }
+    const allModelComponents = await this.legacyScope.list();
+    const modelComponentsToList = includeCache
+      ? allModelComponents
+      : allModelComponents.filter((modelComponent) => this.exists(modelComponent));
 
-    const componentsIds = modelComponents.map((component) =>
-      ComponentID.fromLegacy(component.toBitIdWithLatestVersion())
+    const ids = modelComponentsToList.map((component) =>
+      ComponentID.fromLegacy(component.toBitIdWithLatestVersion(), component.scope || this.name)
     );
-    this.logger.debug(`scope listIds: componentsIds after filter scope: ${JSON.stringify(componentsIds)}`);
-    return componentsIds;
+    this.logger.debug(`scope listIds: componentsIds after filter scope: ${JSON.stringify(ids, undefined, 2)}`);
+    return ids;
   }
 
   /**
