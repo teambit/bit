e4c609b98369785e7d17e03968348f3b77500a32
Guysaar <guy@bit.dev>
2021-10-19 18:25:17 +0300

    lint

--- scopes/react/react/react.app-type.ts
+++ scopes/react/react/react.app-type.ts
@@ -4,9 +4,16 @@ import { ReactAppOptions } from './react-app-options';
 import { ReactApp } from './react.application';
 
 export class ReactAppType implements ApplicationType<ReactAppOptions> {
-  constructor(readonly name: string, private reactEnv: ReactEnv) { }
+  constructor(readonly name: string, private reactEnv: ReactEnv) {}
 
   createApp(options: ReactAppOptions) {
-    return new ReactApp(options.name, options.entry, options.portRange || [3000, 4000], this.reactEnv, options.deploy, options.prerenderRoutes);
+    return new ReactApp(
+      options.name,
+      options.entry,
+      options.portRange || [3000, 4000],
+      this.reactEnv,
+      options.deploy,
+      options.prerenderRoutes
+    );
   }
 }
