bf7bc3fd86ce908ebb7f25d652b391d4ccc0a3c7
oded <oded@bit.dev>
2021-10-24 14:25:46 +0300

    linter fix

--- scopes/ui-foundation/tree/folder-tree-node/folder-tree-node.mock.ts
+++ scopes/ui-foundation/tree/folder-tree-node/folder-tree-node.mock.ts
@@ -1,12 +1,3 @@
-import React from 'react';
-
-const CustomImg = (
-    <img
-      style={{ width: 16, marginRight: 8 }}
-      src="https://bitsrc.imgix.net/bf5970b9b97dfb045867dd2842eaefd1e623e328.png?size=35&w=70&h=70&crop=faces&fit=crop&bg=fff"
-    />
-  );
-
 export const node = {
     id: 'composing',
     payload: {
