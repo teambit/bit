f2b6fcd2f76a080f6d0631ad8432da02ea03f5e0
David First <davidnet@gmail.com>
2021-11-12 17:55:29 -0500

    introduce "bit use" command to setup an aspect into workspace/scope

---
+++ scopes/harmony/use/index.ts
@@ -0,0 +1,5 @@
+import { UseAspect } from './use.aspect';
+
+export type { UseMain } from './use.main.runtime';
+export default UseAspect;
+export { UseAspect };
---
+++ scopes/harmony/use/use.aspect.ts
@@ -0,0 +1,5 @@
+import { Aspect } from '@teambit/harmony';
+
+export const UseAspect = Aspect.create({
+  id: 'teambit.bit/use',
+});
