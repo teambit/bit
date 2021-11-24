be53cb340a9a103a2369de447fff5794df1fc993
Kutner JS <urikutner@gmail.com>
2021-11-24 17:08:20 +0200

    highlighter improvements (#5073)
    - fix highlighter not working inside exclusion zone
    - fix highlighter targeting inner-most component
    - update docs
    - tests
    verified on Evangelist Enterprise page

---
+++ scopes/react/component-highlighter/hover-highlighter/index.ts
@@ -0,0 +1,5 @@
+export { useHoverHighlighter } from './use-hover-highlighter';
+export type { useHoverHighlighterOptions } from './use-hover-highlighter';
+
+export { HoverHighlighter } from './hover-highlighter';
+export type { HoverHighlighterProps } from './hover-highlighter';
