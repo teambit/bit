teambit/bit

commit 7dd6dede01f026ba8011783ae34b180d934a9163

feat: installing missing peer dependencies (#5162)

scopes/workspace/workspace/workspace.ts
@@ -1,3 +1,4 @@
+/* eslint-disable max-lines */
 import chalk from 'chalk';
 import memoize from 'memoizee';
 import mapSeries from 'p-map-series';
@@ -19,6 +20,7 @@ import {
   InvalidComponent,
 } from '@teambit/component';
 import { Importer } from '@teambit/importer';
+import { BitError } from '@teambit/bit-error';
 import { ComponentScopeDirMap, ConfigMain } from '@teambit/config';
 import {
   WorkspaceDependencyLifecycleType,
@@ -111,6 +113,7 @@ export interface EjectConfOptions {
 }
 
 export type WorkspaceInstallOptions = {
+  addMissingPeers?: boolean;
   variants?: string;
   lifecycleType?: WorkspaceDependencyLifecycleType;
   dedupe: boolean;
@@ -1305,6 +1308,30 @@ export class Workspace implements ComponentFactory {
    * @memberof Workspace
    */
   async install(packages?: string[], options?: WorkspaceInstallOptions) {
+    if (options?.addMissingPeers) {
+      if (packages?.length) {
+        throw new BitError(
+          'Adding new dependencies and adding missing peer dependencies at the same time is currently not supported'
+        );
+      }
+      const compDirMap = await this.getComponentsDirectory([]);
+      const mergedRootPolicy = this.dependencyResolver.getWorkspacePolicy();
+      const depsFilterFn = await this.generateFilterFnForDepsFromLocalRemote();
+      const pmInstallOptions: PackageManagerInstallOptions = {
+        dedupe: options?.dedupe,
+        copyPeerToRuntimeOnRoot: options?.copyPeerToRuntimeOnRoot ?? true,
+        copyPeerToRuntimeOnComponents: options?.copyPeerToRuntimeOnComponents ?? false,
+        dependencyFilterFn: depsFilterFn,
+        overrides: this.dependencyResolver.config.overrides,
+      };
+      const missingPeers = await this.dependencyResolver.getMissingPeerDependencies(
+        this.path,
+        mergedRootPolicy,
+        compDirMap,
+        pmInstallOptions
+      );
+      packages = Object.entries(missingPeers).map(([peerName, range]) => `${peerName}@${range}`);
+    }
     if (packages && packages.length) {
       if (!options?.variants && (options?.lifecycleType as string) === 'dev') {
         throw new DependencyTypeNotSupportedInPolicy(options?.lifecycleType as string);

e2e/harmony/install-missing-peer-dependencies.e2e.ts
@@ -1,0 +1,90 @@
+import chai, { expect } from 'chai';
+import path from 'path';
+import Helper from '../../src/e2e-helper/e2e-helper';
+import NpmCiRegistry, { supportNpmCiRegistryTesting } from '../npm-ci-registry';
+
+chai.use(require('chai-fs'));
+
+(supportNpmCiRegistryTesting ? describe : describe.skip)('install --add-missing-peers', function () {
+  let npmCiRegistry: NpmCiRegistry;
+  let helper: Helper;
+  this.timeout(0);
+  before(async () => {
+    helper = new Helper();
+    npmCiRegistry = new NpmCiRegistry(helper);
+    await npmCiRegistry.init();
+  });
+  after(() => {
+    helper.scopeHelper.destroy();
+    npmCiRegistry.destroy();
+  });
+  describe(`using pnpm as a package manager`, () => {
+    before(() => {
+      helper.scopeHelper.reInitLocalScopeHarmony({ registry: npmCiRegistry.ciRegistry });
+      helper.extensions.bitJsonc.addKeyValToDependencyResolver('packageManager', `teambit.dependencies/pnpm`);
+      helper.command.install('has-foo100-peer@1.0.0 has-foo101-peer@1.0.0 abc');
+      helper.command.install('--add-missing-peers');
+    });
+    it('should install the missing peer dependencies to node_modules', function () {
+      expect(path.join(helper.fixtures.scopes.localPath, 'node_modules/peer-a')).to.be.a.path();
+      expect(path.join(helper.fixtures.scopes.localPath, 'node_modules/peer-b')).to.be.a.path();
+      expect(path.join(helper.fixtures.scopes.localPath, 'node_modules/peer-c')).to.be.a.path();
+    });
+    it('should not install the missing peer dependencies that have conflicting versions to node_modules', function () {
+      expect(path.join(helper.fixtures.scopes.localPath, 'node_modules/foo')).not.to.be.a.path();
+    });
+    it('should add the missing peer dependencies to workspace.jsonc', () => {
+      expect(
+        helper.bitJsonc.read()['teambit.dependencies/dependency-resolver'].policy.peerDependencies
+      ).to.have.property('peer-a');
+      expect(
+        helper.bitJsonc.read()['teambit.dependencies/dependency-resolver'].policy.peerDependencies
+      ).to.have.property('peer-b');
+      expect(
+        helper.bitJsonc.read()['teambit.dependencies/dependency-resolver'].policy.peerDependencies
+      ).to.have.property('peer-c');
+    });
+    it('should not add the missing peer dependencies that have conflicting versions to workspace.jsonc', () => {
+      expect(
+        helper.bitJsonc.read()['teambit.dependencies/dependency-resolver'].policy.peerDependencies
+      ).not.to.have.property('foo');
+    });
+    it('should throw an exception if new dependencies are added and missing dependencies are added', () => {
+      expect(() => helper.command.install('lodash --add-missing-peers')).throw(
+        /Adding new dependencies and adding missing peer dependencies at the same time is currently not supported/
+      );
+    });
+  });
+  describe(`using Yarn as a package manager`, () => {
+    before(() => {
+      helper.scopeHelper.reInitLocalScopeHarmony({ registry: npmCiRegistry.ciRegistry });
+      helper.extensions.bitJsonc.addKeyValToDependencyResolver('packageManager', `teambit.dependencies/yarn`);
+      helper.command.install('has-foo100-peer@1.0.0 has-foo101-peer@1.0.0 abc');
+      helper.command.install('--add-missing-peers');
+    });
+    it('should install the missing peer dependencies to node_modules', function () {
+      expect(path.join(helper.fixtures.scopes.localPath, 'node_modules/peer-a')).to.be.a.path();
+      expect(path.join(helper.fixtures.scopes.localPath, 'node_modules/peer-b')).to.be.a.path();
+      expect(path.join(helper.fixtures.scopes.localPath, 'node_modules/peer-c')).to.be.a.path();
+    });
+    it('should not install the missing peer dependencies that have conflicting versions to node_modules', function () {
+      expect(path.join(helper.fixtures.scopes.localPath, 'node_modules/foo')).not.to.be.a.path();
+    });
+    it('should add the missing peer dependencies to workspace.jsonc', () => {
+      expect(
+        helper.bitJsonc.read()['teambit.dependencies/dependency-resolver'].policy.peerDependencies
+      ).to.have.property('peer-a');
+      expect(
+        helper.bitJsonc.read()['teambit.dependencies/dependency-resolver'].policy.peerDependencies
+      ).to.have.property('peer-b');
+      expect(
+        helper.bitJsonc.read()['teambit.dependencies/dependency-resolver'].policy.peerDependencies
+      ).to.have.property('peer-c');
+    });
+    it('should not add the missing peer dependencies that have conflicting versions to workspace.jsonc', () => {
+      expect(
+        helper.bitJsonc.read()['teambit.dependencies/dependency-resolver'].policy.peerDependencies
+      ).not.to.have.property('foo');
+    });
+  });
+});

e2e/npm-ci-registry.ts
@@ -1,5 +1,7 @@
 /* eslint no-console: 0 */
+import { REGISTRY_MOCK_PORT } from '@pnpm/registry-mock';
 import { ChildProcess } from 'child_process';
+import fetch from 'cross-fetch';
 import execa from 'execa';
 import fs from 'fs-extra';
 import * as path from 'path';
@@ -37,7 +39,7 @@ export default class NpmCiRegistry {
   // @ts-ignore AUTO-ADDED-AFTER-MIGRATION-PLEASE-FIX!
   registryServer: ChildProcess;
   helper: Helper;
-  ciRegistry = 'http://localhost:4873';
+  ciRegistry = `http://localhost:${REGISTRY_MOCK_PORT}`;
   ciDefaultScope = '@ci';
   constructor(helper: Helper) {
     this.helper = helper;
@@ -56,14 +58,22 @@ export default class NpmCiRegistry {
   }
 
   _establishRegistry(): Promise<void> {
-    return new Promise((resolve) => {
-      this.registryServer = execa('verdaccio', { detached: true });
+    return new Promise((resolve, reject) => {
+      this.registryServer = execa('registry-mock', { detached: true });
+      let resolved = false;
       // @ts-ignore AUTO-ADDED-AFTER-MIGRATION-PLEASE-FIX!
-      this.registryServer.stdout.on('data', (data) => {
+      this.registryServer.stdout.on('data', async (data): void => {
         if (this.helper.debugMode) console.log(`stdout: ${data}`);
-        if (data.includes('4873')) {
-          if (this.helper.debugMode) console.log('Verdaccio server is up and running');
-          resolve();
+        if (!resolved && data.includes(REGISTRY_MOCK_PORT)) {
+          resolved = true;
+          // eslint-disable-next-line
+          await new Promise<void>((resolve) => setTimeout(() => resolve(), 1000));
+          if ((await fetch(`http://localhost:${REGISTRY_MOCK_PORT}`)).status !== 200) {
+            reject(new Error('Registry has not started'));
+          } else {
+            if (this.helper.debugMode) console.log('Verdaccio server is up and running');
+            resolve();
+          }
         }
       });
       // @ts-ignore AUTO-ADDED-AFTER-MIGRATION-PLEASE-FIX!
@@ -91,7 +101,7 @@ EOD`;
       !addUserResult.stdout.includes('Logged in as ci to scope @ci') && // npm<7
       !addUserResult.stdout.includes(`Logged in as ci on ${this.ciRegistry}`) // npm>=7
     ) {
-      throw new Error(`failed executing npm adduser ${addUserResult.stderr}`);
+      throw new Error(`failed executing npm adduser ${addUserResult.stderr || addUserResult.stdout}`);
     }
     if (this.helper.debugMode) console.log('default user has been added successfully to Verdaccio');
     fs.removeSync('adduser.sh');

scopes/dependencies/dependency-resolver/dependency-resolver.main.runtime.ts
@@ -1,6 +1,6 @@
 import mapSeries from 'p-map-series';
 import { MainRuntime } from '@teambit/cli';
-import ComponentAspect, { Component, ComponentMain } from '@teambit/component';
+import ComponentAspect, { Component, ComponentMap, ComponentMain } from '@teambit/component';
 import type { ConfigMain } from '@teambit/config';
 import { get, pick } from 'lodash';
 import { ConfigAspect } from '@teambit/config';
@@ -57,7 +57,11 @@ import {
   WorkspacePolicyEntry,
   SerializedVariantPolicy,
 } from './policy';
-import { PackageManager } from './package-manager';
+import {
+  PackageManager,
+  PeerDependencyIssuesByProjects,
+  PackageManagerGetPeerDependencyIssuesOptions,
+} from './package-manager';
 
 import {
   SerializedDependency,
@@ -627,6 +631,41 @@ export class DependencyResolverMain {
 
   private async getProxyConfigFromGlobalConfig(): Promise<ProxyConfig> {
     return Http.getProxyConfig();
+  }
+
+  /**
+   * Return the peer dependencies and their ranges that may be installed
+   * without causing unmet peer dependency issues in some of the dependencies.
+   */
+  async getMissingPeerDependencies(
+    rootDir: string,
+    rootPolicy: WorkspacePolicy,
+    componentDirectoryMap: ComponentMap<string>,
+    options: PackageManagerGetPeerDependencyIssuesOptions
+  ): Promise<Record<string, string>> {
+    this.logger.setStatusLine('finding missing peer dependencies');
+    const packageManager = this.packageManagerSlot.get(this.config.packageManager);
+    let peerDependencyIssues!: PeerDependencyIssuesByProjects;
+    if (packageManager?.getPeerDependencyIssues && typeof packageManager?.getPeerDependencyIssues === 'function') {
+      peerDependencyIssues = await packageManager?.getPeerDependencyIssues(
+        rootDir,
+        rootPolicy,
+        componentDirectoryMap,
+        options
+      );
+    } else {
+      const systemPm = this.getSystemPackageManager();
+      if (!systemPm.getPeerDependencyIssues)
+        throw new Error('system package manager must implement `getPeerDependencyIssues()`');
+      peerDependencyIssues = await systemPm?.getPeerDependencyIssues(
+        rootDir,
+        rootPolicy,
+        componentDirectoryMap,
+        options
+      );
+    }
+    this.logger.consoleSuccess();
+    return peerDependencyIssues['.']?.intersections;
   }
 
   async getRegistries(): Promise<Registries> {

scopes/dependencies/dependency-resolver/package-manager.ts
@@ -1,8 +1,11 @@
+import { PeerDependencyIssuesByProjects } from '@pnpm/core';
 import { ComponentMap } from '@teambit/component';
 import { Registries } from './registry';
 import { DepsFilterFn } from './manifest';
 import { WorkspacePolicy } from './policy';
 import { NetworkConfig, ProxyConfig } from './dependency-resolver.main.runtime';
+
+export { PeerDependencyIssuesByProjects };
 
 export type PackageManagerInstallOptions = {
   cacheRootDir?: string;
@@ -19,6 +22,8 @@ export type PackageManagerInstallOptions = {
 
   overrides?: Record<string, string>;
 };
+
+export type PackageManagerGetPeerDependencyIssuesOptions = PackageManagerInstallOptions;
 
 export type ResolvedPackageVersion = {
   packageName: string;
@@ -50,6 +55,13 @@ export interface PackageManager {
     packageName: string,
     options: PackageManagerResolveRemoteVersionOptions
   ): Promise<ResolvedPackageVersion>;
+
+  getPeerDependencyIssues?(
+    rootDir: string,
+    rootPolicy: WorkspacePolicy,
+    componentDirectoryMap: ComponentMap<string>,
+    options: PackageManagerGetPeerDependencyIssuesOptions
+  ): Promise<PeerDependencyIssuesByProjects>;
 
   getRegistries?(): Promise<Registries>;
 

scopes/dependencies/pnpm/lynx.ts
@@ -12,7 +12,8 @@ import {
   PackageManagerProxyConfig,
   PackageManagerNetworkConfig,
 } from '@teambit/dependency-resolver';
-import { MutatedProject, mutateModules } from '@pnpm/core';
+import { MutatedProject, mutateModules, PeerDependencyIssuesByProjects, ProjectOptions } from '@pnpm/core';
+import * as pnpm from '@pnpm/core';
 import createResolverAndFetcher, { ClientOptions } from '@pnpm/client';
 import pickRegistryForPackage from '@pnpm/pick-registry-for-package';
 import { Logger } from '@teambit/logger';
@@ -23,6 +24,8 @@ type RegistriesMap = {
   default: string;
   [registryName: string]: string;
 };
+
+const STORE_CACHE: Record<string, { ctrl: StoreController; dir: string }> = {};
 
 async function createStoreController(
   rootDir: string,
@@ -50,9 +53,15 @@ async function createStoreController(
     maxSockets: networkConfig.maxSockets,
     networkConcurrency: networkConfig.networkConcurrency,
   };
-  // Although it would be enough to call createNewStoreController(),
-  // that doesn't resolve the store directory location.
-  return createOrConnectStoreController(opts);
+  // We should avoid the recreation of store.
+  // The store holds cache that makes subsequent resolutions faster.
+  const cacheKey = JSON.stringify(opts);
+  if (!STORE_CACHE[cacheKey]) {
+    // Although it would be enough to call createNewStoreController(),
+    // that doesn't resolve the store directory location.
+    STORE_CACHE[cacheKey] = await createOrConnectStoreController(opts);
+  }
+  return STORE_CACHE[cacheKey];
 }
 
 async function generateResolverAndFetcher(
@@ -86,9 +95,53 @@ async function generateResolverAndFetcher(
   return result;
 }
 
+export async function getPeerDependencyIssues(
+  rootManifest,
+  manifestsByPaths: Record<string, any>,
+  opts: {
+    storeDir: string;
+    cacheDir: string;
+    registries: Registries;
+    proxyConfig: PackageManagerProxyConfig;
+    networkConfig: PackageManagerNetworkConfig;
+    overrides?: Record<string, string>;
+  }
+): Promise<PeerDependencyIssuesByProjects> {
+  const projects: ProjectOptions[] = [];
+  const workspacePackages = {};
+  for (const [rootDir, manifest] of Object.entries(manifestsByPaths)) {
+    projects.push({
+      manifest,
+      rootDir,
+    });
+    workspacePackages[manifest.name] = workspacePackages[manifest.name] || {};
+    workspacePackages[manifest.name][manifest.version] = { dir: rootDir, manifest };
+  }
+  projects.push({
+    manifest: rootManifest.manifest,
+    rootDir: rootManifest.rootDir,
+  });
+  const registriesMap = getRegistriesMap(opts.registries);
+  const storeController = await createStoreController(
+    rootManifest.rootDir,
+    opts.storeDir,
+    opts.cacheDir,
+    opts.registries,
+    opts.proxyConfig,
+    opts.networkConfig
+  );
+  return pnpm.getPeerDependencyIssues(projects, {
+    storeController: storeController.ctrl,
+    storeDir: storeController.dir,
+    overrides: opts.overrides,
+    workspacePackages,
+    registries: registriesMap,
+  });
+}
+
 export async function install(
-  rootPathToManifest,
-  pathsToManifests,
+  rootManifest,
+  manifestsByPaths,
   storeDir: string,
   cacheDir: string,
   registries: Registries,
@@ -116,8 +169,8 @@ export async function install(
   // This is the rational behind not deleting this completely, but need further check that it really works
 
   // eslint-disable-next-line
-  for (const rootDir in pathsToManifests) {
-    const manifest = pathsToManifests[rootDir];
+  for (const rootDir in manifestsByPaths) {
+    const manifest = manifestsByPaths[rootDir];
     packagesToBuild.push({
       buildIndex: 0, // workspace components should be installed before the root
       manifest,
@@ -129,14 +182,14 @@ export async function install(
   }
   packagesToBuild.push({
     buildIndex: 1, // install the root package after the workspace components were installed
-    manifest: rootPathToManifest.manifest,
+    manifest: rootManifest.manifest,
     mutation: 'install',
-    rootDir: rootPathToManifest.rootDir,
+    rootDir: rootManifest.rootDir,
   });
   const registriesMap = getRegistriesMap(registries);
   const authConfig = getAuthConfig(registries);
   const storeController = await createStoreController(
-    rootPathToManifest.rootDir,
+    rootManifest.rootDir,
     storeDir,
     cacheDir,
     registries,
@@ -145,7 +198,7 @@ export async function install(
   );
   const opts = {
     storeDir: storeController.dir,
-    dir: rootPathToManifest.rootDir,
+    dir: rootManifest.rootDir,
     storeController: storeController.ctrl,
     workspacePackages,
     preferFrozenLockfile: true,

scopes/dependencies/pnpm/pnpm.package-manager.ts
@@ -17,6 +17,8 @@ import {
 import { Logger } from '@teambit/logger';
 import { memoize, omit } from 'lodash';
 import { PkgMain } from '@teambit/pkg';
+import { PeerDependencyIssuesByProjects } from '@pnpm/core';
+import { ProjectManifest } from '@pnpm/types';
 import { join } from 'path';
 import userHome from 'user-home';
 import { readConfig } from './read-config';
@@ -24,22 +26,21 @@ import { readConfig } from './read-config';
 const defaultStoreDir = join(userHome, '.pnpm-store');
 const defaultCacheDir = join(userHome, '.pnpm-cache');
 
+interface Manifests {
+  componentsManifests: Record<string, ProjectManifest>;
+  rootManifest: ProjectManifest;
+}
+
 export class PnpmPackageManager implements PackageManager {
   private readConfig = memoize(readConfig);
   constructor(private depResolver: DependencyResolverMain, private pkg: PkgMain, private logger: Logger) {}
 
-  async install(
+  private async _componentsToPnpmWorkspaceProjects(
     rootDir: string,
     rootPolicy: WorkspacePolicy,
     componentDirectoryMap: ComponentMap<string>,
     installOptions: PackageManagerInstallOptions = {}
-  ): Promise<void> {
-    // require it dynamically for performance purpose. the pnpm package require many files - do not move to static import
-    // eslint-disable-next-line global-require, import/no-dynamic-require
-    const { install } = require('./lynx');
-    const storeDir = installOptions?.cacheRootDir ? join(installOptions?.cacheRootDir, '.pnpm-store') : defaultStoreDir;
-    const cacheDir = installOptions?.cacheRootDir ? join(installOptions?.cacheRootDir, '.pnpm-cache') : defaultCacheDir;
-
+  ): Promise<Manifests> {
     const components = componentDirectoryMap.toArray().map(([component]) => component);
     const options: CreateFromComponentsOptions = {
       filterComponentsFromManifests: true,
@@ -67,6 +68,34 @@ export class PnpmPackageManager implements PackageManager {
       // !options.dedupe
       installOptions.copyPeerToRuntimeOnComponents
     );
+    return {
+      componentsManifests,
+      rootManifest,
+    };
+  }
+
+  _getGlobalPnpmDirs(cacheRootDir?: string) {
+    const storeDir = cacheRootDir ? join(cacheRootDir, '.pnpm-store') : defaultStoreDir;
+    const cacheDir = cacheRootDir ? join(cacheRootDir, '.pnpm-cache') : defaultCacheDir;
+    return { storeDir, cacheDir };
+  }
+
+  async install(
+    rootDir: string,
+    rootPolicy: WorkspacePolicy,
+    componentDirectoryMap: ComponentMap<string>,
+    installOptions: PackageManagerInstallOptions = {}
+  ): Promise<void> {
+    // require it dynamically for performance purpose. the pnpm package require many files - do not move to static import
+    // eslint-disable-next-line global-require, import/no-dynamic-require
+    const { install } = require('./lynx');
+
+    const { componentsManifests, rootManifest } = await this._componentsToPnpmWorkspaceProjects(
+      rootDir,
+      rootPolicy,
+      componentDirectoryMap,
+      installOptions
+    );
     this.logger.debug('root manifest for installation', rootManifest);
     this.logger.debug('components manifests for installation', componentsManifests);
     this.logger.setStatusLine('installing dependencies using pnpm');
@@ -75,6 +104,7 @@ export class PnpmPackageManager implements PackageManager {
     const registries = await this.depResolver.getRegistries();
     const proxyConfig = await this.depResolver.getProxyConfig();
     const networkConfig = await this.depResolver.getNetworkConfig();
+    const { storeDir, cacheDir } = this._getGlobalPnpmDirs(installOptions?.cacheRootDir);
     await install(
       rootManifest,
       componentsManifests,
@@ -94,11 +124,40 @@ export class PnpmPackageManager implements PackageManager {
     this.logger.consoleSuccess('installing dependencies using pnpm');
   }
 
+  async getPeerDependencyIssues(
+    rootDir: string,
+    rootPolicy: WorkspacePolicy,
+    componentDirectoryMap: ComponentMap<string>,
+    installOptions: PackageManagerInstallOptions = {}
+  ): Promise<PeerDependencyIssuesByProjects> {
+    const { storeDir, cacheDir } = this._getGlobalPnpmDirs(installOptions?.cacheRootDir);
+    const proxyConfig = await this.depResolver.getProxyConfig();
+    const networkConfig = await this.depResolver.getNetworkConfig();
+    const registries = await this.depResolver.getRegistries();
+    // require it dynamically for performance purpose. the pnpm package require many files - do not move to static import
+    // eslint-disable-next-line global-require, import/no-dynamic-require
+    const lynx = require('./lynx');
+    const { componentsManifests, rootManifest } = await this._componentsToPnpmWorkspaceProjects(
+      rootDir,
+      rootPolicy,
+      componentDirectoryMap,
+      installOptions
+    );
+    return lynx.getPeerDependencyIssues(rootManifest, componentsManifests, {
+      storeDir,
+      cacheDir,
+      proxyConfig,
+      registries,
+      networkConfig,
+      overrides: installOptions.overrides,
+    });
+  }
+
   private computeComponentsManifests(
     componentDirectoryMap: ComponentMap<string>,
     componentsManifestsFromWorkspace: ComponentsManifestsMap,
     copyPeerToRuntime = false
-  ) {
+  ): Record<string, ProjectManifest> {
     return componentDirectoryMap.toArray().reduce((acc, [component, dir]) => {
       const packageName = this.pkg.getPackageName(component);
       if (componentsManifestsFromWorkspace.has(packageName)) {

scopes/dependencies/yarn/yarn.package-manager.ts
@@ -1,4 +1,3 @@
-import * as semver from 'semver';
 import parsePackageName from 'parse-package-name';
 import {
   WorkspacePolicy,
@@ -36,6 +35,7 @@ import parseOverrides from '@pnpm/parse-overrides';
 import { PkgMain } from '@teambit/pkg';
 import userHome from 'user-home';
 import { Logger } from '@teambit/logger';
+import versionSelectorType from 'version-selector-type';
 
 type BackupJsons = {
   [path: string]: Buffer | undefined;
@@ -371,7 +371,8 @@ export class YarnPackageManager implements PackageManager {
   ): Promise<ResolvedPackageVersion> {
     const parsedPackage = parsePackageName(packageName);
     const parsedVersion = parsedPackage.version;
-    if (parsedVersion && semver.valid(parsedVersion)) {
+    const versionType = parsedVersion && versionSelectorType(parsedVersion)?.type;
+    if (versionType === 'version') {
       return {
         packageName: parsedPackage.name,
         version: parsedVersion,
@@ -395,7 +396,7 @@ export class YarnPackageManager implements PackageManager {
     const report = new LightReport({ configuration: config, stdout: process.stdout });
 
     // Handle cases when the version is a dist tag like dev / latest for example bit install lodash@latest
-    if (parsedPackage.version) {
+    if (versionType === 'tag') {
       resolver = new NpmTagResolver();
       range = `npm:${parsedPackage.version}`;
     }

commit bdf37b972f64d31d32a3ca5504d190c74475456a

fix duplicate identifier error caused by #5162

scopes/workspace/workspace/workspace.ts
@@ -65,7 +65,6 @@ import { ExtensionDataList, ExtensionDataEntry } from '@teambit/legacy/dist/cons
 import { pathIsInside } from '@teambit/legacy/dist/utils';
 import componentIdToPackageName from '@teambit/legacy/dist/utils/bit/component-id-to-package-name';
 import { PathOsBased, PathOsBasedRelative, PathOsBasedAbsolute } from '@teambit/legacy/dist/utils/path';
-import { BitError } from '@teambit/bit-error';
 import fs from 'fs-extra';
 import { slice, uniqBy, difference, compact, pick } from 'lodash';
 import path from 'path';