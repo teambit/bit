35e5f7e9642fb0656d64a795e11642ea7f9794e0
Josh Kuttler <yoshuakuttler@gmail.com>
2021-12-22 13:35:59 +0200

    add preview support to react-native aspect

--- scopes/react/react-native/templates/react-native-env/files/preview.runtime.ts
+++ scopes/react/react-native/templates/react-native-env/files/preview.runtime.ts
@@ -2,21 +2,20 @@ import { ComponentContext } from '@teambit/generator';
 
 export function previewRuntimeFile({ namePascalCase: Name, nameCamelCase: nameCamel, name }: ComponentContext) {
   return `import { PreviewRuntime } from '@teambit/preview';
-import { ReactAspect, ReactPreview } from '@teambit/react';
+import { ReactNativeAspect, ReactNativePreview } from '@teambit/react-native';
 // create your theme and import it here
 // import { ThemeCompositions } from '@my-company/my-scope.theme.theme-compositions';
-
 import { ${Name}Aspect } from './${name}.aspect';
 
 export class ${Name}PreviewMain {
   static runtime = PreviewRuntime;
 
-  static dependencies = [ReactAspect];
+  static dependencies = [ReactNativeAspect];
 
-  static async provider([react]: [ReactPreview]) {
+  static async provider([reactNative]: [ReactNativePreview]) {
     const ${nameCamel}PreviewMain = new ${Name}PreviewMain();
     // uncomment the line below to register a new provider to wrap all compositions using this environment with a custom theme.
-    // react.registerProvider([ThemeCompositions]);
+    // reactNative.registerProvider([ThemeCompositions]);
 
     return ${nameCamel}PreviewMain;
   }
