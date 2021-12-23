e5ce894ac754cfa311bca06c423d56fab7ed913f
Zoltan Kochan <z@kochan.io>
2021-12-23 17:13:41 +0200

    move yarn and npm consumer boxes to appropriate aspects

--- scopes/component/component/component.ui.runtime.tsx
+++ scopes/component/component/component.ui.runtime.tsx
@@ -133,46 +133,6 @@ export class ComponentUI {
     };
   };
 
-  // TODO - move to npm aspect
-  private npmMethod: ConsumePlugin = (comp) => {
-    const registry = comp.packageName.split('/')[0];
-    const packageVersion = comp.version === comp.latest ? '' : `@${comp.version}`;
-    return {
-      Title: <img style={{ width: '30px' }} src="http://static.bit.dev/brands/logo-npm-new.svg" />,
-      Component: (
-        <Install
-          config={`npm config set '${registry}:registry' https://node.bit.dev`}
-          componentName={comp.id.name}
-          packageManager="npm"
-          copyString={`npm i ${comp.packageName}${packageVersion}`}
-          registryName={registry}
-        />
-      ),
-      order: 10,
-    };
-  };
-
-  // TODO - move to yarn? aspect
-  private yarnMethod: ConsumePlugin = (comp) => {
-    const registry = comp.packageName.split('/')[0];
-    const packageVersion = comp.version === comp.latest ? '' : `@${comp.version}`;
-    return {
-      Title: (
-        <img style={{ height: '17px', paddingTop: '4px' }} src="https://static.bit.dev/brands/logo-yarn-text.svg" />
-      ),
-      Component: (
-        <Install
-          config={`npm config set '${registry}:registry' https://node.bit.dev`}
-          componentName={comp.id.name}
-          packageManager="yarn"
-          copyString={`yarn add ${comp.packageName}${packageVersion}`}
-          registryName={registry}
-        />
-      ),
-      order: 20,
-    };
-  };
-
   registerPubSub() {
     this.pubsub.sub(PreviewAspect.id, (be: BitBaseEvent<any>) => {
       if (be.type === ClickInsideAnIframeEvent.TYPE) {
@@ -286,7 +246,7 @@ export class ComponentUI {
     componentUI.registerMenuItem(componentUI.menuItems);
     componentUI.registerRoute(section.route);
     componentUI.registerWidget(section.navigationLink, section.order);
-    componentUI.registerConsumeMethod(componentUI.bitMethod, componentUI.npmMethod, componentUI.yarnMethod);
+    componentUI.registerConsumeMethod(componentUI.bitMethod);
     return componentUI;
   }
 }
---
+++ scopes/pkg/pkg/pkg.ui.runtime.tsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import { UIRuntime } from '@teambit/ui';
+import { ConsumePlugin } from '@teambit/component/ui/menu';
+import { Install } from '@teambit/ui-foundation.ui.use-box.menu';
+import ComponentAspect, { ComponentUI } from '@teambit/component';
+import { PkgAspect } from './pkg.aspect';
+
+export class PkgUI {
+  static runtime = UIRuntime;
+
+  static dependencies = [ComponentAspect];
+
+  static async provider([componentUI]: [ComponentUI]) {
+    const npm = new PkgUI();
+    componentUI.registerConsumeMethod(npm.npmMethod);
+    return npm;
+  }
+
+  private npmMethod: ConsumePlugin = (comp) => {
+    const registry = comp.packageName.split('/')[0];
+    const packageVersion = comp.version === comp.latest ? '' : `@${comp.version}`;
+    return {
+      Title: <img style={{ width: '30px' }} src="http://static.bit.dev/brands/logo-npm-new.svg" />,
+      Component: (
+        <Install
+          config={`npm config set '${registry}:registry' https://node.bit.dev`}
+          componentName={comp.id.name}
+          packageManager="npm"
+          copyString={`npm i ${comp.packageName}${packageVersion}`}
+          registryName={registry}
+        />
+      ),
+      order: 10,
+    };
+  };
+}
+
+export default PkgUI;
+
+PkgAspect.addRuntime(PkgUI);
