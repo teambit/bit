17bcc5c051931dedb359707398ba899f0d2ab735
Zoltan Kochan <z@kochan.io>
2022-01-05 12:38:46 +0200

    feat: pass nodeLinker to Yarn

--- e2e/harmony/node-linker.e2e.ts
+++ e2e/harmony/node-linker.e2e.ts
@@ -36,4 +36,30 @@ describe('installing with non-default nodeLinker', function () {
       });
     });
   });
+  describe('using Yarn as a package manager', () => {
+    describe(`setting nodeLinker to "hoisted"`, () => {
+      before(() => {
+        helper.scopeHelper.reInitLocalScopeHarmony();
+        helper.extensions.bitJsonc.addKeyValToDependencyResolver('packageManager', `teambit.dependencies/yarn`);
+        helper.extensions.bitJsonc.addKeyValToDependencyResolver('nodeLinker', 'hoisted');
+        helper.command.install('is-positive');
+      });
+      it('should create a hoisted node_modules', function () {
+        const depPath = path.join(helper.fixtures.scopes.localPath, 'node_modules/is-positive');
+        expect(fs.realpathSync(depPath)).to.eq(depPath);
+      });
+    });
+    describe(`setting nodeLinker to "isolated"`, () => {
+      before(() => {
+        helper.scopeHelper.reInitLocalScopeHarmony();
+        helper.extensions.bitJsonc.addKeyValToDependencyResolver('packageManager', `teambit.dependencies/yarn`);
+        helper.extensions.bitJsonc.addKeyValToDependencyResolver('nodeLinker', 'isolated');
+        helper.command.install('is-positive');
+      });
+      it('should create a hoisted node_modules', function () {
+        const depPath = path.join(helper.fixtures.scopes.localPath, 'node_modules/is-positive');
+        expect(fs.realpathSync(depPath)).to.contain('.store');
+      });
+    });
+  });
 });
