d9288206df10529d8c1510d6388db6041af50f1c
Kutner JS <urikutner@gmail.com>
2022-01-16 13:24:54 +0200

    fix highlighter for forwardRef components (#5207)

--- scopes/react/dom-to-react/index.ts
+++ scopes/react/dom-to-react/index.ts
@@ -1,2 +1,4 @@
 export { domToReact, domToReacts, toRootElement } from './dom-to-react';
 export { domToFiber, toRootFiber, fiberToPrototype } from './dom-to-fiber';
+
+export type { ForwardRefInstance } from './fiber-node';
