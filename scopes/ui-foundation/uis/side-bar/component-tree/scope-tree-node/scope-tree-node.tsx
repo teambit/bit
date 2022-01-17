a9dad6c87f08581e72e689a2948d6673a63e33ae
oded <oded@bit.dev>
2022-01-17 17:38:22 +0200

    namespace and scope node

--- scopes/ui-foundation/uis/side-bar/component-tree/scope-tree-node/scope-tree-node.tsx
+++ scopes/ui-foundation/uis/side-bar/component-tree/scope-tree-node/scope-tree-node.tsx
@@ -18,8 +18,8 @@ export function ScopeTreeNode({ node, depth }: ScopeTreeNodeProps) {
   const isActive = activePath?.startsWith(node.id);
 
   const initialOpen = isActive || !isCollapsed;
-  // rename to open
-  const [collapsed, collapse] = useState<boolean | void>(!initialOpen);
+
+  const [open, toggle] = useState<boolean | void>(initialOpen);
 
   const displayName = getName(node.id.replace(/\/$/, ''));
 
@@ -27,36 +27,36 @@ export function ScopeTreeNode({ node, depth }: ScopeTreeNodeProps) {
   useEffect(() => {
     const current = firstRun.current;
     if (current) return;
-    if (isActive === true) return collapse(false);
+    if (isActive === true) return toggle(true);
   }, [isActive]);
 
   useEffect(() => {
     const current = firstRun.current;
     if (current) return;
-    collapse(isCollapsed);
+    toggle(!isCollapsed);
   }, [isCollapsed]);
 
   useEffect(() => {
     firstRun.current = false;
   }, []);
 
-  const highlighted = collapsed && isActive;
+  const highlighted = !open && isActive;
 
   return (
     <div>
       {node.id && (
         <div
           className={classNames(indentClass, styles.scope, highlighted && styles.highlighted)}
-          onClick={() => collapse(!collapsed)}
+          onClick={() => toggle(!open)}
         >
           <div className={styles.left}>
-            <Icon className={classNames(styles.arrow, collapsed && styles.collapsed)} of="fat-arrow-down" />
+            <Icon className={classNames(styles.arrow, !open && styles.collapsed)} of="fat-arrow-down" />
             <Icon className={styles.arrow} of="collection-full" />
             <span className={styles.name}>{displayName}</span>
           </div>
         </div>
       )}
-      <AnimateHeight height={collapsed ? 0 : 'auto'}>
+      <AnimateHeight height={open ? 'auto' : 0}>
         <div style={indentStyle(depth + 1)} className={classNames(styles.componentTree)}>
           {node.children && <TreeLayer childNodes={node.children} depth={depth + 1} />}
         </div>
