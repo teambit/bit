95473d637b804929e96b17cd62926c87535e3d43
oded <oded@bit.dev>
2022-01-18 12:44:15 +0200

    a

--- scopes/design/ui/tree/tree.spec.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import { BasicTree } from './tree.composition';
-
-it('should render with the correct text', () => {
-  const { getByText } = render(<BasicTree />);
-  const rendered = getByText('hello from Tree');
-  expect(rendered).toBeTruthy();
-});
