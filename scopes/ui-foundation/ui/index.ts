c4276ecb75d2796247f1caed1c96a8dd62385fb4
Kutner JS <urikutner@gmail.com>
2022-01-30 14:13:03 +0200

    Extract ssr logic to its own file (#5209)

--- scopes/ui-foundation/ui/index.ts
+++ scopes/ui-foundation/ui/index.ts
@@ -1,16 +1,20 @@
 import { UIAspect, UIRuntime } from './ui.aspect';
 
+export { UIAspect, UIRuntime, UIAspect as default };
+
 export * from './events';
 export { UIRoot, PostStartOptions, ProxyEntry } from './ui-root';
 export type { UiMain, PreStartOpts } from './ui.main.runtime';
-export type { UiUI, ContextProps } from './ui.ui.runtime';
-export { StartPlugin, StartPluginOptions } from './start-plugin';
-export type { RenderLifecycle } from './render-lifecycle';
-export { UIRootUI, UIRootFactory } from './ui-root.ui';
+export type { UiUI } from './ui.ui.runtime';
+export type { StartPlugin, StartPluginOptions } from './start-plugin';
+export type {
+  BrowserData,
+  RenderPlugins,
+  /** @deprecated - legacy name, use RenderPlugins */
+  RenderPlugins as RenderLifecycle,
+} from './react-ssr';
+export type { UIRootUI, UIRootFactory } from './ui-root.ui';
 export type { UIServer } from './ui-server';
-export { UIAspect, UIRuntime };
-export type { BrowserData } from './ssr/request-browser';
-export default UIAspect;
 
 // using `useDataQuery` from this package is deprecated, use `@teambit/ui-foundation.ui.hooks.use-data-query` directly
 export { DataQueryResult, useDataQuery } from '@teambit/ui-foundation.ui.hooks.use-data-query';
