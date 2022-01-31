b761a571448c00abc337006d8abc166da85375cd
Cameron Sechrist <cameron@bestreamlined.io>
2022-01-31 18:11:15 -0500

    [fix]: Remove some testing stuff

--- src/remotes/remote-resolver/remote-resolver.ts
+++ src/remotes/remote-resolver/remote-resolver.ts
@@ -36,7 +36,6 @@ const SCOPE_GET = gql`
 // comment this out once on production
 async function getScope(name: string) {
   if (scopeCache[name]) return scopeCache[name];
-  console.log(symphonyUrl);
   const token = getSync(CFG_USER_TOKEN_KEY);
   const authHeader = getSync(CFG_AUTH_HEADER);
   const headers = token ? getAuthHeader(token, authHeader) : {};
