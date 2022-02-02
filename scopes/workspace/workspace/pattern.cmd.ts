7f68e6ed01d907080d9d944fa4ba28581fb4c2d3
David First <davidnet@gmail.com>
2022-02-02 10:15:17 -0500

    feat: improve "pattern" to accept multiple patterns and negate syntax (#5346)
    - introduce `idsByPattern` API for the workspace that returns all matching component ids according to the pattern. A pattern may consist multiple patterns separated by a comma. Also, a pattern can exclude ids by the `!` syntax.
    - introduce `bit pattern <pattern>` command to list the ids matching the pattern.

---
+++ scopes/workspace/workspace/pattern.cmd.ts
@@ -0,0 +1,24 @@
+import { Command, CommandOptions } from '@teambit/cli';
+import chalk from 'chalk';
+import { Workspace } from './workspace';
+
+export class PatternCommand implements Command {
+  name = 'pattern <pattern>';
+  alias = '';
+  description = 'list the component ids matching the given pattern';
+  group = 'development';
+  private = false;
+  options = [['j', 'json', 'return the output as JSON']] as CommandOptions;
+
+  constructor(private workspace: Workspace) {}
+
+  async report([pattern]: [string]) {
+    const ids = await this.workspace.idsByPattern(pattern);
+    const title = chalk.green(`found ${chalk.bold(ids.length.toString())} components matching the pattern`);
+    return `${title}\n${ids.join('\n')}`;
+  }
+
+  async json([pattern]: [string]) {
+    return this.workspace.idsByPattern(pattern);
+  }
+}
