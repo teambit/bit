803b02d8f17b7ea66a3b90320d455e73598e52e8
Oded Reuveny <oded@bit.dev>
2022-02-20 15:31:57 +0200

    New component descriptor (#5397)

---
+++ scopes/component/component-descriptor/component-descriptor.ts
@@ -0,0 +1,58 @@
+import { ComponentID } from '@teambit/component';
+import { AspectListProps, AspectList } from './aspect-list';
+
+export type ComponentDescriptorProps = {
+  /**
+   * serialized component ID.
+   */
+  id: string;
+  /**
+   * aspects map data
+   */
+  aspectList?: AspectListProps;
+};
+
+export class ComponentDescriptor {
+  constructor(
+    /**
+     *  Component ID
+     */
+    readonly id: ComponentID,
+    /**
+     *  aspect map
+     */
+    readonly aspectList: AspectList
+  ) {}
+
+  get scope() {
+    return this.id.scope;
+  }
+
+  get<T>(aspectId: string): T | undefined {
+    return this.aspectList.get<T>(aspectId);
+  }
+
+  toObject(): ComponentDescriptorProps {
+    return {
+      id: this.id.toString(),
+      aspectList: this.aspectList.toObject(),
+    };
+  }
+
+  stringify() {
+    return JSON.stringify(this.toObject());
+  }
+
+  toString() {
+    return this.stringify();
+  }
+
+  static fromObject({ id, aspectList }: ComponentDescriptorProps) {
+    const aspects = AspectList.fromObject(aspectList);
+    return new ComponentDescriptor(ComponentID.fromString(id), aspects);
+  }
+
+  static fromArray(componentsDescriptorProps: ComponentDescriptorProps[]) {
+    return componentsDescriptorProps.map((component) => ComponentDescriptor.fromObject(component));
+  }
+}
