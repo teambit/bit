336406ee5271cbac300cccd94d0a1fa394c7849f
Uri Kutner <urikutner@gmail.com>
2022-02-23 19:48:58 +0200

    suggest users use bit to install in the error overlay

--- scopes/react/react/webpack/overlay/formatWebpackMessages.js
+++ scopes/react/react/webpack/overlay/formatWebpackMessages.js
@@ -81,7 +81,7 @@ function formatMessage(message) {
   // Add helpful message for users trying to use Sass for the first time
   if (lines[1] && lines[1].match(/Cannot find module.+sass/)) {
     lines[1] = 'To import Sass files, you first need to install sass.\n';
-    lines[1] += 'Run `npm install sass` or `yarn add sass` inside your workspace.';
+    lines[1] += 'Run `bit install sass` inside your workspace.';
   }
 
   message = lines.join('\n');
