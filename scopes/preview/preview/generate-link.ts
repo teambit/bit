f26e3b325f2b3a892c2f372ca72ee33876ccc6d4
Uri Kutner <urikutner@gmail.com>
2022-03-01 19:40:26 +0200

    esm link

--- scopes/preview/preview/generate-link.ts
+++ scopes/preview/preview/generate-link.ts
@@ -13,19 +13,40 @@ import { linkModules } from '${toWindowsCompatiblePath(require.resolve('./previe
 import harmony from '${toWindowsCompatiblePath(require.resolve('@teambit/harmony'))}';
 ${mainModule ? `import * as mainModule from '${toWindowsCompatiblePath(mainModule)}';` : 'const mainModule = {};'};
 
+${
+  // generate imports:
+  componentMap
+    .toArray()
+    .map(([, modulePath], compIdx) =>
+      modulePath
+        .map(
+          (path, pathIdx) => `import * as ${moduleVarName(compIdx, pathIdx)} from '${toWindowsCompatiblePath(path)}'`
+        )
+        .join('\n')
+    )
+    .join('\n')
+}
+
 linkModules('${prefix}', {
   mainModule,
   isSplitComponentBundle: ${isSplitComponentBundle},
   componentMap: {
-    ${componentMap
-      .toArray()
-      .map(([component, modulePaths]) => {
-        return `'${component.id.fullName}': [${modulePaths
-          .map((path) => `require('${toWindowsCompatiblePath(path)}')`)
-          .join(', ')}]`;
-      })
-      .join(',\n')}
+${
+  // use imports:
+  componentMap
+    .toArray()
+    .map(([component, modulePaths], compIdx) => {
+      return `    '${component.id.fullName}': [${modulePaths
+        .map((_, pathIdx) => moduleVarName(compIdx, pathIdx))
+        .join(', ')}]`;
+    })
+    .join(',\n')
+}
   }
 });
 `;
+}
+
+function moduleVarName(componentIdx: number, fileIdx: number) {
+  return `file_${componentIdx}_${fileIdx}`;
 }
