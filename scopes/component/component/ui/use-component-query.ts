15ea5841525911cede23be517ab7668d1c14b6c7
oded <oded@bit.dev>
2022-03-07 14:37:55 +0200

    updated comment

--- scopes/component/component/ui/use-component-query.ts
+++ scopes/component/component/ui/use-component-query.ts
@@ -21,9 +21,10 @@ const componentFields = gql`
       ...componentIdFields
     }
     aspects(include: ["teambit.preview/preview", "teambit.pipelines/builder"]) {
+      # 'id' property in gql refers to a *global* identifier and used for caching.
+      # this makes aspect data cache under the same key, even when they are under different components.
+      # renaming the property fixes that.
       aspectId: id
-      # TODO - check why aspectId doesnt work
-      # also - consider changing to data -> aspectData. we do it anyway in use-component-query
       aspectData: data
     }
     packageName
