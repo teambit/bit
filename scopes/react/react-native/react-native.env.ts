be17d5808e258f71f10f694e5a1332837b3ba838
bengil <benjyg@gmail.com>
2022-03-09 20:32:09 +0200

    sync rn deps from react

--- scopes/react/react-native/react-native.env.ts
+++ scopes/react/react-native/react-native.env.ts
@@ -19,6 +19,9 @@ export class ReactNativeEnv implements Environment, DependenciesEnv {
           '@types/react': '^17.0.8',
           '@types/react-dom': '^17.0.5',
           '@types/react-native': '^0.64.1',
+          // This is added as dev dep since our jest file transformer uses babel plugins that require this to be installed
+          '@babel/runtime': '7.12.18',
+          '@types/testing-library__jest-dom': '5.9.5',
         },
         peerDependencies: {
           react: '^16.8.0 || ^17.0.0',
