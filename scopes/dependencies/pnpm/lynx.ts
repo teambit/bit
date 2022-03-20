7d5ca08316b477a7878f1ea30a2e86b24047f795
Zoltan Kochan <z@kochan.io>
2022-03-20 15:45:21 +0200

    fix: workspace projects should be built in correct order (#5564)

--- scopes/dependencies/pnpm/lynx.ts
+++ scopes/dependencies/pnpm/lynx.ts
@@ -4,6 +4,7 @@ import defaultReporter from '@pnpm/default-reporter';
 import { streamParser } from '@pnpm/logger';
 import { StoreController, WantedDependency } from '@pnpm/package-store';
 import { createOrConnectStoreController, CreateStoreControllerOptions } from '@pnpm/store-connection-manager';
+import sortPackages from '@pnpm/sort-packages';
 import {
   ResolvedPackageVersion,
   Registries,
@@ -25,6 +26,7 @@ import pickRegistryForPackage from '@pnpm/pick-registry-for-package';
 import { ProjectManifest } from '@pnpm/types';
 import { Logger } from '@teambit/logger';
 import toNerfDart from 'nerf-dart';
+import pkgsGraph from 'pkgs-graph';
 import { readConfig } from './read-config';
 
 type RegistriesMap = {
@@ -151,7 +153,7 @@ export async function getPeerDependencyIssues(
 
 export async function install(
   rootManifest,
-  manifestsByPaths,
+  manifestsByPaths: Record<string, ProjectManifest>,
   storeDir: string,
   cacheDir: string,
   registries: Registries,
@@ -164,38 +166,9 @@ export async function install(
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   logger?: Logger
 ) {
-  const packagesToBuild: MutatedProject[] = []; // @pnpm/core will use this to install the packages
-  const workspacePackages = {}; // @pnpm/core will use this to link packages to each other
-
-  // This will create local link by pnpm to a component exists in the ws.
-  // it will later deleted by the link process
-  // we keep it here to better support case like this:
-  // compA@1.0.0 uses compB@1.0.0
-  // I have compB@2.0.0 in my workspace
-  // now I install compA@1.0.0
-  // compA is hoisted to the root and install B@1.0.0 hoisted to the root as well
-  // now we will make link to B@2.0.0 and A will break
-  // with this we will have a link to the local B by pnpm so it will install B@1.0.0 inside A
-  // then when overriding the link, A will still works
-  // This is the rational behind not deleting this completely, but need further check that it really works
-
-  // eslint-disable-next-line
-  for (const rootDir in manifestsByPaths) {
-    const manifest = manifestsByPaths[rootDir];
-    packagesToBuild.push({
-      buildIndex: 0, // workspace components should be installed before the root
-      manifest,
-      rootDir,
-      mutation: 'install',
-    });
-    workspacePackages[manifest.name] = workspacePackages[manifest.name] || {};
-    workspacePackages[manifest.name][manifest.version] = { dir: rootDir, manifest };
-  }
-  packagesToBuild.push({
-    buildIndex: 1, // install the root package after the workspace components were installed
-    manifest: rootManifest.manifest,
-    mutation: 'install',
-    rootDir: rootManifest.rootDir,
+  const { packagesToBuild, workspacePackages } = groupPkgs({
+    ...manifestsByPaths,
+    [rootManifest.rootDir]: rootManifest.manifest,
   });
   const registriesMap = getRegistriesMap(registries);
   const authConfig = getAuthConfig(registries);
@@ -235,6 +208,43 @@ export async function install(
   } finally {
     stopReporting();
   }
+}
+
+function groupPkgs(manifestsByPaths: Record<string, ProjectManifest>) {
+  const pkgs = Object.entries(manifestsByPaths).map(([dir, manifest]) => ({ dir, manifest }));
+  const { graph } = pkgsGraph(pkgs);
+  const chunks = sortPackages(graph as any);
+
+  // This will create local link by pnpm to a component exists in the ws.
+  // it will later deleted by the link process
+  // we keep it here to better support case like this:
+  // compA@1.0.0 uses compB@1.0.0
+  // I have compB@2.0.0 in my workspace
+  // now I install compA@1.0.0
+  // compA is hoisted to the root and install B@1.0.0 hoisted to the root as well
+  // now we will make link to B@2.0.0 and A will break
+  // with this we will have a link to the local B by pnpm so it will install B@1.0.0 inside A
+  // then when overriding the link, A will still works
+  // This is the rational behind not deleting this completely, but need further check that it really works
+  const packagesToBuild: MutatedProject[] = []; // @pnpm/core will use this to install the packages
+  const workspacePackages = {}; // @pnpm/core will use this to link packages to each other
+
+  chunks.forEach((dirs, buildIndex) => {
+    for (const rootDir of dirs) {
+      const manifest = manifestsByPaths[rootDir];
+      packagesToBuild.push({
+        buildIndex,
+        manifest,
+        rootDir,
+        mutation: 'install',
+      });
+      if (manifest.name) {
+        workspacePackages[manifest.name] = workspacePackages[manifest.name] || {};
+        workspacePackages[manifest.name][manifest.version] = { dir: rootDir, manifest };
+      }
+    }
+  });
+  return { packagesToBuild, workspacePackages };
 }
 
 export async function resolveRemoteVersion(
