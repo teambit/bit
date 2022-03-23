c855f65b9ea88949a639862bb5610337f5f6bfae
Luv Kapur <luv@bit.dev>
2022-03-23 14:05:33 -0400

    clean up

--- scopes/component/component-filters/deprecate-filter.tsx
+++ scopes/component/component-filters/deprecate-filter.tsx
@@ -8,7 +8,7 @@ export type DeprecateFilterCriteria = ComponentFilterCriteria<boolean>;
 
 export const DeprecateFilter: DeprecateFilterCriteria = {
   id: 'deprecate',
-  match: (component, state) => state || !component.deprecation?.isDeprecate,
+  match: (component, active) => active || !component.deprecation?.isDeprecate,
   state: false,
   order: 0,
   render: deprecateFilter,
