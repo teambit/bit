81b2e230c8849bfcad656a5984af4c9fa622a91e
Luv Kapur <luv@bit.dev>
2022-03-23 09:49:19 -0400

    clean up

--- scopes/component/component-drawer/component-drawer.context.tsx
+++ scopes/component/component-drawer/component-drawer.context.tsx
@@ -1,10 +1,7 @@
 import React, { createContext, ReactNode, useState, useContext } from 'react';
 import { SlotRegistry } from '@teambit/harmony';
-import { ComponentModel } from '@teambit/component';
 
 export type DrawerWidgetSlot = SlotRegistry<ReactNode[]>;
-export type DrawerComponentModel = { model: ComponentModel; isHidden?: boolean };
-
 export type ComponentTreeContextType = {
   collapsed: boolean;
   setCollapsed: (collapsed: boolean) => void;
@@ -40,37 +37,3 @@ export const ComponentFilterWidgetProvider = ({ children }: { children: ReactNod
     </ComponentFilterWidgetContext.Provider>
   );
 };
-
-// export type DrawerComponentsContextType = {
-//   components: DrawerComponentModel[];
-//   loading?: boolean;
-//   setLoading: (loading: boolean) => void;
-//   setComponents: (components: DrawerComponentModel[]) => void;
-// };
-
-// export const DrawerComponentsContext = createContext<DrawerComponentsContextType>({
-//   components: [],
-//   setComponents: () => {},
-//   setLoading: () => {},
-// });
-
-// export const DrawerComponentsProvider = ({
-//   children,
-//   components = [],
-//   loading = false,
-// }: {
-//   children: ReactNode;
-//   components: ComponentModel[];
-//   loading?: boolean;
-// }) => {
-//   const { matches, filters } = useContext(ComponentFilterContext);
-//   const [loadingState, setLoadingState] = useState<boolean>(loading);
-//   const [filteredComponents, setComponents] = useState<DrawerComponentModel[]>(matches(filters, components));
-//   return (
-//     <DrawerComponentsContext.Provider
-//       value={{ components: filteredComponents, setComponents, loading: loadingState, setLoading: setLoadingState }}
-//     >
-//       {children}
-//     </DrawerComponentsContext.Provider>
-//   );
-// };
