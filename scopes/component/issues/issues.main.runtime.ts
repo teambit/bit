a3afbf25fc51624a19cf804da318a2eca6aa9906
David First <davidnet@gmail.com>
2022-03-24 15:46:16 -0400

    add ability to ignore certain issues from bit-status

--- scopes/component/issues/issues.main.runtime.ts
+++ scopes/component/issues/issues.main.runtime.ts
@@ -1,9 +1,29 @@
+import { BitError } from '@teambit/bit-error';
 import { CLIAspect, CLIMain, MainRuntime } from '@teambit/cli';
 import { IssuesClasses, IssuesList } from '@teambit/component-issues';
 import { ComponentIssuesCmd } from './issues-cmd';
 import { IssuesAspect } from './issues.aspect';
 
+export type IssuesConfig = {
+  ignoreIssues: string[];
+};
+
 export class IssuesMain {
+  constructor(private config: IssuesConfig) {}
+
+  getIssuesToIgnore(): string[] {
+    const allIssues = this.listIssues().map((issue) => issue.name);
+    const issuesToIgnore = this.config.ignoreIssues || [];
+    issuesToIgnore.forEach((issueToIgnore) => {
+      if (!allIssues.includes(issueToIgnore)) {
+        throw new BitError(
+          `fatal: a non-existing component-issue "${issueToIgnore}" was configured for ${IssuesAspect.id} aspect`
+        );
+      }
+    });
+    return issuesToIgnore;
+  }
+
   listIssues() {
     const instances = Object.keys(IssuesClasses).map((issueClass) => new IssuesClasses[issueClass]());
     const issuesList = new IssuesList(instances);
@@ -24,8 +44,8 @@ export class IssuesMain {
     ignoreIssues: [],
   };
   static runtime = MainRuntime;
-  static async provider([cli]: [CLIMain]) {
-    const issuesMain = new IssuesMain();
+  static async provider([cli]: [CLIMain], config: IssuesConfig) {
+    const issuesMain = new IssuesMain(config);
     cli.register(new ComponentIssuesCmd(issuesMain));
     return issuesMain;
   }
