1de57451f8f14845b119ab3691b822cbf1a128ee
David First <davidnet@gmail.com>
2022-03-25 11:25:58 -0400

    fix insights aspect to have the modern aspect foramt

--- scopes/explorer/insights/insights.main.runtime.ts
+++ scopes/explorer/insights/insights.main.runtime.ts
@@ -1,17 +1,31 @@
-import { CLIAspect, MainRuntime } from '@teambit/cli';
-import { GraphAspect } from '@teambit/graph';
-
-import { provide } from './insight.provider';
+import { CLIAspect, MainRuntime, CLIMain } from '@teambit/cli';
+import { GraphAspect, GraphBuilder } from '@teambit/graph';
 import { InsightsAspect } from './insights.aspect';
+import getCoreInsights from './core-insights-getter';
+import { Insight } from './insight';
+import { InsightManager } from './insight-manager';
+import InsightsCmd from './insights.cmd';
 
-export const InsightsMain = {
-  runtime: MainRuntime,
-  name: 'insights',
-  dependencies: [GraphAspect, CLIAspect],
-  config: {
+export type InsightDeps = [GraphBuilder, CLIMain];
+
+export class InsightsMain {
+  static slots = [];
+  static dependencies = [GraphAspect, CLIAspect];
+  static runtime = MainRuntime;
+  static config = {
     silence: false,
-  },
-  provider: provide,
-};
+  };
+  static async provider([graphBuilder, cli]: InsightDeps) {
+    const insightsMain = new InsightsMain();
+    // get all insights from registry
+    const initialInsights: Insight[] = getCoreInsights(graphBuilder);
+    // register all insights in cli
+    // TODO - get user-defined insights as well, and use them when instantiating InsightManager and InsightsCmd
+    const insightManager = new InsightManager(initialInsights);
+    const insightsCmd = new InsightsCmd(insightManager);
+    cli.register(insightsCmd);
+    return insightsMain;
+  }
+}
 
 InsightsAspect.addRuntime(InsightsMain);
