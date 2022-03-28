ac485c15f3520ba242c470645c1b21482ae15bff
Gilad Shoham <shoham.gilad@gmail.com>
2022-03-28 13:06:31 +0300

    use terser esbuild in order to minify code during build/tag (#5612)
    - using esbuild for code minify, instead of default terser.
    - improve build performance dramatically
    - do not minify identifiers by default
    - minify identifiers for react apps
    - reduce memory consumption during build/tag

--- scopes/react/react/apps/web/react.application.ts
+++ scopes/react/react/apps/web/react.application.ts
@@ -2,6 +2,8 @@ import { join, basename } from 'path';
 import { Application, AppContext, AppBuildContext } from '@teambit/application';
 import { Bundler, DevServer, BundlerContext, DevServerContext, BundlerHtmlConfig } from '@teambit/bundler';
 import { Port } from '@teambit/toolbox.network.get-port';
+import { remove } from 'lodash';
+import TerserPlugin from 'terser-webpack-plugin';
 import { WebpackConfigTransformer } from '@teambit/webpack';
 import { ReactEnv } from '../../react.env';
 import { prerenderSPAPlugin } from './plugins';
@@ -101,6 +103,21 @@ export class ReactApp implements Application {
     const defaultTransformer: WebpackConfigTransformer = (configMutator) => {
       const config = configMutator.addTopLevel('output', { path: staticDir, publicPath: `/` });
       if (this.prerenderRoutes) config.addPlugin(prerenderSPAPlugin(this.prerenderRoutes, staticDir));
+      if (config.raw.optimization?.minimizer) {
+        remove(config.raw.optimization?.minimizer, (minimizer) => {
+          return minimizer.constructor.name === 'TerserPlugin';
+        });
+        config.raw.optimization?.minimizer.push(
+          new TerserPlugin({
+            minify: TerserPlugin.esbuildMinify,
+            // `terserOptions` options will be passed to `esbuild`
+            // Link to options - https://esbuild.github.io/api/#minify
+            terserOptions: {
+              minify: true,
+            },
+          })
+        );
+      }
       return config;
     };
     const transformers = [defaultTransformer, ...(this.transformers ?? [])];
