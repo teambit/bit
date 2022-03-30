04f1c6cc3c3c1b6c11d29a125f196b796cb7ff2d
David First <davidnet@gmail.com>
2022-03-29 23:11:46 -0400

    feat: introduce a new component-issue CircularDependencies (#5606)
    - show a warning in `bit status` when components have circular dependencies.
    - block `bit tag` when components have circular dependencies. (can be bypassed by `--ignore-issues` flag or config).

--- scopes/explorer/insights/insights.cmd.ts
+++ scopes/explorer/insights/insights.cmd.ts
@@ -1,10 +1,10 @@
 import chalk from 'chalk';
 import { Command, CommandOptions } from '@teambit/cli';
 import { InsightResult } from './insight';
-import { InsightManager, RunInsightOptions } from './insight-manager';
+import { InsightsMain } from './insights.main.runtime';
 
 export default class InsightsCmd implements Command {
-  name = 'insights [...names]';
+  name = 'insights [names...]';
   description = 'Insights on component graph';
   group = 'development';
   private = true;
@@ -12,37 +12,23 @@ export default class InsightsCmd implements Command {
     ['l', 'list', 'list all insights'],
     ['j', 'json', 'return the insights in json format'],
   ] as CommandOptions;
-  insightManager: InsightManager;
-  constructor(insightManager: InsightManager) {
-    this.insightManager = insightManager;
-  }
+  constructor(private insights: InsightsMain) {}
 
-  async report(names: [string[]], options: { list: boolean }): Promise<string> {
+  async report([names]: [string[]], options: { list: boolean }): Promise<string> {
     if (options.list) {
-      const results = await this.json(names, options);
+      const results = await this.json([names], options);
       return JSON.stringify(results, null, 2);
     }
-    const results = await this.runInsights(names, { renderData: true });
+    const results = await this.insights.runInsights(names, { renderData: true });
     return template(results);
   }
 
-  async json(names: [string[]], { list }: { list: boolean }) {
+  async json([names]: [string[]], { list }: { list: boolean }) {
     if (list) {
-      const results = this.insightManager.listInsights();
+      const results = this.insights.listInsights();
       return results;
     }
-    return this.runInsights(names, { renderData: false });
-  }
-
-  private async runInsights([names]: [string[]], opts: RunInsightOptions) {
-    if (names) {
-      let results: InsightResult[] = [];
-      const namesArr = typeof names === 'string' ? [names] : names;
-      results = await this.insightManager.run(namesArr, opts);
-      return results;
-    }
-    const results = await this.insightManager.runAll(opts);
-    return results;
+    return this.insights.runInsights(names, { renderData: false });
   }
 }
 
