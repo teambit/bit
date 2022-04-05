db7b283c36998e25e0912b9ddb4ecf55adcdc7e6
Uri Kutner <urikutner@gmail.com>
2022-04-05 18:24:31 +0300

    eject said components

--- scopes/react/component-highlighter/hover-highlighter/index.ts
+++
@@ -1,5 +0,0 @@
-export { useHoverHighlighter } from './use-hover-highlighter';
-export type { useHoverHighlighterOptions } from './use-hover-highlighter';
-
-export { HoverHighlighter } from './hover-highlighter';
-export type { HoverHighlighterProps } from './hover-highlighter';
