9c560c0d77b9d7d9e6b15705f799c1d5acfd64ce
Luv Kapur <luv@bit.dev>
2022-04-06 15:48:05 -0400

    sync bitmap with workspace lanes on merge

--- scopes/lanes/lanes/lanes.main.runtime.ts
+++ scopes/lanes/lanes/lanes.main.runtime.ts
@@ -156,7 +156,7 @@ export class LanesMain {
       ...options,
     });
     await this.workspace.consumer.onDestroy();
-
+    await this.workspace.consumer.bitMap.syncWithLanes();
     return mergeResults;
   }
 
