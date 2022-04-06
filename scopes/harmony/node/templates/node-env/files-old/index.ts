4eceac55e0a0b8a1a269040c4471d11cc49beb17
bengil <benjyg@gmail.com>
2022-04-06 14:44:07 +0300

    remove previous node-env template

--- scopes/harmony/node/templates/node-env/files-old/index.ts
+++
@@ -1,8 +0,0 @@
-import { ComponentContext } from '@teambit/generator';
-
-export function indexFile({ namePascalCase: Name, name }: ComponentContext) {
-  return `import { ${Name}Extension } from './${name}.extension';
-export { ${Name}Extension };
-export default ${Name}Extension;
-`;
-}
