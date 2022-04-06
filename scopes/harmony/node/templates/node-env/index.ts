37fbbb1c562d67a3824f1524a584db8221ad068c
bengil <benjyg@gmail.com>
2022-04-06 14:40:12 +0300

    update node env to the new aspect format

--- scopes/harmony/node/templates/node-env/index.ts
+++ scopes/harmony/node/templates/node-env/index.ts
@@ -1,9 +1,16 @@
 import { ComponentContext, ComponentTemplate } from '@teambit/generator';
 import { indexFile } from './files/index';
 import { docFile } from './files/doc';
-import { extensionFile } from './files/extension';
+import { mainRuntimeFile } from './files/main.runtime';
+import { previewRuntimeFile } from './files/preview.runtime';
+import { aspectFile } from './files/aspect';
+import { webpackConfigFile } from './files/webpack.config';
+import { typescriptConfigFile } from './files/typescript/typescript.config';
+import { globalStylesFile } from './files/typescript/styles';
+import { tsTransformerFile } from './files/typescript/ts-transformer';
+import { jestConfigFile } from './files/jest.config';
 
-export const nodeEnvTemplate: ComponentTemplate = {
+export const reactEnvTemplate: ComponentTemplate = {
   name: 'node-env',
   description: 'customize the base Node env with your configs and tools',
   generateFiles: (context: ComponentContext) => {
@@ -15,11 +22,39 @@ export const nodeEnvTemplate: ComponentTemplate = {
       },
       {
         relativePath: `${context.name}.docs.mdx`,
-        content: docFile(),
+        content: docFile(context),
       },
       {
-        relativePath: `${context.name}.extension.ts`,
-        content: extensionFile(context),
+        relativePath: `${context.name}.main.runtime.ts`,
+        content: mainRuntimeFile(context),
+      },
+      {
+        relativePath: `${context.name}.preview.runtime.ts`,
+        content: previewRuntimeFile(context),
+      },
+      {
+        relativePath: `${context.name}.aspect.ts`,
+        content: aspectFile(context),
+      },
+      {
+        relativePath: `webpack/webpack-transformers.ts`,
+        content: webpackConfigFile(),
+      },
+      {
+        relativePath: `typescript/tsconfig.json`,
+        content: typescriptConfigFile(),
+      },
+      {
+        relativePath: `typescript/styles.d.ts`,
+        content: globalStylesFile(),
+      },
+      {
+        relativePath: `typescript/ts-transformer.ts`,
+        content: tsTransformerFile(),
+      },
+      {
+        relativePath: `jest/jest.config.js`,
+        content: jestConfigFile(),
       },
     ];
   },
