6011873b0992de38df4c13d3743b05f48c6163c4
David First <davidnet@gmail.com>
2022-04-08 10:46:19 -0400

    refactor: rename the "cyclic dependencies" insight to "circular dependencies" to be consistent with the component-issue

--- scopes/component/component-issues/circular-dependencies.ts
+++ scopes/component/component-issues/circular-dependencies.ts
@@ -1,8 +1,9 @@
+import { INSIGHT_CIRCULAR_DEPS_NAME } from '@teambit/insights';
 import { ComponentIssue, formatTitle } from './component-issue';
 
 export class CircularDependencies extends ComponentIssue {
   description = 'circular dependencies';
-  solution = 'run `bit insights "cyclic dependencies"` to get the component-ids participating in the circular';
+  solution = `run \`bit insights "${INSIGHT_CIRCULAR_DEPS_NAME}"\` to get the component-ids participating in the circular`;
   data: boolean;
   isTagBlocker = true;
   outputForCLI() {
