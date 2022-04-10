b0796b0d852351e5e5a5e9368cebb4e83c7c2a3e
Kutner JS <urikutner@gmail.com>
2022-04-10 15:33:42 +0300

    remove - command bar context (#5677)

--- scopes/explorer/command-bar/index.ts
+++ scopes/explorer/command-bar/index.ts
@@ -1,7 +1,6 @@
 export { CommandBarAspect, CommandBarAspect as default } from './command-bar.aspect';
 export { commandBarCommands } from './command-bar.commands';
 export { ComponentSearcher } from './searchers';
-export { CommandBarContext } from './ui/command-bar-context';
 export type { ComponentResultPlugin, SearchProvider } from './searchers';
 export type { CommandBarUI, CommandEntry } from './command-bar.ui.runtime';
 export type { CommandHandler, CommandId, Keybinding } from './types';
