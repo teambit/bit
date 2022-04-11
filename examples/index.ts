c14ed0e1bc7778e225952f89d32c567b175d16e7
Luv Kapur <luv@bit.dev>
2022-04-11 16:18:04 -0400

    clean up

--- examples/index.ts
+++
@@ -1,1 +0,0 @@
-export { default as MdxExample, default } from './mdx-example.mdx';
--- examples/mdx-example.composition.tsx
+++
@@ -1,4 +0,0 @@
-import React from 'react';
-import { MdxExample } from './index';
-
-export const BasicMdxExample = () => <MdxExample />;
