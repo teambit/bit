06ef8ad7feeb32c9574e64709578b1fcf522dd1b
David First <davidnet@gmail.com>
2022-04-27 21:05:06 -0400

    fix types

--- scopes/component/component-issues/circular-dependencies.ts
+++ scopes/component/component-issues/circular-dependencies.ts
@@ -1,9 +1,8 @@
-import { INSIGHT_CIRCULAR_DEPS_NAME } from '@teambit/insights';
 import { ComponentIssue, formatTitle } from './component-issue';
 
 export class CircularDependencies extends ComponentIssue {
   description = 'circular dependencies';
-  solution = `run \`bit insights "${INSIGHT_CIRCULAR_DEPS_NAME}"\` to get the component-ids participating in the circular`;
+  solution = 'run `bit insights "circular dependencies"` to get the component-ids participating in the circular';
   data: boolean;
   isTagBlocker = true;
   outputForCLI() {
