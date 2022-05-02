ca651f58d535f1b3cde2544a10ceea4b8b6451c5
Luv Kapur <luv@bit.dev>
2022-05-02 12:14:18 -0400

    remove unused export

--- scopes/component/component/ui/index.ts
+++ scopes/component/component/ui/index.ts
@@ -1,5 +1,5 @@
 export { Component } from './component';
 export { ComponentModel, ComponentModelProps } from './component-model';
-export { ComponentContext, ComponentProvider, ComponentDescriptorProvider } from './context';
+export { ComponentContext, ComponentProvider } from './context';
 export { useComponent } from './use-component';
 export { componentIdFields, componentOverviewFields, componentFields } from './use-component-query';
