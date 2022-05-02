56041d550a4465deab2b6e8c5cdd385caa5a0810
Luv Kapur <luv@bit.dev>
2022-05-02 12:01:48 -0400

    add missing lanes details styles

--- scopes/lanes/ui/overview/lanes-overview.tsx
+++ scopes/lanes/ui/overview/lanes-overview.tsx
@@ -1,23 +1,31 @@
 import React, { useMemo, ComponentType } from 'react';
-import { LaneDetails, useLanesContext, useLaneComponents, LaneModel, LanesModel } from '@teambit/lanes.ui.lanes';
+import {
+  LaneDetails,
+  useLanesContext,
+  useLaneComponentsQuery,
+  LaneModel,
+  LanesModel,
+  LanesHost,
+} from '@teambit/lanes.ui.lanes';
 import { ComponentGrid } from '@teambit/explorer.ui.gallery.component-grid';
 import { RouteSlot, SlotSubRouter } from '@teambit/ui-foundation.ui.react-router.slot-router';
 import { WorkspaceComponentCard } from '@teambit/workspace.ui.workspace-component-card';
 import flatten from 'lodash.flatten';
 import { SlotRegistry } from '@teambit/harmony';
+import { ScopeComponentCard } from '@teambit/scope';
 import { EmptyLane } from './empty-lane-overview';
 
-import styles from './lane-gallery.module.scss';
+import styles from './lanes-overview.module.scss';
 
 export type LaneOverviewLine = ComponentType;
 export type LaneOverviewLineSlot = SlotRegistry<LaneOverviewLine[]>;
 
-export type LaneGalleryProps = {
+export type LanesOverviewProps = {
   routeSlot: RouteSlot;
   overviewSlot?: LaneOverviewLineSlot;
+  host: LanesHost;
 };
-
-export function LaneGallery({ routeSlot, overviewSlot }: LaneGalleryProps) {
+export function LanesOverview({ routeSlot, overviewSlot, host }: LanesOverviewProps) {
   const lanesContext = useLanesContext();
   const overviewItems = useMemo(() => flatten(overviewSlot?.values()), [overviewSlot]);
 
@@ -26,38 +34,49 @@ export function LaneGallery({ routeSlot, overviewSlot }: LaneGalleryProps) {
   if (!currentLane || !currentLane.id) return null;
   if (currentLane.components.length === 0) return <EmptyLane name={currentLane.name} />;
 
-  return <LaneGalleryWithPreview currentLane={currentLane} overviewItems={overviewItems} routeSlot={routeSlot} />;
+  return (
+    <LaneOverviewWithPreview
+      host={host}
+      currentLane={currentLane}
+      overviewItems={overviewItems}
+      routeSlot={routeSlot}
+    />
+  );
 }
 
 type LaneOverviewWithPreviewProps = {
   currentLane: LaneModel;
   overviewItems: LaneOverviewLine[];
   routeSlot: RouteSlot;
+  host: LanesHost;
 };
 
-function LaneGalleryWithPreview({ currentLane, overviewItems, routeSlot }: LaneOverviewWithPreviewProps) {
-  const { loading, components } = useLaneComponents(currentLane);
+function LaneOverviewWithPreview({ currentLane, overviewItems, routeSlot, host }: LaneOverviewWithPreviewProps) {
+  const { loading, components } = useLaneComponentsQuery(currentLane);
 
   if (loading) return null;
+
+  const ComponentCard =
+    host === 'workspace'
+      ? ({ component }) => (
+          <WorkspaceComponentCard
+            component={component}
+            componentUrl={LanesModel.getLaneComponentUrl(component.id, currentLane.id)}
+          />
+        )
+      : ({ component }) => <ScopeComponentCard component={component} />;
 
   return (
     <div className={styles.container}>
       <LaneDetails
-        className={styles.laneDetails}
         laneName={currentLane.id}
         description={''}
         componentCount={currentLane.components.length}
       ></LaneDetails>
       <ComponentGrid>
-        {components?.map((component, index) => {
-          return (
-            <WorkspaceComponentCard
-              key={index}
-              component={component}
-              componentUrl={LanesModel.getLaneComponentUrl(component.id, currentLane.id)}
-            />
-          );
-        })}
+        {components?.map((component, index) => (
+          <ComponentCard component={component} key={index} />
+        ))}
       </ComponentGrid>
       {routeSlot && <SlotSubRouter slot={routeSlot} />}
       {overviewItems.length > 0 && overviewItems.map((Item, index) => <Item key={index} />)}
