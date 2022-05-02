fa918efb092a77f110e40ceaafdc870210e2d350
David First <davidnet@gmail.com>
2022-05-02 17:19:47 -0400

    fix types for composition

--- scopes/semantics/entities/semantic-schema/schemas/module.ts
+++ scopes/semantics/entities/semantic-schema/schemas/module.ts
@@ -1,8 +1,13 @@
 import { SchemaNode } from '../schema-node';
+import { Export } from '../schemas';
 
 export class Module implements SchemaNode {
   namespace?: string;
   constructor(readonly exports: SchemaNode[]) {}
+
+  getExportSchemas(): Export[] {
+    return this.exports.filter((e) => e instanceof Export) as Export[];
+  }
 
   toObject(): Record<string, any> {
     return this.exports.map((exp) => exp.toObject());
