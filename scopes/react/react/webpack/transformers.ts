2e5fecc3606314f3f15f0b7f8a8eaf641c3fe200
Gilad Shoham <shoham.gilad@gmail.com>
2022-05-11 17:15:14 +0300

    add $ for all keys in the generated webpack peers aliases

--- scopes/react/react/webpack/transformers.ts
+++ scopes/react/react/webpack/transformers.ts
@@ -2,7 +2,8 @@ import { WebpackConfigMutator } from '@teambit/webpack.modules.config-mutator';
 
 export function generateAddAliasesFromPeersTransformer(peers: string[]) {
   const peerAliases = peers.reduce((acc, peerName) => {
-    acc[peerName] = require.resolve(peerName);
+    const keyName = `${peerName}$`;
+    acc[keyName] = require.resolve(peerName);
     return acc;
   }, {});
   return (config: WebpackConfigMutator): WebpackConfigMutator => {
