2adc554a254dac98f1ab7ecc77fcdba136c23195
Gilad Shoham <shoham.gilad@gmail.com>
2022-05-12 12:33:00 +0300

    Update scopes/react/react/webpack/transformers.ts
    Co-authored-by: Kutner JS <urikutner@gmail.com>

--- scopes/react/react/webpack/transformers.ts
+++ scopes/react/react/webpack/transformers.ts
@@ -3,12 +3,11 @@ import { WebpackConfigMutator } from '@teambit/webpack.modules.config-mutator';
 
 export function generateAddAliasesFromPeersTransformer(peers: string[]) {
   const peerAliases = peers.reduce((acc, peerName) => {
-    const keyName = `${peerName}`;
     // Get the dir of the package, to support cases like import react-dom/test-utils
     // in case we just do require.resolve('react=dom') here it wil be resolved to something like
     // node_modules/react-dom/index.js/test-utils which won't work
     // once resolve to the folder it will resolve correctly
-    acc[keyName] = getResolvedDirOrFile(peerName);
+    acc[peerName] = getResolvedDirOrFile(peerName);
     return acc;
   }, {});
   return (config: WebpackConfigMutator): WebpackConfigMutator => {
