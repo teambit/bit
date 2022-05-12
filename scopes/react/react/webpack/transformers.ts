52ae94e8f4acab2a9c48e67569eaf0f46317a1a2
Gilad Shoham <shoham.gilad@gmail.com>
2022-05-12 12:50:21 +0300

    log unresolved aliases

--- scopes/react/react/webpack/transformers.ts
+++ scopes/react/react/webpack/transformers.ts
@@ -1,13 +1,17 @@
 import findRoot from 'find-root';
 import { WebpackConfigMutator } from '@teambit/webpack.modules.config-mutator';
+import { Logger } from '@teambit/logger';
 
-export function generateAddAliasesFromPeersTransformer(peers: string[]) {
+export function generateAddAliasesFromPeersTransformer(peers: string[], logger: Logger) {
   const peerAliases = peers.reduce((acc, peerName) => {
     // Get the dir of the package, to support cases like import react-dom/test-utils
     // in case we just do require.resolve('react=dom') here it wil be resolved to something like
     // node_modules/react-dom/index.js/test-utils which won't work
     // once resolve to the folder it will resolve correctly
-    acc[peerName] = getResolvedDirOrFile(peerName);
+    const resolved = getResolvedDirOrFile(peerName, logger);
+    if (resolved) {
+      acc[peerName] = resolved;
+    }
     return acc;
   }, {});
   return (config: WebpackConfigMutator): WebpackConfigMutator => {
@@ -21,12 +25,18 @@ export function generateAddAliasesFromPeersTransformer(peers: string[]) {
  * @param peerName
  * @returns
  */
-function getResolvedDirOrFile(peerName: string): string {
-  const resolved = require.resolve(peerName);
+function getResolvedDirOrFile(peerName: string, logger: Logger): string | undefined {
+  let resolved;
   try {
+    resolved = require.resolve(peerName);
     const folder = findRoot(resolved);
     return folder;
   } catch (e) {
+    if (resolved) {
+      logger.warn(`Couldn't find root dir for "${peerName}" from path "${resolved}" to add it as webpack alias`);
+    } else {
+      logger.warn(`Couldn't resolve "${peerName}" to add it as webpack alias`);
+    }
     return resolved;
   }
 }
