f73da4382f840d83cb4fc919bbb254d6861e84f0
Uri Kutner <urikutner@gmail.com>
2022-05-17 17:26:58 +0300

    fix

--- scopes/react/ui/docs-app/index.tsx
+++ scopes/react/ui/docs-app/index.tsx
@@ -2,3 +2,4 @@ export { DocsApp } from './docs-app';
 export type { DocsAppProps } from './docs-app';
 
 export { default } from './docs.app-root';
+export type { ReactDocsRootParams } from './docs.app-root';
