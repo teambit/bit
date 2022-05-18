053c2c5148ce9e9e0d3dcdb1a46845fb72ca9005
Bhanu Bandaranayake <bhanu@bit.dev>
2022-05-18 12:15:35 -0400

    chore: add side-by-side view

--- scopes/component/ui/component-compare-composition/component-compare-composition.tsx
+++ scopes/component/ui/component-compare-composition/component-compare-composition.tsx
@@ -1,9 +1,20 @@
+import classNames from "classnames";
 import React from "react";
+import styles from "./component-compare-composition.module.scss";
 
 export function ComponentCompareComposition() {
     return (
-        <div>
-            <h1>Hello Component Composition Compare</h1>
+        <div className={styles.mainContainer}>
+            <div className={styles.subContainer}>
+                <div className={classNames([styles.subView, styles.leftView])}>
+                    <h1>Hello Component Composition Compare</h1>
+                </div>
+            </div>
+            <div className={styles.subContainer}>
+                <div className={classNames([styles.subView, styles.rightView])}>
+                    <h1>Hello Component Composition Compare</h1>
+                </div>
+            </div>
         </div>
     );
 }
