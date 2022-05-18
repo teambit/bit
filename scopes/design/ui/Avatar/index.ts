c9cfaec968e5690424304c287b067910ed1e1577
Josh Kuttler <yoshuakuttler@gmail.com>
2022-05-18 16:21:54 +0300

    Remove avatar and owner-avatar from the repo, there are managed in Design

--- scopes/design/ui/Avatar/index.ts
+++
@@ -1,6 +0,0 @@
-export * from './avatar';
-export { UserAvatar } from './user-avatar';
-export type { UserAvatarProps } from './user-avatar';
-export { OrgAvatar } from './org-avatar';
-export type { OrgAvatarProps } from './org-avatar';
-export { DefaultAvatar } from './default-avatar';
