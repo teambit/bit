4d25d3c977014bda64247ca8b4c985e8f311ba97
Bhanu Bandaranayake <bhanu@bit.dev>
2022-05-19 13:04:22 -0400

    prettified

--- scopes/component/ui/component-compare-composition/component-compare-composition.tsx
+++ scopes/component/ui/component-compare-composition/component-compare-composition.tsx
@@ -1,81 +1,101 @@
-import { useComponentCompareContext, useComponentCompareParams, getComponentCompareUrl } from "@teambit/component.ui.component-compare";
-import { Composition, CompositionContent } from "@teambit/compositions";
-import { CompositionContextProvider } from "@teambit/compositions.ui.hooks.use-composition";
-import classNames from "classnames";
+import {
+  useComponentCompareContext,
+  useComponentCompareParams,
+  getComponentCompareUrl,
+} from '@teambit/component.ui.component-compare';
+import { Composition, CompositionContent } from '@teambit/compositions';
+import { CompositionContextProvider } from '@teambit/compositions.ui.hooks.use-composition';
+import classNames from 'classnames';
 import queryString from 'query-string';
-import React, { useMemo, useState } from "react";
-import styles from "./component-compare-composition.module.scss";
-import { CompositionDropdown } from "./composition-dropdown";
+import React, { useMemo, useState } from 'react';
+import styles from './component-compare-composition.module.scss';
+import { CompositionDropdown } from './composition-dropdown';
 
 export function ComponentCompareComposition() {
-    const component = useComponentCompareContext();
+  const component = useComponentCompareContext();
 
-    if (component === undefined) {
-        return <></>;
-    }
+  if (component === undefined) {
+    return <></>;
+  }
 
-    const { loading, base, compare } = component;
-    const baseCompositions = base.compositions;
-    const compareCompositions = compare.compositions;
-    const { componentId, ...params } = useComponentCompareParams();
+  const { loading, base, compare } = component;
+  const baseCompositions = base.compositions;
+  const compareCompositions = compare.compositions;
+  const { componentId, ...params } = useComponentCompareParams();
 
-    // compositionBase -> filename
+  // compositionBase -> filename
 
-    const [selectedBaseComp, setSelectedBaseComp] = useState<Composition>(
-        params.selectedCompositionBaseFile && baseCompositions.find(c => c.identifier === params.selectedCompositionBaseFile) || baseCompositions[0]);
-    const [selectedCompareComp, setSelectedCompareComp] = useState<Composition>(
-        params.selectedCompositionCompareFile && compareCompositions.find(c => c.identifier === params.selectedCompositionCompareFile) || compareCompositions[0]);
+  const [selectedBaseComp, setSelectedBaseComp] = useState<Composition>(
+    (params.selectedCompositionBaseFile &&
+      baseCompositions.find((c) => c.identifier === params.selectedCompositionBaseFile)) ||
+      baseCompositions[0]
+  );
+  const [selectedCompareComp, setSelectedCompareComp] = useState<Composition>(
+    (params.selectedCompositionCompareFile &&
+      compareCompositions.find((c) => c.identifier === params.selectedCompositionCompareFile)) ||
+      compareCompositions[0]
+  );
 
-    const href = getComponentCompareUrl({ ...params, selectedCompositionBaseFile: selectedBaseComp.identifier, selectedCompositionCompareFile: selectedBaseComp.identifier });
-    const baseCompositionDropdownSource = baseCompositions.map(c => ({ label: c.displayName, value: href }));
-    const compareCompositionDropdownSource = compareCompositions.map(c => ({ label: c.displayName, value: href }));
+  const href = getComponentCompareUrl({
+    ...params,
+    selectedCompositionBaseFile: selectedBaseComp.identifier,
+    selectedCompositionCompareFile: selectedBaseComp.identifier,
+  });
+  const baseCompositionDropdownSource = baseCompositions.map((c) => ({ label: c.displayName, value: href }));
+  const compareCompositionDropdownSource = compareCompositions.map((c) => ({ label: c.displayName, value: href }));
 
-    const [baseCompositionParams, setBaseCompositionParams] = useState<Record<string, any>>({});
-    const baseCompQueryParams = useMemo(() => queryString.stringify(baseCompositionParams), [baseCompositionParams]);
+  const [baseCompositionParams, setBaseCompositionParams] = useState<Record<string, any>>({});
+  const baseCompQueryParams = useMemo(() => queryString.stringify(baseCompositionParams), [baseCompositionParams]);
 
-    const [compareCompositionParams, setCompareCompositionParams] = useState<Record<string, any>>({});
-    const compareCompQueryParams = useMemo(() => queryString.stringify(compareCompositionParams), [compareCompositionParams]);
+  const [compareCompositionParams, setCompareCompositionParams] = useState<Record<string, any>>({});
+  const compareCompQueryParams = useMemo(
+    () => queryString.stringify(compareCompositionParams),
+    [compareCompositionParams]
+  );
 
-    // if (loading) {
-    //     return <div>Loading...</div>
-    // }
+  // if (loading) {
+  //     return <div>Loading...</div>
+  // }
 
-    return (
-        <>
-            <div className={styles.dropdownContainer}>
-                <div className={styles.leftDropdown}>
-                    <CompositionDropdown dropdownItems={baseCompositionDropdownSource} />
-                </div>
-                <div className={styles.rightDropdown}>
-                    <CompositionDropdown dropdownItems={compareCompositionDropdownSource} />
-                </div>
-            </div>
-            <div className={styles.mainContainer}>
-                <div className={styles.subContainerLeft}>
-                    <div className={styles.subView}>
-                        <CompositionContextProvider queryParams={baseCompositionParams} setQueryParams={setBaseCompositionParams}>
-                            <CompositionContent
-                                emptyState={undefined} // todo: has to come from emptyStateSlot
-                                component={base}
-                                selected={selectedBaseComp}
-                                queryParams={baseCompQueryParams}
-                            />
-                        </CompositionContextProvider>
-                    </div>
-                </div>
-                <div className={styles.subContainerRight}>
-                    <div className={styles.subView}>
-                        <CompositionContextProvider queryParams={compareCompositionParams} setQueryParams={setCompareCompositionParams}>
-                            <CompositionContent
-                                emptyState={undefined} // todo: has to come from emptyStateSlot
-                                component={compare}
-                                selected={selectedCompareComp}
-                                queryParams={compareCompQueryParams}
-                            />
-                        </CompositionContextProvider>
-                    </div>
-                </div>
-            </div>
-        </>
-    );
+  return (
+    <>
+      <div className={styles.dropdownContainer}>
+        <div className={styles.leftDropdown}>
+          <CompositionDropdown dropdownItems={baseCompositionDropdownSource} />
+        </div>
+        <div className={styles.rightDropdown}>
+          <CompositionDropdown dropdownItems={compareCompositionDropdownSource} />
+        </div>
+      </div>
+      <div className={styles.mainContainer}>
+        <div className={styles.subContainerLeft}>
+          <div className={styles.subView}>
+            <CompositionContextProvider queryParams={baseCompositionParams} setQueryParams={setBaseCompositionParams}>
+              <CompositionContent
+                emptyState={undefined} // todo: has to come from emptyStateSlot
+                component={base}
+                selected={selectedBaseComp}
+                queryParams={baseCompQueryParams}
+              />
+            </CompositionContextProvider>
+          </div>
+        </div>
+        <div className={styles.subContainerRight}>
+          <div className={styles.subView}>
+            <CompositionContextProvider
+              queryParams={compareCompositionParams}
+              setQueryParams={setCompareCompositionParams}
+            >
+              <CompositionContent
+                emptyState={undefined} // todo: has to come from emptyStateSlot
+                component={compare}
+                selected={selectedCompareComp}
+                queryParams={compareCompQueryParams}
+              />
+            </CompositionContextProvider>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
--- scopes/component/ui/component-compare-composition/composition-dropdown.tsx
+++ scopes/component/ui/component-compare-composition/composition-dropdown.tsx
@@ -3,50 +3,47 @@ import { MenuLinkItem } from '@teambit/design.ui.surfaces.menu.link-item';
 import { Icon } from '@teambit/evangelist.elements.icon';
 import { Dropdown } from '@teambit/evangelist.surfaces.dropdown';
 import { useQuery } from '@teambit/ui-foundation.ui.react-router.use-query';
-import React from "react";
-import styles from "./composition-dropdown.module.scss";
+import React from 'react';
+import styles from './composition-dropdown.module.scss';
 
 export type CompositionDropdownProps = {
-    dropdownItems: Array<{ label: string, value: string }>,
-}
+  dropdownItems: Array<{ label: string; value: string }>;
+};
 
-const baseCompQueryParam = "compositionBase";
-const compareCompQueryParam = "compositionCompare";
+const baseCompQueryParam = 'compositionBase';
+const compareCompQueryParam = 'compositionCompare';
 
 export function CompositionDropdown(props: CompositionDropdownProps) {
-    const { dropdownItems: data } = props;
-    const query = useQuery();
+  const { dropdownItems: data } = props;
+  const query = useQuery();
 
-    // const href = (link: string) => {
-    //     const { componentId, ...params } = useComponentCompareParams();
-    //     params.selectedCompositionBaseFile = link;
-    //     params.selectedCompositionCompareFile = link;
-    //     const result = getComponentCompareUrl(params);
-    //     return result;
-    // }
+  // const href = (link: string) => {
+  //     const { componentId, ...params } = useComponentCompareParams();
+  //     params.selectedCompositionBaseFile = link;
+  //     params.selectedCompositionCompareFile = link;
+  //     const result = getComponentCompareUrl(params);
+  //     return result;
+  // }
 
-    return (
-        <Dropdown
-            dropClass={styles.menu}
-            placeholder={
-                <div className={styles.placeholder}>
-                    <div className={styles.placeholderTitle}>Helloooo</div>
-                    <Icon of="fat-arrow-down" />
-                </div>
-            }
-        >
-            <div>
-                {
-                    data.map((item, index) => {
-
-                        return (
-                            <MenuLinkItem key={index} isActive={() => false} href={item.value}>
-                                <div>{item.label}</div>
-                            </MenuLinkItem>
-                        );
-                    })
-                }
-            </div>
-        </Dropdown>
-    )
+  return (
+    <Dropdown
+      dropClass={styles.menu}
+      placeholder={
+        <div className={styles.placeholder}>
+          <div className={styles.placeholderTitle}>Helloooo</div>
+          <Icon of="fat-arrow-down" />
+        </div>
+      }
+    >
+      <div>
+        {data.map((item, index) => {
+          return (
+            <MenuLinkItem key={index} isActive={() => false} href={item.value}>
+              <div>{item.label}</div>
+            </MenuLinkItem>
+          );
+        })}
+      </div>
+    </Dropdown>
+  );
 }
