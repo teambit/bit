8c2cac67961bb896eebd9ef9e7773949e3e8e135
David First <davidnet@gmail.com>
2022-05-23 15:14:59 -0400

    schema: types improvements and support component-id (#5838)
    - support enum
    - make the TransformerNotFound error clearer by showing the exact location
    - add optional typeArguments to typeRef schema
    - save a pkg in the type-ref as ComponentID if applicable
    - implement type-parenthesized schema
    - implement type-predicate schema
    - implement ObjectBindingPattern and ArrayBindingPattern
    - implement indexed-access-type

--- scopes/semantics/entities/semantic-schema/schemas/inference-type.ts
+++ scopes/semantics/entities/semantic-schema/schemas/inference-type.ts
@@ -4,11 +4,14 @@ import { Location, SchemaNode } from '../schema-node';
  * where there is no explicit type, the type is taken from the "quickinfo" of tsserver
  */
 export class InferenceTypeSchema extends SchemaNode {
-  constructor(readonly location: Location, readonly type: string) {
+  constructor(readonly location: Location, readonly type: string, readonly name?: string) {
     super();
   }
 
   toString() {
+    if (this.name) {
+      return `${this.name}: ${this.type}`;
+    }
     return this.type;
   }
 }
