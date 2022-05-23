08407c5ca4be28600c3da5556be3d3a3c02807d6
Kutner JS <urikutner@gmail.com>
2022-05-23 12:55:30 +0300

    show app build errors (#5843)

--- scopes/harmony/application/app-build-result.ts
+++ scopes/harmony/application/app-build-result.ts
@@ -2,4 +2,14 @@ import { ArtifactDefinition } from '@teambit/builder';
 
 export interface AppBuildResult {
   artifacts?: ArtifactDefinition[];
+
+  /**
+   * errors thrown during the build process.
+   */
+  errors?: Error[];
+
+  /**
+   * warnings thrown during the build process.
+   */
+  warnings?: string[];
 }
