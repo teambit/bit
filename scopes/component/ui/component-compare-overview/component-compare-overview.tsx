4a92824dd6d8b553679ab3cdaf4b32b472ed3316
Bhanu Bandaranayake <bhanu@bit.dev>
2022-05-25 15:54:32 -0400

    chore: make both left & right panels scroll together

--- scopes/component/ui/component-compare-overview/component-compare-overview.tsx
+++ scopes/component/ui/component-compare-overview/component-compare-overview.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useRef, UIEvent } from 'react';
 import styles from './component-compare-overview.module.scss';
 import { useComponentCompareContext } from '@teambit/component.ui.component-compare';
 import { ComponentProvider, ComponentDescriptorProvider } from '@teambit/component';
@@ -16,18 +16,28 @@ export function ComponentCompareOverview(props: ComponentCompareOverviewProps) {
   }
 
   const { base, compare } = componentCompare;
+  const leftPanelRef = useRef<HTMLDivElement>(null);
+  const rightPanelRef = useRef<HTMLDivElement>(null);
+
+  function handleLeftPanelScroll(event: UIEvent<HTMLDivElement>) {
+    rightPanelRef.current?.scrollTo({ top: event.currentTarget.scrollTop, left: event.currentTarget.scrollLeft });
+  }
+
+  function handleRightPanelScroll(event: UIEvent<HTMLDivElement>) {
+    leftPanelRef.current?.scrollTo({ top: event.currentTarget.scrollTop, left: event.currentTarget.scrollLeft });
+  }
 
   return (
     <div className={styles.mainContainer}>
       <div className={styles.subContainerLeft}>
-        <div className={styles.subView}>
+        <div className={styles.subView} ref={leftPanelRef} onScroll={handleLeftPanelScroll}>
           <ComponentProvider component={base}>
             <Overview titleBadges={titleBadges} />
           </ComponentProvider>
         </div>
       </div>
       <div className={styles.subContainerRight}>
-        <div className={styles.subView}>
+        <div className={styles.subView} ref={rightPanelRef} onScroll={handleRightPanelScroll}>
           <ComponentProvider component={compare}>
             <Overview titleBadges={titleBadges} />
           </ComponentProvider>
