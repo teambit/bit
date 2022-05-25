72159e9c61f5b1def11a62d31dd4045a480328d2
Bhanu Bandaranayake <bhanu@bit.dev>
2022-05-25 10:43:47 -0400

    chore: export GraphFilters and GraphFilter

--- scopes/component/graph/index.ts
+++ scopes/component/graph/index.ts
@@ -1,6 +1,7 @@
 export { GraphAspect as default, GraphAspect } from './graph.aspect';
 
 export { Dependency } from './model/dependency';
+export { GraphFilter } from './model/graph-filters';
 export { DuplicateDependency, VersionSubgraph } from './duplicate-dependency';
 export type { ComponentGraph } from './component-graph';
 export type { GraphBuilder } from './graph-builder';
@@ -10,3 +11,4 @@ export type { GraphUI, ComponentWidget, ComponentWidgetSlot, ComponentWidgetProp
 export { useGraph, useGraphQuery, GraphModel, EdgeModel, NodeModel } from './ui/query';
 export { objectListToGraph, IdGraph } from './object-list-to-graph';
 export { depTypeToClass, depTypeToLabel, calcMinimapColors } from './ui/dependencies-graph';
+export { GraphFilters } from './ui/graph-page';
