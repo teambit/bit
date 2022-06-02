ed1c4bee83513f52a4ffabf26222b71097b0f2c5
Luv Kapur <luv@bit.dev>
2022-06-02 16:05:14 -0400

    refactor

--- scopes/component/ui/compare/aspects/compare-aspects-context.ts
+++ scopes/component/ui/compare/aspects/compare-aspects-context.ts
@@ -1,5 +1,5 @@
 import { createContext, useContext } from 'react';
-import { ComponentAspectData } from './use-compare-aspects';
+import { ComponentAspectData } from './use-compare-aspects-query';
 
 export type ComponentCompareAspectsModel = {
   base: ComponentAspectData[];
