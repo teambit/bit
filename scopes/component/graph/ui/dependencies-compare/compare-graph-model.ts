9f9a94541bec2376fb1b72cd2d375ed2ad933506
Bhanu Bandaranayake <bhanu@bit.dev>
2022-06-02 11:11:27 -0400

    refactor: remove unused methods

--- scopes/component/graph/ui/dependencies-compare/compare-graph-model.ts
+++ scopes/component/graph/ui/dependencies-compare/compare-graph-model.ts
@@ -1,18 +1,8 @@
-import { EdgeModel, GraphModel, RawGraph, NodeModel } from '@teambit/graph';
+import { EdgeModel, GraphModel } from '@teambit/graph';
 import { CompareNodeModel } from './compare-node-model';
 
 export class CompareGraphModel extends GraphModel {
   constructor(public nodes: CompareNodeModel[], public edges: EdgeModel[]) {
     super(nodes, edges);
-  }
-
-  static fromCompareNodeModels(nodes: Array<CompareNodeModel>, edges: Array<EdgeModel>) {
-    return new CompareGraphModel(nodes, edges);
-  }
-
-  static from(rawGraph: RawGraph) {
-    const nodes = rawGraph.nodes.map(NodeModel.from);
-    const edges = rawGraph.edges.map(EdgeModel.from);
-    return new GraphModel(nodes, edges);
   }
 }
