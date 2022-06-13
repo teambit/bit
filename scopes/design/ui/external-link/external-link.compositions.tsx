0877078c5f5d13853bb83487d84bce85dadece75
Uri Kutner <urikutner@gmail.com>
2022-06-13 12:40:22 +0300

    remove unused componetns

--- scopes/design/ui/external-link/external-link.compositions.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { ThemeCompositions } from '@teambit/documenter.theme.theme-compositions';
-import { ExternalLink } from './external-link';
-
-export const ExternalLinkExample = () => (
-  <ThemeCompositions>
-    <ExternalLink href="https://bit.dev">some link text - take me to bit.dev!!</ExternalLink>
-  </ThemeCompositions>
-);
