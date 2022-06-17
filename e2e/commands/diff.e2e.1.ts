a3e456f22c41f2e84d2f247ac1aeeccc6dc2d7de
David First <davidnet@gmail.com>
2022-06-17 10:39:35 -0400

    remove .only from diff tests

--- e2e/commands/diff.e2e.1.ts
+++ e2e/commands/diff.e2e.1.ts
@@ -13,7 +13,7 @@ const barFooV3 = "module.exports = function foo() { return 'got foo v3'; };\n";
 const noDiffMessage = 'no diff for';
 const successDiffMessage = 'showing diff for';
 
-describe.only('bit diff command', function () {
+describe('bit diff command', function () {
   this.timeout(0);
   let helper: Helper;
   before(() => {
