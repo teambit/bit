113ad501a5b6aa12818d3811260855d939a6325a
Uri Kutner <urikutner@gmail.com>
2022-06-27 12:36:23 +0300

    add comment

--- scopes/preview/preview/bundler/chunks.ts
+++ scopes/preview/preview/bundler/chunks.ts
@@ -1,5 +1,15 @@
 import type { BundlerEntryMap } from '@teambit/bundler';
 
+// TODO - we can remove the need to depenedOn preview-root, if we output chunks as "jsonp".
+//
+// for example:
+//  config.entry.someJsonpChunk = {
+//    import: "./xxx",
+//    library: { type: "jsonp", name: "foobar" }
+//  }
+// will result in this output:
+//  foobar(() => { ...; return MODULE })
+
 export const CHUNK_NAMES = {
   previewRoot: 'preview-root',
   peers: 'peers',
