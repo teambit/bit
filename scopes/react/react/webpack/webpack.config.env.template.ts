f9deb52e81dafef7f63ccb77aa94f7458cd31cda
Uri Kutner <urikutner@gmail.com>
2022-06-30 15:16:47 +0300

    set chunkLoadingGlobal

--- scopes/react/react/webpack/webpack.config.env.template.ts
+++ scopes/react/react/webpack/webpack.config.env.template.ts
@@ -1,38 +1,15 @@
 import { Configuration } from 'webpack';
 
-// when editing, make sure you are letter pefect!
-// "browser" -> browser optimized file (best)
-// "main" -> usually cjs, when package is not `type: "module"`, and esm when package is `type: "module"`
-// "module" -> usually esm, when package is not `type: "module"`
-const preferCjs = ['browser', 'main', 'module'];
-
 export function templateWebpackConfigFactory(): Configuration {
   return {
     name: 'react.env-template',
     output: {
       filename: '[name].[chunkhash].js',
-    },
-
-    // prefering cjs when when available,
-    // because expose-loader doesn't work well with esm packages with tree-shaking
-    resolve: {
-      mainFields: preferCjs,
-      extensions: [
-        '.web.js',
-        '.js', // <-- prefering .js before .mjs (important for packages like 'graphql')
-        '.cjs',
-        '.web.mjs',
-        '.mjs',
-        '.web.ts',
-        '.ts',
-        '.web.tsx',
-        '.tsx',
-        '.json',
-        '.web.jsx',
-        '.jsx',
-        '.mdx',
-        '.md',
-      ],
+      // might run into this error, when running multiple chunks created by different webpack builds:
+      //    "Uncaught TypeError: __webpack_require__.n is not a function"
+      // this is because each build creates its own runtime, and they compete on the same global name.
+      // setting a separate global name for the template ensures it will not conflict with preview chunks loaded in runtime
+      chunkLoadingGlobal: '__loadChunks_EnvTemplate',
     },
   };
 }
