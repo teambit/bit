ddd1fcffaa33b120d51221743dd7b03d10f89957
David First <davidnet@gmail.com>
2022-06-30 10:13:35 -0400

    fix: support running bit-use for new aspects (#6040)
    Currently, running `bit use` on a new component throws `ComponentNotFound` error.

--- scopes/workspace/workspace/workspace.provider.ts
+++ scopes/workspace/workspace/workspace.provider.ts
@@ -39,6 +39,7 @@ import { EnvsUnsetCmd } from './envs-subcommands/envs-unset.cmd';
 import { PatternCommand } from './pattern.cmd';
 import { EnvsReplaceCmd } from './envs-subcommands/envs-replace.cmd';
 import { ScopeSetCmd } from './scope-subcommands/scope-set.cmd';
+import { UseCmd } from './use.cmd';
 
 export type WorkspaceDeps = [
   PubsubMain,
@@ -210,6 +211,7 @@ export default async function provideWorkspace(
     commands.push(new WatchCommand(pubsub, logger, watcher));
     cli.unregister('link');
     commands.push(new LinkCommand(workspace, logger, community.getDocsDomain()));
+    commands.push(new UseCmd(workspace));
   }
   commands.push(new PatternCommand(workspace));
   cli.register(...commands);
