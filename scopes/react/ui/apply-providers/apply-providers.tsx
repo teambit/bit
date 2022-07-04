006cbb3038fa7aae93b1ca43533d4e260787e418
bengil <benjyg@gmail.com>
2022-07-04 21:19:59 +0300

    extract base and rename to apply-providers

--- scopes/react/ui/apply-providers/apply-providers.tsx
+++ scopes/react/ui/apply-providers/apply-providers.tsx
@@ -3,34 +3,29 @@ import { ErrorBoundary } from 'react-error-boundary';
 import classNames from 'classnames';
 import { Composer } from '@teambit/base-ui.utils.composer';
 import { ErrorFallback } from '@teambit/react.ui.error-fallback';
+
 import { RenderingContext } from '@teambit/preview';
 import { ReactAspect } from '@teambit/react';
-import styles from './base.module.scss';
+import styles from './apply-providers.modules.scss';
 
-// import { ExamplesOverview } from './examples-overview';
 
-export type DocsSectionProps = {
+export type ApplyProvidersProps = {
   renderingContext: RenderingContext;
   children: React.ReactNode;
 } & HTMLAttributes<HTMLDivElement>;
 
-
-
 /**
  * base template for react component documentation
  */
-export function Base({ renderingContext, children, ...rest }: DocsSectionProps) {
+export function ApplyProviders({ renderingContext, children, ...rest }: ApplyProvidersProps) {
 
   const { providers = [] } = renderingContext.get(ReactAspect.id) || {};
-
-  // const { examples = [] } = docs;
 
   return (
     <div className={classNames(styles.docsMainBlock)} {...rest}>
       <ErrorBoundary FallbackComponent={ErrorFallback}>
         <Composer components={providers}>
           {children}
-          {/* <ExamplesOverview examples={Content.examples || examples} /> */}
         </Composer>
       </ErrorBoundary>
     </div>
