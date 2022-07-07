00e793d37122f831a812abc0dd595735b886e591
bengil <benjyg@gmail.com>
2022-07-07 14:54:02 +0300

    update html docs root with new property

--- scopes/html/html/html-docs-app.ts
+++ scopes/html/html/html-docs-app.ts
@@ -1,21 +1,23 @@
 import mapObject from 'map-obj';
-import DocsRoot, { ReactDocsRoot } from '@teambit/react.ui.docs-app';
+import { DocsRootProps } from '@teambit/docs';
+import DocsRoot from '@teambit/react.ui.docs-app';
 import { htmlToReact } from './html-to-react';
 
-const htmlDocsRoot: ReactDocsRoot = function({
+const htmlDocsRoot = function({
   Provider,
   componentId,
   docs,
   compositions,
   ...rest
-}
-) {
+}: DocsRootProps) {
 
   // should be mapObject<Record<string, any>, Record<string, () => any>>
   // @ts-ignore TODO fix
   const reactCompositions = mapObject(compositions, (key, value) => [key, htmlToReact(value as HTMLElement)]);
 
-  return DocsRoot({Provider, componentId, docs, reactCompositions, ...rest});
+  return DocsRoot({Provider, componentId, docs, compositions: reactCompositions, ...rest});
 }
+
+htmlDocsRoot.apiObject = true;
 
 export default htmlDocsRoot;
