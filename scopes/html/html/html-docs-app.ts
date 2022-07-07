b77b1abcc6836dcf99c262d556316e5b053aa566
bengil <benjyg@gmail.com>
2022-07-07 13:56:55 +0300

    update html env mount function to use new api

--- scopes/html/html/html-docs-app.ts
+++ scopes/html/html/html-docs-app.ts
@@ -2,14 +2,20 @@ import mapObject from 'map-obj';
 import DocsRoot, { ReactDocsRoot } from '@teambit/react.ui.docs-app';
 import { htmlToReact } from './html-to-react';
 
-const htmlDocsRoot: ReactDocsRoot = function(...args) {
-  const [Provider, componentId, docs, compositions, ...rest] = args;
+const htmlDocsRoot: ReactDocsRoot = function({
+  Provider,
+  componentId,
+  docs,
+  compositions,
+  ...rest
+}
+) {
 
   // should be mapObject<Record<string, any>, Record<string, () => any>>
   // @ts-ignore TODO fix
   const reactCompositions = mapObject(compositions, (key, value) => [key, htmlToReact(value as HTMLElement)]);
 
-  return DocsRoot(Provider, componentId, docs, reactCompositions, ...rest);
+  return DocsRoot({Provider, componentId, docs, reactCompositions, ...rest});
 }
 
 export default htmlDocsRoot;
