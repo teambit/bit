da63e951eab8d511fbae59aed7d0da123907d214
bengil <benjyg@gmail.com>
2022-07-10 12:35:13 +0300

    PR refactor

--- scopes/html/html/html-docs-app.ts
+++ scopes/html/html/html-docs-app.ts
@@ -3,12 +3,12 @@ import { DocsRootProps } from '@teambit/docs';
 import DocsRoot from '@teambit/react.ui.docs-app';
 import { htmlToReact } from './html-to-react';
 
-const htmlDocsRoot = function ({ Provider, componentId, docs, compositions, ...rest }: DocsRootProps) {
+const htmlDocsRoot = function ({ compositions, ...rest }: DocsRootProps) {
   // should be mapObject<Record<string, any>, Record<string, () => any>>
   // @ts-ignore TODO fix
   const reactCompositions = mapObject(compositions, (key, value) => [key, htmlToReact(value as HTMLElement)]);
 
-  return DocsRoot({ Provider, componentId, docs, compositions: reactCompositions, ...rest });
+  return DocsRoot({ compositions: reactCompositions, ...rest });
 };
 
 // for backwards compatibility while users update their  - can be removed by end of 2022
