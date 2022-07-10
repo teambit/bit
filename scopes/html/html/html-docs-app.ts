637289acfadefbdafc1aa53866dc70beafa1ea99
benjgil <67269267+benjgil@users.noreply.github.com>
2022-07-10 13:25:14 +0300

    Refactor docs root input (#6088)

--- scopes/html/html/html-docs-app.ts
+++ scopes/html/html/html-docs-app.ts
@@ -1,15 +1,17 @@
 import mapObject from 'map-obj';
-import DocsRoot, { ReactDocsRoot } from '@teambit/react.ui.docs-app';
+import { DocsRootProps } from '@teambit/docs';
+import DocsRoot from '@teambit/react.ui.docs-app';
 import { htmlToReact } from './html-to-react';
 
-const htmlDocsRoot: ReactDocsRoot = function(...args) {
-  const [Provider, componentId, docs, compositions, ...rest] = args;
-
+const htmlDocsRoot = function ({ compositions, ...rest }: DocsRootProps) {
   // should be mapObject<Record<string, any>, Record<string, () => any>>
   // @ts-ignore TODO fix
   const reactCompositions = mapObject(compositions, (key, value) => [key, htmlToReact(value as HTMLElement)]);
 
-  return DocsRoot(Provider, componentId, docs, reactCompositions, ...rest);
-}
+  return DocsRoot({ compositions: reactCompositions, ...rest });
+};
+
+// for backwards compatibility while users update their  - can be removed by end of 2022
+htmlDocsRoot.apiObject = true;
 
 export default htmlDocsRoot;
--- scopes/react/ui/docs-app/index.tsx
+++ scopes/react/ui/docs-app/index.tsx
@@ -2,9 +2,8 @@ export { DocsTheme } from './docs-theme';
 export type { DocsAppProps } from './docs-theme';
 
 export { default } from './docs.app-root';
-export type { ReactDocsRoot } from './docs.app-root';
 
 export { defaultDocs } from './docs-app';
 export type { ReactDocsAppProps } from './docs-app';
 
-export {default as docsStyles} from './docs-app.module.scss';
+export { default as docsStyles } from './docs-app.module.scss';
