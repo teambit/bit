110be4f31c2c88d74a7f46b0282843d592e7a93f
bengil <benjyg@gmail.com>
2022-07-10 21:29:23 +0300

    add badges for all sections of component header section

--- scopes/component/ui/component-meta/component-overview.tsx
+++ scopes/component/ui/component-meta/component-overview.tsx
@@ -1,4 +1,4 @@
-import React, { ComponentType } from 'react';
+import React, { ComponentType, ReactNode } from 'react';
 import type { ComponentDescriptor } from '@teambit/component-descriptor';
 import { textColumn } from '@teambit/base-ui.layout.page-frame';
 import { ConsumableLink } from '@teambit/documenter.ui.consumable-link';
@@ -11,9 +11,18 @@ import { isBrowser } from '@teambit/ui-foundation.ui.is-browser';
 import { ComponentModel } from '@teambit/component';
 import styles from './component-overview.module.scss';
 
+export enum TitleBadgePosition {
+  Title,
+  SubTitle,
+  Labels,
+  Package,
+  ElementsPackage,
+}
+
 export type TitleBadge = {
   component: ComponentType<any>;
   weight?: number;
+  position?: TitleBadgePosition;
 };
 
 export type ComponentOverviewProps = {
@@ -44,30 +53,59 @@ export function ComponentOverview({
     const origin = isBrowser ? window.location.origin : undefined;
     finalElementsUrl = origin && elementsUrl ? `${origin}${elementsUrl}` : undefined;
   }
+
+  type CompWithTitleBadgesProps = { position: TitleBadgePosition; children: ReactNode };
+  function ComponentWithTitleBadges({ position, children }: CompWithTitleBadgesProps) {
+    return (
+      <div className={styles.componentWithBadgeContainer}>
+        {children}
+        <div className={styles.badgeContainer}>
+          {titleBadges
+            // eslint-disable-next-line react/prop-types
+            ?.filter((badge) => {
+              return (
+                (position === TitleBadgePosition.Title && !badge.position) || // default position is title
+                badge.position === position
+              );
+            })
+            // @ts-ignore
+            ?.sort((a, b) => a?.weight - b?.weight)
+            ?.map((titleBadge, index) => {
+              return (
+                <titleBadge.component
+                  key={index}
+                  componentDescriptor={componentDescriptor}
+                  legacyComponentModel={component}
+                />
+              );
+            })}
+        </div>
+      </div>
+    );
+  }
+
   return (
     <Section {...rest}>
       <div className={textColumn}>
-        <div className={styles.componentTitle}>
-          <H1>{displayName}</H1>
-          <div className={styles.badgeContainer}>
-            {titleBadges
-              // @ts-ignore
-              ?.sort((a, b) => a?.weight - b?.weight)
-              ?.map((titleBadge, index) => {
-                return (
-                  <titleBadge.component
-                    key={index}
-                    componentDescriptor={componentDescriptor}
-                    legacyComponentModel={component}
-                  />
-                );
-              })}
+        <ComponentWithTitleBadges position={TitleBadgePosition.Title}>
+          (
+          <div className={styles.componentTitle}>
+            <H1>{displayName}</H1>
           </div>
-        </div>
-        {abstract && <Subtitle className={styles.subTitle}>{abstract}</Subtitle>}
-        <LabelList>{labels}</LabelList>
-        <ConsumableLink title="Package name" link={packageName}></ConsumableLink>
-        {finalElementsUrl && <ConsumableLink title="Elements url" link={finalElementsUrl}></ConsumableLink>}
+          )
+        </ComponentWithTitleBadges>
+        <ComponentWithTitleBadges position={TitleBadgePosition.SubTitle}>
+          {abstract && <Subtitle className={styles.subTitle}>{abstract}</Subtitle>}
+        </ComponentWithTitleBadges>
+        <ComponentWithTitleBadges position={TitleBadgePosition.Labels}>
+          <LabelList>{labels}</LabelList>
+        </ComponentWithTitleBadges>
+        <ComponentWithTitleBadges position={TitleBadgePosition.Package}>
+          <ConsumableLink title="Package name" link={packageName}></ConsumableLink>
+        </ComponentWithTitleBadges>
+        <ComponentWithTitleBadges position={TitleBadgePosition.ElementsPackage}>
+          {finalElementsUrl && <ConsumableLink title="Elements url" link={finalElementsUrl}></ConsumableLink>}
+        </ComponentWithTitleBadges>
       </div>
       <Separator isPresentational />
     </Section>
