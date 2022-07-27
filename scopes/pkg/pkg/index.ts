abd8decb2b7d660e8103144e28f25848c7f31212
Luv Kapur <luv@bit.dev>
2022-07-27 15:07:49 -0400

    sync pkg/index.ts with origin master

--- scopes/pkg/pkg/index.ts
+++ scopes/pkg/pkg/index.ts
@@ -1,10 +1,4 @@
-export type {
-  PkgMain,
-  PackageJsonProps,
-  ComponentPackageManifest,
-  ComponentPkgExtensionData,
-  VersionPackageManifest,
-} from './pkg.main.runtime';
+export type { PkgMain, PackageJsonProps, ComponentPackageManifest, ComponentPkgExtensionData, VersionPackageManifest } from './pkg.main.runtime';
 export type { PackageDependency, PackageDependencyFactory } from './package-dependency';
 export { PkgAspect as default, PkgAspect } from './pkg.aspect';
 export { PkgUI } from './pkg.ui.runtime';
