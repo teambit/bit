ebb96c8b07b78ef86d4e67c36a45b80c91a0f67d
David First <davidnet@gmail.com>
2022-07-28 15:18:50 -0400

    fix(bit-lane-diff): avoid throwing when one of the components failed to diff (#6217)

---
+++ e2e/harmony/lanes/lanes-with-issues.e2e.ts
@@ -0,0 +1,38 @@
+import chai, { expect } from 'chai';
+import Helper from '../../../src/e2e-helper/e2e-helper';
+
+chai.use(require('chai-fs'));
+
+describe('lanes with various issues', function () {
+  this.timeout(0);
+  let helper: Helper;
+  before(() => {
+    helper = new Helper();
+  });
+  after(() => {
+    helper.scopeHelper.destroy();
+  });
+  describe('issue - object of head of main is missing from the filesystem and remote', () => {
+    before(() => {
+      helper.scopeHelper.setNewLocalAndRemoteScopesHarmony();
+      helper.bitJsonc.setupDefault();
+      helper.fixtures.populateComponents(3);
+      helper.command.tagWithoutBuild();
+      const comp2Head = helper.command.getHead('comp2');
+      helper.command.export();
+      helper.command.createLane();
+      helper.fixtures.populateComponents(3, undefined, 'v3');
+      helper.command.snapAllComponentsWithoutBuild();
+      helper.command.export();
+      const objectPath = helper.general.getHashPathOfObject(comp2Head);
+      helper.fs.deleteObject(objectPath);
+      helper.fs.deleteRemoteObject(objectPath);
+    });
+    it('bit diff diff should not throw', () => {
+      expect(() => helper.command.diffLane()).not.to.throw();
+      const output = helper.command.diffLane();
+      expect(output).to.have.string('Diff failed on the following component(s)');
+      expect(output).to.have.string('ENOENT: no such file or directory');
+    });
+  });
+});
