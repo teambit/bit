99cc5a4bc2aba53ae7313cd899577dbde20f1a8e
David First <davidnet@gmail.com>
2022-08-02 09:49:52 -0400

    fix(bit-lane-diff): fix errors when components are newly introduced in "from" lane

--- scopes/lanes/diff/lane-diff.cmd.ts
+++ scopes/lanes/diff/lane-diff.cmd.ts
@@ -25,19 +25,23 @@ bit lane diff from to => diff between "from" lane and "to" lane.`;
 
   async report([values = []]: [string[]], { pattern }: { pattern?: string }) {
     const laneDiffGenerator = new LaneDiffGenerator(this.workspace, this.scope);
-    const { compsWithDiff, newComps, toLaneName, failures } = await laneDiffGenerator.generate(
-      values,
-      undefined,
-      pattern
-    );
+    const { compsWithDiff, newCompsFrom, newCompsTo, toLaneName, fromLaneName, failures } =
+      await laneDiffGenerator.generate(values, undefined, pattern);
+
+    const newCompsOutput = (laneName: string, ids: string[]) => {
+      if (!ids.length) return '';
+      const newCompsIdsStr = ids.map((id) => chalk.bold(id)).join('\n');
+      const newCompsTitle = `\nThe following components were introduced in ${chalk.bold(laneName)} lane`;
+      return newCompsFrom.length ? `${chalk.inverse(newCompsTitle)}\n${newCompsIdsStr}` : '';
+    };
 
     const diffResultsStr = outputDiffResults(compsWithDiff);
-    const newCompsIdsStr = newComps.map((id) => chalk.bold(id)).join('\n');
-    const newCompsTitle = `The following components were introduced in ${chalk.bold(toLaneName)} lane`;
-    const newCompsStr = newComps.length ? `${chalk.inverse(newCompsTitle)}\n${newCompsIdsStr}` : '';
+
     const failuresTitle = `\n\nDiff failed on the following component(s)`;
     const failuresIds = failures.map((f) => `${f.id.toString()} - ${chalk.red(f.msg)}`);
     const failuresStr = failures.length ? `${chalk.inverse(failuresTitle)}\n${failuresIds}` : '';
-    return `${diffResultsStr}\n${newCompsStr}${failuresStr}`;
+    const newCompsToStr = newCompsOutput(toLaneName, newCompsTo);
+    const newCompsFromStr = newCompsOutput(fromLaneName, newCompsFrom);
+    return `${diffResultsStr}${newCompsToStr}${newCompsFromStr}${failuresStr}`;
   }
 }
