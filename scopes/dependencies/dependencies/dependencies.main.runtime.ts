79476c99d38426ca946a0c0341dc99a1892e0bae
David First <davidnet@gmail.com>
2022-08-05 13:52:15 -0400

    feat(bit-deps): introduce a new sub-command: "bit dependencies set"

--- scopes/dependencies/dependencies/dependencies.main.runtime.ts
+++ scopes/dependencies/dependencies/dependencies.main.runtime.ts
@@ -1,18 +1,59 @@
 import { CLIAspect, CLIMain, MainRuntime } from '@teambit/cli';
-import { DependenciesCmd, DependenciesDebug, DependenciesGet } from './dependencies-cmd';
+import { DependencyResolverAspect } from '@teambit/dependency-resolver';
+import WorkspaceAspect, { Workspace } from '@teambit/workspace';
+import {
+  DependenciesCmd,
+  DependenciesDebugCmd,
+  DependenciesGetCmd,
+  DependenciesSetCmd,
+  SetDependenciesFlags,
+} from './dependencies-cmd';
 import { DependenciesAspect } from './dependencies.aspect';
 
 export class DependenciesMain {
+  constructor(private workspace: Workspace) {}
+
+  async setDependency(componentPattern: string, pkg: string, options: SetDependenciesFlags): Promise<string[]> {
+    const compIds = await this.workspace.idsByPattern(componentPattern);
+    const getDepField = () => {
+      if (options.dev) return 'devDependencies';
+      if (options.peer) return 'peerDependencies';
+      return 'dependencies';
+    };
+    const packageSplit = pkg.split('@');
+    if (packageSplit.length !== 2) {
+      throw new Error(`invalid package "${pkg}" syntax, expected "package@version"`);
+    }
+    const config = {
+      policy: {
+        [getDepField()]: {
+          [packageSplit[0]]: packageSplit[1],
+        },
+      },
+    };
+    await Promise.all(
+      compIds.map(async (compId) => {
+        await this.workspace.addSpecificComponentConfig(compId, DependencyResolverAspect.id, config, true);
+      })
+    );
+
+    await this.workspace.bitMap.write();
+
+    return compIds.map((compId) => compId.toStringWithoutVersion());
+  }
+
   static slots = [];
-  static dependencies = [CLIAspect];
+  static dependencies = [CLIAspect, WorkspaceAspect];
 
   static runtime = MainRuntime;
 
-  static async provider([cli]: [CLIMain]) {
+  static async provider([cli, workspace]: [CLIMain, Workspace]) {
+    const depsMain = new DependenciesMain(workspace);
     const depsCmd = new DependenciesCmd();
-    depsCmd.commands = [new DependenciesGet(), new DependenciesDebug()];
+    depsCmd.commands = [new DependenciesGetCmd(), new DependenciesDebugCmd(), new DependenciesSetCmd(depsMain)];
     cli.register(depsCmd);
-    return new DependenciesMain();
+
+    return depsMain;
   }
 }
 
