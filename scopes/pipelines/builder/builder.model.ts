a986a3691e86b740c47569541d0f419a922a8292
Luv Kapur <luv@bit.dev>
2022-08-08 11:08:18 -0400

    revert - resolver map changes

--- scopes/pipelines/builder/builder.model.ts
+++ scopes/pipelines/builder/builder.model.ts
@@ -1,6 +1,5 @@
 import { ArtifactObject } from '@teambit/legacy/dist/consumer/component/sources/artifact-files';
 import { AspectData, PipelineReport } from './build-pipeline-result-list';
-import { ArtifactsStorageResolver } from './storage';
 
 export type BuilderData = {
   pipeline: PipelineReport[];
@@ -10,16 +9,3 @@ export type BuilderData = {
 };
 
 export const FILE_PATH_PARAM_DELIM = '~';
-export type ArtifactPropsToPopulate = {
-  aspectName: string;
-  /**
-   * Name of the artifact to populate
-   */
-  name?: string;
-};
-
-export type ArtifactsToPopulate = ArtifactPropsToPopulate[];
-
-export type StorageResolversMap = {
-  [resolverName: string]: ArtifactsStorageResolver;
-};
