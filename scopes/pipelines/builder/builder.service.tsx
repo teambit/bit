19f89ecf82076d5c64ae5b9aae77725cf653a91a
Luv Kapur <luv@bit.dev>
2022-08-08 10:47:56 -0400

    wip - remove storage resolver migration

--- scopes/pipelines/builder/builder.service.tsx
+++ scopes/pipelines/builder/builder.service.tsx
@@ -9,6 +9,7 @@ import { BuildPipe, TaskResults } from './build-pipe';
 import { TaskResultsList } from './task-results-list';
 import { TaskSlot } from './builder.main.runtime';
 import { BuildContext, BuildTaskHelper } from './build-task';
+import { ArtifactFactory } from './artifact';
 import { calculatePipelineOrder } from './build-pipeline-order';
 import { BuilderAspect } from './builder.aspect';
 
@@ -69,6 +70,7 @@ export class BuilderService implements EnvService<BuildServiceResults, BuilderDe
      * pipe name to display on the console during the execution
      */
     private displayPipeName: string,
+    private artifactFactory: ArtifactFactory,
     private scope: ScopeMain
   ) {}
 
@@ -108,11 +110,18 @@ export class BuilderService implements EnvService<BuildServiceResults, BuilderDe
         const buildContext = Object.assign(executionContext, {
           capsuleNetwork,
           previousTasksResults: [],
+          dev: options.dev,
         });
         envsBuildContext[executionContext.id] = buildContext;
       })
     );
-    const buildPipe = BuildPipe.from(tasksQueue, envsBuildContext, this.logger);
+    const buildPipe = BuildPipe.from(
+      tasksQueue,
+      envsBuildContext,
+      this.logger,
+      this.artifactFactory,
+      options.previousTasksResults
+    );
     const buildResults = await buildPipe.execute();
     longProcessLogger.end();
     buildResults.hasErrors() ? this.logger.consoleFailure() : this.logger.consoleSuccess();
--- scopes/pipelines/builder/index.ts
+++ scopes/pipelines/builder/index.ts
@@ -8,16 +8,10 @@ export {
   BuildTaskHelper,
   CAPSULE_ARTIFACTS_DIR,
 } from './build-task';
-export type { BuilderMain, BuilderData, StorageResolversMap } from './builder.main.runtime';
+export type { BuilderMain, BuilderData } from './builder.main.runtime';
 export type { PipelineReport } from './build-pipeline-result-list';
-export {
-  ArtifactFileStoreResult,
-  ArtifactListStoreResult,
-  ArtifactsStorageResolver,
-  ArtifactStoreResult,
-} from './storage';
-export { Artifact, ArtifactList, ArtifactFactory, ArtifactDefinition } from './artifact';
+export { BuilderAspect } from './builder.aspect';
+export { WholeArtifactStorageResolver, FileStorageResolver, ArtifactStorageResolver } from './storage';
+export { Artifact, ArtifactList, ArtifactFactory, ArtifactDefinition, ArtifactModelDefinition } from './artifact';
 export { TaskResultsList } from './task-results-list';
 export { ArtifactVinyl } from '@teambit/legacy/dist/consumer/component/sources/artifact';
-export { BuilderAspect } from './builder.aspect';
-export { BuilderData, StorageResolversMap } from './builder.model';
