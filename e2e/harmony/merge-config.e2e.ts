57434cd1bd038dae102a26ae06054fc5066684ba
David First <davidnet@gmail.com>
2022-08-11 10:45:21 -0400

    improvement(merge): support merging config of the unmerged head (#6296)
    When merging lanes and conflicts found, the checked out versions of the components remain the same. as a result, when loading the components, it loads the config from the current version and disregard the config of the target version. 
    This PR takes into account the target version when calculating the config.
    
    It also fixes an issue when merging a lane and some components are not part of the workspace to merge them correctly.
    (previously, it didn't try to do any diff/merge and always overrode with the target components).

---
+++ e2e/harmony/merge-config.e2e.ts
@@ -0,0 +1,94 @@
+import chai, { expect } from 'chai';
+import { Extensions } from '../../src/constants';
+import Helper from '../../src/e2e-helper/e2e-helper';
+import NpmCiRegistry, { supportNpmCiRegistryTesting } from '../npm-ci-registry';
+
+chai.use(require('chai-fs'));
+
+describe('merge config scenarios', function () {
+  this.timeout(0);
+  let helper: Helper;
+  before(() => {
+    helper = new Helper();
+  });
+  after(() => {
+    helper.scopeHelper.destroy();
+  });
+  (supportNpmCiRegistryTesting ? describe : describe.skip)('diverge with different component versions', () => {
+    let npmCiRegistry: NpmCiRegistry;
+    let beforeDiverge: string;
+    let beforeMerges: string;
+    before(async () => {
+      helper = new Helper({ scopesOptions: { remoteScopeWithDot: true } });
+      helper.scopeHelper.setNewLocalAndRemoteScopesHarmony();
+      helper.bitJsonc.setupDefault();
+      helper.fixtures.populateComponents(3);
+      npmCiRegistry = new NpmCiRegistry(helper);
+      npmCiRegistry.configureCiInPackageJsonHarmony();
+      await npmCiRegistry.init();
+      helper.command.tagAllComponents();
+      helper.command.export();
+      beforeDiverge = helper.scopeHelper.cloneLocalScope();
+      helper.command.createLane();
+      helper.fixtures.populateComponents(3, undefined, 'on-lane');
+      helper.command.deprecateComponent('comp1');
+      helper.command.snapAllComponents();
+      helper.command.export();
+      helper.command.publish('"**"');
+
+      helper.scopeHelper.getClonedLocalScope(beforeDiverge);
+      helper.fixtures.populateComponents(3, undefined, 'v2');
+      helper.command.tagAllComponents();
+      helper.command.export();
+
+      helper.scopeHelper.reInitLocalScopeHarmony();
+      npmCiRegistry.setResolver();
+      helper.command.importComponent('comp1');
+      beforeMerges = helper.scopeHelper.cloneLocalScope();
+    });
+    after(() => {
+      npmCiRegistry.destroy();
+    });
+    describe('merging the lane to main', () => {
+      before(() => {
+        helper.command.fetchLane(`${helper.scopes.remote}/dev`);
+        helper.command.mergeLane('dev', '--manual');
+        // fixes the conflicts
+        helper.fs.outputFile(`${helper.scopes.remote}/comp1/index.js`);
+        helper.fs.outputFile(`${helper.scopes.remote}/comp2/index.js`);
+        helper.fs.outputFile(`${helper.scopes.remote}/comp3/index.js`);
+      });
+      it('should keep the configuration from the lane', () => {
+        const deprecationData = helper.command.showAspectConfig('comp1', Extensions.deprecation);
+        expect(deprecationData.config.deprecate).to.be.true;
+      });
+    });
+    describe('switching to the lane', () => {
+      before(() => {
+        helper.scopeHelper.getClonedLocalScope(beforeMerges);
+        helper.command.switchRemoteLane('dev', undefined, false);
+      });
+      // previously it was showing it as modified due to dependencies changes
+      it('should not show the component as modified', () => {
+        expect(helper.command.statusComponentIsModified('comp1')).to.be.false;
+      });
+      // previously it tried to install with the snap as the version.
+      it('should be able to install the correct versions after deleting node-modules', () => {
+        helper.fs.deletePath('node_modules');
+        expect(() => helper.command.install()).not.to.throw('No matching version found');
+      });
+      describe('merge from main to the lane', () => {
+        before(() => {
+          helper.command.mergeLane('main', '--manual');
+        });
+        // previous bug, showed only comp1 as componentsDuringMergeState, but the rest, because they're not in the
+        // workspace, it didn't merge them correctly.
+        it('bit status should show all components as componentsDuringMergeState and not in pendingUpdatesFromMain', () => {
+          const status = helper.command.statusJson();
+          expect(status.componentsDuringMergeState).to.have.lengthOf(3);
+          expect(status.pendingUpdatesFromMain).to.have.lengthOf(0);
+        });
+      });
+    });
+  });
+});
