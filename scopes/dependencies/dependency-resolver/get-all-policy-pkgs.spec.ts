62f7b1b35f1e2df9f2174c623e9b03cb19e24ff0
Zoltan Kochan <z@kochan.io>
2022-08-17 12:08:08 +0300

    test: update dedupe

--- scopes/dependencies/dependency-resolver/get-all-policy-pkgs.spec.ts
+++ scopes/dependencies/dependency-resolver/get-all-policy-pkgs.spec.ts
@@ -31,4 +31,48 @@ describe('getAllPolicyPkgs()', () => {
       },
     ]);
   });
+  it('should deduplicate dependencies that are present in root policies', () => {
+    const outdatedPkgs = getAllPolicyPkgs({
+      rootPolicy: new WorkspacePolicy([
+        {
+          dependencyId: 'foo',
+          lifecycleType: 'runtime',
+          value: { version: '1' },
+        },
+      ]),
+      variantPoliciesByPatterns: {},
+      componentPoliciesById: {},
+      componentModelVersions: [
+        {
+          name: 'foo',
+          version: '2',
+          lifecycleType: 'runtime',
+          componentId: 'comp1',
+        },
+        {
+          name: 'bar',
+          version: '2',
+          lifecycleType: 'runtime',
+          componentId: 'comp1',
+        },
+      ],
+    });
+    // @ts-ignore
+    expect(outdatedPkgs).toStrictEqual([
+      {
+        currentRange: '1',
+        name: 'foo',
+        source: 'rootPolicy',
+        targetField: 'dependencies',
+        variantPattern: null,
+      },
+      {
+        componentId: 'comp1',
+        currentRange: '2',
+        name: 'bar',
+        source: 'component-model',
+        targetField: 'dependencies',
+      },
+    ]);
+  });
 });
