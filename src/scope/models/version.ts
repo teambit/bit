bc4b27a4f4550e26790007e4ae2d649a2d9c0bf7
David First <davidnet@gmail.com>
2022-08-17 16:27:28 -0400

    feat: add bit-cloud-username to the Version log (#6331)

--- src/scope/models/version.ts
+++ src/scope/models/version.ts
@@ -40,6 +40,7 @@ export type Log = {
   date: string;
   username: string | undefined;
   email: string | undefined;
+  bitCloudUsername?: string;
 };
 
 export type VersionProps = {
@@ -315,6 +316,7 @@ export default class Version extends BitObject {
           message: this.log.message,
           date: this.log.date,
           username: this.log.username,
+          bitCloudUsername: this.log.bitCloudUsername,
           email: this.log.email,
         },
         docs: this.docs,
@@ -459,6 +461,7 @@ export default class Version extends BitObject {
         message: log.message,
         date: log.date,
         username: log.username,
+        bitCloudUsername: log.bitCloudUsername,
         email: log.email,
       },
       docs,
