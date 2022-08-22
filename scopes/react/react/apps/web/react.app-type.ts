be585eec1e2a9737fc1b7cd3d09f312722d84740
guysaar223 <guy@bit.dev>
2022-08-22 14:54:36 +0300

    react app

--- scopes/react/react/apps/web/react.app-type.ts
+++ scopes/react/react/apps/web/react.app-type.ts
@@ -1,11 +1,17 @@
-import { ApplicationType } from '@teambit/application';
 import type { Logger } from '@teambit/logger';
+import type { DependencyResolverMain } from '@teambit/dependency-resolver';
+import { ApplicationType } from '@teambit/application';
 import { ReactAppOptions } from './react-app-options';
 import { ReactApp } from './react.application';
 import { ReactEnv } from '../../react.env';
 
 export class ReactAppType implements ApplicationType<ReactAppOptions> {
-  constructor(readonly name: string, private reactEnv: ReactEnv, private logger?: Logger) {}
+  constructor(
+    readonly name: string,
+    private reactEnv: ReactEnv,
+    private logger: Logger,
+    private dependencyResolver: DependencyResolverMain
+  ) {}
 
   createApp(options: ReactAppOptions) {
     return new ReactApp(
@@ -14,14 +20,15 @@ export class ReactAppType implements ApplicationType<ReactAppOptions> {
       options.ssr,
       options.portRange || [3000, 4000],
       this.reactEnv,
+      this.logger,
+      this.dependencyResolver,
       options.prerender,
       options.bundler,
       options.ssrBundler,
       options.devServer,
       options.webpackTransformers,
       options.deploy,
-      options.favicon,
-      this.logger
+      options.favicon
     );
   }
 }
