7ce53a330b12767eaed2166e477dc2e6c888d58d
Luv Kapur <luv@bit.dev>
2022-08-24 11:01:58 -0400

    add missing type annotation to hook

--- scopes/lanes/hooks/use-viewed-lane-id/use-viewed-lane-id.tsx
+++ scopes/lanes/hooks/use-viewed-lane-id/use-viewed-lane-id.tsx
@@ -1,8 +1,9 @@
 import { useMemo } from 'react';
 import { LanesModel } from '@teambit/lanes.ui.models.lanes-model';
 import { useLocation } from '@teambit/base-react.navigation.link';
+import { LaneId } from '@teambit/lane-id';
 
-export function useViewedLaneId() {
+export function useViewedLaneId(): LaneId | undefined {
   const location = useLocation();
   return useMemo(() => location && LanesModel.getLaneIdFromPathname(location.pathname), [location?.pathname]);
 }
