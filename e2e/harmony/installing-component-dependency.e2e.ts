teambit/bit

commit 46e7bd1c79e70e9663dfbf2ed57f7aa321079bc4

fix: install the imported package's dependencies (#6281)

src/consumer/component/dependencies/dependency-resolver/dependencies-resolver.ts
@@ -733,7 +733,7 @@ either, use the ignore file syntax or change the require statement to have a mod
       if (this.overridesDependencies.shouldIgnoreComponent(componentId, fileType)) {
         return;
       }
-      const getExistingId = (): BitId | undefined => {
+      const getExistingIdFromBitMapOrModel = (): BitId | undefined => {
         const existingIds = this.consumer.bitmapIdsFromCurrentLane.filterWithoutVersion(componentId);
         if (existingIds.length === 1) {
           depDebug.versionResolvedFrom = 'BitMap';
@@ -748,7 +748,20 @@ either, use the ignore file syntax or change the require statement to have a mod
         }
         return undefined;
       };
-      const existingId = version ? componentId : getExistingId();
+      const getExistingId = (): BitId | undefined => {
+        // Happens when the dep is not in the node_modules or it's there without a version
+        // (it's there without a version when it's in the workspace and it's linked)
+        if (!version) return getExistingIdFromBitMapOrModel();
+
+        // In case it's resolved from the node_modules, and it's also in the ws policy,
+        // use the resolved version from the node_modules / package folder
+        if (this.isPkgInWorkspacePolicies(compDep.name)) return componentId;
+
+        // If there is a version in the node_modules/package folder, but it's not in the ws policy,
+        // prefer the version from the bitmap/model over the version from the node_modules
+        return getExistingIdFromBitMapOrModel() ?? componentId;
+      };
+      const existingId = getExistingId();
       if (existingId) {
         if (existingId.isEqual(this.componentId)) {
           // happens when one of the component files requires another using module path
@@ -762,6 +775,10 @@ either, use the ignore file syntax or change the require statement to have a mod
         this._pushToMissingComponentsIssues(originFile, componentId);
       }
     });
+  }
+
+  private isPkgInWorkspacePolicies(pkgName: string) {
+    return DependencyResolver.getWorkspacePolicy().dependencies?.[pkgName];
   }
 
   private addImportNonMainIssueIfNeeded(filePath: PathLinuxRelative, dependencyPkgData: ResolvedPackageData) {

e2e/harmony/imported-component-deps.e2e.ts
@@ -1,0 +1,52 @@
+import { expect } from 'chai';
+import path from 'path';
+import Helper from '../../src/e2e-helper/e2e-helper';
+import NpmCiRegistry, { supportNpmCiRegistryTesting } from '../npm-ci-registry';
+
+(supportNpmCiRegistryTesting ? describe : describe.skip)(
+  'installing the right versions of dependencies of a new imported component',
+  function () {
+    this.timeout(0);
+    let scope: string;
+    let helper: Helper;
+    let npmCiRegistry: NpmCiRegistry;
+    before(async () => {
+      helper = new Helper({ scopesOptions: { remoteScopeWithDot: true } });
+      helper.scopeHelper.setNewLocalAndRemoteScopesHarmony();
+      helper.bitJsonc.setupDefault();
+      helper.bitJsonc.setPackageManager(`teambit.dependencies/pnpm`);
+      npmCiRegistry = new NpmCiRegistry(helper);
+      await npmCiRegistry.init();
+      npmCiRegistry.configureCiInPackageJsonHarmony();
+      helper.fixtures.populateComponents(3);
+      scope = `@${helper.scopes.remote.replace('.', '/')}.`;
+      helper.fs.outputFile(`comp1/index.js`, `const comp3 = require("${scope}comp3");`);
+      helper.fs.outputFile(`comp2/index.js`, `const comp3 = require("${scope}comp3");`);
+      helper.command.install();
+      helper.command.compile();
+      helper.command.tagComponent('comp3 comp1');
+      helper.command.export();
+      helper.command.removeComponent('comp1');
+      helper.command.tagComponent('comp3 comp2', undefined, '--unmodified');
+      helper.command.export();
+
+      helper.scopeHelper.reInitLocalScopeHarmony();
+      helper.extensions.bitJsonc.setPackageManager(`teambit.dependencies/pnpm`);
+      helper.scopeHelper.addRemoteScope();
+      helper.bitJsonc.setupDefault();
+      helper.command.import(`${helper.scopes.remote}/comp1`);
+      helper.command.import(`${helper.scopes.remote}/comp2`);
+    });
+    it('should install dependencies from their respective models to the imported components', () => {
+      expect(helper.fs.readJsonFile(`node_modules/${scope}comp3/package.json`).version).to.eq('0.0.1');
+      expect(
+        helper.fs.readJsonFile(
+          path.join(helper.scopes.remoteWithoutOwner, `comp2/node_modules/${scope}comp3/package.json`)
+        ).version
+      ).to.eq('0.0.2');
+    });
+    after(() => {
+      npmCiRegistry.destroy();
+    });
+  }
+);

commit c3f28990be9a47be61ca1a502cdfd50e731e0dbb

fix(dependencies-resolution): consider variants/bitmap-config policy (#6385)
This is a regression caused by https://github.com/teambit/bit/pull/6281.
When a component-dependency is not part of the workspace and it's not in the workspace.jsonc, then, the version is resolved from the model. 
This PR fixes it to take into account "overrides", which are the bitmap-config / component.json / workspace.jsonc-overrides.
If the component is specified there, it uses the package.json in the node_modules to resolve the version from.

src/consumer/component/component-loader.ts
@@ -229,8 +229,7 @@ export default class ComponentLoader {
 
   private async _handleOutOfSyncScenarios(component: Component) {
     const { componentFromModel, componentMap } = component;
-    // $FlowFixMe componentMap is set here
-    // @ts-ignore AUTO-ADDED-AFTER-MIGRATION-PLEASE-FIX!
+    // @ts-ignore componentMap is set here
     const currentId: BitId = componentMap.id;
     let newId: BitId | null | undefined;
     if (componentFromModel && !currentId.hasVersion()) {

src/consumer/component/dependencies/dependency-resolver/dependencies-resolver.ts
@@ -699,9 +699,11 @@ either, use the ignore file syntax or change the require statement to have a mod
         // (it's there without a version when it's in the workspace and it's linked)
         if (!version) return getExistingIdFromBitMapOrModel();
 
-        // In case it's resolved from the node_modules, and it's also in the ws policy,
+        // In case it's resolved from the node_modules, and it's also in the ws policy or variants,
         // use the resolved version from the node_modules / package folder
-        if (this.isPkgInWorkspacePolicies(compDep.name)) return componentId;
+        if (this.isPkgInWorkspacePolicies(compDep.name) || this.isPkgInVariants(compDep.name)) {
+          return componentId;
+        }
 
         // If there is a version in the node_modules/package folder, but it's not in the ws policy,
         // prefer the version from the bitmap/model over the version from the node_modules
@@ -725,6 +727,14 @@ either, use the ignore file syntax or change the require statement to have a mod
 
   private isPkgInWorkspacePolicies(pkgName: string) {
     return DependencyResolver.getWorkspacePolicy().dependencies?.[pkgName];
+  }
+  private isPkgInVariants(pkgName: string): boolean {
+    const dependencies = this.overridesDependencies.getDependenciesToAddManually(undefined, this.allDependencies);
+    if (!dependencies) return false;
+    const { components } = dependencies;
+    return DEPENDENCIES_FIELDS.some(
+      (depField) => components[depField] && components[depField].some((depData) => depData.packageName === pkgName)
+    );
   }
 
   private addImportNonMainIssueIfNeeded(filePath: PathLinuxRelative, dependencyPkgData: ResolvedPackageData) {

e2e/harmony/installing-component-dependency.e2e.ts
@@ -1,5 +1,6 @@
 import { expect } from 'chai';
 import path from 'path';
+import { Extensions } from '../../src/constants';
 import Helper from '../../src/e2e-helper/e2e-helper';
 import NpmCiRegistry, { supportNpmCiRegistryTesting } from '../npm-ci-registry';
 
@@ -42,6 +43,15 @@ import NpmCiRegistry, { supportNpmCiRegistryTesting } from '../npm-ci-registry';
     expect(
       helper.fs.exists(path.join(helper.scopes.remoteWithoutOwner, `comp1/node_modules/${scope}comp2/package.json`))
     ).to.eq(false);
+  });
+  it('should not install the version of the component dependency from the model, when the component dependency is in the bitmap config', () => {
+    helper.command.dependenciesSet('comp1', `${scope}comp2@0.0.2`);
+    helper.command.install();
+    expect(helper.fs.readJsonFile(`node_modules/${scope}comp2/package.json`).version).to.eq('0.0.2');
+    const show = helper.command.showAspectConfig('comp1', Extensions.dependencyResolver);
+    const ids: string[] = show.data.dependencies.map((dep) => dep.id);
+    const comp2 = ids.find((id) => id.includes('comp2'));
+    expect(comp2?.endsWith('0.0.2')).to.be.true;
   });
   after(() => {
     npmCiRegistry.destroy();