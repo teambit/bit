70fa6f7fb6f1d2a2f166cc7bfee1c118e8fb00c8
Gilad Shoham <shoham.gilad@gmail.com>
2022-09-07 17:30:15 +0300

    fixes

--- scopes/webpack/plugins/inject-head-webpack-plugin/inject-head-webpack-plugin.composition.tsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import { injectHeadWebpackPlugin } from './inject-head-webpack-plugin';
-
-export function ReturnsCorrectValue() {
-  return <div>{injectHeadWebpackPlugin()}</div>;
-}
