3bf0b65a52bf01ac7418285d3f7b8a75fa3c98c6
David First <davidnet@gmail.com>
2022-09-08 18:04:59 -0400

    fix(bit-status): avoid showing a component as pending-merge when it is in updates-from-main section

--- scopes/component/merging/merge-cmd.ts
+++ scopes/component/merging/merge-cmd.ts
@@ -164,7 +164,7 @@ once ready, snap/tag the components to complete the merge.`;
       })
     ).join('\n');
     if (!body) {
-      return `${chalk.bold(`the merge has been canceled on ${failedComponents.length} component(s) legitimately`)}
+      return `${chalk.bold(`\nthe merge has been canceled on ${failedComponents.length} component(s) legitimately`)}
 (use --verbose to list them next time)`;
     }
     return `\n${chalk.underline(title)}\n${body}\n\n`;
