18418dcdbcf50497fd86ab3ea833da1a51ae141b
Gilad Shoham <shoham.gilad@gmail.com>
2022-09-11 23:53:32 +0300

    add compositions metadata

--- scopes/compositions/compositions/compositions.preview-definition.ts
+++ scopes/compositions/compositions/compositions.preview-definition.ts
@@ -4,6 +4,8 @@ import { PreviewDefinition } from '@teambit/preview';
 import { AbstractVinyl } from '@teambit/legacy/dist/consumer/component/sources';
 
 import { CompositionsMain } from './compositions.main.runtime';
+import { CompositionBrowserMetadataObject } from './composition';
+
 
 export class CompositionPreviewDefinition implements PreviewDefinition {
   readonly prefix = 'compositions';
@@ -22,5 +24,14 @@ export class CompositionPreviewDefinition implements PreviewDefinition {
   async getModuleMap(components: Component[]): Promise<ComponentMap<AbstractVinyl[]>> {
     const map = this.compositions.getPreviewFiles(components);
     return map;
+  }
+
+  async getMetadata(component: Component): Promise<CompositionBrowserMetadataObject> {
+    const compositions = this.compositions
+      .getCompositions(component)
+      .map((composition) => ({displayName: composition.displayName, identifier: composition.identifier}));
+    return {
+      compositions,
+    };
   }
 }
