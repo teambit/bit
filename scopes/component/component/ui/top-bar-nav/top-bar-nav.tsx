ebce4b89082bad9c56a1944003f2585c37a1bac2
Oded Reuveny <oded.reuveny@gmail.com>
2022-09-13 14:18:05 +0300

    Iframe height fix (#6446)
    * iframe height fix
    
    * composition tab -fixed links to open in new tab
    
    * fixed warning in component top bar caused by widget's `displayName`
    
    * composition tab - fixed composition name param in url to open composition in new tab
    
    * type fix

--- scopes/component/component/ui/top-bar-nav/top-bar-nav.tsx
+++ scopes/component/component/ui/top-bar-nav/top-bar-nav.tsx
@@ -1,14 +1,19 @@
 import React from 'react';
 import classnames from 'classnames';
 import { useLocation } from 'react-router-dom';
-import { Link, LinkProps } from '@teambit/base-react.navigation.link';
+import { Link } from '@teambit/base-react.navigation.link';
+import { NavPluginProps } from '../menu/nav-plugin';
 
 import styles from './top-bar-nav.module.scss';
 
-export type TopBarNavProps = {} & LinkProps;
-
-export function TopBarNav({ href, className, activeClassName, children, ...rest }: TopBarNavProps) {
+export function TopBarNav({ href, className, activeClassName, children, displayName, ...rest }: NavPluginProps) {
   const { search } = useLocation(); // sticky query params
+
+  // @hack - this is so that the displayName will not pass to the link and cause a warning in the console.
+  // it is used for component page nav link widgets (code, aspects etc),
+  // but not for regular menu links (overview, compositions, etc).
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const preventPassingDisplayNameToLink = displayName;
 
   const target = `${href}${search}`;
 
