627cb657a96f765e8d367306486a21c61a9fb9fe
David First <davidnet@gmail.com>
2022-09-13 20:40:31 -0400

    refactor: move Importer class functionality into importer.main file (#6448)
    It was needed previously due to circular dependencies between the Workspace and Importer aspects. Now that "import" stuff has moved to a new aspect "install", it's possible to remove this Importer class.

--- scopes/scope/importer/index.ts
+++ scopes/scope/importer/index.ts
@@ -1,6 +1,5 @@
 import { ImporterAspect } from './importer.aspect';
 
 export type { ImporterMain } from './importer.main.runtime';
-export { Importer } from './importer';
 export default ImporterAspect;
 export { ImporterAspect };
