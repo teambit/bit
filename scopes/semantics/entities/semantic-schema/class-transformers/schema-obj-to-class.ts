7529f21db50f94030a3676e33997068e3030937d
Luv Kapur <luv@bit.dev>
2022-10-19 10:25:45 -0400

    lint fix

--- scopes/semantics/entities/semantic-schema/class-transformers/schema-obj-to-class.ts
+++ scopes/semantics/entities/semantic-schema/class-transformers/schema-obj-to-class.ts
@@ -28,26 +28,21 @@ export function schemaObjToInstance({
 }
 
 function transformFromObjectToInstance(obj: Record<string, any>): SchemaNode {
-  console.log('🚀 ~ file: schema-obj-to-class.ts ~ line 31 ~ transformFromObjectToInstance ~ obj', obj);
   if (!obj.__schema) {
     throw new Error(`fatal: "__schema" is missing, make sure the transformer is set on SchemaNode type`);
   }
   const SchemaClass = getClassBySchemaName(obj.__schema);
-  console.log('🚀 ~ file: schema-obj-to-class.ts ~ line 36 ~ transformFromObjectToInstance ~ SchemaClass', SchemaClass);
   if (!SchemaClass) {
     // for backward and forward compatibility, to not break the users, it's better to return an unknown schema than throwing.
     return new UnknownSchema(obj.location || { path: '', line: 0, character: 0 }, obj.__schema, obj);
   }
-  console.log('🚀 ~ file: schema-obj-to-class.ts ~ line 41 ~ transformFromObjectToInstance ~ SchemaClass', SchemaClass);
 
   return plainToInstance(SchemaClass, obj);
 }
 
 function getClassBySchemaName(schemaName: string): ClassConstructor<SchemaNode> | null {
   const schemasClasses = compact(Object.values(Schemas));
-  console.log('🚀 ~ file: schema-obj-to-class.ts ~ line 48 ~ getClassBySchemaName ~ schemasClasses', schemasClasses);
   const schemaClass = schemasClasses.find((schema) => schema.name === schemaName);
-  console.log('🚀 ~ file: schema-obj-to-class.ts ~ line 50 ~ getClassBySchemaName ~ schemaClass', schemaClass);
   if (!schemaClass) {
     return null;
   }
