c223677be50f574f32ab5d8c1776f1db52cdfa23
Luv Kapur <luv@bit.dev>
2022-10-19 15:44:41 -0400

    make artifact node open in new tab by default

--- scopes/component/ui/component-artifacts/artifacts-tree/artifacts-tree.tsx
+++ scopes/component/ui/component-artifacts/artifacts-tree/artifacts-tree.tsx
@@ -1,15 +1,17 @@
-import React, { HTMLAttributes, useMemo, useState } from 'react';
+import React, { HTMLAttributes, useMemo, useState, useContext } from 'react';
 import classNames from 'classnames';
 import { Icon } from '@teambit/evangelist.elements.icon';
-import { WidgetProps } from '@teambit/ui-foundation.ui.tree.tree-node';
+import { WidgetProps, TreeNode as Node } from '@teambit/ui-foundation.ui.tree.tree-node';
 import { DrawerUI } from '@teambit/ui-foundation.ui.tree.drawer';
-import { FileTree } from '@teambit/ui-foundation.ui.tree.file-tree';
+import { FileTree, useFileTreeContext } from '@teambit/ui-foundation.ui.tree.file-tree';
 import { useComponentArtifacts } from '@teambit/component.ui.artifacts.queries.use-component-artifacts';
 import { useLocation } from '@teambit/base-react.navigation.link';
 import { ArtifactFile } from '@teambit/component.ui.artifacts.models.component-artifacts-model';
-import { TreeNode } from '@teambit/design.ui.tree';
+import { TreeNode, TreeNodeProps } from '@teambit/design.ui.tree';
+import { TreeContext } from '@teambit/base-ui.graph.tree.tree-context';
 import { ComponentTreeLoader } from '@teambit/design.ui.skeletons.sidebar-loader';
 import { ComponentID } from '@teambit/component-id';
+import { FolderTreeNode } from '@teambit/ui-foundation.ui.tree.folder-tree-node';
 
 import styles from './artifacts-tree.module.scss';
 
@@ -21,6 +23,31 @@ export type ArtifactsTreeProps = {
   componentId: ComponentID;
   drawerName: string;
 } & HTMLAttributes<HTMLDivElement>;
+
+const fileNodeClicked = (files: (ArtifactFile & { id: string })[], opts: 'download' | 'new tab') => (e, node) => {
+  const { id } = node;
+  const artifactFile = files.find((file) => file.id === id);
+
+  if (artifactFile?.downloadUrl) {
+    fetch(artifactFile.downloadUrl, { method: 'GET' })
+      .then((res) => res.blob())
+      .then((blob) => {
+        // create blob link to download
+        const url = window.URL.createObjectURL(new Blob([blob]));
+        const link = document.createElement('a');
+        link.href = url;
+        if (opts === 'download') link.setAttribute('download', artifactFile.path);
+        if (opts === 'new tab') link.setAttribute('target', '_blank');
+        // append to html page
+        document.body.appendChild(link);
+        // force download
+        link.click();
+        // clean up and remove the link
+        link.parentNode?.removeChild(link);
+      })
+      .catch(() => {});
+  }
+};
 
 export function ArtifactsTree({
   getIcon,
@@ -79,40 +106,17 @@ export function ArtifactsTree({
           files={artifactFilesTree}
           widgets={widgets}
           payloadMap={payloadMap}
+          TreeNode={FileTreeNode}
           selected={selected}
-          onTreeNodeSelected={(id: string) => {
+          onTreeNodeSelected={(id: string, e) => {
             setSelected(id);
+            fileNodeClicked(files, 'new tab')(e, { id });
           }}
         />
       )}
     </DrawerUI>
   );
 }
-
-const fileNodeClicked = (files: (ArtifactFile & { id: string })[], opts: 'download' | 'new tab') => (_, node) => {
-  const { id } = node;
-  const artifactFile = files.find((file) => file.id === id);
-
-  if (artifactFile?.downloadUrl) {
-    fetch(artifactFile.downloadUrl, { method: 'GET' })
-      .then((res) => res.blob())
-      .then((blob) => {
-        // create blob link to download
-        const url = window.URL.createObjectURL(new Blob([blob]));
-        const link = document.createElement('a');
-        link.href = url;
-        if (opts === 'download') link.setAttribute('download', artifactFile.path);
-        if (opts === 'new tab') link.setAttribute('target', '_blank');
-        // append to html page
-        document.body.appendChild(link);
-        // force download
-        link.click();
-        // clean up and remove the link
-        link.parentNode?.removeChild(link);
-      })
-      .catch(() => {});
-  }
-};
 
 function generateWidget(files: (ArtifactFile & { id: string })[]) {
   return function Widget({ node }: WidgetProps<any>) {
@@ -121,11 +125,42 @@ function generateWidget(files: (ArtifactFile & { id: string })[]) {
     if (artifactFile) {
       return (
         <div className={styles.artifactWidgets}>
-          <Icon className={styles.icon} of="open-tab" onClick={(e) => fileNodeClicked(files, 'new tab')(e, node)} />
-          <Icon className={styles.icon} of="download" onClick={(e) => fileNodeClicked(files, 'download')(e, node)} />
+          {/* <Icon className={styles.icon} of="open-tab" onClick={(e) => fileNodeClicked(files, 'new tab')(e, node)} /> */}
+          <Icon
+            className={styles.icon}
+            of="download"
+            onClick={(e) => {
+              fileNodeClicked(files, 'download')(e, node);
+            }}
+          />
         </div>
       );
     }
     return null;
   };
+}
+
+export function FileTreeNode(props: TreeNodeProps<any>) {
+  const { node } = props;
+  const fileTreeContext = useFileTreeContext();
+  const { selected, onSelect } = useContext(TreeContext);
+
+  const href = fileTreeContext?.getHref?.(node);
+  const widgets = fileTreeContext?.widgets;
+  const icon = fileTreeContext?.getIcon?.(node);
+
+  if (!node?.children) {
+    return (
+      <Node
+        {...props}
+        className={styles.link}
+        onClick={onSelect && ((e) => onSelect(node.id, e))}
+        href={href}
+        isActive={node?.id === selected}
+        icon={icon}
+        widgets={widgets}
+      />
+    );
+  }
+  return <FolderTreeNode {...props} />;
 }
