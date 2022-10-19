36834c2b7b6e08a0c4a028988af92067d1746c45
Luv Kapur <luv@bit.dev>
2022-10-19 13:46:54 -0400

    add copy button

--- scopes/harmony/ui/aspect-box/aspect-box.tsx
+++ scopes/harmony/ui/aspect-box/aspect-box.tsx
@@ -1,8 +1,12 @@
 import classNames from 'classnames';
 import React, { useState } from 'react';
 import JSONFormatter from 'json-formatter-js';
-import styles from './aspect-box.module.scss';
 import { isObject } from 'lodash';
+import { CopiedMessage } from '@teambit/documenter.ui.copied-message';
+import { Icon } from '@teambit/evangelist.elements.icon';
+import copy from 'copy-to-clipboard';
+
+import styles from './aspect-box.module.scss';
 
 export type AspectBoxProps = {
   icon?: string;
@@ -17,6 +21,19 @@ const expandIcon = 'https://static.bit.dev/bit-icons/expand.svg';
 export function AspectBox({ icon, name, config, data, className, ...rest }: AspectBoxProps) {
   const [configCollapseState, setConfigCollapseState] = useState<number>(1);
   const [dataCollapseState, setDataCollapseState] = useState<number>(1);
+  const [isCopiedData, setIsCopiedData] = useState(false);
+  const [isCopiedConfig, setIsCopiedConfig] = useState(false);
+
+  const handleClick = (dataToCopy, dataOrConfig: 'data' | 'config') => () => {
+    if (dataOrConfig === 'data') setIsCopiedData(true);
+    else setIsCopiedConfig(true);
+    setTimeout(() => {
+      if (dataOrConfig === 'data') setIsCopiedData(false);
+      else setIsCopiedConfig(false);
+    }, 20000);
+    copy(JSON.stringify(dataToCopy, null, 2));
+  };
+
   const configCollapseExpandIcon = configCollapseState === 1 ? expandIcon : collapsedIcon;
   const dataCollapseExpandIcon = dataCollapseState === 1 ? expandIcon : collapsedIcon;
   const isDataDepthGreaterThanOne = Object.keys(data).some((key) => isObject(data[key]));
@@ -49,14 +66,23 @@ export function AspectBox({ icon, name, config, data, className, ...rest }: Aspe
       </div>
       <div className={styles.sectionTitleContainer}>
         <div className={styles.sectionTitle}>Configuration</div>
-        {isConfigDepthGreaterThanOne && (
-          <div className={styles.expandCollapse}>
-            <img
-              src={configCollapseExpandIcon}
-              onClick={() => setConfigCollapseState((value) => toggleCollapseState(value))}
-            />
+        <div className={styles.toolbar}>
+          <CopiedMessage className={styles.copyMessage} show={isCopiedConfig} />
+          <div className={styles.copy}>
+            <button className={styles.copyButton} onClick={handleClick(configContent.json, 'config')}>
+              <Icon className={styles.copyIcon} of="copy-cmp" />
+            </button>
           </div>
-        )}
+
+          {isConfigDepthGreaterThanOne && (
+            <div className={styles.expandCollapse}>
+              <img
+                src={configCollapseExpandIcon}
+                onClick={() => setConfigCollapseState((value) => toggleCollapseState(value))}
+              />
+            </div>
+          )}
+        </div>
       </div>
       <div className={classNames(styles.log, styles.config)}>
         <div
@@ -67,14 +93,22 @@ export function AspectBox({ icon, name, config, data, className, ...rest }: Aspe
       </div>
       <div className={styles.sectionTitleContainer}>
         <div className={styles.sectionTitle}>Calculated Data</div>
-        {isDataDepthGreaterThanOne && (
-          <div className={styles.expandCollapse}>
-            <img
-              src={dataCollapseExpandIcon}
-              onClick={() => setDataCollapseState((value) => toggleCollapseState(value))}
-            />
+        <div className={styles.toolbar}>
+          <CopiedMessage className={styles.copyMessage} show={isCopiedData} />
+          <div className={styles.copy}>
+            <button className={styles.copyButton} onClick={handleClick(dataContent.json, 'data')}>
+              <Icon className={styles.copyIcon} of="copy-cmp" />
+            </button>
           </div>
-        )}
+          {isDataDepthGreaterThanOne && (
+            <div className={styles.expandCollapse}>
+              <img
+                src={dataCollapseExpandIcon}
+                onClick={() => setDataCollapseState((value) => toggleCollapseState(value))}
+              />
+            </div>
+          )}
+        </div>
       </div>
       <div className={styles.log}>
         <div
