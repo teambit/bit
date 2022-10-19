98c2a80a24ecf57a5208805fb36310a0e3e0e430
Luv Kapur <luv@bit.dev>
2022-10-19 12:35:04 -0400

    allow expand/collapse for nested config/data in aspect page

--- scopes/harmony/ui/aspect-box/aspect-box.tsx
+++ scopes/harmony/ui/aspect-box/aspect-box.tsx
@@ -1,7 +1,8 @@
 import classNames from 'classnames';
-import React from 'react';
+import React, { useState } from 'react';
 import JSONFormatter from 'json-formatter-js';
 import styles from './aspect-box.module.scss';
+import { isObject } from 'lodash';
 
 export type AspectBoxProps = {
   icon?: string;
@@ -11,12 +12,28 @@ export type AspectBoxProps = {
   data: any;
 } & React.HTMLAttributes<HTMLDivElement>;
 
+const collapsedIcon = 'https://static.bit.dev/bit-icons/collapse.svg';
+const expandIcon = 'https://static.bit.dev/bit-icons/expand.svg';
 export function AspectBox({ icon, name, config, data, className, ...rest }: AspectBoxProps) {
-  const configContent = new JSONFormatter(config, 1, {
+  const [configCollapseState, setConfigCollapseState] = useState<number>(1);
+  const [dataCollapseState, setDataCollapseState] = useState<number>(1);
+  const configCollapseExpandIcon = configCollapseState === 1 ? expandIcon : collapsedIcon;
+  const dataCollapseExpandIcon = dataCollapseState === 1 ? expandIcon : collapsedIcon;
+  const isDataDepthGreaterThanOne = Object.keys(data).some((key) => isObject(data[key]));
+  const isConfigDepthGreaterThanOne = Object.keys(config).some((key) => isObject(data[key]));
+
+  const toggleCollapseState = (state: number) => {
+    if (state === 1) return Infinity;
+    return 1;
+  };
+
+  const configContent = new JSONFormatter(config, configCollapseState, {
     theme: 'dark',
     hoverPreviewEnabled: true,
   });
-  const dataContent = new JSONFormatter(data, 1, { theme: 'dark' });
+
+  const dataContent = new JSONFormatter(data, dataCollapseState, { theme: 'dark' });
+
   return (
     <div {...rest} className={classNames(styles.aspectBox, className)}>
       <div className={styles.titleLine}>
@@ -30,19 +47,39 @@ export function AspectBox({ icon, name, config, data, className, ...rest }: Aspe
           <Icon of="open-tab" />
         </a> */}
       </div>
-      <div className={styles.sectionTitle}>Configuration</div>
+      <div className={styles.sectionTitleContainer}>
+        <div className={styles.sectionTitle}>Configuration</div>
+        {isConfigDepthGreaterThanOne && (
+          <div className={styles.expandCollapse}>
+            <img
+              src={configCollapseExpandIcon}
+              onClick={() => setConfigCollapseState((value) => toggleCollapseState(value))}
+            />
+          </div>
+        )}
+      </div>
       <div className={classNames(styles.log, styles.config)}>
         <div
           ref={(nodeElement) => {
-            nodeElement && nodeElement.appendChild(configContent.render());
+            nodeElement && nodeElement.replaceChildren(configContent.render());
           }}
         />
       </div>
-      <div className={styles.sectionTitle}>Calculated data</div>
+      <div className={styles.sectionTitleContainer}>
+        <div className={styles.sectionTitle}>Calculated Data</div>
+        {isDataDepthGreaterThanOne && (
+          <div className={styles.expandCollapse}>
+            <img
+              src={dataCollapseExpandIcon}
+              onClick={() => setDataCollapseState((value) => toggleCollapseState(value))}
+            />
+          </div>
+        )}
+      </div>
       <div className={styles.log}>
         <div
           ref={(nodeElement) => {
-            nodeElement && nodeElement.appendChild(dataContent.render());
+            nodeElement && nodeElement.replaceChildren(dataContent.render());
           }}
         />
       </div>
