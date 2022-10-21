1f53f909613eec3a85c15b15ede7e460e8aac0e1
Luv Kapur <luv@bit.dev>
2022-10-21 13:57:27 -0400

    clean up

--- scopes/semantics/entities/semantic-schema/schemas/unimplemented-schema.ts
+++ scopes/semantics/entities/semantic-schema/schemas/unimplemented-schema.ts
@@ -1,10 +1,5 @@
 import { Location, SchemaNode } from '../schema-node';
 
-/**
- * needed for better backward and forward compatibility.
- * in case a previous version of the semantic-schema had a schema class that doesn't exist anymore, then it'll be
- * wrapped in this class.
- */
 export class UnImplementedSchema extends SchemaNode {
   constructor(readonly location: Location, readonly name: string, readonly type: string) {
     super();
