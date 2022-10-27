e6461ffade5d7f71a109ab09081d94ca10e45465
Luv Kapur <luv@bit.dev>
2022-10-27 10:21:34 -0400

    move schema to metadata instead of artifact file

--- scopes/semantics/schema/index.ts
+++ scopes/semantics/schema/index.ts
@@ -1,11 +1,5 @@
 export { Parser } from './parser';
 export { SchemaExtractor } from './schema-extractor';
-export {
-  SchemaTask,
-  SCHEMA_ARTIFACT_NAME,
-  SCHEMA_TASK_NAME,
-  getSchemaArtifactDef,
-  getSchemaArtifactPath,
-} from './schema.task';
+export { SchemaTask, SCHEMA_ARTIFACT_NAME, SCHEMA_TASK_NAME } from './schema.task';
 export type { SchemaMain } from './schema.main.runtime';
 export { SchemaAspect } from './schema.aspect';
