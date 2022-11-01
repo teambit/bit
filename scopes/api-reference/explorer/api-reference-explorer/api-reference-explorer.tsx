4153cc54076dc7796c68981dfb663f42b3e80286
Luv Kapur <luv@bit.dev>
2022-11-01 11:20:43 -0400

    remove top level drawer from api explorer

--- scopes/api-reference/explorer/api-reference-explorer/api-reference-explorer.tsx
+++ scopes/api-reference/explorer/api-reference-explorer/api-reference-explorer.tsx
@@ -1,7 +1,6 @@
-import React, { HTMLAttributes, useState } from 'react';
+import React, { HTMLAttributes } from 'react';
 import classNames from 'classnames';
 import { FileTree } from '@teambit/ui-foundation.ui.tree.file-tree';
-import { DrawerUI } from '@teambit/ui-foundation.ui.tree.drawer';
 import { useUpdatedUrlFromQuery } from '@teambit/api-reference.hooks.use-api-ref-url';
 import { TreeNode } from '@teambit/design.ui.tree';
 
@@ -14,24 +13,14 @@ export type APIReferenceExplorerProps = {
 } & HTMLAttributes<HTMLDivElement>;
 
 export function APIReferenceExplorer({ apiTree, selectedAPIName, className, getIcon }: APIReferenceExplorerProps) {
-  const [drawerOpen, onToggleDrawer] = useState(true);
-
   return (
     <div className={classNames(styles.apiReferenceExplorer, className)}>
-      <DrawerUI
-        isOpen={drawerOpen}
-        onToggle={() => onToggleDrawer((open) => !open)}
-        name={'API'}
-        contentClass={styles.apiReferenceExplorerDrawerContent}
-        className={classNames(styles.apiReferenceExplorerTabDrawer)}
-      >
-        <FileTree
-          getHref={(node) => useUpdatedUrlFromQuery({ selectedAPI: node.id })}
-          files={apiTree}
-          selected={selectedAPIName}
-          getIcon={getIcon}
-        />
-      </DrawerUI>
+      <FileTree
+        getHref={(node) => useUpdatedUrlFromQuery({ selectedAPI: node.id })}
+        files={apiTree}
+        selected={selectedAPIName}
+        getIcon={getIcon}
+      />
     </div>
   );
 }
