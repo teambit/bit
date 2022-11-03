08fc1628b66281db0e97533c17226c8f11b4b150
Luv Kapur <luv@bit.dev>
2022-11-03 15:37:43 -0400

    parse arrow functions with explicit types correctly

--- scopes/typescript/typescript/transformers/variable-declaration.ts
+++ scopes/typescript/typescript/transformers/variable-declaration.ts
@@ -30,15 +30,34 @@ export class VariableDeclaration implements SchemaTransformer {
     const displaySig = info?.body?.displayString || '';
     const location = context.getLocation(varDec);
     const doc = await context.jsDocToDocSchema(varDec);
+    const modifiers = varDec.modifiers?.map((modifier) => modifier.getText()) || [];
     if (varDec.initializer?.kind === ts.SyntaxKind.ArrowFunction) {
       const args = await getParams((varDec.initializer as ArrowFunction).parameters, context);
+      // example => export const useLanesContext: () => LanesContextModel | undefined = () => {
+      if (varDec.type) {
+        const funcType = await context.resolveType(varDec, '');
+        if (isFunctionLike(funcType)) {
+          return new FunctionLikeSchema(
+            location,
+            name,
+            funcType.params,
+            funcType.returnType,
+            displaySig,
+            modifiers as Modifier[],
+            doc
+          );
+        }
+      }
       const typeStr = parseReturnTypeFromQuickInfo(info);
       const returnType = await context.resolveType(varDec, typeStr);
-      const modifiers = varDec.modifiers?.map((modifier) => modifier.getText()) || [];
       return new FunctionLikeSchema(location, name, args, returnType, displaySig, modifiers as Modifier[], doc);
     }
     const typeStr = parseTypeFromQuickInfo(info);
     const type = await context.resolveType(varDec, typeStr);
     return new VariableLikeSchema(location, name, displaySig, type, false, doc);
   }
+}
+
+function isFunctionLike(node: SchemaNode): node is FunctionLikeSchema {
+  return node instanceof FunctionLikeSchema;
 }
