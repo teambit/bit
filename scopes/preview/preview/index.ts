bd1152a5805224d5a9bce3f401893cc00312f08a
Guy Saar <guy@bit.dev>
2022-11-03 23:50:28 +0200

    Express route priority (#6630)
    * add route priority
    
    * change builder types
    
    * try fix

--- scopes/preview/preview/index.ts
+++ scopes/preview/preview/index.ts
@@ -1,7 +1,13 @@
 export { PreviewAspect as default, PreviewAspect, PreviewRuntime } from './preview.aspect';
 
 export * from './events';
-export type { PreviewMain, EnvPreviewConfig, ComponentPreviewSize, PreviewStrategyName } from './preview.main.runtime';
+export type {
+  PreviewMain,
+  EnvPreviewConfig,
+  ComponentPreviewSize,
+  PreviewStrategyName,
+  PreviewFiles,
+} from './preview.main.runtime';
 export type { PreviewPreview, RenderingContextOptions, RenderingContextProvider } from './preview.preview.runtime';
 export { PreviewDefinition } from './preview-definition';
 export type { PreviewModule, ModuleFile } from './types/preview-module';
