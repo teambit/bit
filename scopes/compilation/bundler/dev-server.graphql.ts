73710bdb91813a96703812e5fb0329938ec2ea3c
Gilad Shoham <shoham.gilad@gmail.com>
2022-11-14 22:08:03 +0200

    undo unrelated changes in dev-server.graphql

--- scopes/compilation/bundler/dev-server.graphql.ts
+++ scopes/compilation/bundler/dev-server.graphql.ts
@@ -23,13 +23,13 @@ export function devServerSchema(bundler: BundlerMain): Schema {
           // This is a bit of a hack to get the requested id. it assumes the variable name of
           // the gotHost.get query is "id".
           // see it in scopes/component/component/component.graphql.ts
-          // const requestedId = context.body.variables.id;
+          const requestedId = context.body.variables.id;
           // if we ask for specific id with specific version it means we want to fetch if from scope
           // so don't return the server url
           // see https://github.com/teambit/bit/issues/5328
-          // if (requestedId && requestedId.includes('@')) {
-          //   return {};
-          // }
+          if (requestedId && requestedId.includes('@')) {
+            return {};
+          }
           const componentServer = bundler.getComponentServer(component);
           if (!componentServer) return {};
 
