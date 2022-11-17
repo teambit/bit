c0331bf0df43fe42b85d9313c60a9c9dfcad33d6
David First <davidnet@gmail.com>
2022-11-17 17:55:09 -0500

    fix(bit-reset): fix reset when the component is diverged on lane and has no head on main (#6678)

---
+++ e2e/harmony/lanes/diverged-from-remote-lane.e2e.ts
@@ -0,0 +1,34 @@
+import chai, { expect } from 'chai';
+import Helper from '../../../src/e2e-helper/e2e-helper';
+
+chai.use(require('chai-fs'));
+
+describe('local is diverged from the remote', function () {
+  this.timeout(0);
+  let helper: Helper;
+  before(() => {
+    helper = new Helper();
+    helper.scopeHelper.setNewLocalAndRemoteScopes();
+    helper.bitJsonc.setupDefault();
+    helper.command.createLane();
+    helper.fixtures.populateComponents(1, false);
+    helper.command.snapAllComponentsWithoutBuild();
+    helper.command.export();
+    const beforeDiverge = helper.scopeHelper.cloneLocalScope();
+    helper.command.snapAllComponentsWithoutBuild('--unmodified');
+    helper.command.export();
+    helper.scopeHelper.getClonedLocalScope(beforeDiverge);
+    helper.command.snapAllComponentsWithoutBuild('--unmodified');
+    helper.command.import();
+  });
+  after(() => {
+    helper.scopeHelper.destroy();
+  });
+  it('bit status should show it as merge-pending', () => {
+    const status = helper.command.statusJson();
+    expect(status.mergePendingComponents).to.have.lengthOf(1);
+  });
+  it('bit reset should not throw', () => {
+    expect(() => helper.command.untagAll()).to.not.throw();
+  });
+});
