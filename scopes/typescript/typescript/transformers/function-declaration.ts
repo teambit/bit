c2184a2bce81621868d93ea840e6698042290898
Luv Kapur <luv@bit.dev>
2022-11-22 14:07:27 -0500

    expand function declaration transformer to support function like nodes

--- scopes/typescript/typescript/transformers/function-declaration.ts
+++ scopes/typescript/typescript/transformers/function-declaration.ts
@@ -1,5 +1,5 @@
 import { SchemaNode } from '@teambit/semantics.entities.semantic-schema';
-import ts, { Node, FunctionDeclaration as FunctionDeclarationNode } from 'typescript';
+import ts, { Node, SignatureDeclaration } from 'typescript';
 import { SchemaExtractorContext } from '../schema-extractor-context';
 import { SchemaTransformer } from '../schema-transformer';
 import { ExportIdentifier } from '../export-identifier';
@@ -7,19 +7,28 @@ import { toFunctionLikeSchema } from './utils/to-function-like-schema';
 
 export class FunctionDeclaration implements SchemaTransformer {
   predicate(node: Node) {
-    return node.kind === ts.SyntaxKind.FunctionDeclaration || node.kind === ts.SyntaxKind.ArrowFunction;
+    return (
+      node.kind === ts.SyntaxKind.FunctionDeclaration ||
+      node.kind === ts.SyntaxKind.ArrowFunction ||
+      node.kind === ts.SyntaxKind.MethodDeclaration ||
+      node.kind === ts.SyntaxKind.CallSignature ||
+      node.kind === ts.SyntaxKind.ConstructSignature ||
+      node.kind === ts.SyntaxKind.IndexSignature ||
+      node.kind === ts.SyntaxKind.FunctionType
+    );
   }
 
   // need to check for anonymous functions assigned for vars, const and let.
-  async getIdentifiers(funcDec: FunctionDeclarationNode) {
+  async getIdentifiers(funcDec: SignatureDeclaration) {
     return [new ExportIdentifier(this.getName(funcDec), funcDec.getSourceFile().fileName)];
   }
 
-  private getName(funcDec: FunctionDeclarationNode) {
+  private getName(funcDec: SignatureDeclaration) {
+    if (funcDec.kind === ts.SyntaxKind.ConstructSignature) return 'new';
     return funcDec.name?.getText() || '';
   }
 
-  async transform(funcDec: FunctionDeclarationNode, context: SchemaExtractorContext): Promise<SchemaNode> {
+  async transform(funcDec: SignatureDeclaration, context: SchemaExtractorContext): Promise<SchemaNode> {
     return toFunctionLikeSchema(funcDec, context);
   }
 }
