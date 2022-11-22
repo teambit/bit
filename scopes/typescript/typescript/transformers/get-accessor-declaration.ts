0b9cc545de4dfb23ce6218973cc92dd65c793436
Luv Kapur <luv@bit.dev>
2022-11-22 14:21:28 -0500

    Get Accessor Declaration Transformer

---
+++ scopes/typescript/typescript/transformers/get-accessor-declaration.ts
@@ -0,0 +1,24 @@
+import ts, { Node, GetAccessorDeclaration } from 'typescript';
+import { GetAccessorSchema } from '@teambit/semantics.entities.semantic-schema';
+import { SchemaTransformer } from '../schema-transformer';
+import { SchemaExtractorContext } from '../schema-extractor-context';
+import { ExportIdentifier } from '../export-identifier';
+import { parseTypeFromQuickInfo } from './utils/parse-type-from-quick-info';
+
+export class GetAccessorDeclarationTransformer implements SchemaTransformer {
+  predicate(node: Node) {
+    return node.kind === ts.SyntaxKind.GetAccessor;
+  }
+
+  async getIdentifiers(): Promise<ExportIdentifier[]> {
+    return [];
+  }
+
+  async transform(node: GetAccessorDeclaration, context: SchemaExtractorContext) {
+    const info = await context.getQuickInfo(node.name);
+    const displaySig = info?.body?.displayString || '';
+    const typeStr = parseTypeFromQuickInfo(info);
+    const type = await context.resolveType(node, typeStr);
+    return new GetAccessorSchema(context.getLocation(node), node.name.getText(), type, displaySig);
+  }
+}
--- scopes/typescript/typescript/transformers/index.ts
+++ scopes/typescript/typescript/transformers/index.ts
@@ -9,6 +9,7 @@ export { ClassDeclarationTransformer } from './class-declaration';
 export { ConstructorDeclarationTransformer } from './contructor-declaration';
 export { PropertyDeclarationTransformer } from './property-declaration';
 export { SetAccessorDeclarationTransformer } from './set-accessor-declaration';
+export { GetAccessorDeclarationTransformer } from './get-accessor-declaration';
 export { InterfaceDeclarationTransformer } from './interface-declaration';
 export { EnumDeclarationTransformer } from './enum-declaration';
 export { BindingElementTransformer } from './binding-element';
