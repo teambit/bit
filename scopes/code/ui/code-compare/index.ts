34560bac554ff7b785ff1f6b8a76890a7e95d96f
Luv Kapur <luv@bit.dev>
2022-12-03 20:27:37 -0500

    lint fix

--- scopes/code/ui/code-compare/index.ts
+++ scopes/code/ui/code-compare/index.ts
@@ -2,4 +2,3 @@ export { CodeCompare, CodeCompareProps } from './code-compare';
 export { CodeCompareTree, CodeCompareTreeProps } from './code-compare-tree/code-compare-tree';
 export { CodeCompareView, CodeCompareViewProps } from './code-compare-view/code-compare-view';
 export { CodeCompareModel, CodeCompareContext, useCodeCompare } from './code-compare-context';
-export { CompareDependencyTree, CompareDependencyTreeProps } from './code-compare-dependency-tree';
