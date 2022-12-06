501da335c37ba5dcaf3b73c099689ee1885ec34e
David First <davidnet@gmail.com>
2022-12-06 13:16:41 -0500

    improvement(lanes): add an API to get lanes diff

--- scopes/lanes/lanes/lanes.spec.ts
+++ scopes/lanes/lanes/lanes.spec.ts
@@ -4,7 +4,7 @@ import SnappingAspect, { SnappingMain } from '@teambit/snapping';
 import { mockWorkspace, destroyWorkspace, WorkspaceData } from '@teambit/workspace.testing.mock-workspace';
 import { mockComponents, modifyMockedComponents } from '@teambit/component.testing.mock-components';
 import { LanesAspect } from './lanes.aspect';
-import { LanesMain } from './lanes.main.runtime';
+import { ChangeType, LanesMain } from './lanes.main.runtime';
 
 describe('LanesAspect', function () {
   this.timeout(0);
@@ -62,6 +62,44 @@ describe('LanesAspect', function () {
       await snapping.snap({ pattern: 'comp1', build: false, unmodified: true });
       const isUpToDate = await lanes.isLaneUpToDate(currentLane);
       expect(isUpToDate).to.be.false;
+    });
+  });
+
+  describe('laneDiff', () => {
+    let lanes: LanesMain;
+    let snapping: SnappingMain;
+    let workspaceData: WorkspaceData;
+    before(async () => {
+      workspaceData = mockWorkspace();
+      const { workspacePath } = workspaceData;
+      await mockComponents(workspacePath);
+      snapping = await loadAspect(SnappingAspect, workspacePath);
+      await snapping.tag({ ids: ['comp1'], build: false });
+      lanes = await loadAspect(LanesAspect, workspacePath);
+      await lanes.createLane('stage');
+      const result = await snapping.snap({ pattern: 'comp1', build: false, unmodified: true });
+      // intermediate step, make sure it is snapped
+      expect(result?.snappedComponents.length).to.equal(1);
+    });
+    after(async () => {
+      await destroyWorkspace(workspaceData);
+    });
+    it('should return that the lane is up to date when the lane is ahead of main', async () => {
+      const currentLane = await lanes.getCurrentLane();
+      if (!currentLane) throw new Error('unable to get the current lane');
+      const laneDiffResults = await lanes.laneDiff(currentLane.toLaneId());
+      expect(laneDiffResults[0].upToDate).to.be.true;
+      expect(laneDiffResults[0].changeType).to.equal(ChangeType.NONE);
+    });
+    it('should return that the lane is not up to date when main is ahead', async () => {
+      const currentLane = await lanes.getCurrentLane();
+      if (!currentLane) throw new Error('unable to get the current lane');
+      await lanes.switchLanes('main', { skipDependencyInstallation: true });
+      await snapping.snap({ pattern: 'comp1', build: false, unmodified: true });
+
+      const laneDiffResults = await lanes.laneDiff(currentLane.toLaneId());
+      expect(laneDiffResults[0].upToDate).to.be.false;
+      expect(laneDiffResults[0].changeType).to.equal(ChangeType.NONE);
     });
   });
 });
