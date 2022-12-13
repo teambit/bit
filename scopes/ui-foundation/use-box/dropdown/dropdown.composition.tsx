e0335ffd8519d0fcf3fdffffbcb55d2cd3f5d96e
Oded Reuveny <oded.reuveny@gmail.com>
2022-12-13 10:28:32 +0200

    usebox z-index fix and preview onload event (#6783)

--- scopes/ui-foundation/use-box/dropdown/dropdown.composition.tsx
+++ scopes/ui-foundation/use-box/dropdown/dropdown.composition.tsx
@@ -4,13 +4,27 @@ import { UseBoxDropdown } from './dropdown';
 
 const methods = [
   {
-    Title: <img style={{ width: '30px' }} src="https://static.bit.dev/brands/logo-npm-new.svg" />,
-    Component: <div>install content</div>,
+    Title: (
+      <img
+        style={{ width: '30px' }}
+        src='https://static.bit.dev/brands/logo-npm-new.svg'
+      />
+    ),
+    Component: (
+      <div>install content</div>
+    ),
     order: 1,
   },
   {
-    Title: <img style={{ width: '20px' }} src="https://static.bit.dev/brands/bit-logo-text.svg" />,
-    Component: <div>import content</div>,
+    Title: (
+      <img
+        style={{ width: '20px' }}
+        src='https://static.bit.dev/brands/bit-logo-text.svg'
+      />
+    ),
+    Component: (
+      <div>import content</div>
+    ),
     order: 0,
   },
 ];
@@ -20,7 +34,7 @@ export const UseBoxExample = () => {
     <ThemeCompositions>
       <div style={{ width: 'fit-content', float: 'right' }}>
         <UseBoxDropdown
-          position="bottom-end"
+          position='bottom-end'
           Menu={
             <div>
               {methods.map(({ Title, Component }, index) => (
