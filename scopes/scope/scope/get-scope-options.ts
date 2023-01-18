d37e81b2baa64d7b717662a60bbdab2d7eff43e8
Luv Kapur <luv@bit.dev>
2023-01-18 16:58:27 -0500

    clean up  scopes/scope/scope/get-scope-options.ts

--- scopes/scope/scope/get-scope-options.ts
+++ scopes/scope/scope/get-scope-options.ts
@@ -1,5 +1,3 @@
-import { ComponentModel } from '@teambit/component';
-import { LanesModel } from '@teambit/lanes.ui.models.lanes-model';
 import { DrawerType } from '@teambit/ui-foundation.ui.tree.drawer';
 import { ComponentType, ReactNode } from 'react';
 import { ScopeModel } from '.';
@@ -11,10 +9,5 @@ export type GetScopeOptions = {
   scopeClassName?: string;
   TargetScopeOverview?: ComponentType;
   PaneWrapper?: ComponentType<{ children: ReactNode }>;
-  // drawerSlotProps?: {
-  //   assumeScopeInUrl?: boolean;
-  //   overrideUseComponents?: () => { components: ComponentModel[] };
-  //   useLanes?: () => { lanesModel: LanesModel };
-  // };
   overrideDrawers?: DrawerType[];
 };
