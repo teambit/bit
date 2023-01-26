77d784acab5dbfbed1331bbe530efca1ffa41873
Luv Kapur <luv@bit.dev>
2023-01-26 13:18:48 -0500

    lane compare styles

--- scopes/lanes/ui/compare/lane-compare/lane-compare.tsx
+++ scopes/lanes/ui/compare/lane-compare/lane-compare.tsx
@@ -286,8 +286,8 @@ export function LaneCompare({
 function displayChangeType(changeType: ChangeType): string {
   switch (changeType) {
     case ChangeType.SOURCE_CODE:
-      return 'Code';
+      return 'code';
     default:
-      return changeType;
+      return changeType.toLowerCase();
   }
 }
