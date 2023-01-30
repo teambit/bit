fd0feb73d80cb07a0b2fb24b505086cbe0cac540
David First <davidnet@gmail.com>
2023-01-30 12:49:30 -0500

    refactor, remove legacy link-file and link-content files (#6977)
    * refactor, remove legacy link-file
    
    * remove forDistOutsideComponentsDir prop from Symlink class
    
    * remove legacy link-content

--- src/links/index.ts
+++ src/links/index.ts
@@ -1,5 +1,4 @@
-import LinkFile from './link-file';
 import { linkAllToNodeModules } from './linker';
 import NodeModuleLinker from './node-modules-linker';
 
-export { NodeModuleLinker, linkAllToNodeModules, LinkFile };
+export { NodeModuleLinker, linkAllToNodeModules };
