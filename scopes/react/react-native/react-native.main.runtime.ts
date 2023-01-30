6f971a2eb35ee17666b1913e578b26d7601b4321
guysaar223 <guy@bit.dev>
2023-01-30 11:31:25 +0200

    one if

--- scopes/react/react-native/react-native.main.runtime.ts
+++ scopes/react/react-native/react-native.main.runtime.ts
@@ -123,8 +123,12 @@ export class ReactNativeMain {
       react.compose([react.useWebpack(webpackModifiers), react.overrideJestConfig(jestConfig)])
     );
     envs.registerEnv(reactNativeComposedEnv);
-    if (generator) generator.registerComponentTemplate(componentTemplates);
-    if (generator) generator.registerWorkspaceTemplate(workspaceTemplates);
+
+    if (generator) {
+      generator.registerComponentTemplate(componentTemplates);
+      generator.registerWorkspaceTemplate(workspaceTemplates);
+    }
+
     return new ReactNativeMain(react, reactNativeComposedEnv, envs);
   }
 }
--- scopes/react/react/react.main.runtime.ts
+++ scopes/react/react/react.main.runtime.ts
@@ -443,8 +443,11 @@ export class ReactMain {
     const react = new ReactMain(reactEnv, envs, application, appType, dependencyResolver, logger);
     graphql.register(reactSchema(react));
     envs.registerEnv(reactEnv);
-    if (generator) generator.registerComponentTemplate(componentTemplates);
-    if (generator) generator.registerWorkspaceTemplate(workspaceTemplates);
+    if (generator) {
+      generator.registerComponentTemplate(componentTemplates);
+      generator.registerWorkspaceTemplate(workspaceTemplates);
+    }
+
     if (application) application.registerAppType(appType);
 
     return react;
