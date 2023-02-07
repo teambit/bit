fc9c7361f007dd9fcce565a826258ba3b5129d90
David First <davidnet@gmail.com>
2023-02-07 07:59:29 -0500

    fix pkg-json generation during tag

--- scopes/pkg/pkg/pkg.main.runtime.ts
+++ scopes/pkg/pkg/pkg.main.runtime.ts
@@ -475,11 +475,9 @@ export class PkgMain implements CloneConfig {
     legacyComponent: LegacyComponent,
     packageJsonObject: Record<string, any>
   ): Promise<Record<string, any>> {
-    // const newId = await this.workspace.resolveComponentId(component.id);
-    // const newComponent = await this.workspace.get(newId);
     const host = this.componentAspect.getHost();
-    const id = await host.resolveComponentId(legacyComponent.id);
-    const newComponent = await host.get(id);
+    const components = await host.getManyByLegacy([legacyComponent]);
+    const newComponent = components[0];
     if (!newComponent) throw new Error(`cannot transform package.json of component: ${legacyComponent.id.toString()}`);
     const newProps = this.getPackageJsonModifications(newComponent);
     return Object.assign(packageJsonObject, newProps);
