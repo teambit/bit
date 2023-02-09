2fc6cd5c7374dc03ab9499bade7a1900626f79fc
David First <davidnet@gmail.com>
2023-02-09 17:59:00 -0500

    fix tests

--- scopes/workspace/modules/node-modules-linker/node-modules-linker.ts
+++ scopes/workspace/modules/node-modules-linker/node-modules-linker.ts
@@ -53,7 +53,7 @@ export default class NodeModuleLinker {
     this.dataToPersist = new DataToPersist();
     await pMapSeries(this.components, async (component) => {
       const componentId = component.id.toString();
-      if (!this.bitMap.isExistWithSameVersion(component.id._legacy)) {
+      if (!this.bitMap.getComponentIfExist(component.id._legacy)) {
         logger.debug(`skip linking component to node_modules: ${componentId}. it's not part of the workspace`);
         return;
       }
