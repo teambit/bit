b5227065ff70d374cd445fad88654b09d606153d
Luv Kapur <luv@bit.dev>
2023-02-27 15:50:11 -0500

    Workspace: Components Drawer: Local changes to Lane Components (#7072)

--- scopes/workspace/workspace/component-tree.widget.tsx
+++ scopes/workspace/workspace/component-tree.widget.tsx
@@ -1,22 +1,9 @@
+import React from 'react';
 import { ComponentTreeNode, ComponentTreeNodeProps } from '@teambit/component-tree';
 import { ComponentStatusResolver } from '@teambit/component.ui.component-status-resolver';
-import { useLanes } from '@teambit/lanes.hooks.use-lanes';
-import React, { useMemo } from 'react';
 
 export class ComponentTreeWidget implements ComponentTreeNode {
   widget = ({ component }: ComponentTreeNodeProps) => {
-    const { lanesModel } = useLanes();
-
-    const isInCurrentLane = useMemo(() => {
-      return component.id && lanesModel?.isComponentOnLaneButNotOnMain(component.id);
-    }, [lanesModel?.viewedLane, component.id]);
-
-    return (
-      <ComponentStatusResolver
-        status={component.status}
-        issuesCount={component.issuesCount}
-        isInCurrentLane={isInCurrentLane}
-      />
-    );
+    return <ComponentStatusResolver status={component.status} issuesCount={component.issuesCount} />;
   };
 }
