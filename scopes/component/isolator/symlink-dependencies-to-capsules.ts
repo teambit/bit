74974b52a9e1dffbf91cd03b122668651b6227fb
Zoltan Kochan <z@kochan.io>
2023-03-19 20:00:32 +0200

    refactor: don't symlink in symlink-dependencies-to-capsules

--- scopes/component/isolator/symlink-dependencies-to-capsules.ts
+++ scopes/component/isolator/symlink-dependencies-to-capsules.ts
@@ -3,7 +3,6 @@ import { LinkDetail } from '@teambit/dependency-resolver';
 import { Logger } from '@teambit/logger';
 import { BitId } from '@teambit/legacy-bit-id';
 import ConsumerComponent from '@teambit/legacy/dist/consumer/component';
-import Symlink from '@teambit/legacy/dist/links/symlink';
 import componentIdToPackageName from '@teambit/legacy/dist/utils/bit/component-id-to-package-name';
 import path from 'path';
 
@@ -31,22 +30,15 @@ export async function symlinkOnCapsuleRoot(
   capsuleRoot: string
 ): Promise<LinkDetail[]> {
   const modulesPath = path.join(capsuleRoot, 'node_modules');
-  const linkResults: LinkDetail[] = [];
-  const symlinks = capsuleList.map((capsule) => {
+  return capsuleList.map((capsule) => {
     const packageName = componentIdToPackageName(capsule.component.state._consumer);
     const dest = path.join(modulesPath, packageName);
-    const src = path.relative(path.resolve(dest, '..'), capsule.path);
-
-    linkResults.push({
+    return {
       from: capsule.path,
       to: dest,
       packageName,
-    });
-    return new Symlink(src, dest, capsule.component.id._legacy);
+    };
   });
-
-  await Promise.all(symlinks.map((symlink) => symlink.write()));
-  return linkResults;
 }
 
 async function symlinkComponent(
@@ -57,8 +49,7 @@ async function symlinkComponent(
   const componentCapsule = capsuleList.getCapsuleIgnoreScopeAndVersion(new ComponentID(component.id));
   if (!componentCapsule) throw new Error(`unable to find the capsule for ${component.id.toString()}`);
   const allDeps = component.getAllDependenciesIds();
-  const linkResults: Record<string, string> = {};
-  const symlinks = allDeps.map((depId: BitId) => {
+  const linkResults = allDeps.reduce((acc, depId: BitId) => {
     // TODO: this is dangerous - we might have 2 capsules for the same component with different version, then we might link to the wrong place
     const devCapsule = capsuleList.getCapsuleIgnoreScopeAndVersion(new ComponentID(depId));
     if (!devCapsule) {
@@ -66,18 +57,13 @@ async function symlinkComponent(
       logger.debug(
         `symlinkComponentToCapsule: unable to find the capsule for ${depId.toStringWithoutVersion()}. skipping`
       );
-      return null;
+      return acc;
     }
     const packageName = componentIdToPackageName(devCapsule.component.state._consumer);
     const devCapsulePath = devCapsule.path;
-    // @todo: this is a hack, the capsule should be the one responsible to symlink, this works only for FS capsules.
-    const dest = path.join(componentCapsule.path, 'node_modules', packageName);
-    // use relative symlink in capsules to make it really isolated from the machine fs
-    const src = path.relative(path.resolve(dest, '..'), devCapsulePath);
-    linkResults[packageName] = `link:${devCapsulePath}`;
-    return new Symlink(src, dest, component.id);
-  });
+    acc[packageName] = `link:${devCapsulePath}`;
+    return acc;
+  }, {});
 
-  await Promise.all(symlinks.map((symlink) => symlink && symlink.write()));
   return [componentCapsule.path, linkResults];
 }
