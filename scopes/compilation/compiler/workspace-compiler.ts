teambit/bit

commit e392324b2883d7e5816dbc7f22effbf0cff8e94e

fix(compile): avoid ignoring components that their ComponentMap is missing  (#7183)
from the ConsumerComponent instance.
Although it should not happen. It did happened due to a different bug.
Regardless, no need to check for this rootDir anymore, because on
Harmony all components have rootDir.

scopes/compilation/compiler/workspace-compiler.ts
@@ -318,9 +318,8 @@ export class WorkspaceCompiler {
     components.forEach((c) => {
       const environment = this.envs.getEnv(c).env;
       const compilerInstance = environment.getCompiler?.();
-      // if there is no componentDir (e.g. author that added files, not dir), then we can't write the dists
-      // inside the component dir.
-      if (compilerInstance && c.state._consumer.componentMap?.getComponentDir()) {
+
+      if (compilerInstance) {
         const compilerName = compilerInstance.constructor.name || 'compiler';
         componentsCompilers.push(
           new ComponentCompiler(
@@ -333,6 +332,8 @@ export class WorkspaceCompiler {
             this.dependencyResolver
           )
         );
+      } else {
+        this.logger.warn(`unable to find a compiler instance for ${c.id.toString()}`);
       }
     });
     const resultOnWorkspace = await mapSeries(componentsCompilers, (componentCompiler) =>

commit 41cca5ec276f7a82c9a68fe304e08c6bef083249

fix, avoid compiling scope components (#7188)
This is a regression caused by https://github.com/teambit/bit/pull/7183.
When a component is coming from the scope (happens when loadAspects from
scope fails, which is register to `onAspectLoadErrorSlot`), don't try to
compile it, otherwise, it throws ComponentNotFound.

scopes/compilation/compiler/workspace-compiler.ts
@@ -365,7 +365,8 @@ export class WorkspaceCompiler {
     changed = false
   ): Promise<ComponentID[]> {
     if (componentsIds.length) {
-      return this.workspace.resolveMultipleComponentIds(componentsIds);
+      const componentIds = await this.workspace.resolveMultipleComponentIds(componentsIds);
+      return this.workspace.filterIds(componentIds);
     }
     if (changed) {
       return this.workspace.getNewAndModifiedIds();

scopes/workspace/workspace/workspace.ts
@@ -359,6 +359,14 @@ export class Workspace implements ComponentFactory {
   }
 
   /**
+   * given component-ids, return the ones that are part of the workspace
+   */
+  async filterIds(ids: ComponentID[]): Promise<ComponentID[]> {
+    const workspaceIds = await this.listIds();
+    return ids.filter((id) => workspaceIds.find((wsId) => wsId.isEqual(id)));
+  }
+
+  /**
    * whether or not a workspace has a component with the given name
    */
   async hasName(name: string): Promise<boolean> {