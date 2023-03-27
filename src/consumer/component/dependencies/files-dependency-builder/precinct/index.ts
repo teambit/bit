1ab5f47e93581675b22c2cc696729ff1e9179f24
Jinjiang <zhaojinjiang@me.com>
2023-03-27 11:49:35 +0800

    refactor: precinct code

--- src/consumer/component/dependencies/files-dependency-builder/precinct/index.ts
+++ src/consumer/component/dependencies/files-dependency-builder/precinct/index.ts
@@ -17,17 +17,33 @@ import detectiveSass from '../detectives/detective-sass';
 import detectiveScss from '../detectives/detective-scss';
 import detectiveTypeScript from '../detectives/detective-typescript';
 
-import { SUPPORTED_EXTENSIONS } from '../../../../../constants';
 import { BuiltinDeps, DependencyDetector, DetectorHook } from '../detector-hook';
+
+/**
+ * The file info object.
+ * - `ext` is the file extension.
+ * - `content` is the input file content.
+ * - `type` is the parsed file type from ext or content.
+ * - `ast` is the eventually consumed content by the corresponding detective.
+ */
+type FileInfo = {
+  ext: string;
+  content: string | object;
+  type: string;
+  ast: any;
+};
 
 type Options = {
   envDetectors?: DependencyDetector[];
   useContent?: boolean;
   includeCore?: boolean;
+  type?: string;
   [lang: string]: any;
 };
 
 type Detective = (fileContent: string | object, options?: any) => BuiltinDeps;
+
+const jsExt = ['.js', '.jsx', '.cjs', '.mjs'];
 
 const extToType = {
   '.css': 'css',
@@ -40,8 +56,6 @@ const extToType = {
   '.ts': 'ts',
   '.tsx': 'ts',
 };
-
-const jsExt = ['.js', '.jsx', '.cjs', '.mjs'];
 
 const typeToDetective: Record<string, Detective> = {
   css: detectiveCss,
@@ -62,19 +76,40 @@ const natives = process.binding('natives');
 
 const detectorHook = new DetectorHook();
 
-type FileInfo = {
-  ext: string;
-  content: string | object;
-  ast: any;
-  type: string;
+const assign = (o1, o2) => {
+  // eslint-disable-next-line
+  for (const key in o2) {
+    // eslint-disable-next-line
+    if (o2.hasOwnProperty(key)) {
+      o1[key] = o2[key];
+    }
+  }
+
+  return o1;
 };
 
+/**
+ * Get file info from the given file path.
+ */
 const getFileInfo = (filename: string): FileInfo => {
   const ext = path.extname(filename);
   const content = fs.readFileSync(filename, 'utf8');
-  return { ext, content, ast: content, type: '' };
+  return {
+    ext,
+    content,
+    // determined later
+    type: '',
+    // initialized with the content
+    ast: content,
+  };
 };
 
+/**
+ * Get the non-JS detective for the given file info.
+ * The type of the file would be determined as well.
+ * Return undefined if no proper detective found, perhaps it's plain
+ * JavaScript or unknown content. We can deal with it later.
+ */
 const getDetector = (fileInfo: FileInfo, options?: Options): Detective | undefined => {
   const { ext } = fileInfo;
   const normalizedOptions: Options = options || {};
@@ -90,7 +125,8 @@ const getDetector = (fileInfo: FileInfo, options?: Options): Detective | undefin
   }
 
   // from builtin detectors
-  const type = extToType[ext];
+  // - check `fileInfo.type` first to support `precinct(content, { type })`
+  const type = fileInfo.type || extToType[ext];
   if (typeToDetective[type]) {
     const detective = typeToDetective[type];
     fileInfo.type = type;
@@ -115,6 +151,10 @@ const getDetector = (fileInfo: FileInfo, options?: Options): Detective | undefin
   return undefined;
 };
 
+/**
+ * Get the JS detective (amd/es6/cjs) for the given file info.
+ * The type and ast of the file would be determined as well.
+ */
 const getJsDetector = (fileInfo: FileInfo, options?: Options): Detective | undefined => {
   if (!jsExt.includes(fileInfo.ext)) {
     return undefined;
@@ -137,6 +177,9 @@ const getJsDetector = (fileInfo: FileInfo, options?: Options): Detective | undef
   return detector;
 };
 
+/**
+ * Normalize the deps into an array.
+ */
 const normalizeDeps = (deps: BuiltinDeps, includeCore?: boolean): string[] => {
   const normalizedDeps = Array.isArray(deps) ? deps : Object.keys(deps);
   return includeCore ? normalizedDeps : normalizedDeps.filter((d) => !natives[d]);
@@ -158,8 +201,6 @@ const getDepsFromFile = (filename: string, options?: Options): string[] => {
   return normalizeDeps(deps, normalizedOptions?.includeCore);
 };
 
-// Legacy implementation
-
 /**
  * Finds the list of dependencies for the given file
  *
@@ -169,188 +210,28 @@ const getDepsFromFile = (filename: string, options?: Options): string[] => {
  * @return {String[]}
  */
 // eslint-disable-next-line complexity
-function precinct(content, options) {
-  options = options || {};
-  let dependencies = [];
-  let ast;
-  let type = options.type;
-
+const precinct = (content: string | object, options?: string | Options): BuiltinDeps => {
   // Legacy form backCompat where type was the second parameter
-  if (typeof options === 'string') {
-    type = options;
-    options = {};
+  const normalizedOptions: Options = typeof options === 'string' ? { type: options } : options || {};
+  const type = normalizedOptions.type || '';
+
+  debug('options given: ', normalizedOptions);
+
+  const fileInfo: FileInfo = {
+    content,
+    ast: content,
+    ext: '',
+    type,
+  };
+
+  const detective = getDetector(fileInfo, normalizedOptions) || getJsDetector(fileInfo, normalizedOptions);
+  if (!detective) {
+    return [];
   }
 
-  debug('options given: ', options);
-
-  // We assume we're dealing with a JS file
-  if (!type && typeof content !== 'object') {
-    const walker = new Walker();
-
-    try {
-      // Parse once and distribute the AST to all detectives
-      ast = walker.parse(content);
-      // @ts-ignore AUTO-ADDED-AFTER-MIGRATION-PLEASE-FIX!
-      precinct.ast = ast;
-    } catch (e: any) {
-      // In case a previous call had it populated
-      // @ts-ignore AUTO-ADDED-AFTER-MIGRATION-PLEASE-FIX!
-      precinct.ast = null;
-      debug('could not parse content: %s', e.message);
-      throw e;
-    }
-    // SASS files shouldn't be parsed by Acorn
-  } else {
-    ast = content;
-
-    if (typeof content === 'object') {
-      // @ts-ignore AUTO-ADDED-AFTER-MIGRATION-PLEASE-FIX!
-      precinct.ast = content;
-    }
-  }
-
-  type = options.useContent ? getModuleType.fromSource(content) : type || getModuleType.fromSource(ast);
-  debug('module type: ', type);
-
-  let theDetective;
-  let detector;
-
-  switch (type) {
-    case 'commonjs':
-    case 'es6':
-      theDetective = detectiveEs6;
-      break;
-    case 'amd':
-      theDetective = detectiveAmd;
-      break;
-    case 'sass':
-      theDetective = detectiveSass;
-      break;
-    case 'less':
-      theDetective = detectiveLess;
-      break;
-    case 'scss':
-      theDetective = detectiveScss;
-      break;
-    case 'css':
-      theDetective = detectiveCss;
-      break;
-    case 'stylus':
-      theDetective = detectiveStylus;
-      break;
-    case 'ts':
-    case 'tsx':
-      theDetective = detectiveTypeScript;
-      break;
-    default:
-      detector = detectorHook.getDetector(type);
-      if (detector) theDetective = detector.detect.bind(detector);
-      break;
-  }
-
-  if (theDetective) {
-    // dependencies = type === 'vue' ? theDetective(ast, options) : theDetective(ast, options[type] || {});
-    dependencies = theDetective(ast, options[type] || {});
-  }
-
-  // For non-JS files that we don't parse
-  if (theDetective && theDetective.ast) {
-    // @ts-ignore AUTO-ADDED-AFTER-MIGRATION-PLEASE-FIX!
-    precinct.ast = theDetective.ast;
-  }
-
-  return dependencies;
-}
-
-function assign(o1, o2) {
-  // eslint-disable-next-line
-  for (const key in o2) {
-    // eslint-disable-next-line
-    if (o2.hasOwnProperty(key)) {
-      o1[key] = o2[key];
-    }
-  }
-
-  return o1;
-}
+  return detective(fileInfo.ast, normalizedOptions[fileInfo.type]);
+};
 
 precinct.paperwork = getDepsFromFile;
 
 export default precinct;
-
-/**
- * Returns the dependencies for the given file path
- *
- * @param {String} filename
- * @param {Object} [options]
- * @param {Boolean} [options.includeCore=true] - Whether or not to include core modules in the dependency list
- * @return {String[]}
- */
-export const legacyPaperwork = function (filename, options) {
-  options = assign(
-    {
-      includeCore: true,
-    },
-    options || {}
-  );
-
-  const content = fs.readFileSync(filename, 'utf8');
-  const ext = path.extname(filename);
-
-  const getType = () => {
-    switch (ext) {
-      case '.css':
-      case '.scss':
-      case '.sass':
-      case '.less':
-      case '.ts':
-      case '.vue':
-        return ext.replace('.', '');
-      case '.styl':
-        return 'stylus';
-      case '.tsx':
-        if (!options.ts) options.ts = {};
-        options.ts.jsx = true;
-        return 'ts';
-      case '.mts':
-      case '.cts':
-        return 'ts';
-      case '.jsx':
-      case '.mjs':
-        return 'es6';
-      default:
-        if (detectorHook.isSupported(ext)) {
-          return ext;
-        }
-
-        return null;
-    }
-  };
-
-  const getDeps = () => {
-    if (SUPPORTED_EXTENSIONS.includes(ext) || detectorHook.isSupported(ext)) return precinct(content, options);
-    debug(`skipping unsupported file ${filename}`);
-    return [];
-  };
-
-  const type = getType();
-  options.type = type;
-
-  const deps = getDeps();
-
-  if (deps && !options.includeCore) {
-    if (Array.isArray(deps)) {
-      return deps.filter(function (d) {
-        return !natives[d];
-      });
-    }
-    return Object.keys(deps).reduce((acc, value) => {
-      if (!natives[value]) acc[value] = deps[value];
-      return acc;
-    }, {});
-  }
-
-  return deps;
-};
-
-export const detectors = [];
