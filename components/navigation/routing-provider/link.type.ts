7984859b38721124d6ea54ea5da6c21e364cdca1
Luv Kapur <luv@bit.dev>
2023-03-29 15:16:58 -0400

    set env to teambit.react/react

--- components/navigation/routing-provider/link.type.ts
+++ components/navigation/routing-provider/link.type.ts
@@ -13,17 +13,6 @@ export interface LinkProps extends RefAttributes<HTMLAnchorElement>, AnchorHTMLA
 
   /** open the link in a new page (renders as native link) */
   external?: boolean;
-  /**
-   * @HACK
-   * typescript complains about autoFocus being required for LinkProps
-   *
-   * @error
-   * Property 'autoFocus' is missing in type '{ children: Element[]; external: true; href: string; className: any; }' but required in type 'Pick<LinkProps, "dir" | "type" | "id" | "hidden" | "target" | "lang" | "state" | "active" | "key" | "title" | "property" | "defaultValue" | "style" | "color" | "is" | "href" | ... 257 more ... | "onTransitionEndCapture">'.
-   * <Link external href={links.scopedRegistry} className={linkStyles}>
-   *
-   * this forces it to align with the AnchorHTMLAttributes<HTMLAnchorElement>
-   */
-  autoFocus?: boolean;
 }
 
 export interface NavLinkProps extends LinkProps {
