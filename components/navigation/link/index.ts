f5f08218934a929321ebe8f9146a5ed593bcca40
Luv Kapur <luv@bit.dev>
2023-03-30 17:47:44 -0400

    eject and upgrade to latest - link

--- components/navigation/link/index.ts
+++
@@ -1,7 +0,0 @@
-export { Link } from './link';
-export type { LinkProps, LinkType, Location, UseLocation, Navigator, UseNavigate } from './link.type';
-export { useLocation } from './use-location';
-export { useNavigate } from './use-navigate';
-
-export { useNavigation, NavigationProvider } from './navigation-provider';
-export type { RouterContextType } from './navigation-provider';
