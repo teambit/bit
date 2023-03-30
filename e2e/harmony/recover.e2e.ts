a695cb892c9eb2d9014f4b24d2af1ad0f1681709
David First <davidnet@gmail.com>
2023-03-30 15:29:47 -0400

    fix minor typo

--- e2e/harmony/recover.e2e.ts
+++ e2e/harmony/recover.e2e.ts
@@ -4,7 +4,7 @@ import Helper from '../../src/e2e-helper/e2e-helper';
 
 chai.use(require('chai-fs'));
 
-describe('bit remove command', function () {
+describe('bit recover command', function () {
   this.timeout(0);
   let helper: Helper;
   before(() => {
@@ -13,53 +13,51 @@ describe('bit remove command', function () {
   after(() => {
     helper.scopeHelper.destroy();
   });
-  describe('recover command', () => {
-    describe('recover before snapping', () => {
-      before(() => {
-        helper.scopeHelper.setNewLocalAndRemoteScopes();
-        helper.fixtures.populateComponents(2);
-        helper.command.tagWithoutBuild();
-        helper.command.export();
+  describe('recover before snapping', () => {
+    before(() => {
+      helper.scopeHelper.setNewLocalAndRemoteScopes();
+      helper.fixtures.populateComponents(2);
+      helper.command.tagWithoutBuild();
+      helper.command.export();
 
-        helper.command.softRemoveComponent('comp2');
-        helper.command.recover('comp2');
-      });
-      it('bit status should not show a section of removed components', () => {
-        const status = helper.command.statusJson();
-        expect(status.locallySoftRemoved).to.have.lengthOf(0);
-      });
-      it('bit status should not show the dependent component with an issue because the dependency is now back to the workspace', () => {
-        helper.command.expectStatusToNotHaveIssue(IssuesClasses.MissingPackagesDependenciesOnFs.name);
-      });
-      it('bit list should show the removed component', () => {
-        const list = helper.command.listParsed();
-        expect(list).to.have.lengthOf(2);
-      });
+      helper.command.softRemoveComponent('comp2');
+      helper.command.recover('comp2');
     });
-    describe('recover after snapping', () => {
-      before(() => {
-        helper.scopeHelper.setNewLocalAndRemoteScopes();
-        helper.fixtures.populateComponents(2);
-        helper.command.tagWithoutBuild();
-        helper.command.export();
+    it('bit status should not show a section of removed components', () => {
+      const status = helper.command.statusJson();
+      expect(status.locallySoftRemoved).to.have.lengthOf(0);
+    });
+    it('bit status should not show the dependent component with an issue because the dependency is now back to the workspace', () => {
+      helper.command.expectStatusToNotHaveIssue(IssuesClasses.MissingPackagesDependenciesOnFs.name);
+    });
+    it('bit list should show the removed component', () => {
+      const list = helper.command.listParsed();
+      expect(list).to.have.lengthOf(2);
+    });
+  });
+  describe('recover after snapping', () => {
+    before(() => {
+      helper.scopeHelper.setNewLocalAndRemoteScopes();
+      helper.fixtures.populateComponents(2);
+      helper.command.tagWithoutBuild();
+      helper.command.export();
 
-        helper.command.softRemoveComponent('comp2');
-        helper.fs.outputFile('comp1/index.js', '');
-        helper.command.tagAllWithoutBuild();
-        helper.command.recover(`${helper.scopes.remote}/comp2`);
-      });
-      it('should put it back in .bitmap file', () => {
-        const bitMap = helper.bitMap.read();
-        expect(bitMap.comp2).to.not.be.undefined;
-      });
-      it('bit status should not show the component as remotelySoftRemoved', () => {
-        const status = helper.command.statusJson();
-        expect(status.remotelySoftRemoved).to.have.lengthOf(0);
-      });
-      it('bit list should show the component', () => {
-        const list = helper.command.listParsed();
-        expect(list).to.have.lengthOf(2);
-      });
+      helper.command.softRemoveComponent('comp2');
+      helper.fs.outputFile('comp1/index.js', '');
+      helper.command.tagAllWithoutBuild();
+      helper.command.recover(`${helper.scopes.remote}/comp2`);
+    });
+    it('should put it back in .bitmap file', () => {
+      const bitMap = helper.bitMap.read();
+      expect(bitMap.comp2).to.not.be.undefined;
+    });
+    it('bit status should not show the component as remotelySoftRemoved', () => {
+      const status = helper.command.statusJson();
+      expect(status.remotelySoftRemoved).to.have.lengthOf(0);
+    });
+    it('bit list should show the component', () => {
+      const list = helper.command.listParsed();
+      expect(list).to.have.lengthOf(2);
     });
   });
 });
