e27593d9e71b944d1b054a0d95208ffb7e7d70ae
David First <davidnet@gmail.com>
2023-04-05 18:11:16 -0400

    test(recover): add e2e-tests for bit-recover when on a lane

--- e2e/harmony/recover.e2e.ts
+++ e2e/harmony/recover.e2e.ts
@@ -91,7 +91,6 @@ describe('bit recover command', function () {
       expect(isModified).to.be.true;
     });
   });
-
   describe('import soft-removed component', () => {
     before(() => {
       helper.scopeHelper.setNewLocalAndRemoteScopes();
@@ -127,6 +126,92 @@ describe('bit recover command', function () {
       it('bit status should not show the component as soft-removed anymore', () => {
         const status = helper.command.statusJson();
         expect(status.remotelySoftRemoved).to.have.lengthOf(0);
+      });
+    });
+  });
+  describe('recover on a lane', () => {
+    describe('recover before snapping', () => {
+      before(() => {
+        helper.scopeHelper.setNewLocalAndRemoteScopes();
+        helper.command.createLane();
+        helper.fixtures.populateComponents(2);
+        helper.command.snapAllComponentsWithoutBuild();
+        helper.command.export();
+
+        helper.command.softRemoveComponent('comp2');
+        helper.command.recover('comp2');
+      });
+      it('bit status should not show a section of removed components', () => {
+        const status = helper.command.statusJson();
+        expect(status.locallySoftRemoved).to.have.lengthOf(0);
+      });
+      it('bit status should not show the dependent component with an issue because the dependency is now back to the workspace', () => {
+        helper.command.expectStatusToNotHaveIssue(IssuesClasses.MissingPackagesDependenciesOnFs.name);
+      });
+      it('bit list should show the removed component', () => {
+        const list = helper.command.listParsed();
+        expect(list).to.have.lengthOf(2);
+      });
+    });
+    describe('recover after snapping', () => {
+      before(() => {
+        helper.scopeHelper.setNewLocalAndRemoteScopes();
+        helper.command.createLane();
+        helper.fixtures.populateComponents(2);
+        helper.command.snapAllComponentsWithoutBuild();
+        helper.command.export();
+
+        helper.command.softRemoveComponent('comp2');
+        helper.fs.outputFile('comp1/index.js', '');
+        helper.command.snapAllComponentsWithoutBuild();
+        helper.command.recover(`${helper.scopes.remote}/comp2`);
+      });
+      it('should put it back in .bitmap file', () => {
+        const bitMap = helper.bitMap.read();
+        expect(bitMap.comp2).to.not.be.undefined;
+      });
+      it('bit status should not show the component as remotelySoftRemoved', () => {
+        const status = helper.command.statusJson();
+        expect(status.remotelySoftRemoved).to.have.lengthOf(0);
+      });
+      it('bit list should show the component', () => {
+        const list = helper.command.listParsed();
+        expect(list).to.have.lengthOf(2);
+      });
+      it('bit status should show the component as modified because the RemoveAspect has changed', () => {
+        const head = helper.command.getHeadOfLane('dev', 'comp2');
+        const isModified = helper.command.statusComponentIsModified(`${helper.scopes.remote}/comp2@${head}`);
+        expect(isModified).to.be.true;
+      });
+    });
+    describe('export the soft-remove component, start a new workspace and recover from there', () => {
+      before(() => {
+        helper.scopeHelper.setNewLocalAndRemoteScopes();
+        helper.command.createLane();
+        helper.fixtures.populateComponents(2);
+        helper.command.snapAllComponentsWithoutBuild();
+        helper.command.export();
+
+        helper.command.softRemoveComponent('comp2');
+        helper.fs.outputFile('comp1/index.js', '');
+        helper.command.snapAllComponentsWithoutBuild();
+        helper.command.export();
+
+        helper.scopeHelper.reInitLocalScope();
+        helper.scopeHelper.addRemoteScope();
+        helper.command.importLane(`dev`, '-x');
+        helper.command.recover(`${helper.scopes.remote}/comp2`);
+      });
+      it('should import the component', () => {
+        const list = helper.command.listParsed();
+        expect(list).to.have.lengthOf(2);
+
+        helper.bitMap.expectToHaveId('comp2');
+      });
+      it('bit status should show the component as modified because the RemoveAspect has changed', () => {
+        const head = helper.command.getHeadOfLane('dev', 'comp2');
+        const isModified = helper.command.statusComponentIsModified(`${helper.scopes.remote}/comp2@${head}`);
+        expect(isModified).to.be.true;
       });
     });
   });
