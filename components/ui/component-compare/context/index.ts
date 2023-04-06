c9877d4981e41be56370b46d309b5429c9221a24
Luv Kapur <luv@bit.dev>
2023-04-06 13:16:18 -0400

    chore(UI): Eject UI fixes (#7254)

--- components/ui/component-compare/context/index.ts
+++
@@ -1,2 +0,0 @@
-export { ComponentCompareContext, useComponentCompare } from './component-compare-context';
-export type { StateAndHooks, ComponentCompareContextType } from './component-compare-context';
--- components/ui/component-compare/utils/group-by-version/groupByVersion.ts
+++
@@ -1,8 +0,0 @@
-import { LegacyComponentLog } from '@teambit/legacy-component-log';
-
-export const groupByVersion = (accum: Map<string, LegacyComponentLog>, current: LegacyComponentLog) => {
-  if (!accum.has(current.tag || current.hash)) {
-    accum.set(current.tag || current.hash, current);
-  }
-  return accum;
-};
