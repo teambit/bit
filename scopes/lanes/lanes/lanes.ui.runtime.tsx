417a211f6893d6842eea05cbbd3fe1c4d475fc17
Luv Kapur <luv@bit.dev>
2023-04-11 13:13:05 -0400

    fix lane selector and code compare

--- scopes/lanes/lanes/lanes.ui.runtime.tsx
+++ scopes/lanes/lanes/lanes.ui.runtime.tsx
@@ -245,6 +245,7 @@ export class LanesUI {
       <LaneSwitcher
         groupByScope={this.lanesHost === 'workspace'}
         mainIcon={this.lanesHost === 'scope' ? mainIcon : undefined}
+        useLanes={this.getUseLanes()}
       />
     );
 
