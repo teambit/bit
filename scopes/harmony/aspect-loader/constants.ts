74b8ef5cad9e9f2391552e0e73d007a90ff367a6
Gilad Shoham <shoham.gilad@gmail.com>
2023-04-19 17:36:56 +0300

    linting

--- scopes/harmony/aspect-loader/constants.ts
+++ scopes/harmony/aspect-loader/constants.ts
@@ -4,7 +4,7 @@ export const UNABLE_TO_LOAD_EXTENSION = (id: string, errMsg?: string) =>
   `error: Bit received an error loading "${id}", due to the error "${
     errMsg || '<unknown-error>'
   }", please use the '--log=error' flag for the full error.`;
-export const UNABLE_TO_LOAD_EXTENSION_FROM_LIST = (ids: string[], errMsg?: string, neededFor?: string, err?: any) => {
+export const UNABLE_TO_LOAD_EXTENSION_FROM_LIST = (ids: string[], errMsg?: string, neededFor?: string) => {
   // const installOutput = err?.code === 'MODULE_NOT_FOUND' ? `try running "bit install" to install the missing dependencies` : '';
   const installOutput = `try running ${chalk.cyan('"bit install"')} to fix this issue`;
   return `Bit received an error loading ${chalk.cyan(ids.join(', '))}, due to the error:
