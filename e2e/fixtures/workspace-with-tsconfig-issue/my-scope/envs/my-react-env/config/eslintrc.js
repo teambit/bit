eda27c2d74fb35726cffd525ce9790803f035cab
Zoltan Kochan <z@kochan.io>
2023-04-25 14:38:11 +0300

    simplify fixture

--- e2e/fixtures/workspace-with-tsconfig-issue/my-scope/envs/my-react-env/config/eslintrc.js
+++
@@ -1,6 +0,0 @@
-/**
- * @see https://bit.dev/reference/eslint/eslint-config
- */
-module.exports = {
-  extends: [require.resolve('@teambit/react.react-env/config/eslintrc')],
-};
--- e2e/fixtures/workspace-with-tsconfig-issue/my-scope/envs/my-react-env/config/prettier.config.js
+++
@@ -1,8 +0,0 @@
-/**
- * @see https://bit.dev/reference/prettier/prettier-config
- */
-const { prettierConfig } = require('@teambit/react.react-env');
-
-module.exports = {
-  ...prettierConfig,
-};
