f21c73fee75f15ea39f4cfc2744ee3e1ea297a73
Konstantinos Leimonis <k.leimonis@gmail.com>
2023-04-28 13:47:40 +0300

    feat: generate hash during pre bundle task

--- scopes/preview/preview/ui-pre-bundle.task.ts
+++ scopes/preview/preview/ui-pre-bundle.task.ts
@@ -1,4 +1,5 @@
 import { join } from 'path';
+import { existsSync, mkdirSync, writeFileSync } from 'fs';
 import { BuildContext, BuildTask, BuiltTaskResult, TaskLocation } from '@teambit/builder';
 import { Logger } from '@teambit/logger';
 import { UIAspect, UiMain } from '@teambit/ui';
@@ -9,7 +10,7 @@ export const UI_PRE_BUNDLE_TASK_NAME = 'GenerateUiPreBundle';
 export class UiPreBundleTask implements BuildTask {
   aspectId = 'teambit.ui-foundation/ui';
   name = UI_PRE_BUNDLE_TASK_NAME;
-  location: TaskLocation = 'start';
+  location: TaskLocation = 'end';
 
   constructor(private ui: UiMain, private logger: Logger) {}
 
@@ -20,21 +21,32 @@ export class UiPreBundleTask implements BuildTask {
     if (!capsule) {
       return { componentsResults: [] };
     }
+
     const outputPath = join(capsule.path, getArtifactDirectory());
-    this.logger.info(`Generating UI pre-bundle at ${outputPath}...`);
+    this.logger.info(`Generating prebundled ui-build at ${outputPath}...`);
     try {
       await this.ui.build(undefined, outputPath);
+      await this.generateHash(outputPath);
     } catch (error) {
-      this.logger.error('Generating UI pre-bundle failed');
-      return {
-        componentsResults: [],
-      };
+      this.logger.error('Generating prebundled ui-build failed');
+      throw new Error('Generating prebundled ui-build failed');
     }
     const artifacts = getArtifactDef();
     return {
       componentsResults: [],
       artifacts,
     };
+  }
+
+  private async generateHash(outputPath: string): Promise<void> {
+    const maybeUiRoot = this.ui.getUi();
+    if (!maybeUiRoot) throw new Error('no uiRoot found');
+
+    const [, uiRoot] = maybeUiRoot;
+    const hash = await this.ui.buildUiHash(uiRoot);
+
+    if (!existsSync(outputPath)) mkdirSync(outputPath);
+    writeFileSync(join(outputPath, '.hash'), hash);
   }
 }
 
