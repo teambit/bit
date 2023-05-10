e935b934fe3c9fb656dfd6b685d116001b2e197f
David First <davidnet@gmail.com>
2023-05-10 16:29:59 -0400

    fix lint

--- scopes/scope/scope/scope.main.runtime.ts
+++ scopes/scope/scope/scope.main.runtime.ts
@@ -39,7 +39,7 @@ import { Remotes } from '@teambit/legacy/dist/remotes';
 import { isMatchNamespacePatternItem } from '@teambit/workspace.modules.match-pattern';
 import { Scope } from '@teambit/legacy/dist/scope';
 import { CompIdGraph, DepEdgeType } from '@teambit/graph';
-import chokidar, { FSWatcher } from 'chokidar';
+import chokidar from 'chokidar';
 import { Types } from '@teambit/legacy/dist/scope/object-registrar';
 import { FETCH_OPTIONS } from '@teambit/legacy/dist/api/scope/lib/fetch';
 import { ObjectList } from '@teambit/legacy/dist/scope/objects/object-list';
