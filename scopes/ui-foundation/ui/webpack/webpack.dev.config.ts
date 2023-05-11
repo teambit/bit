5a33e95300787b1bdd5d9e003ae209d5ab2830a5
Gilad Shoham <shoham.gilad@gmail.com>
2023-05-11 16:40:33 +0300

    chore(webpack) - upgrade webpack and webpack dev server to latest versions (#7389)

--- scopes/ui-foundation/ui/webpack/webpack.dev.config.ts
+++ scopes/ui-foundation/ui/webpack/webpack.dev.config.ts
@@ -144,11 +144,16 @@ export function devConfig(workspaceDir, entryFiles, title): WebpackConfigWithDev
         // @ts-ignore @types/wds mismatch
         app.use(evalSourceMapMiddleware(wds));
         // This lets us open files from the runtime error overlay.
+        // @ts-ignore
         app.use(errorOverlayMiddleware());
       },
 
-      onAfterSetupMiddleware({ app }) {
+      onAfterSetupMiddleware(wds) {
+        // @ts-ignore
+        const { app } = wds;
+
         // Redirect to `PUBLIC_URL` or `homepage` from `package.json` if url not match
+        // @ts-ignore
         app.use(redirectServedPath(publicUrlOrPath));
 
         // This service worker file is effectively a 'no-op' that will reset any
@@ -156,6 +161,7 @@ export function devConfig(workspaceDir, entryFiles, title): WebpackConfigWithDev
         // We do this in development to avoid hitting the production cache if
         // it used the same host and port.
         // https://github.com/facebook/create-react-app/issues/2272#issuecomment-302832432
+        // @ts-ignore
         app.use(noopServiceWorkerMiddleware(publicUrlOrPath));
       },
 
