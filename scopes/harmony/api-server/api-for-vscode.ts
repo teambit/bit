42d29a3af7c7241b0b9ad8a623cb02fd4e413f61
David First <davidnet@gmail.com>
2023-05-30 12:50:06 -0400

    add vscode-api of getting component files (#7458)

--- scopes/harmony/api-server/api-for-vscode.ts
+++ scopes/harmony/api-server/api-for-vscode.ts
@@ -1,6 +1,6 @@
 import path from 'path';
 import { Workspace } from '@teambit/workspace';
-import { PathOsBasedAbsolute } from '@teambit/legacy/dist/utils/path';
+import { PathOsBasedAbsolute, PathOsBasedRelative } from '@teambit/legacy/dist/utils/path';
 
 export class APIForVSCode {
   constructor(private workspace: Workspace) {}
@@ -17,5 +17,13 @@ export class APIForVSCode {
     const compId = await this.workspace.resolveComponentId(id);
     const comp = await this.workspace.get(compId);
     return path.join(this.workspace.componentDir(compId), comp.state._consumer.mainFile);
+  }
+
+  async getCompFiles(id: string): Promise<{ dirAbs: string; filesRelative: PathOsBasedRelative[] }> {
+    const compId = await this.workspace.resolveComponentId(id);
+    const comp = await this.workspace.get(compId);
+    const dirAbs = this.workspace.componentDir(compId);
+    const filesRelative = comp.state.filesystem.files.map((file) => file.relative);
+    return { dirAbs, filesRelative };
   }
 }
