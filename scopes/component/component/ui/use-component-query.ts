55afe532fcbd7379880315e9402da8dec7614e18
Luv Kapur <luv@bit.dev>
2023-06-16 09:38:23 -0400

    fix(component version): loading versions (#7529)

--- scopes/component/component/ui/use-component-query.ts
+++ scopes/component/component/ui/use-component-query.ts
@@ -1,167 +1,41 @@
 import { useMemo, useEffect, useRef } from 'react';
-import { gql } from '@apollo/client';
 import { useDataQuery } from '@teambit/ui-foundation.ui.hooks.use-data-query';
 import { ComponentID, ComponentIdObj } from '@teambit/component-id';
 import { ComponentDescriptor } from '@teambit/component-descriptor';
-
 import { ComponentModel } from './component-model';
+import { ComponentQueryResult, Filters } from './use-component.model';
+import {
+  GET_COMPONENT,
+  SUB_COMPONENT_CHANGED,
+  SUB_COMPONENT_REMOVED,
+  SUB_SUBSCRIPTION_ADDED,
+} from './use-component.fragments';
+import { useComponentLogs } from './use-component-logs';
 import { ComponentError } from './component-error';
 
-export const componentIdFields = gql`
-  fragment componentIdFields on ComponentID {
-    name
-    version
-    scope
-  }
-`;
-
-export const componentOverviewFields = gql`
-  fragment componentOverviewFields on Component {
-    id {
-      ...componentIdFields
-    }
-    aspects(include: ["teambit.preview/preview", "teambit.envs/envs"]) {
-      # 'id' property in gql refers to a *global* identifier and used for caching.
-      # this makes aspect data cache under the same key, even when they are under different components.
-      # renaming the property fixes that.
-      id
-      data
-    }
-    elementsUrl
-    description
-    deprecation {
-      isDeprecate
-      newId
-    }
-    labels
-    displayName
-    server {
-      env
-      url
-      host
-      basePath
-    }
-    buildStatus
-    env {
-      id
-      icon
-    }
-    size {
-      compressedTotal
-    }
-    preview {
-      includesEnvTemplate
-      legacyHeader
-      isScaling
-      skipIncludes
-    }
-    compositions {
-      identifier
-      displayName
-    }
-  }
-  ${componentIdFields}
-`;
-
-export const componentFields = gql`
-  fragment componentFields on Component {
-    id {
-      ...componentIdFields
-    }
-    ...componentOverviewFields
-    packageName
-    latest
-    compositions {
-      identifier
-      displayName
-    }
-    tags {
-      version
-    }
-    logs(type: $logType, offset: $logOffset, limit: $logLimit, head: $logHead, sort: $logSort) {
-      id
-      message
-      username
-      email
-      date
-      hash
-      tag
-    }
-  }
-  ${componentIdFields}
-  ${componentOverviewFields}
-`;
-
-const GET_COMPONENT = gql`
-  query Component(
-    $id: String!
-    $extensionId: String!
-    $logType: String
-    $logOffset: Int
-    $logLimit: Int
-    $logHead: String
-    $logSort: String
-  ) {
-    getHost(id: $extensionId) {
-      id # used for GQL caching
-      get(id: $id) {
-        ...componentFields
-      }
-    }
-  }
-  ${componentFields}
-`;
-
-const SUB_SUBSCRIPTION_ADDED = gql`
-  subscription OnComponentAdded($logType: String, $logOffset: Int, $logLimit: Int, $logHead: String, $logSort: String) {
-    componentAdded {
-      component {
-        ...componentFields
-      }
-    }
-  }
-  ${componentFields}
-`;
-
-const SUB_COMPONENT_CHANGED = gql`
-  subscription OnComponentChanged(
-    $logType: String
-    $logOffset: Int
-    $logLimit: Int
-    $logHead: String
-    $logSort: String
-  ) {
-    componentChanged {
-      component {
-        ...componentFields
-      }
-    }
-  }
-  ${componentFields}
-`;
-
-const SUB_COMPONENT_REMOVED = gql`
-  subscription OnComponentRemoved {
-    componentRemoved {
-      componentIds {
-        ...componentIdFields
-      }
-    }
-  }
-  ${componentIdFields}
-`;
-export type Filters = {
-  log?: { logType?: string; logOffset?: number; logLimit?: number; logHead?: string; logSort?: string };
-};
 /** provides data to component ui page, making sure both variables and return value are safely typed and memoized */
-export function useComponentQuery(componentId: string, host: string, filters?: Filters, skip?: boolean) {
+export function useComponentQuery(
+  componentId: string,
+  host: string,
+  filters?: Filters,
+  skip?: boolean
+): ComponentQueryResult {
   const idRef = useRef(componentId);
   idRef.current = componentId;
-  const { data, error, loading, subscribeToMore, ...rest } = useDataQuery(GET_COMPONENT, {
-    variables: { id: componentId, extensionId: host, ...(filters?.log || {}) },
+  const variables = {
+    id: componentId,
+    extensionId: host,
+  };
+
+  const { data, error, loading, subscribeToMore } = useDataQuery(GET_COMPONENT, {
+    variables,
     skip,
     errorPolicy: 'all',
   });
+
+  const { loading: loadingLogs, componentLogs: { logs } = {} } = useComponentLogs(componentId, host, filters, skip);
+
+  const rawComponent = data?.getHost?.get;
 
   useEffect(() => {
     // @TODO @Kutner fix subscription for scope
@@ -247,8 +121,26 @@ export function useComponentQuery(componentId: string, host: string, filters?: F
     };
   }, []);
 
-  const rawComponent = data?.getHost?.get;
-  return useMemo(() => {
+  const idDepKey = rawComponent?.id
+    ? `${rawComponent?.id?.scope}/${rawComponent?.id?.name}@${rawComponent?.id?.version}}`
+    : undefined;
+
+  const id: ComponentID | undefined = useMemo(
+    () => (rawComponent ? ComponentID.fromObject(rawComponent.id) : undefined),
+    [idDepKey]
+  );
+
+  const componentError =
+    error && !data
+      ? new ComponentError(500, error.message)
+      : (!rawComponent && !loading && new ComponentError(404)) || undefined;
+
+  const component = useMemo(
+    () => (rawComponent ? ComponentModel.from({ ...rawComponent, host, logs }) : undefined),
+    [id?.toString(), logs]
+  );
+
+  const componentDescriptor = useMemo(() => {
     const aspectList = {
       entries: rawComponent?.aspects.map((aspectObject) => {
         return {
@@ -258,15 +150,20 @@ export function useComponentQuery(componentId: string, host: string, filters?: F
         };
       }),
     };
-    const id = rawComponent && ComponentID.fromObject(rawComponent.id);
-    const componentError =
-      error && !data ? new ComponentError(500, error.message) : !rawComponent && !loading && new ComponentError(404);
+
+    return id ? ComponentDescriptor.fromObject({ id: id.toString(), aspectList }) : undefined;
+  }, [id?.toString()]);
+
+  return useMemo(() => {
     return {
-      componentDescriptor: id ? ComponentDescriptor.fromObject({ id: id.toString(), aspectList }) : undefined,
-      component: rawComponent ? ComponentModel.from({ ...rawComponent, host }) : undefined,
+      componentDescriptor,
+      component,
+      componentLogs: {
+        loading: loadingLogs,
+        logs,
+      },
       error: componentError || undefined,
       loading,
-      ...rest,
     };
-  }, [rawComponent, host, error]);
+  }, [host, component, componentDescriptor, componentError]);
 }
