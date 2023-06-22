9467ec54c54805432184dca04473c28b7a0b987b
David First <davidnet@gmail.com>
2023-06-22 10:50:07 -0400

    improve LaneNotFound error to suggest specifying scope-name if missing

--- src/api/scope/lib/exceptions/lane-not-found.ts
+++ src/api/scope/lib/exceptions/lane-not-found.ts
@@ -3,7 +3,10 @@ import { BitError } from '@teambit/bit-error';
 export class LaneNotFound extends BitError {
   code: number;
   constructor(public scopeName: string, public laneName: string) {
-    super(`lane "${laneName}" was not found in scope "${scopeName}"`);
+    let msg = `lane "${laneName}" was not found`;
+    msg += scopeName ? ` in scope "${scopeName}"` : '. please specify a scope-name.';
+
+    super(msg);
     this.code = 138;
   }
 }
