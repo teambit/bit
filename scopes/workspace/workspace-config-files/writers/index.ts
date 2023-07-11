5584023a7e8bff8a4a8f501d92707a70bf017c7a
Gilad Shoham <shoham.gilad@gmail.com>
2023-07-11 14:01:52 +0300

    fix types

--- scopes/workspace/workspace-config-files/writers/index.ts
+++ scopes/workspace/workspace-config-files/writers/index.ts
@@ -2,4 +2,8 @@ export { handleExtendingConfigFiles } from './extending-config-files';
 export { handleRealConfigFiles } from './real-config-files';
 
 export type { EnvsWrittenRealConfigFiles, EnvsWrittenRealConfigFile, WrittenConfigFile } from './real-config-files';
-export type { EnvsWrittenExtendingConfigFiles, EnvsWrittenExtendingConfigFile } from './extending-config-files';
+export type {
+  EnvsWrittenExtendingConfigFiles,
+  EnvsWrittenExtendingConfigFile,
+  ExtendingConfigFilesMap,
+} from './extending-config-files';
