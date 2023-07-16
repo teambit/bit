31b03680f03f898758baed084d5ff928e5b1c5a9
Luv Kapur <luv@bit.dev>
2023-07-16 10:53:00 -0400

    handle specific expression statements

--- scopes/typescript/typescript/sourceFileTransformers/expression-statement.ts
+++ scopes/typescript/typescript/sourceFileTransformers/expression-statement.ts
@@ -5,20 +5,33 @@ export const expressionStatementTransformer: SourceFileTransformer = (mapping: R
   return (context) => {
     const visit: ts.Visitor = (node) => {
       if (ts.isExpressionStatement(node)) {
-        const identifierName = node.getText();
-        let newIdentifierName = mapping[identifierName] || identifierName;
+        return ts.factory.updateExpressionStatement(node, ts.visitNode(node.expression, visit));
+      }
 
-        for (const [oldName, newName] of Object.entries(mapping)) {
-          if (identifierName.startsWith(oldName) || identifierName.endsWith(oldName)) {
-            newIdentifierName = identifierName.replace(oldName, newName);
-          }
-        }
+      if (ts.isCallExpression(node)) {
+        return ts.factory.updateCallExpression(
+          node,
+          ts.visitNode(node.expression, visit),
+          node.typeArguments,
+          node.arguments.map((arg) => ts.visitNode(arg, visit))
+        );
+      }
 
-        return ts.factory.createIdentifier(newIdentifierName);
+      if (ts.isPropertyAccessExpression(node)) {
+        return ts.factory.updatePropertyAccessExpression(
+          node,
+          ts.visitNode(node.expression, visit),
+          typeof mapping[node.name.text] === 'string' ? ts.factory.createIdentifier(mapping[node.name.text]) : node.name
+        );
+      }
+
+      if (ts.isIdentifier(node)) {
+        return typeof mapping[node.text] === 'string' ? ts.factory.createIdentifier(mapping[node.text]) : node;
       }
 
       return ts.visitEachChild(node, visit, context);
     };
+
     return (node) => ts.visitNode(node, visit);
   };
 };
