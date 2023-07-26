a640fa5e65d134106a1183f9ced37f192b8b855a
Gilad Shoham <shoham.gilad@gmail.com>
2023-07-26 15:50:43 +0300

    update main flow

--- scopes/harmony/bit/bundle/bundle.ts
+++ scopes/harmony/bit/bundle/bundle.ts
@@ -1,11 +1,16 @@
 import { runEsbuild } from './esbuild';
 import { generateCoreAspectsBarrelFile } from './generate-core-aspects-exports';
 import { generateCoreAspectsModules } from './generate-core-aspects-modules';
+import { generatePackageJson } from './create-package-json';
+import { generateNpmrc } from './generate-npmrc';
 
-const outDir = '/Users/giladshoham/dev/bit/bit/bundle';
+// const outDir = '/Users/giladshoham/dev/bit/bit/bundle';
+const outDir = '/tmp/bit-bundle';
 const appFile = 'bit.app.js';
 
 async function runBundle() {
+  generateNpmrc(outDir);
+  await generatePackageJson(outDir);
   await generateCoreAspectsBarrelFile();
   const esbuildRes = await runEsbuild(outDir, appFile);
   await generateCoreAspectsModules(outDir, appFile);
