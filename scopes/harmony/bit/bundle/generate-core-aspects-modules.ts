4a342a667c938694f083616d8016e2f957ae4fb8
Gilad Shoham <shoham.gilad@gmail.com>
2023-07-26 15:50:08 +0300

    copy pacakge.json of core aspects

--- scopes/harmony/bit/bundle/generate-core-aspects-modules.ts
+++ scopes/harmony/bit/bundle/generate-core-aspects-modules.ts
@@ -1,3 +1,4 @@
+import { getConsumerInfo } from '@teambit/legacy/dist/consumer';
 import camelcase from 'camelcase';
 import { getCoreAspectName, getCoreAspectPackageName } from '@teambit/aspect-loader';
 import fs from 'fs-extra';
@@ -5,7 +6,17 @@ import { join } from 'path';
 
 import { coreAspectsIds } from '../core-aspects-ids';
 
-export function generateCoreAspectsModules(bundleDir: string, appName: string) {
+let wsRootDir: string;
+
+async function loadWsRootDir() {
+  const consumerInfo = await getConsumerInfo(process.cwd());
+  if (!consumerInfo) throw new Error('unable to find consumer');
+  wsRootDir = consumerInfo.path;
+  return consumerInfo.path;
+}
+
+export async function generateCoreAspectsModules(bundleDir: string, appName: string) {
+  await loadWsRootDir();
   const generateOneAspectP = coreAspectsIds.map((id) => {
     const name = getCoreAspectName(id);
     const packageName = getCoreAspectPackageName(id);
@@ -19,9 +30,20 @@ export function generateCoreAspectsModules(bundleDir: string, appName: string) {
 async function handleOneAspect(bundleDir: string, name: string, packageName: string, appName: string) {
   const dirPath = join(bundleDir, 'node_modules', packageName);
   await fs.ensureDir(dirPath);
+  await generateIndexFile(dirPath, name, appName);
+  await copyPackageJson(dirPath, packageName);
+}
+
+async function generateIndexFile(dirPath: string, name: string, appName: string) {
   const indexFilePath = join(dirPath, 'index.js');
   const indexFileContent = getIndexContent(name, appName);
   return fs.outputFile(indexFilePath, indexFileContent);
+}
+
+async function copyPackageJson(dirPath: string, packageName: string) {
+  const packageJsonPath = join(wsRootDir, 'node_modules', packageName, 'package.json');
+  const targetPath = join(dirPath, 'package.json');
+  return fs.copyFile(packageJsonPath, targetPath);
 }
 
 function getIndexContent(name: string, appName: string) {
