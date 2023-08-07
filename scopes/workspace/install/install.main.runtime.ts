13f38cee90fba108e33d2de60014a764ddd996b1
Zoltan Kochan <z@kochan.io>
2023-08-07 17:45:39 +0300

    feat: add an option to the update command to pick what version is allowed to bump

--- scopes/workspace/install/install.main.runtime.ts
+++ scopes/workspace/install/install.main.runtime.ts
@@ -576,7 +576,11 @@ export class InstallMain {
    *
    * @param options.all {Boolean} updates all outdated dependencies without showing a prompt.
    */
-  async updateDependencies(options: { patterns?: string[]; all: boolean }): Promise<ComponentMap<string> | null> {
+  async updateDependencies(options: {
+    forceVersionBump?: 'major' | 'minor' | 'patch';
+    patterns?: string[];
+    all: boolean;
+  }): Promise<ComponentMap<string> | null> {
     const { componentConfigFiles, componentPoliciesById } = await this._getComponentsWithDependencyPolicies();
     const variantPatterns = this.variants.raw();
     const variantPoliciesByPatterns = this._variantPatternsToDepPolicesDict(variantPatterns);
@@ -587,6 +591,7 @@ export class InstallMain {
       componentPoliciesById,
       components,
       patterns: options.patterns,
+      forceVersionBump: options.forceVersionBump,
     });
     let outdatedPkgsToUpdate!: OutdatedPkg[];
     if (options.all) {
