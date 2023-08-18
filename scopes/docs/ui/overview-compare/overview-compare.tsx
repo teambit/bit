4a5e23830c2fea7fd9dc57de39d145905a313166
Luv Kapur <luv@bit.dev>
2023-08-18 12:58:58 -0400

    allow passing preview props via overview

--- scopes/docs/ui/overview-compare/overview-compare.tsx
+++ scopes/docs/ui/overview-compare/overview-compare.tsx
@@ -1,86 +1,98 @@
 import { ComponentProvider } from '@teambit/component';
 import { CompareSplitLayoutPreset } from '@teambit/component.ui.component-compare.layouts.compare-split-layout-preset';
 import { useComponentCompare } from '@teambit/component.ui.component-compare.context';
-import { Toggle } from '@teambit/design.inputs.toggle-switch';
 import { RoundLoader } from '@teambit/design.ui.round-loader';
 import { Overview } from '@teambit/docs';
 import type { TitleBadgeSlot, OverviewOptionsSlot } from '@teambit/docs';
-import React, { UIEvent, useMemo, useRef, useState } from 'react';
+import React, { useMemo, useRef } from 'react';
+import { ComponentPreviewProps } from '@teambit/preview.ui.component-preview';
 
 import styles from './overview-compare.module.scss';
 
 export type OverviewCompareProps = {
   titleBadges: TitleBadgeSlot;
   overviewOptions: OverviewOptionsSlot;
+  previewProps?: ComponentPreviewProps;
+  Widgets?: {
+    Right?: React.ReactNode;
+    Left?: React.ReactNode;
+  };
 };
 
 export function OverviewCompare(props: OverviewCompareProps) {
-  const { titleBadges, overviewOptions } = props;
+  const { titleBadges, overviewOptions, previewProps, Widgets } = props;
   const componentCompare = useComponentCompare();
-  const [isScrollingSynced, setIsScrollingSynced] = useState<boolean>(true);
 
   const leftPanelRef = useRef<HTMLDivElement>(null);
   const rightPanelRef = useRef<HTMLDivElement>(null);
 
-  function handleLeftPanelScroll(event: UIEvent<HTMLDivElement>) {
-    if (!isScrollingSynced) return;
-
-    rightPanelRef.current?.scrollTo({ top: event.currentTarget?.scrollTop, left: event.currentTarget?.scrollLeft });
-  }
-
-  function handleRightPanelScroll(event: UIEvent<HTMLDivElement>) {
-    if (!isScrollingSynced) return;
-
-    leftPanelRef.current?.scrollTo({ top: event.currentTarget?.scrollTop, left: event.currentTarget?.scrollLeft });
-  }
-
-  function handleScrollingSyncChange() {
-    rightPanelRef.current?.scrollTo({ top: leftPanelRef.current?.scrollTop, left: leftPanelRef.current?.scrollLeft });
-    setIsScrollingSynced((prev) => !prev);
-  }
-
   const BaseLayout = useMemo(() => {
     if (componentCompare?.base === undefined) {
-      return <></>;
+      return null;
     }
 
     return (
-      <div className={styles.subView} ref={leftPanelRef} onScroll={handleLeftPanelScroll}>
+      <div className={styles.subView} ref={leftPanelRef}>
         <ComponentProvider component={componentCompare.base.model}>
-          <Overview titleBadges={titleBadges} overviewOptions={overviewOptions} />
+          <Overview titleBadges={titleBadges} overviewOptions={overviewOptions} previewProps={previewProps} />
         </ComponentProvider>
       </div>
     );
-  }, [componentCompare?.base, isScrollingSynced]);
+  }, [componentCompare?.base]);
 
   const CompareLayout = useMemo(() => {
     if (componentCompare?.compare === undefined) {
-      return <></>;
+      return null;
     }
 
     return (
-      <div className={styles.subView} ref={rightPanelRef} onScroll={handleRightPanelScroll}>
+      <div className={styles.subView} ref={rightPanelRef}>
         <ComponentProvider component={componentCompare.compare.model}>
-          <Overview titleBadges={titleBadges} overviewOptions={overviewOptions} />
+          <Overview titleBadges={titleBadges} overviewOptions={overviewOptions} previewProps={previewProps} />
         </ComponentProvider>
       </div>
     );
-  }, [componentCompare?.compare, isScrollingSynced]);
+  }, [componentCompare?.compare]);
+
+  const OverviewToolbar = () => {
+    if (!componentCompare?.base && !componentCompare?.compare) {
+      return null;
+    }
+
+    return (
+      <div className={styles.toolbar}>
+        {componentCompare?.base && (
+          <div className={styles.left}>
+            <div className={styles.widgets}>{Widgets?.Left}</div>
+          </div>
+        )}
+        <div className={styles.right}>
+          <div className={styles.widgets}>{Widgets?.Right}</div>
+        </div>
+      </div>
+    );
+  };
 
   return (
-    <>
+    <React.Fragment key="overview-compare">
       {componentCompare?.loading && (
         <div className={styles.loader}>
           <RoundLoader />
         </div>
       )}
-      <div className={styles.checkboxContainer}>
+      {/* <div className={styles.checkboxContainer}>
         <div className={styles.toggleContainer}>
           <Toggle checked={isScrollingSynced} onInputChanged={handleScrollingSyncChange} className={styles.toggle} />
           Synchronize Scrolling
         </div>
-      </div>
-      <CompareSplitLayoutPreset base={BaseLayout} compare={CompareLayout} className={styles.splitLayout} />
-    </>
+      </div> */}
+      <OverviewToolbar />
+      <CompareSplitLayoutPreset
+        key={`${componentCompare?.base?.model.id.toString()}-${componentCompare?.compare?.model.id.toString()}-overview-split-layout`}
+        base={BaseLayout}
+        compare={CompareLayout}
+        className={styles.splitLayout}
+      />
+    </React.Fragment>
   );
 }
