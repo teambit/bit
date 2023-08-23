a31c44f435caa66bbceac0aae43b83b0d0ea1b95
Nitsan Cohen <77798308+NitsanCohen770@users.noreply.github.com>
2023-08-23 04:01:13 +0300

    remove commented code and save file

--- scopes/community/community/community.main.runtime.ts
+++ scopes/community/community/community.main.runtime.ts
@@ -45,38 +45,3 @@ export class CommunityMain {
 }
 
 CommunityAspect.addRuntime(CommunityMain);
-// import { MainRuntime } from '@teambit/cli';
-// import { BASE_COMMUNITY_DOMAIN, BASE_DOCS_DOMAIN } from '@teambit/legacy/dist/constants';
-// import { helloWorldStarter} from '@teambit/community.starters.hello-world'
-
-// import { CommunityAspect } from './community.aspect';
-
-// export interface CommunityWorkspaceConfig {
-//   communityDomain: string;
-//   docsDomain: string;
-// }
-
-// export class CommunityMain {
-//   constructor(private config: CommunityWorkspaceConfig, private Generator: GeneratorMain) {}
-
-//   getBaseDomain(): string {
-//     return this.config.communityDomain;
-//   }
-
-//   getDocsDomain(): string {
-//     return this.config.docsDomain;
-//   }
-
-//   static slots = [];
-//   static dependencies = [];
-//   static runtime = MainRuntime;
-//   static defaultConfig: CommunityWorkspaceConfig = {
-//     communityDomain: BASE_COMMUNITY_DOMAIN,
-//     docsDomain: BASE_DOCS_DOMAIN,
-//   };
-//   static async provider(_deps, config: CommunityWorkspaceConfig) {
-//     return new CommunityMain(config);
-//   }
-// }
-
-// CommunityAspect.addRuntime(CommunityMain);
