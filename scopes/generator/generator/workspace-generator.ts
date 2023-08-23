3b87a1ffdc5cf0a2462d94424522898f1296bd6c
Nitsan Cohen <77798308+NitsanCohen770@users.noreply.github.com>
2023-08-24 00:11:00 +0300

    fix the a case where the logger can be undefined

--- scopes/generator/generator/workspace-generator.ts
+++ scopes/generator/generator/workspace-generator.ts
@@ -31,7 +31,7 @@ export class WorkspaceGenerator {
   private workspace: Workspace;
   private install: InstallMain;
   private importer: ImporterMain;
-  private logger: Logger;
+  private logger?: Logger;
   private forking: ForkingMain;
   private git: GitMain;
   private wsConfigFiles: WorkspaceConfigFilesMain;
@@ -75,7 +75,7 @@ export class WorkspaceGenerator {
       await this.compileComponents(true);
       await this.wsConfigFiles.writeConfigFiles({});
     } catch (err: any) {
-      this.logger.error(`failed generating a new workspace, will delete the dir ${this.workspacePath}`, err);
+      this.logger?.error(`failed generating a new workspace, will delete the dir ${this.workspacePath}`, err);
       await fs.remove(this.workspacePath);
       throw err;
     }
@@ -114,6 +114,7 @@ export class WorkspaceGenerator {
       empty: this.options.empty,
       aspectComponent: this.aspectComponent,
       template: this.template,
+      skipGit: this.options.skipGit,
     };
   }
 
