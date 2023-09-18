b47473667c89d6464f263769b2388c331355824c
David First <davidnet@gmail.com>
2023-09-18 12:07:41 -0400

    fix lint

--- src/cli/commands/private-cmds/_list-cmd.ts
+++ src/cli/commands/private-cmds/_list-cmd.ts
@@ -1,13 +1,18 @@
-import { migrate } from '../../../api/consumer';
-import { scopeList } from '../../../api/scope';
+/**
+ * no point of fixing this file, the SSH is not supported anymore
+ */
+
+/* eslint-disable @typescript-eslint/no-unused-vars */
+// import { migrate } from '../../../api/consumer';
+// import { scopeList } from '../../../api/scope';
 import { ListScopeResult } from '../../../consumer/component/components-list';
-import logger from '../../../logger/logger';
-import { checkVersionCompatibilityOnTheServer } from '../../../scope/network/check-version-compatibility';
-import { buildCommandMessage, fromBase64, packCommand, unpackCommand } from '../../../utils';
-import clientSupportCompressedCommand from '../../../utils/ssh/client-support-compressed-command';
+// import logger from '../../../logger/logger';
+// import { checkVersionCompatibilityOnTheServer } from '../../../scope/network/check-version-compatibility';
+// import { buildCommandMessage, fromBase64, packCommand, unpackCommand } from '../../../utils';
+// import clientSupportCompressedCommand from '../../../utils/ssh/client-support-compressed-command';
 import { LegacyCommand } from '../../legacy-command';
 
-let compressResponse;
+// let compressResponse;
 
 export default class List implements LegacyCommand {
   name = '_list <path> <args>';
@@ -17,18 +22,20 @@ export default class List implements LegacyCommand {
   alias = '';
   opts = [];
 
-  action([path, args]: [string, string]): Promise<ListScopeResult[]> {
-    const { payload, headers } = unpackCommand(args);
-    compressResponse = clientSupportCompressedCommand(headers.version);
-    checkVersionCompatibilityOnTheServer(headers.version);
-    logger.info('Checking if a migration is needed');
-    const scopePath = fromBase64(path);
-    return migrate(scopePath, false).then(() => {
-      return scopeList(scopePath, payload);
-    });
+  async action([path, args]: [string, string]): Promise<ListScopeResult[]> {
+    return [];
+    // const { payload, headers } = unpackCommand(args);
+    // compressResponse = clientSupportCompressedCommand(headers.version);
+    // checkVersionCompatibilityOnTheServer(headers.version);
+    // logger.info('Checking if a migration is needed');
+    // const scopePath = fromBase64(path);
+    // return migrate(scopePath, false).then(() => {
+    //   return scopeList(scopePath, payload);
+    // });
   }
 
   report(str: string): string {
-    return packCommand(buildCommandMessage(str, undefined, compressResponse), true, compressResponse);
+    return '';
+    // return packCommand(buildCommandMessage(str, undefined, compressResponse), true, compressResponse);
   }
 }
