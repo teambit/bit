412d0e6825a9f57e1a43f6089ff62d8fa44bad4b
Luv Kapur <luv@bit.dev>
2023-09-18 10:14:35 -0400

    add suspense fallback

--- scopes/code/ui/code-compare/code-compare-editor/code-compare-editor.tsx
+++ scopes/code/ui/code-compare/code-compare-editor/code-compare-editor.tsx
@@ -28,36 +28,38 @@ export function CodeCompareEditor({
   DiffEditor,
 }: CodeCompareEditorProps) {
   return (
-    <DiffEditor
-      modified={modifiedFileContent || undefined}
-      original={originalFileContent || undefined}
-      language={language}
-      originalModelPath={originalPath}
-      modifiedModelPath={modifiedPath}
-      height={'100%'}
-      onMount={handleEditorDidMount}
-      className={darkMode}
-      theme={'vs-dark'}
-      options={{
-        ignoreTrimWhitespace: ignoreWhitespace,
-        readOnly: true,
-        renderSideBySide: editorViewMode === 'split',
-        minimap: { enabled: false },
-        scrollbar: { alwaysConsumeMouseWheel: !wordWrap, vertical: 'auto' },
-        scrollBeyondLastLine: false,
-        folding: false,
-        overviewRulerLanes: 0,
-        overviewRulerBorder: false,
-        wordWrap: (wordWrap && 'on') || 'off',
-        wrappingStrategy: (wordWrap && 'advanced') || undefined,
-        fixedOverflowWidgets: true,
-        renderLineHighlight: 'none',
-        lineHeight: 20,
-        padding: { top: 8 },
-        hover: { enabled: false },
-        cursorBlinking: 'smooth',
-      }}
-      loading={Loader}
-    />
+    <React.Suspense fallback={Loader ?? <></>}>
+      <DiffEditor
+        modified={modifiedFileContent || undefined}
+        original={originalFileContent || undefined}
+        language={language}
+        originalModelPath={originalPath}
+        modifiedModelPath={modifiedPath}
+        height={'100%'}
+        onMount={handleEditorDidMount}
+        className={darkMode}
+        theme={'vs-dark'}
+        options={{
+          ignoreTrimWhitespace: ignoreWhitespace,
+          readOnly: true,
+          renderSideBySide: editorViewMode === 'split',
+          minimap: { enabled: false },
+          scrollbar: { alwaysConsumeMouseWheel: !wordWrap, vertical: 'auto' },
+          scrollBeyondLastLine: false,
+          folding: false,
+          overviewRulerLanes: 0,
+          overviewRulerBorder: false,
+          wordWrap: (wordWrap && 'on') || 'off',
+          wrappingStrategy: (wordWrap && 'advanced') || undefined,
+          fixedOverflowWidgets: true,
+          renderLineHighlight: 'none',
+          lineHeight: 20,
+          padding: { top: 8 },
+          hover: { enabled: false },
+          cursorBlinking: 'smooth',
+        }}
+        loading={Loader}
+      />
+    </React.Suspense>
   );
 }
