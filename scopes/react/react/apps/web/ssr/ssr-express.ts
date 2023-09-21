430469c7fd4a75e6284dfbd46e5c2b02e0f74b8a
Gilad Shoham <shoham.gilad@gmail.com>
2023-09-21 11:12:01 +0300

    fix linting

--- scopes/react/react/apps/web/ssr/ssr-express.ts
+++ scopes/react/react/apps/web/ssr/ssr-express.ts
@@ -35,6 +35,7 @@ export function createExpressSsr({ name, workdir, port, app, assets, logger }: E
     }
     response.sendFile(resolve(workdir, calcOutputPath(name, 'browser'), MAGIC_FOLDER, 'index.html'));
   });
+  // eslint-disable-next-line @typescript-eslint/no-misused-promises
   express.use(async (request, response) => {
     logger?.info(`[react.application] [ssr] handling "${request.url}"`);
     const browser = browserFromExpress(request, port);
