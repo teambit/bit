42d054b10a90174feb90a7fff76896cb3e931d20
Luv Kapur <luv@bit.dev>
2023-09-26 16:18:43 -0400

    fix(schema): add class name to ThisType schema (#7965)

--- scopes/semantics/entities/semantic-schema/schemas/this-type.ts
+++ scopes/semantics/entities/semantic-schema/schemas/this-type.ts
@@ -4,16 +4,17 @@ import { SchemaLocation, SchemaNode } from '../schema-node';
  * e.g. `class A { createA(): this {} }`
  */
 export class ThisTypeSchema extends SchemaNode {
-  constructor(readonly location: SchemaLocation) {
+  constructor(readonly location: SchemaLocation, readonly name: string) {
     super();
   }
 
   toString() {
-    return 'this';
+    return this.name;
   }
 
   static fromObject(obj: Record<string, any>): ThisTypeSchema {
     const location = obj.location;
-    return new ThisTypeSchema(location);
+    const name = obj.name;
+    return new ThisTypeSchema(location, name);
   }
 }
