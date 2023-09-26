be860299bc4dd206c449b42d59d9fcc9f58de93a
Ran Mizrahi <ranm8@users.noreply.github.com>
2023-09-26 18:04:16 +0300

    New empty state slot (#7963)

--- scopes/docs/docs/overview.section.tsx
+++ scopes/docs/docs/overview.section.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Section } from '@teambit/component';
 import { Overview, TitleBadgeSlot, OverviewOptionsSlot } from './overview';
+import { DocsUI } from './docs.ui.runtime';
 
 export class OverviewSection implements Section {
   constructor(
@@ -8,7 +9,8 @@ export class OverviewSection implements Section {
      * title badge slot.
      */
     private titleBadgeSlot: TitleBadgeSlot,
-    private overviewOptionsSlot: OverviewOptionsSlot
+    private overviewOptionsSlot: OverviewOptionsSlot,
+    private docs: DocsUI
   ) {}
 
   navigationLink = {
@@ -19,7 +21,13 @@ export class OverviewSection implements Section {
 
   route = {
     index: true,
-    element: <Overview titleBadges={this.titleBadgeSlot} overviewOptions={this.overviewOptionsSlot} />,
+    element: (
+      <Overview
+        titleBadges={this.titleBadgeSlot}
+        overviewOptions={this.overviewOptionsSlot}
+        getEmptyState={this.docs.getEmptyState.bind(this.docs)}
+      />
+    ),
   };
 
   order = 10;
