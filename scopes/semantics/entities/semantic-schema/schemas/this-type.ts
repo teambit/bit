883cdb858721f41b510cb72ce8af1dd5bb8f3ad1
Luv Kapur <luv@bit.dev>
2023-09-26 15:18:06 -0400

    sync  with origin master

--- scopes/semantics/entities/semantic-schema/schemas/this-type.ts
+++ scopes/semantics/entities/semantic-schema/schemas/this-type.ts
@@ -4,17 +4,16 @@ import { SchemaLocation, SchemaNode } from '../schema-node';
  * e.g. `class A { createA(): this {} }`
  */
 export class ThisTypeSchema extends SchemaNode {
-  constructor(readonly location: SchemaLocation, readonly name: string) {
+  constructor(readonly location: SchemaLocation) {
     super();
   }
 
   toString() {
-    return this.name;
+    return 'this';
   }
 
   static fromObject(obj: Record<string, any>): ThisTypeSchema {
     const location = obj.location;
-    const name = obj.name;
-    return new ThisTypeSchema(location, name);
+    return new ThisTypeSchema(location);
   }
 }
