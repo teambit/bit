202991c43e1c67f5de8a27a70061e68154ea1ca0
David First <davidnet@gmail.com>
2023-10-02 11:24:38 -0400

    warn when tagging on main without build

--- scopes/component/snapping/tag-cmd.ts
+++ scopes/component/snapping/tag-cmd.ts
@@ -240,6 +240,16 @@ to ignore multiple issues, separate them by a comma and wrap with quotes. to ign
       else if (persist === 'skip-build') build = false;
       else throw new BitError(`unknown value for --persist, use either --persist or --persist=skip-build`);
     }
+    if (!build) {
+      this.logger.consoleWarning(
+        `tagging components on "main" lane when using remote build is not recommended. To avoid SemVer versions of your component with failing builds, please refer to:
+- Snap changes in a different lane and merge to "main" on your remote (learn more on lanes - https://bit.dev/reference/lanes/getting-started-with-lanes)
+- Use \`bit tag --build\` to build your components locally.
+- Use \`snap\` or \`build\` first to validate your build passing, and then version and export safely.
+
+To undo local tag use the "bit reset" command.`
+      );
+    }
 
     const params = {
       ids: patterns,
