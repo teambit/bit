a775cae3f4bdc426624a0f29a3d24bd04fa30367
Luv Kapur <luv@bit.dev>
2023-10-06 16:13:14 -0400

    implement interface overview renderer

--- components/renderers/interface/interface.renderer.tsx
+++ components/renderers/interface/interface.renderer.tsx
@@ -3,10 +3,12 @@ import { InterfaceSchema } from '@teambit/semantics.entities.semantic-schema';
 import { APINodeRenderProps, APINodeRenderer } from '@teambit/api-reference.models.api-node-renderer';
 import { APINodeDetails } from '@teambit/api-reference.renderers.api-node-details';
 import { GroupedSchemaNodesSummary } from '@teambit/api-reference.renderers.grouped-schema-nodes-summary';
+import { SchemaNodesSummary } from '@teambit/api-reference.overview.renderers.grouped-schema-nodes-overview-summary';
 
 export const interfaceRenderer: APINodeRenderer = {
   predicate: (node) => node.__schema === InterfaceSchema.name,
   Component: InterfaceComponent,
+  OverviewComponent: InterfaceOverviewComponent,
   nodeType: 'Interfaces',
   icon: { name: 'Interface', url: 'https://static.bit.dev/api-reference/interface.svg' },
   default: true,
@@ -25,5 +27,28 @@ function InterfaceComponent(props: APINodeRenderProps) {
     <APINodeDetails {...props} displaySignature={displaySignature}>
       <GroupedSchemaNodesSummary nodes={members} apiNodeRendererProps={props} />
     </APINodeDetails>
+  );
+}
+
+function InterfaceOverviewComponent(props: APINodeRenderProps) {
+  const {
+    apiNode: { api, renderer },
+  } = props;
+  const classNode = api as InterfaceSchema;
+  const { members, doc } = classNode;
+
+  const icon = renderer.icon;
+  const description =
+    doc?.comment ??
+    doc?.tags?.filter((tag) => tag.comment).reduce((acc, tag) => acc.concat(`${tag.comment}\n` ?? ''), '');
+
+  return (
+    <SchemaNodesSummary
+      name={classNode.name}
+      description={description}
+      icon={icon}
+      nodes={members}
+      apiNodeRendererProps={props}
+    />
   );
 }
