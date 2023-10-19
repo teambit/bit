d2fddcce6026490f7865ba932ba8e1c2fb076ff7
David First <davidnet@gmail.com>
2023-10-19 17:08:52 -0400

    eject component-id and legacy-bit-id components

--- components/component-id/exceptions/missing-scope.ts
+++
@@ -1,8 +0,0 @@
-export class MissingScope extends Error {
-  constructor(src: any) {
-    super(`scope is missing from a component-id "${src.toString()}"`);
-  }
-  report() {
-    return this.message;
-  }
-}
