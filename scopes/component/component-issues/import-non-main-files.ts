4d5f7d826ac087b51d46dcffde26bd1b512b7c24
David First <davidnet@gmail.com>
2023-10-20 15:26:04 -0400

    eject components used by legacy and move legacy deps from workspace.jsonc to package.json (#8045)

--- scopes/component/component-issues/import-non-main-files.ts
+++
@@ -1,8 +0,0 @@
-import { ComponentIssue, StringsPerFilePath } from './component-issue';
-
-export class ImportNonMainFiles extends ComponentIssue {
-  description = 'importing non-main files';
-  solution = 'the dependency should expose its API from the main file';
-  data: StringsPerFilePath = {};
-  isCacheBlocker = false;
-}
