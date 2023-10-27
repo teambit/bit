da383771f5f97ea67f322f1f7d591d60603a5703
Luv Kapur <luv@bit.dev>
2023-10-27 12:08:47 -0400

    optimize preview loading

--- scopes/compositions/composition-card/composition-card.tsx
+++ scopes/compositions/composition-card/composition-card.tsx
@@ -1,11 +1,10 @@
-import React, { useState } from 'react';
+import React from 'react';
 import classnames from 'classnames';
 import type { ComponentModel } from '@teambit/component';
 import type { Composition } from '@teambit/compositions';
 import { Icon } from '@teambit/evangelist.elements.icon';
 import { ComponentComposition } from '@teambit/compositions';
 import { ellipsis } from '@teambit/design.ui.styles.ellipsis';
-import { PreviewSkeleton } from './composition-card-skeleton';
 import styles from './composition-card.module.scss';
 
 export type CompositionCardProps = {
@@ -13,8 +12,6 @@ export type CompositionCardProps = {
   composition: Composition;
   openCompositionLink?: string;
   previewClass?: string;
-  isLoading?: boolean;
-  parentRef?: React.RefObject<HTMLDivElement>;
 } & React.HTMLAttributes<HTMLDivElement>;
 
 export const CompositionCard = React.memo(_CompositionCard);
@@ -25,39 +22,26 @@ function _CompositionCard({
   className,
   openCompositionLink,
   previewClass,
-  isLoading = true,
-  parentRef,
   ...rest
 }: CompositionCardProps) {
-  const [previewLoading, setLoading] = useState(isLoading);
-
-  React.useEffect(() => {
-    setLoading(isLoading);
-  }, [isLoading]);
-
-  const [isNearViewport, ref] = useNearViewport(parentRef);
+  const Composition = React.useMemo(() => {
+    return (
+      <ComponentComposition
+        className={previewClass}
+        includeEnv={false}
+        loading={'lazy'}
+        composition={composition}
+        component={component}
+        viewport={1280}
+        scrolling="no"
+        previewName="compositions"
+      />
+    );
+  }, [composition.identifier, component.id.toString(), previewClass]);
 
   return (
-    <div {...rest} ref={ref} key={composition.identifier} className={classnames(styles.compositionCard, className)}>
-      <div className={styles.compositionPreview}>
-        {!previewLoading && (
-          <ComponentComposition
-            onLoad={() => {
-              setLoading(false);
-            }}
-            onError={() => {
-              // we need to handle exceptions in ther iframe and what to show here
-              setLoading(false);
-            }}
-            loading={isNearViewport ? 'eager' : 'lazy'}
-            className={previewClass}
-            composition={composition}
-            component={component}
-            pubsub={false}
-          />
-        )}
-        <div className={styles.previewOverlay}>{previewLoading && <PreviewSkeleton />}</div>
-      </div>
+    <div {...rest} key={composition.identifier} className={classnames(styles.compositionCard, className)}>
+      <div className={styles.compositionPreview}>{Composition}</div>
       <div className={styles.bottom}>
         <span className={classnames(ellipsis, styles.displayName)}>{composition.displayName}</span>
         {openCompositionLink && (
@@ -68,35 +52,4 @@ function _CompositionCard({
       </div>
     </div>
   );
-}
-
-function useNearViewport(parentRef, offset = 800): [boolean, React.RefObject<HTMLDivElement>] {
-  const [isNear, setIsNear] = useState(false);
-  const ref = React.useRef<HTMLDivElement>(null);
-
-  React.useEffect(() => {
-    const handleScroll = () => {
-      if (!ref.current) return;
-
-      const rect = ref.current.getBoundingClientRect();
-      const leftIsNear = rect.left >= 0 && rect.left <= window.innerWidth + offset;
-      const rightIsNear = rect.right >= -offset && rect.right <= window.innerWidth;
-
-      setIsNear(leftIsNear || rightIsNear);
-    };
-
-    if (parentRef.current) {
-      parentRef.current.addEventListener('scroll', handleScroll, { passive: true });
-    }
-
-    handleScroll();
-
-    return () => {
-      if (parentRef.current) {
-        parentRef.current.removeEventListener('scroll', handleScroll);
-      }
-    };
-  }, [parentRef, offset]);
-
-  return [isNear, ref];
 }
--- scopes/compositions/panels/composition-gallery/composition-gallery.tsx
+++ scopes/compositions/panels/composition-gallery/composition-gallery.tsx
@@ -3,7 +3,7 @@ import type { ComponentModel } from '@teambit/component';
 import { Icon } from '@teambit/design.elements.icon';
 import { LinkedHeading } from '@teambit/documenter.ui.linked-heading';
 import { useNavigate } from '@teambit/base-react.navigation.link';
-import { CompositionCard } from '@teambit/composition-card';
+import { CompositionCard, CompositionCardSkeleton } from '@teambit/composition-card';
 import styles from './composition-gallery.module.scss';
 
 export type CompositionGalleryProps = {
@@ -14,27 +14,27 @@ export type CompositionGalleryProps = {
 export function CompositionGallery({ component, isLoading }: CompositionGalleryProps) {
   const navigate = useNavigate();
   const hasCompositions = component.compositions.length > 0;
-  const carouselRef = React.useRef<HTMLDivElement>(null);
 
   if (!hasCompositions) return null;
+
   return (
     <div className={styles.compositionGallery}>
       {/* TODO - @oded replace with panelCard */}
       <LinkedHeading size="sm" className={styles.title}>
         <Icon of="eye" /> <span>PREVIEW</span>
       </LinkedHeading>
-      <div className={styles.carousel} ref={carouselRef}>
-        {component.compositions.map((composition) => {
+      <div className={styles.carousel}>
+        {component.compositions.map((composition, index) => {
+          if (isLoading && index > 2) return null;
+          if (isLoading) return <CompositionCardSkeleton />;
           return (
             <CompositionCard
-              isLoading={isLoading}
               key={composition.identifier.toLowerCase()}
               onClick={() => navigate(`~compositions/${composition.identifier.toLowerCase()}`)}
               className={styles.compositionGalleryCard}
               previewClass={styles.preview}
               composition={composition}
               component={component}
-              parentRef={carouselRef}
             />
           );
         })}
