fe981b3d2ccdbd98a6efc2b6484b7095eaa7ce75
Luv Kapur <luv@bit.dev>
2023-11-02 16:06:03 -0400

    fix code compare nav

---
+++ components/ui/code-compare/code-compare-editor/index.ts
@@ -0,0 +1,3 @@
+export { CodeCompareEditor } from './code-compare-editor';
+export type { CodeCompareEditorProps } from './code-compare-editor';
+export { useCodeCompareEditor, CodeCompareEditorProvider } from './code-compare-editor.provider';
