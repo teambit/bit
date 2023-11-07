d27b899b1505150f9d132415b445e85d64d7446d
David First <davidnet@gmail.com>
2023-11-07 18:21:27 -0500

    re-adding heapdump for remote memory profiling

--- scopes/harmony/bit/app.ts
+++ scopes/harmony/bit/app.ts
@@ -8,6 +8,10 @@ process.on('uncaughtException', (err) => {
 
 import { nativeCompileCache } from '@teambit/toolbox.performance.v8-cache';
 
+// to activate this on demand, run in the terminal "kill -USR2 <pid>".
+// eslint-disable-next-line @typescript-eslint/no-unused-vars, import/order
+const heapdump = require('heapdump');
+
 // Enable v8 compile cache, keep this before other imports
 nativeCompileCache?.install();
 
@@ -15,6 +19,14 @@ import './hook-require';
 import { bootstrap } from '@teambit/legacy/dist/bootstrap';
 import { handleErrorAndExit } from '@teambit/legacy/dist/cli/handle-errors';
 import { runCLI } from './load-bit';
+
+// eslint-disable-next-line
+if (process.env.SCOPE_NAME && process.env.HEAPDUMP_PATH) {
+  process.on('SIGUSR2', function () {
+    // eslint-disable-next-line
+    heapdump.writeSnapshot(`${process.env.HEAPDUMP_PATH}/${process.env.SCOPE_NAME}` + Date.now() + '.heapsnapshot');
+  });
+}
 
 // eslint-disable-next-line @typescript-eslint/no-floating-promises
 initApp();
