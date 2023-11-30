9088199c22918b6e9cdf93024797ed07046dda58
Ran Mizrahi <ranm8@users.noreply.github.com>
2023-12-01 00:14:31 +0200

    fixes for apps build and deploy context (#8221)

--- scopes/harmony/application/app-build-context.ts
+++ scopes/harmony/application/app-build-context.ts
@@ -4,12 +4,25 @@ import { BuildContext, PipeName, TaskResults } from '@teambit/builder';
 import { LaneId } from '@teambit/lane-id';
 import { AppContext } from './app-context';
 
+export type AppBuildContextCreate = {
+  appContext: AppContext;
+  buildContext: BuildContext;
+  name: string;
+  appComponent: Component;
+  artifactsDir: string;
+  capsule: Capsule;
+};
+
 export class AppBuildContext extends AppContext implements BuildContext {
   constructor(
     readonly appContext: AppContext,
     readonly capsuleNetwork: Network,
     readonly previousTasksResults: TaskResults[],
     readonly pipeName: PipeName,
+    readonly capsule: Capsule,
+    readonly name: string,
+    readonly appComponent: Component,
+    readonly artifactsDir: string,
     readonly laneId?: LaneId | undefined
   ) {
     super(
@@ -25,31 +38,17 @@ export class AppBuildContext extends AppContext implements BuildContext {
       appContext.envVariables
     );
   }
-  /**
-   * name of the type of the app. e.g. `react-app`
-   */
-  name: string;
-  /**
-   * Application capsule
-   */
-  capsule: Capsule;
 
-  /**
-   * app Component object
-   */
-  appComponent: Component;
-
-  /**
-   * A path (relative to the capsule root) that contain artifacts that will be picked and store by default
-   */
-  artifactsDir: string;
-
-  static create(appContext: AppContext, buildContext: BuildContext) {
+  static create({ name, capsule, appComponent, artifactsDir, appContext, buildContext }: AppBuildContextCreate) {
     return new AppBuildContext(
       appContext,
       buildContext.capsuleNetwork,
       buildContext.previousTasksResults,
       buildContext.pipeName,
+      capsule,
+      name,
+      appComponent,
+      artifactsDir,
       buildContext.laneId
     );
   }
