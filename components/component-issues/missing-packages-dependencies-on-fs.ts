a5e0464e192eccf426e66f71f8c90af3bc0d80c1
David First <davidnet@gmail.com>
2023-12-01 12:13:01 -0500

    refactor, add the data whether a file is a dev-file in the missing-packages issue (#8223)
    currently in order to know whether a missing package is a dev or runtime
    we have to call `getDevFiles` which is expensive and not always
    available.

--- components/component-issues/missing-packages-dependencies-on-fs.ts
+++ components/component-issues/missing-packages-dependencies-on-fs.ts
@@ -1,7 +1,18 @@
-import { ComponentIssue, StringsPerFilePath } from './component-issue';
+import { ComponentIssue, ISSUE_FORMAT_SPACE } from './component-issue';
+
+export type MissingPackagesData = {
+  filePath: string;
+  isDevFile: boolean;
+  missingPackages: string[];
+};
 
 export class MissingPackagesDependenciesOnFs extends ComponentIssue {
   description = `missing packages or links from node_modules to the source`;
   solution = `run "bit install --add-missing-deps" to fix both issues`;
-  data: StringsPerFilePath = {};
+  data: MissingPackagesData[] = [];
+  dataToString(): string {
+    return this.data
+      .map((d) => `${ISSUE_FORMAT_SPACE}${d.filePath} -> ${this.formatDataFunction(d.missingPackages)}`)
+      .join('\n');
+  }
 }
