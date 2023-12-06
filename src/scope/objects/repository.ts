734f78125e7c46810984507a7a38605810fa77b3
David First <davidnet@gmail.com>
2023-12-05 20:43:47 -0500

    perf, improve performance of scope.list() by parsing only the given types (#8225)
    When running `bit cc`, the index.json is deleted, so any command
    afterwards needs to rebuild this file. To build it, it needs to read all
    local objects to check whether they're ModelComponent or Lane objects
    and if so, add them to the index.
    For large workspaces, this takes very long because it not only reads
    every single file in the local scope but it also parses it.
    
    This PR improves it by extracting the header after reading the file and
    checking the type before parsing it. So then only for the given types
    the parsing take place.
    
    The improvement is huge. For a scope with 168,266 files, it was 107
    seconds, and with this PR it's 24 seconds.

--- src/scope/objects/repository.ts
+++ src/scope/objects/repository.ts
@@ -204,38 +204,45 @@ export default class Repository {
     const refs = await this.listRefs();
     const concurrency = concurrentIOLimit();
     const objects: BitObject[] = [];
+    const loadGracefully = process.argv.includes('--never-exported');
+    const isTypeIncluded = (obj: BitObject) => types.some((type) => type.name === obj.constructor.name); // avoid using "obj instanceof type" for Harmony to call this function successfully
     await pMapPool(
       refs,
       async (ref) => {
-        let object: BitObject;
-        try {
-          object = await this.load(ref);
-        } catch (err: any) {
-          // this is needed temporarily to allow `bit reset --never-exported` to fix the bit-id-comp-id error.
-          // in a few months, we can remove this condition (around min 2024)
-          if (
-            process.argv.includes('--never-exported') &&
-            (err.constructor.name === 'BitIdCompIdError' || err.constructor.name === 'MissingScope')
-          ) {
-            logger.debug(`bit-id-comp-id error, moving an object to trash ${ref.toString()}`);
-            await this.moveOneObjectToTrash(ref);
-            return;
-          }
-          throw err;
-        }
-
-        types.forEach((type) => {
-          if (
-            object instanceof type ||
-            type.name === object.constructor.name // needed when Harmony calls this function
-          )
-            objects.push(object);
-        });
+        const object = loadGracefully
+          ? await this.loadRefDeleteIfInvalid(ref)
+          : await this.loadRefOnlyIfType(ref, types);
+        if (!object) return;
+        if (loadGracefully && !isTypeIncluded(object)) return;
+        objects.push(object);
       },
       { concurrency }
     );
-
     return objects;
+  }
+
+  async loadRefDeleteIfInvalid(ref: Ref) {
+    try {
+      return await this.load(ref, true);
+    } catch (err: any) {
+      // this is needed temporarily to allow `bit reset --never-exported` to fix the bit-id-comp-id error.
+      // in a few months, we can remove this condition (around min 2024)
+      if (err.constructor.name === 'BitIdCompIdError' || err.constructor.name === 'MissingScope') {
+        logger.debug(`bit-id-comp-id error, moving an object to trash ${ref.toString()}`);
+        await this.moveOneObjectToTrash(ref);
+        return undefined;
+      }
+      throw err;
+    }
+  }
+
+  async loadRefOnlyIfType(ref: Ref, types: Types): Promise<BitObject | null> {
+    const objectPath = this.objectPath(ref);
+    const fileContentsRaw = await fs.readFile(objectPath);
+    const fileContents = this.onRead(fileContentsRaw);
+    const typeNames = types.map((type) => type.name);
+    const parsedObject = await BitObject.parseObjectOnlyIfType(fileContents, typeNames, objectPath);
+    return parsedObject;
   }
 
   async listRefs(cwd = this.getPath()): Promise<Array<Ref>> {
