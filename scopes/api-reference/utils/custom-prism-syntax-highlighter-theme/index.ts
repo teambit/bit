ff4426d7ad4088b048db1b3f4c551938c221e1b8
Luv Kapur <luv@bit.dev>
2023-12-07 10:37:27 -0500

    fix build

--- scopes/api-reference/utils/custom-prism-syntax-highlighter-theme/index.ts
+++ scopes/api-reference/utils/custom-prism-syntax-highlighter-theme/index.ts
@@ -1,3 +1,4 @@
-import DefaultBitPrismTheme from './custom-theme';
+import _DefaultBitPrismTheme from './custom-theme';
 
+const DefaultBitPrismTheme = _DefaultBitPrismTheme as { [key: string]: React.CSSProperties };
 export default DefaultBitPrismTheme;
