cbf8b43df12a44ce68d3b3b451e86ef99cb591af
David First <davidnet@gmail.com>
2023-12-20 14:57:27 -0500

    change .eslintrc to .eslintrc.js

---
+++ e2e/.eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  rules: {
+    'no-unused-expressions': [0],
+    'default-case': 'off',
+  },
+  env: {
+    mocha: true,
+  },
+  globals: {
+    describe: true,
+    it: true,
+  },
+};
