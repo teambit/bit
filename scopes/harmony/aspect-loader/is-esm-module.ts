3230aa43431f187a304b7b9ed3b24f41b0e4272b
Gilad Shoham <shoham.gilad@gmail.com>
2023-12-28 13:25:15 +0200

    fix(aspects) - improve isEsmModule function (support .mjs file paths)

--- scopes/harmony/aspect-loader/is-esm-module.ts
+++ scopes/harmony/aspect-loader/is-esm-module.ts
@@ -4,6 +4,7 @@ import { readFileSync } from 'fs';
 
 export function isEsmModule(path: string) {
   try {
+    if (path.endsWith('.mjs')) return true;
     const root = findRoot(path);
     const packageJsonString = readFileSync(join(root, 'package.json')).toString('utf-8');
     const packageJson = JSON.parse(packageJsonString);
