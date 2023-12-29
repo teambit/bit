5e685d26d80fa476056563edb00ae12bd1194299
Jinjiang <zhaojinjiang@me.com>
2023-12-29 18:14:00 +0800

    share linked modules in preview

--- scopes/preview/preview/preview-modules.tsx
+++ scopes/preview/preview/preview-modules.tsx
@@ -1,28 +1,32 @@
-import type { PreviewModule, ModuleFile } from './types/preview-module';
+import {
+  PreviewModules,
+  PreviewModule,
+  PREVIEW_MODULES as defaultPreviewModules,
+} from '@teambit/preview.modules.preview-modules';
 
-type ModuleId = string;
-
-export class PreviewModules extends Map<ModuleId, PreviewModule> {
-  onSet = new Set<() => void>();
-
-  override set(id: ModuleId, preview: PreviewModule) {
-    super.set(id, preview);
-    this.onSet.forEach((callback) => callback());
-    return this;
-  }
-
-  loadComponentPreviews(compId: string, previews: Record<string, ModuleFile[]>) {
-    Object.entries(previews).forEach(([previewName, moduleFile]) => {
-      const preview = this.get(previewName);
-      if (!preview) return; // TODO - ok for now
-
-      preview.componentMap[compId] = moduleFile;
-    });
+declare global {
+  interface Window {
+    __bit_preview_modules: PreviewModules;
   }
 }
 
-export const PREVIEW_MODULES = new PreviewModules();
+// singleton for the browser
+function getPreviewModules() {
+  let modules = defaultPreviewModules;
+  if (typeof window !== 'undefined') {
+    if (!(window as any).__bit_preview_modules) {
+      window.__bit_preview_modules = defaultPreviewModules;
+    } else {
+      modules = window.__bit_preview_modules;
+    }
+  }
+  return modules;
+}
+
+const PREVIEW_MODULES = getPreviewModules();
 
 export function linkModules(previewName: string, previewModule: PreviewModule) {
   PREVIEW_MODULES.set(previewName, previewModule);
 }
+
+export { PreviewModules, PREVIEW_MODULES };
