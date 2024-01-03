80b74d4cbf8c30a4d72b40bd5b3b71e53fb8b031
Luv Kapur <luv@bit.dev>
2024-01-03 11:43:28 -0500

    sync with origin/master

--- scopes/component/dev-files/dev-files.main.runtime.ts
+++ scopes/component/dev-files/dev-files.main.runtime.ts
@@ -193,9 +193,7 @@ export class DevFilesMain {
   async getDevFilesForConsumerComp(consumerComponent: LegacyComponent): Promise<string[]> {
     const componentId = await this.workspace.resolveComponentId(consumerComponent.id);
     // Do not change the storeInCache=false arg. if you think you need to change it, please talk to Gilad first
-    const component = await this.workspace.get(componentId, consumerComponent, true, false, {
-      loadExtensions: false,
-    });
+    const component = await this.workspace.get(componentId, consumerComponent, true, false, { loadExtensions: false });
     if (!component) throw Error(`failed to transform component ${consumerComponent.id.toString()} in harmony`);
     const computedDevFiles = await this.computeDevFiles(component);
     return computedDevFiles.list();
