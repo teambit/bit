772b01512c5dcf589d999a89901402ae17935fcc
odedre <oded.reuveny@gmail.com>
2024-01-10 20:19:16 +0200

    imported components and updated version dd & test-compare with color tokens

---
+++ components/ui/version-dropdown/index.ts
@@ -0,0 +1,4 @@
+export { SimpleVersion, DetailedVersion } from './version-dropdown-placeholder';
+export type { VersionProps } from './version-dropdown-placeholder';
+export { VersionDropdown } from './version-dropdown';
+export type { DropdownComponentVersion, GetActiveTabIndex } from './version-dropdown';
