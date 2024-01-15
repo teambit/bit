af16a415db0a0eb80bc1bf01621ac05579317048
Oded Reuveny <oded.reuveny@gmail.com>
2024-01-15 17:42:54 +0200

    display user bar in component route (#8376)

---
+++ components/ui/version-dropdown/version-dropdown.composition.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import { MemoryRouter } from 'react-router-dom';
+import { ThemeCompositions } from '@teambit/documenter.theme.theme-compositions';
+import { VersionDropdown } from './version-dropdown';
+
+const style = { display: 'flex', justifyContent: 'center', alignContent: 'center' };
+
+export const VersionDropdownWithOneVersion = () => {
+  return (
+    <ThemeCompositions style={style}>
+      <VersionDropdown
+        useComponentVersions={() => ({
+          tags: [{ version: '0.1' }],
+        })}
+        currentVersion="0.1"
+      />
+    </ThemeCompositions>
+  );
+};
+
+export const VersionDropdownWithMultipleVersions = () => {
+  const versions = ['0.3', '0.2', '0.1'].map((version) => ({ version }));
+
+  return (
+    <ThemeCompositions style={style}>
+      <MemoryRouter>
+        <VersionDropdown
+          useComponentVersions={() => ({
+            tags: [{ version: '0.1' }],
+          })}
+          currentVersion={versions[0].version}
+        />
+      </MemoryRouter>
+    </ThemeCompositions>
+  );
+};
