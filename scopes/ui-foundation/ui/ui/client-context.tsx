6dc57ef5ad263c0306f3e4cac7ec226a3aad0aab
odedre <oded.reuveny@gmail.com>
2024-01-21 15:43:06 +0200

    save theme to local storage

--- scopes/ui-foundation/ui/ui/client-context.tsx
+++ scopes/ui-foundation/ui/ui/client-context.tsx
@@ -1,6 +1,6 @@
 import React, { ReactNode } from 'react';
-// import { Theme } from '@teambit/base-ui.theme.theme-provider';
 import { ThemeSwitcher } from '@teambit/design.themes.theme-toggler';
+import { useLocalStorage } from '@teambit/community.ui.hooks.use-local-storage';
 import { IconFont } from '@teambit/design.theme.icons-font';
 import { LoaderRibbon } from '@teambit/base-ui.loaders.loader-ribbon';
 import { Roboto } from '@teambit/base-ui.theme.fonts.roboto';
@@ -11,13 +11,14 @@ import styles from './client-context.module.scss';
 
 export function ClientContext({ children }: { children: ReactNode }) {
   const [loaderApi, isLoading] = useLoaderApi();
+  const [theme, setTheme] = useLocalStorage('theme', 'light');
 
   return (
     <React.StrictMode>
       {/* TODO - try moving LoaderContext to contextSlot, and LoaderRibbon to hudSlot */}
       <LoaderContext.Provider value={loaderApi}>
         <IconFont query="c7vhhb" />
-        <ThemeSwitcher>
+        <ThemeSwitcher activeTheme={theme} handleThemeChange={(t) => setTheme(t.themeName)}>
           <Roboto />
           <LoaderRibbon active={isLoading} className={styles.loader} />
           {children}
