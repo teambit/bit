0465ce260e35c1e6adea8ec37cb1ff280a91ed09
David First <davidnet@gmail.com>
2024-01-26 13:26:14 -0500

    improvement(scope-rename), support renaming exported components (#8456)
    Currently, `bit scope rename` supports only new components. For
    exported, it suggests users to run `bit rename` one by one, which can be
    an hassle.
    This PR combines the logic from "rename", "renameScope" and
    "renameOwner" to one place "renameMultiple" and allows renaming multiple
    components, some can be exported, some can be new.
    Previously, some of the actions during the rename were done only on
    "rename" or only on "renameScope", now all of them are aligned.

--- scopes/component/renaming/rename.cmd.ts
+++ scopes/component/renaming/rename.cmd.ts
@@ -14,7 +14,7 @@ export type RenameOptions = {
 export class RenameCmd implements Command {
   name = 'rename <current-name> <new-name>';
   description =
-    'rename component. if tagged/exported, create a new component and deprecate the original component. otherwise just renames current component';
+    'rename component. if exported, create a new component and deprecate the original component. otherwise just renames current component';
   helpUrl = 'reference/components/renaming-components';
   arguments = [
     {
@@ -31,15 +31,15 @@ export class RenameCmd implements Command {
   alias = '';
   options = [
     ['s', 'scope <scope-name>', 'define the scope for the newly created component'],
+    ['r', 'refactor', 'update the import/require statements in all dependent components (in the same workspace)'],
+    ['', 'preserve', 'avoid renaming files and variables/classes according to the new component name'],
+    ['', 'ast', 'EXPERIMENTAL. use ast to transform files instead of regex'],
+    ['', 'delete', 'EXPERIMENTAL. instead of deprecating the original component, delete it'],
     [
       'p',
       'path <relative-path>',
       'relative path in the workspace to place new component in. by default, the directory of the new component is from your workspace\'s "defaultScope" value',
     ],
-    ['r', 'refactor', 'update the import/require statements in all dependent components (in the same workspace)'],
-    ['', 'preserve', 'avoid renaming files and variables/classes according to the new component name'],
-    ['', 'ast', 'EXPERIMENTAL. use ast to transform files instead of regex'],
-    ['', 'delete', 'EXPERIMENTAL. instead of deprecating the original component, delete it'],
   ] as CommandOptions;
   loader = true;
   remoteOp = true;
--- src/utils/string/replace-package-name.ts
+++ src/utils/string/replace-package-name.ts
@@ -10,25 +10,16 @@ const doubleQuotes = '"';
  * (${quoteType}~?) => string starts with a quote, and then zero or one tilda (~). this whole part got replaced by $1
  * ${oldName} => this is the only part in the string that really get changed, all other are preserved
  * (/|${quoteType}) => string that is either a slash or a quote. (must be one of the two). this whole part got replace by $2
+ *
+ * @todo:
+ * it can be easily replace with:
+ * return str.replace(new RegExp(`${oldName}(['"/])`, 'g'), `${newName}$1`);
+ * the only different is that it allows anything before the package-name, not only tilda.
  */
-export default function replacePackageName(
-  str: string,
-  oldName: string,
-  newName: string,
-  prefixBeforeInternalPath?: string
-): string {
-  if (prefixBeforeInternalPath) {
-    [singleQuote, doubleQuotes].forEach((quoteType) => {
-      str = str.replace(new RegExp(`(${quoteType}~?)${oldName}(${quoteType})`, 'g'), `$1${newName}$2`);
-    });
-    [singleQuote, doubleQuotes].forEach((quoteType) => {
-      str = str.replace(new RegExp(`(${quoteType}~?)${oldName}(/)`, 'g'), `$1${newName}$2${prefixBeforeInternalPath}/`);
-    });
-  } else {
-    [singleQuote, doubleQuotes].forEach((quoteType) => {
-      str = str.replace(new RegExp(`(${quoteType}~?)${oldName}(/|${quoteType})`, 'g'), `$1${newName}$2`);
-    });
-  }
+export default function replacePackageName(str: string, oldName: string, newName: string): string {
+  [singleQuote, doubleQuotes].forEach((quoteType) => {
+    str = str.replace(new RegExp(`(${quoteType}~?)${oldName}(/|${quoteType})`, 'g'), `$1${newName}$2`);
+  });
 
   return str;
 }
