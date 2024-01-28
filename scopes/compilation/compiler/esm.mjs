2b09cea782f06a79be24c34a04b4bfe7a16b5f21
Gilad Shoham <shoham.gilad@gmail.com>
2024-01-28 11:54:18 +0200

    add missing exports in esm files

--- scopes/compilation/compiler/esm.mjs
+++ scopes/compilation/compiler/esm.mjs
@@ -4,5 +4,6 @@ import cjsModule from './index.js';
 export const CompilerAspect = cjsModule.CompilerAspect;
 export const CompilerTask = cjsModule.CompilerTask;
 export const CompilationInitiator = cjsModule.CompilationInitiator;
+export const CompilerErrorEvent = cjsModule.CompilerErrorEvent;
 
 export default cjsModule;
---
+++ scopes/workspace/config-merger/esm.mjs
@@ -0,0 +1,7 @@
+// eslint-disable-next-line import/no-unresolved
+import cjsModule from './index.js';
+
+export const ConfigMergerAspect = cjsModule.ConfigMergerAspect;
+export const ComponentConfigMerger = cjsModule.ComponentConfigMerger;
+
+export default cjsModule;
