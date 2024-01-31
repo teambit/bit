85f2e5c242175ed54f7c55c8926d5b214a70f703
David First <davidnet@gmail.com>
2024-01-31 12:31:01 -0500

    add back latest-versions

--- src/api/scope/index.ts
+++ src/api/scope/index.ts
@@ -7,5 +7,6 @@ import fetch from './lib/fetch';
 import put from './lib/put';
 import * as scopeConfig from './lib/scope-config';
 import initScope from './lib/scope-init';
+import latestVersions from './lib/latest-versions';
 
-export { catObject, catComponent, catLane, initScope, put, fetch, scopeConfig, catScope, remove };
+export { catObject, catComponent, catLane, initScope, put, fetch, scopeConfig, catScope, remove, latestVersions };
