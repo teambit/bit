983f714b9c4b91719de90c998290c7ea910486b7
David First <davidnet@gmail.com>
2024-02-02 13:24:12 -0500

    fix tests

--- e2e/harmony/update-dependencies.e2e.ts
+++ e2e/harmony/update-dependencies.e2e.ts
@@ -134,9 +134,10 @@ describe('update-dependencies command', function () {
       before(() => {
         helper.scopeHelper.getClonedScope(secondScopeBeforeUpdate, secondRemotePath);
         updateRemote = helper.scopeHelper.getNewBareScope('-remote-update');
+        // edit: this is not the case anymore. now it calculates the flattened-deps, so it does import the minimal possible
         // delete the remote from the update-remote scope. it should not reach the remote
         // for the dependencies, it should only install via registry.
-        helper.scopeHelper.removeRemoteScope(helper.scopes.remote, false, updateRemote.scopePath);
+        // helper.scopeHelper.removeRemoteScope(helper.scopes.remote, false, updateRemote.scopePath);
         helper.scopeHelper.addRemoteScope(secondRemotePath, updateRemote.scopePath);
         const data = [
           {
