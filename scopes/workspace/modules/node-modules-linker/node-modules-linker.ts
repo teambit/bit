5d050eb859ad886f3fad0579d7a28340c9aa6ba5
David First <davidnet@gmail.com>
2024-02-22 12:59:43 -0500

    avoid clearing the cache if not needed

--- scopes/workspace/modules/node-modules-linker/node-modules-linker.ts
+++ scopes/workspace/modules/node-modules-linker/node-modules-linker.ts
@@ -44,6 +44,11 @@ export default class NodeModuleLinker {
     this.components = this.components.filter((component) => this.bitMap.getComponentIfExist(component.id));
     const links = await this.getLinks();
     const linksResults = this.getLinksResults();
+    if (!linksResults.length) {
+      // avoid clearing the cache if it ends up with no links. (e.g. happens when mistakenly generating links for a
+      // component not in the workspace)
+      return [];
+    }
     const workspacePath = this.workspace.path;
     links.addBasePath(workspacePath);
     await links.persistAllToFS();
