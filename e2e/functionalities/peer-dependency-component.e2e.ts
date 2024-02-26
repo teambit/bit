5278164e5bddbcf80cdf5df36a94e07dc88e3e57
Zoltan Kochan <z@kochan.io>
2024-02-26 18:40:29 +0100

    test: set peer

--- e2e/functionalities/peer-dependency-component.e2e.ts
+++ e2e/functionalities/peer-dependency-component.e2e.ts
@@ -1,7 +1,9 @@
 import path from 'path';
-import { expect } from 'chai';
+import chai, { expect } from 'chai';
 import fs from 'fs-extra';
 import Helper from '../../src/e2e-helper/e2e-helper';
+
+chai.use(require('chai-string'));
 
 describe('set-peer', function () {
   this.timeout(0);
@@ -96,6 +98,55 @@ describe('set-peer', function () {
           expect(bitMap.comp2.config['teambit.dependencies/dependency-resolver'].peer).to.eq(true);
           expect(bitMap.comp2.config['teambit.dependencies/dependency-resolver'].defaultPeerRange).to.eq('0');
         });
+      });
+    });
+  });
+});
+
+describe('set-peer for existing component', function () {
+  this.timeout(0);
+  let helper: Helper;
+  before(() => {
+    helper = new Helper();
+  });
+  after(() => {
+    helper.scopeHelper.destroy();
+  });
+  describe('a component is a peer dependency', () => {
+    let workspaceCapsulesRootDir: string;
+    before(() => {
+      helper.scopeHelper.reInitLocalScope();
+      helper.fixtures.populateComponents(2);
+      helper.command.install();
+      helper.command.snapAllComponents();
+      helper.command.setPeer('comp2', '0');
+      helper.command.install();
+      helper.command.build();
+      workspaceCapsulesRootDir = helper.command.capsuleListParsed().workspaceCapsulesRootDir;
+    });
+    it('should save the peer dependency in the model', () => {
+      const { head } = helper.command.catComponent('comp2');
+      const output = helper.command.showComponentParsed(`${helper.scopes.remote}/comp1`);
+      expect(output.peerDependencies[0]).to.deep.equal({
+        id: `${helper.scopes.remote}/comp2@${head}`,
+        relativePaths: [],
+        packageName: `@${helper.scopes.remote}/comp2`,
+        versionRange: '0',
+      });
+      const depResolver = output.extensions.find(({ name }) => name === 'teambit.dependencies/dependency-resolver');
+      const peerDep = depResolver.data.dependencies[0];
+      expect(peerDep.packageName).to.eq(`@${helper.scopes.remote}/comp2`);
+      expect(peerDep.lifecycle).to.eq('peer');
+      expect(peerDep.version).to.eq(head);
+      expect(peerDep.versionRange).to.eq('0');
+    });
+    it('adds peer dependency to the generated package.json', () => {
+      const { head } = helper.command.catComponent('comp1');
+      const pkgJson = fs.readJsonSync(
+        path.join(workspaceCapsulesRootDir, `${helper.scopes.remote}_comp1@${head}/package.json`)
+      );
+      expect(pkgJson.peerDependencies).to.deep.equal({
+        [`@${helper.scopes.remote}/comp2`]: '0',
       });
     });
   });
