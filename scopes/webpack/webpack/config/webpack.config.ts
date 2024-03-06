d30cf29f790ce36bfbd757aa6b8fc3f2e8071b88
Konstantinos Leimonis <k.leimonis@gmail.com>
2024-03-06 12:50:08 +0000

    chore: filter entries - teambit.api-reference - 20-30

--- scopes/webpack/webpack/config/webpack.config.ts
+++ scopes/webpack/webpack/config/webpack.config.ts
@@ -44,7 +44,7 @@ export function configFactory(target: Target, context: BundlerContext): Configur
     }, {});
 
     truthyEntries = Object.keys(truthyEntries)
-      .slice(0, 30)
+      .slice(20, 30)
       .reduce((acc, entryKey) => {
         acc[entryKey] = truthyEntries[entryKey];
         return acc;
