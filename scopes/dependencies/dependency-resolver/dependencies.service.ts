469453df274bd76dad6a977b733360c413287637
Gilad Shoham <shoham.gilad@gmail.com>
2024-03-06 16:52:54 +0200

    linting

--- scopes/dependencies/dependency-resolver/dependencies.service.ts
+++ scopes/dependencies/dependency-resolver/dependencies.service.ts
@@ -1,7 +1,7 @@
 import { EnvService, EnvDefinition, Env, EnvContext, ServiceTransformationMap } from '@teambit/envs';
 import highlight from 'cli-highlight';
-import { DependencyDetector } from './dependency-detector';
 import chalk from 'chalk';
+import { DependencyDetector } from './dependency-detector';
 
 export type DependenciesDescriptor = {
   id: string;
