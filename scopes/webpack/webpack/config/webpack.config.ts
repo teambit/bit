1e558fc8b052a4bef694c0d4fb5b22f95fc92a57
Konstantinos Leimonis <k.leimonis@gmail.com>
2024-03-11 16:35:02 +0000

    chore: remove one more entry

--- scopes/webpack/webpack/config/webpack.config.ts
+++ scopes/webpack/webpack/config/webpack.config.ts
@@ -44,7 +44,7 @@ export function configFactory(target: Target, context: BundlerContext): Configur
     }, {});
 
     truthyEntries = Object.keys(truthyEntries)
-      .slice(35, 47)
+      .slice(35, 46)
       .reduce((acc, entryKey) => {
         acc[entryKey] = truthyEntries[entryKey];
         return acc;
