7b5301a35d7f6f72979f9afe29cf346547989121
Luv Kapur <luv@bit.dev>
2024-04-19 14:54:14 -0400

    fix vulnerabilities (1)

--- scopes/react/react-native/react-native.env.ts
+++ scopes/react/react-native/react-native.env.ts
@@ -45,7 +45,7 @@ export class ReactNativeEnv implements Environment, DependenciesEnv, PreviewEnv 
         '@types/jest': '^26.0.0',
         '@types/react': '^17.0.8',
         '@types/react-dom': '^17.0.5',
-        '@types/react-native': '^0.64.1',
+        '@types/react-native': '^0.68.5',
         // This is added as dev dep since our jest file transformer uses babel plugins that require this to be installed
         '@babel/runtime': '7.23.2',
         '@types/testing-library__jest-dom': '5.9.5',
@@ -53,8 +53,8 @@ export class ReactNativeEnv implements Environment, DependenciesEnv, PreviewEnv 
       peerDependencies: {
         react: '^16.8.0 || ^17.0.0',
         'react-dom': '^16.8.0 || ^17.0.0',
-        'react-native': '^0.64.1',
-        'react-native-web': '^0.16.0',
+        'react-native': '^0.68.0',
+        'react-native-web': '^0.16.5',
       },
     };
   }
