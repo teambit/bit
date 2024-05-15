b2765a063315e84432d9e45fd8c39b11b56b6096
David First <davidnet@gmail.com>
2024-05-15 16:04:56 -0400

    log only when an env variable is provided

--- scopes/harmony/bit/autocomplete.ts
+++ scopes/harmony/bit/autocomplete.ts
@@ -3,13 +3,31 @@ import json from 'comment-json';
 
 const COMPLETION_FLAG = '--get-yargs-completions';
 
-const log = (msg) => fs.writeFileSync('compilation.log', `${msg}\n`);
+// to get the log working, add `export BIT_DEBUG_AUTOCOMPLETE=true` to your bash profile
+// prefixing the command with this won't work for some reason.
+const shouldLog = process.env.BIT_DEBUG_AUTOCOMPLETE === 'true';
+const logFile = 'compilation.log';
+if (shouldLog) fs.ensureFileSync(logFile);
+const log = (msg: string) => (shouldLog ? fs.appendFileSync(logFile, `${msg}\n`) : null);
 
-type Cmd = { name: string; options: string[]; description?: string };
+type Cmd = { name: string; options: string[]; description?: string; commands?: Cmd[] };
 
 export function autocomplete() {
+  log('[*] autocomplete started');
+  const results = getAutocompleteResults();
+  if (!results.length) {
+    log(`[*] autocomplete completed, no results found`);
+    return;
+  }
+  log(`[*] autocomplete completed with ${results.length} results`);
+  process.stdout.write(results.join('\n'), () => {
+    process.exit(0);
+  });
+}
+
+function getAutocompleteResults(): string[] {
   const argv = process.argv.slice(2).filter((arg) => arg !== COMPLETION_FLAG);
-  // remove the first element, it is always the command itself "bit"
+  // remove the first element, it is always the executable "bit"
   argv.shift();
   log(`argv: ${argv.join(' ')}, length: ${argv.length}`);
   if (argv.length === 0) {
@@ -18,8 +36,7 @@ export function autocomplete() {
   if (argv.length === 1) {
     // either "bit " or "bit x" where x can be any string without a space
     // in both cases, we should return all commands
-    printCommandNames();
-    process.exit(0);
+    return getAllCommandNames();
   }
 
   // argv has at least 2 elements, the first one is the command name.
@@ -27,38 +44,44 @@ export function autocomplete() {
   const allCommands = getAllCommands();
   const matchedCommand = allCommands.find((cmd) => cmd.name === command || cmd.name.startsWith(`${command} `));
   if (!matchedCommand) {
-    return;
+    return [];
   }
   log(`matchedCommand: ${matchedCommand.name}`);
   const currentArg = argv.length ? argv[argv.length - 1] : '';
   if (currentArg.startsWith('-')) {
-    printCommandFlags(matchedCommand);
-    process.exit(0);
+    return getCommandFlags(matchedCommand);
   }
   const commandArgs = matchedCommand.name.split(' ').slice(1);
   const firstCmdArg = commandArgs[0];
   const commandNamePrefixes = ['<component-name', '<component-pattern', '[component-name', '[component-pattern'];
   if (firstCmdArg && commandNamePrefixes.some((prefix) => firstCmdArg.startsWith(prefix))) {
     log(`completing component name`);
-    const compIds = getCompsFromBitmap();
-    process.stdout.write(compIds.join('\n'));
+    return getCompsFromBitmap();
   }
-  process.exit(0);
+  const subCommands = matchedCommand.commands || [];
+  if (subCommands.length && argv.length === 2) {
+    return getCommandNames(subCommands);
+  }
+  return [];
 }
 
-function printCommandFlags(cmd: Cmd): void {
-  cmd.options.map((opt) => {
+function getCommandFlags(cmd: Cmd): string[] {
+  return cmd.options.map((opt) => {
     const [, name, description] = opt;
-    process.stdout.write(`--${name}:${description}\n`);
+    return `--${name}:${description}`;
   });
 }
 
-function printCommandNames() {
+function getAllCommandNames(): string[] {
   const allCommands = getAllCommands();
-  allCommands.forEach((cmd) => {
+  return getCommandNames(allCommands);
+}
+
+function getCommandNames(commands: Cmd[]): string[] {
+  return commands.map((cmd) => {
     const name = cmd.name.split(' ')[0];
     const desc = cmd.description || '';
-    process.stdout.write(`${name}:${desc}\n`);
+    return `${name}:${desc}`;
   });
 }
 
