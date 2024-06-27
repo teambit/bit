4476b66e19450e17a49bb47ec5adab20dc6430ef
David First <davidnet@gmail.com>
2024-06-27 15:56:19 -0400

    add missing files

---
+++ scopes/harmony/doctor/doctor.main.runtime.ts
@@ -0,0 +1,17 @@
+import CLIAspect, { CLIMain, MainRuntime } from '@teambit/cli';
+import { DoctorAspect } from './doctor.aspect';
+import { DoctorCmd } from './doctor-cmd';
+
+export class DoctorMain {
+  static slots = [];
+  static dependencies = [CLIAspect];
+  static runtime = MainRuntime;
+  static async provider([cliMain]: [CLIMain]) {
+    cliMain.register(new DoctorCmd());
+    return new DoctorMain();
+  }
+}
+
+DoctorAspect.addRuntime(DoctorMain);
+
+export default DoctorMain;
