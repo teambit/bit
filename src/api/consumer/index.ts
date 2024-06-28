c08de5457716705e9dfac686fea7473e5de92f5a
David First <davidnet@gmail.com>
2024-06-28 11:00:21 -0400

    refactor, move clear-cache file from legacy to ClearCache aspect (#8985)

--- src/api/consumer/index.ts
+++ src/api/consumer/index.ts
@@ -4,16 +4,5 @@ import getScopeComponent from './lib/get-scope-component';
 import { listScope } from './lib/list-scope';
 import { add as remoteAdd, list as remoteList, remove as remoteRm } from './lib/remote';
 import show from './lib/show';
-import { clearCache } from './lib/clear-cache';
 
-export {
-  listScope,
-  getConsumerComponent,
-  getScopeComponent,
-  getComponentLogs,
-  remoteAdd,
-  remoteList,
-  remoteRm,
-  show,
-  clearCache,
-};
+export { listScope, getConsumerComponent, getScopeComponent, getComponentLogs, remoteAdd, remoteList, remoteRm, show };
