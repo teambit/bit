c6ba5a8a4141384b7ae51862095c5f1e207b8a55
David First <davidnet@gmail.com>
2024-06-28 11:54:51 -0400

    refactor, remove the unused get-component-logs file (#8986)

--- src/api/consumer/index.ts
+++ src/api/consumer/index.ts
@@ -1,8 +1,7 @@
-import getComponentLogs from './lib/get-component-logs';
 import getConsumerComponent from './lib/get-consumer-component';
 import getScopeComponent from './lib/get-scope-component';
 import { listScope } from './lib/list-scope';
 import { add as remoteAdd, list as remoteList, remove as remoteRm } from './lib/remote';
 import show from './lib/show';
 
-export { listScope, getConsumerComponent, getScopeComponent, getComponentLogs, remoteAdd, remoteList, remoteRm, show };
+export { listScope, getConsumerComponent, getScopeComponent, remoteAdd, remoteList, remoteRm, show };
