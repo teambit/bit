b2c34ca98072ea2f3adfb8e5654078b6b28f50b7
David First <davidnet@gmail.com>
2024-08-01 11:02:00 -0400

    add missing file scopes/harmony/cli/exceptions/yargs-exit-workaround.ts

---
+++ scopes/harmony/cli/exceptions/yargs-exit-workaround.ts
@@ -0,0 +1,7 @@
+import { BitError } from '@teambit/bit-error';
+
+export class YargsExitWorkaround extends BitError {
+  constructor(public exitCode: number, public helpMsg: string) {
+    super('Workaround for yargs exit issue');
+  }
+}
