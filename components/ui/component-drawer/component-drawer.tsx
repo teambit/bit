a1ba338b415e4dccbed58b489065e47fdb47fa1f
Luv Kapur <luv@bit.dev>
2024-08-08 10:41:41 -0400

    lint fix

--- components/ui/component-drawer/component-drawer.tsx
+++ components/ui/component-drawer/component-drawer.tsx
@@ -1,6 +1,7 @@
 import React, { useCallback, useContext, ReactNode, useMemo } from 'react';
 import classNames from 'classnames';
-import { ComponentTree, PayloadType } from '@teambit/ui-foundation.ui.side-bar';
+import { ComponentTree } from '@teambit/ui-foundation.ui.side-bar';
+import type { PayloadType } from '@teambit/ui-foundation.ui.side-bar';
 import { ComponentTreeSlot } from '@teambit/component-tree';
 import type { DrawerType } from '@teambit/ui-foundation.ui.tree.drawer';
 import { mutedItalic } from '@teambit/design.ui.styles.muted-italic';
