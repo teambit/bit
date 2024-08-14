0ac45323a6d249e996700e7d5ae11ebe2dcff905
Gilad Shoham <shoham.gilad@gmail.com>
2024-08-14 13:44:22 +0300

    fix(jest) - restore deleted jest.config file for backward compatibility

---
+++ scopes/react/react/jest/jest.config.js
@@ -0,0 +1,7 @@
+// This file is used for backward compatibility for old
+// envs that directly reference this file.
+// Please use the jest.cjs.config.js file instead.
+// Please do not delete this file.
+const cjsConfig = require('./jest.cjs.config');
+
+module.exports = cjsConfig;
