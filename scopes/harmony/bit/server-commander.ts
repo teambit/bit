8341f7c92431ed3068213e978d024c027dbd4d8e
David First <davidnet@gmail.com>
2024-10-09 14:34:42 -0400

    fix(bit-server), exclude "bit update" command

--- scopes/harmony/bit/server-commander.ts
+++ scopes/harmony/bit/server-commander.ts
@@ -212,7 +212,8 @@ export class ServerCommander {
 }
 
 export function shouldUseBitServer() {
-  const commandsToSkip = ['start', 'run', 'watch', 'server'];
+  // update command is interactive with option to press up/down, which doesn't work with the server.
+  const commandsToSkip = ['start', 'run', 'watch', 'server', 'update'];
   const hasFlag =
     process.env.BIT_CLI_SERVER === 'true' ||
     process.env.BIT_CLI_SERVER === '1' ||
