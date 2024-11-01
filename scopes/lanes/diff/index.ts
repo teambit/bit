92966ecc8060d8e50b89d3ac4297a9ec9cd4301b
David First <davidnet@gmail.com>
2024-11-01 15:49:56 -0400

    feat(lane-history-diff), introduce a new command "bit lane history-diff" (#9285)
    To show diff between two history-ids.

--- scopes/lanes/diff/index.ts
+++ scopes/lanes/diff/index.ts
@@ -1,2 +1,3 @@
 export { LaneDiffCmd } from './lane-diff.cmd';
+export { LaneHistoryDiffCmd } from './lane-history-diff.cmd';
 export { LaneDiffGenerator, LaneDiffResults } from './lane-diff-generator';
