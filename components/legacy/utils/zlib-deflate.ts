a5b58c2eb97ea920d7fc169bd969c6f4a80250a7
Gilad Shoham <shoham.gilad@gmail.com>
2024-12-01 16:16:40 +0200

    add tsignore

--- components/legacy/utils/zlib-deflate.ts
+++ components/legacy/utils/zlib-deflate.ts
@@ -4,6 +4,7 @@ import zlib from 'zlib';
 export default async function deflate(buffer: Buffer, filePath?: string): Promise<Buffer> {
   const deflateP = promisify(zlib.deflate);
   try {
+    // @ts-ignore should be fixed
     return await deflateP(buffer);
   } catch (err: any) {
     const filePathStr = filePath ? ` of "${filePath}"` : '';
