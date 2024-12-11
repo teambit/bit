24bc17768d8eeeffa11a65ed291f127ce4d3068f
David First <davidnet@gmail.com>
2024-12-11 12:12:26 -0500

    fix test

--- components/crypto/sha1/sha1.ts
+++ components/crypto/sha1/sha1.ts
@@ -7,6 +7,7 @@ import * as crypto from 'crypto';
  *  sha1('foo bar') // => '3773dea65156909838fa6c22825cafe090ff8030'
  * ```
  */
+// @ts-ignore
 export function sha1(data: string | Buffer, encoding: crypto.BinaryToTextEncoding = 'hex'): string {
   // @ts-ignore should be fixed after upgrading @types/node from '12.20.4' to > 20.
   return crypto.createHash('sha1').update(data).digest(encoding);
