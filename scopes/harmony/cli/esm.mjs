e8a7fbc73a6615c0fdd6b5bf76b1a5a2c8f415b1
Gilad Shoham <shoham.gilad@gmail.com>
2024-12-16 16:36:53 +0200

    internal(cli) - add missing exports to cli esm.mjs

--- scopes/harmony/cli/esm.mjs
+++ scopes/harmony/cli/esm.mjs
@@ -3,5 +3,7 @@ import cjsModule from './index.js';
 
 export const CLIAspect = cjsModule.CLIAspect;
 export const MainRuntime = cjsModule.MainRuntime;
+export const handleUnhandledRejection = cjsModule.handleUnhandledRejection;
+export const handleErrorAndExit = cjsModule.handleErrorAndExit;
 
 export default cjsModule;
