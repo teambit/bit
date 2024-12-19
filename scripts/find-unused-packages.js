7ef454541f991993b84c59eca42e77b36e803ae9
David First <davidnet@gmail.com>
2024-12-18 21:35:12 -0500

    fix find-unused-packages script to avoid searching in node_modules (#9393)
    Also, add an option to the script to auto-delete the unsued packages. 
    Delete 62 unused packages from workspace.jsonc.

--- scripts/find-unused-packages.js
+++ scripts/find-unused-packages.js
@@ -1,6 +1,6 @@
 const fs = require('fs-extra');
 const globby = require('globby');
-const { parse } = require('comment-json');
+const { parse, stringify } = require('comment-json');
 const pkgJson = require('../package.json');
 const allDeps = [...Object.keys(pkgJson.dependencies), ...Object.keys(pkgJson.devDependencies)];
 
@@ -10,7 +10,9 @@ const policy = workspaceJsonParsed['teambit.dependencies/dependency-resolver'].p
 const workspacePackages = [...Object.keys(policy.dependencies), ...Object.keys(policy.peerDependencies)];
 const sourceCode = ['src', 'scopes', 'e2e', 'components'];
 const sourceCodeAbs = sourceCode.map((dir) => `${__dirname}/../${dir}`);
-const sourceFiles = globby.sync(sourceCodeAbs);
+const sourceFiles = globby.sync(sourceCodeAbs, {
+  ignore: ['**/node_modules/**'],
+});
 
 let unused = [...allDeps];
 let unusedWorkspace = [...workspacePackages];
@@ -23,6 +25,10 @@ const whitelist = [
   'babel-plugin-ramda',
   'eslint-plugin-mocha',
   'eslint-plugin-promise',
+  'eslint-plugin-import',
+  'eslint-plugin-react',
+  'eslint-import-resolver-node',
+  'lint-staged',
   'mocha-circleci-reporter',
   'mocha-junit-reporter',
   'prettier-eslint',
@@ -90,3 +96,17 @@ console.log('[-] total packages', allDeps.length + workspacePackages.length);
 console.log('[-] total packages in use:', used.length);
 console.log('[-] total packages in package.json not in use:', unused.length);
 console.log('[-] total packages in workspace.jsonc not in use:', unusedWorkspace.length);
+
+function deleteUnusedFromWorkspaceJsonc() {
+  unusedWorkspace.forEach((dep) => {
+    delete policy.dependencies[dep];
+    delete policy.peerDependencies[dep];
+  });
+  fs.writeFileSync(`${__dirname}/../workspace.jsonc`, stringify(workspaceJsonParsed, null, 2));
+}
+
+// check whether "delete" was added to the command line arguments
+const deleteUnused = process.argv.includes('delete');
+if (deleteUnused) {
+  deleteUnusedFromWorkspaceJsonc();
+}
