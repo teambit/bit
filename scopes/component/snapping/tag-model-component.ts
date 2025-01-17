teambit/bit

commit 383539d7d93a9e5703789f34dd615f3a83ac37ff

refactor, reduce the usage of legacy scope/comp for snapping API (#9437)
Improve the Snapping API by changing from ConsumerComponent to Component
and prefer using Scope over LegacyScope when working with Remotes.

scopes/component/snapping/tag-model-component.ts
@@ -371,7 +371,7 @@ export async function tagModelComponent({
 
       addIntegritiesToConsumerComponentsGraphs(packageIntegritiesByPublishedPackages, allComponentsToTag);
       addBuildStatus(componentsToBuild, BuildStatus.Succeed);
-      await mapSeries(componentsToBuild, (consumerComponent) => snapping._enrichComp(consumerComponent));
+      await mapSeries(harmonyComps, (comp) => snapping.enrichComp(comp));
       if (populateArtifactsFrom) await updateHiddenProp(scope, populateArtifactsFrom);
     }
   }

components/legacy/scope/component-ops/scope-components-importer.ts
@@ -479,7 +479,7 @@ export class ScopeComponentsImporter {
 
   async importLanes(remoteLaneIds: LaneId[], includeLaneHistory = false): Promise<Lane[]> {
     const remotes = await getScopeRemotes(this.scope);
-    const objectsStreamPerRemote = await remotes.fetch(groupByScopeName(remoteLaneIds), this.scope, {
+    const objectsStreamPerRemote = await remotes.fetch(groupByScopeName(remoteLaneIds), {
       type: 'lane',
       includeLaneHistory,
     });
@@ -540,7 +540,7 @@ export class ScopeComponentsImporter {
     const remotes: Remotes = await getScopeRemotes(this.scope);
     const remote = await remotes.resolve(remoteName);
     const getExistingLegacy = async () => {
-      const multipleStreams = await remotes.fetch({ [remoteName]: hashes }, this.scope, { type: 'object' });
+      const multipleStreams = await remotes.fetch({ [remoteName]: hashes }, { type: 'object' });
       const existing = await this.streamToHashes(remoteName, multipleStreams[remoteName]);
       return existing;
     };
@@ -673,7 +673,7 @@ export class ScopeComponentsImporter {
     const remotes = await getScopeRemotes(this.scope);
     let bitObjectsList: BitObjectList;
     try {
-      const streams = await remotes.fetch({ [id.scope as string]: [id.toString()] }, this.scope);
+      const streams = await remotes.fetch({ [id.scope as string]: [id.toString()] });
       bitObjectsList = await this.multipleStreamsToBitObjects(streams);
     } catch (err: any) {
       logger.error(`getRemoteComponent, failed to get ${id.toString()}`, err);
@@ -695,7 +695,7 @@ export class ScopeComponentsImporter {
     });
     const remotes = await getScopeRemotes(this.scope);
     const grouped = groupByScopeName(ids);
-    const streams = await remotes.fetch(grouped, this.scope);
+    const streams = await remotes.fetch(grouped);
     return this.multipleStreamsToBitObjects(streams);
   }
 

components/legacy/scope/objects-fetcher/objects-fetcher.ts
@@ -158,7 +158,7 @@ ${failedScopesErr.join('\n')}`);
     const shouldThrowOnUnavailableScope = this.throwOnUnavailableScope && !this.fetchOptions.withoutDependencies;
     let remote: Remote;
     try {
-      remote = await this.remotes.resolve(scopeName, this.scope);
+      remote = await this.remotes.resolve(scopeName);
     } catch (err: any) {
       if (err instanceof ScopeNotFoundOrDenied) {
         throw new Error(`unable to import the following component(s): ${ids.join(', ')}.

components/scope/remotes/get-remote-by-name.ts
@@ -6,7 +6,7 @@ import { Remotes } from './remotes';
 export async function getRemoteByName(remoteName: string, consumer?: Consumer): Promise<Remote> {
   if (consumer) {
     const remotes: Remotes = await getScopeRemotes(consumer.scope);
-    return remotes.resolve(remoteName, consumer.scope);
+    return remotes.resolve(remoteName);
   }
   return Remotes.getScopeRemote(remoteName);
 }

components/scope/remotes/remotes.ts
@@ -6,7 +6,7 @@ import pMap from 'p-map';
 import { CURRENT_FETCH_SCHEMA, FETCH_OPTIONS } from '@teambit/legacy.scope-api';
 import { GlobalRemotes } from './global-remotes';
 import { logger } from '@teambit/legacy.logger';
-import { Scope, ScopeNotFound } from '@teambit/legacy.scope';
+import { Scope as LegacyScope, ScopeNotFound } from '@teambit/legacy.scope';
 import { DependencyGraph } from '@teambit/legacy.dependency-graph';
 import { prependBang } from '@teambit/legacy.utils';
 import { concurrentFetchLimit } from '@teambit/harmony.modules.concurrency';
@@ -18,7 +18,10 @@ import { ObjectItemsStream } from '@teambit/scope.objects';
 import { ScopeNotFoundOrDenied } from './exceptions/scope-not-found-or-denied';
 
 export class Remotes extends Map<string, Remote> {
-  constructor(remotes: [string, Remote][] = []) {
+  constructor(
+    remotes: [string, Remote][] = [],
+    protected thisScope?: LegacyScope
+  ) {
     super(remotes);
   }
 
@@ -29,11 +32,11 @@ export class Remotes extends Map<string, Remote> {
     return this.forEach((remote) => remote.validate());
   }
 
-  async resolve(scopeName: string, thisScope?: Scope | undefined): Promise<Remote> {
+  async resolve(scopeName: string): Promise<Remote> {
     const remote = super.get(scopeName);
     if (remote) return Promise.resolve(remote);
-    const scopeHost = await remoteResolver(scopeName, thisScope);
-    return new Remote(scopeHost, scopeName, undefined, thisScope?.name);
+    const scopeHost = await remoteResolver(scopeName, this.thisScope);
+    return new Remote(scopeHost, scopeName, undefined, this.thisScope?.name);
   }
 
   isHub(scope: string): boolean {
@@ -43,7 +46,6 @@ export class Remotes extends Map<string, Remote> {
 
   async fetch(
     idsGroupedByScope: { [scopeName: string]: string[] }, // option.type determines the id: component-id/lane-id/object-id (hash)
-    thisScope: Scope,
     options: Partial<FETCH_OPTIONS> = {},
     context?: Record<string, any>
   ): Promise<{ [remoteName: string]: ObjectItemsStream }> {
@@ -66,7 +68,7 @@ export class Remotes extends Map<string, Remote> {
       Object.keys(idsGroupedByScope),
       async (scopeName) => {
         try {
-          const remote = await this.resolve(scopeName, thisScope);
+          const remote = await this.resolve(scopeName);
           const objectsStream = await remote.fetch(idsGroupedByScope[scopeName], fetchOptions, context);
           objectsStreamPerRemote[scopeName] = objectsStream;
           return objectsStream;
@@ -107,11 +109,11 @@ ${failedScopesErr.join('\n')}`);
     return objectsStreamPerRemote;
   }
 
-  async latestVersions(ids: ComponentID[], thisScope: Scope): Promise<ComponentID[]> {
+  async latestVersions(ids: ComponentID[]): Promise<ComponentID[]> {
     const groupedIds = this._groupByScopeName(ids);
 
     const promises = Object.entries(groupedIds).map(([scopeName, scopeIds]) =>
-      this.resolve(scopeName, thisScope).then((remote) => remote.latestVersions(scopeIds))
+      this.resolve(scopeName).then((remote) => remote.latestVersions(scopeIds))
     );
 
     const components = await Promise.all(promises);
@@ -125,10 +127,10 @@ ${failedScopesErr.join('\n')}`);
    * entire scope graph. however, when asking for multiple ids in the same scope, which is more
    * likely to happen, it'll harm the performance.
    */
-  async scopeGraphs(ids: ComponentID[], thisScope: Scope): Promise<DependencyGraph[]> {
+  async scopeGraphs(ids: ComponentID[]): Promise<DependencyGraph[]> {
     const groupedIds = this._groupByScopeName(ids);
     const graphsP = Object.keys(groupedIds).map(async (scopeName) => {
-      const remote = await this.resolve(scopeName, thisScope);
+      const remote = await this.resolve(scopeName);
       const dependencyGraph = await remote.graph();
       dependencyGraph.setScopeName(scopeName);
       return dependencyGraph;
@@ -173,10 +175,10 @@ ${failedScopesErr.join('\n')}`);
     return Remotes.load(remotes);
   }
 
-  static load(remotes: { [key: string]: string }, thisScope?: Scope): Remotes {
+  static load(remotes: { [key: string]: string }, thisScope?: LegacyScope): Remotes {
     const models = [];
 
-    if (!remotes) return new Remotes();
+    if (!remotes) return new Remotes(undefined, thisScope);
 
     forEach(remotes, (name, host) => {
       const remote = Remote.load(name, host, thisScope);
@@ -184,6 +186,6 @@ ${failedScopesErr.join('\n')}`);
       models.push([remote.name, remote]);
     });
 
-    return new Remotes(models);
+    return new Remotes(models, thisScope);
   }
 }

components/scope/remotes/scope-remotes.ts
@@ -1,16 +1,16 @@
 import { ComponentID } from '@teambit/component-id';
 import { GlobalRemotes } from './global-remotes';
-import { Scope } from '@teambit/legacy.scope';
+import { Scope as LegacyScope } from '@teambit/legacy.scope';
 import { Remotes } from './remotes';
 
-export async function getScopeRemotes(scope: Scope): Promise<Remotes> {
+export async function getScopeRemotes(scope: LegacyScope): Promise<Remotes> {
   const globalRemotes = await GlobalRemotes.load();
   const globalObj = globalRemotes.toPlainObject();
   return Remotes.load({ ...globalObj, ...scope.scopeJson.remotes }, scope);
 }
 
-export async function fetchRemoteVersions(scope: Scope, componentIds: ComponentID[]): Promise<ComponentID[]> {
+export async function fetchRemoteVersions(scope: LegacyScope, componentIds: ComponentID[]): Promise<ComponentID[]> {
   const externals = componentIds.filter((id) => !scope.isLocal(id));
   const remotes = await getScopeRemotes(scope);
-  return remotes.latestVersions(externals, scope);
+  return remotes.latestVersions(externals);
 }

scopes/cloud/cloud/cloud.main.runtime.ts
@@ -602,7 +602,7 @@ export class CloudMain {
   static PRESET_ORGS = ['bitdev', 'teambit', 'bitdesign', 'frontend', 'backend'];
 
   async getCloudScopes(scopes: string[]): Promise<ScopeDescriptor[]> {
-    const remotes = await this.scope._legacyRemotes();
+    const remotes = await this.scope.getRemoteScopes();
     const filteredScopesToFetch = scopes.filter((scope) => {
       return ScopeID.isValid(scope) && remotes.isHub(scope);
     });

scopes/component/remove/remove-components.ts
@@ -8,7 +8,7 @@ import { CENTRAL_BIT_HUB_NAME, CENTRAL_BIT_HUB_URL, LATEST_BIT_VERSION } from '@
 import { BitError } from '@teambit/bit-error';
 import { logger } from '@teambit/legacy.logger';
 import { Http } from '@teambit/scope.network';
-import { Remotes, getScopeRemotes } from '@teambit/scope.remotes';
+import { Remotes } from '@teambit/scope.remotes';
 import { deleteComponentsFiles } from './delete-component-files';
 import { ComponentsList } from '@teambit/legacy.component-list';
 import { RemovedObjects } from '@teambit/legacy.scope';
@@ -77,7 +77,7 @@ async function removeRemote(
   force: boolean
 ): Promise<RemovedObjects[]> {
   const groupedBitsByScope = groupArray(bitIds, 'scope');
-  const remotes = workspace ? await getScopeRemotes(workspace.scope.legacyScope) : await Remotes.getGlobalRemotes();
+  const remotes = workspace ? await workspace.scope.getRemoteScopes() : await Remotes.getGlobalRemotes();
   const shouldGoToCentralHub = remotes.shouldGoToCentralHub(Object.keys(groupedBitsByScope));
   if (shouldGoToCentralHub) {
     const http = await Http.connect(CENTRAL_BIT_HUB_URL, CENTRAL_BIT_HUB_NAME);
@@ -88,7 +88,7 @@ async function removeRemote(
   }
   const context = {};
   const removeP = Object.keys(groupedBitsByScope).map(async (key) => {
-    const resolvedRemote = await remotes.resolve(key, workspace?.scope.legacyScope);
+    const resolvedRemote = await remotes.resolve(key);
     const idsStr = groupedBitsByScope[key].map((id) => id.toStringWithoutVersion());
     return resolvedRemote.deleteMany(idsStr, force, context);
   });

commit 7d6d91ba7283cfb599cdf76258c7a8ef3484de0d

fix(tag), fix buildStatus when tagging with --build aspects (#9477)
In some cases, when tagging or snapping with `--build` flag, the
buildStatus remain `pending`, instead of `succeed`.
This is a regression caused by https://github.com/teambit/bit/pull/9437.
It happens for example when tagging/snapping aspects.
It's happening because the `ConsumerComponent instance inside the
harmony component is different than the one originally created. Although
the harmony component is created by the legacy, but in case the
`Workspace` aspect already had this component in the cache, it won't use
the provided legacy, but create a new one.
As a result, the buildStatus was set to one instance of
ConsumerComponent and another instance was saved into the filesystem.

This PR fixes it by making sure it's using the same instance of
`ConsumerComponent`.

scopes/component/snapping/tag-model-component.ts
@@ -255,7 +255,7 @@ export async function tagModelComponent({
   const autoTagComponents = autoTagData.map((autoTagItem) => autoTagItem.component);
   const autoTagComponentsFiltered = autoTagComponents.filter((c) => !idsToTag.has(c.id));
   const autoTagIds = ComponentIdList.fromArray(autoTagComponentsFiltered.map((autoTag) => autoTag.id));
-  const allComponentsToTag = [...componentsToTag, ...autoTagComponentsFiltered];
+  let allComponentsToTag = [...componentsToTag, ...autoTagComponentsFiltered];
 
   const messagesFromEditorFetcher = new MessagePerComponentFetcher(idsToTag, autoTagIds);
   const getMessagePerId = async () => {
@@ -309,42 +309,52 @@ export async function tagModelComponent({
   if (soft) {
     if (!consumer) throw new Error(`unable to soft-tag without consumer`);
     consumer.updateNextVersionOnBitmap(allComponentsToTag, preReleaseId);
-  } else {
-    await snapping._addFlattenedDependenciesToComponents(allComponentsToTag, rebuildDepsGraph);
-    await snapping._addDependenciesGraphToComponents(components);
-    await snapping.throwForDepsFromAnotherLane(allComponentsToTag);
-    if (!build) emptyBuilderData(allComponentsToTag);
-    addBuildStatus(allComponentsToTag, BuildStatus.Pending);
+    return {
+      taggedComponents: componentsToTag,
+      autoTaggedResults: autoTagData,
+      publishedPackages: [],
+      stagedConfig,
+    };
+  }
 
-    const currentLane = consumer ? consumer.getCurrentLaneId() : undefined;
-    await mapSeries(allComponentsToTag, async (component) => {
-      const results = await snapping._addCompToObjects({
-        source: component,
-        lane,
-        shouldValidateVersion: Boolean(build),
-        addVersionOpts: {
-          addToUpdateDependentsInLane: updateDependentsOnLane,
-          setHeadAsParent,
-          detachHead,
-          overrideHead: overrideHead,
-        },
-      });
-      if (workspace) {
-        const modelComponent = component.modelComponent || (await legacyScope.getModelComponent(component.id));
-        await updateVersions(workspace, stagedConfig!, currentLane!, modelComponent, results.addedVersionStr, true);
-      } else {
-        const tagData = tagDataPerComp?.find((t) => t.componentId.isEqualWithoutVersion(component.id));
-        if (tagData?.isNew) results.version.removeAllParents();
-      }
+  await snapping._addFlattenedDependenciesToComponents(allComponentsToTag, rebuildDepsGraph);
+  await snapping._addDependenciesGraphToComponents(components);
+  await snapping.throwForDepsFromAnotherLane(allComponentsToTag);
+  if (!build) emptyBuilderData(allComponentsToTag);
+  addBuildStatus(allComponentsToTag, BuildStatus.Pending);
+
+  const currentLane = consumer ? consumer.getCurrentLaneId() : undefined;
+  await mapSeries(allComponentsToTag, async (component) => {
+    const results = await snapping._addCompToObjects({
+      source: component,
+      lane,
+      shouldValidateVersion: Boolean(build),
+      addVersionOpts: {
+        addToUpdateDependentsInLane: updateDependentsOnLane,
+        setHeadAsParent,
+        detachHead,
+        overrideHead: overrideHead,
+      },
     });
-
     if (workspace) {
-      await workspace.scope.legacyScope.stagedSnaps.write();
+      const modelComponent = component.modelComponent || (await legacyScope.getModelComponent(component.id));
+      await updateVersions(workspace, stagedConfig!, currentLane!, modelComponent, results.addedVersionStr, true);
+    } else {
+      const tagData = tagDataPerComp?.find((t) => t.componentId.isEqualWithoutVersion(component.id));
+      if (tagData?.isNew) results.version.removeAllParents();
     }
+  });
+
+  if (workspace) {
+    await workspace.scope.legacyScope.stagedSnaps.write();
   }
 
   const publishedPackages: string[] = [];
-  let harmonyComps: Component[] = [];
+  const harmonyCompsToTag = await (workspace || scope).getManyByLegacy(allComponentsToTag);
+  // this is not necessarily the same as the previous allComponentsToTag. although it should be, because
+  // harmonyCompsToTag is created from allComponentsToTag. however, for aspects, the getMany returns them from cache
+  // and therefore, their instance of ConsumerComponent can be different than the one in allComponentsToTag.
+  allComponentsToTag = harmonyCompsToTag.map((c) => c.state._consumer);
   if (build) {
     const onTagOpts: OnTagOpts = {
       disableTagAndSnapPipelines,
@@ -358,40 +368,39 @@ export async function tagModelComponent({
     const isolateOptions = { packageManagerConfigRootDir, seedersOnly, populateArtifactsIgnorePkgJson };
     const builderOptions = { exitOnFirstFailedTask, skipTests, skipTasks: skipTasksParsed };
 
-    const componentsToBuild = allComponentsToTag.filter((c) => !c.isRemoved());
+    const componentsToBuild = harmonyCompsToTag.filter((c) => !c.isDeleted());
     if (componentsToBuild.length) {
-      await scope.reloadAspectsWithNewVersion(componentsToBuild);
-      harmonyComps = await (workspace || scope).getManyByLegacy(componentsToBuild);
-      const { builderDataMap } = await builder.tagListener(harmonyComps, onTagOpts, isolateOptions, builderOptions);
+      const componentsToBuildLegacy: ConsumerComponent[] = componentsToBuild.map((c) => c.state._consumer);
+      await scope.reloadAspectsWithNewVersion(componentsToBuildLegacy);
+      const { builderDataMap } = await builder.tagListener(
+        componentsToBuild,
+        onTagOpts,
+        isolateOptions,
+        builderOptions
+      );
       const buildResult = scope.builderDataMapToLegacyOnTagResults(builderDataMap);
 
-      snapping._updateComponentsByTagResult(componentsToBuild, buildResult);
-      const packageIntegritiesByPublishedPackages = snapping._getPublishedPackages(componentsToBuild);
+      snapping._updateComponentsByTagResult(componentsToBuildLegacy, buildResult);
+      const packageIntegritiesByPublishedPackages = snapping._getPublishedPackages(componentsToBuildLegacy);
       publishedPackages.push(...Array.from(packageIntegritiesByPublishedPackages.keys()));
 
       addIntegritiesToConsumerComponentsGraphs(packageIntegritiesByPublishedPackages, allComponentsToTag);
-      addBuildStatus(componentsToBuild, BuildStatus.Succeed);
-      await mapSeries(harmonyComps, (comp) => snapping.enrichComp(comp));
+      addBuildStatus(componentsToBuildLegacy, BuildStatus.Succeed);
+      await mapSeries(componentsToBuild, (comp) => snapping.enrichComp(comp));
       if (populateArtifactsFrom) await updateHiddenProp(scope, populateArtifactsFrom);
     }
   }
 
-  let removedComponents: ComponentIdList | undefined;
-  if (!soft) {
-    removedComponents = await removeDeletedComponentsFromBitmap(allComponentsToTag, workspace);
-    if (lane) {
-      const msgStr = message ? ` (${message})` : '';
-      const laneHistory = await legacyScope.lanes.updateLaneHistory(lane, `snap${msgStr}`);
-      legacyScope.objects.add(laneHistory);
-    }
-    await legacyScope.objects.persist();
-    await removeMergeConfigFromComponents(unmergedComps, allComponentsToTag, workspace);
-    if (workspace) {
-      await linkToNodeModulesByComponents(
-        harmonyComps.length ? harmonyComps : await workspace.scope.getManyByLegacy(allComponentsToTag),
-        workspace
-      );
-    }
+  const removedComponents = await removeDeletedComponentsFromBitmap(allComponentsToTag, workspace);
+  if (lane) {
+    const msgStr = message ? ` (${message})` : '';
+    const laneHistory = await legacyScope.lanes.updateLaneHistory(lane, `snap${msgStr}`);
+    legacyScope.objects.add(laneHistory);
+  }
+  await legacyScope.objects.persist();
+  await removeMergeConfigFromComponents(unmergedComps, allComponentsToTag, workspace);
+  if (workspace) {
+    await linkToNodeModulesByComponents(harmonyCompsToTag, workspace);
   }
 
   // clear all objects. otherwise, ModelComponent has the wrong divergeData