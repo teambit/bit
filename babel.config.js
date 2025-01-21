544a404a816422356d75805ff4990232f97a4f7d
David First <davidnet@gmail.com>
2025-01-21 14:48:21 -0500

    add back babel.config.js

---
+++ babel.config.js
@@ -0,0 +1,38 @@
+module.exports = function (api) {
+  api.cache(true);
+
+  const presets = [
+    '@babel/preset-react',
+    '@babel/typescript',
+    [
+      '@babel/preset-env',
+      {
+        targets: {
+          node: 8,
+        },
+        useBuiltIns: 'usage',
+        corejs: 3,
+      },
+    ],
+  ];
+  const plugins = [
+    [
+      '@babel/plugin-transform-modules-commonjs',
+      {
+        lazy: () => true,
+      },
+    ],
+    'babel-plugin-transform-typescript-metadata',
+    ['@babel/plugin-proposal-decorators', { legacy: true }],
+    ['@babel/plugin-transform-runtime'],
+    ['@babel/plugin-transform-object-rest-spread'],
+    ['@babel/plugin-transform-class-properties'],
+  ];
+
+  return {
+    presets,
+    plugins,
+    only: ['**/*.ts', '**/*.tsx'],
+    ignore: ['components/*'],
+  };
+};
