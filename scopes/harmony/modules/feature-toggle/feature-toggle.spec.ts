827bb0f65b167c70391fffeaf87a42d6bd34030a
David First <davidnet@gmail.com>
2025-01-29 13:13:28 -0500

    upgrade mocha-babel env

--- scopes/harmony/modules/feature-toggle/feature-toggle.spec.ts
+++ scopes/harmony/modules/feature-toggle/feature-toggle.spec.ts
@@ -3,7 +3,7 @@ import { expect } from 'chai';
 import { addFeature, ENV_VAR_FEATURE_TOGGLE, isFeatureEnabled } from './feature-toggle';
 
 describe('featureToggle', () => {
-  afterAll(() => {
+  after(() => {
     process.env[ENV_VAR_FEATURE_TOGGLE] = '';
   });
   describe('isFeatureEnabled', () => {
