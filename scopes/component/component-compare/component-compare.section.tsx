d32b8a40a912b582f34747c5e5fca6f529732f82
Luv Kapur <luv@bit.dev>
2025-02-06 15:41:15 -0500

    pinned widgets in minimal mode

--- scopes/component/component-compare/component-compare.section.tsx
+++ scopes/component/component-compare/component-compare.section.tsx
@@ -2,14 +2,16 @@ import React from 'react';
 import { Section } from '@teambit/component';
 import { MenuWidgetIcon } from '@teambit/ui-foundation.ui.menu-widget-icon';
 import { ComponentCompareUI } from './component-compare.ui.runtime';
+import { useWorkspaceMode } from '@teambit/workspace.ui.use-workspace-mode';
 
 export class ComponentCompareSection implements Section {
-  constructor(private componentCompare: ComponentCompareUI) {}
+  constructor(private componentCompare: ComponentCompareUI, private pinned: boolean) { }
 
   navigationLink = {
     href: '~compare',
     displayName: 'Compare',
-    children: <MenuWidgetIcon icon="compare" tooltipContent="Compare" />,
+    children: <CodeCompareMenuWidget />,
+    hideInMinimalMode: !this.pinned,
   };
 
   route = {
@@ -17,5 +19,11 @@ export class ComponentCompareSection implements Section {
     element: this.componentCompare.getComponentComparePage(),
   };
 
-  order = 35;
+  order = this.pinned ? 20 : 35;
+}
+
+function CodeCompareMenuWidget() {
+  const { isMinimal } = useWorkspaceMode();
+  if (!isMinimal) return null;
+  return <MenuWidgetIcon icon="compare" tooltipContent="Compare" />;
 }
