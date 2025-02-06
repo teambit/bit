3c5e03cc0ab54d8029340b9eca9fad1c8698ff3b
David First <davidnet@gmail.com>
2025-02-06 12:09:38 -0500

    feat(pkg), add a new API "modifyPackageJson" to the env

--- scopes/pkg/pkg/package-generator.ts
+++ scopes/pkg/pkg/package-generator.ts
@@ -1,9 +1,14 @@
 import { EnvContext, EnvHandler } from '@teambit/envs';
+import { Component } from '@teambit/component';
 import { PackageJsonProps } from './pkg.main.runtime';
+
+export type ModifyPackageJsonFunc = (
+  component: Component, packageJsonObject: PackageJsonProps) => Promise<PackageJsonProps>;
 
 export type PackageGeneratorOptions = {
   packageJson: PackageJsonProps;
   npmIgnore?: string[];
+  modifyPackageJson?: ModifyPackageJsonFunc;
 };
 
 /**
@@ -14,7 +19,8 @@ export class PackageGenerator {
   constructor(
     private _packageJson: PackageJsonProps = {},
     private _npmIgnore: string[] = [],
-    private context: EnvContext
+    private context: EnvContext,
+    private _modifyPackageJson?: ModifyPackageJsonFunc,
   ) {}
 
   get packageJsonProps() {
@@ -25,9 +31,13 @@ export class PackageGenerator {
     return this._npmIgnore;
   }
 
+  get modifyPackageJson(): ModifyPackageJsonFunc | undefined {
+    return this._modifyPackageJson;
+  }
+
   static from(options: PackageGeneratorOptions): EnvHandler<PackageGenerator> {
     return (context: EnvContext) => {
-      return new PackageGenerator(options.packageJson, options.npmIgnore, context);
+      return new PackageGenerator(options.packageJson, options.npmIgnore, context, options.modifyPackageJson);
     };
   }
 }
