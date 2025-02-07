563ce5ebdbad47eb19f3e53cf6eca2fb6e5fb186
Luv Kapur <luv@bit.dev>
2025-02-07 13:07:27 -0500

    feat(workspace): pinned widgets in minimal mode + lazy loading workspace overview compositions (#9531)
    This PR implements a pinned widget slot in the Component Aspect to allow
    pinning widgets to the component navigation bar. This PR also implements
    two pinned widgets - Code and Component Compare widget to be rendered
    only in minimal mode. It also enables lazy loading for workspace
    overview compositions, resulting in faster load times.

--- scopes/compositions/compositions/ui/composition-preview.tsx
+++ scopes/compositions/compositions/ui/composition-preview.tsx
@@ -30,6 +30,9 @@ export function ComponentComposition({ composition, component, queryParams = [],
       style={{ width: '100%', height: '100%' }}
       previewName="compositions"
       queryParams={compositionParams}
+      loading={'lazy'}
+      includeEnv={false}
+      viewport={1280}
     />
   );
 }
