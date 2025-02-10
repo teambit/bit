teambit/bit

commit 7097ddc5bbe34540ee1a5cde05bbfda882fe72dd

feat(pkg), add a new API "modifyPackageJson" to the env (#9530)
Until now, it was only possible to provide static props. 
This PR provides the ability to modify the props per component
dynamically before writing it to the filesystem.

scopes/pkg/pkg/pkg.main.runtime.ts
@@ -491,7 +491,12 @@ export class PkgMain {
     packageJsonObject: Record<string, any>
   ): Promise<Record<string, any>> {
     const newProps = this.getPackageJsonModifications(component);
-    return Object.assign(packageJsonObject, newProps);
+    const pkgJsonObj = Object.assign(packageJsonObject, newProps);
+    const env = this.envs.getEnv(component).env;
+    if (env.modifyPackageJson) {
+      return env.modifyPackageJson(component, pkgJsonObj);
+    }
+    return pkgJsonObj;
   }
 }
 

scopes/scope/export/export.main.runtime.ts
@@ -220,7 +220,7 @@ if the export fails with missing objects/versions/components, run "bit fetch --l
     );
     const updatedIds = _updateIdsOnBitMap(consumer.bitMap, updatedLocally);
     // re-generate the package.json, this way, it has the correct data in the componentId prop.
-    await linkToNodeModulesByIds(this.workspace, updatedIds, true);
+    await linkToNodeModulesByIds(this.workspace, updatedIds);
     await this.removeFromStagedConfig(exported);
     // ideally we should delete the staged-snaps only for the exported snaps. however, it's not easy, and it's ok to
     // delete them all because this file is mainly an optimization for the import process.

scopes/envs/envs/environment.ts
@@ -7,7 +7,7 @@ import type { Bundler, BundlerContext, DevServer, DevServerContext } from '@team
 import type { BuildTask } from '@teambit/builder';
 import type { SchemaExtractor } from '@teambit/schema';
 import type { WebpackConfigTransformer } from '@teambit/webpack';
-import type { PackageJsonProps } from '@teambit/pkg';
+import type { ModifyPackageJsonFunc, PackageJsonProps } from '@teambit/pkg';
 import type { DependencyDetector, EnvPolicyConfigObject } from '@teambit/dependency-resolver';
 import type { Capsule } from '@teambit/isolator';
 import type { Component } from '@teambit/component';
@@ -119,6 +119,12 @@ export interface PackageEnv extends Environment {
    * return `.npmignore` entries to be written before packing the component
    */
   getNpmIgnore?: (npmIgnoreContext?: GetNpmIgnoreContext) => string[];
+
+  /**
+   * in case the static "getPackageJsonProps" is not enough, and the package.json props need to be modified
+   * per component, use this function.
+   */
+  modifyPackageJson?: ModifyPackageJsonFunc;
 }
 
 export interface LinterEnv extends Environment {

scopes/pkg/pkg/index.ts
@@ -7,6 +7,6 @@ export type {
 } from './pkg.main.runtime';
 export type { PackageDependency, PackageDependencyFactory } from './package-dependency';
 export type { PackageEnv } from './package-env-type';
-export { PackageGenerator } from './package-generator';
+export { PackageGenerator, ModifyPackageJsonFunc } from './package-generator';
 export { PkgAspect as default, PkgAspect } from './pkg.aspect';
 export { PkgUI } from './pkg.ui.runtime';

scopes/pkg/pkg/package-generator.ts
@@ -1,9 +1,14 @@
 import { EnvContext, EnvHandler } from '@teambit/envs';
+import { Component } from '@teambit/component';
 import { PackageJsonProps } from './pkg.main.runtime';
+
+export type ModifyPackageJsonFunc = (
+  component: Component, packageJsonObject: PackageJsonProps) => Promise<PackageJsonProps>;
 
 export type PackageGeneratorOptions = {
   packageJson: PackageJsonProps;
   npmIgnore?: string[];
+  modifyPackageJson?: ModifyPackageJsonFunc;
 };
 
 /**
@@ -12,9 +17,10 @@ export type PackageGeneratorOptions = {
  */
 export class PackageGenerator {
   constructor(
+    private context: EnvContext,
     private _packageJson: PackageJsonProps = {},
     private _npmIgnore: string[] = [],
-    private context: EnvContext
+    private _modifyPackageJson?: ModifyPackageJsonFunc,
   ) {}
 
   get packageJsonProps() {
@@ -25,9 +31,13 @@ export class PackageGenerator {
     return this._npmIgnore;
   }
 
+  get modifyPackageJson(): ModifyPackageJsonFunc | undefined {
+    return this._modifyPackageJson;
+  }
+
   static from(options: PackageGeneratorOptions): EnvHandler<PackageGenerator> {
     return (context: EnvContext) => {
-      return new PackageGenerator(options.packageJson, options.npmIgnore, context);
+      return new PackageGenerator(context, options.packageJson, options.npmIgnore, options.modifyPackageJson);
     };
   }
 }

scopes/pkg/pkg/pkg.service.ts
@@ -45,6 +45,7 @@ export class PkgService implements EnvService<{}, PkgDescriptor> {
       getPackageJsonProps: () => packageGenerator.packageJsonProps,
       // TODO: somehow handle context here? used in the aspect env
       getNpmIgnore: () => packageGenerator.npmIgnore,
+      modifyPackageJson: packageGenerator.modifyPackageJson,
     };
   }
 

commit 7de1911fe1cb26373687700511a661143658c90c

fix(new), fix env-not-found regression error by not requiring the env to be loaded (#9544)
This is a regression caused by https://github.com/teambit/bit/pull/9530.
That API needed the env to be loaded when writing the package.json. In
some cases, this is hard to achieve. For `bit new`, when the env is not
there, it threw an error:

> environment with ID: X configured on component Y was not found

This PR fixes it by not throwing an error in such case.

scopes/pkg/pkg/pkg.main.runtime.ts
@@ -492,7 +492,7 @@ export class PkgMain {
   ): Promise<Record<string, any>> {
     const newProps = this.getPackageJsonModifications(component);
     const pkgJsonObj = Object.assign(packageJsonObject, newProps);
-    const env = this.envs.getEnv(component).env;
+    const env = this.envs.getOrCalculateEnv(component).env;
     if (env.modifyPackageJson) {
       return env.modifyPackageJson(component, pkgJsonObj);
     }

scopes/scope/export/export.main.runtime.ts
@@ -220,7 +220,7 @@ if the export fails with missing objects/versions/components, run "bit fetch --l
     );
     const updatedIds = _updateIdsOnBitMap(consumer.bitMap, updatedLocally);
     // re-generate the package.json, this way, it has the correct data in the componentId prop.
-    await linkToNodeModulesByIds(this.workspace, updatedIds);
+    await linkToNodeModulesByIds(this.workspace, updatedIds, true);
     await this.removeFromStagedConfig(exported);
     // ideally we should delete the staged-snaps only for the exported snaps. however, it's not easy, and it's ok to
     // delete them all because this file is mainly an optimization for the import process.