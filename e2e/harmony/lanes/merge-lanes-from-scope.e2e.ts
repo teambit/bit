0c81843d6bd8383c7110014b3d126fc80675211f
David First <davidnet@gmail.com>
2025-02-21 12:02:52 -0500

    improvement(merge-from-scope), skip loading aspects when merging main to a lane (#9570)
    When merging main into a lane from scope, it has to make a new snap with
    2 parents: main and lane. During the snap, we used to load aspects
    because when trying to avoid the aspect-loading, aspects data weren't
    saved correctly.
    The reason is that aspects-data gets saved on the component object
    during load. If aspect-loading doesn't happen, the env is not loaded and
    then when the component is loaded, it doesn't find the env and fallback
    to the default node-env.
    
    This PR introduces a new mechanism of merging aspects-data. In case both
    sides: lane and main has the same env, we can simply compare the
    aspect-data and if there is no conflict, we can keep or copy the data as
    needed.
    Eventually, before snap, we check which components were merged their
    data properly and for them, we don't load aspects. In case of data
    conflicts or env changes we do load aspects.

--- e2e/harmony/lanes/merge-lanes-from-scope.e2e.ts
+++ e2e/harmony/lanes/merge-lanes-from-scope.e2e.ts
@@ -618,4 +618,59 @@ describe('merge lanes from scope', function () {
       expect(fileContent).to.include('on-main');
     });
   });
+  describe('merge from scope, main to lane when they are diverged with custom env and dependencies update', () => {
+    let bareMerge;
+    let envId: string;
+    before(() => {
+      helper.scopeHelper.setNewLocalAndRemoteScopes();
+      helper.fixtures.populateComponents(2);
+
+      const envName = helper.env.setCustomEnv('node-env-dev-dep');
+      envId = `${helper.scopes.remote}/${envName}`;
+      helper.command.setEnv('comp1', envId);
+      helper.command.setEnv('comp2', envId);
+
+      helper.command.tagAllWithoutBuild();
+      helper.command.export();
+      helper.command.removeComponent(envId);
+      helper.command.createLane();
+      helper.command.snapComponentWithoutBuild('comp1', '--unmodified');
+      helper.command.export();
+
+      helper.command.switchLocalLane('main', '-x');
+      helper.command.tagAllWithoutBuild('--unmodified -m "second tag on main"');
+      helper.command.export();
+
+      bareMerge = helper.scopeHelper.getNewBareScope('-bare-merge');
+      helper.scopeHelper.addRemoteScope(helper.scopes.remotePath, bareMerge.scopePath);
+      helper.command.mergeLaneFromScope(bareMerge.scopePath, 'main', `${helper.scopes.remote}/dev`);
+    });
+    it('should keep the env data correctly', () => {
+      const comp1HeadOnLane = helper.command.getHeadOfLane(`${helper.scopes.remote}/dev`, `comp1`, bareMerge.scopePath);
+      const obj = helper.command.catComponent(`comp1@${comp1HeadOnLane}`, bareMerge.scopePath);
+      const envExt = obj.extensions.find((e) => e.name === 'teambit.envs/envs');
+      expect(envExt.config.env).to.equal(envId);
+      expect(envExt.data.id).to.equal(envId);
+    });
+    it('should update the dependencies according to main', () => {
+      const comp1HeadOnLane = helper.command.getHeadOfLane(`${helper.scopes.remote}/dev`, `comp1`, bareMerge.scopePath);
+      const obj = helper.command.catComponent(`comp1@${comp1HeadOnLane}`, bareMerge.scopePath);
+      expect(obj.dependencies[0].id.name).to.equal('comp2');
+      expect(obj.dependencies[0].id.version).to.equal('0.0.2');
+      expect(obj.flattenedDependencies[0].version).to.equal('0.0.2');
+      const depsResolver = obj.extensions.find((e) => e.name === 'teambit.dependencies/dependency-resolver');
+      const comp2 = depsResolver.data.dependencies.find((d) => d.id.includes('comp2'));
+      expect(comp2.version).to.equal('0.0.2');
+    });
+    it('should save the policy and deps from policy correctly', () => {
+      const comp1HeadOnLane = helper.command.getHeadOfLane(`${helper.scopes.remote}/dev`, `comp1`, bareMerge.scopePath);
+      const obj = helper.command.catComponent(`comp1@${comp1HeadOnLane}`, bareMerge.scopePath);
+      const depsExt = obj.extensions.find((e) => e.name === 'teambit.dependencies/dependency-resolver');
+      const policy = depsExt.data.policy;
+      expect(policy).to.have.lengthOf(1);
+      expect(policy[0].dependencyId).to.equal('is-positive');
+      const isPositive = depsExt.data.dependencies.find((d) => d.id === 'is-positive');
+      expect(isPositive.source).to.equal('env');
+    });
+  });
 });
