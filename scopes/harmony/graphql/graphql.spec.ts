teambit/bit

commit 3d15277fa876b3a60967a7b2b33f84202fadb113

perf, change Graph aspect "register" method to accept a function that returns schema to lazy-load graphql lib (#9588)
It helps reduce the number of files loaded with each bit command by 58.

scopes/harmony/graphql/graphql.main.runtime.ts
@@ -32,7 +32,7 @@ export type GraphQLConfig = {
 
 export type GraphQLServerSlot = SlotRegistry<GraphQLServer>;
 
-export type SchemaSlot = SlotRegistry<Schema>;
+export type SchemaSlot = SlotRegistry<Schema | (() => Schema)>;
 
 export type PubSubSlot = SlotRegistry<PubSubEngine>;
 
@@ -193,8 +193,10 @@ export class GraphqlMain {
 
   /**
    * register a new graphql module.
+   * @param schema a function that returns Schema. avoid passing the Schema directly, it's supported only for backward
+   * compatibility but really bad for performance. it pulls the entire graphql library.
    */
-  register(schema: Schema) {
+  register(schema: Schema | (() => Schema)) {
     // const module = new GraphQLModule(schema);
     this.moduleSlot.register(schema);
     return this;
@@ -263,7 +265,8 @@ export class GraphqlMain {
 
   private buildModules(schemaSlot?: SchemaSlot) {
     const schemaSlots = schemaSlot ? schemaSlot.toArray() : this.moduleSlot.toArray();
-    return schemaSlots.map(([extensionId, schema]) => {
+    return schemaSlots.map(([extensionId, schemaOrFunc]) => {
+      const schema = typeof schemaOrFunc === 'function' ? schemaOrFunc() : schemaOrFunc;
       const moduleDeps = this.getModuleDependencies(extensionId);
 
       const module = new GraphQLModule({

scopes/cloud/cloud/cloud.main.runtime.ts
@@ -718,7 +718,7 @@ export class CloudMain {
     const npmrc = new NpmrcCmd();
     npmrc.commands = [npmrcGenerateCmd];
     cli.register(loginCmd, logoutCmd, whoamiCmd, npmrc);
-    graphql.register(cloudSchema(cloudMain));
+    graphql.register(() => cloudSchema(cloudMain));
     if (workspace) {
       ui.registerOnStart(async () => {
         await cloudMain.setupAuthListener();

scopes/compilation/bundler/bundler.main.runtime.ts
@@ -150,7 +150,7 @@ export class BundlerMain {
     const bundler = new BundlerMain(config, pubsub, envs, devServerService, runtimeSlot, devServerTransformerSlot);
     envs.registerService(devServerService, new BundlerService());
 
-    graphql.register(devServerSchema(bundler));
+    graphql.register(() => devServerSchema(bundler));
 
     return bundler;
   }

scopes/component/component-compare/component-compare.main.runtime.ts
@@ -319,7 +319,7 @@ export class ComponentCompareMain {
       workspace
     );
     cli.register(new DiffCmd(componentCompareMain));
-    graphql.register(componentCompareSchema(componentCompareMain));
+    graphql.register(() => componentCompareSchema(componentCompareMain));
     return componentCompareMain;
   }
 }

scopes/component/component-sizer/component-sizer.main.runtime.ts
@@ -22,7 +22,7 @@ export class ComponentSizerMain {
   static runtime = MainRuntime;
   static async provider([preview, graphql]: [PreviewMain, GraphqlMain]) {
     const componentSizer = new ComponentSizerMain(preview);
-    graphql.register(componentSizerSchema(componentSizer));
+    graphql.register(() => componentSizerSchema(componentSizer));
     return componentSizer;
   }
 }

scopes/component/component/component.main.runtime.ts
@@ -173,7 +173,7 @@ export class ComponentMain {
       new FilesFragment(),
       new ExtensionsFragment(),
     ]);
-    graphql.register(componentSchema(componentExtension));
+    graphql.register(() => componentSchema(componentExtension));
 
     return componentExtension;
   }

scopes/component/deprecation/deprecation.main.runtime.ts
@@ -179,7 +179,7 @@ export class DeprecationMain {
     issues.registerAddComponentsIssues(deprecation.addDeprecatedDependenciesIssues.bind(deprecation));
     cli.register(new DeprecateCmd(deprecation), new UndeprecateCmd(deprecation));
     componentAspect.registerShowFragments([new DeprecationFragment(deprecation)]);
-    graphql.register(deprecationSchema(deprecation));
+    graphql.register(() => deprecationSchema(deprecation));
 
     return deprecation;
   }

scopes/component/dev-files/dev-files.main.runtime.ts
@@ -269,7 +269,7 @@ export class DevFilesMain {
       scope.registerOnCompAspectReCalc(calcDevOnLoad);
     }
 
-    graphql.register(devFilesSchema(devFiles));
+    graphql.register(() => devFilesSchema(devFiles));
     return devFiles;
   }
 }

commit 1292dc94b1262a0b76be7c903fd246230a9f5e30

fix(graph), fix getSchemas API to always return Schema[] (#9605)
This regression was introduced by #9588. The `register` method allows a
function to return a schema, but the getSchemas method was not updated
to support this change.

scopes/harmony/graphql/graphql.main.runtime.ts
@@ -87,21 +87,25 @@ export class GraphqlMain {
   /**
    * returns the schema for a specific aspect by its id.
    */
-  getSchema(aspectId: string) {
-    return this.moduleSlot.get(aspectId);
+  getSchema(aspectId: string): Schema | undefined {
+    const schemaOrFunc = this.moduleSlot.get(aspectId);
+    if (!schemaOrFunc) return undefined;
+    const schema = typeof schemaOrFunc === 'function' ? schemaOrFunc() : schemaOrFunc;
+    return schema;
   }
 
   /**
    * get multiple schema by aspect ids.
+   * used by the cloud.
    */
-  getSchemas(aspectIds: string[]) {
+  getSchemas(aspectIds: string[]): Schema[] {
     return this.moduleSlot
       .toArray()
       .filter(([aspectId]) => {
         return aspectIds.includes(aspectId);
       })
-      .map(([, schema]) => {
-        return schema;
+      .map(([, schemaOrFunc]) => {
+        return typeof schemaOrFunc === 'function' ? schemaOrFunc() : schemaOrFunc;
       });
   }
 

scopes/harmony/graphql/graphql.spec.ts
@@ -1,0 +1,39 @@
+import chai, { expect } from 'chai';
+import { loadManyAspects } from '@teambit/harmony.testing.load-aspect';
+import { WorkspaceAspect } from '@teambit/workspace';
+import { mockWorkspace, destroyWorkspace, WorkspaceData } from '@teambit/workspace.testing.mock-workspace';
+import { GraphqlAspect } from './graphql.aspect';
+import { GraphqlMain } from './graphql.main.runtime';
+import { LanesAspect } from '@teambit/lanes';
+
+chai.use(require('chai-fs'));
+
+describe('GraphQL Aspect', function () {
+  this.timeout(0);
+
+  describe('getSchemas and getSchema APIs', () => {
+    let graphql: GraphqlMain;
+    let workspaceData: WorkspaceData;
+    before(async () => {
+      workspaceData = mockWorkspace();
+      const { workspacePath } = workspaceData;
+      const harmony = await loadManyAspects([WorkspaceAspect, GraphqlAspect, LanesAspect], workspacePath);
+      graphql = harmony.get<GraphqlMain>(GraphqlAspect.id);
+    });
+    after(async () => {
+      await destroyWorkspace(workspaceData);
+    });
+    it('should bring the schemas when calling getSchemas API', async () => {
+      const schemas = graphql.getSchemas([LanesAspect.id]);
+      expect(schemas).to.be.an('array');
+      expect(schemas).to.have.lengthOf(1);
+      expect(schemas[0]).to.be.an('object');
+      expect(schemas[0]).to.not.be.an('function');
+    });
+    it('should bring the schemas when calling getSchema API', async () => {
+      const schema = graphql.getSchema(LanesAspect.id);
+      expect(schema).to.be.an('object');
+      expect(schema).to.not.be.an('function');
+    });
+  });
+});