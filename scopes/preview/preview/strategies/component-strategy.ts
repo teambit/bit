teambit/bit

commit e9bb00125ec142efac5355bfa6b43403d956f6ef

fix(preview) - fix conflicting preview chunk names (#9622)

scopes/preview/preview/strategies/component-strategy.ts
@@ -17,10 +17,10 @@ import type { ComponentPreviewMetaData, PreviewMain } from '../preview.main.runt
 import { generateComponentLink } from './generate-component-link';
 import { PreviewOutputFileNotFound } from '../exceptions';
 
-export const PREVIEW_CHUNK_SUFFIX = 'preview';
-export const COMPONENT_CHUNK_SUFFIX = 'component';
-export const PREVIEW_CHUNK_FILENAME_SUFFIX = `${PREVIEW_CHUNK_SUFFIX}.js`;
-export const COMPONENT_CHUNK_FILENAME_SUFFIX = `${COMPONENT_CHUNK_SUFFIX}.js`;
+export const PREVIEW_CHUNK_SUFFIX = 'preview-chunk';
+export const COMPONENT_CHUNK_SUFFIX = 'component-chunk';
+export const PREVIEW_CHUNK_FILENAME_SUFFIX = 'preview.js';
+export const COMPONENT_CHUNK_FILENAME_SUFFIX = 'component.js';
 
 export const COMPONENT_STRATEGY_SIZE_KEY_NAME = 'size';
 export const COMPONENT_STRATEGY_ARTIFACT_NAME = 'preview-component';
@@ -133,7 +133,7 @@ export class ComponentBundlingStrategy implements BundlingStrategy {
 
     const chunks = {
       componentPreview: this.getComponentChunkId(component.id, 'preview'),
-      component: context.splitComponentBundle ? component.id.toStringWithoutVersion() : undefined,
+      component: context.splitComponentBundle ? this.getComponentChunkId(component.id, 'component') : undefined,
     };
 
     const entries = {
@@ -172,7 +172,7 @@ export class ComponentBundlingStrategy implements BundlingStrategy {
   private getComponentChunkId(componentId: ComponentID, type: 'component' | 'preview') {
     const id =
       type === 'component'
-        ? componentId.toStringWithoutVersion()
+        ? `${componentId.toStringWithoutVersion()}-${COMPONENT_CHUNK_SUFFIX}`
         : `${componentId.toStringWithoutVersion()}-${PREVIEW_CHUNK_SUFFIX}`;
     return id;
   }
@@ -226,7 +226,7 @@ export class ComponentBundlingStrategy implements BundlingStrategy {
   ): Asset[] | undefined {
     if (!assets) return undefined;
 
-    const componentEntryId = component.id.toStringWithoutVersion();
+    const componentEntryId = this.getComponentChunkId(component.id, 'component');
     const componentPreviewEntryId = this.getComponentChunkId(component.id, 'preview');
     const componentFiles = entriesAssetsMap[componentEntryId]?.assets || [];
     const componentAuxiliaryFiles = entriesAssetsMap[componentEntryId]?.auxiliaryAssets || [];

commit 00e014ea03561e5f289b99d447a48a31cf1a933c

fix(preview) - fix regression caused by #9622

scopes/preview/preview/strategies/component-strategy.ts
@@ -17,6 +17,7 @@ import type { ComponentPreviewMetaData, PreviewMain } from '../preview.main.runt
 import { generateComponentLink } from './generate-component-link';
 import { PreviewOutputFileNotFound } from '../exceptions';
 
+export const PREVIEW_GLOBAL_NAME_SUFFIX = 'preview';
 export const PREVIEW_CHUNK_SUFFIX = 'preview-chunk';
 export const COMPONENT_CHUNK_SUFFIX = 'component-chunk';
 export const PREVIEW_CHUNK_FILENAME_SUFFIX = 'preview.js';
@@ -136,6 +137,11 @@ export class ComponentBundlingStrategy implements BundlingStrategy {
       component: context.splitComponentBundle ? this.getComponentChunkId(component.id, 'component') : undefined,
     };
 
+    const libNames = {
+      componentPreview: this.getComponentChunkGlobalName(component.id, 'preview'),
+      component: context.splitComponentBundle ? this.getComponentChunkGlobalName(component.id, 'component') : undefined,
+    };
+
     const entries = {
       [chunks.componentPreview]: {
         filename: this.getComponentChunkFileName(
@@ -147,11 +153,11 @@ export class ComponentBundlingStrategy implements BundlingStrategy {
         ),
         import: componentPreviewPath,
         dependOn: chunks.component,
-        library: { name: chunks.componentPreview, type: 'umd' },
+        library: { name: libNames.componentPreview, type: 'umd' },
       },
     };
 
-    if (chunks.component) {
+    if (chunks.component && libNames.component) {
       entries[chunks.component] = {
         filename: this.getComponentChunkFileName(
           component.id.toString({
@@ -162,7 +168,7 @@ export class ComponentBundlingStrategy implements BundlingStrategy {
         ),
         dependOn: undefined,
         import: componentPath,
-        library: { name: chunks.component, type: 'umd' },
+        library: { name: libNames.component, type: 'umd' },
       };
     }
 
@@ -174,6 +180,14 @@ export class ComponentBundlingStrategy implements BundlingStrategy {
       type === 'component'
         ? `${componentId.toStringWithoutVersion()}-${COMPONENT_CHUNK_SUFFIX}`
         : `${componentId.toStringWithoutVersion()}-${PREVIEW_CHUNK_SUFFIX}`;
+    return id;
+  }
+
+  private getComponentChunkGlobalName(componentId: ComponentID, type: 'component' | 'preview') {
+    const id =
+      type === 'component'
+        ? componentId.toStringWithoutVersion()
+        : `${componentId.toStringWithoutVersion()}-${PREVIEW_GLOBAL_NAME_SUFFIX}`;
     return id;
   }
 