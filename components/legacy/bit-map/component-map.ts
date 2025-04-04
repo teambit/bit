teambit/bit

commit fb8bde0d9e1f90c82730df084cddcf9dae515193

add trackMany API (#9661)
To add multiple components with optimized performance. 
Also, removed the old `trackDir` and `test` properties from
`ComponentMap`.

components/legacy/bit-map/component-map.ts
@@ -32,7 +32,6 @@ export type Config = { [aspectId: string]: Record<string, any> | '-' };
 export type ComponentMapFile = {
   name: string;
   relativePath: PathLinux;
-  test: boolean;
 };
 
 export type NextVersion = {
@@ -49,7 +48,6 @@ export type ComponentMapData = {
   defaultScope?: string;
   mainFile: PathLinux;
   rootDir: PathLinux;
-  trackDir?: PathLinux;
   wrapDir?: PathLinux;
   exported?: boolean;
   onLanesOnly?: boolean;
@@ -67,11 +65,6 @@ export class ComponentMap {
   defaultScope?: string;
   mainFile: PathLinux;
   rootDir: PathLinux;
-  // reason why trackDir and not re-use rootDir is because using rootDir requires all paths to be
-  // relative to rootDir for consistency, then, when saving into the model changing them back to
-  // be relative to consumer-root. (we can't save in the model relative to rootDir, otherwise the
-  // dependencies paths won't work).
-  trackDir: PathLinux | undefined; // relevant for AUTHORED only when a component was added as a directory, used for tracking changes in that dir
   wrapDir: PathLinux | undefined; // a wrapper directory needed when a user adds a package.json file to the component root so then it won't collide with Bit generated one
   // wether the compiler / tester are detached from the workspace global configuration
   // @ts-ignore AUTO-ADDED-AFTER-MIGRATION-PLEASE-FIX!
@@ -100,7 +93,6 @@ export class ComponentMap {
     defaultScope,
     mainFile,
     rootDir,
-    trackDir,
     wrapDir,
     onLanesOnly,
     localOnly,
@@ -113,7 +105,6 @@ export class ComponentMap {
     this.defaultScope = defaultScope;
     this.mainFile = mainFile;
     this.rootDir = rootDir;
-    this.trackDir = trackDir;
     this.wrapDir = wrapDir;
     this.onLanesOnly = onLanesOnly;
     this.localOnly = localOnly;
@@ -135,7 +126,6 @@ export class ComponentMap {
       defaultScope: this.defaultScope,
       mainFile: this.mainFile,
       rootDir: this.rootDir,
-      trackDir: this.trackDir,
       wrapDir: this.wrapDir,
       exported: this.exported,
       onLanesOnly: this.onLanesOnly || null, // if false, change to null so it won't be written
@@ -202,7 +192,6 @@ export class ComponentMap {
       file.relativePath = newPath;
     });
     this.rootDir = newRootDir;
-    this.trackDir = undefined; // if there is trackDir, it's not needed anymore.
   }
 
   addRootDirToDistributedFiles(rootDir: PathOsBased) {
@@ -254,21 +243,6 @@ export class ComponentMap {
 
   getAllFilesPaths(): PathLinux[] {
     return this.files.map((file) => file.relativePath);
-  }
-
-  getFilesGroupedByBeingTests(): { allFiles: string[]; nonTestsFiles: string[]; testsFiles: string[] } {
-    const allFiles = [];
-    const nonTestsFiles = [];
-    const testsFiles = [];
-    this.files.forEach((file: ComponentMapFile) => {
-      // @ts-ignore AUTO-ADDED-AFTER-MIGRATION-PLEASE-FIX!
-      allFiles.push(file.relativePath);
-      // @ts-ignore AUTO-ADDED-AFTER-MIGRATION-PLEASE-FIX!
-      if (file.test) testsFiles.push(file.relativePath);
-      // @ts-ignore AUTO-ADDED-AFTER-MIGRATION-PLEASE-FIX!
-      else nonTestsFiles.push(file.relativePath);
-    });
-    return { allFiles, nonTestsFiles, testsFiles };
   }
 
   /**

components/legacy/consumer-component/consumer-component.ts
@@ -593,9 +593,7 @@ async function getLoadedFiles(
   await componentMap.trackDirectoryChangesHarmony(consumer.getPath());
   const sourceFiles = componentMap.files.map((file) => {
     const filePath = path.join(bitDir, file.relativePath);
-    const sourceFile = SourceFile.load(filePath, bitDir, consumer.getPath(), {
-      test: file.test,
-    });
+    const sourceFile = SourceFile.load(filePath, bitDir, consumer.getPath());
     return sourceFile;
   });
   const filePaths = componentMap.getAllFilesPaths();

scopes/component/new-component-helper/new-component-helper.main.runtime.ts
@@ -137,6 +137,10 @@ export class NewComponentHelperMain {
     await this.workspace.triggerOnComponentAdd(targetId, { compile: options?.compile ?? true }, options?.loadOptions);
   }
 
+  /**
+   * @deprecated
+   * not needed anymore.
+   */
   async writeAndAddNewCompFromFiles(
     files: File[],
     targetId: ComponentID,
@@ -189,7 +193,7 @@ export class NewComponentHelperMain {
     return results;
   }
 
-  private async throwForExistingPath(targetPath: string) {
+  async throwForExistingPath(targetPath: string) {
     try {
       const stat = await fs.stat(targetPath);
       if (!stat.isDirectory()) {

scopes/component/sources/source-file.ts
@@ -11,7 +11,7 @@ export default class SourceFile extends AbstractVinyl {
     filePath: PathOsBased,
     base: PathOsBased,
     consumerPath: PathOsBased,
-    extendedProps: Record<string, any>
+    extendedProps: Record<string, any> = {}
   ): SourceFile {
     try {
       // @ts-ignore AUTO-ADDED-AFTER-MIGRATION-PLEASE-FIX!

scopes/component/tracker/add-cmd.ts
@@ -22,7 +22,7 @@ type AddResults = {
 
 export class AddCmd implements Command {
   name = 'add [path...]';
-  description = 'Add any subset of files to be tracked as a component(s).';
+  description = 'track one or more directories as new components';
   group = 'development';
   extendedDescription = 'Learn the recommended workflow for tracking directories as components, in the link below.';
   helpUrl = 'reference/workspace/component-directory';

scopes/component/tracker/add-components.ts
@@ -30,6 +30,7 @@ import { glob, isDir, pathNormalizeToLinux, PathLinux, PathLinuxRelative, PathOs
 import { linkToNodeModulesByIds } from '@teambit/workspace.modules.node-modules-linker';
 import { Workspace } from '@teambit/workspace';
 import determineMainFile from './determine-main-file';
+import { ResolvedTrackData } from './tracker.main.runtime';
 
 export type AddResult = { id: ComponentID; files: ComponentMapFile[] };
 export type Warnings = {
@@ -112,6 +113,60 @@ export default class AddComponents {
     this.defaultScope = addProps.defaultScope;
     this.config = addProps.config;
     this.shouldHandleOutOfSync = addProps.shouldHandleOutOfSync;
+  }
+
+  async add(): Promise<AddActionResults> {
+    this.ignoreList = await this.getIgnoreList();
+    this.gitIgnore = ignore().add(this.ignoreList); // add ignore list
+
+    let componentPathsStats: PathsStats = {};
+
+    const resolvedComponentPathsWithoutGitIgnore = (
+      await Promise.all(this.componentPaths.map((componentPath) => glob(componentPath)))
+    ).flat();
+
+    const resolvedComponentPathsWithGitIgnore = this.gitIgnore.filter(resolvedComponentPathsWithoutGitIgnore);
+    // Run diff on both arrays to see what was filtered out because of the gitignore file
+    // this is not the same as "lodash.difference".
+    const diff = arrayDiff(resolvedComponentPathsWithGitIgnore, resolvedComponentPathsWithoutGitIgnore);
+
+    if (!resolvedComponentPathsWithoutGitIgnore.length) {
+      throw new PathsNotExist(this.componentPaths);
+    }
+    if (resolvedComponentPathsWithGitIgnore.length) {
+      componentPathsStats = validatePaths(resolvedComponentPathsWithGitIgnore);
+    } else {
+      throw new NoFiles(diff);
+    }
+    Object.keys(componentPathsStats).forEach((compPath) => {
+      if (!componentPathsStats[compPath].isDir) {
+        throw new AddingIndividualFiles(compPath);
+      }
+    });
+    if (Object.keys(componentPathsStats).length > 1 && this.id) {
+      throw new BitError(
+        `the --id flag (${this.id}) is used for a single component only, however, got ${this.componentPaths.length} paths`
+      );
+    }
+    // if a user entered multiple paths and entered an id, he wants all these paths to be one component
+    // conversely, if a user entered multiple paths without id, he wants each dir as an individual component
+    const isMultipleComponents = Object.keys(componentPathsStats).length > 1;
+    if (isMultipleComponents) {
+      await this.addMultipleComponents(componentPathsStats);
+    } else {
+      logger.debugAndAddBreadCrumb('add-components', 'adding one component');
+      // when a user enters more than one directory, he would like to keep the directories names
+      // so then when a component is imported, it will write the files into the original directories
+      const addedOne = await this.addOneComponent(Object.keys(componentPathsStats)[0]);
+      await this._removeNamespaceIfNotNeeded([addedOne]);
+      if (addedOne.files.length) {
+        const addedResult = await this.addOrUpdateComponentInBitMap(addedOne);
+        if (addedResult) this.addedComponents.push(addedResult);
+      }
+    }
+    await this.linkComponents(this.addedComponents.map((item) => item.id));
+    Analytics.setExtraData('num_components', this.addedComponents.length);
+    return { addedComponents: this.addedComponents, warnings: this.warnings };
   }
 
   /**
@@ -393,7 +448,6 @@ you can add the directory these files are located at and it'll change the root d
           }
           files.push({
             relativePath: pathNormalizeToLinux(generatedFile),
-            test: false,
             name: path.basename(generatedFile),
           });
           mainFile = generatedFile;
@@ -415,7 +469,6 @@ you can add the directory these files are located at and it'll change the root d
       }
       files.push({
         relativePath: pathNormalizeToLinux(mainFileRelativeToConsumer),
-        test: false,
         name: path.basename(mainFileRelativeToConsumer),
       });
       return mainFileRelativeToConsumer;
@@ -447,7 +500,7 @@ you can add the directory these files are located at and it'll change the root d
     }
     const relativeComponentPath = this.consumer.getPathRelativeToConsumer(componentPath);
     this._throwForOutsideConsumer(relativeComponentPath);
-    this.throwForExistingParentDir(relativeComponentPath);
+    throwForExistingParentDir(this.bitMap, relativeComponentPath);
     const matches = await glob(path.join(relativeComponentPath, '**'), {
       cwd: this.consumer.getPath(),
       nodir: true,
@@ -503,60 +556,6 @@ you can add the directory these files are located at and it'll change the root d
   async getIgnoreList(): Promise<string[]> {
     const consumerPath = this.consumer.getPath();
     return getIgnoreListHarmony(consumerPath);
-  }
-
-  async add(): Promise<AddActionResults> {
-    this.ignoreList = await this.getIgnoreList();
-    this.gitIgnore = ignore().add(this.ignoreList); // add ignore list
-
-    let componentPathsStats: PathsStats = {};
-
-    const resolvedComponentPathsWithoutGitIgnore = (
-      await Promise.all(this.componentPaths.map((componentPath) => glob(componentPath)))
-    ).flat();
-    this.gitIgnore = ignore().add(this.ignoreList); // add ignore list
-
-    const resolvedComponentPathsWithGitIgnore = this.gitIgnore.filter(resolvedComponentPathsWithoutGitIgnore);
-    // Run diff on both arrays to see what was filtered out because of the gitignore file
-    const diff = arrayDiff(resolvedComponentPathsWithGitIgnore, resolvedComponentPathsWithoutGitIgnore);
-
-    if (!resolvedComponentPathsWithoutGitIgnore.length) {
-      throw new PathsNotExist(this.componentPaths);
-    }
-    if (resolvedComponentPathsWithGitIgnore.length) {
-      componentPathsStats = validatePaths(resolvedComponentPathsWithGitIgnore);
-    } else {
-      throw new NoFiles(diff);
-    }
-    Object.keys(componentPathsStats).forEach((compPath) => {
-      if (!componentPathsStats[compPath].isDir) {
-        throw new AddingIndividualFiles(compPath);
-      }
-    });
-    if (Object.keys(componentPathsStats).length > 1 && this.id) {
-      throw new BitError(
-        `the --id flag (${this.id}) is used for a single component only, however, got ${this.componentPaths.length} paths`
-      );
-    }
-    // if a user entered multiple paths and entered an id, he wants all these paths to be one component
-    // conversely, if a user entered multiple paths without id, he wants each dir as an individual component
-    const isMultipleComponents = Object.keys(componentPathsStats).length > 1;
-    if (isMultipleComponents) {
-      await this.addMultipleComponents(componentPathsStats);
-    } else {
-      logger.debugAndAddBreadCrumb('add-components', 'adding one component');
-      // when a user enters more than one directory, he would like to keep the directories names
-      // so then when a component is imported, it will write the files into the original directories
-      const addedOne = await this.addOneComponent(Object.keys(componentPathsStats)[0]);
-      await this._removeNamespaceIfNotNeeded([addedOne]);
-      if (addedOne.files.length) {
-        const addedResult = await this.addOrUpdateComponentInBitMap(addedOne);
-        if (addedResult) this.addedComponents.push(addedResult);
-      }
-    }
-    await this.linkComponents(this.addedComponents.map((item) => item.id));
-    Analytics.setExtraData('num_components', this.addedComponents.length);
-    return { addedComponents: this.addedComponents, warnings: this.warnings };
   }
 
   async linkComponents(ids: ComponentID[]) {
@@ -654,23 +653,24 @@ you can add the directory these files are located at and it'll change the root d
       throw new PathOutsideConsumer(relativeToConsumerPath);
     }
   }
+}
 
-  private throwForExistingParentDir(relativeToConsumerPath: PathOsBased) {
-    const isParentDir = (parent: string) => {
-      const relative = path.relative(parent, relativeToConsumerPath);
-      return relative && !relative.startsWith('..') && !path.isAbsolute(relative);
-    };
-    this.bitMap.components.forEach((componentMap) => {
-      if (!componentMap.rootDir) return;
-      if (isParentDir(componentMap.rootDir)) {
-        throw new ParentDirTracked(
-          componentMap.rootDir,
-          componentMap.id.toStringWithoutVersion(),
-          relativeToConsumerPath
-        );
-      }
-    });
-  }
+
+function throwForExistingParentDir(bitMap: BitMap, relativeToConsumerPath: PathOsBased) {
+  const isParentDir = (parent: string) => {
+    const relative = path.relative(parent, relativeToConsumerPath);
+    return relative && !relative.startsWith('..') && !path.isAbsolute(relative);
+  };
+  bitMap.components.forEach((componentMap) => {
+    if (!componentMap.rootDir) return;
+    if (isParentDir(componentMap.rootDir)) {
+      throw new ParentDirTracked(
+        componentMap.rootDir,
+        componentMap.id.toStringWithoutVersion(),
+        relativeToConsumerPath
+      );
+    }
+  });
 }
 
 /**
@@ -725,4 +725,41 @@ function calculateFileInfo(relativePath: string): { PARENT: string; FILE_NAME: s
 async function isAutoGeneratedFile(filePath: PathOsBased): Promise<boolean> {
   const line = await firstline(filePath);
   return line.includes(AUTO_GENERATED_STAMP);
+}
+
+export async function addMultipleFromResolvedTrackData(workspace: Workspace,
+  trackData: ResolvedTrackData[]
+): Promise<ComponentID[]> {
+  const bitMap = workspace.consumer.bitMap;
+  const ignoreList = await getIgnoreListHarmony(workspace.path);
+  const gitIgnore = ignore().add(ignoreList);
+  const componentMaps = trackData.map(data => {
+    const { rootDir, files, componentName, defaultScope, mainFile, config } = data;
+    if (path.isAbsolute(rootDir)) throw new BitError(`path is absolute, got ${rootDir}`);
+    throwForExistingParentDir(bitMap, rootDir);
+
+    const filtered = gitIgnore.filter(files);
+    if (!filtered.length) {
+      throw new NoFiles(files);
+    }
+
+    const componentFiles = filtered.map((match: PathOsBased) => {
+      return { relativePath: pathNormalizeToLinux(match), name: path.basename(match) };
+    });
+
+    const componentMap = bitMap.addComponent({
+      componentId: ComponentID.fromObject({ name: componentName }, defaultScope),
+      files: componentFiles,
+      defaultScope,
+      config,
+      mainFile,
+      rootDir
+    });
+    return componentMap;
+  });
+
+  const allIds = componentMaps.map(c => c.id);
+  await linkToNodeModulesByIds(workspace, allIds);
+
+  return allIds;
 }

scopes/component/tracker/determine-main-file.ts
@@ -16,7 +16,7 @@ export default function determineMainFile(
 ): PathLinux {
   const mainFile = addedComponent.mainFile;
   const componentIdStr = addedComponent.componentId.toString();
-  const files = addedComponent.files.filter((file) => !file.test);
+  const files = addedComponent.files;
   const rootDir = existingComponentMap && existingComponentMap.rootDir;
   const strategies: Function[] = [
     getExistingIfNotChanged,

scopes/component/tracker/index.ts
@@ -2,6 +2,6 @@ import { TrackerAspect } from './tracker.aspect';
 export { AddingIndividualFiles } from './exceptions/adding-individual-files';
 export { ParentDirTracked } from './exceptions/parent-dir-tracked';
 export { MainFileIsDir, PathOutsideConsumer, VersionShouldBeRemoved } from './exceptions';
-export type { TrackerMain } from './tracker.main.runtime';
+export type { TrackerMain, ResolvedTrackData, TrackData } from './tracker.main.runtime';
 export default TrackerAspect;
 export { TrackerAspect };

commit 339be02345705d463e11c351b58921b42acd9ca2

fix a regression that cause all components to be shown as modified (#9663)
Caused by https://github.com/teambit/bit/pull/9661 which removed the
"test" prop. This PR adds it back for now.

components/legacy/bit-map/component-map.ts
@@ -32,6 +32,10 @@ export type Config = { [aspectId: string]: Record<string, any> | '-' };
 export type ComponentMapFile = {
   name: string;
   relativePath: PathLinux;
+  /**
+   * @deprecated should be safe to remove around August 2025
+   */
+  test?: boolean;
 };
 
 export type NextVersion = {

components/legacy/consumer-component/consumer-component.ts
@@ -593,7 +593,7 @@ async function getLoadedFiles(
   await componentMap.trackDirectoryChangesHarmony(consumer.getPath());
   const sourceFiles = componentMap.files.map((file) => {
     const filePath = path.join(bitDir, file.relativePath);
-    const sourceFile = SourceFile.load(filePath, bitDir, consumer.getPath());
+    const sourceFile = SourceFile.load(filePath, bitDir, consumer.getPath(), { test: file.test || false });
     return sourceFile;
   });
   const filePaths = componentMap.getAllFilesPaths();