d38be448e0b784ed0c54c09cb034d3ae68db598e
Jinjiang <zhaojinjiang@me.com>
2025-06-17 09:04:57 +0800

    feat: improve api ref table for vue renderer

--- scopes/react/react/vue.renderer.tsx
+++ scopes/react/react/vue.renderer.tsx
@@ -3,14 +3,31 @@
 import React from 'react';
 import type { APINodeRenderProps, APINodeRenderer } from '@teambit/api-reference.models.api-node-renderer';
 import type { APINode } from '@teambit/api-reference.models.api-reference-model';
-import { PropTable } from '@teambit/documenter.ui.property-table';
 import type { RowType } from '@teambit/documenter.ui.table-row';
+import { Table } from '@teambit/documenter.ui.table';
 import type { ComponentMeta } from 'vue-component-meta';
 
 import styles from './vue.renderer.module.scss';
 
+function predicate(node: any): boolean {
+  return node.__schema === 'VueSchema';
+}
+
 function useMeta(apiNode: APINode): ComponentMeta {
   return (apiNode.api as any).meta as ComponentMeta;
+}
+
+function VueSFCTable({
+  rows,
+  hasDescription,
+  expanded,
+}: {
+  rows: RowType[];
+  hasDescription?: boolean;
+  expanded?: boolean;
+}) {
+  const headings = hasDescription ? ['name', 'type', 'default', 'description'] : ['name', 'type'];
+  return <Table headings={headings} rows={rows} isListView={expanded} />;
 }
 
 function VueSFCProps({ props, expanded = false }: { props: ComponentMeta['props']; expanded?: boolean }) {
@@ -39,7 +56,7 @@ function VueSFCProps({ props, expanded = false }: { props: ComponentMeta['props'
   return (
     <div>
       <h2 className={styles.title}>Props</h2>
-      <PropTable rows={rows} showListView={expanded} listViewResolution={320} />
+      <VueSFCTable rows={rows} hasDescription expanded={expanded} />
     </div>
   );
 }
@@ -57,7 +74,7 @@ function VueSFCEmits({ events, expanded = false }: { events: ComponentMeta['even
   return (
     <div>
       <h2 className={styles.title}>Emits</h2>
-      <PropTable rows={rows} showListView={expanded} listViewResolution={320} />
+      <VueSFCTable rows={rows} expanded={expanded} />
     </div>
   );
 }
@@ -75,7 +92,7 @@ function VueSFCSlots({ slots, expanded }: { slots: ComponentMeta['slots']; expan
   return (
     <div>
       <h2 className={styles.title}>Slots</h2>
-      <PropTable rows={rows} showListView={expanded} listViewResolution={320} />
+      <VueSFCTable rows={rows} expanded={expanded} />
     </div>
   );
 }
@@ -104,10 +121,6 @@ function VueOverviewComponent({ apiNode }: APINodeRenderProps) {
       </div>
     </div>
   );
-}
-
-function predicate(node: any): boolean {
-  return node.__schema === 'VueSchema';
 }
 
 export const vueRenderer: APINodeRenderer = {
