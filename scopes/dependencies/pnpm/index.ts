c4e46e1a4c4def7895e457c042a145c3b57aad54
Zoltan Kochan <z@kochan.io>
2025-06-17 18:26:56 +0200

    fix: properly export function

--- scopes/dependencies/pnpm/index.ts
+++ scopes/dependencies/pnpm/index.ts
@@ -2,4 +2,4 @@ export type { PnpmMain } from './pnpm.main.runtime';
 export type { PnpmUI } from './pnpm.ui.runtime';
 export { PnpmAspect as default, PnpmAspect } from './pnpm.aspect';
 export type { PnpmPackageManager, InstallResult, RebuildFn } from './pnpm.package-manager';
-export type { BitLockfile, BitLockfileFile, createReadPackageHooks } from './lynx';
+export { type BitLockfile, type BitLockfileFile, createReadPackageHooks } from './lynx';
