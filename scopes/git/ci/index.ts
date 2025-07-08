5bb65d2a26cce808c22d966e5d3261914c51a372
David First <davidnet@gmail.com>
2025-07-08 09:27:42 -0400

    feat(ci): add teambit.git/ci as a core aspect

---
+++ scopes/git/ci/index.ts
@@ -0,0 +1,4 @@
+import { CiAspect } from './ci.aspect.js';
+
+export default CiAspect;
+export { CiAspect };
--- scopes/lanes/lanes/index.ts
+++ scopes/lanes/lanes/index.ts
@@ -1,6 +1,6 @@
 import { LanesAspect } from './lanes.aspect';
 
-export type { CreateLaneResult, LanesMain, Lane } from './lanes.main.runtime';
+export type { CreateLaneResult, LanesMain, Lane, SwitchLaneOptions } from './lanes.main.runtime';
 export type { LanesQuery } from '@teambit/lanes.ui.models.lanes-model';
 export { LanesModel } from '@teambit/lanes.ui.models.lanes-model';
 export type { LanesUI, LaneCompareProps, LaneProviderIgnoreSlot } from './lanes.ui.runtime';
