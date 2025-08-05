cbd675166ef18b513c3a7552500d411ed985dcfa
David First <davidnet@gmail.com>
2025-08-05 14:43:52 -0400

    feat: convert Jest tests to Mocha

--- scopes/dependencies/dependency-resolver/manifest/update-dependency-version.spec.ts
+++ scopes/dependencies/dependency-resolver/manifest/update-dependency-version.spec.ts
@@ -1,29 +1,41 @@
+import { expect } from 'chai';
+import sinon from 'sinon';
+import sinonChai from 'sinon-chai';
+import chai from 'chai';
 import { updateDependencyVersion } from './update-dependency-version';
 import type { DependencyLifecycleType } from '../dependencies';
 
+// Configure chai to use sinon-chai
+chai.use(sinonChai);
+
 describe('updateDependencyVersion()', function () {
   it('should pick version from root policy, when no variation policy is present', function () {
+    const setVersionStub = sinon.stub();
+    const getValidSemverDepVersionStub = sinon.stub();
+
     const dependency = {
       getPackageName: () => 'foo',
       lifecycle: 'dev',
       version: '1.0.0',
-      // @ts-ignore
-      setVersion: jest.fn(),
+      setVersion: setVersionStub,
     } as any; // eslint-disable-line
+
     const rootPolicy = {
-      // @ts-ignore
-      getValidSemverDepVersion: jest.fn((pkgName: string, lifecycle: DependencyLifecycleType) =>
-        lifecycle === 'runtime' ? '2.0.0' : undefined
+      getValidSemverDepVersion: getValidSemverDepVersionStub.callsFake(
+        (pkgName: string, lifecycle: DependencyLifecycleType) => (lifecycle === 'runtime' ? '2.0.0' : undefined)
       ),
     } as any; // eslint-disable-line
+
     const variationPolicy = {
       getDepVersion: () => undefined,
     } as any; // eslint-disable-line
+
     updateDependencyVersion(dependency, rootPolicy, variationPolicy);
+
     // The lifecycle type is changed to runtime
     // root policies don't have a separate property for dev dependencies
     // both runtime and dev dependencies are specified through "dependencies"
-    expect(rootPolicy.getValidSemverDepVersion).toHaveBeenCalledWith('foo', 'runtime');
-    expect(dependency.setVersion).toHaveBeenCalledWith('2.0.0');
+    expect(getValidSemverDepVersionStub).to.have.been.calledWith('foo', 'runtime');
+    expect(setVersionStub).to.have.been.calledWith('2.0.0');
   });
 });
