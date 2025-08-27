c2f7d3681564a101c5f97667005a339a6dfb337d
David First <davidnet@gmail.com>
2025-08-27 10:54:25 -0400

    feat(api-server): support web-socket client at /websocket-server/subscriptions (#9961)

--- scopes/generator/generator/index.ts
+++ scopes/generator/generator/index.ts
@@ -1,4 +1,4 @@
-export type { GeneratorMain } from './generator.main.runtime';
+export type { GeneratorMain, TemplateDescriptor } from './generator.main.runtime';
 export type {
   ComponentContext,
   ComponentTemplate,
