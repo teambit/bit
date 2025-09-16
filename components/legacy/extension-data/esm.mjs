9a93c7b19207fa08b7c0d950f8d9707906e613b1
David First <davidnet@gmail.com>
2025-09-16 11:17:12 -0400

    feat: add ESM bridge files for bundling compatibility
    Add esm.mjs files to components that use node-babel-mocha env to support
    ESM imports when components are imported in bundled environment.

---
+++ components/legacy/extension-data/esm.mjs
@@ -0,0 +1,12 @@
+// eslint-disable-next-line import/no-unresolved
+import cjsModule from './index.js';
+
+export const ExtensionDataEntry = cjsModule.ExtensionDataEntry;
+export const ExtensionDataList = cjsModule.ExtensionDataList;
+export const REMOVE_EXTENSION_SPECIAL_SIGN = cjsModule.REMOVE_EXTENSION_SPECIAL_SIGN;
+export const ValidateBeforePersistResult = cjsModule.ValidateBeforePersistResult;
+export const configEntryToDataEntry = cjsModule.configEntryToDataEntry;
+export const getCompareExtPredicate = cjsModule.getCompareExtPredicate;
+export const removeInternalConfigFields = cjsModule.removeInternalConfigFields;
+
+export default cjsModule;
---
+++ components/modules/find-scope-path/esm.mjs
@@ -0,0 +1,6 @@
+// eslint-disable-next-line import/no-unresolved
+import cjsModule from './index.js';
+
+export const findScopePath = cjsModule.findScopePath;
+
+export default cjsModule;
