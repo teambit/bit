teambit/bit

commit 8361d1906e1c3a9507eb138a456073a9561d60a5

fix: use symlink-dir for creating symlinks to avoid any race conditions (#9709)
## Proposed Changes

-
-
-

scopes/toolbox/fs/link-or-symlink/create-link-or-symlink.ts
@@ -4,6 +4,7 @@ import fsNative from 'fs';
 import { BitError } from '@teambit/bit-error';
 import * as path from 'path';
 import { logger } from '@teambit/legacy.logger';
+import symlinkDir from 'symlink-dir';
 
 /**
  * create a link (hard-link). if not possible (e.g. it's a directory) or avoidHardLink is true, use symlink.
@@ -47,7 +48,7 @@ Original error: ${err}`);
   }
 
   function symlink() {
-    IS_WINDOWS ? symlinkOrHardLink() : fs.symlinkSync(srcPath, destPath);
+    IS_WINDOWS ? symlinkOrHardLink() : symlinkDir.sync(srcPath, destPath);
   }
 
   /**
@@ -55,7 +56,7 @@ Original error: ${err}`);
    */
   function symlinkOrHardLink() {
     try {
-      fs.symlinkSync(srcPath, destPath);
+      symlinkDir.sync(srcPath, destPath);
       logger.trace(`createLinkOrSymlink, symlinkOrHardLink() successfully created the symlink`);
     } catch {
       // it can be a file or directory, we don't know. just run link(), it will junction for dirs and hard-link for files.

commit 1034255eef4ce76895212632082357978680a2d8

fix: do not create broken junctions to files (#10030)
Related PR: https://github.com/pnpm/symlink-dir/pull/65

## Proposed Changes

- Fixing a regression introduce by #9709
-
-

scopes/toolbox/fs/link-or-symlink/create-link-or-symlink.ts
@@ -56,7 +56,9 @@ Original error: ${err}`);
    */
   function symlinkOrHardLink() {
     try {
-      symlinkDir.sync(srcPath, destPath);
+      symlinkDir.sync(srcPath, destPath, {
+        noJunction: true,
+      });
       logger.trace(`createLinkOrSymlink, symlinkOrHardLink() successfully created the symlink`);
     } catch {
       // it can be a file or directory, we don't know. just run link(), it will junction for dirs and hard-link for files.

workspace.jsonc
@@ -608,7 +608,7 @@
         "strip-ansi": "6.0.0",
         "stylus-lookup": "3.0.2",
         "subscriptions-transport-ws": "0.9.18",
-        "symlink-dir": "^5.1.1",
+        "symlink-dir": "^7.1.0",
         "table": "6.7.3",
         "tar": "7.4.3",
         "tar-fs": "2.1.3",