497147486b0c7ffd82c6bf91d7b5ccfdcde0f880
David First <davidnet@gmail.com>
2025-10-09 13:01:19 -0400

    feat: add --restore-deleted-components flag to bit lane revert

--- e2e/harmony/lanes/lane-history-with-deleted-components.e2e.ts
+++ e2e/harmony/lanes/lane-history-with-deleted-components.e2e.ts
@@ -61,33 +61,151 @@ describe('lane history with deleted components', function () {
   });
 
   describe('reverting to a point in history before components were deleted using bit delete --lane', () => {
-    let historyBeforeDelete: string;
-    before(() => {
-      helper.scopeHelper.setWorkspaceWithRemoteScope();
-      helper.fixtures.populateComponents(3);
-      helper.command.createLane('dev');
-      helper.command.snapAllComponentsWithoutBuild('-m "add three components"');
-      helper.command.exportLane();
+    describe('without --restore-deleted-components flag', () => {
+      let historyBeforeDelete: string;
+      before(() => {
+        helper.scopeHelper.setWorkspaceWithRemoteScope();
+        helper.fixtures.populateComponents(3);
+        helper.command.createLane('dev');
+        helper.command.snapAllComponentsWithoutBuild('-m "add three components"');
+        helper.command.exportLane();
 
-      // Get the history ID before deletion
-      const historyOutput = helper.command.runCmd('bit lane history --json');
-      const history = JSON.parse(historyOutput);
-      if (!history || !history.length) {
-        throw new Error('Could not find history in output');
-      }
-      // Get the most recent history item
-      historyBeforeDelete = history[history.length - 1].id;
+        // Get the history ID before deletion
+        const historyOutput = helper.command.runCmd('bit lane history --json');
+        const history = JSON.parse(historyOutput);
+        if (!history || !history.length) {
+          throw new Error('Could not find history in output');
+        }
+        // Get the most recent history item
+        historyBeforeDelete = history[history.length - 1].id;
 
-      // Delete components using bit delete --lane
-      helper.command.softRemoveOnLane('comp1');
-      helper.command.softRemoveOnLane('comp2');
-      helper.command.snapAllComponentsWithoutBuild('--unmodified -m "delete comp1 and comp2"');
-      helper.command.exportLane();
+        // Delete components using bit delete --lane
+        helper.command.softRemoveOnLane('comp1');
+        helper.command.softRemoveOnLane('comp2');
+        helper.command.snapAllComponentsWithoutBuild('--unmodified -m "delete comp1 and comp2"');
+        helper.command.exportLane();
+
+        // Verify components are actually deleted
+        const listAfterDelete = helper.command.listParsed();
+        if (listAfterDelete.length !== 1) {
+          throw new Error(`Expected 1 component after deletion, got ${listAfterDelete.length}`);
+        }
+      });
+
+      it('should not throw an error when reverting to a point before deletion', () => {
+        // Revert doesn't restore deleted components by default, but it should not throw an error
+        expect(() => helper.command.runCmd(`bit lane revert ${historyBeforeDelete}`)).to.not.throw();
+      });
     });
 
-    it('should not throw an error when reverting to a point before deletion', () => {
-      // Revert doesn't restore deleted components, but it should not throw an error
-      expect(() => helper.command.runCmd(`bit lane revert ${historyBeforeDelete}`)).to.not.throw();
+    describe('with --restore-deleted-components flag', () => {
+      let historyBeforeDelete: string;
+      before(() => {
+        helper.scopeHelper.setWorkspaceWithRemoteScope();
+        helper.fixtures.populateComponents(3);
+        helper.command.createLane('dev');
+        helper.command.snapAllComponentsWithoutBuild('-m "add three components"');
+        helper.command.exportLane();
+
+        // Get the history ID before deletion
+        const historyOutput = helper.command.runCmd('bit lane history --json');
+        const history = JSON.parse(historyOutput);
+        if (!history || !history.length) {
+          throw new Error('Could not find history in output');
+        }
+        // Get the most recent history item
+        historyBeforeDelete = history[history.length - 1].id;
+
+        // Delete components using bit delete --lane
+        helper.command.softRemoveOnLane('comp1');
+        helper.command.softRemoveOnLane('comp2');
+        helper.command.snapAllComponentsWithoutBuild('--unmodified -m "delete comp1 and comp2"');
+        helper.command.exportLane();
+
+        // Verify components are actually deleted
+        const listAfterDelete = helper.command.listParsed();
+        if (listAfterDelete.length !== 1) {
+          throw new Error(`Expected 1 component after deletion, got ${listAfterDelete.length}`);
+        }
+      });
+
+      it('should restore deleted components when using the flag', () => {
+        helper.command.runCmd(`bit lane revert ${historyBeforeDelete} --restore-deleted-components`);
+        const list = helper.command.listParsed();
+        expect(list).to.have.lengthOf(3);
+        const comp1 = list.find((c) => c.id.includes('comp1'));
+        const comp2 = list.find((c) => c.id.includes('comp2'));
+        const comp3 = list.find((c) => c.id.includes('comp3'));
+        expect(comp1).to.not.be.undefined;
+        expect(comp2).to.not.be.undefined;
+        expect(comp3).to.not.be.undefined;
+      });
+    });
+
+    describe('with mixed scenario: modified component and deleted component', () => {
+      let historyBeforeChanges: string;
+      let comp3VersionBeforeChanges: string;
+      before(() => {
+        helper.scopeHelper.setWorkspaceWithRemoteScope();
+        helper.fixtures.populateComponents(3);
+        helper.command.createLane('dev');
+        helper.command.snapAllComponentsWithoutBuild('-m "add three components"');
+        helper.command.exportLane();
+
+        // Get the history ID and comp3 version before any changes
+        const historyOutput = helper.command.runCmd('bit lane history --json');
+        const history = JSON.parse(historyOutput);
+        historyBeforeChanges = history[history.length - 1].id;
+        const listBefore = helper.command.listParsed();
+        const comp3Before = listBefore.find((c) => c.id.includes('comp3'));
+        comp3VersionBeforeChanges = comp3Before!.id.split('@')[1];
+
+        // Modify comp3
+        helper.fs.outputFile('comp3/index.js', 'console.log("modified");');
+        helper.command.snapAllComponentsWithoutBuild('-m "modify comp3"');
+
+        // Delete comp1 and comp2
+        helper.command.softRemoveOnLane('comp1');
+        helper.command.softRemoveOnLane('comp2');
+        helper.command.snapAllComponentsWithoutBuild('--unmodified -m "delete comp1 and comp2"');
+        helper.command.exportLane();
+      });
+
+      it('should keep modified component version in bitmap but restore its files', () => {
+        const listBeforeRevert = helper.command.listParsed();
+        const comp3BeforeRevert = listBeforeRevert.find((c) => c.id.includes('comp3'));
+        if (!comp3BeforeRevert) {
+          throw new Error('comp3 not found before revert');
+        }
+        const comp3VersionAfterModification = comp3BeforeRevert.currentVersion || comp3BeforeRevert.version;
+        if (!comp3VersionAfterModification) {
+          throw new Error(
+            `comp3 version not found. Available fields: ${JSON.stringify(Object.keys(comp3BeforeRevert))}`
+          );
+        }
+
+        helper.command.runCmd(`bit lane revert ${historyBeforeChanges} --restore-deleted-components`);
+
+        // All three components should be in the list
+        const list = helper.command.listParsed();
+        expect(list).to.have.lengthOf(3);
+
+        // comp3 should keep the modified version in bitmap (revert behavior)
+        const comp3 = list.find((c) => c.id.includes('comp3'));
+        const comp3VersionAfterRevert = comp3!.currentVersion || comp3!.version;
+        expect(comp3VersionAfterRevert).to.equal(comp3VersionAfterModification);
+        expect(comp3VersionAfterRevert).to.not.equal(comp3VersionBeforeChanges);
+
+        // comp1 and comp2 should be restored with their historical versions
+        const comp1 = list.find((c) => c.id.includes('comp1'));
+        const comp2 = list.find((c) => c.id.includes('comp2'));
+        expect(comp1).to.not.be.undefined;
+        expect(comp2).to.not.be.undefined;
+
+        // comp3 files should be reverted to the old content
+        const comp3Content = helper.fs.readFile('comp3/index.js');
+        expect(comp3Content).to.not.include('modified');
+      });
     });
   });
 });
