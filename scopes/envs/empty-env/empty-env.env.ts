0e8f47e9608c9517a82a27ee9178f398be4f9db6
David First <davidnet@gmail.com>
2025-11-20 16:00:23 -0500

    add __getDescriptor to the empty-env

--- scopes/envs/empty-env/empty-env.env.ts
+++ scopes/envs/empty-env/empty-env.env.ts
@@ -3,6 +3,15 @@
  * This env has no compiler, tester, linter, or any other tools configured.
  * It's used as the default env when no other env is specified.
  */
-export class EmptyEnv {}
+export class EmptyEnv {
+  /**
+   * mandatory! otherwise, it is not recognized as an env. (see getEnvDescriptorFromEnvDef)
+   */
+  async __getDescriptor() {
+    return {
+      type: 'empty',
+    };
+  }
+}
 
 export default new EmptyEnv();
