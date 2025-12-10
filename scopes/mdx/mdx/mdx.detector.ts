0f393dad7259dc6b655cd1e71183e758cf1818c5
Jinjiang <zhaojinjiang@me.com>
2025-12-10 19:17:05 +0800

    chore: fix mdx compiler type error

--- scopes/mdx/mdx/mdx.detector.ts
+++ scopes/mdx/mdx/mdx.detector.ts
@@ -1,6 +1,23 @@
 import type { DependencyDetector, FileContext } from '@teambit/dependency-resolver';
-import { compileSync, type ImportSpecifier } from '@mdx-js/mdx';
+import { compileSync } from '@mdx-js/mdx';
 import { mdxOptions } from '@teambit/mdx.modules.mdx-v3-options';
+
+type ImportSpecifier = {
+  /**
+   * relative/absolute or module name. e.g. the `y` in the example of `import x from 'y';`
+   */
+  fromModule: string;
+
+  /**
+   * is default import (e.g. `import x from 'y';`)
+   */
+  isDefault?: boolean;
+
+  /**
+   * the name used to identify the module, e.g. the `x` in the example of `import x from 'y';`
+   */
+  identifier?: string;
+};
 
 export class MDXDependencyDetector implements DependencyDetector {
   constructor(private supportedExtensions: string[]) {}
