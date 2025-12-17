82461b84f42ad67eebdf51dc22fa5abc08ab718f
Jinjiang <zhaojinjiang@me.com>
2025-12-17 09:39:01 +0800

    Update mdx.detector.spec.ts

--- scopes/mdx/mdx/mdx.detector.spec.ts
+++ scopes/mdx/mdx/mdx.detector.spec.ts
@@ -1,9 +1,32 @@
 import { expect } from 'chai';
 
-describe('MDXDependencyDetector', () => {
-  describe('detect', () => {
-    it('should work', () => {
-      expect(true).to.equal(true);
-    });
-  });
+it('should work', () => {
+  expect(true).to.equal(true);
 });
+
+// // Skip these mdx detector test cases since esm imports are not well-supported in jest tester
+// import { MDXDependencyDetector } from './mdx.detector';
+//
+// describe('MDXDependencyDetector', () => {
+//   function expectDependencies(src: string, expectedDeps: string[]) {
+//     expect(new MDXDependencyDetector(['mdx']).detect(src)).to.deep.equal(expectedDeps);
+//   }
+//   describe('detect', () => {
+//     it('should correctly detect default import', () => {
+//       const src = 'import x from "y";';
+//       expectDependencies(src, ['y']);
+//     });
+//     it('should correctly detect star import', () => {
+//       const src = 'import * as y from "y";';
+//       expectDependencies(src, ['y']);
+//     });
+//     it('should correctly detect named import', () => {
+//       const src = 'import { x } from "y";';
+//       expectDependencies(src, ['y']);
+//     });
+//     it('should correctly detect import with no identifier', () => {
+//       const src = 'import "y";';
+//       expectDependencies(src, ['y']);
+//     });
+//   });
+// });
