051183567844d43c875bda109a2d7e489bf1fa63
David First <davidnet@gmail.com>
2026-01-15 11:30:14 -0500

    fix ReferenceError: __dirname is not defined

--- e2e/performance/filesystem-read.e2e.ts
+++ e2e/performance/filesystem-read.e2e.ts
@@ -5,6 +5,11 @@ import path from 'path';
 import { expect } from 'chai';
 import { Helper } from '@teambit/legacy.e2e-helper';
 import * as _ from 'lodash';
+import { fileURLToPath } from 'url';
+
+// @ts-ignore
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const MAX_FILES_READ = 1050;
 const MAX_FILES_READ_STATUS = 1500;
