581eabd45d83780ed64e8cba3580eabe9d70d737
Luv Kapur <luv@bit.dev>
2026-05-13 16:35:47 -0400

    feat(component-compare): add DiffModeProvider/useDiffMode primitive

--- components/ui/component-compare/component-compare/index.ts
+++ components/ui/component-compare/component-compare/index.ts
@@ -1,2 +1,4 @@
 export { ComponentCompare } from './component-compare';
 export type { APIDiffResult, APIDiffChange, APIDiffDetail } from './component-compare';
+export { DiffModeProvider, useDiffMode } from './diff-mode-context';
+export type { DiffDisplayMode } from './diff-mode-context';
