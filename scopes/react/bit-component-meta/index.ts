9af855be8803ea66955b04c9aa6a100fcc07851b
David First <davidnet@gmail.com>
2026-05-15 12:28:32 -0400

    chore: remove bit-component-meta from workspace, pin to 0.0.43

--- scopes/react/bit-component-meta/index.ts
+++
@@ -1,4 +0,0 @@
-export { hasComponentMeta, componentMetaField, componentMetaProperties } from './component-meta.js';
-export type { ComponentMeta, ComponentMetaHolder, ReactComponentMetaHolder } from './component-meta.js';
-
-export { MockedComponentWithMeta } from './component-meta.mock.js';
