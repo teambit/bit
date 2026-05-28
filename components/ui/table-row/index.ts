df33b3daf3cbdaaa25b3e61545921515adc06ed0
Luv Kapur <luv@bit.dev>
2026-05-28 15:58:27 -0400

    eject api ref comps

--- components/ui/table-row/index.ts
+++
@@ -1,5 +0,0 @@
-export { TableRow } from './table-row';
-export type { TableRowProps, RowType } from './table-row';
-export { DefaultValueCell } from './default-value-cell';
-export type { DefaultValueCellProps } from './default-value-cell';
-export { formatSourceValue } from './format-source-value';
