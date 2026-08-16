90edb4c59968bb996dc670b52ce3ac6036c2eeed
Gilad Shoham <shoham.gilad@gmail.com>
2026-08-16 13:24:28 +0300

    docs(webpack): deprecate the core aspect's bundler/dev-server surface
    react/node/aspect envs now build with @teambit/webpack.webpack-bundler and
    @teambit/webpack.webpack-dev-server directly (#10610), not this aspect - see
    https://bit.cloud/teambit/webpack/~change-requests/decouple-webpack-bundler-from-webpack-aspect.
    
    Marks WebpackMain.createBundler/createDevServer/mergeConfig, the
    WebpackBundler/WebpackDevServer classes, and the rest of the public exports
    as @deprecated so anyone still importing from @teambit/webpack gets a signal
    to move to the standalone packages instead. No behavior change - a follow-up
    will delete the deprecated code once callers have migrated.

--- scopes/webpack/webpack/plugins/webpack-bit-reporter-plugin.ts
+++ scopes/webpack/webpack/plugins/webpack-bit-reporter-plugin.ts
@@ -1,1 +1,5 @@
+/**
+ * @deprecated thin re-export kept for backward-compatible `from '@teambit/webpack'` imports;
+ * import directly from `@teambit/webpack.plugins.webpack-bit-reporter-plugin` instead.
+ */
 export { WebpackBitReporterPlugin } from '@teambit/webpack.plugins.webpack-bit-reporter-plugin';
