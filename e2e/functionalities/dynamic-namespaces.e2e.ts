034bc3fc83b752cb8c13318362c2bb6c17959813
David First <davidnet@gmail.com>
2026-08-24 11:41:39 -0400

    test(e2e): trim dynamic-namespaces e2e tests

--- e2e/functionalities/dynamic-namespaces.e2e.ts
+++ e2e/functionalities/dynamic-namespaces.e2e.ts
@@ -15,53 +15,44 @@ describe('dynamic namespaces', function () {
     helper.scopeHelper.destroy();
   });
   const veryLongName = 'this/is/a/very/large/name/for/a/component';
-  const veryShortName = 'short';
-  [veryLongName, veryShortName].forEach((componentName) => {
-    describe(`long and short namespaces. using name "${componentName}"`, () => {
-      let tagOutput;
-      let catComp;
+  describe(`multi-level namespace. using name "${veryLongName}"`, () => {
+    const componentName = veryLongName;
+    let catComp;
 
+    before(() => {
+      helper.scopeHelper.setWorkspaceWithRemoteScope();
+      helper.fs.createFile('bar', 'foo.js');
+      const addOutput = helper.command.addComponent('bar', { i: componentName });
+      expect(addOutput).to.have.string('added');
+      helper.command.tagAllWithoutBuild();
+      catComp = helper.command.catComponent(componentName);
+    });
+    it('should save the component correctly on the model', () => {
+      expect(catComp.name).to.equal(componentName);
+    });
+    it('bit status should show the component as staged', () => {
+      const statusOutput = helper.command.runCmd('bit status');
+      expect(statusOutput).to.have.string('staged');
+      expect(statusOutput).to.have.string(componentName);
+    });
+    it('bit list should list the component', () => {
+      const listOutput = helper.command.listLocalScope();
+      expect(listOutput).to.have.string(componentName);
+    });
+    it('bit show should show the component with the correct name', () => {
+      const showOutput = helper.command.showComponentParsed(componentName);
+      expect(showOutput.name).to.equal(componentName);
+    });
+    describe('after import', () => {
       before(() => {
-        helper.scopeHelper.setWorkspaceWithRemoteScope();
-        helper.fs.createFile('bar', 'foo.js');
-        const addOutput = helper.command.addComponent('bar', { i: componentName });
-        expect(addOutput).to.have.string('added');
-        tagOutput = helper.command.tagAllWithoutBuild();
-        catComp = helper.command.catComponent(componentName);
+        helper.command.export();
+        helper.scopeHelper.reInitWorkspace();
+        helper.scopeHelper.addRemoteScope();
+        helper.command.importComponent(componentName);
       });
-      it('should be tagged successfully', () => {
-        expect(tagOutput).to.have.string('tagged');
-      });
-      it('should save the component correctly on the model', () => {
-        expect(catComp.name).to.equal(componentName);
-      });
-      it('should not save anything to the box field', () => {
-        expect(catComp).to.not.have.property('box');
-      });
-      it('bit status should show the component as staged', () => {
-        const statusOutput = helper.command.runCmd('bit status');
-        expect(statusOutput).to.have.string('staged');
-        expect(statusOutput).to.have.string(componentName);
-      });
-      it('bit list should list the component', () => {
-        const listOutput = helper.command.listLocalScope();
-        expect(listOutput).to.have.string(componentName);
-      });
-      it('bit show should show the component with the correct name', () => {
-        const showOutput = helper.command.showComponentParsed(componentName);
-        expect(showOutput.name).to.equal(componentName);
-      });
-      describe('after import', () => {
-        before(() => {
-          helper.command.export();
-          helper.scopeHelper.reInitWorkspace();
-          helper.scopeHelper.addRemoteScope();
-          helper.command.importComponent(componentName);
-        });
-        it('should create the directories according to the multiple namespaces', () => {
-          expect(path.join(helper.scopes.localPath, helper.scopes.remote, componentName)).to.be.a.path();
-          expect(path.join(helper.scopes.localPath, helper.scopes.remote, componentName, 'foo.js')).to.be.a.file();
-        });
+      it('should create the directories according to the multiple namespaces', () => {
+        expect(path.join(helper.scopes.localPath, helper.scopes.remote, componentName)).to.be.a.path();
+        expect(path.join(helper.scopes.localPath, helper.scopes.remote, componentName, 'foo.js')).to.be.a.file();
       });
     });
   });
