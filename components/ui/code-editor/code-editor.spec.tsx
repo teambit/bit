9546b02e69b1fb4702f91364b833d7ce4a1708f2
Luv Kapur <luv@bit.dev>
2026-09-15 14:08:56 -0400

    test(ui): use node server renderer in capsules

--- components/ui/code-editor/code-editor.spec.tsx
+++ components/ui/code-editor/code-editor.spec.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { renderToStaticMarkup } from 'react-dom/server';
+import { renderToStaticMarkup } from 'react-dom/server.node';
 import { CodeEditor } from './code-editor';
 import { CodeEditorProvider } from './code-editor.provider';
 
--- components/ui/diff-viewer/diff-viewer.spec.tsx
+++ components/ui/diff-viewer/diff-viewer.spec.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { renderToStaticMarkup } from 'react-dom/server';
+import { renderToStaticMarkup } from 'react-dom/server.node';
 import { DiffViewer } from './diff-viewer';
 
 jest.mock('./highlighter', () => ({
